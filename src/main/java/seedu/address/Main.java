854181adabb57a84bd3a51f33cf2298a80d2215e
Jonathan Tay <jonathantaymy@gmail.com>
2022-09-08 11:11:28 +0800

    Add launch console log

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -20,6 +20,7 @@ import javafx.application.Application;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Launching...");
         Application.launch(MainApp.class, args);
     }
 }
