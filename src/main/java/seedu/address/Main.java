4856ec53660bf13871d085f4550c7b496aaa4f7d
Chee Jer Hong <e0407708@u.nus.edu>
2022-09-12 22:59:48 +0800

    Edit dummy commit

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -17,8 +17,6 @@ import javafx.application.Application;
  *
  * By having a separate main class (Main) that doesn't extend Application
  * to be the entry point of the application, we avoid this issue.
- *
- * 
  */
 public class Main {
     public static void main(String[] args) {
