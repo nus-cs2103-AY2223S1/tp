9ae96b5b33779dae131d7cddf2a4cef0abdef5cb
Fuzanwenn <e0902049@u.nus.edu>
2022-09-13 21:19:19 +0800

    Add a new command.

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a Remark object.
+     *
+     * @param remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -16,6 +16,8 @@ public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
 
+    public final Person person;
+
     @FXML
     private Label remark;
 
@@ -26,9 +28,7 @@ public class PersonCard extends UiPart<Region> {
      *
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
-
-    public final Person person;
-
+    
     @FXML
     private HBox cardPane;
     @FXML
