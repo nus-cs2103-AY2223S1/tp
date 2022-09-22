8501e3feef9e200082b66d9a23f8c123cac4938a
JustinS00 <justinsiaw00@gmail.com>
2022-09-22 22:50:14 +0800

    Update CategoryParser

--- src/main/java/seedu/address/logic/parser/CategoryParser.java
+++ src/main/java/seedu/address/logic/parser/CategoryParser.java
@@ -3,11 +3,18 @@ package seedu.address.logic.parser;
 
 import seedu.address.model.person.Category;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
+
 /**
  * Parses input arguments
  */
 public class CategoryParser {
 
+
+    private static final String CATEGORY_PATTERN = PREFIX_CATEGORY.getPrefix() + "*";
     /**
      * Parses the given {@code String} of arguments in the context of categories available
      * and returns a Category
@@ -15,21 +22,30 @@ public class CategoryParser {
      */
     public static Category parse(String keyword) throws ClassNotFoundException {
         keyword = keyword.toLowerCase().trim();
-        switch (keyword) {
-        case "name":
-        case "n":
-            return Category.NAME;
-        case "address":
-        case "a":
-            return Category.ADDRESS;
-        case "email":
-        case "e":
-            return Category.EMAIL;
-        case "phone":
-        case "p":
-            return Category.PHONE;
-        default:
-            throw new ClassNotFoundException();
+        Pattern p = Pattern.compile(PREFIX_CATEGORY.getPrefix() + "(.+)");
+        Matcher m = p.matcher(keyword);
+        if (m.matches()) {
+            keyword = keyword.replace(PREFIX_CATEGORY.getPrefix(), "");
+            switch (keyword) {
+            case "name":
+            case "n":
+                return Category.NAME;
+            case "address":
+            case "a":
+                return Category.ADDRESS;
+            case "email":
+            case "e":
+                return Category.EMAIL;
+            case "phone":
+            case "p":
+                return Category.PHONE;
+            case "tag":
+            case "t":
+                return Category.TAG;
+            default:
+                throw new ClassNotFoundException();
+            }
         }
+        throw new ClassNotFoundException();
     }
 }
