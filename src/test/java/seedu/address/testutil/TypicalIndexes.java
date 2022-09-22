e41c51865582fc3420133cb845f26a197ab7028f
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-09-22 23:13:14 +0800

    Update tests related to delete command

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -1,5 +1,7 @@
 package seedu.address.testutil;
 
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
 
 /**
@@ -9,4 +11,9 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PERSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PERSON = Index.fromOneBased(3);
+    public static final Set<Index> INDEX_SET_FIRST_PERSON = Set.of(Index.fromOneBased(1));
+    public static final Set<Index> INDEX_SET_FIRST_AND_SECOND_PERSON =
+            Set.of(Index.fromOneBased(1), Index.fromOneBased(2));
+    public static final Set<Index> INDEX_SET_SECOND_AND_FIRST_PERSON =
+            Set.of(Index.fromOneBased(2), Index.fromOneBased(1));
 }
