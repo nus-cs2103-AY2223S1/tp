8a27f5ff12c107c4a687ffc1de1abbc50b102e03
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-09-22 13:31:56 +0800

    Add the `tagSet` attribute to `AddressBook`
    As per the user guide, we require users to create tags before they can be used.
    
    This necessitates storing the created tags in `AddressBook`.
    
    We further encapsulate the set of tags in its own class to provide helper methods like `asUnmodifiableSet`, and to disallow `null` tags.

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -2,11 +2,16 @@ package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.TagSet;
 
 /**
  * Wraps all data at the address-book level
@@ -15,6 +20,8 @@ import seedu.address.model.person.UniquePersonList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+
+    private final TagSet tags;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -25,6 +32,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     {
         persons = new UniquePersonList();
+        tags = new TagSet();
     }
 
     public AddressBook() {}
@@ -47,6 +55,12 @@ public class AddressBook implements ReadOnlyAddressBook {
         this.persons.setPersons(persons);
     }
 
+    public void setTags(Set<Tag> tags) {
+        for (Tag tag: tags) {
+            this.tags.add(tag);
+        }
+    }
+
     /**
      * Resets the existing data of this {@code AddressBook} with {@code newData}.
      */
@@ -54,6 +68,7 @@ public class AddressBook implements ReadOnlyAddressBook {
         requireNonNull(newData);
 
         setPersons(newData.getPersonList());
+        setTags(newData.getTags());
     }
 
     //// person-level operations
@@ -93,6 +108,33 @@ public class AddressBook implements ReadOnlyAddressBook {
         persons.remove(key);
     }
 
+    /**
+     * Removes the given tags from the person
+     * {@code tagsToRemove} must already be tagged to the person.
+     */
+    public void removeTags(Person target, Collection<Tag> tagsToRemove) {
+        Set<Tag> newTags = new HashSet<>(target.getTags());
+        newTags.removeAll(tagsToRemove);
+
+        Person untaggedPerson = new Person(
+                target.getName(), target.getPhone(), target.getEmail(),
+                target.getAddress(), newTags);
+        persons.setPerson(target, untaggedPerson);
+    }
+
+    //// tag operations
+
+
+    public boolean hasTag(Tag tag) {
+        requireNonNull(tag);
+        return tags.contains(tag);
+    }
+
+    public void createTag(Tag tag) {
+        requireNonNull(tag);
+        tags.add(tag);
+    }
+
     //// util methods
 
     @Override
@@ -104,6 +146,14 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public ObservableList<Person> getPersonList() {
         return persons.asUnmodifiableObservableList();
+    }
+
+    /**
+     * Returns the set of tags as an unmodifiable {@code UnmodifiableSet}.
+     */
+    @Override
+    public Set<Tag> getTags() {
+        return tags.asUnmodifiableSet();
     }
 
     @Override
