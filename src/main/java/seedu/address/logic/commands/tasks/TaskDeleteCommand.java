06a8830f8e4421d15570b9201a194bba9e22ccd7
Jin Wei <seetohjinwei@gmail.com>
2022-09-23 16:07:34 +0800

    Add skeleton for add, delete, list task commands

---
+++ src/main/java/seedu/address/logic/commands/tasks/TaskDeleteCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands.tasks;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Deletes a task identified using it's displayed index from the task book.
+ */
+public class TaskDeleteCommand extends Command {
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException("Task delete command not implemented yet.");
+    }
+}
---
+++ src/main/java/seedu/address/logic/commands/tasks/TaskListCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands.tasks;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Lists all tasks in the task book to the user.
+ */
+public class TaskListCommand extends Command {
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException("Task list command not implemented yet.");
+    }
+}
