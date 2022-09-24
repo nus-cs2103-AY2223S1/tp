b4bc496bce3534e18fbb39dbbd6a774ecbd79ab4
Bill <xiaobill8@gmail.com>
2022-09-24 15:04:11 +0800

    Fix minor issues

--- src/main/java/seedu/address/model/task/exceptions/DuplicateTaskException.java
+++ src/main/java/seedu/address/model/task/exceptions/DuplicateTaskException.java
@@ -2,7 +2,7 @@ package seedu.address.model.task.exceptions;
 
 /**
  * Signals that the operation will result in duplicate Tasks
- * Persons are considered duplicates if they have the same // TODO.
+ * Tasks are considered duplicates if they have the same // TODO.
  */
 public class DuplicateTaskException extends RuntimeException {
     public DuplicateTaskException() {
