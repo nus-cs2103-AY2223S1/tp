48ad8279e3330fee847d7b970d2662ffc385b8ca
chao890 <chaoyungchor@gmail.com>
2022-09-27 22:23:18 +0800

    Revert "Added Remark feature"
    This reverts commit 3081d4476934b01b5a7bff30be7a25f32a57525f.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,8 +39,6 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
-    private Label remark;
-    @FXML
     private FlowPane tags;
 
     /**
@@ -57,7 +55,6 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
-        remark.setText(person.getRemark().value);
     }
 
     @Override
--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -10,7 +10,7 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
-import seedu.address.model.person.Remark;
+
 /**
  * A utility class to help with building Person objects.
  */
@@ -20,13 +20,12 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_REMARK = "She likes aardvarks.";
+
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
     private Set<Tag> tags;
-    private Remark remark;
 
     /**
      * Creates a {@code PersonBuilder} with the default details.
@@ -37,7 +36,6 @@ public class PersonBuilder {
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
         tags = new HashSet<>();
-        remark = new Remark(DEFAULT_REMARK);
     }
 
     /**
@@ -49,7 +47,6 @@ public class PersonBuilder {
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
         tags = new HashSet<>(personToCopy.getTags());
-        remark = personToCopy.getRemark();
     }
 
     /**
@@ -92,16 +89,8 @@ public class PersonBuilder {
         return this;
     }
 
-    /**
-     * Sets the {@code Email} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withRemark(String remark) {
-        this.remark = new Remark(remark);
-        return this;
-    }
-
     public Person build() {
-        return new Person(name, phone, email, address, tags, remark);
+        return new Person(name, phone, email, address, tags);
     }
 
 }
