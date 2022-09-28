8f3ebb84bedbaeca739a413e7e7db400e025717b
Justin Cheng <justcheng2000@gmail.com>
2022-09-28 14:25:41 +0800

    hi

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -60,10 +60,10 @@ public class PersonBuilder {
     /**
      * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
      */
-    public PersonBuilder withTags(String ... tags) {
-        this.tags = SampleDataUtil.getTagSet(tags);
-        return this;
-    }
+//    public PersonBuilder withTags(String ... tags) {
+//        this.tags = SampleDataUtil.getTagSet(tags);
+//        return this;
+//    }
 
     /**
      * Sets the {@code Address} of the {@code Person} that we are building.
