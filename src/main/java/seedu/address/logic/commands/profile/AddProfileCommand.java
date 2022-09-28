4dca82e366bdfaa449d2f0562b85f74d194af869
Shashank Acharya <acharya.s@icloud.com>
2022-09-28 17:59:36 +0800

    Add AddProfileCommand with parser
    Follows the same syntax as the `add` command from v1.1.

---
+++ src/main/java/seedu/address/logic/commands/profile/AddProfileCommand.java
@@ -0,0 +1,65 @@
+package seedu.address.logic.commands.profile;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.profile.Profile;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+/**
+ * Adds a profile to the address book.
+ */
+public class AddProfileCommand extends ProfileCommand {
+
+    public static final String COMMAND_OPTION = "a";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " " + PREFIX_OPTION + COMMAND_OPTION
+            + ": Adds a profile to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_TAG + "friends "
+            + PREFIX_TAG + "owesMoney";
+
+    public static final String MESSAGE_SUCCESS = "New profile added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PROFILE = "This profile already exists in the address book";
+
+    private final Profile toAdd;
+
+    /**
+     * Creates an AddProfileCommand to add the specified {@code Profile}
+     */
+    public AddProfileCommand(Profile profile) {
+        requireNonNull(profile);
+        toAdd = profile;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasProfile(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PROFILE);
+        }
+
+        model.addProfile(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddProfileCommand // instanceof handles nulls
+                && toAdd.equals(((AddProfileCommand) other).toAdd));
+    }
+}
