a3e46c0ea0b27bbf921b3ec1d6c302a265276e1c
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-09-28 23:07:24 +0800

    Reminder Command: Add ReminderCommand.java

---
+++ src/main/java/seedu/address/logic/commands/ReminderCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Gets reminders of all upcoming birthdays for the user.
+ */
+public class ReminderCommand extends Command {
+    public static final String COMMAND_WORD = "remind";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Gets remind for upcoming birthdays.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Remind command not implemented yet!";
+
+    public static final String SHOWING_REMINDER_MESSAGE = "Displaying reminders.";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return new CommandResult(SHOWING_REMINDER_MESSAGE, true);
+    }
+}
