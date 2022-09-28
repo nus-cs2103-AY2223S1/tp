bbcf33e475c58524facb697cb2dbf570c8a19a91
Tjan Eng Ger, Kevin <kevintjan27@gmail.com>
2022-09-28 21:00:10 +0800

    Update javadocs part 4

--- src/main/java/seedu/address/ui/TaskListCard.java
+++ src/main/java/seedu/address/ui/TaskListCard.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Region;
 import seedu.address.model.task.Task;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code TaskList}.
  */
 public class TaskListCard extends UiPart<Region> {
 
