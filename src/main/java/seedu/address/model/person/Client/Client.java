46d182b76c2d0209abfb9d366b7dc9fb349ae5bd
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-09-29 11:56:55 +0800

    Add client class extending Person class
    A Client object has been designed after a Person object such that:
    - String of client name is the only mandatory input
    - User input, if given, used for other fields
    - If not, default values used to construct Client

---
+++ src/main/java/seedu/address/model/person/Client/Client.java
@@ -0,0 +1,65 @@
+package seedu.address.model.person.Client;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+
+public class Client extends Person {
+
+    /**
+     * Constructs a client with name given by user and the rest of the fields set to default values.
+     * @param name String representing name of the client
+     */
+    public Client(String name){
+        super(new Name(name), new Phone("000"), new Email("notSpecified@gmail.com"), new Address("notSpecified"), Set.of(new Tag("notSpecified")));
+    }
+
+    /**
+     * Constructs a client with name, phone number given by user and the rest of the fields set to default values.
+     * @param name String representing name of the client
+     * @param phone String representing phone number of the client
+     */
+    public Client(String name, String phone){
+        super(new Name(name), new Phone(phone), new Email("notSpecified@gmail.com"), new Address("notSpecified"), Set.of(new Tag("notSpecified")));
+    }
+
+    /**
+     * Constructs a client with name, phone number, email given by user and the rest of the fields set to default
+     * values.
+     * @param name String representing name of the client
+     * @param phone String representing phone number of the client
+     * @param email String representing email address of the client
+     */
+    public Client(String name, String phone, String email){
+        super(new Name(name), new Phone(phone), new Email(email), new Address("notSpecified"), Set.of(new Tag("notSpecified")));
+    }
+
+    /**
+     * Constructs a client with name, phone number, email, address given by user and the rest of the fields set to
+     * default values.
+     * @param name String representing name of the client
+     * @param phone String representing phone number of the client
+     * @param email String representing email address of the client
+     * @param address String representing location of the client
+     */
+    public Client(String name, String phone, String email, String address){
+        super(new Name(name), new Phone(phone), new Email(email), new Address(address), Set.of(new Tag("notSpecified")));
+    }
+
+    /**
+     * Constructs a client with inputs given by the user.
+     * @param name String representing name of the client
+     * @param phone String representing phone number of the client
+     * @param email String representing email address of the client
+     * @param address String representing location of the client
+     * @param tag String representing the tag associated with the client
+     */
+    public Client(String name, String phone, String email, String address, String tag){
+        super(new Name(name), new Phone(phone), new Email(email), new Address(address), Set.of(new Tag(tag)));
+    }
+}
