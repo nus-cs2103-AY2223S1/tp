a8004511bf10c49f98ecdf91b286ba366e0b8d23
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-09-29 14:29:45 +0800

    Reminder Command: Add ReminderCard.java and ReminderListPanel.java

---
+++ src/main/java/seedu/address/ui/ReminderCard.java
@@ -0,0 +1,61 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Person;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class ReminderCard extends UiPart<Region> {
+
+    private static final String FXML = "ReminderListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Person person;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label birthday;
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public ReminderCard(Person person, int displayedIndex) {
+        super(FXML);
+        this.person = person;
+        name.setText(person.getName().fullName);
+        birthday.setText(person.getAddress().value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof PersonCard)) {
+            return false;
+        }
+
+        // state check
+        ReminderCard card = (ReminderCard) other;
+        return id.getText().equals(card.id.getText())
+                && person.equals(card.person);
+    }
+}
---
+++ src/main/java/seedu/address/ui/ReminderListPanel.java
@@ -0,0 +1,49 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.person.Person;
+
+/**
+ * Panel containing the list of persons.
+ */
+public class ReminderListPanel extends UiPart<Region> {
+    private static final String FXML = "ReminderListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(ReminderListPanel.class);
+
+    @FXML
+    private ListView<Person> reminderListView;
+
+    /**
+     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     */
+    public ReminderListPanel(ObservableList<Person> personList) {
+        super(FXML);
+        reminderListView.setItems(personList);
+        reminderListView.setCellFactory(listView -> new ReminderListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class ReminderListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new ReminderCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
