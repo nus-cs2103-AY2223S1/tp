1f1b88f969337704956973347729118939c968a7
Daniel Luo <hurinturin123@gmail.com>
2022-09-29 21:48:22 +0800

    Add new fields and methods in Meeting class Allows user to set description, location, date&time for their meeting Enables the input date&time from the user to be validated to be in the correct format.

--- src/main/java/seedu/address/model/meeting/Meeting.java
+++ src/main/java/seedu/address/model/meeting/Meeting.java
@@ -1,12 +1,65 @@
 package seedu.address.model.meeting;
 
-import seedu.address.model.person.Person;
+import java.text.ParseException;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.Locale;
 
+import seedu.address.model.person.Person;
+import seedu.address.model.util.DateTimeProcessor;
+
+/**
+ * Class for a new Meeting
+ */
 public class Meeting {
 
-    private Person person;
+    private final Person person;
+    private String description;
+    private String location;
+    private String meetingDateAndTime;
 
-    public Meeting (Person person) {
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.US)
+        .withResolverStyle(ResolverStyle.SMART);
+    private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm", Locale.US)
+        .withResolverStyle(ResolverStyle.SMART);
+    private final DateTimeProcessor validator = new DateTimeProcessor(dateFormatter, timeFormatter);
+
+    /**
+     * Constructor for a new Meeting
+     *
+     * @param person the person whom the user is meeting with
+     */
+    public Meeting(Person person) {
         this.person = person;
     }
+
+    /**
+     * sets the location of the meeting
+     *
+     * @param location of the meeting
+     */
+    public void setMeetingLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * sets the description of the meeting
+     *
+     * @param description of the meeting
+     */
+    public void setMeetingDescription(String description) {
+        this.description = description;
+    }
+
+    // might want to check for parseException earlier tho
+    /**
+     * sets the date and time of the meeting
+     *
+     * @param dateAndTime of the meeting
+     * @throws ParseException when the dateAndTime is in the wrong format
+     */
+    public void setMeetingDateAndTime(String dateAndTime) throws ParseException {
+        this.meetingDateAndTime = validator.processDateTime(dateAndTime);
+    }
+
 }
---
+++ src/main/java/seedu/address/model/util/DateTimeProcessor.java
@@ -0,0 +1,114 @@
+package seedu.address.model.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * Checks whether the format of a due is in valid date-time format
+ * in yyyy-MM-dd HHmm
+ */
+public class DateTimeProcessor {
+
+    private final DateTimeFormatter dateFormatter;
+    private final DateTimeFormatter timeFormatter;
+
+    /**
+     * Constructor for a new DateValidator object
+     *
+     * @param dateFormatter determines the accepted date&time format of the input string
+     */
+    public DateTimeProcessor(DateTimeFormatter dateFormatter, DateTimeFormatter timeFormatter) {
+        this.dateFormatter = dateFormatter;
+        this.timeFormatter = timeFormatter;
+    }
+
+    /**
+     * Checks if the input string matches the accepted date format of the formatter
+     *
+     * @return Whether date format is valid
+     */
+    private boolean isDateValid(String dateStr) {
+        try {
+            this.dateFormatter.parse(dateStr);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the input string matches the accepted time format of the formatter
+     *
+     * @return Whether time format is valid
+     */
+    private boolean isTimeValid(String timeStr) {
+        try {
+            this.timeFormatter.parse(timeStr);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * converts input date&time from yyyy-MM-dd HHmm to MONTH dd yyyy hh:mm aa format
+     *
+     * @param dateAndTime date and time of meeting
+     * @return date and time in MONTH dd yyyy hh:mm aa format
+     */
+    public String processDateTime(String dateAndTime) throws ParseException {
+        String[] tempStringArray = dateAndTime.strip().split("\\s+", 2);
+
+        String dueDate = tempStringArray[0];
+        String dueTime = tempStringArray.length < 2 ? "" : tempStringArray[1];
+
+        if (!isDateValid(dueDate)) {
+            throw new ParseException("Meeting date is not in yyyy-MM-dd format", 0);
+        }
+
+        LocalDate inputDue = LocalDate.parse(dueDate);
+        String year = String.valueOf(inputDue.getYear());
+        String month = String.valueOf(inputDue.getMonth());
+        String date = String.valueOf(inputDue.getDayOfMonth());
+
+        //time pattern of input date in 24 hour format -- HH for 24h, hh for 12h
+        DateFormat inputTimeFormat = new SimpleDateFormat("HHmm");
+        inputTimeFormat.setLenient(false);
+
+        //Date/time pattern of desired output date
+        DateFormat outputTimeFormat = new SimpleDateFormat("hh:mm aa"); // aa for AM/ PM
+
+        if (Objects.equals(dueTime, "")) {
+            return month + " " + date + " " + year;
+
+        } else if (!isTimeValid(dueTime)) {
+            throw new ParseException("Meeting date is not in HHmm format", 0);
+        }
+
+        Date inputTime = inputTimeFormat.parse(dueTime);
+        String outputTime = outputTimeFormat.format(inputTime);
+        return month + " " + date + " " + year + " " + outputTime;
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.US)
+            .withResolverStyle(ResolverStyle.SMART);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm", Locale.US)
+            .withResolverStyle(ResolverStyle.SMART);
+        DateTimeProcessor validator = new DateTimeProcessor(dateFormatter, timeFormatter);
+        try {
+            validator.processDateTime("asdasd");
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+    }
+
+}
