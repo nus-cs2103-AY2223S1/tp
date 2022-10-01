8a12af88d69864af5d91e49d4614f6bfd67f7052
MuhdMusab <musab653@gmail.com>
2022-10-01 22:36:16 +0800

    Add LocalDateTime parsing function

--- src/main/java/seedu/address/logic/parser/DateTimeParser.java
+++ src/main/java/seedu/address/logic/parser/DateTimeParser.java
@@ -1,4 +1,25 @@
 package seedu.address.logic.parser;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Parses string representation of a date and time and creates
+ * a new LocalDateTime object
+ */
 public class DateTimeParser {
+    private static final String DATE_TIME_FORMAT = "d-MMM-yyyy hh:mm a";
+    private static final DateTimeFormatter dateTimeFormatter = java.time.format
+            .DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+
+    /**
+     * Parses the given {@code String} representing a date and time
+     * and returns a LocalDateTime object.
+     *
+     * @param str String representing the LocalDateTime to be returned.
+     * @return LocalDateTime parsed from the input String.
+     */
+    public LocalDateTime getLocalDateTimeFromString(String str) {
+        return LocalDateTime.parse(str, DateTimeParser.dateTimeFormatter);
+    }
 }
