7e9fb42100d2704460302a1dd05f5356ae067829
seanflyyy <seanerwenhan@gmail.com>
2022-10-01 11:06:24 +0800

    Add ScheduleListPanel to MainWindow.java
    Follow up: Replace scheduleListPanel with getSchedule() method call on line 119 of MainWindow.java

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -36,6 +36,8 @@ public class MainWindow extends UiPart<Stage> {
     private HelpWindow helpWindow;
     private StatisticsWindow statisticsWindow;
 
+    private ScheduleListPanel scheduleListPanel;
+
     @FXML
     private StackPane commandBoxPlaceholder;
 
@@ -45,6 +47,8 @@ public class MainWindow extends UiPart<Stage> {
     @FXML
     private StackPane personListPanelPlaceholder;
 
+    @FXML
+    private StackPane scheduleListPanelPlaceholder;
     @FXML
     private StackPane resultDisplayPlaceholder;
 
@@ -114,6 +118,10 @@ public class MainWindow extends UiPart<Stage> {
     void fillInnerParts() {
         personListPanel = new PersonListPanel(logic.getFilteredPersonList());
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+
+        // replace this with getSchedule() method to be implemented
+        scheduleListPanel = new ScheduleListPanel(logic.getFilteredPersonList());
+        scheduleListPanelPlaceholder.getChildren().add(scheduleListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
