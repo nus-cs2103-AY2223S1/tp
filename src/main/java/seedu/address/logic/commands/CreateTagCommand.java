58d055c5e011e956a0bc9e530ce1fcd18583b8d8
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-10-01 20:57:10 +0800

    Refactor code to conform to style guide

--- src/main/java/seedu/address/logic/commands/CreateTagCommand.java
+++ src/main/java/seedu/address/logic/commands/CreateTagCommand.java
@@ -52,10 +52,12 @@ public class CreateTagCommand extends TagCommandGroup {
             }
         }
 
-        String duplicateMessage = duplicateTags.isEmpty() ?
-                "" : String.format(MESSAGE_DUPLICATE_TAGS, Tag.toString(duplicateTags)) + "\n";
-        String successMessage = nonDuplicateTags.isEmpty() ?
-                "" : String.format(MESSAGE_SUCCESS, Tag.toString(nonDuplicateTags));
+        String duplicateMessage = duplicateTags.isEmpty()
+                ? ""
+                : String.format(MESSAGE_DUPLICATE_TAGS, Tag.toString(duplicateTags)) + "\n";
+        String successMessage = nonDuplicateTags.isEmpty()
+                ? ""
+                : String.format(MESSAGE_SUCCESS, Tag.toString(nonDuplicateTags));
 
         return new CommandResult(duplicateMessage + successMessage);
     }
