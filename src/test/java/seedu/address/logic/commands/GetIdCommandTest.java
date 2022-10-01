387ba30ce563352d8c625e18833ba11cf96f9389
sjoann <joansim19@gmail.com>
2022-10-01 12:30:09 +0800

    Add test cases for GetIdCommand

---
+++ src/test/java/seedu/address/logic/commands/GetIdCommandTest.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class GetIdCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_indexFound() {
+        String expectedMessage = String.format(GetIdCommand.GET_ID_SUCCESS, 1);
+        GetIdCommand command = new GetIdCommand("Alice Pauline");
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+
+        command = new GetIdCommand("alice pauline");
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_indexNotFound() {
+        String expectedMessage = String.format(GetIdCommand.GET_ID_FAILURE);
+        GetIdCommand command = new GetIdCommand("Alice");
+        assertCommandFailure(command, model, expectedMessage);
+    }
+
+    @Test
+    public void execute_missingNameParameter() {
+        String expectedMessage = String.format(GetIdCommand.MISSING_NAME_EXCEPTION);
+        GetIdCommand command = new GetIdCommand("");
+        assertCommandFailure(command, model, expectedMessage);
+    }
+
+}
