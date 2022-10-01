3c05321899cc60e76d0c2ec072a933c1c154c7e6
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2022-10-01 11:46:03 +0800

    Fix checkstyle errors 2

--- src/main/java/seedu/address/logic/parser/AddAddressCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAddressCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
