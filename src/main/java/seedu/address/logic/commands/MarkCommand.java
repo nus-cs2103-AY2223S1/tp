47711994d4f33ecbc895fb4b90547ac4a949f902
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-10-02 14:35:39 +0800

    Add mark command to mark appointments. Currently not savable.

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -20,13 +20,17 @@ public class MarkCommand extends Command {
             + "Parameters: INDEX_OF_PERSON (must be a positive integer) "
             + "INDEX_OF_APPOINTMENT (must be a positive integer)";
 
-    public static final String MESSAGE_MARK_PERSON_SUCCESS = "Marked appointment for Person: %1$s";
+    public static final String MESSAGE_MARK_PERSON_SUCCESS = "Marked appointment %1$s for Person: %2$s";
     public static final String MESSAGE_NOT_MARKED = "All 2 indexes must be provided to mark.";
     public static final String MESSAGE_ALREADY_MARKED = "This appointment is already marked.";
 
     private final Index indexOfPerson;
     private final Index indexOfAppointment;
 
+    /**
+     * @param indexOfPerson index of the person in the filtered person list to mark.
+     * @param indexOfAppointment index of the appointment of the specified person to mark.
+     */
     public MarkCommand(Index indexOfPerson, Index indexOfAppointment) {
         requireNonNull(indexOfPerson);
         requireNonNull(indexOfAppointment);
@@ -45,13 +49,20 @@ public class MarkCommand extends Command {
         }
 
         Person personToMarkFor = lastShownList.get(indexOfPerson.getZeroBased());
-        Appointment appointmentToMark = personToMarkFor.getAppointments().get(indexOfAppointment.getZeroBased());
+        List<Appointment> targetAppointmentList = personToMarkFor.getAppointments();
 
+        if (indexOfAppointment.getZeroBased() >= targetAppointmentList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_APPOINTMENT_DISPLAYED_INDEX);
+        }
+
+        Appointment appointmentToMark = targetAppointmentList.get(indexOfAppointment.getZeroBased());
         if (appointmentToMark.isMarked()) {
             throw new CommandException(MESSAGE_ALREADY_MARKED);
         }
 
         appointmentToMark.mark();
-        return new CommandResult(MESSAGE_MARK_PERSON_SUCCESS);
+        return new CommandResult(String.format(MESSAGE_MARK_PERSON_SUCCESS,
+                indexOfAppointment.getOneBased(),
+                personToMarkFor.getName()));
     }
 }
