e89e769297fb3ae3f496f61af842072d9f0c4c56
see leng <seelengxd@gmail.com>
2022-10-02 13:14:39 +0800

    Logic: add delcom command
    - add delcom command class
    - add delcom parser
    - add delcom to ab parser

---
+++ src/main/java/seedu/address/logic/commands/DeleteCommissionCommand.java
@@ -0,0 +1,53 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.commission.Commission;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Deletes the commission of an existing customer in the ArtBuddy.
+ */
+public class DeleteCommissionCommand extends Command {
+
+    public static final String COMMAND_WORD = "delcom";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the commission identified by the index number used in the displayed commission list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_COMMISSION_SUCCESS = "Deleted Commission: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteCommissionCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Commission> lastShownList = model.getFilteredCommissionList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_COMMISSION_DISPLAYED_INDEX);
+        }
+
+        Commission commissionToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteCommission(commissionToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_COMMISSION_SUCCESS, commissionToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteCommissionCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteCommissionCommand) other).targetIndex)); // state check
+    }
+}
