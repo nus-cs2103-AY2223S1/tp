e142b3d42f9abb0df9cdfcc6f572e90b0bb51675
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-10-02 22:14:18 +0800

    Add ModuleCard
    Previously there was only PersonCard
    
    Let's add module card for our new UI

---
+++ src/main/java/seedu/address/ui/Module.java
@@ -0,0 +1,31 @@
+package seedu.address.ui;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * This is a dummy module used to test the UI
+ */
+
+public class Module {
+
+    public String getModuleCode() {
+        return "CS2103";
+    }
+
+    public String getDepartment() {
+        return "Computer Science";
+    }
+
+    public String getWorkload() {
+        return "4 MCs";
+    }
+
+    public String getTitle() {
+        return "Software Engineering";
+    }
+
+    public ArrayList<String> getSemesterData() {
+        return new ArrayList<String>(Arrays.asList("Semester 1", "Semester 2"));
+    }
+}
---
+++ src/main/java/seedu/address/ui/ModuleCard.java
@@ -0,0 +1,73 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Person;
+
+import java.util.Comparator;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class ModuleCard extends UiPart<Region> {
+
+    private static final String FXML = "PersonListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Module module;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label title;
+    @FXML
+    private Label moduleCode;
+    @FXML
+    private Label department;
+    @FXML
+    private Label workload;
+    @FXML
+    private FlowPane semesterData;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public ModuleCard(Module module) {
+        super(FXML);
+        this.module = module;
+        moduleCode.setText(module.getModuleCode());
+        title.setText(module.getTitle());
+        department.setText(module.getDepartment());
+        workload.setText(module.getWorkload());
+        module.getSemesterData().stream()
+                .forEach(tag -> semesterData.getChildren().add(new Label(tag)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ModuleCard)) {
+            return false;
+        }
+
+        // state check
+        ModuleCard card = (ModuleCard) other;
+        return moduleCode.getText().equals(card.moduleCode.getText())
+                && module.equals(card.module);
+    }
+}
