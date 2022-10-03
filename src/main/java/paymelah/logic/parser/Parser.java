85ff0c271b5dd4c16e0da47e3629a96a22860d3a
tsammeow <97447335+tsammeow@users.noreply.github.com>
2022-10-03 17:38:50 +0800

    Rename package seedu.address to paymelah

---
+++ src/main/java/paymelah/logic/parser/Parser.java
@@ -0,0 +1,16 @@
+package paymelah.logic.parser;
+
+import paymelah.logic.commands.Command;
+import paymelah.logic.parser.exceptions.ParseException;
+
+/**
+ * Represents a Parser that is able to parse user input into a {@code Command} of type {@code T}.
+ */
+public interface Parser<T extends Command> {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    T parse(String userInput) throws ParseException;
+}
---
+++ src/main/java/paymelah/model/person/exceptions/PersonNotFoundException.java
@@ -0,0 +1,6 @@
+package paymelah.model.person.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified person.
+ */
+public class PersonNotFoundException extends RuntimeException {}
