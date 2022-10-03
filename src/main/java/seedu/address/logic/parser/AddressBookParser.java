438773dd881e184e3be72dd58905ad083b2d7b7a
Eugene Tay <tayyjeugene@gmail.com>
2022-10-03 18:54:54 +0800

    Fixed CI issues

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -68,7 +68,7 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-        
+
         case RemarkCommand.COMMAND_WORD:
             return new RemarkCommandParser().parse((arguments));
 
