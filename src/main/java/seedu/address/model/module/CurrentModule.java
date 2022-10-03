94b4da27f00a2f1faca91b982e5a8e870506a17c
rjkoh <koh.ruijie123@gmail.com>
2022-10-03 21:02:08 +0800

    Add javadocs for module classes

--- src/main/java/seedu/address/model/module/CurrentModule.java
+++ src/main/java/seedu/address/model/module/CurrentModule.java
@@ -1,8 +1,16 @@
 package seedu.address.model.module;
 
+/**
+ * Module that CS student is currently taking.
+ */
 public class CurrentModule implements Module {
     private String code;
 
+    /**
+     * Constructor for new CurrentModule instance.
+     *
+     * @param code Module code name e.g. CS2103T.
+     */
     public CurrentModule(String code) {
         this.code = code;
     }
--- src/main/java/seedu/address/model/module/PlannedModule.java
+++ src/main/java/seedu/address/model/module/PlannedModule.java
@@ -1,8 +1,16 @@
 package seedu.address.model.module;
 
+/**
+ * Modules CS student plans to take.
+ */
 public class PlannedModule implements Module {
     private String code;
 
+    /**
+     * Constructor for new PlannedModule instance.
+     *
+     * @param code Module code name e.g. CS2103T.
+     */
     public PlannedModule(String code) {
         this.code = code;
     }
