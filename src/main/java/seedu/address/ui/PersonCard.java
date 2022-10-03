fd376ead196b986833f5efdaecf03af715e3079e
marclzh <uiolk@hotmail.com>
2022-10-04 01:17:12 +0800

    Checkstyle fix

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -8,7 +8,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.RecordList;
 
 /**
  * An UI component that displays information of a {@code Person}.
