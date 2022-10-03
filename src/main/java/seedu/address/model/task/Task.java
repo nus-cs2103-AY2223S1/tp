8438210498a1ed57a9a088cfe6fd3f8a53a9b7c8
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-10-03 19:44:02 +0800

    Add utility methods to Task class

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -2,6 +2,8 @@ package seedu.address.model.task;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.Objects;
+
 /**
  * Represents a Task in the task list.
  * Guarantees: details are present and not null, field values are validated, immutable.
@@ -28,5 +30,49 @@ public class Task {
 
     public Deadline getDeadline() {
         return deadline;
+    }
+
+    public boolean isSameTask(Task other) {
+        if (other == this) {
+            return true;
+        }
+
+        return other != null
+                && other.getName().equals(this.getName())
+                && other.getModule().equals(this.getModule());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Task)) {
+            return false;
+        }
+
+        Task otherTask = (Task) other;
+        return otherTask.getName().equals(getName())
+                && otherTask.getModule().equals(this.getModule())
+                && otherTask.getDeadline().equals(this.getDeadline());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.module, this.deadline);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder("Task: ");
+
+        builder.append(this.getName())
+                .append("; Module: ")
+                .append(this.getModule())
+                .append("; Deadline: ")
+                .append(this.getDeadline());
+
+        return builder.toString();
     }
 }
