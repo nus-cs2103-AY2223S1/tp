13260da263e1bb58754819d7f26edf34cd83b66a
quannam0124 <77949597+quannam0124@users.noreply.github.com>
2022-10-04 01:26:58 +0800

    Implement Person Of Contact class
    JeeqTracker does not have a person of contact for respective
    companies.
    
    Let's implement a Person of Contact class that handles
    all contact information with that company.

---
+++ src/main/java/seedu/address/model/poc/PoC.java
@@ -0,0 +1,124 @@
+package seedu.address.model.poc;
+
+import seedu.address.model.company.Email;
+import seedu.address.model.company.Name;
+import seedu.address.model.company.Phone;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.company.Company;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+public class PoC {
+
+    // Identity fields
+    private final Name name;
+    private final Phone phone;
+    private final Email email;
+    private final Company company;
+
+    // Data fields
+    private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public PoC(Name name, Phone phone, Email email,  Company company, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, tags);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.company = company;
+
+        this.tags.addAll(tags);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both people have the same name.
+     * This defines a weaker notion of equality between two persons of contact.
+     */
+    public boolean isSamePerson(seedu.address.model.poc.PoC otherPerson) {
+        if (otherPerson == this) {
+            return true;
+        }
+
+        return otherPerson != null
+                && otherPerson.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both persons of contact have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons of contact.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof seedu.address.model.poc.PoC)) {
+            return false;
+        }
+
+        seedu.address.model.poc.PoC otherPerson = (seedu.address.model.poc.PoC) other;
+        return otherPerson.getName().equals(getName())
+                && otherPerson.getPhone().equals(getPhone())
+                && otherPerson.getEmail().equals(getEmail())
+                && otherPerson.getCompany().equals(getCompany())
+                && otherPerson.getTags().equals(getTags());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, phone, email, company, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Phone: ")
+                .append(getPhone())
+                .append("; Email: ")
+                .append(getEmail())
+                .append("; Company: ")
+                .append(getCompany());
+
+        Set<Tag> tags = getTags();
+        if (!tags.isEmpty()) {
+            builder.append("; Tags: ");
+            tags.forEach(builder::append);
+        }
+        return builder.toString();
+    }
+
+}
