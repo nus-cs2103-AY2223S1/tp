973a3513446b9e6bb2d62833c1663641156ddb8d
MuhdMusab <musab653@gmail.com>
2022-10-03 11:40:41 +0800

    Create AddAppointmentCommandParser

--- src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
@@ -1,4 +1,76 @@
 package seedu.address.logic.parser;
 
-public class AddAppointmentCommandParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.ArgumentMultimap.arePrefixesPresent;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddAppointmentCommand;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Appointment;
+
+/**
+ * Parses input arguments and creates a new AddAppointmentCommand object
+ */
+public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * AddAppointmentCommand and returns an AddAppointmentCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddAppointmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
+                        PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_DATE);
+
+        if (arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
+                || argMultimap.getPreamble().isEmpty() || !arePrefixesPresent(argMultimap, PREFIX_DATE)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+        parseAppointmentsForEdit(argMultimap.getAllValues(PREFIX_DATE))
+                .ifPresent(editPersonDescriptor::setAppointments);
+
+        if (!editPersonDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
+        }
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    AddAppointmentCommand.MESSAGE_USAGE), pe);
+        }
+
+        return new AddAppointmentCommand(index, editPersonDescriptor);
+    }
+
+    /**
+     * Parses {@code Collection<String> datesAndTimes} into a
+     * {@code Set<Appointment>} if {@code datesAndTimes} is non-empty.
+     */
+    private Optional<Set<Appointment>> parseAppointmentsForEdit(Collection<String> datesAndTimes)
+            throws ParseException {
+        assert datesAndTimes != null;
+
+        if (datesAndTimes.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(ParserUtil.parseAppointments(datesAndTimes));
+    }
 }
