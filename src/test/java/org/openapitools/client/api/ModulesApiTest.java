3e727c2ccc94f3f4dee2b63f7af76d6f2e2e1170
shenyihong <shen_yi_hong@outlook.com>
2022-10-03 22:50:29 +0800

    Add simple test for module api

---
+++ src/test/java/org/openapitools/client/api/ModulesApiTest.java
@@ -0,0 +1,45 @@
+package org.openapitools.client.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.model.Module;
+import org.openapitools.client.model.ModuleCondensed;
+import org.openapitools.client.model.ModuleInformation;
+
+
+/**
+ * API tests for ModulesApi
+ */
+public class ModulesApiTest {
+
+    private final ModulesApi api = new ModulesApi();
+
+    @Test
+    public void acadYearModuleInfoJsonGetTest() throws ApiException {
+        String acadYear = "2022-2023";
+        List<ModuleInformation> response = api.acadYearModuleInfoJsonGet(acadYear);
+
+        assertTrue(response.size() > 0);
+    }
+
+    @Test
+    public void acadYearModuleListJsonGetTest() throws ApiException {
+        String acadYear = "2022-2023";
+        List<ModuleCondensed> response = api.acadYearModuleListJsonGet(acadYear);
+
+        assertTrue(response.size() > 0);
+    }
+
+    @Test
+    public void acadYearModulesModuleCodeJsonGetTest() throws ApiException {
+        String acadYear = "2022-2023";
+        String moduleCode = "CS2103";
+        Module response = api.acadYearModulesModuleCodeJsonGet(acadYear, moduleCode);
+
+        assertEquals(response.getTitle(), "Software Engineering");
+    }
+}
