b1357a4279723395dccd0dade7c7c45fe8866627
Marciano Renzo William <marcianorenzowilliam@gmail.com>
2022-10-04 01:18:14 +0800

    add one scrollpane side-by-side

---
+++ src/main/java/modtrekt/ui/TaskListPanel.java
@@ -0,0 +1,49 @@
+package modtrekt.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import modtrekt.commons.core.LogsCenter;
+import modtrekt.model.person.Person;
+
+/**
+ * Panel containing the list of persons.
+ */
+public class TaskListPanel extends UiPart<Region> {
+    private static final String FXML = "TaskListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+
+    @FXML
+    private ListView<Person> taskListView;
+
+    /**
+     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     */
+    public TaskListPanel(ObservableList<Person> personList) {
+        super(FXML);
+        taskListView.setItems(personList);
+        taskListView.setCellFactory(listView -> new TaskListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class TaskListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new TaskCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
