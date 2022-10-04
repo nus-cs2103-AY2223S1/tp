33d28ba9eb9b9e100686eeed8274c679c64c5757
rjkoh <koh.ruijie123@gmail.com>
2022-10-04 15:39:58 +0800

    Tying add command together currently failing checks due to text files lacking modules and yet to update command format

--- src/main/java/seedu/address/storage/JsonAdaptedPreviousModule.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPreviousModule.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.module.Module;
 import seedu.address.model.module.PreviousModule;
 
 /**
