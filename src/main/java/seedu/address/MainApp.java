6e7ba01e268e74dc3c4c427f3f3bc9d4a8a32d7a
Song Ivan <songivan45@gmail.com>
2022-10-04 15:18:54 +0800

    Update arguments ModelManager take in

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -15,12 +15,7 @@ import seedu.address.commons.util.ConfigUtil;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
 import seedu.address.logic.LogicManager;
-import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.UserPrefs;
+import seedu.address.model.*;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.AddressBookStorage;
 import seedu.address.storage.JsonAddressBookStorage;
@@ -90,7 +85,7 @@ public class MainApp extends Application {
             initialData = new AddressBook();
         }
 
-        return new ModelManager(initialData, userPrefs);
+        return new ModelManager(initialData, new TaskList(), userPrefs);
     }
 
     private void initLogging(Config config) {
