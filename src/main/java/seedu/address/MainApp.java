3c022fe7bdb6362453eaecd31276c38f27e5e8fc
songivan00 <71430722+songivan00@users.noreply.github.com>
2022-10-04 17:36:08 +0800

    Update src/main/java/seedu/address/MainApp.java

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -15,7 +15,6 @@ import seedu.address.commons.util.ConfigUtil;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
 import seedu.address.logic.LogicManager;
-
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
