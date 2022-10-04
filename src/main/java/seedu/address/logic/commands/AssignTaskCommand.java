3dee200de973851181eeb2892518280aa39e01e3
LianGuoYang <e0735382@u.nus.edu>
2022-10-04 20:17:55 +0800

    Remove unnecessary imports

--- src/main/java/seedu/address/logic/commands/AssignTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignTaskCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import javafx.collections.ObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Assignment;
 import seedu.address.model.person.Name;
