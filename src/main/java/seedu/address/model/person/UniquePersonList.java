fe1e67f6a03dc888e719c0c35efb2528086c33af
Eugene-Ong-W-X <e0725338@u.nus.edu>
2022-10-05 04:13:57 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -17,7 +17,8 @@ import seedu.address.model.tag.Tag;
  * A list of persons that enforces uniqueness between its elements and does not allow nulls.
  * A person is considered unique by comparing using {@code Person#isSamePerson(Person)}. As such, adding and updating of
  * persons uses Person#isSamePerson(Person) for equality to ensure that the person being added or updated is
- * unique in terms of identity in the UniquePersonList. However, the removal of a person uses Person#equals(Object) to ensure that the person with exactly the same fields will be removed.
+ * unique in terms of identity in the UniquePersonList. However, the removal of a person uses Person#equals(Object) to
+ * ensure that the person with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
