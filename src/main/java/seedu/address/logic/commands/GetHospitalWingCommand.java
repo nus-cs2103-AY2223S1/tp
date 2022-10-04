beabd273e50b6ef7489f6975a9716e6b0cba4753
PeiYee88 <e0550356@u.nus.edu>
2022-10-04 18:53:22 +0800

    Change header of methods

--- src/main/java/seedu/address/logic/commands/GetHospitalWingCommand.java
+++ src/main/java/seedu/address/logic/commands/GetHospitalWingCommand.java
@@ -7,7 +7,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.HospitalWingContainsKeywordsPredicate;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all patients within a hospital wing.
  * Keyword matching is case insensitive.
  */
 public class GetHospitalWingCommand extends Command {
--- src/main/java/seedu/address/logic/parser/GetHospitalWingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/GetHospitalWingCommandParser.java
@@ -9,13 +9,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.HospitalWingContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new GetHospitalWingCommand object
  */
 public class GetHospitalWingCommandParser implements Parser<GetHospitalWingCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the GetCommand
-     * and returns a GetCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the GetHospitalWingcommand
+     * and returns a GetHospitalWingCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public GetHospitalWingCommand parse(String args) throws ParseException {
