e8c678ed2ff719c96824b85f7e26289ca140e6ce
LianGuoYang <e0735382@u.nus.edu>
2022-10-04 20:26:10 +0800

    Remove unnecessary imports

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import java.util.Comparator;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
