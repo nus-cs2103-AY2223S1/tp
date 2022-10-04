c0a8d611778539c180d1a826aa553f5a7dec239d
sharmaine1028 <sharmaine1028@gmail.com>
2022-10-04 23:23:16 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -8,9 +8,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
@@ -84,6 +82,7 @@ public class MainWindow extends UiPart<Stage> {
 
     /**
      * Sets the accelerator of a MenuItem.
+     *
      * @param keyCombination the KeyCombination value of the accelerator
      */
     private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
--- src/main/java/seedu/address/ui/PieChartPanel.java
+++ src/main/java/seedu/address/ui/PieChartPanel.java
@@ -7,25 +7,25 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 
+/**
+ * Panel containing the pie chart diagram.
+ */
+public class PieChartPanel extends UiPart<Region> {
+    private static final String FXML = "PieChartPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(seedu.address.ui.PieChartPanel.class);
+
+    @FXML
+    private Label pieChartTitle;
+
     /**
-     * Panel containing the pie chart diagram.
+     * TODO: Edit image to pie chart
+     * Creates a {@code PieChartPanel} with a default pie chart image
      */
-    public class PieChartPanel extends UiPart<Region> {
-        private static final String FXML = "PieChartPanel.fxml";
-        private final Logger logger = LogsCenter.getLogger(seedu.address.ui.PieChartPanel.class);
-
-        @FXML
-        private Label pieChartTitle;
-
-        /**
-         * TODO: Edit image to pie chart
-         * Creates a {@code PieChartPanel} with a default pie chart image
-         */
-        public PieChartPanel() {
-            super(FXML);
-            // TODO: Edit to title of time period of pie chart
-            pieChartTitle.setText("PennyWise");
-        }
-
+    public PieChartPanel() {
+        super(FXML);
+        // TODO: Edit to title of time period of pie chart
+        pieChartTitle.setText("PennyWise");
     }
+
+}
 
