21cb52bb67c6c8a9071dbacc2878db3812a51760
LianGuoYang <e0735382@u.nus.edu>
2022-10-04 20:40:20 +0800

    Add assignments to existing test files

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -1,9 +1,12 @@
 package seedu.address.testutil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Assignment;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -20,12 +23,15 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_GROUP = "GROUP 1";
+    public static final String DEFAULT_ASSIGNMENT = "Assignment 0";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
     private Set<Tag> tags;
+    private HashMap<String, ArrayList<Assignment>> assignments;
 
     /**
      * Creates a {@code PersonBuilder} with the default details.
@@ -36,6 +42,7 @@ public class PersonBuilder {
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
         tags = new HashSet<>();
+        assignments = new HashMap<>();
     }
 
     /**
@@ -47,6 +54,7 @@ public class PersonBuilder {
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
         tags = new HashSet<>(personToCopy.getTags());
+        assignments = new HashMap<>(personToCopy.getAssignments());
     }
 
     /**
@@ -89,8 +97,19 @@ public class PersonBuilder {
         return this;
     }
 
+    /**
+     * Parses the {@code Assignments} into a {@code HashMap<String, ArrayList<Assignment>>}
+     * and set it to the {@code Person} that we are building.
+     */
+    public PersonBuilder withAssignments(String ... tags) {
+        ArrayList<Assignment> assignmentList = new ArrayList<>();
+        assignmentList.add(new Assignment(DEFAULT_ASSIGNMENT));
+        this.assignments.put(DEFAULT_GROUP, assignmentList);
+        return this;
+    }
+
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, tags, assignments);
     }
 
 }
