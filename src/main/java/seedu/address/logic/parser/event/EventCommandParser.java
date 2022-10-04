c8f0687afa14fa464ce6cfbb026195061a11707c
Shashank Acharya <acharya.s@icloud.com>
2022-10-04 12:34:14 +0800

    Create EventCommandParser
    Currently, only a default case is implemented which throws an error.

---
+++ src/main/java/seedu/address/logic/parser/event/EventCommandParser.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser.event;
+
+import static seedu.address.commons.core.Messages.MESSAGE_FLAG_NOT_SPECIFIED;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_OPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.logic.commands.event.EventCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new EventCommand object
+ */
+public class EventCommandParser implements Parser<EventCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the EventCommand
+     * and returns an EventCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EventCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args,
+                        PREFIX_OPTION, PREFIX_NAME, PREFIX_START_DATE, PREFIX_END_DATE, PREFIX_TAG);
+
+        String option = argMultimap.getValue(PREFIX_OPTION)
+                .orElseThrow(() -> new ParseException(MESSAGE_FLAG_NOT_SPECIFIED));
+
+        switch (option) {
+            default:
+                throw new ParseException(EventCommand.OPTION_UNKNOWN);
+        }
+    }
+}
