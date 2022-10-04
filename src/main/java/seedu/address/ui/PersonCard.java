ce38a71a20fe5d9a2f131165c33733995858bca8
tanhl2000 <tan.hongliang@dhs.sg>
2022-10-04 14:17:08 +0800

    Added tests for the remark tutorial

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -52,6 +52,7 @@ public class PersonCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
+        remark.setText(person.getRemark().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         person.getTags().stream()
--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -32,6 +32,8 @@ public class CommandTestUtil {
     public static final String VALID_PHONE_BOB = "22222222";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
+    public static final String VALID_REMARK_AMY = "She likes aarvarks";
+    public static final String VALID_REMARK_BOB = "He doesnt like beer";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
