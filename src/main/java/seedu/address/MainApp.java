2393874b91fb3f5fcf7cc7f37ff517bc4f010451
songivan00 <71430722+songivan00@users.noreply.github.com>
2022-10-04 17:36:01 +0800

    Update src/main/java/seedu/address/MainApp.java

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -22,7 +22,7 @@ import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.TaskList;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.ModelManager;
-import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
 import seedu.address.model.util.SampleDataUtil;
 
 import seedu.address.storage.AddressBookStorage;
