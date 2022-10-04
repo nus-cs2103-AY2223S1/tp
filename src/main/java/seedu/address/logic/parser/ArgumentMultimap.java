7c406554b7b31b331dabaf181c213658946f8b0d
MuhdMusab <musab653@gmail.com>
2022-10-04 19:56:52 +0800

    Add checks for any present prefixes

--- src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
@@ -66,4 +66,12 @@ public class ArgumentMultimap {
     public static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
+
+    /**
+     * Returns true if any of the prefixes are present {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    public static boolean anyPrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
