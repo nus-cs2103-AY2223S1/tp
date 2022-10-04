e11c0ea2ec19484875a230dbfe5c61127f02a6c5
WuHaohui1231 <thewuhh@gmail.com>
2022-10-04 16:53:59 +0800

    create seperate branch for updating find

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -39,4 +39,5 @@ public class FindCommand extends Command {
                 || (other instanceof FindCommand // instanceof handles nulls
                 && predicate.equals(((FindCommand) other).predicate)); // state check
     }
+    // Update find command (search function)
 }
