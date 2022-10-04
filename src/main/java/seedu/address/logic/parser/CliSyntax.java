41ce05dbd775fffac27f3ce3aed51ce2051e350d
Shashank Acharya <acharya.s@icloud.com>
2022-10-04 12:33:43 +0800

    Add event prefixes
    Added prefixes for start and end dates

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    public static final Prefix PREFIX_START_DATE = new Prefix("start/");
+    public static final Prefix PREFIX_END_DATE = new Prefix("end/");
 }
