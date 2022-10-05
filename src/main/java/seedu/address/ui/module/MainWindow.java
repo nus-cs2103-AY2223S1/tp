66c3f9287387f43ee12a0ea3c84f897c414bb550
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-10-05 13:15:19 +0800

    Add new CSS files to style the elements
    Previously the css style sheet uses that of AB3
    
    Let's make new style sheets

--- src/main/java/seedu/address/ui/module/MainWindow.java
+++ src/main/java/seedu/address/ui/module/MainWindow.java
@@ -143,6 +143,7 @@ public class MainWindow extends UiPart<Stage> {
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
+//        TODO: Resolve logic issue
         StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
 
