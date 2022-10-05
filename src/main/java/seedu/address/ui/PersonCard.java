ddb0f975dc2ca5d18ad2d824513e802fa0a8d1ae
Meng Qiaoran <e0492494@u.nus.edu>
2022-10-05 17:59:08 +0800

    Add gender field in GUI.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -41,6 +41,9 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    @FXML
+    private Label gender;
+
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
@@ -55,6 +58,7 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        gender.setText(person.getGender().value.toString()); // value is enum, need toString method to convert it to string
     }
 
     @Override
