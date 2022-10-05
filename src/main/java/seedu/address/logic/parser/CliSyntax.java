04f66fbb5fa7372509e6d79e8ff721575b0d2db1
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-05 10:30:22 +0800

    Edit CliSyntax.java: Add 4 prefixes associated with Events

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,10 +6,16 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
+    // Prefix associated with Person
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
+    // Prefix associated with Event
+    public static final Prefix PREFIX_DATE = new Prefix("d/");
+    public static final Prefix PREFIX_TIME = new Prefix("t/"); //same as PREFIX_TAG
+    public static final Prefix PREFIX_PURPOSE = new Prefix("p/"); //same as PREFIX_PHONE
+    public static final Prefix PREFIX_EVENT_TITLE = new Prefix("e/");
 }
