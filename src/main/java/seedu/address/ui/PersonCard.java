2472cabf836fc783bc0cc149324ae449662d53ee
phyliciachristel <e0775641@u.nus.edu>
2022-10-05 23:49:53 +0800

    Integrate lesson plan feature into FXML

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,6 +39,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label lessonPlan;
+    @FXML
     private Label homework;
     @FXML
     private FlowPane tags;
@@ -54,6 +56,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        lessonPlan.setText(person.getLessonPlan().value);
         homework.setText(person.getHomeworkList().toString());
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
