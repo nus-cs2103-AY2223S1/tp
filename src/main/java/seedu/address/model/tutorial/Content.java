94f0e3cb8a9c39f82756133858a816bb4853b5b7
Shawn532 <E0556732@u.nus.edu>
2022-10-05 19:36:57 +0800

    Update the Content class

--- src/main/java/seedu/address/model/tutorial/Content.java
+++ src/main/java/seedu/address/model/tutorial/Content.java
@@ -1,4 +1,56 @@
 package seedu.address.model.tutorial;
 
+import seedu.address.model.person.Name;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Content {
+    public final String content;
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Content should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    // I still don't understand what it means right now, and I will find out later.
+
+    /**
+     * Constructs a {@code Content}.
+     *
+     * @param content A valid content.
+     */
+    public Content(String content) {
+        requireNonNull(content);
+        //checkArgument(isValidName(content), MESSAGE_CONSTRAINTS);
+        this.content = content;
+    }
+
+    /**
+     * Returns true if a given string is a valid content.
+     */
+    public static boolean isValidContent(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Content // instanceof handles nulls
+                && content.equals(((Content) other).content)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return content.hashCode();
+    }
 }
