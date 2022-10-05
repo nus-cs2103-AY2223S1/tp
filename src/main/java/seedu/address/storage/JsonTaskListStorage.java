c27f428f8ba4ffd9d8dac483416e9eb367cb3b9a
angkl0 <kuanglong456@gmail.com>
2022-10-05 23:32:23 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/storage/JsonTaskListStorage.java
+++ src/main/java/seedu/address/storage/JsonTaskListStorage.java
@@ -2,17 +2,17 @@ package seedu.address.storage;
 
 import static java.util.Objects.requireNonNull;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.logging.Logger;
+
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.ReadOnlyTaskList;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Optional;
-import java.util.logging.Logger;
 
 /**
  * A class to access TaskList data stored as a json file on the hard disk.
