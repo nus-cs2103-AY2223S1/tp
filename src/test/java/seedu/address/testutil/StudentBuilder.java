335a6512bbec8539c8eb219740b8918a7e67aa6e
Lu Yiting <e0774189@u.nus.edu>
2022-10-05 09:53:22 +0800

    Fix stylechecks errors

--- src/test/java/seedu/address/testutil/StudentBuilder.java
+++ src/test/java/seedu/address/testutil/StudentBuilder.java
@@ -3,7 +3,12 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.student.*;
+import seedu.address.model.student.Address;
+import seedu.address.model.student.Email;
+import seedu.address.model.student.Name;
+import seedu.address.model.student.Phone;
+import seedu.address.model.student.Student;
+import seedu.address.model.student.TutorialGroup;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
