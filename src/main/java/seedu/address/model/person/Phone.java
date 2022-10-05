6a1cee2341791872281e783732c1a004a5b4aaff
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-05 13:11:45 +0800

    Add method to create Phone object with default field values

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -13,6 +13,9 @@ public class Phone {
     public static final String MESSAGE_CONSTRAINTS =
             "Phone numbers should only contain numbers, and it should be at least 3 digits long";
     public static final String VALIDATION_REGEX = "\\d{3,}";
+
+    public static final String DEFAULT_PHONE = "000";
+
     public final String value;
 
     /**
@@ -24,6 +27,14 @@ public class Phone {
         requireNonNull(phone);
         checkArgument(isValidPhone(phone), MESSAGE_CONSTRAINTS);
         value = phone;
+    }
+
+    /**
+     * Returns a Phone object containing the default String
+     * @return Phone object with the default phone field.
+     */
+    public Phone makeDefaultPhone(){
+        return new Phone(DEFAULT_PHONE);
     }
 
     /**
