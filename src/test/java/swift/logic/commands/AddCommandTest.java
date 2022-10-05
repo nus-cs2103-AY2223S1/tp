42f8582661a7a83099294b46b8052d0fd609104d
peppapighs <ptk.prasertsuk@gmail.com>
2022-10-05 22:41:32 +0800

    Fix: fix stub classes interface implementation

--- src/test/java/swift/logic/commands/AddCommandTest.java
+++ src/test/java/swift/logic/commands/AddCommandTest.java
@@ -21,6 +21,7 @@ import swift.model.Model;
 import swift.model.ReadOnlyAddressBook;
 import swift.model.ReadOnlyUserPrefs;
 import swift.model.person.Person;
+import swift.model.task.Task;
 import swift.testutil.PersonBuilder;
 
 public class AddCommandTest {
@@ -114,6 +115,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void addTask(Task task) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void setAddressBook(ReadOnlyAddressBook newData) {
             throw new AssertionError("This method should not be called.");
         }
@@ -145,6 +151,21 @@ public class AddCommandTest {
 
         @Override
         public void updateFilteredPersonList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasTask(Task task) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteTask(Task target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setTask(Task target, Task editedTask) {
             throw new AssertionError("This method should not be called.");
         }
     }
