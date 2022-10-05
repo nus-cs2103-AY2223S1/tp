cb3fdd510a18f3119f25e694433b01f406c95a14
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-05 22:20:47 +0800

    Remove static inner class and overloaded constructors

--- src/main/java/seedu/address/model/person/client/Client.java
+++ src/main/java/seedu/address/model/person/client/Client.java
@@ -1,6 +1,5 @@
 package seedu.address.model.person.client;
 
-import static seedu.address.model.person.Name.DEFAULT_NAME;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -20,60 +19,6 @@ public class Client extends Person {
 
     //Represents a Collection of projects that the client is responsible for
     private Set<Project> projects = new HashSet<>();
-
-    /**
-     * Constructs a client with name given by user and the rest of the fields set to default values.
-     * @param name String representing name of the client
-     */
-    public Client(String name) {
-        super(new Name(name), Phone.makeDefaultPhone(), Email.makeDefaultEmail(), Address.makeDefaultAddress(),
-                Set.of(Tag.makeDefaultTag()));
-    }
-
-    /**
-     * Represents an Empty Client for the project.
-     */
-    private static class EmptyClient extends Client {
-        private static final Client EMPTY_CLIENT = new EmptyClient();
-        private EmptyClient() {
-            super(DEFAULT_NAME);
-        }
-    }
-
-    /**
-     * Constructs a client with name, phone number given by user and the rest of the fields set to default values.
-     * @param name String representing name of the client
-     * @param phone String representing phone number of the client
-     */
-    public Client(String name, String phone) {
-        super(new Name(name), new Phone(phone), Email.makeDefaultEmail(), Address.makeDefaultAddress(),
-                Set.of(Tag.makeDefaultTag()));
-    }
-
-    /**
-     * Constructs a client with name, phone number, email given by user and the rest of the fields set to default
-     * values.
-     * @param name String representing name of the client
-     * @param phone String representing phone number of the client
-     * @param email String representing email address of the client
-     */
-    public Client(String name, String phone, String email) {
-        super(new Name(name), new Phone(phone), new Email(email), Address.makeDefaultAddress(),
-                Set.of(Tag.makeDefaultTag()));
-    }
-
-    /**
-     * Constructs a client with name, phone number, email, address given by user and the rest of the fields set to
-     * default values.
-     * @param name String representing name of the client
-     * @param phone String representing phone number of the client
-     * @param email String representing email address of the client
-     * @param address String representing location of the client
-     */
-    public Client(String name, String phone, String email, String address) {
-        super(new Name(name), new Phone(phone), new Email(email), new Address(address),
-                Set.of(Tag.makeDefaultTag()));
-    }
 
     /**
      * Constructs a client with inputs given by the user.
