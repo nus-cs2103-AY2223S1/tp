221b2e4b2e5eb7e2c1a40a2d2dcdcb394d27287f
chengda300 <chengda300@gmail.com>
2022-10-06 00:41:51 +0800

    Add find and list for internships

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -14,8 +14,10 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindInternshipCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListInternshipCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -63,8 +65,14 @@ public class AddressBookParser {
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
 
+        case FindInternshipCommand.COMMAND_WORD:
+            return new FindInternshipCommandParser().parse(arguments);
+
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListInternshipCommand.COMMAND_WORD:
+            return new ListInternshipCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
