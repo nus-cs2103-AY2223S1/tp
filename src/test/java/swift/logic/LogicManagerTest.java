eca5df9dc3ded3046fd84265e003b16f94d58c2d
ShenyiCui <shenyicui@outlook.com>
2022-10-06 03:03:25 +0800

    Refactor: refactor `add_contact` tests

--- src/test/java/swift/logic/LogicManagerTest.java
+++ src/test/java/swift/logic/LogicManagerTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import swift.logic.commands.AddCommand;
+import swift.logic.commands.AddContactCommand;
 import swift.logic.commands.CommandResult;
 import swift.logic.commands.ListCommand;
 import swift.logic.commands.exceptions.CommandException;
@@ -79,7 +79,7 @@ public class LogicManagerTest {
         logic = new LogicManager(model, storage);
 
         // Execute add command
-        String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY
+        String addCommand = AddContactCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY
                 + ADDRESS_DESC_AMY;
         Person expectedPerson = new PersonBuilder(AMY).withTags().build();
         ModelManager expectedModel = new ModelManager();
