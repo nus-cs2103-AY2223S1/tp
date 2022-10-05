af43e67198bf344bf311e745dfc09d4c79ffed84
e1010101 <ezratio15195@gmail.com>
2022-10-05 11:54:33 +0800

    Set up skeleton for authentication system

---
+++ src/main/java/seedu/address/auth/AuthHandler.java
@@ -0,0 +1,11 @@
+package seedu.address.auth;
+
+public class AuthHandler {
+
+    // HOW IT WORKS
+    // 1) Users will be given a hardcoded username/password
+    // 2) When the user logs in, the app will check its credentials against the hardcoded ones
+    // 3) If the credentials are correct, the app will allow the user to access the app
+    
+    
+}
---
+++ src/main/java/seedu/address/auth/Credentials.java
@@ -0,0 +1,8 @@
+package seedu.address.auth;
+
+public class Credentials {
+
+    public String DEBUG_USERNAME = "admin";
+    public String DEBUG_PASSWORD = "admin";
+    
+}
