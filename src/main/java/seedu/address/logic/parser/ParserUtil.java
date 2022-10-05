c5d32ee09d01ecf2b9354e8cc86247c8c16c0ce0
Echomo Xinyu <seanxinyu2@gmail.com>
2022-10-06 01:30:01 +0800

    Add Parser and relevant functions for Class field
    - Class String constraint and relevant error messages are included.
    - Validation regular expression and validation methods are included.
    - Parser for Class String has been implemented.

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -7,11 +7,17 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.AdditionalNotes;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Class;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.MoneyOwed;
 import seedu.address.model.person.MoneyPaid;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 /**
  * Contains utility methods used for parsing strings in the various *Parser classes.
@@ -91,6 +97,60 @@ public class ParserUtil {
             throw new ParseException(Email.MESSAGE_CONSTRAINTS);
         }
         return new Email(trimmedEmail);
+    }
+
+    /**
+     * Parses a {@code String classDatetime} into a {@code Class}
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code dateTime}
+     */
+    public static Class parseClass(String classDatetime) throws ParseException {
+        requireNonNull(classDatetime);
+        String trimmedClassDatetime = classDatetime.trim();
+        if (!Class.isValidClassString(trimmedClassDatetime)) {
+            throw new ParseException(Class.MESSAGE_CONSTRAINTS);
+        }
+
+        // the format has been validated in isValidClassString method
+        // ie yyyy-MM-dd 0000-2359
+        String datetimeStr = trimmedClassDatetime.substring(0, 10);
+        String startTimeStr = trimmedClassDatetime.substring(11, 15);
+        String endTimeStr = trimmedClassDatetime.substring(16);
+
+        LocalDate date = parseDate(datetimeStr);
+        LocalTime startTime = parseTime(startTimeStr);
+        LocalTime endTime = parseTime(endTimeStr);
+
+        return new Class(date, startTime, endTime);
+    }
+
+    /**
+     * Helper method to parse {@code date} as part of {@code parseClass}.
+     */
+    private static LocalDate parseDate(String date) throws ParseException {
+        LocalDate result;
+        try {
+            result = LocalDate.parse(date);
+        } catch (DateTimeParseException de) {
+            throw new ParseException(Class.INVALID_DATETIME_ERROR_MESSAGE);
+        }
+        return result;
+    }
+
+    /**
+     * Helper method to parse {@code time} as part of {@code parseClass}.
+     */
+    private static LocalTime parseTime(String time) throws ParseException {
+        Integer hour = Integer.valueOf(time.substring(0, 2));
+        Integer minute = Integer.valueOf(time.substring(2));
+        LocalTime result;
+        try {
+            result = LocalTime.of(hour, minute);
+        } catch (DateTimeException de) {
+            throw new ParseException(Class.INVALID_TIME_ERROR_MESSAGE);
+        }
+        return result;
     }
 
     /**
--- src/main/java/seedu/address/model/person/Class.java
+++ src/main/java/seedu/address/model/person/Class.java
@@ -1,14 +1,23 @@
 package seedu.address.model.person;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 /**
  * Represents a Person's class date in the address book.
  * Guarantees: immutable.
  */
 public class Class {
-
+    public static final String MESSAGE_CONSTRAINTS = "Class can take any string"
+            + " in the format of 'yyyy-MM-dd 0000-2300'";
+    public static final String INVALID_DATETIME_ERROR_MESSAGE = "Date should be a valid date";
+    public static final String INVALID_TIME_ERROR_MESSAGE = "Time should be in the range of 0000 - 2359";
+    public static final String VALIDATION_DATETIME_REGEX = "[1-9][0-9]{3}[-][01][0-9][-][0123][0-9]";
+    public static final String VALIDATION_TIME_REGEX = "[012][0-9][0-5][0-9]";
+    public static final String VALIDATION_CLASS_REGEX = VALIDATION_DATETIME_REGEX
+            + "[ ]" + VALIDATION_TIME_REGEX + "[-]" + VALIDATION_TIME_REGEX;
     public final LocalDate date;
     public final LocalTime startTime;
     public final LocalTime endTime;
@@ -27,4 +36,47 @@ public class Class {
         this.endTime = endTime;
     }
 
+    /**
+     * Validates whether classDatetime is valid.
+     * @param classDatetime the string to be validated.
+     * @return true if a given string fits the format of 'yyyy-MM-d 0000-2300'
+     */
+    public static boolean isValidClassString(String classDatetime) {
+        if (!classDatetime.matches(VALIDATION_CLASS_REGEX)) {
+            return false;
+        }
+
+        String datetimeStr = classDatetime.substring(0, 10);
+        String startTimeStr = classDatetime.substring(11, 15);
+        String endTimeStr = classDatetime.substring(16);
+
+        return isValidDatetimeString(datetimeStr) && isValidTimeString(startTimeStr) && isValidTimeString(endTimeStr);
+    }
+
+    /**
+     * Helper method to validate {@code date}
+     */
+    private static boolean isValidDatetimeString(String date) {
+        try {
+            LocalDate.parse(date);
+        } catch (DateTimeParseException de) {
+            // text cannot be parsed
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Helper method to validate {@code time}
+     */
+    private static boolean isValidTimeString(String time) {
+        Integer hour = Integer.valueOf(time.substring(0,2));
+        Integer minute = Integer.valueOf(time.substring(2));
+        try {
+            LocalTime.of(hour, minute);
+        } catch (DateTimeException de) {
+            return false;
+        }
+        return true;
+    }
 }
