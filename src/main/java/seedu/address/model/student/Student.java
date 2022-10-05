3a46729ccaa0a5380f221185139a275279039fde
Lu Yiting <e0774189@u.nus.edu>
2022-10-05 17:03:52 +0800

    Fix style

--- src/main/java/seedu/address/model/student/Student.java
+++ src/main/java/seedu/address/model/student/Student.java
@@ -19,7 +19,7 @@ public class Student {
     private final Name name;
     private final Phone phone;
     private final Email email;
-     private final TutorialGroup tutorialGroup;
+    private final TutorialGroup tutorialGroup;
 
     // Data fields
     private final Address address;
