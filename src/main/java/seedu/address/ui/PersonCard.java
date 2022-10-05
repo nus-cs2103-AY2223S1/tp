fa1e5d76216f55eb5068c18f4ec25907e8f9639d
Shawn532 <E0556732@u.nus.edu>
2022-10-05 21:13:06 +0800

    no message

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -41,6 +41,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+
+
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
