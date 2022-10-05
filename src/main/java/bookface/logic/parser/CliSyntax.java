67416f518840cc98a1c44d89f6efb9f4a32f0ec0
Edfernape <edwardymualimy@gmail.com>
2022-10-06 00:31:54 +0800

    Add new prefixes author and title for the add book command.

--- src/main/java/bookface/logic/parser/CliSyntax.java
+++ src/main/java/bookface/logic/parser/CliSyntax.java
@@ -10,5 +10,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_TITLE = new Prefix("t/");
+    public static final Prefix PREFIX_AUTHOR = new Prefix("a/");
 
 }
