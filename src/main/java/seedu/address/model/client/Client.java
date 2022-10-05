aa8d9902832d594e3e2ae6a5c30eadaca3a878de
junweimoo <junweimoo@gmail.com>
2022-10-05 15:22:58 +0800

    Add meeting field to Client

--- src/main/java/seedu/address/model/client/Client.java
+++ src/main/java/seedu/address/model/client/Client.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import seedu.address.model.meeting.Meeting;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -23,6 +24,7 @@ public class Client {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
+    private Meeting meeting;
 
     /**
      * Every field must be present and not null.
@@ -34,6 +36,14 @@ public class Client {
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+    }
+
+    public Meeting getMeeting() {
+        return meeting;
+    }
+
+    public void setMeeting(Meeting meeting) {
+        this.meeting = meeting;
     }
 
     public Name getName() {
