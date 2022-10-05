91d206e1a4191c7d1765007fd480856db39af954
bryanljx1 <bryanljx1@gmail.com>
2022-10-05 21:59:16 +0800

    Revert "Update restrictions in Name class"
    This reverts commit 03e27b6b554ebf94e968b29f1d2654a585bec037.

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,23 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters, spaces, and the punctuations within the " +
-                    "list of allowed punctuations. " +
-                    "Names must start with an alphanumeric character.";
+            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
 
     /*
-     * Regex to check for valid punctuation
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String AllOWED_PUNCTUATION = "[\\?\\.\\'\\\"\\[\\]\\{\\}\\+\\^\\$\\*\\(\\)\\-<>,:;~@!#%&_=`]";
-
-    /*
-     * Only alphanumeric characters, whitespaces and punctuation within the ALLOWED_PUNCTUATION list are allowed.
-     * The first character of the name must be alphanumeric.
-     */
-    public static final String VALIDATION_REGEX = String.format(
-            "[\\p{Alnum}][\\p{Alnum} | \\p{Space} | %s]*",
-            AllOWED_PUNCTUATION
-    );
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String fullName;
 
