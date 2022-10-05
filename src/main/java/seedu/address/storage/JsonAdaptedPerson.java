72a7dbea426aaa1777c45b841783ab18a414c9f1
Meng Qiaoran <e0492494@u.nus.edu>
2022-10-05 18:32:58 +0800

    Add gender field in storage.

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -16,6 +16,8 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.person.Gender;
+import static seedu.address.model.person.GenderType.NA;
 
 /**
  * Jackson-friendly version of {@link Person}.
@@ -30,13 +32,15 @@ class JsonAdaptedPerson {
     private final String address;
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
 
+    private final String gender;
+
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
             @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+            @JsonProperty("tagged") List<JsonAdaptedTag> tagged, @JsonProperty("address") String gender) {
         this.name = name;
         this.phone = phone;
         this.email = email;
@@ -44,6 +48,7 @@ class JsonAdaptedPerson {
         if (tagged != null) {
             this.tagged.addAll(tagged);
         }
+        this.gender = gender;
     }
 
     /**
@@ -57,6 +62,7 @@ class JsonAdaptedPerson {
         tagged.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
+        gender = source.getGender().value.toString();
     }
 
     /**
@@ -102,8 +108,18 @@ class JsonAdaptedPerson {
         }
         final Address modelAddress = new Address(address);
 
+        // add in optional field gender
+        Gender modelGender;
+        if (gender == null) {
+            modelGender = new Gender(NA);
+        }
+        if (!Gender.isValidGender(gender)) {
+            throw new IllegalValueException(Gender.MESSAGE_CONSTRAINTS);
+        }
+        modelGender = new Gender(gender);
+
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags, modelGender);
     }
 
 }
