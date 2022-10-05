cc91269a1f25787b7b40f28e1d5769dac79414ee
Shawn532 <E0556732@u.nus.edu>
2022-10-05 19:43:36 +0800

    Update Content Group Time classes

--- src/main/java/seedu/address/model/tutorial/Content.java
+++ src/main/java/seedu/address/model/tutorial/Content.java
@@ -12,7 +12,7 @@ public class Content {
             "Content should only contain alphanumeric characters and spaces, and it should not be blank";
 
     /*
-     * The first character of the address must not be a whitespace,
+     * The first character of the content must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
@@ -25,7 +25,7 @@ public class Content {
      */
     public Content(String content) {
         requireNonNull(content);
-        //checkArgument(isValidName(content), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidContent(content), MESSAGE_CONSTRAINTS);
         this.content = content;
     }
 
--- src/main/java/seedu/address/model/tutorial/Group.java
+++ src/main/java/seedu/address/model/tutorial/Group.java
@@ -1,4 +1,54 @@
 package seedu.address.model.tutorial;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Group {
+    public final String group;
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "group should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the group must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    // I still don't understand what it means right now, and I will find out and change it later.
+
+    /**
+     * Constructs a {@code group}.
+     *
+     * @param group A valid group.
+     */
+    public Group(String group) {
+        requireNonNull(group);
+        checkArgument(isValidGroup(group), MESSAGE_CONSTRAINTS);
+        this.group = group;
+    }
+
+    /**
+     * Returns true if a given string is a valid group.
+     */
+    public static boolean isValidGroup(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return group;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Group // instanceof handles nulls
+                && group.equals(((Group) other).group)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return group.hashCode();
+    }
 }
