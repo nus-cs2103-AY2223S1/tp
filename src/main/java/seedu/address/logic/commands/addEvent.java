a3f51c52b92f5dcda6ad41255b6f889dcdbc2971
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-05 10:25:15 +0800

    Create base addEvent.java class

---
+++ src/main/java/seedu/address/logic/commands/addEvent.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Adds an Event to the application.
+ */
+public class addEvent extends Command {
+
+    public static final String COMMAND_WORD = "addEvent";
+    
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+}
