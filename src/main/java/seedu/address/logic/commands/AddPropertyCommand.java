f9bdd19b3d581b018babd7b7f20f63058e56c290
Zsigmond <e0725064@u.nus.edu>
2022-10-05 12:58:11 +0800

    Add AddPropertyCommand class

---
+++ src/main/java/seedu/address/logic/commands/AddPropertyCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRICE;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Adds a person to the address book.
+ */
+public class AddPropertyCommand extends Command {
+
+    public static final String COMMAND_WORD = "addprop";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a property to Cobb. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + PREFIX_PRICE + "PRICE "
+            + PREFIX_DESCRIPTION + "DESCRIPTION "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "Clementi Ave 2 Second Floor Flat "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_PRICE + "500 000 "
+            + PREFIX_DESCRIPTION + "ready to be bought ";   // consider changing description to tags, as implemented in addcommand
+
+    public static final String MESSAGE_SUCCESS = "New property added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PROPERTY = "This property already exists in the address book";
+
+    private final Property toAdd;
+
+    /**
+     * Creates an AddPropertyCommand to add the specified {@code Property}
+     */
+    public AddPropCommand(Property property) {
+        requireNonNull(property);
+        toAdd = property;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasProperty(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PROPERTY);
+        }
+
+        model.addProperty(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddCommand // instanceof handles nulls
+                && toAdd.equals(((AddCommand) other).toAdd));
+    }
+}
