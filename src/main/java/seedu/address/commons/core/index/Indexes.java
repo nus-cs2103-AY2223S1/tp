f6a8701e702aad5d9a4b290deb3e59bbe726efdd
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-10-05 21:25:15 +0800

    Add custom equals method for Indexes class

--- src/main/java/seedu/address/commons/core/index/Indexes.java
+++ src/main/java/seedu/address/commons/core/index/Indexes.java
@@ -2,6 +2,7 @@ package seedu.address.commons.core.index;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalIndexException;
 import seedu.address.logic.parser.ParserUtil;
@@ -13,7 +14,7 @@ import seedu.address.model.person.UniquePersonList;
  * Represents a unique collection of Index objects.
  */
 public class Indexes {
-    private HashSet<Index> indexes;
+    private Set<Index> indexes;
 
     /**
      * Constructs a new Indexes object given an array of strings.
@@ -27,6 +28,10 @@ public class Indexes {
         for (String arg : unParsedIndexes) {
             indexes.add(ParserUtil.parseIndex(arg));
         }
+    }
+
+    public Indexes(Set<Index> indexes) {
+        this.indexes = indexes;
     }
 
     public UniquePersonList getAllPersonsFromIndexes(List<Person> personList)
@@ -43,5 +48,12 @@ public class Indexes {
         }
 
         return selectedPersons;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Indexes // instanceof handles nulls
+                && indexes.equals(((Indexes) other).indexes)); // state check
     }
 }
