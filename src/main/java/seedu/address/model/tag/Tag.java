5407aed286514aaedecb8805ab0b0f257f557848
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-06 01:05:49 +0800

    Edit Tag class to allow spaces

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -9,8 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric or spaces";
+    public static final String VALIDATION_REGEX = "[a-zA-Z ]+";
 
     public final String tagName;
 
