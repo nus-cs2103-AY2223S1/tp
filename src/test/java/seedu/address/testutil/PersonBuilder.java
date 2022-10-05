09bb9ec4fd7244785fc41180f768fb1d48816a49
Meng Qiaoran <e0492494@u.nus.edu>
2022-10-05 20:13:21 +0800

    Fix checkstyle errors 3.

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -5,11 +5,11 @@ import java.util.Set;
 
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Gender;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.person.Gender;
 import seedu.address.model.util.SampleDataUtil;
 
 /**
