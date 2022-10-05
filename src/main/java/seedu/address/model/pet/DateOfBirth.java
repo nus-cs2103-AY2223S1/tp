8c4842f604dd92a242353cfd85609fb1325dbaf2
Hongyi6328 <6328huang@gmail.com>
2022-10-05 15:18:49 +0800

    Update DateOfBirth.java

--- src/main/java/seedu/address/model/pet/DateOfBirth.java
+++ src/main/java/seedu/address/model/pet/DateOfBirth.java
@@ -7,10 +7,23 @@ import java.time.format.DateTimeParseException;
 import seedu.address.commons.exceptions.IllegalValueException;
 
 public class DateOfBirth {
+    //@@author Hongyi6328-reused
+    //Reused from https://github.com/RussellDash332/ip/blob/master/src/main/java/stashy/parser/Parser.java
+    //with minor modification, it is a pretty good way to organise and extend the acceptable date formats.
+    private static final String[] ACCEPTABLE_DATE_FORMATS = new String[]{
+        "MMM dd yyyy",
+        "dd/MM/yyyy",
+        "yyyy/MM/dd",
+        "yyyy-MM-dd",
+        "dd MMM yyyy",
+        "dd MMM yyyy",
+        "MMM dd, yyyy",
+        "MMM dd, yyyy"
+    };
 
-    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd";
+    private static final String PREFERRED_DATE_FORMAT = "yyyy-MM-dd";
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+    private static final DateTimeFormatter PREFERRED_FORMATTER = DateTimeFormatter.ofPattern(PREFERRED_DATE_FORMAT);
 
     private final LocalDate date;
 
@@ -20,17 +33,20 @@ public class DateOfBirth {
 
     public static DateOfBirth parseString(String toParse) throws IllegalValueException {
         LocalDate output;
-        try {
-            output = LocalDate.parse(toParse, FORMATTER);
-        } catch (DateTimeParseException exception) {
-            throw new IllegalValueException("The date of birth should be in format: " + DATE_TIME_FORMAT);
+        for (String format: ACCEPTABLE_DATE_FORMATS) {
+            try {
+                output = LocalDate.parse(toParse, DateTimeFormatter.ofPattern(format));
+                return new DateOfBirth(output);
+            } catch (DateTimeParseException exception) {
+                //Do nothing because it will eventually throw an exception if no formats match
+            }
         }
-        return new DateOfBirth(output);
+        throw new IllegalValueException("The date of birth should be in format: " + PREFERRED_DATE_FORMAT);
     }
 
     @Override
     public String toString() {
-        return date.format(FORMATTER);
+        return date.format(PREFERRED_FORMATTER);
     }
 
     @Override
