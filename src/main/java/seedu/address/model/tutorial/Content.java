0f025f8e0784d992fdb357ff6b7320d675335ae9
Shawn532 <E0556732@u.nus.edu>
2022-10-05 15:17:23 +0800

    Create Tutorial, Content, Group, Time classes

---
+++ src/main/java/seedu/address/model/tutorial/Content.java
@@ -0,0 +1,4 @@
+package seedu.address.model.tutorial;
+
+public class Content {
+}
---
+++ src/main/java/seedu/address/model/tutorial/Tutorial.java
@@ -0,0 +1,80 @@
+package seedu.address.model.tutorial;
+
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+
+import java.util.Objects;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Represents a Tutorial in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Tutorial {
+
+    private final Group group;
+    private final Content content;
+    private final Time time;
+
+    public Tutorial(Group group, Content content, Time time) {
+        requireAllNonNull(group, content, time);
+        this.group = group;
+        this.content = content;
+        this.time = time;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    /**
+     * Returns true if both tutorials have the same fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Tutorial)) {
+            return false;
+        }
+
+        Tutorial otherTutorial = (Tutorial) other;
+
+        return other != null
+                && otherTutorial.getGroup().equals(getGroup())
+                && otherTutorial.getContent().equals(getContent())
+                && otherTutorial.getTime().equals(getTime());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(group, content, time);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Tutorial")
+                .append("; Group: ")
+                .append(getGroup())
+                .append("; Content: ")
+                .append(getContent())
+                .append("; Time: ")
+                .append(getTime());
+
+        return builder.toString();
+    }
+}
