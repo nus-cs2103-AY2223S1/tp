ef4563be7e09e268dcbb80518f77552193d43414
Tex Tang <tex_tang@outlook.com>
2022-10-05 19:14:05 +0800

    Ui: Add MainPanel class
    MainPanel class is used to define a main panel by inheriting it.

---
+++ src/main/java/seedu/address/ui/MainPanel.java
@@ -0,0 +1,19 @@
+package seedu.address.ui;
+
+import javafx.scene.layout.Region;
+
+/**
+ * Represents main panel of the UI. e.g. PersonListPanel, etc.
+ * It is main scene of the UI.
+ */
+public abstract class MainPanel extends UiPart<Region> {
+
+    public MainPanel(String fxmlFileName) {
+        super(fxmlFileName);
+    }
+
+    /**
+     * Returns the panel's name of the MainPanel
+     */
+    abstract public MainPanelName getPanelName();
+}
