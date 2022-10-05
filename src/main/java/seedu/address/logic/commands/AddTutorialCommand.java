3f9e773c5cb4a38cfc140aebf7056d4ea08b0ad7
Shawn532 <E0556732@u.nus.edu>
2022-10-05 14:59:49 +0800

    Hook AddTutorialCommand into the application

--- src/main/java/seedu/address/logic/commands/AddTutorialCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTutorialCommand.java
@@ -3,15 +3,30 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+import static seedu.address.logic.parser.CliSyntax.*;
+
+
 /**
- *  Adds a tutorial in the address book.
+ * Adds a tutorial in the address book.
  */
-public class AddTutorialCommand extends Command{
+public class AddTutorialCommand extends Command {
 
     public static final String COMMAND_WORD = "addtut";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a tutorial to the address book. "
+            + "Parameters: "
+            + PREFIX_GROUP + "GROUP NUMBER "
+            + PREFIX_CONTENT + "CONTENT "
+            + PREFIX_TIME + "TIME "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_GROUP + "T08 "
+            + PREFIX_CONTENT + "UML Diagram "
+            + PREFIX_TIME + "2022-10-01T08:00:00";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Addtut command not implemented yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from addtut");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_GROUP = new Prefix("g/");
+    public static final Prefix PREFIX_CONTENT = new Prefix("c/");
+    public static final Prefix PREFIX_TIME = new Prefix("t/");
 
 }
