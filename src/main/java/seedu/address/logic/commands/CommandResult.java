c7541b26ff602d4d4e6fe4983aa018349d075e51
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-10-05 15:21:20 +0800

    Reminder Command: Update CommandResult equal() and hashcode() methods

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -80,12 +80,13 @@ public class CommandResult {
         CommandResult otherCommandResult = (CommandResult) other;
         return feedbackToUser.equals(otherCommandResult.feedbackToUser)
                 && showHelp == otherCommandResult.showHelp
-                && exit == otherCommandResult.exit;
+                && exit == otherCommandResult.exit
+                && showReminder == otherCommandResult.showReminder;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(feedbackToUser, showHelp, exit);
+        return Objects.hash(feedbackToUser, showHelp, exit, showReminder);
     }
 
 }
