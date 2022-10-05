2d913889c6f9ced48c38b591ddd49e161eeef697
Rama <kmram0908@gmail.com>
2022-10-05 12:48:25 +0800

    Add TODO for updating model and patient

--- src/main/java/seedu/address/logic/commands/DeletePatientCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePatientCommand.java
@@ -30,6 +30,7 @@ public class DeletePatientCommand extends Command {
         this.targetIndex = targetIndex;
     }
 
+    // TODO - Update model and patient once they are done
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
