1139f3d1d707c2a8394b4719dc9479562092303b
TZL0 <tianzeee@outlook.com>
2022-10-06 20:12:48 +0800

    Fix Tests with remarks

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -33,14 +33,18 @@ public class TypicalPersons {
             .withEmail("johnd@example.com").withPhone("98765432")
             .withTags("owesMoney", "friends").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").build();
+            .withEmail("heinz@example.com").withAddress("wall street").withRemark(PersonBuilder.DEFAULT_REMARK).build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").build();
-    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
+            .withEmail("cornelia@example.com").withRemark(PersonBuilder.DEFAULT_REMARK)
+            .withAddress("10th street").withTags("friends").build();
+    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer")
+            .withPhone("9482224").withRemark(PersonBuilder.DEFAULT_REMARK)
             .withEmail("werner@example.com").withAddress("michegan ave").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
+            .withRemark(PersonBuilder.DEFAULT_REMARK)
             .withEmail("lydia@example.com").withAddress("little tokyo").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
+            .withRemark(PersonBuilder.DEFAULT_REMARK)
             .withEmail("anna@example.com").withAddress("4th street").build();
 
     // Manually added
