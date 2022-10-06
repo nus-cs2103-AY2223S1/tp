74dbe2912dfb7560d458527811ead20a3461e8de
Lu Yiting <e0774189@u.nus.edu>
2022-10-06 20:45:05 +0800

    Implement TutorialGroupList command

---
+++ src/main/java/seedu/address/logic/commands/TutorialGroupListCommand.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+import seedu.address.model.student.TutorialGroup;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TUTORIAL_GROUPS;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class TutorialGroupListCommand extends Command {
+
+    public static final String COMMAND_WORD = "tutorialList";
+
+    public static final String MESSAGE_SUCCESS = "Listed all tutorial groups";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredTutorialGroupList(PREDICATE_SHOW_ALL_TUTORIAL_GROUPS);
+        List<TutorialGroup> groups = model.getFilteredTutorialGroupList();
+        String display = "";
+
+        for (int i = 0; i < groups.size(); i++) {
+            display += groups.get(i).toString() + "\n";
+        }
+
+        return new CommandResult(display + MESSAGE_SUCCESS);
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,17 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.TutorialGroupAddCommand;
-import seedu.address.logic.commands.TutorialGroupDeleteCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -73,6 +63,9 @@ public class AddressBookParser {
 
         case TutorialGroupDeleteCommand.COMMAND_WORD:
             return new TutorialGroupDeleteCommandParser().parse(arguments);
+
+        case TutorialGroupListCommand.COMMAND_WORD:
+            return new TutorialGroupListCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
