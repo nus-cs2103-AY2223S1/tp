c0889862c094526e58bf53787effd0f1a1eadf48
Shawn532 <E0556732@u.nus.edu>
2022-10-06 17:15:08 +0800

    Update the ReadOnlyAddressBook interface and AddressBook to include tutorials

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -15,6 +15,7 @@ import seedu.address.model.person.UniquePersonList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+    private final UniqueTutorialList tutorials;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
