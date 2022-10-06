f4ea315b883ed4df3d616125b625149acaa9527b
TZL0 <tianzeee@outlook.com>
2022-10-06 12:55:44 +0800

    Rename EditCommandParser to EditPersonCommandParser

--- src/main/java/seedu/address/logic/parser/EditPersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditPersonCommandParser.java
@@ -20,13 +20,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new EditCommand object
+ * Parses input arguments and creates a new EditPersonCommand object
  */
 public class EditPersonCommandParser implements Parser<EditPersonCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the EditCommand
-     * and returns an EditCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the EditPersonCommand
+     * and returns an EditPersonCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditPersonCommand parse(String args) throws ParseException {
