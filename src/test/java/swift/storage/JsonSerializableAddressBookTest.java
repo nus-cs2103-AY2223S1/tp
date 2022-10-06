d785b7f66ef67ea6ab97498a255ed97b119d5274
peppapighs <ptk.prasertsuk@gmail.com>
2022-10-06 15:31:35 +0800

    Feat: add tests for JsonSerializableAddressBook

--- src/test/java/swift/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/swift/storage/JsonSerializableAddressBookTest.java
@@ -12,6 +12,7 @@ import swift.commons.exceptions.IllegalValueException;
 import swift.commons.util.JsonUtil;
 import swift.model.AddressBook;
 import swift.testutil.TypicalPersons;
+import swift.testutil.TypicalTasks;
 
 public class JsonSerializableAddressBookTest {
 
@@ -19,6 +20,10 @@ public class JsonSerializableAddressBookTest {
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
+
+    private static final Path TYPICAL_TASKS_FILE = TEST_DATA_FOLDER.resolve("typicalTasksAddressBook.json");
+    private static final Path INVALID_TASKS_FILE = TEST_DATA_FOLDER.resolve("invalidTasksAddressBook.json");
+    private static final Path DUPLICATE_TASKS_FILE = TEST_DATA_FOLDER.resolve("duplicateTasksAddressBook.json");
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
@@ -41,6 +46,30 @@ public class JsonSerializableAddressBookTest {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
                 JsonSerializableAddressBook.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON,
+                dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_typicalTasksFile_success() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_TASKS_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+        AddressBook typicalTasksAddressBook = TypicalTasks.getTypicalAddressBook();
+        assertEquals(addressBookFromFile, typicalTasksAddressBook);
+    }
+
+    @Test
+    public void toModelType_invalidTasksFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_TASKS_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_duplicateTasks_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_TASKS_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_TASK,
                 dataFromFile::toModelType);
     }
 
