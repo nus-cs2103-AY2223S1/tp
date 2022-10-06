5abc8f9ecff671fea061e3de6cd1f800f206e289
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-07 03:06:02 +0800

    Change Sample Data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -31,7 +31,7 @@ public class SampleDataUtil {
                 getTagSet()),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                new Id("183698"), new Title("Data Scientist Intern"),
+                new Id("165997"), new Title("[Campus Recruitment] Backend Engineer Intern (Dec'23 - May'23)"),
                 getTagSet("assessment", "rejected")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
