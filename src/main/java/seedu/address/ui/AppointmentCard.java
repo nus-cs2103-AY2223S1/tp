f0135e3a0d48ad3f79238c381cdf2cd9a5e233ab
TZL0 <tianzeee@outlook.com>
2022-10-07 02:45:21 +0800

    Add AppointmentCard.java, AppointmentListPanel.java, Add AppointmentListCard.fxml, AppointmentListPanel.fxml.

---
+++ src/main/java/seedu/address/ui/AppointmentCard.java
@@ -0,0 +1,67 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.appointment.Appointment;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class AppointmentCard extends UiPart<Region> {
+
+    private static final String FXML = "AppointmentListCard.fxml";
+
+    public final Appointment appointment;
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label id;
+    @FXML
+    private Label name;
+    @FXML
+    private Label medicalTest;
+    @FXML
+    private Label slot;
+    @FXML
+    private Label doctor;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public AppointmentCard(Appointment appointment, int displayedIndex) {
+        super(FXML);
+        this.appointment = appointment;
+        id.setText(displayedIndex + ". ");
+        name.setText(appointment.getName().fullName);
+        slot.setText(appointment.getSlot().toString());
+        doctor.setText(appointment.getDoctor().toString());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AppointmentCard)) {
+            return false;
+        }
+
+        // state check
+        AppointmentCard card = (AppointmentCard) other;
+        return id.getText().equals(card.id.getText())
+                && appointment.equals(card.appointment);
+    }
+}
