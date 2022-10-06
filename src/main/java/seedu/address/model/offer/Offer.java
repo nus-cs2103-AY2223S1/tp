575cf4ad7a641102676c0b926e57714f3229ceef
ama-chi <89921925+ama-chi@users.noreply.github.com>
2022-10-06 08:08:34 +0800

    Add Offer Class

--- src/main/java/seedu/address/model/offer/Offer.java
+++ src/main/java/seedu/address/model/offer/Offer.java
@@ -1,4 +1,70 @@
 package seedu.address.model.offer;
 
+import seedu.address.model.listing.Listing;
+import seedu.address.model.person.Person;
+
+/**
+ * Offer Class represents a clients offer for a listing, containing an offer price.
+ */
 public class Offer {
+
+    /**
+     * Client making the offer.
+     */
+    private final Person person;
+    /**
+     * Listing the offer is for.
+     */
+    private final Listing listing;
+    /**
+     * Price client is offering.
+     */
+    private final int offerPrice;
+
+    /**
+     * Constructor for offer object.
+     *
+     * @param client Person
+     * @param listing Listing
+     * @param offerPrice int
+     */
+    public Offer(Person client, Listing listing, int offerPrice) {
+        this.person = client;
+        this.listing = listing;
+        this.offerPrice = offerPrice;
+    }
+
+    /**
+     * Getter for client.
+     * @return Person
+     */
+    public Person getClient() {
+        return person;
+    }
+
+    /**
+     * Getter for listing.
+     * @return Listing
+     */
+    public Listing getListing() {
+        return listing;
+    }
+
+    /**
+     * Getter for offer price.
+     * @return int
+     */
+    public int getOfferPrice() {
+        return offerPrice;
+    }
+
+    /**
+     * String representation of offer.
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return String.format("%s is offering $%s for %s",
+                this.person.getName(), this.offerPrice, this.listing.getAddress());
+    }
 }
