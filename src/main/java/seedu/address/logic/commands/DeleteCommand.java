626e42087d016e474f8db89b52549d1282e49d73
shamooose <e0726246@u.nus.edu>
2022-10-06 22:18:20 +0800

    Organise code quality better

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -2,13 +2,8 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
@@ -27,6 +22,6 @@ public class DeleteCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        return null;
+        return new CommandResult("");
     }
 }
--- src/main/java/seedu/address/model/person/user/EmptyUser.java
+++ src/main/java/seedu/address/model/person/user/EmptyUser.java
@@ -1,23 +1,13 @@
 package seedu.address.model.person.user;
 
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
-
-import java.util.Objects;
-
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
 /**
  * Represents an Empty User field, which is present in the address book when ConnectNUS is first opened or when the
  * User has been deleted.
- * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class EmptyUser extends User {
 
     /**
-     * Every field must be present and not null.
+     * EmptyUser does not contain any fields as it is empty
      */
     public EmptyUser() {
         super();
