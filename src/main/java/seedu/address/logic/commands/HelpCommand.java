28ed87799cb6ec8562b2c4770ec9214e7eb30260
junwei-tan <junweitan@u.nus.edu>
2022-10-06 15:07:46 +0800

    Update help command
    Help command opens a pop-up window with a link to user guide.
    
    Users may benefit from viewing a brief list of commands within the
    application with the option of going to UG page if they require more
    information.
    
    Let's update the help command to return commonly used commands.

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -12,10 +12,23 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    private static final String USER_GUIDE_URL = "https://ay2223s1-cs2103t-f11-4.github.io/tp/UserGuide.html#quick-start";
+
+    public static final String SHOWING_HELP_MESSAGE = "Sample usage:\n\t"
+            + AddCommand.MESSAGE_USAGE + "\n\t"
+            + DeleteCommand.MESSAGE_USAGE + "\n\t"
+            + EditCommand.MESSAGE_USAGE + "\n\t"
+            + FindCommand.MESSAGE_USAGE + "\n\t"
+            + HelpCommand.MESSAGE_USAGE + "\n\n"
+            + "Other Commands:\n\t"
+            + ClearCommand.COMMAND_WORD + "\n\t"
+            + ListCommand.COMMAND_WORD + "\n\t"
+            + ExitCommand.COMMAND_WORD + "\n\n"
+            + "Further help:\n"
+            + USER_GUIDE_URL;
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(SHOWING_HELP_MESSAGE, false, false);
     }
 }
