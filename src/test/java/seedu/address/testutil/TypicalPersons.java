4d7284129238e07e2d1da492f1120853ce1aee2f
Charles1026 <xiaozhehao@gmail.com>
2022-10-06 21:53:06 +0800

    Remove Unused Import

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -1,11 +1,18 @@
 package seedu.address.testutil;
 
+import seedu.address.model.AddressBook;
+import seedu.address.model.person.PatientType.PatientTypes;
+import seedu.address.model.person.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FLOOR_NUMBER_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_HOSPITAL_WING_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MEDICATION_IBUPROFEN;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MEDICATION_PARACETAMOL;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NEXT_OF_KIN_AMY;
@@ -15,14 +22,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PATIENT_TYPE_BO
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_WARD_NUMBER_AMY;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import seedu.address.model.AddressBook;
-import seedu.address.model.person.PatientType.PatientTypes;
-import seedu.address.model.person.Person;
 
 
 
