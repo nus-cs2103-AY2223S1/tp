8bf08168992589f143a9f461d2529ed5b3329a38
alvintfl <alvintanfulong@gmail.com>
2022-10-07 01:21:42 +0800

    Fix bug in AddCommandTest where a method was not overriden

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -153,6 +153,11 @@ public class AddCommandTest {
         public ObservableList<Task> getFilteredTaskList() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void updateFilteredTaskList(Predicate<Task> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
