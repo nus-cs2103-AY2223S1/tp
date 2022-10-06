1ba073f168febddbe7b4719aae233ae5d0fbf049
LiuFangrui <e0772663@u.nus.edu>
2022-10-06 14:45:28 +0800

    Fix checkstyle related errors

--- src/main/java/seedu/address/logic/commands/FindPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/FindPersonCommand.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
