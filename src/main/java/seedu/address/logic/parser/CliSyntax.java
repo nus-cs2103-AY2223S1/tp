9e343993732bd7b5d66723afc976db251c1e89ba
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-06 22:01:28 +0800

    Add PREFIX_JOBID, PREFIX_JOBTITLE

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,6 +10,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_JOBID = new Prefix("id/");
+    public static final Prefix PREFIX_JOBTITLE = new Prefix("title/");
     public static final Prefix PREFIX_TAG = new Prefix("tag/");
 
 }
