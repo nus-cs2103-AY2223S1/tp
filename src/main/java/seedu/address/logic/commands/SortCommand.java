8cea81a6929bed5771e620f8f164ee281c22572f
SpecOps2016 <gregory.wong@ymail.com>
2022-10-06 18:10:04 +0800

    Added Sort Command + Fixed Style

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,10 +1,12 @@
 package seedu.address.logic.commands;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
-//import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+/**
+ * Sorts data in Address Book.
+ */
 public class SortCommand {
     public static final String COMMAND_WORD = "sort";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the list of people according to the specified field."
+    public static final String MESSAGE_USAGE =
+            COMMAND_WORD + ": Sorts the list of people according to the specified field."
             + "by alphabetical order.\n"
             + "Existing remark will be overwritten by the input.\n"
             + "Parameters: FIELD (must not be empty) "
