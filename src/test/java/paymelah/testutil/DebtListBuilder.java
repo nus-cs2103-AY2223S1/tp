8b06653e98345cc59d2c9b300e75c29f30428e77
tsammeow <97447335+tsammeow@users.noreply.github.com>
2022-10-06 21:42:39 +0800

    Add tests for DebtList

---
+++ src/test/java/paymelah/testutil/DebtListBuilder.java
@@ -0,0 +1,44 @@
+package paymelah.testutil;
+
+import static paymelah.testutil.TypicalDebts.CHICKEN_RICE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import paymelah.model.debt.Debt;
+import paymelah.model.debt.DebtList;
+
+/**
+ * A utility class to help with building Debt objects.
+ */
+public class DebtListBuilder {
+    public static final Debt DEFAULT_DEBT = CHICKEN_RICE;
+
+    private List<Debt> debtList = new ArrayList<>();
+
+    /**
+     * Creates a {@code DebtListBuilder} with the default details.
+     */
+    public DebtListBuilder() {
+        debtList.add(DEFAULT_DEBT);
+    }
+
+    /**
+     * Initializes the {@code DebtListBuilder} with the data of {@code debtToCopy}.
+     */
+    public DebtListBuilder(DebtList debtToCopy) {
+        debtList.addAll(debtToCopy.asList());
+    }
+
+    /**
+     * Sets the {@code Description} of the {@code Debt} that we are building.
+     */
+    public DebtListBuilder withDebt(Debt debt) {
+        debtList.add(debt);
+        return this;
+    }
+
+    public DebtList build() {
+        return DebtList.fromList(debtList);
+    }
+}
