d5a6ec8fbb6d71af43858fdc46a95ec71a41b273
KJunWei <khorjunweiwei@gmail.com>
2022-10-06 10:16:03 +0800

    Edit for checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -11,8 +11,10 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.position.*;
 import seedu.address.model.person.Remark;
+import seedu.address.model.person.position.Professor;
+import seedu.address.model.person.position.Student;
+import seedu.address.model.person.position.TeachingAssistant;
 import seedu.address.model.tag.Tag;
 
 /**
