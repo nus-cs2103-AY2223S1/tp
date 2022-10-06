eee6b8813245bad299db4f5aca20e7c53aeb7309
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-10-06 12:03:26 +0800

    Close application with command
    The app already has an "exit" command to close application
    which works well.
    
    Only the acknowledgement message could be made
    more specific to our own app.
    
    Let's change Address Book to Mass Linkers in
    acknowledgement message of ExitCommand class.

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -9,7 +9,7 @@ public class ExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exit";
 
-    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Address Book as requested ...";
+    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Mass Linkers as requested ...";
 
     @Override
     public CommandResult execute(Model model) {
