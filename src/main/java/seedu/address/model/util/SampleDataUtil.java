8798d1dddce59b5bd6f65e7c7eb08b5951b1513d
angkl0 <kuanglong456@gmail.com>
2022-10-06 13:28:40 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -13,9 +13,9 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.TaskList;
 import seedu.address.model.task.Deadline;
 import seedu.address.model.task.Module;
-import seedu.address.model.TaskList;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.TaskName;
 
