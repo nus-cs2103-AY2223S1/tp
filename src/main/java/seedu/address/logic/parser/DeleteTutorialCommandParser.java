c9bda1dca704631db41c2076c4b63a808e188134
Shawn532 <E0556732@u.nus.edu>
2022-10-06 18:50:17 +0800

    FIx the order of the import statement

--- src/main/java/seedu/address/logic/parser/DeleteTutorialCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTutorialCommandParser.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteTutorialCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 /**
  * Parses input arguments and creates a new DeleteTutorialCommand object
@@ -13,6 +13,7 @@ public class DeleteTutorialCommandParser {
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteTutorialCommand
      * and returns a DeleteTutorialCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteTutorialCommand parse(String args) throws ParseException {
