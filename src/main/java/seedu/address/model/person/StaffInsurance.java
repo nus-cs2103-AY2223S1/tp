926cb84d76012d734fed244e8bacd71e048a747f
maxng17 <maxngjl@gmail.com>
2022-10-07 00:34:20 +0800

    Add skeletal files for staff fields
    To enable the group to implement staff related logic,
    skeletal files for the staff fields should be added so that
    we can create staff objects.
    
    So lets add classes for the different staff fields.

---
+++ src/main/java/seedu/address/model/person/StaffInsurance.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import java.util.Locale;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class StaffInsurance {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Staff insurance should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    // TODO: 7/10/2022 check if this is the proper regex
+    public static final String VALIDATION_REGEX = "(true|false)";
+
+    public final String staffInsurance;
+
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param insurance A valid name.
+     */
+    public StaffInsurance(String insurance) {
+        requireNonNull(insurance);
+        checkArgument(isValidName(insurance.toLowerCase()), MESSAGE_CONSTRAINTS);
+        staffInsurance = insurance;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return staffInsurance;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof StaffInsurance // instanceof handles nulls
+                && staffInsurance.equals(((StaffInsurance) other).staffInsurance)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return staffInsurance.hashCode();
+    }
+}
