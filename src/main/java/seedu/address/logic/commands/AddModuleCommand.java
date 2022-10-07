a25deb0c4a05bf7c18914e8278ee481d459b2d84
Tan Chee Heng <ch.tan020@gmail.com>
2022-10-07 22:05:57 +0800

    Update add-module description in AddModuleCommand

--- src/main/java/seedu/address/logic/commands/AddModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/AddModuleCommand.java
@@ -1,11 +1,8 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -16,22 +13,15 @@ import seedu.address.model.module.Module;
  */
 public class AddModuleCommand extends Command {
 
-    public static final String COMMAND_WORD = "add";
+    public static final String COMMAND_WORD = "add-module";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a module to Plannit."
             + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "PHONE "
-            + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + PREFIX_MODULE + "MODULE_CODE "
+            + "[" + PREFIX_DESCRIPTION + "MODULE_TITLE]\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_MODULE + "CS1231S "
+            + PREFIX_DESCRIPTION + "Discrete Structures";
 
     public static final String MESSAGE_SUCCESS = "New module added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "Oops, it looks like you have already "
