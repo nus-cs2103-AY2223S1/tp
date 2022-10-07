a2679c9f7e26eb7b484ae9411d4ee4826ed91d19
nvmdava <86460216+nvmdava@users.noreply.github.com>
2022-10-07 12:26:13 +0800

    Update src/main/java/seedu/address/logic/parser/OpenCustomerCommandParser.java
    Co-authored-by: seelengxd <34371483+seelengxd@users.noreply.github.com>

--- src/main/java/seedu/address/logic/parser/OpenCustomerCommandParser.java
+++ src/main/java/seedu/address/logic/parser/OpenCustomerCommandParser.java
@@ -12,8 +12,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class OpenCustomerCommandParser implements Parser<OpenCustomerCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the OpenCustomerCommand
+     * and returns a OpenCustomerCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public OpenCustomerCommand parse(String args) throws ParseException {
