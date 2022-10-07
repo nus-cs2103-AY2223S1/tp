f4583ec1076ba15291150761c2dc385184ef7482
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-10-07 14:47:07 +0800

    Id: trimmed methods

--- src/main/java/seedu/address/model/person/Id.java
+++ src/main/java/seedu/address/model/person/Id.java
@@ -1,39 +1,22 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
+/**
+ * Represents a Person's id in the address book.
+ */
 public class Id {
 
     public static final String MESSAGE_CONSTRAINTS = "Id should only contain 3 digits and 1 character";
     public static final String VALIDATION_REGEX = "\\d{3}[a-zA-Z]";
     public final String value;
 
+    /**
+     * Constructor for Id.
+     * @param id A valid id.
+     */
     public Id(String id) {
         requireNonNull(id);
-        checkArgument(isValidId(id), MESSAGE_CONSTRAINTS);
         value = id.toUpperCase();
     }
-
-    public static boolean isValidId(String test) {
-        return test.length() == 4 && test.matches(VALIDATION_REGEX);
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this
-                || (other instanceof Id
-                && value.equals(((Id) other).value));
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
-
 }
