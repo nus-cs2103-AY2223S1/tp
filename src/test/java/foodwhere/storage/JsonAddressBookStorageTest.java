6a43082ec8c6bd7c4fd9caaf49908833a6c21e02
Clarence Chew <62918570+clarence-chew@users.noreply.github.com>
2022-10-07 15:30:21 +0800

    Fix checkstyle issue

--- src/test/java/foodwhere/storage/JsonAddressBookStorageTest.java
+++ src/test/java/foodwhere/storage/JsonAddressBookStorageTest.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
