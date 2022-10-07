2c89978ed6ef9a0a6963616d8eea0062d5596467
pratham31012002 <71367149+pratham31012002@users.noreply.github.com>
2022-10-07 15:23:56 +0800

    Update CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,7 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_SKILLTAG = new Prefix(("st/"));
-    public static final Prefix PREFIX_DEGREETAG = new Prefix(("st/"));
-    public static final Prefix PREFIX_JOBTYPETAG = new Prefix(("st/"));
+    public static final Prefix PREFIX_DEGREETAG = new Prefix(("dt/"));
+    public static final Prefix PREFIX_JOBTYPETAG = new Prefix(("jtt/"));
 
 }
