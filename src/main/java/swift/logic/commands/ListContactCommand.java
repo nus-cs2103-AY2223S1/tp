9419076ad753013af920bb526ba3f8dc293f1734
peppapighs <ptk.prasertsuk@gmail.com>
2022-10-08 00:40:57 +0800

    Chore: rename ListCommand to ListContactCommand

--- src/main/java/swift/logic/commands/ListContactCommand.java
+++ src/main/java/swift/logic/commands/ListContactCommand.java
@@ -8,7 +8,7 @@ import swift.model.Model;
 /**
  * Lists all persons in the address book to the user.
  */
-public class ListCommand extends Command {
+public class ListContactCommand extends Command {
 
     public static final String COMMAND_WORD = "list_contact";
 
--- src/test/java/swift/logic/commands/ListCommandTest.java
+++ src/test/java/swift/logic/commands/ListCommandTest.java
@@ -13,7 +13,7 @@ import swift.model.ModelManager;
 import swift.model.UserPrefs;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for ListContactCommand.
  */
 public class ListCommandTest {
 
@@ -28,12 +28,12 @@ public class ListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListContactCommand(), model, ListContactCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListContactCommand(), model, ListContactCommand.MESSAGE_SUCCESS, expectedModel);
     }
 }
