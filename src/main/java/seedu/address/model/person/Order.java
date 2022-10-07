1d6240ef754696080c35280d253d986afc6fb112
wweqg <e0726629@u.nus.edu>
2022-10-07 20:27:03 +0800

    Update simple sortCommand

--- src/main/java/seedu/address/model/person/Order.java
+++
@@ -1,9 +0,0 @@
-package seedu.address.model.person;
-
-//stubs for Order class
-public class Order {
-
-    public int compareTo(Order order) {
-        return 0;
-    }
-}
--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import seedu.address.model.order.Order;
 import seedu.address.model.tag.Tag;
 
 /**
