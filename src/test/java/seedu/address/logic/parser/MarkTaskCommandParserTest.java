0cf8d3db30756c248aba401c35c018cb56e296de
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-10-07 23:06:24 +0800

    Add unit tests for MarkTaskCommandParser to improve coverage

---
+++ src/test/java/seedu/address/logic/parser/MarkTaskCommandParserTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.MarkTaskCommand;
+
+public class MarkTaskCommandParserTest {
+    private MarkTaskCommandParser parser = new MarkTaskCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsMarkTaskCommand() {
+        assertParseSuccess(parser, "1", new MarkTaskCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkTaskCommand.MESSAGE_USAGE));
+    }
+}
