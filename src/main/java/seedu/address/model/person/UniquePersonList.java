a89305d149e88ef373c08784f9bf5122977b2186
ChongCheeKaiClarence <e0725313.u.nus.edu>
2022-10-07 10:00:12 +0800

    Update CI

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -3,7 +3,6 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
