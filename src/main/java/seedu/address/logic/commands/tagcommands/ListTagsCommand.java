150fba3b3a55086d78c0322fc993e19323cff240
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-10-07 17:01:35 +0800

    Organize commands to subpackages based on their type.

---
+++ src/main/java/seedu/address/logic/commands/tagcommands/ListTagsCommand.java
@@ -0,0 +1,5 @@
+package seedu.address.logic.commands.tagcommands;
+
+public class ListTagsCommand {
+    //To be implemented
+}
--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -18,7 +18,9 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.generalcommands.ClearCommand;
+import seedu.address.logic.commands.personcommands.EditCommand;
+import seedu.address.logic.commands.personcommands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
