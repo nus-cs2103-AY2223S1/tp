080aff1578407e6a8abcee6aac4065985f632f25
therealdaofu <xu.daofu20@gmail.com>
2022-10-07 19:36:06 +0800

    Add List Module command

--- src/main/java/seedu/address/logic/commands/ListModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/ListModuleCommand.java
@@ -5,9 +5,9 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
+/**
+ * List all modules in the address book to the user
+ */
 public class ListModuleCommand extends Command {
 
     public static final String COMMAND_WORD = "list m";
