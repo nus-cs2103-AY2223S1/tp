70c7898549b506fea3753ea87343cadbf4aadea8
Ryan <ryanlml123@gmail.com>
2022-10-08 06:58:45 +0800

    Revise order of import statements

--- src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
@@ -1,15 +1,18 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.ViewCommand;
-import seedu.address.model.person.PersonContainsAttributePredicate;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ViewCommand;
+import seedu.address.model.person.PersonContainsAttributePredicate;
+
+
 
 public class ViewCommandParserTest {
 
