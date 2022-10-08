0ed17daf97299325ce13a2365520d51e108985cc
tantzeyeong <tzeyeongtan@gmail.com>
2022-10-08 18:41:27 +0800

    Delete unused exceptions

--- src/main/java/seedu/address/model/person/student/exceptions/StudentNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.person.student.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified student.
- */
-public class StudentNotFoundException extends RuntimeException {}
--- src/main/java/seedu/address/model/person/tutor/exceptions/TutorNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.person.tutor.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified tutor.
- */
-public class TutorNotFoundException extends RuntimeException {}
