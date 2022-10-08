498122b58ae3bff932f3ccf9c28740fb777c9acc
lpohsien <popopo4407@gmail.com>
2022-10-08 15:51:38 +0800

    Feature: Add addtag for single index

--- src/main/java/seedu/address/logic/commands/AddTagCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -1,19 +1,28 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
+import seedu.address.MainApp;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 
 /**
  * Add tag for an existing person in the address book.
  */
 public class AddTagCommand extends Command {
+
+    public static final Logger LOGGER = LogsCenter.getLogger(MainApp.class);
 
     public static final String COMMAND_WORD = "addtag";
 
@@ -30,13 +39,19 @@ public class AddTagCommand extends Command {
         + PREFIX_TAG + "tutorial "
         + PREFIX_TAG + "needRemedial";
 
-    /** 
-     * Index of the person to add the tags for. 
+    public static final String MESSAGE_ADD_TAGS_SUCCESS = "Added tags %1$s Person: %2$s";
+
+    public static final String MESSAGE_TAGS_NOT_ADDED = "At least one tag must be added";
+
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+
+    /**
+     * Index of the person to add the tags for.
      * If index is null, the tags will be added for every person in the list.
      */
     private Index index;
-    
-    /** The tags to be added. */
+
+    /** The tags to be added.*/
     private Set<Tag> tags;
 
     /**
@@ -51,8 +66,27 @@ public class AddTagCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Set<Tag> existingTags = personToEdit.getTags();
+        for (Tag tag : existingTags) {
+            if (!this.tags.contains(tag)) {
+                this.tags.add(tag);
+            }
+        }
+        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+        editPersonDescriptor.setTags(this.tags);
+        LOGGER.info((new EditCommand(index, editPersonDescriptor)).execute(model).getFeedbackToUser());
+
+
         String indexStr = this.index == null ? "all" : String.valueOf(index.getOneBased());
-        return new CommandResult(String.format(MESSAGE_ARGUMENTS, indexStr, this.tags));
+        return new CommandResult(String.format(MESSAGE_ADD_TAGS_SUCCESS, this.tags.toString(), indexStr));
     }
 }
--- src/main/java/seedu/address/logic/parser/AddTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddTagCommandParser.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Optional;
 import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
@@ -15,25 +14,33 @@ import seedu.address.logic.commands.AddTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
 
+/**
+ * Parses input arguments and creates a new AddTagCommand object
+ */
 public class AddTagCommandParser implements Parser<AddTagCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddTagCommand
+     * and returns an AddTagCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public AddTagCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
-                PREFIX_TAG);
-    
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
         Index index;
         Set<Tag> tags;
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
             tags = parseTagsSet(argMultimap.getAllValues(PREFIX_TAG));
             if (tags.size() == 0) {
-                throw new IllegalValueException("Tags cannot be empty for addtag");
+                throw new ParseException(AddTagCommand.MESSAGE_TAGS_NOT_ADDED);
             }
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     AddTagCommand.MESSAGE_USAGE), ive);
         }
-    
+
         return new AddTagCommand(index, tags);
     }
 
