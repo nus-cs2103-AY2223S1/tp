75d73c53694c95aeb1816cf03baebf5501f0cd3c
see leng <seelengxd@gmail.com>
2022-10-08 16:22:28 +0800

    Test: add tests for fee

--- src/test/java/seedu/address/model/commission/TitleTest.java
+++ src/test/java/seedu/address/model/commission/TitleTest.java
@@ -1,12 +1,12 @@
 package seedu.address.model.commission;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class TitleTest {
 
