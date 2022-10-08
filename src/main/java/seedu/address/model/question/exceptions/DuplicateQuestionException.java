38faab82aa9fa307d7422b81b8ab0e1989e169f6
ketamethane <cheonghsien@gmail.com>
2022-10-08 20:24:35 +0800

    Dummy commit

--- src/main/java/seedu/address/model/question/exceptions/DuplicateQuestionException.java
+++ src/main/java/seedu/address/model/question/exceptions/DuplicateQuestionException.java
@@ -2,8 +2,7 @@ package seedu.address.model.question.exceptions;
 
 /**
  * Signals that the operation will result in duplicate Questions (Questions are considered duplicates if they have the
- * same
- * identity).
+ * same identity).
  */
 public class DuplicateQuestionException extends RuntimeException {
     public DuplicateQuestionException() {
