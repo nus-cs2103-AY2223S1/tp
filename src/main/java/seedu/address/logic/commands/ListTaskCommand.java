3eb5aa11758ba90024a7515111df4e509c51a14a
BlopApple <junjie1363@gmail.com>
2022-10-08 12:19:07 +0800

    Add ListTaskCommand and update AddressBookParser

---
+++ src/main/java/seedu/address/logic/commands/ListTaskCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.model.Model;
+
+/**
+ * Lists all tasks associated to all patients in the address book to the user.
+ */
+public class ListTaskCommand extends Command {
+
+    public static final String COMMAND_WORD = "listTask";
+
+    public static final String MESSAGE_SUCCESS = "Listed all tasks";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
