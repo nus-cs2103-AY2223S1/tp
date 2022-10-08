92234f32658830468db41729b9396fce3d256c48
wrewsama <andrewlzs512@gmail.com>
2022-10-08 17:57:42 +0800

    Ui: Add dp and profile info container to ProfileBox

--- src/main/java/seedu/address/ui/ProfileBox.java
+++ src/main/java/seedu/address/ui/ProfileBox.java
@@ -1,8 +1,12 @@
 package seedu.address.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 
 
 public class ProfileBox extends UiPart<Region> {
@@ -11,7 +15,16 @@ public class ProfileBox extends UiPart<Region> {
     @FXML
     private HBox box;
 
+    @FXML
+    private ImageView dp;
+
+    @FXML
+    private VBox profileInfoContainer;
+
+    private Image defaultDp = new Image(this.getClass().getResourceAsStream("/images/default.png"));
+
     public ProfileBox() {
         super(FXML);
+        dp.setImage(defaultDp);
     }
 }
