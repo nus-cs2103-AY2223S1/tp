8e9daad37dde2fa0ec23a0759595c52023a4636c
wrewsama <andrewlzs512@gmail.com>
2022-10-08 18:19:30 +0800

    Ui: Add placeholders for ProfileInfoBox

--- src/main/java/seedu/address/ui/ProfileInfoBox.java
+++ src/main/java/seedu/address/ui/ProfileInfoBox.java
@@ -1,6 +1,7 @@
 package seedu.address.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
@@ -10,7 +11,23 @@ public class ProfileInfoBox extends UiPart<Region> {
     @FXML
     private VBox box;
 
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label github;
+
+    @FXML
+    private Label timezone;
+
     public ProfileInfoBox() {
         super(FXML);
+        name.setText("name");
+        title.setText("title");
+        github.setText("github");
+        timezone.setText("time zone");
     }
 }
