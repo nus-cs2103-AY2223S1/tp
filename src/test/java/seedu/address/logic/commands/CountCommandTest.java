0ae7c914f0b89b68e7c60134417f10a82df13990
kxrt <kvrtikeya@gmail.com>
2022-10-09 01:38:07 +0800

    Fix import order

--- src/test/java/seedu/address/logic/commands/CountCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CountCommandTest.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 class CountCommandTest {
 
