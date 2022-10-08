fad0c5094e10158641bb4943e01888a84ea16682
ALWHgithub <alwhgithub@gmail.com>
2022-10-08 12:25:21 +0800

    add Deadline and Category

---
+++ src/main/java/seedu/address/model/task/TaskCategory.java
@@ -0,0 +1,69 @@
+package seedu.address.model.task;
+
+
+
+public class TaskCategory {
+    private int level;
+    private TaskCategoryType taskCategoryType;
+
+    public TaskCategory(int level, TaskCategoryType taskCategoryType){
+        this.level = level;
+        this.taskCategoryType = taskCategoryType;
+    }
+
+    /**
+     * Returns the category of a task.
+     *
+     * @return The task category.
+     */
+    public TaskCategoryType TaskCategoryType() {
+        return this.taskCategoryType;
+    }
+
+    /**
+     * Returns the level of a task.
+     *
+     * @return The task level.
+     */
+    public int getLevel() {
+        return this.level;
+    }
+
+
+    /**
+     * Returns hashcode of the current object
+     *
+     * @return Hashcode of the object.
+     */
+    @Override
+    public int hashCode() {
+        return taskCategoryType.hashCode();
+    }
+
+
+    /**
+     * Returns the string representation of the task category.
+     *
+     * @return Task priority.
+     */
+    @Override
+    public String toString() {
+        return level + taskCategoryType.name();
+    }
+
+    /**
+     * Compares another object with the Category object.
+     *
+     * @param other The other object to be compared to.
+     * @return If the two objects are equal.
+     */
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TaskCategory // instanceof handles nulls
+                && ((TaskCategory) other).level == this.level
+                && ((TaskCategory) other).taskCategoryType.equals(this.taskCategoryType)
+        );
+    }
+
+}
---
+++ src/main/java/seedu/address/model/task/TaskCategoryType.java
@@ -0,0 +1,11 @@
+package seedu.address.model.task;
+
+enum TaskCategoryType {
+        DATABASE,
+        Backend,
+        Frontend,
+        UIUX,
+        PRESENTATION,
+        OTHERS;
+}
+
