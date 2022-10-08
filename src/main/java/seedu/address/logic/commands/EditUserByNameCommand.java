6f135f0f355a29e62e016024aa981e5caf781a3a
CedricChia123 <cedricchia01@gmail.com>
2022-10-08 23:34:11 +0800

    Add edituserbyname command

---
+++ src/main/java/seedu/address/logic/commands/EditUserByNameCommand.java
@@ -0,0 +1,132 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Assignment;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.FullNamePredicate;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Edits the details of an existing person in the address book when user provides a name.
+ */
+public class EditUserByNameCommand extends Command {
+
+    public static final String COMMAND_WORD = "editbyname";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
+            + "by their full name. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: NAME (must be exactly the same as person's name) "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_PHONE + "91234567 "
+            + PREFIX_EMAIL + "johndoe@example.com";
+
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+
+    private final EditCommand.EditPersonDescriptor editPersonDescriptor;
+    private final FullNamePredicate predicate;
+
+    /**
+     * @param predicate condition to test if queried person has same name as that in the list
+     * @param editPersonDescriptor details to edit the person with
+     */
+    public EditUserByNameCommand(FullNamePredicate predicate, EditCommand.EditPersonDescriptor editPersonDescriptor) {
+        requireNonNull(editPersonDescriptor);
+
+        this.editPersonDescriptor = new EditCommand.EditPersonDescriptor(editPersonDescriptor);
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        int targetIndex = -1;
+        for (int i = 0; i < lastShownList.size(); i++) {
+            Person currentPerson = lastShownList.get(i);
+            if (predicate.test(currentPerson)) {
+                targetIndex = i;
+                break;
+            }
+        }
+        if (targetIndex == -1) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_NAME);
+        }
+
+        Person personToEdit = lastShownList.get(targetIndex);
+        Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
+
+        if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Person createEditedPerson(Person personToEdit, EditCommand.EditPersonDescriptor
+            editPersonDescriptor) {
+        assert personToEdit != null;
+
+        Name updatedName = editPersonDescriptor.getName().orElse(personToEdit.getName());
+        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
+        Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
+        Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
+        Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
+
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedTags,
+                personToEdit.getAssignments());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditUserByNameCommand)) {
+            return false;
+        }
+
+        // state check
+        EditUserByNameCommand e = (EditUserByNameCommand) other;
+        return predicate.equals((e.predicate))
+                && editPersonDescriptor.equals(e.editPersonDescriptor);
+    }
+
+}
