a75bc98055194e741cb290158f106f260f7552a9
Chee Jer Hong <e0407708@u.nus.edu>
2022-10-08 23:34:41 +0800

    Standardize dateString input format for InterviewDate

--- src/main/java/seedu/address/model/application/interview/InterviewDate.java
+++ src/main/java/seedu/address/model/application/interview/InterviewDate.java
@@ -15,13 +15,13 @@ public class InterviewDate {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Date should be in the format of dd-mm-yyyy with proper month and leap year support";
-    //Only valid if dd-mm-yyyy with month and leap year support
+    //Only valid if yyyy-mm-dd with month and leap year support
     //Retrieve from https://stackoverflow.com/questions/15491894/regex-to-validate-date-formats-dd-mm-yyyy-dd-mm-yyyy
     // -dd-mm-yyyy-dd-mmm-yyyy
-    public static final String VALIDATION_REGEX = "^(?:(?:31(-)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)"
-            + "(-)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(-)0?2\\3(?:("
-            + "?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))"
-            + "$|^(?:0?[1-9]|1\\d|2[0-8])(-)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+    public static final String VALIDATION_REGEX = "^(?:(?:1[6-9]|[2-9]\\d)?\\d{2})(?:(?:(\\/|-|\\.)(?:0?[13578]|1[02])"
+            + "\\1(?:31))|(?:(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2(?:29|30)))$|^(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468]"
+            + "[048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(\\/|-|\\.)0?2\\3(?:29)$|^(?:(?:1[6-9]|[2-9]\\"
+            + "d)?\\d{4})(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:0?[1-9]|1\\d|2[0-8])$";
     public final LocalDate value;
 
     /**
