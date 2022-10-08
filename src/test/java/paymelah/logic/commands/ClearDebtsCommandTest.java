7e3d1ed694195fda076aa36a440b788d2dae2941
WR3nd3 <wongrende@gmail.com>
2022-10-08 20:22:16 +0800

    Add tests for ClearDebtsCommand

--- src/test/java/paymelah/logic/commands/ClearDebtsCommandTest.java
+++ src/test/java/paymelah/logic/commands/ClearDebtsCommandTest.java
@@ -2,15 +2,24 @@ package paymelah.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static paymelah.logic.commands.CommandTestUtil.assertCommandFailure;
+import static paymelah.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static paymelah.logic.commands.CommandTestUtil.showDebtors;
 import static paymelah.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static paymelah.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static paymelah.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import paymelah.commons.core.Messages;
+import paymelah.commons.core.index.Index;
+import paymelah.model.AddressBook;
 import paymelah.model.Model;
 import paymelah.model.ModelManager;
 import paymelah.model.UserPrefs;
+import paymelah.model.debt.DebtList;
+import paymelah.model.person.Person;
+
 
 
 /**
@@ -22,6 +31,63 @@ public class ClearDebtsCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     //TODO Add more tests
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Person debtorToClear = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        ClearDebtsCommand clearDebtsCommand = new ClearDebtsCommand(INDEX_FIRST_PERSON);
+
+        Person clearedDebtor = new Person(debtorToClear.getName(), debtorToClear.getPhone(),
+                debtorToClear.getEmail(), debtorToClear.getAddress(), debtorToClear.getTags(),
+                new DebtList());
+
+        String expectedMessage = String.format(ClearDebtsCommand.MESSAGE_CLEAR_DEBTS_SUCCESS, clearedDebtor);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), clearedDebtor);
+
+        assertCommandSuccess(clearDebtsCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        ClearDebtsCommand clearDebtsCommand = new ClearDebtsCommand(outOfBoundIndex);
+
+        assertCommandFailure(clearDebtsCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showDebtors(model);
+
+        Person debtorToClear = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        ClearDebtsCommand clearDebtsCommand = new ClearDebtsCommand(INDEX_SECOND_PERSON);
+
+        Person clearedDebtor = new Person(debtorToClear.getName(), debtorToClear.getPhone(),
+                debtorToClear.getEmail(), debtorToClear.getAddress(), debtorToClear.getTags(),
+                new DebtList());
+
+        String expectedMessage = String.format(ClearDebtsCommand.MESSAGE_CLEAR_DEBTS_SUCCESS, clearedDebtor);
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList()
+                .get(INDEX_SECOND_PERSON.getZeroBased()), clearedDebtor);
+
+        assertCommandSuccess(clearDebtsCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showDebtors(model);
+
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        ClearDebtsCommand clearDebtsCommand = new ClearDebtsCommand(outOfBoundIndex);
+
+        assertCommandFailure(clearDebtsCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
 
     @Test
     public void equals() {
