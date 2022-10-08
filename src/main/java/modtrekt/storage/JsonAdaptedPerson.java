221386bc2d141d5b6e9ac332ae1823c6a6a31dad
domoboom <berzindominic@gmail.com>
2022-10-08 12:11:35 +0800

    Modify files to ensure that entered data is saved to storage

--- src/main/java/modtrekt/storage/JsonAdaptedPerson.java
+++ src/main/java/modtrekt/storage/JsonAdaptedPerson.java
@@ -81,29 +81,29 @@ class JsonAdaptedPerson {
         }
         final Name modelName = new Name(name);
 
-        if (phone == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
-        }
-        if (!Phone.isValidPhone(phone)) {
-            throw new IllegalValueException(Phone.MESSAGE_CONSTRAINTS);
-        }
-        final Phone modelPhone = new Phone(phone);
-
-        if (email == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
-        }
-        if (!Email.isValidEmail(email)) {
-            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
-        }
-        final Email modelEmail = new Email(email);
-
-        if (address == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
-        }
-        if (!Address.isValidAddress(address)) {
-            throw new IllegalValueException(Address.MESSAGE_CONSTRAINTS);
-        }
-        final Address modelAddress = new Address(address);
+//        if (phone == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
+//        }
+//        if (!Phone.isValidPhone(phone)) {
+//            throw new IllegalValueException(Phone.MESSAGE_CONSTRAINTS);
+//        }
+//        final Phone modelPhone = new Phone(phone);
+//
+//        if (email == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+//        }
+//        if (!Email.isValidEmail(email)) {
+//            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
+//        }
+//        final Email modelEmail = new Email(email);
+//
+//        if (address == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
+//        }
+//        if (!Address.isValidAddress(address)) {
+//            throw new IllegalValueException(Address.MESSAGE_CONSTRAINTS);
+//        }
+//        final Address modelAddress = new Address(address);
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
         return new Person(modelName);
---
+++ src/main/java/modtrekt/storage/JsonAdaptedTask.java
@@ -0,0 +1,60 @@
+package modtrekt.storage;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import modtrekt.commons.exceptions.IllegalValueException;
+import modtrekt.model.person.Name;
+import modtrekt.model.person.Person;
+import modtrekt.model.tag.Tag;
+
+public class JsonAdaptedTask {
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Person's %s field is missing!";
+
+    private final String name;
+    private final List<JsonAdaptedTag> tagged = new ArrayList<>();
+
+    /**
+     * Constructs a {@code JsonAdaptedPerson} with the given person details.
+     */
+    @JsonCreator
+    public JsonAdaptedTask(@JsonProperty("name") String name) {
+        this.name = name;
+    }
+
+    /**
+     * Converts a given {@code Person} into this class for Jackson use.
+     */
+    public JsonAdaptedTask(Person source) {
+        name = source.getName().fullName;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted person object into the model's {@code Person} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person.
+     */
+    public Person toModelType() throws IllegalValueException {
+        final List<Tag> personTags = new ArrayList<>();
+        for (JsonAdaptedTag tag : tagged) {
+            personTags.add(tag.toModelType());
+        }
+
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name);
+
+        final Set<Tag> modelTags = new HashSet<>(personTags);
+        return new Person(modelName);
+    }
+
+}
