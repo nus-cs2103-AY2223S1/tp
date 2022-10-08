6dffcfe8d2f4e22e16a2f614f703e33728999dff
Aloynz <aloysiusneozeen@gmail.com>
2022-10-08 23:33:15 +0800

    Add functionalities for deleteuser command

---
+++ src/main/java/seedu/address/logic/parser/DeleteUserByNameCommandParser.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import seedu.address.logic.commands.DeleteUserByNameCommand;
+import seedu.address.model.person.FullNamePredicate;
+
+public class DeleteUserByNameCommandParser implements Parser<DeleteUserByNameCommand> {
+
+    public DeleteUserByNameCommand parse(String args) throws ParseException {
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args);
+            if (!argumentMultimap.getPreamble().isEmpty()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteUserByNameCommand.MESSAGE_USAGE));
+            }
+
+            String name = argumentMultimap.getPreamble();
+            return new DeleteUserByNameCommand(new FullNamePredicate(name));
+
+
+
+    }
+}
