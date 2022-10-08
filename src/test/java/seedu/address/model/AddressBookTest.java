b0d5c1f1bcb74c0e62147eaf3b1f92bd5a76757d
dlimyy <douglas.lim.yy@gmail.com>
2022-10-09 01:09:19 +0800

    Update test classes
    Updated test classes to handle newly added Task class.

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -21,6 +21,7 @@ import javafx.collections.ObservableList;
 import seedu.address.model.module.Module;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.task.Task;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddressBookTest {
@@ -103,6 +104,11 @@ public class AddressBookTest {
         @Override
         public ObservableList<Module> getModuleList() {
             return modules;
+        }
+
+        @Override
+        public ObservableList<Task> getTaskList() {
+            return null;
         }
     }
 
