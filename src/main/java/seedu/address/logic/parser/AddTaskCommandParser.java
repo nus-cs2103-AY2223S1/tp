847dc1af49c16d0cd18dc38d33cc9a0f662873f9
DevanshShah1309 <devanshshah1309@gmail.com>
2022-10-08 21:13:51 +0800

    Add tests for AddTaskCommandParser

--- src/main/java/seedu/address/logic/parser/AddTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddTaskCommandParser.java
@@ -10,7 +10,7 @@ import seedu.address.model.team.Task;
 /**
  * Parses input arguments and creates a new AddTaskCommand object
  */
-public class AddTaskCommandParser {
+public class AddTaskCommandParser implements Parser<AddTaskCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the AddTaskCommand
