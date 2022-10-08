7c703875e911791c06f61ff45d3a77411847aea6
BlopApple <junjie1363@gmail.com>
2022-10-08 18:39:51 +0800

    Fix ListTaskCommand to only display patients with tasks

--- src/main/java/seedu/address/logic/commands/ListTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTaskCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
@@ -19,7 +18,7 @@ public class ListTaskCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setTaskListFlag(true);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredPersonList(p -> !(p.getTasks().isEmpty()));
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
