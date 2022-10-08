c7b61f4e21316996d26c02850fb89ff2aee4aa4e
Julian Wong <e0725200@u.nus.edu>
2022-10-08 14:25:38 +0800

    Update AddCommand.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Adds a person to the address book.
+ * Adds a person to uNivUSal.
  */
 public class AddCommand extends Command {
 
