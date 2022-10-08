5fed7327236ac60e84fcd66ba87c3890d4c1ebce
DevanshShah1309 <devanshshah1309@gmail.com>
2022-10-08 21:01:01 +0800

    Add TaskCard and TaskListPanel component

---
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -0,0 +1,49 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+import javax.swing.plaf.synth.Region;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.team.Task;
+
+/**
+ * Panel containing the list of tasks.
+ */
+public class TaskListPanel extends UiPart<Region> {
+
+    private static final String FXML = "TaskListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+
+    @javafx.fxml.FXML
+    private ListView<Task> taskListView;
+
+    /**
+     * Creates a {@code TaskListPanel} with the given {@code ObservableList}.
+     */
+    public TaskListPanel(ObservableList<Task> taskList) {
+        super(FXML);
+        taskListView.setItems(taskList);
+        taskListView.setCellFactory(listView -> new TaskListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Task} using a {@code TaskCard}.
+     */
+    class TaskListViewCell extends ListCell<Task> {
+        @Override
+        protected void updateItem(Task task, boolean empty) {
+            super.updateItem(task, empty);
+
+            if (empty || task == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new TaskCard(task, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
