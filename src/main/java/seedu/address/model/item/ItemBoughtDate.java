18fa14a6857c60b0457a65bb50c55bb4871d7821
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-08 12:28:43 +0800

    Split ItemDate.java into ItemBoughtDate.java and ItemExpiryDate.java
    This allows ItemDates to follow the open-closed principle.

--- src/main/java/seedu/address/model/item/ItemBoughtDate.java
+++ src/main/java/seedu/address/model/item/ItemBoughtDate.java
@@ -5,13 +5,13 @@ import static java.util.Objects.requireNonNull;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import seedu.address.model.item.itemvalidator.ItemDateValidator;
+import seedu.address.model.item.itemvalidator.ItemBoughtDateValidator;
 
 /**
  * Represents an item date in an {@link Item}.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
-public class ItemDate {
+public class ItemBoughtDate {
 
     private static final String DATE_OUTPUT_PATTERN_REGEX = "dd/MM/yyyy";
 
@@ -22,21 +22,21 @@ public class ItemDate {
      *
      * @param dateString a string that represents the itemDate of the format
      */
-    public ItemDate(String dateString) {
+    public ItemBoughtDate(String dateString) {
         requireNonNull(dateString);
-        ItemDateValidator.validate(dateString);
+        ItemBoughtDateValidator.validate(dateString);
         itemDate = LocalDate.parse(dateString);
     }
 
     /**
-     * Returns true if both {@link ItemDate#itemDate} have the same date by
+     * Returns true if both {@link ItemBoughtDate#itemDate} have the same date by
      * {@link LocalDate#equals(Object)}.
      */
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof ItemDate // instanceof handles nulls
-                && itemDate.equals(((ItemDate) other).itemDate)); // state check
+                || (other instanceof ItemBoughtDate // instanceof handles nulls
+                && itemDate.equals(((ItemBoughtDate) other).itemDate)); // state check
     }
 
     /**
