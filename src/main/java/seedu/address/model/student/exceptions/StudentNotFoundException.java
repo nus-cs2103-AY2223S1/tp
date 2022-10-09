68cde436c0b0b7e9b0eb2d96df3780fbc8592097
qiaoen17 <92434807+qiaoen17@users.noreply.github.com>
2022-10-09 20:41:44 +0800

    Add exceptions for student class

---
+++ src/main/java/seedu/address/model/student/exceptions/StudentNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.student.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified question.
+ */
+public class StudentNotFoundException extends RuntimeException {}
