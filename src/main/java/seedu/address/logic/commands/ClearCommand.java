6821a4bde44264bf51946b31e8ba8eb69682a298
Jonaspng <jonaspng@hotmail.com>
2022-10-09 22:11:35 +0800

    Modify clear command

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
@@ -10,13 +11,18 @@ import seedu.address.model.Model;
  */
 public class ClearCommand extends Command {
 
-    public static final String COMMAND_WORD = "clear";
+    public static final String COMMAND_WORD = "clear-confirm";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-
+    public static final String MESSAGE_CONFIRMATION = "You are attempting to clear all addresses in the memory."
+            + " Add '-confirm' to clear all addresses.";
+    public static final String MESSAGE_EMPTY_LIST = "Address book is already empty!";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        if (model.getAddressBook().getPersonList().size() == 0) {
+            throw new CommandException(MESSAGE_EMPTY_LIST);
+        }
         model.setAddressBook(new AddressBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
