8acd76c51842cdfbbc60e20b5b7fd6f727328bb6
Deon <deeyonn@gmail.com>
2022-10-09 21:24:54 +0800

    Modify PREFIX_ADDRESS in CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -9,7 +9,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("address/");
     public static final Prefix PREFIX_GENDER = new Prefix("gender/");
     public static final Prefix PREFIX_UNIVERSITY = new Prefix("university/");
     public static final Prefix PREFIX_TAG = new Prefix("tag/");
