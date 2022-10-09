70935553f2caf3ccfe5de642b05760e1915f3591
sltsheryl <sltsheryl@gmail.com>
2022-10-09 11:29:41 +0800

    Fix AddressBookParserTest error * Add GitHub to PersonUtil class

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -20,7 +20,7 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
-            + "Parameters: "
+            + "\nParameters: "
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
