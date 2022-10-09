26bd9a3288923e09520fd7f80ce01418a5863128
neosunhan <neosunhan@gmail.com>
2022-10-09 22:47:56 +0800

    Rename TaskListCard.fxml

--- src/main/java/jarvis/ui/TaskCard.java
+++ src/main/java/jarvis/ui/TaskCard.java
@@ -11,7 +11,7 @@ import javafx.scene.layout.Region;
  */
 public class TaskCard extends UiPart<Region> {
 
-    private static final String FXML = "TaskListCard.fxml";
+    private static final String FXML = "TaskCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
