44294a5d245cee6b61cae1d1b10c86121e84f89f
AkkFiros <81766175+AkkFiros@users.noreply.github.com>
2022-10-09 13:56:40 +0800

    Fix CheckStyle Error

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,27 +1,13 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CALORIE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.Prefix;
-import seedu.address.model.Calorie;
 import seedu.address.model.Model;
-import seedu.address.model.person.DateTime;
 import seedu.address.model.person.Food;
-import seedu.address.model.person.IsFoodAddedTodayPredicate;
-import seedu.address.model.person.Name;
-import seedu.address.model.tag.Tag;
 
 /**
  * Adds a person to the address book.
