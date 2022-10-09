53a207642a201354a284305ff70829a9ebcc081e
emptygx <e0717559@u.nus.edu>
2022-10-09 19:03:52 +0800

    Fix checkstyle issues with imports

--- src/main/java/seedu/address/logic/parser/FindPhoneCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindPhoneCommandParser.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.FindPhoneCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.PhoneContainsKeywordsPredicate;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import seedu.address.logic.commands.FindPhoneCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.PhoneContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
--- src/main/java/seedu/address/model/person/PhoneContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/PhoneContainsKeywordsPredicate.java
@@ -1,9 +1,9 @@
 package seedu.address.model.person;
 
-import seedu.address.commons.util.StringUtil;
-
 import java.util.List;
 import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
 
 /**
  * Tests that a {@code Person}'s {@code Address} matches any of the keywords given.
