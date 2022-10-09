2c601eefde3ead86d33b562845ee671ec7247917
ElginL <elginlee_2000@hotmail.com>
2022-10-09 15:04:50 +0800

    Update JeeqTrackerParser
    Add view as a valid command

--- src/main/java/seedu/address/logic/parser/JeeqTrackerParser.java
+++ src/main/java/seedu/address/logic/parser/JeeqTrackerParser.java
@@ -16,6 +16,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.UserGuideCommand;
+import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -71,6 +72,9 @@ public class JeeqTrackerParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommandParser().parse(arguments);
+
+        case ViewCommand.COMMAND_WORD:
+            return new ViewCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
