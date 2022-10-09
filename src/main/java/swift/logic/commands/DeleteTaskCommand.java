8a205779d5de54bd3ae8b1967c85c9302b446f32
ShenyiCui <shenyicui@outlook.com>
2022-10-10 00:20:34 +0800

    Feat: add Delete Task Command

---
+++ src/main/java/swift/logic/commands/DeleteTaskCommand.java
@@ -0,0 +1,50 @@
+package swift.logic.commands;
+
+import swift.commons.core.Messages;
+import swift.commons.core.index.Index;
+import swift.logic.commands.exceptions.CommandException;
+import swift.model.Model;
+import swift.model.task.Task;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class DeleteTaskCommand extends Command {
+
+    public static final String COMMAND_WORD = "delete_task";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the task identified by the index number used in the displayed task list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted task: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteTaskCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Task> lastShownList = model.getFilteredTaskList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        Task taskToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteTask(taskToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteTaskCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteTaskCommand) other).targetIndex)); // state check
+    }
+}
