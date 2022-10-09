9f1303a6d595dcb531200419f21f828a6e524c42
yuehernkang <yuehernkang@hotmail.com>
2022-10-09 14:59:58 +0800

    Add PropertyTest.java for testing Property model class

---
+++ src/test/java/seedu/condonery/model/property/PropertyTest.java
@@ -0,0 +1,15 @@
+package seedu.condonery.model.property;
+
+import static seedu.condonery.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PropertyTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Property(null, null, null));
+    }
+
+}
