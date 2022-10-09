02c0e8f1af6578202430c6a98298c013d378dcf8
Koh Chen Huang, Samuel <samsamsailorman@gmail.com>
2022-10-09 15:43:31 +0800

    Improve 'Find' command
    Find command now supports specific queries for Phone numbers and Email addresses, and should only return 1 specific resulting Customer.

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,13 +1,19 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Email;
 import seedu.address.model.person.Person;
-
+import seedu.address.model.person.Phone;
 
 
 /**
@@ -25,14 +31,46 @@ public class FindCommand extends Command {
 
     private final Predicate<Person> predicate;
 
+    private Index targetIndex;
+
+    private FindPersonDescriptor findPersonDescriptor;
+
     public FindCommand(Predicate<Person> predicate) {
         this.predicate = predicate;
     }
 
+    /**
+     * Constructor for FindCommand.
+     *
+     * @param findPersonDescriptor contains details for the customer we are finding.
+     */
+    public FindCommand(FindPersonDescriptor findPersonDescriptor) {
+        this.predicate = null;
+        this.findPersonDescriptor = findPersonDescriptor;
+    }
+
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        if (!isNull(predicate)) {
+            model.updateFilteredPersonList(predicate);
+        } else {
+            if (findPersonDescriptor.isPhoneEmpty) {
+                this.targetIndex = Index.fromZeroBased(model.findEmail(findPersonDescriptor.getEmail()));
+            } else {
+                this.targetIndex = Index.fromZeroBased(model.findNum(findPersonDescriptor.getPhone()));
+            }
+            List<Person> lastShownList = model.getFilteredPersonList();
+
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+            Person personToFind = lastShownList.get(targetIndex.getZeroBased());
+            Predicate<Person> filterPersonToFind = p -> p.equals(personToFind);
+            model.updateFilteredPersonList(filterPersonToFind);
+        }
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
@@ -43,5 +81,39 @@ public class FindCommand extends Command {
                 || (other instanceof FindCommand // instanceof handles nulls
                 && predicate.equals(((FindCommand) other).predicate)); // state check
     }
-    // Update find command (search function)
+
+    /**
+     * Stores the details of the Person to find (either the phone number or email).
+     */
+    public static class FindPersonDescriptor {
+        private Email email;
+        private Phone phone;
+        private boolean isPhoneEmpty = true;
+        private boolean isEmailEmpty = true;
+
+        public FindPersonDescriptor() {
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = phone;
+            this.isPhoneEmpty = false;
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+            this.isEmailEmpty = false;
+        }
+
+        public Phone getPhone() {
+            return this.phone;
+        }
+
+        public Email getEmail() {
+            return this.email;
+        }
+
+        public boolean isAnyFilled() {
+            return this.isPhoneEmpty || this.isEmailEmpty;
+        }
+    }
 }
