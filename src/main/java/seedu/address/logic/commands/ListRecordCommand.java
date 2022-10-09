0ecd8c1ff12757eb8ed963519000ff4ba9838399
hanwenlai <aaronlhw02@gmail.com>
2022-10-10 04:35:11 +0800

    Fix Checkstyle Errors

--- src/main/java/seedu/address/logic/commands/ListRecordCommand.java
+++ src/main/java/seedu/address/logic/commands/ListRecordCommand.java
@@ -3,12 +3,13 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RECORDS;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-import java.util.List;
 
 /**
  * Lists all records for a specific patient to the user.
