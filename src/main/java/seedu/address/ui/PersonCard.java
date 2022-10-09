3b185c8ce0b7ff5dc65030cb10cce227800e37ea
teoyuqi <teo.yuqi@gmail.com>
2022-10-09 17:21:24 +0800

    Add independent UI component for task list

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -2,12 +2,15 @@ package seedu.address.ui;
 
 import java.util.Comparator;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.task.Task;
 
 /**
  * An UI component that displays information of a {@code Person}.
@@ -40,6 +43,10 @@ public class PersonCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
+    // Independent UI parts
+    private TaskListPanel taskListPanel;
+    @FXML
+    private StackPane taskListPanelPlaceholder;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -55,6 +62,9 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        ObservableList<Task> taskList = person.getTasks();
+        taskListPanel = new TaskListPanel(taskList);
+        taskListPanelPlaceholder.getChildren().add(taskListPanel.getRoot());
     }
 
     @Override
