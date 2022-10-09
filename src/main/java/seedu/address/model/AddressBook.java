2ad2000e9d51930c9a4e353b7f42df69f50fde79
highorbit25 <baobin25@gmail.com>
2022-10-09 23:00:09 +0800

    Fix Checkstyle in AddressBook.java

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -16,13 +16,13 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
 
-        /*
-         * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
-         * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
-         *
-         * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
-         *   among constructors.
-         */
+    /*
+     * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
+     * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
+     *
+     * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
+     *   among constructors.
+     */
     {
         persons = new UniquePersonList();
     }
