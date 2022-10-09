d5e8d5493f1b90cb800f6c1dc111c639fcc41a99
yuehernkang <yuehernkang@hotmail.com>
2022-10-09 14:42:11 +0800

    Fix imports for NameTest and AddressTest

--- src/test/java/seedu/condonery/model/property/AddressTest.java
+++ src/test/java/seedu/condonery/model/property/AddressTest.java
@@ -1,10 +1,10 @@
 package seedu.condonery.model.property;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.condonery.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class AddressTest {
 
