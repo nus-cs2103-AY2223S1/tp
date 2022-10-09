11b2157f259dce525882f253191a4f09c5e8dda9
guanzhou03 <guanzhou03@gmail.com>
2022-10-09 22:22:03 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/commands/CancelCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CancelCommandTest.java
@@ -1,10 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
