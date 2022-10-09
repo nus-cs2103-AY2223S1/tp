d2194c6457f51f99e86e3bcbb26d4aa75307f2e3
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-10-09 20:39:40 +0800

    Add PositionalParameer and ModuleCode classes

---
+++ src/main/java/nus/climods/commons/core/module/ModuleCode.java
@@ -0,0 +1,12 @@
+package nus.climods.commons.core.module;
+
+/**
+ * Represents a Module code supplied by the user.
+ */
+public class ModuleCode {
+    private String moduleCode;
+
+    public ModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+}
