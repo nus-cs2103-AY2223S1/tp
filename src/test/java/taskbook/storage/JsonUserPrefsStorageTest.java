c03bfed8b251b917b33f26200180f76eb352c497
Bill <xiaobill8@gmail.com>
2022-10-09 15:41:12 +0800

    Testcase Fixes

--- src/test/java/taskbook/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/taskbook/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setTaskBookFilePath(Paths.get("AddressBook.json"));
+        userPrefs.setTaskBookFilePath(Paths.get("data/taskbook.json"));
         return userPrefs;
     }
 
