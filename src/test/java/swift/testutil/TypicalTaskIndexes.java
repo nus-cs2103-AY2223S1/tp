6cf82407ced673fbf1fc98564767e9a8b9b21de7
ShenyiCui <shenyicui@outlook.com>
2022-10-10 01:23:01 +0800

    Test: add TypicalTask Indexes

---
+++ src/test/java/swift/testutil/TypicalTaskIndexes.java
@@ -0,0 +1,8 @@
+package swift.testutil;
+
+import swift.commons.core.index.Index;
+
+public class TypicalTaskIndexes {
+    public static final Index INDEX_FIRST_TASK = Index.fromOneBased(1);
+    public static final Index INDEX_SECOND_TASK = Index.fromOneBased(2);
+}
