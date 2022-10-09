7a797a558774545c8240cfa647d40b3ddb10aaa9
DevanshShah1309 <devanshshah1309@gmail.com>
2022-10-09 09:07:43 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -8,8 +8,6 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
-import seedu.address.model.team.Task;
-import seedu.address.model.team.TaskList;
 import seedu.address.model.team.Team;
 
 /**
