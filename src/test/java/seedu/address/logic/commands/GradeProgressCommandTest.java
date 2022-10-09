0da47fc0bc8e251e8b1942fff4d55d966d15fb8c
Ashiqur Rahman <e0689695@u.nus.edu>
2022-10-09 23:05:11 +0800

    Removed unused imports

--- src/test/java/seedu/address/logic/commands/GradeProgressCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GradeProgressCommandTest.java
@@ -1,9 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GRADE_PROGRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GRADE_PROGRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.GradeProgressTestTypicalPersons.getTypicalAddressBook;
@@ -12,8 +8,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
