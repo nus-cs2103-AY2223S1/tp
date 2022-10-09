0424427b6b743a2c360c85ab82fd19082d8ec8c6
samuelcheongws <samuelcheong2000@gmail.com>
2022-10-09 16:16:26 +0800

    Fix error bug and Change SampleDataUtil.java to show scholarship names

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,22 +21,22 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Scholarship("Blk 30 Geylang Street 29, #06-40"), new ApplicationStatus("pending"),
+                new Scholarship("NUS Global Merit Scholarship"), new ApplicationStatus("pending"),
                 getTagSet("friends")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Scholarship("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                new Scholarship("NUS Merit Scholarship"),
                 new ApplicationStatus("pending"), getTagSet("colleagues", "friends")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Scholarship("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                new Scholarship("NUS Sports Scholarship"),
                 new ApplicationStatus("accepted"), getTagSet("neighbours")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Scholarship("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                new Scholarship("NUS Performing & Visual Arts Scholarship"),
                 new ApplicationStatus("rejected"), getTagSet("family")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Scholarship("Blk 47 Tampines Street 20, #17-35"), new ApplicationStatus("pending"),
+                new Scholarship("NUS Merit Scholarship"), new ApplicationStatus("pending"),
                 getTagSet("classmates")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Scholarship("Blk 45 Aljunied Street 85, #11-31"), new ApplicationStatus("rejected"),
+                new Scholarship("NUS Performing & Visual Arts Scholarship"), new ApplicationStatus("rejected"),
                 getTagSet("colleagues"))
         };
     }
