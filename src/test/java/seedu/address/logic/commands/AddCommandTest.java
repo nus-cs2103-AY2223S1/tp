a12a9faa1145dfce0307ef0f11cf742c435a48c9
Brendan Tan <brendantan@outlook.com>
2022-10-09 17:17:36 +0800

    Update test and test data

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -147,6 +147,11 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<Internship> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void updateFilteredInternshipList(Predicate<Internship> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
