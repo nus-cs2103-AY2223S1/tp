0e2e6213fdf029c4a78cf0a454c696add0189b59
sarrrdin <sarrrdin@gmail.com>
2022-10-10 00:56:01 +0800

    Added relevant methods for each class.

--- src/main/java/nus/climods/model/module/Module.java
+++ src/main/java/nus/climods/model/module/Module.java
@@ -1,2 +1,97 @@
-package nus.climods.model.module;public class Module {
+package nus.climods.model.module;
+
+import static nus.climods.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import nus.climods.model.tag.Tag;
+
+public class Module {
+    // Identity fields
+    private final ModuleCode code;
+    private final Tutorial tutorial;
+
+    //Data fields
+    private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Module(ModuleCode code, Tutorial tutorial, Set<Tag> tags) {
+        requireAllNonNull(code, tutorial, tags);
+        this.code = code;
+        this.tutorial = tutorial;
+        this.tags.addAll(tags);
+    }
+
+    public ModuleCode getCode() {
+        return this.code;
+    }
+
+    public Tutorial getTutorial() {
+        return this.tutorial;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException} if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both modules have the same name. This defines a weaker notion of equality between two modules.
+     */
+    public boolean isSameModule(Module otherModule) {
+        if (otherModule == this) {
+            return true;
+        }
+
+        return otherModule != null
+                && otherModule.getCode().equals(getCode());
+    }
+
+    /**
+     * Returns true if both modules have the same identity and data fields. This defines a stronger notion of equality
+     * between two modules.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Module)) {
+            return false;
+        }
+
+        Module otherModule = (Module) other;
+        return otherModule.getCode().equals(getCode())
+                && otherModule.getTutorial().equals(getTutorial())
+                && otherModule.getTags().equals(getTags());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(code, tutorial, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getCode())
+                .append("; Tutorial: ")
+                .append(getTutorial());
+
+        Set<Tag> tags = getTags();
+        if (!tags.isEmpty()) {
+            builder.append("; Tags: ");
+            tags.forEach(builder::append);
+        }
+        return builder.toString();
+    }
 }
