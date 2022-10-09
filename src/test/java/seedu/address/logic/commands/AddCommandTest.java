22c03ce8d5243d5f91ed7df723c6c1acfe3b8019
highorbit25 <baobin25@gmail.com>
2022-10-09 23:42:23 +0800

    Fix ModelStub in AddCommandTest.java

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -147,6 +147,11 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<Person> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void sortFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
