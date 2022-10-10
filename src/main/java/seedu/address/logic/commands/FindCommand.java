76cd8e548575ab3fafd7ccd9d1151743f70cc833
Elben85 <elbert.benedict@gmail.com>
2022-10-10 13:33:19 +0800

    Modify Find Command Description

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -17,8 +17,8 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose specified category"
             + " contain any of the specified keywords (case-insensitive) "
             + "and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Parameters: find [c/CATEGORY] KEYWORDS...\n"
+            + "Example: " + COMMAND_WORD + " c/p engineer writer";
 
     private final ContainsKeywordsPredicate predicate;
 
