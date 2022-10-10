665f6859de9c1f324a8ced17693dacf25a488a3d
Jessica Mary <jessicamary472@gmail.com>
2022-10-10 19:59:24 +0800

    Update CapTest.java

--- src/test/java/seedu/address/model/person/CapTest.java
+++ src/test/java/seedu/address/model/person/CapTest.java
@@ -11,14 +11,15 @@ class CapTest {
     void isValidCap() {
         // invalid cap
         assertFalse(Cap.isValidCap(-5.0, 5.0)); // negative cap value
+        assertFalse(Cap.isValidCap(4.0, -4.0)); // negative maximum cap value
         assertFalse(Cap.isValidCap(10.0, 5.0)); // cap value greater than its maximum
 
         // valid cap
-        assertTrue(Cap.isValidCap(4.99, 5.0));
+        assertTrue(Cap.isValidCap(4.99, 5.0)); // max value can be of any value greater than or equal to cap value
         assertTrue(Cap.isValidCap(5.0, 5.0));
         assertTrue(Cap.isValidCap(1.0, 4.0));
         assertTrue(Cap.isValidCap(0.0, 4.0));
-        assertTrue(Cap.isValidCap(2, 3)); // input can be an integer, max can be of any value > 0
+        assertTrue(Cap.isValidCap(2, 3)); // both cap value and maximum cap value can be an integer
         assertTrue(Cap.isValidCap(5, 5));
         assertTrue(Cap.isValidCap(0, 5));
     }
