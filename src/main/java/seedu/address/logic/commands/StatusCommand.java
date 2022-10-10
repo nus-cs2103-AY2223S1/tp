b6519e902ef2eaf57789de18e348a731f49e4670
kangqiao322 <kangqiao803@gmail.com>
2022-10-11 01:30:55 +0800

    Checkstyle

--- src/main/java/seedu/address/logic/commands/StatusCommand.java
+++ src/main/java/seedu/address/logic/commands/StatusCommand.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import  seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.internship.Internship;
 import seedu.address.model.internship.Status;
