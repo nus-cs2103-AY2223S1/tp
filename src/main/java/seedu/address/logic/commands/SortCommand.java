4885aed76dc84d8d468ba8b4270f6cbb4dfbdbaf
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-10-10 17:20:34 +0800

    Add SortCommand

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,2 +1,19 @@
-package seedu.address.logic.commands;public class SortCommand {
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class SortCommand extends Command{
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_SUCCESS = "Listed all persons in alphabetical order";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.sortPerson();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
