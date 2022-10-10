6ebd3a4363f927e2954a1c4bb32f59f5d72c444f
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-10-11 00:02:43 +0800

    Add tests for FocusCommand#equals()

--- src/test/java/seedu/taassist/logic/commands/FocusCommandTest.java
+++ src/test/java/seedu/taassist/logic/commands/FocusCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.taassist.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.taassist.testutil.Assert.assertThrows;
 
@@ -36,6 +37,31 @@ class FocusCommandTest {
                 commandResult.getFeedbackToUser());
         assertTrue(modelStub.isFocusedModuleClass(moduleClass));
     }
+
+    @Test
+    void equals() {
+        ModuleClass moduleClass = new ModuleClass("CS2103T");
+        FocusCommand focusCommand = new FocusCommand(moduleClass);
+
+        // same object -> returns true
+        assertTrue(focusCommand.equals(focusCommand));
+
+        // same value -> returns true
+        FocusCommand focusCommandCopy = new FocusCommand(moduleClass);
+        assertTrue(focusCommand.equals(focusCommandCopy));
+
+        // different types -> returns false
+        assertFalse(focusCommand.equals(new BackCommand()));
+
+        // null -> returns false
+        assertFalse(focusCommand.equals(null));
+
+        // different module class -> returns false
+        ModuleClass differentModuleClass = new ModuleClass("CS2101");
+        assertFalse(focusCommand.equals(new FocusCommand(differentModuleClass)));
+    }
+
+
 
     /**
      * A Model stub that pretends to have no module classes.
