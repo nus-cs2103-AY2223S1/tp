0ffef473e41ac2d82d3b0664d7eaa18dad21b35a
jeromepui <jeromepui00@gmail.com>
2022-10-10 20:19:53 +0800

    Update DeleteCommand to delete students using their ID or name

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,13 +1,17 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
 
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.student.Id;
+import seedu.address.model.student.IdPredicate;
+import seedu.address.model.student.Name;
+import seedu.address.model.student.NamePredicate;
 import seedu.address.model.student.Student;
 
 /**
@@ -18,36 +22,80 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the student record identified by the student name or "
+            + "student ID used in the displayed student list.\n"
+            + "Parameters: NAME (should only contain alphanumeric characters and spaces) or"
+            + "\n ID (should only contain 3 digits and 1 character)"
+            + "Example: " + COMMAND_WORD + " n/Jonathan Tan" + " or " + COMMAND_WORD + " id/123A";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Student: %1$s";
 
-    private final Index targetIndex;
+    private final Id targetId;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    private final Name targetName;
+
+    private final IdPredicate idPredicate;
+
+    private final NamePredicate namePredicate;
+
+    /**
+     * @param targetId of the student in the filtered student list to delete
+     * @param idPredicate to filter the student list
+     */
+    public DeleteCommand(Id targetId, IdPredicate idPredicate) {
+        this.targetId = targetId;
+        this.targetName = null;
+        this.idPredicate = idPredicate;
+        this.namePredicate = null;
+    }
+
+    /**
+     * @param targetName of the student in the filtered student list to delete
+     * @param namePredicate to filter the student list
+     */
+    public DeleteCommand(Name targetName, NamePredicate namePredicate) {
+        this.targetId = null;
+        this.targetName = targetName;
+        this.idPredicate = null;
+        this.namePredicate = namePredicate;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Student> lastShownList = model.getFilteredStudentList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        if (targetId == null) {
+            model.updateFilteredStudentList(namePredicate);
+        } else {
+            model.updateFilteredStudentList(idPredicate);
         }
 
-        Student personToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deleteStudent(personToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+        List<Student> studentList = model.getFilteredStudentList();
+
+        if (studentList.size() == 0) {
+            if (targetId == null) {
+                throw new CommandException(Messages.MESSAGE_INVALID_STUDENT_NAME);
+            } else {
+                throw new CommandException(Messages.MESSAGE_INVALID_STUDENT_ID);
+            }
+        }
+
+        Student studentToDelete = studentList.get(0);
+        model.deleteStudent(studentList.get(0));
+        model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, studentToDelete));
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof DeleteCommand // instanceof handles nulls
-                && targetIndex.equals(((DeleteCommand) other).targetIndex)); // state check
+        if (other == this) {
+            return true;
+        }
+
+        if (targetId == null) {
+            return other instanceof DeleteCommand && targetName.equals(((DeleteCommand) other).targetName);
+        } else {
+            return other instanceof DeleteCommand && targetId.equals(((DeleteCommand) other).targetId);
+        }
     }
 }
