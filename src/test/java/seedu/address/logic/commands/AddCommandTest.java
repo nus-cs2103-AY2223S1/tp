5e9bda3b7bd6ff1302ce5c4e98afc96a96de8657
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-10 23:01:05 +0800

    Minor formatting changes

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -20,6 +20,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.event.Event;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
@@ -133,6 +134,15 @@ public class AddCommandTest {
             throw new AssertionError("This method should not be called.");
         }
 
+        @Override
+        public void addEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void removeEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
         @Override
         public void setPerson(Person target, Person editedPerson) {
             throw new AssertionError("This method should not be called.");
