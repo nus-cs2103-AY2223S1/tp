d819100cd0871fb815405389df3bfb27a3e29a88
Effixion <syt1702@gmail.com>
2022-10-10 14:55:30 +0800

    Change the COMMAND_WORD to reflect the action of adding a module rather than a person

--- src/main/java/seedu/address/logic/commands/AddModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/AddModuleCommand.java
@@ -19,7 +19,7 @@ public class AddModuleCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     //TODO update
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a module to the address book. "
             + "Parameters: "
             + PREFIX_MODULE + "MODULE "
             + PREFIX_LECTURE + "LECTURE "
