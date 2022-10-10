c94d2dff595f97a268e8d0b4876cecc00110a879
linuschancs <mrlinuschan@gmail.com>
2022-10-11 02:33:45 +0800

    Factor in Class field for AddStudCommand and EditCommand

--- src/main/java/seedu/address/logic/commands/AddStudCommand.java
+++ src/main/java/seedu/address/logic/commands/AddStudCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLASS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENT_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_NAME;
@@ -23,6 +24,7 @@ public class AddStudCommand extends Command {
             + "Parameters: "
             + PREFIX_STUDENT_NAME + "STUDENT NAME "
             + PREFIX_ID + "ID "
+            + PREFIX_CLASS + "CLASS "
             + PREFIX_PARENT_NAME + "PARENT NAME "
             + PREFIX_PHONE + "PHONE NUMBER "
             + PREFIX_ADDRESS + "ADDRESS "
@@ -30,6 +32,7 @@ public class AddStudCommand extends Command {
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_STUDENT_NAME + "John Doe "
             + PREFIX_ID + "928C "
+            + PREFIX_CLASS + "1A "
             + PREFIX_PARENT_NAME + "Bob Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
