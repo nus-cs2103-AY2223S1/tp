edb19b83a4643d0558a84a26d206bf36545cf3a0
Aloynz <aloysiusneozeen@gmail.com>
2022-10-10 17:00:56 +0800

    Add Ui for Display group

---
+++ src/main/java/seedu/address/ui/GroupCard.java
@@ -0,0 +1,56 @@
+package seedu.address.ui;
+
+import java.util.Comparator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
+import seedu.address.model.group.Group;
+
+public class GroupCard extends UiPart<Region> {
+
+    private static final String FXML = "GroupListCard.fxml";
+
+    public final Group group;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private FlowPane members;
+
+    public GroupCard(Group group) {
+        super(FXML);
+        this.group = group;
+        name.setText(group.getName().toString());
+        group.getMembers().stream()
+                .sorted(Comparator.comparing(member ->
+                    member.getName().toString()))
+                .forEach(member ->
+                    members.getChildren().add(new Label(member.getName().toString())));
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof GroupCard)) {
+            return false;
+        }
+
+        // state check
+        GroupCard card = (GroupCard) other;
+        return name.getText().equals(card.name.getText())
+                && group.equals(card.group);
+    }
+
+}
---
+++ src/main/java/seedu/address/ui/GroupListPanel.java
@@ -0,0 +1,41 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.group.Group;
+
+public class GroupListPanel extends UiPart<Region> {
+    private static final String FXML = "GroupListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(GroupListPanel.class);
+
+    @FXML
+    private ListView<Group> groupListView;
+
+    public GroupListPanel(ObservableList<Group> groupList) {
+        super(FXML);
+        groupListView.setItems(groupList);
+        groupListView.setCellFactory(listView -> new GroupListViewCell());
+    }
+
+    class GroupListViewCell extends ListCell<Group> {
+        @Override
+        protected void updateItem(Group group, boolean empty) {
+            super.updateItem(group, empty);
+
+            if (empty || group == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new GroupCard(group).getRoot());
+            }
+        }
+    }
+
+
+}
