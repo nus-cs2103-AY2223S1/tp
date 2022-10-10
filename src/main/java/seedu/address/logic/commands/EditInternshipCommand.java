a06a02aa1439c7b8ad3746db97c0c085151b479f
ish1506 <ishiuanlee@gmail.com>
2022-10-10 22:32:40 +0800

    Fix usage message bug for EditInternshipCommand

--- src/main/java/seedu/address/logic/commands/EditInternshipCommand.java
+++ src/main/java/seedu/address/logic/commands/EditInternshipCommand.java
@@ -1,11 +1,10 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP_ROLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP_STATUS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERVIEW_DATE;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_INTERNSHIPS;
 
 import java.util.List;
@@ -35,14 +34,12 @@ public class EditInternshipCommand extends Command {
             + "by the index number used in the displayed internship list. "
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
-            + "[" + PREFIX_NAME + "NAME] "
-            + "[" + PREFIX_PHONE + "PHONE] "
-            + "[" + PREFIX_EMAIL + "EMAIL] "
-            + "[" + PREFIX_ADDRESS + "ADDRESS] "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "[" + PREFIX_COMPANY_NAME + "COMPANY_NAME] "
+            + "[" + PREFIX_INTERNSHIP_ROLE + "INTERNSHIP_ROLE] "
+            + "[" + PREFIX_INTERNSHIP_STATUS + "INTERNSHIP_STATUS] "
+            + "[" + PREFIX_INTERVIEW_DATE + "INTERVIEW_DATE]\n"
             + "Example: " + COMMAND_WORD + " 1 "
-            + PREFIX_PHONE + "91234567 "
-            + PREFIX_EMAIL + "johndoe@example.com";
+            + PREFIX_INTERNSHIP_STATUS + "ACCEPTED ";
 
     public static final String MESSAGE_EDIT_INTERNSHIP_SUCCESS = "Edited Internship: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
