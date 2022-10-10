8ed564790044c0bc0828d3d4b8d996d12a4a0df7
avock <chunkhai.k@gmail.com>
2022-10-10 15:57:26 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.CONTACT_COMMAND_IDENTIFIER;
-import static seedu.address.logic.parser.CliSyntax.MODULE_COMMAND_IDENTIFIER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
