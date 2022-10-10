67be7a72e81cfacac93b7eca2af03fbe506fce52
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-10-10 15:36:48 +0800

    Remove Add Command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddStuCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -44,9 +43,6 @@ public class AddressBookParser {
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
         switch (commandWord) {
-
-//        case AddCommand.COMMAND_WORD:
-//            return new AddCommandParser().parse(arguments);
 
         case AddStuCommand.COMMAND_WORD:
             return new AddStuCommandParser().parse(arguments);
