6c9b0ce2eb247f1cecc0a71584a8a46a7ee5eeef
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-10 10:46:32 +0800

    Add tests for ItemUnitValidator

---
+++ src/test/java/seedu/address/model/item/ItemUnitValidatorTest.java
@@ -0,0 +1,55 @@
+package seedu.address.model.item;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.item.itemvalidator.ItemUnitValidator;
+
+public class ItemUnitValidatorTest {
+    // TODO: Test for uniqueness of Item Unit
+
+    @Test
+    public void test_unitIsValidLength() {
+        assertTrue(ItemUnitValidator.isUnitBlank(""));
+        assertFalse(ItemUnitValidator.isUnitBlank("a"));
+        assertFalse(ItemUnitValidator.isUnitLengthMoreThanMaxLength("Lorem ipsum dolor si"));
+        assertTrue(ItemUnitValidator.isUnitLengthMoreThanMaxLength("Lorem ipsum dolor sim"));
+    }
+
+    @Test
+    public void test_unitHasValidSymbols() {
+        // Invalid names - Spaces
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("")); // Cannot have nothing
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters(" "));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("       "));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("    a"));
+
+        // Invalid names - Wrong Symbols
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("\\"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("\\/"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("|"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("á"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("你"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("あ"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("，")); // Chinese comma
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram|container"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram/container"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram\\container"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram$container"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram*container"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("*"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("$"));
+        assertTrue(ItemUnitValidator.doesUnitContainInvalidCharacters("%"));
+
+
+        // Valid names
+        assertFalse(ItemUnitValidator.doesUnitContainInvalidCharacters("a"));
+        assertFalse(ItemUnitValidator.doesUnitContainInvalidCharacters("kg"));
+        assertFalse(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram"));
+        assertFalse(ItemUnitValidator.doesUnitContainInvalidCharacters("kilogram/kg"));
+        assertFalse(ItemUnitValidator.doesUnitContainInvalidCharacters("KG"));
+        assertFalse(ItemUnitValidator.doesUnitContainInvalidCharacters("100"));
+    }
+}
