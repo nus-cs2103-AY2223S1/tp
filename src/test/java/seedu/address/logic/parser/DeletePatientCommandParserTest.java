7cb035f153e59cb1215681e1092b4f99b4eeaa4a
Wadia <g.wadia08@gmail.com>
2022-10-10 18:22:56 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/DeletePatientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeletePatientCommandParserTest.java
@@ -27,6 +27,7 @@ public class DeletePatientCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePatientCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeletePatientCommand.MESSAGE_USAGE));
     }
 }
