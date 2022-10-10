55117087880ef5dd9b6f603a9fc76a1a66e91c25
njxue <ngjxue@gmail.com>
2022-10-10 15:17:07 +0800

    Add test cases for UniqueModuleClassList

---
+++ src/test/java/seedu/taassist/testutil/TypicalModuleClasses.java
@@ -0,0 +1,11 @@
+package seedu.taassist.testutil;
+
+import seedu.taassist.model.moduleclass.ModuleClass;
+
+/**
+ * A utility class containing a list of {@code ModuleClass} objects to be used in tests.
+ */
+public class TypicalModuleClasses {
+    public static final ModuleClass CS1101S = new ModuleClass("CS1101S");
+    public static final ModuleClass CS1231S = new ModuleClass("CS1231S");
+}
