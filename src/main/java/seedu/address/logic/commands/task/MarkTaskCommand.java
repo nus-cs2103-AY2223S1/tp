1bf795742714f9ef513d8a848909155fb826997b
Wang Xinyi <e0817822@u.nus.edu>
2022-10-11 00:02:28 +0800

    feat: create mark task command parser

--- src/main/java/seedu/address/logic/commands/task/MarkTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/task/MarkTaskCommand.java
@@ -1,4 +1,22 @@
 package seedu.address.logic.commands.task;
 
-public class MarkTaskCommand {
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.TaskCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.task.Task;
+
+/**
+ * Marks a task of a person as complete or incomplete.
+ */
+public class MarkTaskCommand extends TaskCommand{
+
+    public static final String COMMAND_WORD = "mark";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException{
+        return new CommandResult("Hello from remark");
+    }
 }
