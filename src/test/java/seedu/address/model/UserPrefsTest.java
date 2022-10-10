851e63cfbf8351a6f1c1c6c445e53334d3c49f61
maxtance <e0772757@u.nus.edu>
2022-10-10 20:09:14 +0800

    Add test cases

--- src/test/java/seedu/address/model/UserPrefsTest.java
+++ src/test/java/seedu/address/model/UserPrefsTest.java
@@ -1,5 +1,8 @@
 package seedu.address.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -18,4 +21,18 @@ public class UserPrefsTest {
         assertThrows(NullPointerException.class, () -> userPrefs.setStudentRecordFilePath(null));
     }
 
+    @Test
+    public void equals() {
+        UserPrefs userPrefs = new UserPrefs();
+        UserPrefs otherUserPrefs = new UserPrefs();
+
+        // Same object
+        assertEquals(userPrefs, userPrefs);
+
+        // Not an instance of UserPrefs
+        assertNotEquals(userPrefs, new Object());
+
+        // Equality
+        assertEquals(userPrefs, otherUserPrefs);
+    }
 }
