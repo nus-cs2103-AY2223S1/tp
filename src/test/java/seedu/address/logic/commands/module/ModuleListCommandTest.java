dc1db002218a1778eccff50ae24e1959ae000f31
bryanngzh <bryanngzihao@gmail.com>
2022-10-10 16:14:46 +0800

    Fix ModuleListCommand and ListCommand test cases

--- src/test/java/seedu/address/logic/commands/module/ModuleListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/module/ModuleListCommandTest.java
@@ -6,6 +6,7 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -25,6 +26,8 @@ public class ModuleListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ModuleListCommand(), model, ModuleListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ModuleListCommand(), model,
+                new CommandResult(ModuleListCommand.MESSAGE_SUCCESS, false, false,
+                        true, false), expectedModel);
     }
 }
