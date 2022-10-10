12a146f213f37ee518f2dc79455075b14cf6c586
myangat <mingyangy31@gmail.com>
2022-10-11 03:43:45 +0800

    Add ui for ViewScheduleCommand

--- src/main/java/seedu/address/logic/commands/ViewModuleScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewModuleScheduleCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SCHEDULES;
 
 import seedu.address.model.Model;
 
@@ -19,9 +20,8 @@ public class ViewModuleScheduleCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        // model.updateFilteredScheduleList(PREDICATE_SHOW_ALL_SCHEDULES);
-        return new CommandResult(MESSAGE_SUCCESS);
-
+        model.updateFilteredScheduleList(PREDICATE_SHOW_ALL_SCHEDULES);
+        return new CommandResult(MESSAGE_SUCCESS, false, false, false, false, true);
     }
 
 }
--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -15,7 +15,7 @@ public class ExitCommandTest {
     @Test
     public void execute_exit_success() {
         CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false,
-                true, false, false);
+                true, false, false, false);
         assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
     }
 }
