40cf5c895d71932b02187fc737c2a687ae12b1ba
ming wei <43949290+malwaregarry@users.noreply.github.com>
2022-10-10 22:17:54 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -11,7 +11,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
