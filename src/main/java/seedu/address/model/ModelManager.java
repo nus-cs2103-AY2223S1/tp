7c9868674d3fc5d7f2eb499d98770b43334685e9
anthonyhotenghow@gmail.com <anthony1hotw@gmail.com>
2022-10-10 21:06:56 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -14,7 +14,6 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Record;
-import seedu.address.model.person.RecordList;
 
 /**
  * Represents the in-memory model of the address book data.
