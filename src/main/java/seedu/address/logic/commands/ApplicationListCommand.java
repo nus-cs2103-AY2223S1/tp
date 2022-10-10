b379a851c59bdcd6f28abbd9dea9db20366501aa
eesung00 <e0407725@u.nus.edu>
2022-10-10 21:44:38 +0800

    Fix list command bug

--- src/main/java/seedu/address/logic/commands/ApplicationListCommand.java
+++ src/main/java/seedu/address/logic/commands/ApplicationListCommand.java
@@ -10,7 +10,7 @@ import seedu.address.model.ApplicationModel;
  */
 public class ApplicationListCommand extends ApplicationCommand {
 
-    public static final String COMMAND_WORD = "list application";
+    public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all applications";
 
