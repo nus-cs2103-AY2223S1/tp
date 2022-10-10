55274d29ddd1922cfd4f284fc871a8cd297a2770
rachaelsng <rachaelsng@u.nus.edu>
2022-10-10 15:57:27 +0800

    Update tests

--- src/test/java/jarvis/logic/LogicManagerTest.java
+++ src/test/java/jarvis/logic/LogicManagerTest.java
@@ -58,7 +58,7 @@ public class LogicManagerTest {
 
     @Test
     public void execute_commandExecutionError_throwsCommandException() {
-        String deleteCommand = "delete 9";
+        String deleteCommand = "deletestudent 9";
         assertCommandException(deleteCommand, MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX);
     }
 
