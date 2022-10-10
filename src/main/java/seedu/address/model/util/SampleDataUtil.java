b2e2f66e6209fb699876721a51a0ba0fc7f21d40
Mohamed Hamas <e0726186@u.nus.edu>
2022-10-10 09:22:44 +0800

    SampleData adapted to suppliers and Supplier tag added

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,21 +1,16 @@
 package seedu.address.model.util;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyTaskList;
 import seedu.address.model.TaskList;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Item;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.Price;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.task.Task;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
@@ -23,24 +18,24 @@ import seedu.address.model.task.Task;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Price("$1.00"), new Item("Ginger"),
+            new Person(new Name("ABC Pte Ltd"), new Phone("67089005"), new Price("$1.00"), new Item("Ginger"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("supplier")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Price("$1.00"), new Item("Butter"),
+                getTagSet("Supplier")),
+            new Person(new Name("Ya Shu Egg"), new Phone("63450864"), new Price("$1.10"), new Item("Egg"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("supplier")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Price("$1.00"), new Item("Ginger"),
+                getTagSet("Supplier")),
+            new Person(new Name("KyEggs"), new Phone("61240985"), new Price("$1.00"), new Item("Egg"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("supplier")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Price("$1.00"), new Item("Garlic"),
+                getTagSet("Supplier")),
+            new Person(new Name("Goh Supplies"), new Phone("69008045"), new Price("$1.50"), new Item("Garlic"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("supplier")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Price("$1.00"), new Item("Egg"),
+                getTagSet("Supplier")),
+            new Person(new Name("Soho Singapore"), new Phone("64300567"), new Price("$1.85"), new Item("Chicken"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("supplier")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Price("$1.00"), new Item("Egg"),
+                getTagSet("Supplier")),
+            new Person(new Name("Balas Market"), new Phone("62624417"), new Price("$1.80"), new Item("Chicken"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("supplier"))
+                getTagSet("Supplier"))
         };
     }
 
