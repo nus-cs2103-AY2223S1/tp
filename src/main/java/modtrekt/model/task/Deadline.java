b4203f5fa3ab538cc4264abee8940d99f4919137
vvidday <davids89034@gmail.com>
2022-10-10 21:10:32 +0800

    Add deadline class

---
+++ src/main/java/modtrekt/model/task/Deadline.java
@@ -0,0 +1,48 @@
+package modtrekt.model.task;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Represents a deadline in the task list.
+ * Similar to task, but accepts a due date.
+ */
+public class Deadline extends Task {
+    private final LocalDate dueDate;
+
+    /**
+     * Constructor for deadline object.
+     *
+     * @param description description of task
+     * @param dueDate date object representing due date
+     */
+    public Deadline(Description description, LocalDate dueDate) {
+        super(description);
+        this.dueDate = dueDate;
+    }
+
+    /**
+     * Gets due date of deadline.
+     * @return Due date of deadline
+     */
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (super.equals(other) && other instanceof Deadline) {
+            return ((Deadline) other).getDueDate().isEqual(dueDate);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", description.toString());
+    }
+
+}
