a7ab85ec17cf4aecaf5e2cd0199eef3b345b483e
angkl0 <kuanglong456@gmail.com>
2022-10-10 20:07:34 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/commands/ListTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListTaskCommandTest.java
@@ -7,7 +7,6 @@ import static seedu.address.testutil.TypicalTasks.getTypicalTaskList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
--- src/test/java/seedu/address/logic/commands/UnmarkTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkTaskCommandTest.java
@@ -13,8 +13,8 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.task.Task;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.task.Task;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
