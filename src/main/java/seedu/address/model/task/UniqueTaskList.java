50f4cfd16ee047c8dbe91c6b719fdbbd28242bb2
JonathanWiguna <jonathan.wiguna1@gmail.com>
2022-10-10 22:37:04 +0800

    Remove unused import in UniqueTaskList

--- src/main/java/seedu/address/model/task/UniqueTaskList.java
+++ src/main/java/seedu/address/model/task/UniqueTaskList.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import seedu.address.commons.core.index.Index;
 import seedu.address.model.task.exceptions.DuplicateTaskException;
 import seedu.address.model.task.exceptions.TaskNotFoundException;
 
