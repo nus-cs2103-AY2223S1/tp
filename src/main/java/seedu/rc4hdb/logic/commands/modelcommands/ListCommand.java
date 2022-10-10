855a637bb0c3bba860b221f0e8dcaf19c2fca26e
nseah21 <nseah21@gmail.com>
2022-10-10 14:32:00 +0800

    Update ListCommand to work with a field list

--- src/main/java/seedu/rc4hdb/logic/commands/modelcommands/ListCommand.java
+++ src/main/java/seedu/rc4hdb/logic/commands/modelcommands/ListCommand.java
@@ -1,10 +1,13 @@
 package seedu.rc4hdb.logic.commands.modelcommands;
 
+import java.util.List;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.rc4hdb.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.rc4hdb.logic.commands.CommandResult;
 import seedu.rc4hdb.model.Model;
+import seedu.rc4hdb.model.person.Fields;
 
 /**
  * Lists all persons in the address book to the user.
@@ -15,10 +18,31 @@ public class ListCommand extends ModelCommand {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    public static final String SHOW_ONLY_SPECIFIED = "Listed all persons. Only specified fields are shown.";
+
+    private final List<String> fieldList;
+
+    public ListCommand() {
+        // Set the required list of fields to the global field list
+        this.fieldList = Fields.fields;
+    }
+
+    public ListCommand(List<String> fieldList) {
+        requireNonNull(fieldList);
+        this.fieldList = fieldList;
+    }
+
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        model.setObservableFields(fieldList);
+
+        // Determine which ListCommand constructor was invoked
+        if (fieldList == Fields.fields) {
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            return new CommandResult(SHOW_ONLY_SPECIFIED);
+        }
     }
 }
