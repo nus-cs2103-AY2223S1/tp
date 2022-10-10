c61bab468fd6265451b0975de35841cca08d1104
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2022-10-10 23:32:34 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddListingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddListingCommandParser.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
 
-
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddListingCommand;
