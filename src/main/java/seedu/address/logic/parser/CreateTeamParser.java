1a765659a02b9c62bb052a24db74bce2a5165b4f
zicot <zicotjia@gmail.com>
2022-10-10 13:49:36 +0800

    Change team to use UniquePersonList to hold member and adapt changes to storage

--- src/main/java/seedu/address/logic/parser/CreateTeamParser.java
+++ src/main/java/seedu/address/logic/parser/CreateTeamParser.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAM_NAME;
 
 import java.util.stream.Stream;
 
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CreateTeamCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.team.Name;
--- src/main/java/seedu/address/model/team/Team.java
+++ src/main/java/seedu/address/model/team/Team.java
@@ -2,7 +2,6 @@ package seedu.address.model.team;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
