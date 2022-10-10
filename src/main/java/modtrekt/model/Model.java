2b02bc1a9d45e6263b02b9d533948b4dd586b321
Jonathan Tay <jonathantaymy@gmail.com>
2022-10-10 17:46:07 +0800

    Add predicates for task display for archive status

--- src/main/java/modtrekt/model/Model.java
+++ src/main/java/modtrekt/model/Model.java
@@ -15,6 +15,8 @@ public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Task> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
     Predicate<Module> PREDICATE_SHOW_ALL_MODULES = unused -> true;
+    Predicate<Task> PREDICATE_HIDE_ARCHIVED_TASKS = task -> !task.isArchived();
+    Predicate<Task> PREDICATE_SHOW_ALL_TASKS = task -> true;
 
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
