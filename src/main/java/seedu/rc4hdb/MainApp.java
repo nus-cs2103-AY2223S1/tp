6262b9693036a032d3a3fcf4e0d378bcaca3ba72
jq1836 <95712150+jq1836@users.noreply.github.com>
2022-10-11 03:19:10 +0800

    Change logger file path

--- src/main/java/seedu/rc4hdb/MainApp.java
+++ src/main/java/seedu/rc4hdb/MainApp.java
@@ -48,7 +48,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing RC4HDB ]================================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
