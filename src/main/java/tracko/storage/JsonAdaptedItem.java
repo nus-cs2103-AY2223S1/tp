ae693c5c2d1dad87866c53140e00b84c0ad99c57
Jiacheng-y <e0773814@u.nus.edu>
2022-10-10 15:28:00 +0800

    Add Storage support for Items

---
+++ src/main/java/tracko/storage/JsonAdaptedItem.java
@@ -0,0 +1,80 @@
+package tracko.storage;
+
+import java.util.HashSet;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import tracko.commons.exceptions.IllegalValueException;
+import tracko.model.items.Description;
+import tracko.model.items.Item;
+import tracko.model.items.ItemName;
+import tracko.model.items.Quantity;
+
+/**
+ * Jackson-friendly version of {@link Item}.
+ */
+public class JsonAdaptedItem {
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Item's %s field is missing!";
+
+    private final String itemName;
+    private final Integer quantity;
+    private final String description;
+
+    /**
+     * Constructs a {@code JsonAdaptedItem} with the given item details.
+     */
+    @JsonCreator
+    public JsonAdaptedItem(@JsonProperty("itemName") String itemName, @JsonProperty("quantity") Integer quantity,
+                            @JsonProperty("description") String description) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    /**
+     * Converts a given {@code Item} into this class for Jackson use.
+     */
+    public JsonAdaptedItem(Item source) {
+        itemName = source.getItemName().itemName;
+        quantity = source.getQuantity().getQuantity();
+        description = source.getDescription().value;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted item object into the model's {@code Item} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted item.
+     */
+    public Item toModelType() throws IllegalValueException {
+
+        if (itemName == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    ItemName.class.getSimpleName()));
+        }
+        if (!ItemName.isValidItemName(itemName)) {
+            throw new IllegalValueException(ItemName.MESSAGE_CONSTRAINTS);
+        }
+        final ItemName modelItemName = new ItemName(itemName);
+
+        if (quantity == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Quantity.class.getSimpleName()));
+        }
+        if (!Quantity.isValidQuantity(quantity)) {
+            throw new IllegalValueException(Quantity.MESSAGE_CONSTRAINTS);
+        }
+        final Quantity modelQuantity = new Quantity(quantity);
+
+        if (description == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Description.class.getSimpleName()));
+        }
+        if (!Description.isValidDescription(description)) {
+            throw new IllegalValueException(Description.MESSAGE_CONSTRAINTS);
+        }
+        final Description modelDescription = new Description(description);
+
+        return new Item(modelItemName, modelDescription, modelQuantity, new HashSet<>());
+    }
+}
