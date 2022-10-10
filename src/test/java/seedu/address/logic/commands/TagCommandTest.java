54a342fdb4c8a9fe1c1f1c1289ff0be46f54c116
weiquany <weiquan7122@gmail.com>
2022-10-10 21:47:13 +0800

    Update remove tag command to support show command and add tests

--- src/test/java/seedu/address/logic/commands/TagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.ModelStub;
+import static seedu.address.logic.commands.TagCommand.MESSAGE_DUPLICATE_TAGS;
 import static seedu.address.logic.commands.TagCommand.MESSAGE_NO_TAGS_ADDED;
 import static seedu.address.logic.commands.TagCommand.MESSAGE_SUCCESS;
+import static seedu.address.logic.commands.TagCommand.MESSAGE_TAGS_NOT_FOUND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalTags.VALID_TAG_FRIENDS;
@@ -22,6 +24,7 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.TargetPerson;
 import seedu.address.model.tag.Tag;
 
 
@@ -40,28 +43,89 @@ public class TagCommandTest {
 
     @Test
     public void execute_indexDoesNotExist_throwsCommandException() {
-        ModelStubWithPersonList modelStub = new ModelStubWithPersonList();
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
         assertThrows(
                 CommandException.class, () -> new TagCommand(
                         Index.fromZeroBased(0), new HashSet<>()).execute(modelStub));
     }
 
     @Test
+    public void execute_noTargetPerson_throwsCommandException() {
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
+        // no index provided
+        assertThrows(CommandException.class, () -> new TagCommand(new HashSet<>()).execute(modelStub));
+    }
+
+    @Test
     public void execute_noTagsPassedIn_userNotified() throws Exception {
-        ModelStubWithPersonList modelStub = new ModelStubWithPersonList();
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
         modelStub.personList.add(ALICE);
         CommandResult commandResult = new TagCommand(Index.fromZeroBased(0), new HashSet<>()).execute(modelStub);
         assertEquals(MESSAGE_NO_TAGS_ADDED, commandResult.getFeedbackToUser());
     }
 
     @Test
-    public void execute_userDoesNotAlreadyHaveTags_success() throws Exception {
-        ModelStubWithPersonList modelStub = new ModelStubWithPersonList();
+    public void execute_personDoesNotAlreadyHaveTags_success() throws Exception {
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
         modelStub.personList.add(ALICE);
         Set<Tag> tagsToAdd = new HashSet<>();
         tagsToAdd.add(VALID_TAG_OWES_MONEY);
         CommandResult commandResult = new TagCommand(Index.fromZeroBased(0), tagsToAdd).execute(modelStub);
         assertEquals(String.format(MESSAGE_SUCCESS, Tag.toString(tagsToAdd)), commandResult.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_targetPersonDoesNotAlreadyHaveTags_success() throws Exception {
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
+        modelStub.personList.add(ALICE);
+        modelStub.setTargetPerson(ALICE);
+        Set<Tag> tagsToAdd = new HashSet<>();
+        tagsToAdd.add(VALID_TAG_OWES_MONEY);
+        // no index provided
+        CommandResult commandResult = new TagCommand(tagsToAdd).execute(modelStub);
+        assertEquals(String.format(MESSAGE_SUCCESS, Tag.toString(tagsToAdd)), commandResult.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_tagDoesNotExist_tagsNotFound() throws Exception {
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
+        modelStub.personList.add(ALICE);
+        Set<Tag> tagsToAdd = new HashSet<>();
+        tagsToAdd.add(new Tag("FakeTag"));
+        CommandResult commandResult = new TagCommand(Index.fromZeroBased(0), tagsToAdd).execute(modelStub);
+        assertEquals(String.format(MESSAGE_TAGS_NOT_FOUND, Tag.toString(tagsToAdd)), commandResult.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_personAlreadyHaveTagsAndNoNewTagsAdded_duplicateTagsAndNoTagsAddedMessage() throws Exception {
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
+        modelStub.personList.add(ALICE);
+        Set<Tag> tagsToAdd = new HashSet<>();
+        tagsToAdd.add(VALID_TAG_OWES_MONEY);
+        CommandResult command0Result = new TagCommand(Index.fromZeroBased(0), tagsToAdd).execute(modelStub);
+        assertEquals(String.format(MESSAGE_SUCCESS, Tag.toString(tagsToAdd)), command0Result.getFeedbackToUser());
+
+        CommandResult command1Result = new TagCommand(Index.fromZeroBased(0), tagsToAdd).execute(modelStub);
+        StringBuilder expectedResult = new StringBuilder();
+        expectedResult.append(String.format(MESSAGE_DUPLICATE_TAGS, Tag.toString(tagsToAdd)) + "\n");
+        expectedResult.append(String.format(MESSAGE_NO_TAGS_ADDED));
+        assertEquals(expectedResult.toString(), command1Result.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_personAlreadyHaveTagsAndANewTagAdded_duplicateTagsAndTagAddedAddedMessage() throws Exception {
+        ModelStubWithPersonListAndTargetPerson modelStub = new ModelStubWithPersonListAndTargetPerson();
+        modelStub.personList.add(ALICE);
+        Set<Tag> tagsToAdd = new HashSet<>();
+        tagsToAdd.add(VALID_TAG_OWES_MONEY);
+        // Alice already have tag friends
+        tagsToAdd.add(VALID_TAG_FRIENDS);
+
+        CommandResult commandResult = new TagCommand(Index.fromZeroBased(0), tagsToAdd).execute(modelStub);
+        StringBuilder expectedResult = new StringBuilder();
+        expectedResult.append(String.format(MESSAGE_DUPLICATE_TAGS, VALID_TAG_FRIENDS.tagName) + "\n");
+        expectedResult.append(String.format(MESSAGE_SUCCESS, VALID_TAG_OWES_MONEY.tagName));
+        assertEquals(expectedResult.toString(), commandResult.getFeedbackToUser());
     }
 
     @Test
@@ -87,11 +151,12 @@ public class TagCommandTest {
         assertFalse(tagCommand1.equals(null));
     }
 
-    static class ModelStubWithPersonList extends ModelStub {
+    static class ModelStubWithPersonListAndTargetPerson extends ModelStub {
         private Set<Tag> tags = new HashSet<>();
         private ObservableList<Person> personList = FXCollections.observableArrayList();
+        private TargetPerson targetPerson = new TargetPerson();
 
-        public ModelStubWithPersonList() {
+        public ModelStubWithPersonListAndTargetPerson() {
             tags.add(VALID_TAG_FRIENDS);
             tags.add(VALID_TAG_OWES_MONEY);
         }
@@ -138,6 +203,31 @@ public class TagCommandTest {
                     target.getAddress(), target.getRemark(), newTags);
             int i = personList.indexOf(target);
             personList.set(i, untaggedPerson);
+        }
+
+        @Override
+        public void setTargetPerson(Person person) {
+            targetPerson.set(person);
+        }
+
+        @Override
+        public void clearTargetPerson() {
+            targetPerson.clear();
+        }
+
+        @Override
+        public boolean isTargetPerson(Person person) {
+            return targetPerson.isSamePerson(person);
+        }
+
+        @Override
+        public boolean hasTargetPerson() {
+            return targetPerson.isPresent();
+        }
+
+        @Override
+        public Person getTargetPerson() {
+            return targetPerson.get();
         }
     }
 }
