38174960c9198ab09bf9e786a44ddf8ddfcf86aa
jasonqiu212 <jason_qiu@hotmail.com>
2022-10-10 16:56:39 +0800

    Test: add tests for TaskNameContainsKeywordsPredicate

---
+++ src/test/java/swift/logic/parser/FindTaskCommandParserTest.java
@@ -0,0 +1,33 @@
+package swift.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import swift.logic.commands.FindCommand;
+import swift.model.person.PersonNameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static swift.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static swift.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static swift.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class FindTaskCommandParserTest {
+
+    private FindCommandParser parser = new FindCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PersonNameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
+        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+    }
+
+}
