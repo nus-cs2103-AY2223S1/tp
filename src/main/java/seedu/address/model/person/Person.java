5c8114c4e29055718a86e73b10da36fcd78f2b11
weesiau <e0725311@u.nus.edu>
2022-10-10 09:36:20 +0800

    Change behaviour of duplicate checking by phone
    Similar phone numbers are now considered duplicates and not allowed to be added

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -67,7 +67,7 @@ public class Person {
     }
 
     /**
-     * Returns true if both persons have the same name.
+     * Returns true if both persons have the same name and/or phone number.
      * This defines a weaker notion of equality between two persons.
      */
     public boolean isSamePerson(Person otherPerson) {
@@ -76,7 +76,8 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && (otherPerson.getName().equals(getName())
+                || otherPerson.getPhone().equals(getPhone()));
     }
 
     /**
