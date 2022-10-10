8d293853656ff4bc9d24757b6c6db76f09d1f380
DevanshShah1309 <devanshshah1309@gmail.com>
2022-10-10 16:17:41 +0800

    Add tests for Task class

---
+++ src/test/java/seedu/address/model/team/TaskTest.java
@@ -0,0 +1,34 @@
+package seedu.address.model.team;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TaskTest {
+
+    @Test
+    public void null_constructor_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Task(null));
+    }
+
+    @Test
+    public void invalidName_constructor_throwsInvalidArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Task(" "));
+    }
+
+    @Test
+    public void equals() {
+        Task task1 = new Task("task");
+        Task task2 = new Task("task");
+        assertTrue(task1.equals(task2));
+    }
+
+    @Test
+    public void valid_toString_equalNames() {
+        Task task1 = new Task("task");
+        assertEquals("task", task1.toString());
+    }
+}
