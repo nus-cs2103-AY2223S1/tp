2f49fd65dd5154815d766adc0196102804f24a5b
domoboom <berzindominic@gmail.com>
2022-10-10 20:15:59 +0800

    fix minor checkstyle error for an unused import

--- src/main/java/modtrekt/ui/TaskCard.java
+++ src/main/java/modtrekt/ui/TaskCard.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import modtrekt.commons.util.StringUtil;
 import modtrekt.model.task.Task;
 
 /**
