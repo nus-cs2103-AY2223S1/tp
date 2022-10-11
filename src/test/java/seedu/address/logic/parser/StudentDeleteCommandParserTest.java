cb56c89434f48ceb069d880a5e8b8c60eab58a18
limweijun <97403500+limweijun@users.noreply.github.com>
2022-10-11 14:40:10 +0800

    Add test files for student add and delete (#65)

---
+++ src/test/java/seedu/address/logic/parser/StudentDeleteCommandParserTest.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.StudentDeleteCommand;
+
+public class StudentDeleteCommandParserTest {
+    private StudentDeleteCommandParser parser = new StudentDeleteCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteCommand() {
+        assertParseSuccess(parser, "1", new StudentDeleteCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                           StudentDeleteCommand.MESSAGE_USAGE));
+    }
+}
