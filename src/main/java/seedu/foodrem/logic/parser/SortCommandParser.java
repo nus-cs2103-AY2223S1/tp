0d7fb6a5fdc55e6aae5b774b83a9d2a5da79980e
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-11 17:16:51 +0800

    Add SortCommandParser
    Currently only sorts by name. Future iterations
    should allow sorting by specified arguments.

---
+++ src/main/java/seedu/foodrem/logic/parser/SortCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.foodrem.logic.parser;
+
+import static seedu.foodrem.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.foodrem.logic.commands.FindCommand;
+import seedu.foodrem.logic.commands.SortCommand;
+import seedu.foodrem.logic.parser.exceptions.ParseException;
+import seedu.foodrem.model.item.NameItemComparator;
+
+/**
+ * Parses input arguments and creates a new
+ * SortCommand object
+ */
+public class SortCommandParser implements Parser<SortCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SortCommand
+     * and returns a SortCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SortCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        return new SortCommand(new NameItemComparator());
+    }
+}
