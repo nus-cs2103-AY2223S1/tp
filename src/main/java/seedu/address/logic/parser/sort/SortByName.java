ff9922140b26aa6523d1f4ff02d7f134d9ef2196
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-10-11 15:04:05 +0800

    Add functionality to sort by name

--- src/main/java/seedu/address/logic/parser/sort/SortByName.java
+++ src/main/java/seedu/address/logic/parser/sort/SortByName.java
@@ -1,2 +1,12 @@
-package seedu.address.logic.parser.sort;public class SortByName {
+package seedu.address.logic.parser.sort;
+
+import seedu.address.model.person.Person;
+
+import  java.util.Comparator;
+public class SortByName implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().fullName.compareTo(p2.getName().fullName);
+    }
 }
