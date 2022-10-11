6f26db88ac8f9487cb1434fe0bc74f01fcde0602
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-10-11 14:52:51 +0800

    Add a way to parse in sorting command to addressbook

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -16,6 +16,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -72,7 +73,10 @@ public class AddressBookParser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
-        default:
+        case SortCommand.COMMAND_WORD:
+            return new SortCommandParser().parse(arguments);
+
+            default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
