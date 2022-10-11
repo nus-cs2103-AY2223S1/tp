44574383c9e0facc5181c87fe4a1685a7a688f9f
ShawnChew <shawnchew200@gmail.com>
2022-10-11 16:18:13 +0800

    Add test for ProfileCommandParser for -f

--- src/test/java/seedu/address/logic/parser/profile/ProfileCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/profile/ProfileCommandParserTest.java
@@ -1,11 +1,21 @@
 package seedu.address.logic.parser.profile;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_OPTION;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.profile.FindProfileCommand;
 import seedu.address.logic.commands.profile.ProfileCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.profile.NameContainsKeywordsPredicate;
 
 public class ProfileCommandParserTest {
 
@@ -35,4 +45,16 @@ public class ProfileCommandParserTest {
                 ProfileCommand.OPTION_UNKNOWN + ProfileCommand.VALID_FLAGS);
     }
 
+    @Test
+    public void parse_profileOption_find() {
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        try {
+            FindProfileCommand command = (FindProfileCommand) parser.parse(
+                    " " + PREFIX_OPTION + FindProfileCommand.COMMAND_OPTION + " "
+                            + keywords.stream().collect(Collectors.joining(" ")));
+            assertEquals(new FindProfileCommand(new NameContainsKeywordsPredicate(keywords)), command);
+        } catch (ParseException e) {
+            fail();
+        }
+    }
 }
