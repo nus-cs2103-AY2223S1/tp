a2c7a8d44a42bb77dcdc77aef746d8741937662a
teoyuqi <teo.yuqi@gmail.com>
2022-10-12 03:13:57 +0800

    Add unit testing for `JsonAdaptedTask`

---
+++ src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
@@ -0,0 +1,36 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTasks.TASK_A;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.module.task.Task;
+
+public class JsonAdaptedTaskTest {
+    @Test
+    public void toModelType_validTaskDetails_returnsTask() throws Exception {
+        JsonAdaptedTask task = new JsonAdaptedTask(TASK_A);
+        assertEquals(TASK_A, task.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidTaskDescription_throwsIllegalValueException() {
+        JsonAdaptedTask task =
+                new JsonAdaptedTask("");
+        String expectedMessage = Task.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage,
+                task::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullTaskDescription_throwsNullPointerException() {
+        JsonAdaptedTask task = new JsonAdaptedTask((String) null);
+        String expectedMessage = Task.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage,
+                task::toModelType);
+    }
+
+}
