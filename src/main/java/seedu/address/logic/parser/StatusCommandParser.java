2b447651a177d7e6bca413a5b2ec4428227c36dd
kangqiao322 <kangqiao803@gmail.com>
2022-10-11 23:27:56 +0800

    CLI fix

--- src/main/java/seedu/address/logic/parser/StatusCommandParser.java
+++ src/main/java/seedu/address/logic/parser/StatusCommandParser.java
@@ -2,7 +2,6 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
