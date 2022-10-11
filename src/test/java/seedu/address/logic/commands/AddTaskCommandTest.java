fc8ad8ca28fb50efd48db9051ba2cf4f40d09515
izzahaj <izzah.jalil02@gmail.com>
2022-10-11 23:13:40 +0800

    Add tests for constructor

--- src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
@@ -1,7 +1,26 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalTasks.TASK_INSULIN;
+
+import org.junit.jupiter.api.Test;
+
 /**
- * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for AddTaskCommand.
  */
 public class AddTaskCommandTest {
+    // test utils; to be moved to a separate utility class
+
+    @Test
+    public void constructor_nullTask_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddTaskCommand(INDEX_FIRST_PERSON, null));
+    }
+
+    @Test
+    public void constructor_nullIndex_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddTaskCommand(null, TASK_INSULIN));
+    }
+
+
 }
