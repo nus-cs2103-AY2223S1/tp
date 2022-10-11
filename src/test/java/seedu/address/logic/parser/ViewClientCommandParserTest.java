81443013a825304149c606f2198868a1772a85ed
ThomasHoooo <e0725843@u.nus.edu>
2022-10-11 22:30:49 +0800

    Resolve conflicts

--- src/test/java/seedu/address/logic/parser/ViewClientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewClientCommandParserTest.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ELEMENT;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +14,7 @@ public class ViewClientCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, " i/1", new ViewClientCommand(INDEX_FIRST_CLIENT));
+        assertParseSuccess(parser, " i/1", new ViewClientCommand(INDEX_FIRST_ELEMENT));
     }
 
     @Test
