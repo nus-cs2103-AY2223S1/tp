b46fc802c8b3222d5401e8c740260aec8ba497c3
ningtan11 <e0773603@u.nus.edu>
2022-10-11 16:36:11 +0800

    Checkstyle

--- src/main/java/seedu/waddle/ui/ItemListPanel.java
+++ src/main/java/seedu/waddle/ui/ItemListPanel.java
@@ -1,5 +1,7 @@
 package seedu.waddle.ui;
 
+import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -7,8 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.waddle.commons.core.LogsCenter;
 import seedu.waddle.model.itinerary.Item;
-
-import java.util.logging.Logger;
 
 /**
  * Panel containing the list of Items.
