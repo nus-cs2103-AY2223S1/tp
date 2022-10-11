42e709f4293c7ecfc78344e3e5d860db1f752659
benjytan45678 <Benjytan45678@gmail.com>
2022-10-11 15:53:58 +0800

    Grammar fixes

--- src/test/java/seedu/address/model/applicant/ApplicationStatusPredicateTest.java
+++ src/test/java/seedu/address/model/applicant/ApplicationStatusPredicateTest.java
@@ -3,10 +3,6 @@ package seedu.address.model.applicant;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.ApplicantBuilder;
