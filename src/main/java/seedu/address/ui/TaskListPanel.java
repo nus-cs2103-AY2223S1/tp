38234e5102a10473b8af9d13f8454cfaa6297538
thamruicong <e0726692@u.nus.edu>
2022-10-11 18:42:37 +0800

    Add Task list header

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
@@ -13,12 +14,16 @@ import java.util.logging.Logger;
 public class TaskListPanel extends UiPart<Region> {
     private static final String FXML = "TaskListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+    private static final String TASK_HEADER = "Tasks";
 
+    @FXML
+    private Label header;
     @FXML
     private ListView<Task> taskListView;
 
     public TaskListPanel(ObservableList<Task> taskList) {
         super(FXML);
+        header.setText(TASK_HEADER);
         taskListView.setItems(taskList);
         taskListView.setCellFactory(listView -> new TaskListViewCell());
     }
