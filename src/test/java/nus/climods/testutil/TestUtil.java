554ec550567ed34eff3d024ed3379ef851ced580
shenyihong <shen_yi_hong@outlook.com>
2022-10-11 21:20:38 +0800

    Remove `AB3` tests

--- src/test/java/nus/climods/testutil/TestUtil.java
+++ src/test/java/nus/climods/testutil/TestUtil.java
@@ -31,25 +31,4 @@ public class TestUtil {
         }
         return SANDBOX_FOLDER.resolve(fileName);
     }
-
-    /**
-     * Returns the middle index of the person in the {@code model}'s person list.
-     */
-    public static Index getMidIndex(Model model) {
-        return Index.fromOneBased(model.getFilteredPersonList().size() / 2);
-    }
-
-    /**
-     * Returns the last index of the person in the {@code model}'s person list.
-     */
-    public static Index getLastIndex(Model model) {
-        return Index.fromOneBased(model.getFilteredPersonList().size());
-    }
-
-    /**
-     * Returns the person in the {@code model}'s person list at {@code index}.
-     */
-    public static Person getPerson(Model model, Index index) {
-        return model.getFilteredPersonList().get(index.getZeroBased());
-    }
 }
