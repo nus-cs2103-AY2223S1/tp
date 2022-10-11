4209999e90c4ae7b4c89b2d4f26b0dbfb3a39ff9
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-10-11 22:15:52 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -2,13 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.SortCommand;
-import seedu.address.logic.parser.sort.SortByName;
-
 
 
 public class SortCommandParserTest {
