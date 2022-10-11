774da44cb494ba070d61a293b15ca89e248e6507
Wang Xinyi <e0817822@u.nus.edu>
2022-10-11 12:29:54 +0800

    test: Fix CI error

--- src/test/java/seedu/address/logic/commands/task/MarkTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/task/MarkTaskCommandTest.java
@@ -2,7 +2,9 @@ package seedu.address.logic.commands.task;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
