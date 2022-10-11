0a2657009d02f29d568c8c6687e98d99d55b6ee8
nseah21 <nseah21@gmail.com>
2022-10-12 01:53:21 +0800

    Refactor Fields class to ResidentFields class

---
+++ src/main/java/seedu/rc4hdb/model/resident/fields/ResidentFields.java
@@ -0,0 +1,20 @@
+package seedu.rc4hdb.model.resident.fields;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class ResidentFields {
+    public static final String EMAIL = "Email";
+    public static final String INDEX = "Index";
+    public static final String GENDER = "Gender";
+    public static final String HOUSE = "House";
+    public static final String MATRIC = "Matric";
+    public static final String NAME = "Name";
+    public static final String PHONE = "Phone";
+    public static final String ROOM = "Room";
+    public static final String TAG = "Tags";
+
+    public static final List<String> FIELDS = List.of(EMAIL, INDEX, GENDER, HOUSE, MATRIC, NAME, PHONE, ROOM, TAG);
+}
