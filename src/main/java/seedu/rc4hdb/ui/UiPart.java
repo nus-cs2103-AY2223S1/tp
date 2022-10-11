c506cc227f6778ad0b21bd04be49a32f4030926f
neale <nealetham2000@gmail.com>
2022-10-11 17:10:53 +0800

    Fix javadoc errors

--- src/main/java/seedu/rc4hdb/ui/UiPart.java
+++ src/main/java/seedu/rc4hdb/ui/UiPart.java
@@ -45,7 +45,8 @@ public abstract class UiPart<T> {
 
     /**
      * Constructs a UiPart with the specified FXML file within {@link #FXML_FILE_FOLDER} and root object.
-     * @see #UiPart(URL, T)
+     * @see #UiPart(URL)
+     * @see #UiPart(String)
      */
     public UiPart(String fxmlFileName, T root) {
         this(getFxmlFileUrl(fxmlFileName), root);
