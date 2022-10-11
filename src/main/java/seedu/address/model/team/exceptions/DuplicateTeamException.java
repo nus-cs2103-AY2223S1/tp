02ce9c925bb6bac7e92467971abeb216a7a366e7
richavm14 <richavm14@gmail.com>
2022-10-11 14:51:34 +0800

    Use team exceptions in required methods

--- src/main/java/seedu/address/model/team/exceptions/DuplicateTeamException.java
+++ src/main/java/seedu/address/model/team/exceptions/DuplicateTeamException.java
@@ -6,6 +6,6 @@ package seedu.address.model.team.exceptions;
  */
 public class DuplicateTeamException extends RuntimeException {
     public DuplicateTeamException() {
-        super("Operation would result in duplicate persons");
+        super("Operation would result in duplicate teams");
     }
 }
--- src/main/java/seedu/address/model/team/exceptions/TeamNotFoundException.java
+++ src/main/java/seedu/address/model/team/exceptions/TeamNotFoundException.java
@@ -1,6 +1,6 @@
 package seedu.address.model.team.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified person.
+ * Signals that the operation is unable to find the specified team.
  */
 public class TeamNotFoundException extends RuntimeException {}
