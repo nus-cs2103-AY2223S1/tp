3066039f9ee2ddab5378959ab642b89112cf24fa
sjoann <joansim19@gmail.com>
2022-10-11 23:10:10 +0800

    Update more test cases

--- src/test/java/seedu/address/storage/ClassStorageTest.java
+++ src/test/java/seedu/address/storage/ClassStorageTest.java
@@ -2,12 +2,24 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalTime;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.util.JsonUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
 public class ClassStorageTest {
+
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
+    private static final Path PERSONS_FILE = TEST_DATA_FOLDER.resolve("personsAddressBook.json");
 
     @Test
     public void execute_hasConflictSuccess() {
@@ -26,6 +38,17 @@ public class ClassStorageTest {
         assertFalse(ClassStorage.hasConflict(LocalTime.of(12, 30), LocalTime.of(13, 30),
                 LocalTime.of(11, 0), LocalTime.of(12, 0)));
         assertFalse(ClassStorage.hasConflict(null, null, null, null));
+    }
+
+    @Test
+    public void execute_saveClassFailure() throws Exception {
+        Person person = new PersonBuilder().withName("Daniel Tan").withPhone("81201230")
+                .withEmail("cornelia@example.com").withAddress("10th street").withClass("2022-05-05 1200-1400").build();
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(PERSONS_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+        ClassStorage classStorage = new ClassStorage(addressBookFromFile);
+        assertThrows(CommandException.class, () -> classStorage.saveClass(person));
     }
 
 }
