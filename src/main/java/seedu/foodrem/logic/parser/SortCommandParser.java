ecaf5865055b4fa31862f05ce56b38f7acd8e97a
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-11 21:42:41 +0800

    Modify SortCommandParser to accept multiple Sort arguments

--- src/main/java/seedu/foodrem/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/foodrem/logic/parser/SortCommandParser.java
@@ -7,16 +7,27 @@ import static seedu.foodrem.logic.parser.CliSyntax.PREFIX_SORT_BY_NAME;
 import static seedu.foodrem.logic.parser.CliSyntax.PREFIX_SORT_BY_QTY;
 import static seedu.foodrem.logic.parser.CliSyntax.PREFIX_SORT_BY_TYPE;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import seedu.foodrem.logic.commands.FindCommand;
 import seedu.foodrem.logic.commands.SortCommand;
 import seedu.foodrem.logic.parser.exceptions.ParseException;
+import seedu.foodrem.model.item.Item;
+import seedu.foodrem.model.item.itemcomparators.ItemBoughtDateComparator;
+import seedu.foodrem.model.item.itemcomparators.ItemComparator;
+import seedu.foodrem.model.item.itemcomparators.ItemExpiryDateComparator;
 import seedu.foodrem.model.item.itemcomparators.ItemNameComparator;
+import seedu.foodrem.model.item.itemcomparators.ItemQuantityComparator;
+import seedu.foodrem.model.item.itemcomparators.ItemUnitComparator;
+import seedu.foodrem.model.util.ChainComparator;
 
 /**
  * Parses input arguments and creates a new
  * SortCommand object
  */
 public class SortCommandParser implements Parser<SortCommand> {
+
 
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
@@ -30,14 +41,33 @@ public class SortCommandParser implements Parser<SortCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
-        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args,
+        List<ItemComparator> comparators = new ArrayList<>();
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
                 PREFIX_SORT_BY_NAME,
                 PREFIX_SORT_BY_QTY,
                 PREFIX_SORT_BY_BOUGHT_DATE,
                 PREFIX_SORT_BY_EXPIRY_DATE,
                 PREFIX_SORT_BY_TYPE);
 
+        if (argMultimap.getValue(PREFIX_SORT_BY_NAME).isPresent()) {
+            comparators.add(new ItemNameComparator());
+        }
+        if (argMultimap.getValue(PREFIX_SORT_BY_QTY).isPresent()) {
+            comparators.add(new ItemQuantityComparator());
+        }
+        if (argMultimap.getValue(PREFIX_SORT_BY_BOUGHT_DATE).isPresent()) {
+            comparators.add(new ItemBoughtDateComparator());
+        }
+        if (argMultimap.getValue(PREFIX_SORT_BY_EXPIRY_DATE).isPresent()) {
+            comparators.add(new ItemExpiryDateComparator());
+        }
+        if (argMultimap.getValue(PREFIX_SORT_BY_TYPE).isPresent()) {
+            comparators.add(new ItemUnitComparator());
+        }
+        if (comparators.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+        }
 
-        return new SortCommand(new ItemNameComparator());
+        return new SortCommand(new ChainComparator<Item>(comparators));
     }
 }
