ecacabcceb75e214eeaa0d2974cf401bf2771cdf
richavm14 <richavm14@gmail.com>
2022-10-11 23:54:34 +0800

    Update unassign member from team to use index

--- src/main/java/seedu/address/logic/commands/UnAssignMemberCommand.java
+++ src/main/java/seedu/address/logic/commands/UnAssignMemberCommand.java
@@ -2,9 +2,9 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAM_NAME;
+import static seedu.address.logic.parser.CliSyntax.*;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Name;
@@ -21,35 +21,41 @@ public class UnAssignMemberCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": removes a person from the team. "
             + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_TEAM_NAME + "TEAM NAME \n"
+            + PREFIX_MEMBER_INDEX + "MEMBER INDEX "
+            + PREFIX_TEAM_INDEX + "TEAM INDEX \n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_TEAM_NAME + "Team1";
+            + PREFIX_MEMBER_INDEX + "1 "
+            + PREFIX_TEAM_INDEX + "1";
 
-    public static final String MESSAGE_SUCCESS = "Person removed: %1$s to team: %2$s";
+    public static final String MESSAGE_SUCCESS = "Person removed: %1$s from team: %2$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the team";
     public static final String MESSAGE_ARGUMENTS = "Person: %1$s, Team: %2$s";
 
-    private final Name toUnAssign;
-    private final seedu.address.model.team.Name teamName;
+    private final Index personIndex;
+    private final Index teamIndex;
 
     /**
      * Creates an AddCommand to add the specified {@code Person}
      */
-    public UnAssignMemberCommand(Name person, seedu.address.model.team.Name teamName) {
-        requireAllNonNull(person, teamName);
-        this.toUnAssign = person;
-        this.teamName = teamName;
+    public UnAssignMemberCommand(Index teamIndex, Index personIndex) {
+        requireAllNonNull(personIndex, teamIndex);
+        this.personIndex = personIndex;
+        this.teamIndex = teamIndex;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
+
         requireNonNull(model);
-        Person p = model.getPerson(toUnAssign);
-        Team t = model.getTeam(teamName);
-        model.removePersonFromTeam(p, t);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, toUnAssign, teamName));
+
+        Team team = model.getTeamUsingIndex(teamIndex);
+        Person person = model.getPersonUsingIndex(personIndex);
+
+        seedu.address.model.team.Name teamName = model.getTeamName(teamIndex);
+        seedu.address.model.person.Name personName = model.getPersonName(personIndex);
+        model.removePersonFromTeam(person, team);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, personName, teamName));
     }
 
 }
