538efa2fcdf1c73857c56943785aed1adc3368a4
Agamjyot Singh Chadha <agamjyot01@gmail.com>
2022-10-11 19:37:10 +0800

    Change MESSAGE_USAGE sting

--- src/main/java/eatwhere/foodguide/logic/commands/FindCommand.java
+++ src/main/java/eatwhere/foodguide/logic/commands/FindCommand.java
@@ -17,7 +17,7 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all eateries whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " Chicken Rice";
 
     private final NameContainsKeywordsPredicate predicate;
 
