d3d2771d034c0290bca261c97516413ec4926cd7
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-10-11 22:42:21 +0800

    Add extra test cases to SortCommandParser

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -2,10 +2,12 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.parser.sort.SortByName;
 
 
 public class SortCommandParserTest {
@@ -22,4 +24,16 @@ public class SortCommandParserTest {
     public void parse_invalidArg_throwsParseException() {
         assertParseFailure(parser, "na", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
     }
+
+    // No Trailing or leading whitespaces
+    @Test
+    public void parse_validArgs_returnsSortCommand() {
+        SortCommand expectedSortCommand =
+                new SortCommand(new SortByName(), "name");
+        assertParseSuccess(parser, "name", expectedSortCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n name \n", expectedSortCommand);
+    }
 }
+
