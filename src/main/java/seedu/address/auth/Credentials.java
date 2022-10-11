d611f7cd22038f5a7bd559fc30683e29fe635a6a
Ezra <ezratio15195@gmail.com>
2022-10-12 00:27:07 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/auth/Credentials.java
+++ src/main/java/seedu/address/auth/Credentials.java
@@ -2,6 +2,6 @@ package seedu.address.auth;
 
 public class Credentials {
 
-  public static String DEBUG_USERNAME = "admin";
-  public static String DEBUG_PASSWORD = "admin";
+    public static String DEBUG_USERNAME = "admin";
+    public static String DEBUG_PASSWORD = "admin";
 }
