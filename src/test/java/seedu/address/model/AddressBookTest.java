65c02a5226f8aae5233c46e575429385f36b07ed
Shashank Acharya <acharya.s@icloud.com>
2022-10-11 12:11:18 +0800

    Update AddressBook tests to include events

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -3,10 +3,12 @@ package seedu.address.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_CCA;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalEvents.PRESENTATION;
 import static seedu.address.testutil.TypicalProfiles.ALICE;
-import static seedu.address.testutil.TypicalProfiles.getTypicalAddressBook;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,8 +21,10 @@ import org.junit.jupiter.api.Test;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.model.event.Event;
+import seedu.address.model.event.exceptions.DuplicateEventException;
 import seedu.address.model.profile.Profile;
 import seedu.address.model.profile.exceptions.DuplicateProfileException;
+import seedu.address.testutil.EventBuilder;
 import seedu.address.testutil.ProfileBuilder;
 
 public class AddressBookTest {
@@ -56,8 +60,24 @@ public class AddressBookTest {
     }
 
     @Test
+    public void resetData_withDuplicateEvents_throwsDuplicateEventException() {
+        // Two profiles with the same identity fields
+        Event editedPresentation = new EventBuilder(PRESENTATION)
+                .withTags(VALID_TAG_CCA).build();
+        List<Event> newEvents = Arrays.asList(PRESENTATION, editedPresentation);
+        List<Profile> emptyProfiles = new ArrayList<>();
+        AddressBookStub newData = new AddressBookStub(emptyProfiles, newEvents);
+        assertThrows(DuplicateEventException.class, () -> addressBook.resetData(newData));
+    }
+
+    @Test
     public void hasProfile_nullProfile_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> addressBook.hasProfile(null));
+    }
+
+    @Test
+    public void hasEvent_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> addressBook.hasEvent(null));
     }
 
     @Test
@@ -66,9 +86,20 @@ public class AddressBookTest {
     }
 
     @Test
+    public void hasEvent_eventNotInAddressBook_returnsFalse() {
+        assertFalse(addressBook.hasEvent(PRESENTATION));
+    }
+
+    @Test
     public void hasProfile_profileInAddressBook_returnsTrue() {
         addressBook.addProfile(ALICE);
         assertTrue(addressBook.hasProfile(ALICE));
+    }
+
+    @Test
+    public void hasEvent_eventInAddressBook_returnsTrue() {
+        addressBook.addEvent(PRESENTATION);
+        assertTrue(addressBook.hasEvent(PRESENTATION));
     }
 
     @Test
@@ -80,8 +111,21 @@ public class AddressBookTest {
     }
 
     @Test
+    public void hasEvent_eventWithSameIdentityFieldsInAddressBook_returnsTrue() {
+        addressBook.addEvent(PRESENTATION);
+        Event editedPresentation = new EventBuilder(PRESENTATION).withTags(VALID_TAG_HUSBAND)
+                .build();
+        assertTrue(addressBook.hasEvent(editedPresentation));
+    }
+
+    @Test
     public void getProfileList_modifyList_throwsUnsupportedOperationException() {
         assertThrows(UnsupportedOperationException.class, () -> addressBook.getProfileList().remove(0));
+    }
+
+    @Test
+    public void getEventList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> addressBook.getEventList().remove(0));
     }
 
     /**
