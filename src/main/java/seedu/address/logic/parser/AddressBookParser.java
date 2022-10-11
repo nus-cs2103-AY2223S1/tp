db6252a546e57ad5382cc639c8490814346d4c9e
elyseleong <elyse.xuan@gmail.com>
2022-10-11 20:33:54 +0800

    Add attendance command and parser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,20 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddQCommand;
-import seedu.address.logic.commands.AddTutorialCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.DeleteQCommand;
-import seedu.address.logic.commands.DeleteTutorialCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.MarkTutorialCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -87,6 +74,9 @@ public class AddressBookParser {
 
         case MarkTutorialCommand.COMMAND_WORD:
             return new MarkTutorialCommandParser().parse(arguments);
+
+        case AttendanceCommand.COMMAND_WORD:
+            return new AttendanceCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
---
+++ src/main/java/seedu/address/logic/parser/AttendanceCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AttendanceCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.student.Attendance;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new AttendanceCommand object
+ */
+public class AttendanceCommandParser implements Parser<AttendanceCommand> {
+
+    /**
+     * Parses {@code userInput} in the context of the AttendanceCommand
+     * and returns a AttendanceCommand object for execution.
+     *
+     * @param userInput to parse.
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    @Override
+    public AttendanceCommand parse(String userInput) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(userInput);
+            return new AttendanceCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttendanceCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
