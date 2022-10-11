8cbe5b779df9b8b984fd445fe624422b05d8ad9f
Wang Xinyi <e0817822@u.nus.edu>
2022-10-11 12:06:08 +0800

    fix: Fix testing on AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -174,6 +174,11 @@ public class AddCommandTest {
         public void addTask(Task task) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void markTask(Task task) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
--- src/test/java/seedu/address/logic/commands/task/AddTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/task/AddTaskCommandTest.java
@@ -176,6 +176,11 @@ public class AddTaskCommandTest {
         public void addTask(Task task) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void markTask(Task task) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
