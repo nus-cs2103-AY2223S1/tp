de48bb83be0c17cb9b9f6dcc89feddbee1096fc8
florentianayuwono <e0851439@u.nus.edu>
2022-10-11 22:13:48 +0800

    Delete incorrect test cases

--- src/test/java/seedu/address/logic/commands/SearchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SearchCommandTest.java
@@ -86,16 +86,6 @@ public class SearchCommandTest {
         assertEquals(Collections.emptyList(), model.getFilteredPersonList());
     }
 
-    @Test
-    public void execute_multipleKeywords_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-        ContactContainsAnyKeywordsPredicate predicate = preparePredicate(INDICATOR_NAME, "Kurz Elle Kunz");
-        SearchCommand command = new SearchCommand(predicate);
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredPersonList());
-    }
-
     /**
      * Parses {@code userInput} into a {@code ContactContainsAnyKeywordsPredicate}.
      */
