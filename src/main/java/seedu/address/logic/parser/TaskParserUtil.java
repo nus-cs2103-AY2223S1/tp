01b0e983f715b5430cc0c1e19096a2995af076d3
Amresh <amresh2000@gmail.com>
2022-10-11 21:31:49 +0800

    bug: minor bug fixes

--- src/main/java/seedu/address/logic/parser/TaskParserUtil.java
+++ src/main/java/seedu/address/logic/parser/TaskParserUtil.java
@@ -41,17 +41,5 @@ public class TaskParserUtil {
         }
         return new Title(trimmedTitle);
     }
-
-    /**
-     * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
-     * trimmed.
-     * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
-     */
-    public static Index parseIndex(String oneBasedIndex) throws ParseException {
-        String trimmedIndex = oneBasedIndex.trim();
-        if (!StringUtil.isNonZeroUnsignedInteger(trimmedIndex)) {
-            throw new ParseException(MESSAGE_INVALID_INDEX);
-        }
-        return Index.fromOneBased(Integer.parseInt(trimmedIndex));
-    }
+    
 }
--- src/main/java/seedu/address/logic/parser/task/DeleteTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/task/DeleteTaskCommandParser.java
@@ -2,11 +2,14 @@ package seedu.address.logic.parser.task;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.task.DeleteTaskCommand;
+import seedu.address.logic.commands.task.MarkTaskCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.TaskParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 /**
  * Parses input arguments and creates a new DeleteTaskCommand object
@@ -21,8 +24,13 @@ public class DeleteTaskCommandParser implements Parser<DeleteTaskCommand> {
     public DeleteTaskCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
 
-        Index index = TaskParserUtil.parseIndex(argMultimap.getPreamble());
+        Index targetIndex;
+        try {
+            targetIndex = TaskParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkTaskCommand.MESSAGE_USAGE), pe);
+        }
 
-        return new DeleteTaskCommand(index);
+        return new DeleteTaskCommand(targetIndex);
     }
 }
