4717ec908121378a5f2a89a9e755b9518dfec8e9
normkoh <kohweiquan1@gmail.com>
2022-10-11 15:30:04 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -8,8 +8,8 @@ import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalSetsOfIndex.SET_OF_ONE_INDEX;
 import static seedu.address.testutil.TypicalSetsOfIndex.SET_OF_ONE_AND_SECOND_INDEX;
+import static seedu.address.testutil.TypicalSetsOfIndex.SET_OF_ONE_INDEX;
 import static seedu.address.testutil.TypicalSetsOfIndex.SET_OF_SECOND_INDEX;
 
 import java.util.Arrays;
