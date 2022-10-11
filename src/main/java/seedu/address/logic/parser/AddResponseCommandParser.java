55f90c76b540549e2e3d32e0048155c9350db2e5
qiaoen17 <92434807+qiaoen17@users.noreply.github.com>
2022-10-11 16:30:51 +0800

    Fix import spaces

--- src/main/java/seedu/address/logic/parser/AddResponseCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddResponseCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MESSAGE_COUNT;
 
 import seedu.address.commons.core.index.Index;
-
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddResponseCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
