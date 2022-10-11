7305796002fb863184fd1c56eb8a9e041de673c1
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-11 23:50:35 +0800

    Add IncrementCommand and IncrementCommandParser

---
+++ src/main/java/seedu/foodrem/logic/parser/IncrementCommandParser.java
@@ -0,0 +1,38 @@
+package seedu.foodrem.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.foodrem.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.foodrem.logic.parser.CliSyntax.PREFIX_ITEM_QUANTITY;
+
+import seedu.foodrem.commons.core.index.Index;
+import seedu.foodrem.logic.commands.itemcommands.EditCommand;
+import seedu.foodrem.logic.commands.itemcommands.IncrementCommand;
+import seedu.foodrem.logic.parser.exceptions.ParseException;
+import seedu.foodrem.model.item.ItemQuantity;
+
+/**
+ * Parses input arguments and creates a new IncrementCommand object
+ */
+public class IncrementCommandParser implements Parser<IncrementCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the DecrementCommand
+     * and returns an DecrementCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public IncrementCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ITEM_QUANTITY);
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
+        }
+
+        ItemQuantity incrementQuantity = ParserUtil.parseQuantity(argMultimap.getPresentValue(PREFIX_ITEM_QUANTITY));
+
+        return new IncrementCommand(index, incrementQuantity);
+    }
+}
