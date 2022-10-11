5a46104ff3202e29d521d552ff1ae7202ac60bd0
nqt230 <76938663+nqt230@users.noreply.github.com>
2022-10-12 01:45:34 +0800

    Update `AddInternshipCommand` to support linking
    - Modify `AddInternshipCommandParser` to now parse an `Index`, which will be used to find the person to link the newly created `Internship` to
        - Modify `CLISyntax` prefixes to match UG and new command format for add internship
        - Modify `AddInternshipCommand` class to support linking via use of `Index`
    - Update `AddressBook` methods for add and remove to support 2-way linking
        - Modify `addPerson` such that if the `Person` to add contains a link to some `Internship`, then a link is also established from that `Internship` to this `Person, and vice versa for `addInternship`
        - Modify `removePerson` such that if the `Person` to remove contains a link to some `Internship`, then the link from that `Internship` to this `Person` is also removed, and vice versa for `removeInternship`

--- src/main/java/seedu/address/logic/commands/AddInternshipCommand.java
+++ src/main/java/seedu/address/logic/commands/AddInternshipCommand.java
@@ -6,8 +6,11 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP_ROLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP_STATUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERVIEW_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LINK_INDEX;
 
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.internship.CompanyName;
@@ -30,14 +33,14 @@ public class AddInternshipCommand extends Command {
             + PREFIX_COMPANY_NAME + "COMPANY_NAME "
             + PREFIX_INTERNSHIP_ROLE + "INTERNSHIP_ROLE "
             + PREFIX_INTERNSHIP_STATUS + "INTERNSHIP_STATUS "
-            + "[" + PREFIX_PERSON_ID + "PERSON_ID] "
-            + "[" + PREFIX_INTERVIEW_DATE + "INTERVIEW_DATE]\n"
+            + "[" + PREFIX_INTERVIEW_DATE + "INTERVIEW_DATE] "
+            + "[" + PREFIX_LINK_INDEX + "LINK_INDEX]\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_COMPANY_NAME + "Google "
             + PREFIX_INTERNSHIP_ROLE + "Frontend Engineer "
             + PREFIX_INTERNSHIP_STATUS + "PENDING "
-            + PREFIX_PERSON_ID + "1 "
-            + PREFIX_INTERVIEW_DATE + "2022-10-21 12:00";
+            + PREFIX_INTERVIEW_DATE + "2022-10-21 12:00 "
+            + PREFIX_LINK_INDEX + "1 ";
 
     public static final String MESSAGE_SUCCESS = "New internship added: %1$s";
     public static final String MESSAGE_DUPLICATE_INTERNSHIP = "This internship already exists in the address book";
@@ -47,6 +50,7 @@ public class AddInternshipCommand extends Command {
     private final InternshipStatus internshipStatus;
     private final PersonId contactPersonId;
     private final InterviewDate interviewDate;
+    private final Index linkIndex;
 
     /**
      * Creates an AddCommand to add the specified {@code Internship}
@@ -58,6 +62,7 @@ public class AddInternshipCommand extends Command {
         this.internshipStatus = internship.getInternshipStatus();
         this.contactPersonId = internship.getContactPersonId();
         this.interviewDate = internship.getInterviewDate();
+        this.linkIndex = null;
     }
 
     /**
@@ -69,42 +74,47 @@ public class AddInternshipCommand extends Command {
             CompanyName companyName,
             InternshipRole internshipRole,
             InternshipStatus internshipStatus,
-            PersonId contactPersonId,
-            InterviewDate interviewDate) {
+            InterviewDate interviewDate,
+            Index linkIndex) {
         requireAllNonNull(companyName, internshipRole, internshipStatus);
         this.companyName = companyName;
         this.internshipRole = internshipRole;
         this.internshipStatus = internshipStatus;
-        this.contactPersonId = contactPersonId;
+
+        // contactPersonId is to be gotten using the linkIndex in this case
+        this.contactPersonId = null;
+
         this.interviewDate = interviewDate;
+        this.linkIndex = linkIndex;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        Person contactPerson = model.findPersonById(contactPersonId);
+        // By default, use the contactPersonId field in the command
+        PersonId idToLink = contactPersonId;
 
-        Internship toAdd;
-        if (contactPerson == null) {
-            toAdd = new Internship(
-                    new InternshipId(model.getNextInternshipId()),
-                    companyName,
-                    internshipRole,
-                    internshipStatus,
-                    null,
-                    interviewDate
-            );
-        } else {
-            toAdd = new Internship(
-                    new InternshipId(model.getNextInternshipId()),
-                    companyName,
-                    internshipRole,
-                    internshipStatus,
-                    contactPersonId,
-                    interviewDate
-            );
+        List<Person> lastShownList = model.getFilteredPersonList();
+        // If a linkIndex is supplied to the command,
+        // attempt to find a contactPerson via the provided index in the filtered person list
+        if (linkIndex != null && linkIndex.getZeroBased() < lastShownList.size()) {
+            Person contactPerson = lastShownList.get(linkIndex.getZeroBased());
+
+            // The person to link to must not already be linked to an internship
+            if (contactPerson.getInternshipId() == null) {
+                idToLink = contactPerson.getPersonId();
+            }
         }
+
+        Internship toAdd = new Internship(
+                new InternshipId(model.getNextInternshipId()),
+                companyName,
+                internshipRole,
+                internshipStatus,
+                idToLink,
+                interviewDate
+        );
 
         if (model.hasInternship(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_INTERNSHIP);
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -15,9 +15,11 @@ public class CliSyntax {
 
     /* Prefix definitions (Internship) */
     public static final Prefix PREFIX_INTERNSHIP_ID = new Prefix("iid/");
-    public static final Prefix PREFIX_COMPANY_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_COMPANY_NAME = new Prefix("c/");
     public static final Prefix PREFIX_INTERNSHIP_ROLE = new Prefix("r/");
     public static final Prefix PREFIX_INTERNSHIP_STATUS = new Prefix("s/");
     public static final Prefix PREFIX_INTERVIEW_DATE = new Prefix("d/");
 
+    /* Prefix definitions (Common) */
+    public static final Prefix PREFIX_LINK_INDEX = new Prefix("l/");
 }
