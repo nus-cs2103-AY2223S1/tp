ff413c8e263dfe5b87c53bf49108ab0f5a577bcb
turretDive <apangjun@gmail.com>
2022-10-11 22:15:42 +0800

    Fix Bug on AddCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -147,7 +147,7 @@ public class AddCommandParserTest {
                 Name.MESSAGE_CONSTRAINTS);
 
         // non-empty preamble
-        assertParseFailure(parser,PREAMBLE_NON_EMPTY + OCCUPATION_DESC +  NAME_DESC_BOBgit a
+        assertParseFailure(parser,PREAMBLE_NON_EMPTY + OCCUPATION_DESC +  NAME_DESC_BOB
                         + PHONE_DESC_BOB + EMAIL_DESC_BOB
                 + ADDRESS_DESC_BOB + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
