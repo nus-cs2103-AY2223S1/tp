96c90e2c55bc269269e2a12ae945b84fcd7622cf
Chee Jer Hong <e0407708@u.nus.edu>
2022-10-12 01:11:26 +0800

    Add exception for duplicated Interview

---
+++ src/main/java/seedu/address/model/application/interview/exceptions/DuplicateInterviewException.java
@@ -0,0 +1,11 @@
+package seedu.address.model.application.interview.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Interviews (Interviews are considered duplicates
+ * if they have the same InterviewTime and InterviewDate).
+ */
+public class DuplicateInterviewException extends RuntimeException {
+    public DuplicateInterviewException() {
+        super("Operation would result in duplicate interviews");
+    }
+}
