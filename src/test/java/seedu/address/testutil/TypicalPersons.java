eac7761a06914c8f92b2cfeddfd9c171cce18ada
izzahaj <izzah.jalil02@gmail.com>
2022-10-11 23:00:49 +0800

    Update tasks field of typicalPersons

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -25,11 +25,12 @@ public class TypicalPersons {
 
     public static final Patient ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("94351253")
+            .withPhone("94351253").withTasks("Update health records")
             .withTags("friends").build();
     public static final Patient BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
+            .withTasks("Insert urinary catheter", "Check vitals")
             .withTags("owesMoney", "friends").build();
     public static final Patient CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
             .withEmail("heinz@example.com").withAddress("wall street").build();
