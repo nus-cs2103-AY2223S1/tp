5a011d7a27ffd263815523404d8381202274541c
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2022-10-12 04:21:35 +0800

    Refactor testing code

--- src/test/java/seedu/travelr/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/travelr/logic/commands/CommandTestUtil.java
@@ -1,15 +1,12 @@
 package seedu.travelr.logic.commands;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.Person;
-import seedu.address.testutil.EditPersonDescriptorBuilder;
+import seedu.travelr.commons.core.index.Index;
+import seedu.travelr.logic.commands.exceptions.CommandException;
+import seedu.travelr.model.AddressBook;
+import seedu.travelr.model.Model;
+import seedu.travelr.model.trip.NameContainsKeywordsPredicate;
+import seedu.travelr.model.trip.Trip;
+import seedu.travelr.testutil.EditTripDescriptorBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,59 +14,47 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.travelr.logic.parser.CliSyntax.PREFIX_DESC;
+import static seedu.travelr.logic.parser.CliSyntax.PREFIX_EVENT;
+import static seedu.travelr.logic.parser.CliSyntax.PREFIX_TITLE;
+import static seedu.travelr.testutil.Assert.assertThrows;
 
 /**
  * Contains helper methods for testing commands.
  */
 public class CommandTestUtil {
 
-    public static final String VALID_NAME_AMY = "Amy Bee";
-    public static final String VALID_NAME_BOB = "Bob Choo";
-    public static final String VALID_PHONE_AMY = "11111111";
-    public static final String VALID_PHONE_BOB = "22222222";
-    public static final String VALID_EMAIL_AMY = "amy@example.com";
-    public static final String VALID_EMAIL_BOB = "bob@example.com";
-    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
-    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_TAG_HUSBAND = "husband";
-    public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_TITLE_GERMANY = "Germany trip";
+    public static final String VALID_TITLE_ANTARCTICA = "Antarctic trip";
+    public static final String VALID_DESCRIPTION_GERMANY = "Block 312, Amy Street 1";
+    public static final String VALID_DESCRIPTION_ANTARCTICA = "Block 123, Bobby Street 3";
+    public static final String VALID_EVENT_EATING = "Eating";
+    public static final String VALID_EVENT_SIGHTSEEING = "Sightseeing";
 
-    public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
-    public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
-    public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
-    public static final String PHONE_DESC_BOB = " " + PREFIX_PHONE + VALID_PHONE_BOB;
-    public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
-    public static final String EMAIL_DESC_BOB = " " + PREFIX_EMAIL + VALID_EMAIL_BOB;
-    public static final String ADDRESS_DESC_AMY = " " + PREFIX_ADDRESS + VALID_ADDRESS_AMY;
-    public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
-    public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
-    public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
+    public static final String TITLE_DESC_GERMANY = " " + PREFIX_TITLE + VALID_TITLE_GERMANY;
+    public static final String TITLE_DESC_ANTARCTICA = " " + PREFIX_TITLE + VALID_TITLE_ANTARCTICA;
+    public static final String DESCRIPTION_DESC_GERMANY = " " + PREFIX_DESC + VALID_DESCRIPTION_GERMANY;
+    public static final String DESCRIPTION_DESC_ANTARCTICA = " " + PREFIX_DESC + VALID_DESCRIPTION_ANTARCTICA;
+    public static final String EVENT_DESC_EATING = " " + PREFIX_EVENT + VALID_EVENT_EATING;
+    public static final String EVENT_DESC_SIGHTSEEING = " " + PREFIX_EVENT + VALID_EVENT_SIGHTSEEING;
 
-    public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
-    public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
-    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
-    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
-    public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+    public static final String INVALID_TITLE_DESC = " " + PREFIX_TITLE + "James&"; // '&' not allowed in titles
+    public static final String INVALID_DESCRIPTION_DESC = " " + PREFIX_DESC; // empty string not allowed for description
+    public static final String INVALID_EVENT_DESC = " " + PREFIX_EVENT + "hubby*"; // '*' not allowed in events
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
 
-    public static final EditCommand.EditPersonDescriptor DESC_AMY;
-    public static final EditCommand.EditPersonDescriptor DESC_BOB;
+    public static final EditCommand.EditTripDescriptor DESC_GERMANY;
+    public static final EditCommand.EditTripDescriptor DESC_ANTARCTICA;
 
     static {
-        DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
-                .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withTags(VALID_TAG_FRIEND).build();
-        DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
-                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+        DESC_GERMANY = new EditTripDescriptorBuilder().withTitle(VALID_TITLE_GERMANY)
+                .withDescription(VALID_DESCRIPTION_GERMANY)
+                .withEvents(VALID_EVENT_EATING).build();
+        DESC_ANTARCTICA = new EditTripDescriptorBuilder().withTitle(VALID_TITLE_ANTARCTICA)
+                .withDescription(VALID_DESCRIPTION_ANTARCTICA)
+                .withEvents(VALID_EVENT_SIGHTSEEING, VALID_EVENT_EATING).build();
     }
 
     /**
@@ -102,30 +87,30 @@ public class CommandTestUtil {
      * Executes the given {@code command}, confirms that <br>
      * - a {@code CommandException} is thrown <br>
      * - the CommandException message matches {@code expectedMessage} <br>
-     * - the address book, filtered person list and selected person in {@code actualModel} remain unchanged
+     * - the address book, filtered trip list and selected trip in {@code actualModel} remain unchanged
      */
     public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage) {
         // we are unable to defensively copy the model for comparison later, so we can
         // only do so by copying its components.
         AddressBook expectedAddressBook = new AddressBook(actualModel.getAddressBook());
-        List<Person> expectedFilteredList = new ArrayList<>(actualModel.getFilteredPersonList());
+        List<Trip> expectedFilteredList = new ArrayList<>(actualModel.getFilteredTripList());
 
         assertThrows(CommandException.class, expectedMessage, () -> command.execute(actualModel));
         assertEquals(expectedAddressBook, actualModel.getAddressBook());
-        assertEquals(expectedFilteredList, actualModel.getFilteredPersonList());
+        assertEquals(expectedFilteredList, actualModel.getFilteredTripList());
     }
     /**
-     * Updates {@code model}'s filtered list to show only the person at the given {@code targetIndex} in the
+     * Updates {@code model}'s filtered list to show only the trip at the given {@code targetIndex} in the
      * {@code model}'s address book.
      */
-    public static void showPersonAtIndex(Model model, Index targetIndex) {
-        assertTrue(targetIndex.getZeroBased() < model.getFilteredPersonList().size());
+    public static void showTripAtIndex(Model model, Index targetIndex) {
+        assertTrue(targetIndex.getZeroBased() < model.getFilteredTripList().size());
 
-        Person person = model.getFilteredPersonList().get(targetIndex.getZeroBased());
-        final String[] splitName = person.getName().fullName.split("\\s+");
-        model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
+        Trip trip = model.getFilteredTripList().get(targetIndex.getZeroBased());
+        final String[] splitTitle = trip.getTitle().fullTitle.split("\\s+");
+        model.updateFilteredTripList(new NameContainsKeywordsPredicate(Arrays.asList(splitTitle[0])));
 
-        assertEquals(1, model.getFilteredPersonList().size());
+        assertEquals(1, model.getFilteredTripList().size());
     }
 
 }
--- src/test/java/seedu/travelr/testutil/TestUtil.java
+++ src/test/java/seedu/travelr/testutil/TestUtil.java
@@ -1,8 +1,8 @@
 package seedu.travelr.testutil;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.model.Model;
-import seedu.address.model.person.Person;
+import seedu.travelr.commons.core.index.Index;
+import seedu.travelr.model.Model;
+import seedu.travelr.model.trip.Trip;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -33,23 +33,23 @@ public class TestUtil {
     }
 
     /**
-     * Returns the middle index of the person in the {@code model}'s person list.
+     * Returns the middle index of the trip in the {@code model}'s trip list.
      */
     public static Index getMidIndex(Model model) {
-        return Index.fromOneBased(model.getFilteredPersonList().size() / 2);
+        return Index.fromOneBased(model.getFilteredTripList().size() / 2);
     }
 
     /**
-     * Returns the last index of the person in the {@code model}'s person list.
+     * Returns the last index of the trip in the {@code model}'s trip list.
      */
     public static Index getLastIndex(Model model) {
-        return Index.fromOneBased(model.getFilteredPersonList().size());
+        return Index.fromOneBased(model.getFilteredTripList().size());
     }
 
     /**
-     * Returns the person in the {@code model}'s person list at {@code index}.
+     * Returns the trip in the {@code model}'s trip list at {@code index}.
      */
-    public static Person getPerson(Model model, Index index) {
-        return model.getFilteredPersonList().get(index.getZeroBased());
+    public static Trip getTrip(Model model, Index index) {
+        return model.getFilteredTripList().get(index.getZeroBased());
     }
 }
