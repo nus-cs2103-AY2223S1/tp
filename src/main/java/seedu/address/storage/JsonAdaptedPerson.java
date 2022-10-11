a43a91d06cb22786b7d21fe4c4988b59b007e45a
Hongyi6328 <6328huang@gmail.com>
2022-10-11 22:32:19 +0800

    Update JsonAdaptedPerson.java

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -1,23 +1,23 @@
-//package seedu.address.storage;
-//
-//import java.util.ArrayList;
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Set;
-//import java.util.stream.Collectors;
-//
-//import com.fasterxml.jackson.annotation.JsonCreator;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//
-//import seedu.address.commons.exceptions.IllegalValueException;
-//import seedu.address.model.order.Order;
-//import seedu.address.model.person.*;
-//import seedu.address.model.tag.Tag;
-//
-///**
-// * Jackson-friendly version of {@link Person}.
-// */
-//class JsonAdaptedPerson {
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.order.Order;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Jackson-friendly version of {@link Person}.
+ */
+class JsonAdaptedPerson {
 //
 //    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Person's %s field is missing!";
 //
@@ -114,5 +114,4 @@
 //        final Set<Tag> modelTags = new HashSet<>(personTags);
 //        return new Person(modelPersonCategory, modelName, modelPhone, modelEmail, modelAddress, modelTags);
 //    }
-//
-//}
+}
