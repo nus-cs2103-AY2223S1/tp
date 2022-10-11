87167e61ea225d345ab5d75fbb821dadd1ab3b1b
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-10-11 22:15:09 +0800

    Add prefixes for tag command

--- src/main/java/seedu/foodrem/logic/parser/CliSyntax.java
+++ src/main/java/seedu/foodrem/logic/parser/CliSyntax.java
@@ -11,4 +11,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_ITEM_UNIT = new Prefix("u/");
     public static final Prefix PREFIX_ITEM_BOUGHT_DATE = new Prefix("bgt/");
     public static final Prefix PREFIX_ITEM_EXPIRY_DATE = new Prefix("exp/");
+    public static final Prefix PREFIX_TAG_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_ID = new Prefix("id/");
 }
