84321f8ea85078aba01a59783c5da24006ab4dc1
Amresh <amresh2000@gmail.com>
2022-10-11 16:14:45 +0800

    style: minor checkStyle issues

--- src/test/java/seedu/address/logic/commands/task/DeleteTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/task/DeleteTaskCommandTest.java
@@ -13,8 +13,8 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 import seedu.address.model.TaskPanel;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.task.Task;
 
 public class DeleteTaskCommandTest {
