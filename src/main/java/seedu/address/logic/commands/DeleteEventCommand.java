db74f7907649e70f0b1f6194245009641e643a3d
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-11 08:40:44 +0800

    Create base DeleteEventCommand

---
+++ src/main/java/seedu/address/logic/commands/DeleteEventCommand.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+public class DeleteEventCommand extends Command {
+    
+}
