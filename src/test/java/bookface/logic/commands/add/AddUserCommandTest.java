040f5ec72e7dc8513add7925fb39f1f8b2d6cccd
parth-io <67057645+parth-io@users.noreply.github.com>
2022-10-12 04:25:52 +0800

    Fix checkstyle

--- src/test/java/bookface/logic/commands/add/AddUserCommandTest.java
+++ src/test/java/bookface/logic/commands/add/AddUserCommandTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
