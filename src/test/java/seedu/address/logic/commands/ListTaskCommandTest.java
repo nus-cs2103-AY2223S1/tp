b515534ce3d7b83d1e7821c01a51f9e7fcf147a9
Rama <kmram0908@gmail.com>
2022-10-11 20:40:59 +0800

    Fix checkstyle fails

--- src/test/java/seedu/address/logic/commands/ListTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListTaskCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class ListTaskCommandTest {
