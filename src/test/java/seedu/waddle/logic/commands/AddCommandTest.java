f7e52e90ed3d6fa195d7afa775f0d2b3e9668e0f
seox123 <seanmenglaw2009@gmail.com>
2022-10-11 13:14:03 +0800

    Turn on codecov

---
+++ src/test/java/seedu/waddle/logic/commands/AddCommandTest.java
@@ -0,0 +1,14 @@
+package seedu.waddle.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AddCommandTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1);
+    }
+
+}
