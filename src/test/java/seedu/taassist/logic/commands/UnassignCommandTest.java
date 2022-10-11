9e1735d3b8b3f8c793d955e9b92e9981aa5c8382
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-10-11 15:21:05 +0800

    Add UnassignCommand tests

---
+++ src/test/java/seedu/taassist/logic/commands/UnassignCommandTest.java
@@ -0,0 +1,183 @@
+package seedu.taassist.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.taassist.testutil.Assert.assertThrows;
+import static seedu.taassist.testutil.TypicalStudents.ALICE;
+import static seedu.taassist.testutil.TypicalStudents.BOB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.taassist.commons.core.Messages;
+import seedu.taassist.commons.core.index.Index;
+import seedu.taassist.logic.commands.exceptions.CommandException;
+import seedu.taassist.model.ModelStub;
+import seedu.taassist.model.moduleclass.ModuleClass;
+import seedu.taassist.model.student.Student;
+import seedu.taassist.testutil.StudentBuilder;
+
+class UnassignCommandTest {
+
+    @Test
+    public void execute_noModuleClass_throwsCommandException() {
+        ModelStubWithNoModuleClasses modelStub = new ModelStubWithNoModuleClasses();
+        ModuleClass moduleClass = new ModuleClass("GEN1000");
+        List<Index> indices = new ArrayList<>();
+
+        UnassignCommand unassignCommand = new UnassignCommand(indices, moduleClass);
+        String expectedMessage = String.format(Messages.MESSAGE_MODULE_CLASS_DOES_NOT_EXIST,
+                modelStub.getModuleClassList());
+
+        assertThrows(CommandException.class, expectedMessage, () -> unassignCommand.execute(modelStub));
+    }
+
+    @Test
+    public void execute_indexOutOfRange_throwsCommandException() {
+        ModelStubWithOneStudent modelStub = new ModelStubWithOneStudent();
+        ModuleClass moduleClass = new ModuleClass("GEN1000");
+
+        List<Index> indices = new ArrayList<>();
+        indices.add(Index.fromOneBased(1));
+        indices.add(Index.fromOneBased(2));
+
+        UnassignCommand unassignCommand = new UnassignCommand(indices, moduleClass);
+        String expectedMessage = Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX;
+
+        assertThrows(CommandException.class, expectedMessage, () -> unassignCommand.execute(modelStub));
+    }
+
+    @Test
+    public void execute_unassignOneStudent_success() throws CommandException {
+        ModelStubWithOneStudent modelStub = new ModelStubWithOneStudent();
+        ModuleClass moduleClass = new ModuleClass("GEN1000");
+
+        List<Index> indices = new ArrayList<>();
+        indices.add(Index.fromOneBased(1));
+
+        UnassignCommand unassignCommand = new UnassignCommand(indices, moduleClass);
+        String expectedMessage = String.format(UnassignCommand.MESSAGE_SUCCESS, "index", indices, moduleClass);
+        Student expectedStudent = new StudentBuilder(ALICE).withModuleClasses().build();
+
+        CommandResult commandResult = unassignCommand.execute(modelStub);
+        assertEquals(expectedMessage, commandResult.getFeedbackToUser());
+        assertEquals(expectedStudent, modelStub.student);
+    }
+
+    @Test
+    public void execute_unassignMultipleStudents_success() throws CommandException {
+        ModelStubWithMultipleStudents modelStub = new ModelStubWithMultipleStudents();
+        ModuleClass moduleClass = new ModuleClass("GEN1000");
+
+        List<Index> indices = new ArrayList<>();
+        indices.add(Index.fromOneBased(1));
+        indices.add(Index.fromOneBased(2));
+
+        UnassignCommand unassignCommand = new UnassignCommand(indices, moduleClass);
+        String expectedMessage = String.format(UnassignCommand.MESSAGE_SUCCESS, "indices", indices, moduleClass);
+        Student expectedStudent1 = new StudentBuilder(ALICE).withModuleClasses().build();
+        Student expectedStudent2 = new StudentBuilder(BOB).withModuleClasses().build();
+
+        CommandResult commandResult = unassignCommand.execute(modelStub);
+        assertEquals(expectedMessage, commandResult.getFeedbackToUser());
+        assertEquals(expectedStudent1, modelStub.students.get(0));
+        assertEquals(expectedStudent2, modelStub.students.get(1));
+    }
+
+    /**
+     * A Model stub that pretends to have no module classes.
+     */
+    private class ModelStubWithNoModuleClasses extends ModelStub {
+
+        @Override
+        public boolean hasModuleClass(ModuleClass moduleClass) {
+            return false;
+        }
+
+        @Override
+        public ObservableList<ModuleClass> getModuleClassList() {
+            return FXCollections.observableArrayList();
+        }
+    }
+
+    /**
+     * A Model stub with one filtered student with an assigned class.
+     * Always says it has a module.
+     */
+    private class ModelStubWithOneStudent extends ModelStub {
+
+        private Student student = new StudentBuilder(ALICE).withModuleClasses("GEN1000").build();
+
+        @Override
+        public ObservableList<Student> getFilteredStudentList() {
+            return FXCollections.observableArrayList(student);
+        }
+
+        @Override
+        public void setStudent(Student target, Student editedStudent) {
+            student = editedStudent;
+        }
+
+        @Override
+        public boolean hasModuleClass(ModuleClass moduleClass) {
+            return true;
+        }
+    }
+
+    @Test
+    public void equals() {
+        ModuleClass moduleClass = new ModuleClass("GEN1000");
+
+        UnassignCommand unassignFirstCommand = new UnassignCommand(List.of(Index.fromOneBased(1)), moduleClass);
+        UnassignCommand unassignSecondCommand = new UnassignCommand(List.of(Index.fromOneBased(2)), moduleClass);
+
+        // same object -> returns true
+        assertEquals(unassignFirstCommand, unassignFirstCommand);
+
+        // same values -> returns true
+        UnassignCommand unassignFirstCommandCopy = new UnassignCommand(List.of(Index.fromOneBased(1)), moduleClass);
+        assertEquals(unassignFirstCommand, unassignFirstCommandCopy);
+
+        // different types -> returns false
+        assertNotEquals(unassignFirstCommand, 1);
+
+        // null -> returns false
+        assertNotEquals(unassignFirstCommand, null);
+
+        // different student -> returns false
+        assertNotEquals(unassignFirstCommand, unassignSecondCommand);
+    }
+    /**
+     * Model stub with multiple filtered students with same assigned class.
+     * Always says it has a module.
+     */
+    private class ModelStubWithMultipleStudents extends ModelStub {
+
+        private List<Student> students = new ArrayList<>();
+
+        public ModelStubWithMultipleStudents() {
+            // ALICE and BOB with no module classes
+            students.add(new StudentBuilder(ALICE).withModuleClasses("GEN1000").build());
+            students.add(new StudentBuilder(BOB).withModuleClasses("GEN1000").build());
+        }
+
+        @Override
+        public ObservableList<Student> getFilteredStudentList() {
+            return FXCollections.observableArrayList(students);
+        }
+
+        @Override
+        public void setStudent(Student target, Student editedStudent) {
+            students.set(students.indexOf(target), editedStudent);
+        }
+
+        @Override
+        public boolean hasModuleClass(ModuleClass moduleClass) {
+            return true;
+        }
+    }
+}
