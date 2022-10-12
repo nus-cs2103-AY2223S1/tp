2db7c0e4a6af99cd916ddfa442abddf7b6217638
Jessica Mary <jessicamary472@gmail.com>
2022-10-12 12:14:30 +0800

    Change PREFIX_EMAIL

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -8,7 +8,7 @@ public class CliSyntax {
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("name/");
     public static final Prefix PREFIX_PHONE = new Prefix("phone/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("email/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("address/");
     public static final Prefix PREFIX_GENDER = new Prefix("gender/");
     public static final Prefix PREFIX_UNIVERSITY = new Prefix("university/");
