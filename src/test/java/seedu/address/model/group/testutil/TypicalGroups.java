89d47804609799a553e9cd56eef61b012d1ee6d4
Tan Jin Waye <e0694446@u.nus.edu>
2022-10-13 01:23:05 +0800

    Add test cases for DeleteGroupCommandParser, DeleteGroupCommand classes

--- src/test/java/seedu/address/model/group/testutil/TypicalGroups.java
+++ src/test/java/seedu/address/model/group/testutil/TypicalGroups.java
@@ -8,6 +8,8 @@ import static seedu.address.model.person.testutil.TypicalPersons.ELLE;
 import static seedu.address.model.person.testutil.TypicalPersons.FIONA;
 import static seedu.address.model.person.testutil.TypicalPersons.GEORGE;
 import static seedu.address.model.person.testutil.TypicalPersons.getTypicalPersons;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUPNAME_TP;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUPNAME_IP;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,6 +17,7 @@ import java.util.List;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.group.Group;
+import seedu.address.model.group.GroupName;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.testutil.GroupBuilder;
 
@@ -22,6 +25,9 @@ import seedu.address.model.person.testutil.GroupBuilder;
  * A utility class containing a list of {@code Group} objects to be used in tests.
  */
 public class TypicalGroups {
+
+    public static final GroupName GROUPNAME_TP = new GroupName(VALID_GROUPNAME_TP);
+    public static final GroupName GROUPNAME_IP = new GroupName(VALID_GROUPNAME_IP);
     public static final Group TEAM_PROJECT =
             new GroupBuilder().withName("Team Project").withMembers(ALICE, BENSON, CARL).build();
 
