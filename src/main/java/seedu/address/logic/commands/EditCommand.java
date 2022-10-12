ae7f1af2f1a45e71a666752fcff983da980af5ad
Joseph Aedan Marcus <josephaedan@gmail.com>
2022-10-12 18:04:31 +0800

    Update EditCommand.java

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -158,7 +158,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags, status, note);
         }
 
         public void setName(Name name) {
