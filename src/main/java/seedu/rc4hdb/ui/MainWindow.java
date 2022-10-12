c1f11f8647af106ca0de2d1ac69ccbdb36716a94
nealetham <nealetham2000@gmail.com>
2022-10-12 23:21:54 +0800

    Fix checkstyle issues

--- src/main/java/seedu/rc4hdb/ui/MainWindow.java
+++ src/main/java/seedu/rc4hdb/ui/MainWindow.java
@@ -4,11 +4,9 @@ import java.util.logging.Logger;
 
 import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
