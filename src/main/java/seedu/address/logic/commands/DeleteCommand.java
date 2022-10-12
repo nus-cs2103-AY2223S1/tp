92879ca7b7ae686d4c0214d3c687b120c25d8aea
richavm14 <richavm14@gmail.com>
2022-10-13 00:59:15 +0800

    change teamname prefix

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -40,6 +40,7 @@ public class DeleteCommand extends Command {
         }
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,7 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_MEMBER_INDEX = new Prefix("m/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_TEAM_NAME = new Prefix("teamname/");
+    public static final Prefix PREFIX_TEAM_NAME = new Prefix("n/");
     public static final Prefix PREFIX_TEAM_INDEX = new Prefix("t/");
     public static final Prefix PREFIX_TASK_NAME = new Prefix("n/");
     public static final Prefix PREFIX_TASK_INDEX = new Prefix("task/");
