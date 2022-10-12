54b236dee02fb635ebfcba51771f9e6f8b8eca69
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-10-12 17:36:05 +0800

    Remove unnecessary CommandException import in FindCommand.java

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.core.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
