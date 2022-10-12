f3861802c76c57015bbcf93b08109d2ecae447b0
chunyu <46131429+gunbux@users.noreply.github.com>
2022-10-12 16:51:57 +0800

    Fixed test case (#83)

--- src/test/java/seedu/address/logic/commands/student/StudentAddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/student/StudentAddCommandTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.student;
 
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
