319fa86f15ff08a1875252aff4487ea03309fa61
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-10-12 21:33:41 +0800

    Update starting sample data to include some default sessions

--- src/main/java/seedu/taassist/model/util/SampleDataUtil.java
+++ src/main/java/seedu/taassist/model/util/SampleDataUtil.java
@@ -25,30 +25,31 @@ public class SampleDataUtil {
 
     private static final List<SessionData> DEFAULT_SESSION_DATA_LIST =
             List.of(new SessionData(new Session("Lab 1"), new StudentSessionData(100.0)));
-    private static final List<StudentModuleData> DEFAULT_STUDENT_MODULE_DATA =
-            List.of(new StudentModuleData(new ModuleClass("CS1231S"), DEFAULT_SESSION_DATA_LIST),
-                    new StudentModuleData(new ModuleClass("CS1101S"), DEFAULT_SESSION_DATA_LIST));
+    private static final List<StudentModuleData> DEFAULT_STUDENT_MODULE_DATA_CS1231S =
+            List.of(new StudentModuleData(new ModuleClass("CS1231S"), DEFAULT_SESSION_DATA_LIST));
+    private static final List<StudentModuleData> DEFAULT_STUDENT_MODULE_DATA_CS1101S =
+        List.of(new StudentModuleData(new ModuleClass("CS1101S"), DEFAULT_SESSION_DATA_LIST));
 
     public static Student[] getSampleStudents() {
         return new Student[] {
             new Student(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getModuleClassSet("CS1231S")),
+                getModuleClassSet("CS1231S"), DEFAULT_STUDENT_MODULE_DATA_CS1231S),
             new Student(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getModuleClassSet("CS1101S", "CS1231S")),
+                getModuleClassSet("CS1101S", "CS1231S"), DEFAULT_STUDENT_MODULE_DATA_CS1101S),
             new Student(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getModuleClassSet("neighbours")),
+                getModuleClassSet("CS1231S"), DEFAULT_STUDENT_MODULE_DATA_CS1231S),
             new Student(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getModuleClassSet("family")),
+                getModuleClassSet("CS1101S")),
             new Student(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                getModuleClassSet("classmates")),
+                getModuleClassSet("CS1231S")),
             new Student(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getModuleClassSet("CS1101S"))
+                getModuleClassSet("CS1101S"), DEFAULT_STUDENT_MODULE_DATA_CS1101S)
         };
     }
 
@@ -68,7 +69,7 @@ public class SampleDataUtil {
      */
     public static Set<ModuleClass> getModuleClassSet(String... strings) {
         return Arrays.stream(strings)
-                .map(ModuleClass::new)
+                .map(name -> new ModuleClass(name, List.of(new Session("Lab 1"))))
                 .collect(Collectors.toSet());
     }
 
