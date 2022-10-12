5cc248f0d970dd90c837a6c71bb296d573add135
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-10-12 22:31:22 +0800

    Partially add findCommandParser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -9,14 +9,27 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REASON;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.AddressContainsSequencePredicate;
+import seedu.address.model.person.Appointment;
+import seedu.address.model.person.DateTimeWithinRangePredicate;
+import seedu.address.model.person.EmailContainsSequencePredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.NameContainsSequencePredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonContainsTagsPredicate;
+import seedu.address.model.person.PhoneContainsSequencePredicate;
+import seedu.address.model.person.ReasonContainsSequencePredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -33,14 +46,21 @@ public class FindCommandParser implements Parser<FindCommand> {
                 PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG,
                 PREFIX_REASON, PREFIX_DATE_TIME_START, PREFIX_DATE_TIME_END);
 
-        Optional<String> name = argMultimap.getValue(PREFIX_NAME);
-        Optional<String> phone = argMultimap.getValue(PREFIX_PHONE);
-        Optional<String> email = argMultimap.getValue(PREFIX_EMAIL);
-        Optional<String> address = argMultimap.getValue(PREFIX_ADDRESS);
+
+        Optional<String> startDateTime = argMultimap.getValue(PREFIX_DATE_TIME_START).orElse("");
+        Optional<String> endDateTime = argMultimap.getValue(PREFIX_DATE_TIME_END).orElse("");
+
+        Optional<String name = argMultimap.getValue(PREFIX_NAME).orElse("");
+        Optional<String phone = argMultimap.getValue(PREFIX_PHONE).orElse("");
+        Optional<String email = argMultimap.getValue(PREFIX_EMAIL).orElse("");
+        Optional<String address = argMultimap.getValue(PREFIX_ADDRESS).orElse("");
         List<String> tagList = argMultimap.getAllValues(PREFIX_TAG);
-        Optional<String> reason = argMultimap.getValue(PREFIX_EMAIL);
-        Optional<String> startDate = argMultimap.getValue(PREFIX_DATE_TIME_START);
-        Optional<String> endDate = argMultimap.getValue(PREFIX_DATE_TIME_END);
+        String reason = argMultimap.getValue(PREFIX_EMAIL).orElse("");
+        LocalDateTime startDateTime;
+        LocalDateTime endDateTime;
+
+        Predicate<Person> personPredicate = generatePersonPredicate(name, phone, email, address, tagList);
+        Predicate<Appointment> appointmentPredicate = generateAppointmentPredicate(reason, startDateTime, endDateTime);
 
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
@@ -50,7 +70,30 @@ public class FindCommandParser implements Parser<FindCommand> {
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(personPredicate, appointmentPredicate);
     }
 
+    private Predicate<Person> generatePersonPredicate(Optional<String> name, Optional<String> phone,
+                                                      Optional<String> email, Optional<String> address,
+                                                      List<String> tagList) {
+        List<Predicate<Person>> personPredicates = new ArrayList<>();
+
+        name.ifPresent(s -> personPredicates.add(new NameContainsSequencePredicate(s)));
+        phone.ifPresent(s -> personPredicates.add(new PhoneContainsSequencePredicate(s)));
+        email.ifPresent(s -> personPredicates.add(new EmailContainsSequencePredicate(s)));
+        address.ifPresent(s -> personPredicates.add(new AddressContainsSequencePredicate(s)));
+        if (!tagList.isEmpty()) {
+            personPredicates.add(new PersonContainsTagsPredicate(tagList));
+        }
+
+        return personPredicates.stream().reduce(PREDICATE_SHOW_ALL_PERSONS, Predicate::and);
+    }
+
+    private Predicate<Appointment> generateAppointmentPredicate(Optional<String> reason,
+                                                                Optional<String> startDateTime,
+                                                                Optional<String> endDateTime) {
+        List<Predicate<Appointment>> appointmentPredicates = new ArrayList<>();
+        reason.ifPresent(s -> appointmentPredicates.add(new ReasonContainsSequencePredicate(s)));
+        startDateTime.ifPresent(s -> appointmentPredicates.add(new DateTimeWithinRangePredicate(startDateTime, endDateTime)));
+    }
 }
