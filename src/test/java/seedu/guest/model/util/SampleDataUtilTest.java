d56b94b138f348afb04b15c158d0f1e15a6ed813
jehousoh <e0694467@u.nus.edu>
2022-10-13 00:31:19 +0800

    Rename guests and add test for different guestbook

--- src/test/java/seedu/guest/model/util/SampleDataUtilTest.java
+++ src/test/java/seedu/guest/model/util/SampleDataUtilTest.java
@@ -50,23 +50,27 @@ public class SampleDataUtilTest {
     @Test
     public void getSampleGuestBook() {
         GuestBook exceptedGb = new GuestBook();
-        exceptedGb.addGuest(new Guest(new Name("Alex Yooh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new DateRange("13/09/22 - 15/09/22"),
-                new NumberOfGuests("1")));
-        exceptedGb.addGuest(new Guest(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new DateRange("01/01/23 - 07/01/23"),
-                new NumberOfGuests("4")));
-        exceptedGb.addGuest(new Guest(new Name("Charlotte Oliveiro"), new Phone("93210283"),
-                new Email("charlotte@example.com"), new DateRange("21/10/22 - 22/10/22"),
+        exceptedGb.addGuest(new Guest(new Name("James Oliver"), new Phone("98345344"), new Email("jamesoliver@example.com"),
+                new DateRange("15/09/22 - 19/09/22"),
                 new NumberOfGuests("2")));
-        exceptedGb.addGuest(new Guest(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new DateRange("08/04/23 - 22/04/23"),
+        exceptedGb.addGuest(new Guest(new Name("Kayla Smith"), new Phone("91823471"), new Email("kaylasmith@example.com"),
+                new DateRange("22/01/23 - 25/01/23"),
                 new NumberOfGuests("3")));
-        exceptedGb.addGuest(new Guest(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new DateRange("29/05/23 - 03/06/23"),
+        exceptedGb.addGuest(new Guest(new Name("Charlie Lim"), new Phone("88237126"),
+                new Email("charlie@example.com"), new DateRange("21/02/22 - 22/03/22"),
+                new NumberOfGuests("2")));
+        exceptedGb.addGuest(new Guest(new Name("Ryan Wong"), new Phone("82831111"), new Email("wongryan@example.com"),
+                new DateRange("28/04/23 - 29/04/23"),
+                new NumberOfGuests("2")));
+        exceptedGb.addGuest(new Guest(new Name("Ibrahim Irfan"), new Phone("84912293"), new Email("irfan@example.com"),
+                new DateRange("23/05/23 - 03/06/23"),
                 new NumberOfGuests("1")));
-        exceptedGb.addGuest(new Guest(new Name("Roy Balakrishnan"), new Phone("92624417"),
-                new Email("royb@example.com"), new DateRange("30/12/22 - 02/01/23"), new NumberOfGuests("4")));
+        exceptedGb.addGuest(new Guest(new Name("Dave Balakrishnan"), new Phone("92821374"), new Email("daveb@example.com"),
+                new DateRange("30/12/22 - 02/01/23"),
+                new NumberOfGuests("3")));
+
+        // different values -> return false
+        assertNotEquals(SampleDataUtil.getSampleGuestBook(), exceptedGb);
 
         // null -> return false
         assertNotEquals(SampleDataUtil.getSampleGuestBook(), null);
