9e5c72d1e320d5ad9b23621a7cebf8cbcede206c
Tan-Jia-Rong <tanjiarongtjr@gmail.com>
2022-10-12 19:51:31 +0800

    Add more test cases

--- src/test/java/seedu/address/logic/commands/GoToCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GoToCommandTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_MODULE_LISTED;
 import static seedu.address.commons.core.Messages.MESSAGE_NO_SUCH_MODULE;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalModules.CS2106;
 import static seedu.address.testutil.TypicalModules.MA2001;
@@ -24,17 +26,17 @@ import seedu.address.model.module.ModuleCodeMatchesKeywordPredicate;
  * Contains integration tests (interaction with the Model) for {@code GoToCommand}.
  */
 public class GoToCommandTest {
-    private static final String MODULECODESTR_CS2106 = CS2106.getModuleCodeAsUpperCaseString();
-    private static final String MODULECODESTR_MA2001 = MA2001.getModuleCodeAsUpperCaseString();
+    private static final String MODULE_CODE_STR_CS2106 = CS2106.getModuleCodeAsUpperCaseString();
+    private static final String MODULE_CODE_STR_MA2001 = MA2001.getModuleCodeAsUpperCaseString();
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void equals() {
         ModuleCodeMatchesKeywordPredicate firstPredicate =
-                new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_CS2106);
+                new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_CS2106);
         ModuleCodeMatchesKeywordPredicate secondPredicate =
-                new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_MA2001);
+                new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_MA2001);
 
         GoToCommand gotoFirstCommand = new GoToCommand(firstPredicate);
         GoToCommand gotoSecondCommand = new GoToCommand(secondPredicate);
@@ -67,9 +69,23 @@ public class GoToCommandTest {
     }
 
     @Test
-    public void execute_validKeywords_moduleFound() {
+    public void execute_matchingKeywords_moduleFound() {
         String expectedMessage = MESSAGE_MODULE_LISTED;
-        ModuleCodeMatchesKeywordPredicate predicate = new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_CS2106);
+
+        // Matching keyword test
+        ModuleCodeMatchesKeywordPredicate predicate = new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_CS2106);
+        GoToCommand command = new GoToCommand(predicate);
+        expectedModel.updateFilteredModuleList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(CS2106), model.getFilteredModuleList());
+    }
+
+    @Test
+    public void execute_caseInsensitiveKeywords_moduleFound() {
+        String expectedMessage = MESSAGE_MODULE_LISTED;
+
+        // Case-insensitive keyword test
+        ModuleCodeMatchesKeywordPredicate predicate = new ModuleCodeMatchesKeywordPredicate("cS2106");
         GoToCommand command = new GoToCommand(predicate);
         expectedModel.updateFilteredModuleList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
--- src/test/java/seedu/address/model/module/ModuleCodeMatchesKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/module/ModuleCodeMatchesKeywordsPredicateTest.java
@@ -10,20 +10,22 @@ import org.junit.jupiter.api.Test;
 import seedu.address.testutil.ModuleBuilder;
 
 public class ModuleCodeMatchesKeywordsPredicateTest {
-    private static final String MODULECODESTR_CS2106 = CS2106.getModuleCodeAsUpperCaseString();
-    private static final String MODULECODESTR_MA2001 = MA2001.getModuleCodeAsUpperCaseString();
+    private static final String MODULE_CODE_STR_CS2106 = CS2106.getModuleCodeAsUpperCaseString();
+    private static final String MODULE_CODE_STR_MA2001 = MA2001.getModuleCodeAsUpperCaseString();
 
     @Test
     public void equals() {
-        ModuleCodeMatchesKeywordPredicate firstPredicate = new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_CS2106);
-        ModuleCodeMatchesKeywordPredicate secondPredicate = new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_MA2001);
+        ModuleCodeMatchesKeywordPredicate firstPredicate =
+                new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_CS2106);
+        ModuleCodeMatchesKeywordPredicate secondPredicate =
+                new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_MA2001);
 
         // same object -> returns true
         assertTrue(firstPredicate.equals(firstPredicate));
 
         // same values -> returns true
         ModuleCodeMatchesKeywordPredicate firstPredicateCopy =
-                new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_CS2106);
+                new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_CS2106);
         assertTrue(firstPredicate.equals(firstPredicateCopy));
 
         // different types -> returns false
@@ -39,22 +41,22 @@ public class ModuleCodeMatchesKeywordsPredicateTest {
     @Test
     public void test_moduleCodeMatchesKeywords_returnsTrue() {
         // One keyword
-        ModuleCodeMatchesKeywordPredicate predicate = new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_CS2106);
-        assertTrue(predicate.test(new ModuleBuilder().withModuleCode(MODULECODESTR_CS2106).build()));
+        ModuleCodeMatchesKeywordPredicate predicate = new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_CS2106);
+        assertTrue(predicate.test(new ModuleBuilder().withModuleCode(MODULE_CODE_STR_CS2106).build()));
 
         // Mixed-case keywords
         predicate = new ModuleCodeMatchesKeywordPredicate("cS2106");
-        assertTrue(predicate.test(new ModuleBuilder().withModuleCode(MODULECODESTR_CS2106).build()));
+        assertTrue(predicate.test(new ModuleBuilder().withModuleCode(MODULE_CODE_STR_CS2106).build()));
     }
 
     @Test
     public void test_moduleCodeDoesNotMatchKeywords_returnsFalse() {
         // Zero keywords
         ModuleCodeMatchesKeywordPredicate predicate = new ModuleCodeMatchesKeywordPredicate(" ");
-        assertFalse(predicate.test(new ModuleBuilder().withModuleCode(MODULECODESTR_CS2106).build()));
+        assertFalse(predicate.test(new ModuleBuilder().withModuleCode(MODULE_CODE_STR_CS2106).build()));
 
         // Non-matching keyword
-        predicate = new ModuleCodeMatchesKeywordPredicate(MODULECODESTR_MA2001);
-        assertFalse(predicate.test(new ModuleBuilder().withModuleCode(MODULECODESTR_CS2106).build()));
+        predicate = new ModuleCodeMatchesKeywordPredicate(MODULE_CODE_STR_MA2001);
+        assertFalse(predicate.test(new ModuleBuilder().withModuleCode(MODULE_CODE_STR_CS2106).build()));
     }
 }
