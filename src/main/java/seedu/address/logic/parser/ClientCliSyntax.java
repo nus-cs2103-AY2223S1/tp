47ded046d6aa1c1ffd350dc4bc0c4e5ff5bf6122
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-12 16:09:40 +0800

    Add ClientCliSyntax

---
+++ src/main/java/seedu/address/logic/parser/ClientCliSyntax.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.parser;
+
+public class ClientCliSyntax {
+    /* Prefix definitions */
+    public static final Prefix PREFIX_CLIENT_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_CLIENT_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_CLIENT_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_CLIENT_ID = new Prefix("i/");
+
+}
