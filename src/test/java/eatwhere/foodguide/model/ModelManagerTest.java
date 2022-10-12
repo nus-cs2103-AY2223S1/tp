067bee77dfea0528692c67d3324f40d6643c9e76
Prearo <kohshanghui@gmail.com>
2022-10-12 20:18:27 +0800

    Rename test food guide file path

--- src/test/java/eatwhere/foodguide/model/ModelManagerTest.java
+++ src/test/java/eatwhere/foodguide/model/ModelManagerTest.java
@@ -36,14 +36,14 @@ public class ModelManagerTest {
     @Test
     public void setUserPrefs_validUserPrefs_copiesUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
-        userPrefs.setFoodGuideFilePath(Paths.get("address/book/file/path"));
+        userPrefs.setFoodGuideFilePath(Paths.get("food/guide/file/path"));
         userPrefs.setGuiSettings(new GuiSettings(1, 2, 3, 4));
         modelManager.setUserPrefs(userPrefs);
         assertEquals(userPrefs, modelManager.getUserPrefs());
 
         // Modifying userPrefs should not modify modelManager's userPrefs
         UserPrefs oldUserPrefs = new UserPrefs(userPrefs);
-        userPrefs.setFoodGuideFilePath(Paths.get("new/address/book/file/path"));
+        userPrefs.setFoodGuideFilePath(Paths.get("new/food/guide/file/path"));
         assertEquals(oldUserPrefs, modelManager.getUserPrefs());
     }
 
