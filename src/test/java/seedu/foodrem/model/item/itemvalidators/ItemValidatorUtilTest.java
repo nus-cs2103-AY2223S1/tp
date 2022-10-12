7d2786a4c9483ba4e2cabd5b4a20ecdb49cff9b0
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-13 00:27:31 +0800

    Add ItemValidatorUtilTest utility class

---
+++ src/test/java/seedu/foodrem/model/item/itemvalidators/ItemValidatorUtilTest.java
@@ -0,0 +1,21 @@
+package seedu.foodrem.model.item.itemvalidators;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Supplier;
+
+public class ItemValidatorUtilTest {
+
+    /**
+     * Asserts that the validation of {@code userInput} by {@code validator} is unsuccessful and the error message
+     * equals to {@code expectedMessage}.
+     */
+    public static void assertValidateFailure(Supplier<Void> supplier, String errorMessage) {
+        try {
+            supplier.get();
+        } catch (Exception e) {
+            assertEquals(errorMessage, e.getMessage());
+        }
+    }
+}
