3eb8b0b54136f0d0d31446055f5e4c8d0a70ab0f
Jason Christopher <jason.christopher@u.nus.edu>
2022-10-12 23:33:05 +0800

    Create AddFieldCommand and the respective parser

---
+++ src/main/java/seedu/address/logic/commands/AddFieldCommand.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.Prefix;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FIELD;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Adds a person to the address book.
+ */
+public class AddFieldCommand extends Command {
+
+    public static final String COMMAND_WORD = "field";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a custom field to the address book. "
+            + "Parameters: CUSTOM_PREFIX FIELD";
+
+    public static final String MESSAGE_SUCCESS = "New field added: %1$s";
+
+    private final Prefix prefix;
+    private final String fieldName;
+
+    public AddFieldCommand(Prefix prefix, String fieldName) {
+        requireAllNonNull(prefix, fieldName);
+        this.prefix = prefix;
+        this.fieldName = fieldName;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        try {
+            PREFIX_FIELD.addPrefix(prefix, fieldName);
+        } catch (ParseException err) {
+            throw new CommandException(err.getMessage());
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS, fieldName));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddFieldCommand // instanceof handles nulls
+                && prefix.equals(((AddFieldCommand) other).prefix)
+                && fieldName.equals(((AddFieldCommand) other).fieldName));
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/AddFieldCommandParser.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.AddFieldCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+
+/**
+ * Parses input arguments and creates a new {@code RemarkCommand} object
+ */
+public class AddFieldCommandParser implements Parser<AddFieldCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
+     * and returns a {@code RemarkCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddFieldCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddFieldCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String prefixValue = nameKeywords[0];
+        String prefixName = nameKeywords[1];
+        requireNonNull(prefixValue);
+        requireNonNull(prefixName);
+        if (!prefixValue.endsWith("/") || prefixValue.startsWith("/")) {
+            throw new ParseException("Invalid Prefix Format");
+        }
+        Prefix prefix = new Prefix(prefixValue);
+        return new AddFieldCommand(prefix, prefixName);
+    }
+}
