c2c1256f8a4c7c27bd17b0fde49bd6636976f58a
zylee348 <e0725436@u.nus.edu>
2022-10-12 16:27:48 +0800

    Add testcase

--- src/test/java/seedu/address/logic/commands/AddGroupMemberCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddGroupMemberCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.model.person.testutil.Assert.assertThrows;
+import static seedu.address.model.person.testutil.TypicalPersons.CARL;
 import static seedu.address.model.person.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,12 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.group.Group;
+import seedu.address.model.group.GroupName;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.testutil.PersonBuilder;
+
+import java.util.HashSet;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -25,5 +33,18 @@ public class AddGroupMemberCommandTest {
     @Test
     public void constructor_nullGroup_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new AddGroupMemberCommand(null, "nameHere"));
+    }
+
+    @Test
+    public void execute_addGroupMemberCommand_failure() {
+        Person personToAddGroup = model.getPersonWithName(CARL.getName()).get(0);
+        Person editedPerson = new PersonBuilder(personToAddGroup).build();
+        AddGroupMemberCommand addGroupMemberCommand =
+                new AddGroupMemberCommand("Alpha", CARL.getName().fullName);
+
+        String expectedMessage = String.format(AddGroupMemberCommand.MESSAGE_INVALID_GROUP);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        model.setPerson(personToAddGroup, editedPerson);
+        assertCommandFailure(addGroupMemberCommand, expectedModel, expectedMessage);
     }
 }
