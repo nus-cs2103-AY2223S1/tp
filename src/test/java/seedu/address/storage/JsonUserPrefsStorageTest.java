c8760ff9271291da046b825138356ba03c0ada8a
ruihan00 <83448744+ruihan00@users.noreply.github.com>
2022-10-12 18:22:21 +0800

    Update src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
    Co-authored-by: Emily Ong <emilyohq11@gmail.com>

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -72,7 +72,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setPennyWiseFilePath(Paths.get("data/addressbook.json"));
+        userPrefs.setPennyWiseFilePath(Paths.get("data/pennywise.json"));
         return userPrefs;
     }
 
