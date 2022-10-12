468719e4161ce9d1fcc5e686bb827b4c8bb91913
JustinS00 <justinsiaw00@gmail.com>
2022-10-12 12:17:40 +0800

    Fix styling issues

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -2,14 +2,13 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
-import java.util.Arrays;
+// import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+// import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+// import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -19,7 +18,7 @@ public class FindCommandParserTest {
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
-
+    /*
 //    @Test
 //    public void parse_validArgs_returnsFindCommand() {
 //        // no leading and trailing whitespaces
@@ -30,5 +29,6 @@ public class FindCommandParserTest {
 //        // multiple whitespaces between keywords
 //        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
 //    }
+     */
 
 }
