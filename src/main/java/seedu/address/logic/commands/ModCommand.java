d0901011a09c57bfec6d88d825053b4d0c120f11
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2022-10-12 12:56:29 +0800

    Edit message usage

--- src/main/java/seedu/address/logic/commands/ModCommand.java
+++ src/main/java/seedu/address/logic/commands/ModCommand.java
@@ -9,7 +9,8 @@ public abstract class ModCommand extends Command {
     public static final String COMMAND_WORD = "mod";
     // TODO: Add the other mod commands
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Modifies the modules related to a person. "
-            + "\n add INDEX MODULE [MORE_MODULES]... : Adds the entered mods to the person at the index";
+            + "\nParameters: INDEX (must be a positive integer) MODULE [MORE_MODULES]..."
+            + "\nadd INDEX MODULE [MORE_MODULES]... : Adds the entered mods to the person at the index";
     public static final String MESSAGE_MODS_EMPTY = "Mods cannot be empty!";
     public static final String MESSAGE_INDEX_EMPTY = "Index cannot be empty!";
 }
