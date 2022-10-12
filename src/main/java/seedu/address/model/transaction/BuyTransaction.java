22c6297f1e8b2216f940abb8b4cf5a5cabd45c3f
Elton Chua <notle1706@gmail.com>
2022-10-12 21:30:48 +0800

    Fix test bugs

--- src/main/java/seedu/address/model/transaction/BuyTransaction.java
+++ src/main/java/seedu/address/model/transaction/BuyTransaction.java
@@ -1,5 +1,7 @@
 package seedu.address.model.transaction;
 
+import seedu.address.model.poc.Poc;
+
 /**
  * Store the buy transaction process.
  */
@@ -23,6 +25,26 @@ public class BuyTransaction extends Transaction {
     @Override
     public String toString() {
         return "You bought " + quantity + " quantity of "
-                + goods + " at $" + price + " each";
+                + goods + " at " + price + " each";
+    }
+
+    /**
+     * Returns true if both transactions have the same identity and data fields.
+     * This defines a stronger notion of equality between two transactions.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof BuyTransaction)) {
+            return false;
+        }
+
+        BuyTransaction otherTransaction = (BuyTransaction) other;
+        return otherTransaction.getGoods().equals(getGoods())
+                && otherTransaction.getQuantity().equals(getQuantity())
+                && otherTransaction.getPrice().equals(getPrice());
     }
 }
--- src/test/java/seedu/address/model/transaction/BuyTransactionTest.java
+++ src/test/java/seedu/address/model/transaction/BuyTransactionTest.java
@@ -31,6 +31,7 @@ class BuyTransactionTest {
         Quantity quantity = new Quantity("10");
 
         Transaction transaction = new BuyTransaction(goods, price, quantity);
+        System.out.println(transaction.toString());
         assertTrue(transaction.toString().equals("You bought 10 quantity of Apple at $0.8 each"));
     }
 }
