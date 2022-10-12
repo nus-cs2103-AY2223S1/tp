29a707596aeb1fae6e9ccb59322d9be1407ecea3
ZQ <e0725180@u.nus.edu>
2022-10-12 19:21:08 +0800

    Adds dummy commands to test GUI changes

---
+++ src/main/java/seedu/address/logic/commands/teams/ChangeTeamCommand.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands.teams;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.group.Group;
+import seedu.address.model.item.AbstractContainerItem;
+
+public class ChangeTeamCommand extends Command {
+    public static final String COMMAND_WORD = "cd";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Changes the current context to the index specified\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1\n"
+            + "To go back to the previous context, use\n"
+            + COMMAND_WORD + " ..";
+
+    public static final String SWITCH_SUCCESS = " switched %s%n";
+
+    private final Index targetIndex;
+    
+    public ChangeTeamCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        AbstractContainerItem toSwitch;
+        if (targetIndex == null) {
+            if (model.getContextContainer()!=null) {
+                toSwitch = model.getContextContainer().getParent();
+            } else {
+                return new CommandResult("No more parent!");
+            }
+        } else {
+            List<Group> lastShownList = model.getFilteredTeamList();
+
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+            toSwitch = lastShownList.get(targetIndex.getZeroBased());
+        }
+        model.updateContextContainer(toSwitch);
+        return new CommandResult(String.format(SWITCH_SUCCESS, toSwitch));
+    }
+}
--- src/main/java/seedu/address/logic/commands/teams/TeamCommand.java
+++ src/main/java/seedu/address/logic/commands/teams/TeamCommand.java
@@ -1,7 +1,36 @@
 package seedu.address.logic.commands.teams;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.group.Group;
+
 /**
  * Add a team to the address book.
  */
-public class TeamCommand {
+public class TeamCommand extends Command {
+    public static final String COMMAND_WORD = "mkdir";
+    public static final String MESSAGE_SUCCESS = "New team added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This team already exists in the address book";
+
+    Group toAdd;
+    
+    public TeamCommand(Group toAdd) {
+        this.toAdd = toAdd;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        toAdd.setParent(model.getContextContainer());
+        if (model.hasTeam(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.addTeam(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
 }
