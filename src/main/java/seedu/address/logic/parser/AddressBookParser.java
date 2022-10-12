b713dcb82ccb6d7dc11485af4479f0f1e0c24fd2
Hongyi6328 <6328huang@gmail.com>
2022-10-12 14:15:45 +0800

    Resolve merge conflicts

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -63,9 +63,6 @@ public class AddressBookParser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
-        case FilterCommand.COMMAND_WORD:
-            return new FilterCommandParser().parse(arguments);
-
         case FilterLocCommand.COMMAND_WORD:
             return new FilterLocCommandParser().parse(arguments);
 
