39da5ae7f12440371ca1d8db81e478c570c1a3ef
Yue Hern <yuehernkang@hotmail.com>
2022-10-12 16:54:19 +0800

    Fix Property Command Parser test errors

--- src/test/java/seedu/condonery/logic/parser/AddPropertyCommandParserTest.java
+++ src/test/java/seedu/condonery/logic/parser/AddPropertyCommandParserTest.java
@@ -29,15 +29,16 @@ import static seedu.condonery.testutil.TypicalProperties.PINNACLE;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.condonery.logic.commands.AddCommand;
+import seedu.condonery.logic.commands.property.AddPropertyCommand;
+import seedu.condonery.logic.parser.property.AddPropertyCommandParser;
 import seedu.condonery.model.fields.Address;
 import seedu.condonery.model.fields.Name;
 import seedu.condonery.model.property.Property;
 import seedu.condonery.model.tag.Tag;
 import seedu.condonery.testutil.PropertyBuilder;
 
-public class AddCommandParserTest {
-    private final AddCommandParser parser = new AddCommandParser();
+public class AddPropertyCommandParserTest {
+    private final AddPropertyCommandParser parser = new AddPropertyCommandParser();
 
     @Test
     public void parse_allFieldsPresent_success() {
@@ -45,29 +46,29 @@ public class AddCommandParserTest {
 
         // whitespace only preamble
         assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProperty));
+            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddPropertyCommand(expectedProperty));
 
         // multiple names - last name accepted
         assertParseSuccess(parser, NAME_DESC_AMY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProperty));
+            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddPropertyCommand(expectedProperty));
 
         // multiple phones - last phone accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_AMY + PHONE_DESC_BOB + EMAIL_DESC_BOB
-            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProperty));
+            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddPropertyCommand(expectedProperty));
 
         // multiple emails - last email accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_AMY + EMAIL_DESC_BOB
-            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProperty));
+            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddPropertyCommand(expectedProperty));
 
         // multiple addresses - last address accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_AMY
-            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProperty));
+            + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddPropertyCommand(expectedProperty));
 
         // multiple tags - all accepted
         Property expectedPersonMultipleTags =
                 new PropertyBuilder(PINNACLE).withTags(VALID_TAG_FRIEND, VALID_TAG_HUSBAND).build();
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-            + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, new AddCommand(expectedPersonMultipleTags));
+            + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, new AddPropertyCommand(expectedPersonMultipleTags));
     }
 
     @Test
@@ -75,12 +76,12 @@ public class AddCommandParserTest {
         // zero tags
         Property expectedProperty = new PropertyBuilder(OASIS).withTags().build();
         assertParseSuccess(parser, NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY,
-            new AddCommand(expectedProperty));
+            new AddPropertyCommand(expectedProperty));
     }
 
     @Test
     public void parse_compulsoryFieldMissing_failure() {
-        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPropertyCommand.MESSAGE_USAGE);
 
         // missing name prefix
         assertParseFailure(parser, VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
@@ -124,6 +125,6 @@ public class AddCommandParserTest {
         // non-empty preamble
         assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
                 + ADDRESS_DESC_BOB + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
-            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPropertyCommand.MESSAGE_USAGE));
     }
 }
