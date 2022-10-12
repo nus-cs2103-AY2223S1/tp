ea790f10f974ddd855ff0fcebc7f01b48e3de375
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-10-12 16:33:41 +0800

    Comment out new classes causing build errors

--- src/main/java/nus/climods/logic/parser/AddCommandParser.java
+++ src/main/java/nus/climods/logic/parser/AddCommandParser.java
@@ -1,52 +1,52 @@
-package nus.climods.logic.parser;
-
-import java.util.stream.Stream;
-
-import nus.climods.logic.commands.AddCommand;
-import nus.climods.logic.parser.exceptions.ParseException;
-import nus.climods.logic.parser.parameters.ModuleCodeParameter;
-import nus.climods.model.module.UserModule;
-
-
-/**
- * Parses input arguments and creates a new AddCommand object
- */
-public class AddCommandParser implements Parser<AddCommand> {
-
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given {@code
-     * ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
-    /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand and returns an AddCommand object
-     * for execution.
-     *
-     * @throws ParseException if the user input does not conform the expected format
-     */
-    public AddCommand parse(String args) throws ParseException {
-        /*
-        ArgumentMultimap argMultimap =
-            ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_CODE, CliSyntax.PREFIX_TUTORIAL, CliSyntax.PREFIX_TAG);
-
-        if (!arePrefixesPresent(argMultimap, CliSyntax.PREFIX_CODE, CliSyntax.PREFIX_TUTORIAL)
-            || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-        }
-        */
-
-        ModuleCodeParameter mcp = new ModuleCodeParameter(args);
-
-        String mc = mcp.getArgValue();
-
-        UserModule module = new UserModule(mc);
-
-        return new AddCommand(module);
-
-    }
-
-}
+//package nus.climods.logic.parser;
+//
+//import java.util.stream.Stream;
+//
+//import nus.climods.logic.commands.AddCommand;
+//import nus.climods.logic.parser.exceptions.ParseException;
+//import nus.climods.logic.parser.parameters.ModuleCodeParameter;
+//import nus.climods.model.module.UserModule;
+//
+//
+///**
+// * Parses input arguments and creates a new AddCommand object
+// */
+//public class AddCommandParser implements Parser<AddCommand> {
+//
+//
+//    /**
+//     * Returns true if none of the prefixes contains empty {@code Optional} values in the given {@code
+//     * ArgumentMultimap}.
+//     */
+//    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+//        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+//    }
+//
+//    /**
+//     * Parses the given {@code String} of arguments in the context of the AddCommand and returns an AddCommand object
+//     * for execution.
+//     *
+//     * @throws ParseException if the user input does not conform the expected format
+//     */
+//    public AddCommand parse(String args) throws ParseException {
+//        /*
+//        ArgumentMultimap argMultimap =
+//            ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_CODE, CliSyntax.PREFIX_TUTORIAL, CliSyntax.PREFIX_TAG);
+//
+//        if (!arePrefixesPresent(argMultimap, CliSyntax.PREFIX_CODE, CliSyntax.PREFIX_TUTORIAL)
+//            || !argMultimap.getPreamble().isEmpty()) {
+//            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+//        }
+//        */
+//
+//        ModuleCodeParameter mcp = new ModuleCodeParameter(args);
+//
+//        String mc = mcp.getArgValue();
+//
+//        UserModule module = new UserModule(mc);
+//
+//        return new AddCommand(module);
+//
+//    }
+//
+//}
