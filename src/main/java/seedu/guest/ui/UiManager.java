a9b5a64f161248af7cabebd80c34f62960291cdd
jehousoh <e0694467@u.nus.edu>
2022-10-12 19:55:57 +0800

    Change icon size

--- src/main/java/seedu/guest/ui/UiManager.java
+++ src/main/java/seedu/guest/ui/UiManager.java
@@ -20,7 +20,7 @@ public class UiManager implements Ui {
     public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
 
     private static final Logger logger = LogsCenter.getLogger(UiManager.class);
-    private static final String ICON_APPLICATION = "/images/guest_book_32.png";
+    private static final String ICON_APPLICATION = "/images/guest_book.png";
 
     private Logic logic;
     private MainWindow mainWindow;
