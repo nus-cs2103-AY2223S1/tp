6a84320809e2616dfff8f12f1c4b35de9534427e
zylee348 <e0725436@u.nus.edu>
2022-10-12 17:05:09 +0800

    Add testcase

--- src/test/java/seedu/address/logic/commands/AddGroupMemberCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddGroupMemberCommandTest.java
@@ -1,18 +1,27 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUPNAME_TP;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.model.person.testutil.Assert.assertThrows;
-import static seedu.address.model.person.testutil.TypicalPersons.CARL;
-import static seedu.address.model.person.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.model.person.testutil.TypicalPersons.*;
 
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.parser.AddGroupCommandParser;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.group.Group;
+import seedu.address.model.group.GroupName;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.testutil.GroupBuilder;
 import seedu.address.model.person.testutil.PersonBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -32,13 +41,45 @@ public class AddGroupMemberCommandTest {
     }
 
     @Test
-    public void execute_addGroupMemberCommand_failure() {
+    public void execute_addGroupMemberCommand_failure_Invalid_Group() {
         Person personToAddGroup = model.getPersonWithName(CARL.getName()).get(0);
         Person editedPerson = new PersonBuilder(personToAddGroup).build();
         AddGroupMemberCommand addGroupMemberCommand =
                 new AddGroupMemberCommand("Alpha", CARL.getName().fullName);
 
         String expectedMessage = String.format(AddGroupMemberCommand.MESSAGE_INVALID_GROUP);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        model.setPerson(personToAddGroup, editedPerson);
+        assertCommandFailure(addGroupMemberCommand, expectedModel, expectedMessage);
+    }
+
+    @Test
+    public void execute_addGroupMemberCommand_failure_Invalid_Person() {
+        Person personToAddGroup = model.getPersonWithName(CARL.getName()).get(0);
+        Person editedPerson = new PersonBuilder(personToAddGroup).build();
+        AddGroupMemberCommand addGroupMemberCommand =
+                new AddGroupMemberCommand("Alpha", "Bob");
+
+        String expectedMessage = String.format(AddGroupMemberCommand.MESSAGE_INVALID_PERSON);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        model.setPerson(personToAddGroup, editedPerson);
+        assertCommandFailure(addGroupMemberCommand, expectedModel, expectedMessage);
+    }
+
+    @Test
+    public void execute_addGroupMemberCommand_failure_Duplicate_Person() {
+        Person personToAddGroup = model.getPersonWithName(CARL.getName()).get(0);
+
+        Set<Person> members = new HashSet<>();
+        members.add(personToAddGroup);
+        Group g = new GroupBuilder().withName(VALID_GROUPNAME_TP).withMembers(DANIEL, ELLE, FIONA).build();
+
+        Person editedPerson = new PersonBuilder(personToAddGroup).build();
+
+        AddGroupMemberCommand addGroupMemberCommand =
+                new AddGroupMemberCommand(g.getName().groupName, CARL.getName().fullName);
+
+        String expectedMessage = String.format(AddGroupMemberCommand.MESSAGE_DUPLICATE_PERSON_IN_GROUP);
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         model.setPerson(personToAddGroup, editedPerson);
         assertCommandFailure(addGroupMemberCommand, expectedModel, expectedMessage);
