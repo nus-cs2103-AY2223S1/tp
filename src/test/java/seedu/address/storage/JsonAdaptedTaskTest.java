57fd0b88e5e8ec8acdcca2e7fce54ea3e1397e0d
DevanshShah1309 <devanshshah1309@gmail.com>
2022-10-12 10:07:09 +0800

    Add test for JsonAdaptedTask

--- src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
@@ -1,16 +1,23 @@
 package seedu.address.storage;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.team.Task;
+import seedu.address.testutil.TypicalTasks;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonAdaptedTaskTest {
 
     @Test
-    void getTaskName() {
+    void toModelType_validTask_returnsTask() throws IllegalValueException {
+        JsonAdaptedTask task = new JsonAdaptedTask(TypicalTasks.TASK_1);
+        assertEquals(TypicalTasks.TASK_1, task.toModelType());
     }
 
     @Test
-    void toModelType() {
+    void toModelType_invalidTask_throwsException() {
+        JsonAdaptedTask task = new JsonAdaptedTask(" ");
+        assertThrows(IllegalValueException.class, () -> task.toModelType());
     }
 }
