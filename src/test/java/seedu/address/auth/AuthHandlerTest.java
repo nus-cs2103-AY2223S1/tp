d5b9cbe0ce20641971a40e89efc0a8b3535cee0a
Ezra <ezratio15195@gmail.com>
2022-10-12 13:54:35 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/auth/AuthHandlerTest.java
+++ src/test/java/seedu/address/auth/AuthHandlerTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 
 public class AuthHandlerTest {
 
-    private final String TEST_USERNAME = "admin";
-    private final String TEST_PASSWORD = "admin";
-    
+    private final String testUsername = "admin";
+    private final String testPassword = "admin";
+
     @Test
     public void testCheckCredentials() {
-        assertEquals(TEST_USERNAME, Credentials.getDebugUsername());
-        assertEquals(TEST_PASSWORD, Credentials.getDebugPassword());
+        assertEquals(testUsername, Credentials.getDebugUsername());
+        assertEquals(testPassword, Credentials.getDebugPassword());
     }
 }
