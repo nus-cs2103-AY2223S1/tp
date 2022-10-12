2b368cb64ab44397ea17208fba23a4dbe9f09603
junweimoo <junweimoo@gmail.com>
2022-10-12 11:05:13 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/commands/AddMeetingCommand.java
+++ src/main/java/seedu/address/logic/commands/AddMeetingCommand.java
@@ -2,18 +2,13 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyMyInsuRec;
 import seedu.address.model.client.Client;
 import seedu.address.model.meeting.Description;
 import seedu.address.model.meeting.Meeting;
--- src/main/java/seedu/address/logic/parser/AddMeetingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddMeetingCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
 
 import java.util.stream.Stream;
