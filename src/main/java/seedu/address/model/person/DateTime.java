87e52b5b59d76deb6292cd1fe46f38d55fcfe1ce
Musab <>
2022-10-13 11:25:07 +0800

    Update DateTime to implement Comparable interface

--- src/main/java/seedu/address/model/person/DateTime.java
+++ src/main/java/seedu/address/model/person/DateTime.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
  * Represents a date and time with a formatted String value
  * Guarantees: immutable;
  */
-public class DateTime {
+public class DateTime implements Comparable<DateTime> {
     private final LocalDateTime localDateTime;
 
     /**
@@ -20,6 +20,13 @@ public class DateTime {
         requireNonNull(localDateTime);
         this.localDateTime = localDateTime;
     }
+    public Date getDate() {
+        return new Date(localDateTime.toLocalDate());
+    }
+
+    public Time getTime() {
+        return new Time(localDateTime.toLocalTime());
+    }
 
     @Override
     public String toString() {
@@ -27,6 +34,10 @@ public class DateTime {
                 .DateTimeFormatter.ofPattern("d-MMM-yyyy hh:mm a"));
     }
 
+    @Override
+    public int compareTo(DateTime other) {
+        return this.localDateTime.compareTo(other.localDateTime);
+    }
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
