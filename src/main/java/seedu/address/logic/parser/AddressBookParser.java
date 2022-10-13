da50bb9a0dc0c1b8f2b6abe3a61f8310c9a42d2c
doimoiboi <91471209+doimoiboi@users.noreply.github.com>
2022-10-14 03:07:54 +0800

    Update AddressBookParser.java
    Remove outdated ListCommand import.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,7 +15,6 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.list.ListCommand;
-import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.MarkCommand;
 import seedu.address.logic.commands.UnmarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
