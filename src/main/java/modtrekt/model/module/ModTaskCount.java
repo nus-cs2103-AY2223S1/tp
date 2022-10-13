bce6c5e24fbcf599ff77fb2f242c4e5ec13566e6
HoJunHao2000 <kennethhojunhao@gmail.com>
2022-10-13 09:22:35 +0800

    Update the message constraints for ModTaskCount

--- src/main/java/modtrekt/model/module/ModTaskCount.java
+++ src/main/java/modtrekt/model/module/ModTaskCount.java
@@ -10,13 +10,13 @@ import static modtrekt.commons.util.AppUtil.checkArgument;
 public class ModTaskCount {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Task count should only contain numeric characters and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "\\d+";
 
     private final String taskCount;
 
