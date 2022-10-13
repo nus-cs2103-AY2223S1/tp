ee94c4ab59bbb6b625d8a69dd0d11d0562058743
bryanhce <ho.cheng.en.bryan@gmail.com>
2022-10-13 23:35:10 +0800

    Update new test case with isRoomClean field

--- src/test/java/seedu/guest/model/util/SampleDataUtilTest.java
+++ src/test/java/seedu/guest/model/util/SampleDataUtilTest.java
@@ -8,6 +8,7 @@ import seedu.guest.model.GuestBook;
 import seedu.guest.model.guest.DateRange;
 import seedu.guest.model.guest.Email;
 import seedu.guest.model.guest.Guest;
+import seedu.guest.model.guest.IsRoomClean;
 import seedu.guest.model.guest.Name;
 import seedu.guest.model.guest.NumberOfGuests;
 import seedu.guest.model.guest.Phone;
@@ -20,22 +21,22 @@ public class SampleDataUtilTest {
         Guest[] testGuests = new Guest[]{
             new Guest(new Name("James Oliver"), new Phone("98345344"), new Email("jamesoliver@example.com"),
                     new DateRange("15/09/22 - 19/09/22"),
-                    new NumberOfGuests("2")),
+                    new NumberOfGuests("2"), new IsRoomClean("no")),
             new Guest(new Name("Kayla Smith"), new Phone("91823471"), new Email("kaylasmith@example.com"),
                     new DateRange("22/01/23 - 25/01/23"),
-                    new NumberOfGuests("3")),
+                    new NumberOfGuests("3"), new IsRoomClean("no")),
             new Guest(new Name("Charlie Lim"), new Phone("88237126"),
                     new Email("charlie@example.com"), new DateRange("21/02/22 - 22/03/22"),
-                    new NumberOfGuests("2")),
+                    new NumberOfGuests("2"), new IsRoomClean("yes")),
             new Guest(new Name("Ryan Wong"), new Phone("82831111"), new Email("wongryan@example.com"),
                     new DateRange("28/04/23 - 29/04/23"),
-                    new NumberOfGuests("2")),
+                    new NumberOfGuests("2"), new IsRoomClean("no")),
             new Guest(new Name("Ibrahim Irfan"), new Phone("84912293"), new Email("irfan@example.com"),
                     new DateRange("23/05/23 - 03/06/23"),
-                    new NumberOfGuests("1")),
+                    new NumberOfGuests("1"), new IsRoomClean("yes")),
             new Guest(new Name("Dave Balakrishnan"), new Phone("92821374"), new Email("daveb@example.com"),
                     new DateRange("30/12/22 - 02/01/23"),
-                    new NumberOfGuests("3"))
+                    new NumberOfGuests("3"), new IsRoomClean("yes")),
             };
 
         assertNotEquals(SampleDataUtil.getSampleGuests(), testGuests);
@@ -52,22 +53,22 @@ public class SampleDataUtilTest {
         GuestBook exceptedGb = new GuestBook();
         exceptedGb.addGuest(new Guest(new Name("James Oliver"), new Phone("98345344"),
                 new Email("jamesoliver@example.com"), new DateRange("15/09/22 - 19/09/22"),
-                new NumberOfGuests("2")));
+                new NumberOfGuests("2"), new IsRoomClean("no")));
         exceptedGb.addGuest(new Guest(new Name("Kayla Smith"), new Phone("91823471"),
                 new Email("kaylasmith@example.com"), new DateRange("22/01/23 - 25/01/23"),
-                new NumberOfGuests("3")));
+                new NumberOfGuests("3"), new IsRoomClean("no")));
         exceptedGb.addGuest(new Guest(new Name("Charlie Lim"), new Phone("88237126"),
                 new Email("charlie@example.com"), new DateRange("21/02/22 - 22/03/22"),
-                new NumberOfGuests("2")));
+                new NumberOfGuests("2"), new IsRoomClean("yes")));
         exceptedGb.addGuest(new Guest(new Name("Ryan Wong"), new Phone("82831111"),
                 new Email("wongryan@example.com"), new DateRange("28/04/23 - 29/04/23"),
-                new NumberOfGuests("2")));
+                new NumberOfGuests("2"), new IsRoomClean("no")));
         exceptedGb.addGuest(new Guest(new Name("Ibrahim Irfan"), new Phone("84912293"),
                 new Email("irfan@example.com"), new DateRange("23/05/23 - 03/06/23"),
-                new NumberOfGuests("1")));
+                new NumberOfGuests("1"), new IsRoomClean("yes")));
         exceptedGb.addGuest(new Guest(new Name("Dave Balakrishnan"), new Phone("92821374"),
                 new Email("daveb@example.com"), new DateRange("30/12/22 - 02/01/23"),
-                new NumberOfGuests("3")));
+                new NumberOfGuests("3"), new IsRoomClean("no")));
 
         // different values -> return false
         assertNotEquals(SampleDataUtil.getSampleGuestBook(), exceptedGb);
