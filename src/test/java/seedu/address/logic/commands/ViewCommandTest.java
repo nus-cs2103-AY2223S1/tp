5bce0f37e5194629d1e7a6274ef5b6cfef00799c
Ryan <ryanlml123@gmail.com>
2022-10-13 19:00:18 +0800

    Fix order of import statements

--- src/test/java/seedu/address/logic/commands/ViewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -19,8 +19,8 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.person.PersonContainsAttributePredicate;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.PersonContainsAttributePredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code ViewCommand}.
