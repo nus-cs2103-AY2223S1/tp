4620ca7ab9317173a73af60b58c8cf2e817c2a03
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-10-13 23:02:12 +0800

    Remove unnecc. methods from AddCommandParser

--- src/main/java/nus/climods/logic/parser/AddCommandParser.java
+++ src/main/java/nus/climods/logic/parser/AddCommandParser.java
@@ -11,16 +11,6 @@ import nus.climods.logic.parser.parameters.ModuleCodeParameter;
  * Parses input arguments and creates a new AddCommand object
  */
 public class AddCommandParser implements Parser<AddCommand> {
-
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given {@code
-     * ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand and returns an AddCommand object
      * for execution.
