7d9a39b3a1d8f857bd7e6fb3ceceeccb21966f0d
Arnav Aggarwal <65700476+arnav-ag@users.noreply.github.com>
2022-10-13 11:55:25 +0800

    Parser: Update more readable cli syntax

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,10 +6,10 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("/name ");
-    public static final Prefix PREFIX_PHONE = new Prefix("/phone ");
-    public static final Prefix PREFIX_EMAIL = new Prefix("/email ");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("/address ");
-    public static final Prefix PREFIX_TAG = new Prefix("/tag ");
+    public static final Prefix PREFIX_NAME = new Prefix("/name");
+    public static final Prefix PREFIX_PHONE = new Prefix("/phone");
+    public static final Prefix PREFIX_EMAIL = new Prefix("/email");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("/address");
+    public static final Prefix PREFIX_TAG = new Prefix("/tag");
 
 }
