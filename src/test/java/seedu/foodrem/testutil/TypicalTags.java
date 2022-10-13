0384fc38196c9da77353ab3cb5a7ea43401e91ad
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-10-14 03:06:14 +0800

    Replace static import with qualified test utils

--- src/test/java/seedu/foodrem/testutil/TypicalTags.java
+++ src/test/java/seedu/foodrem/testutil/TypicalTags.java
@@ -1,25 +1,21 @@
 package seedu.foodrem.testutil;
 
-import static seedu.foodrem.logic.commands.CommandTestUtil.VALID_TAG_NAME_FRUITS;
-import static seedu.foodrem.logic.commands.CommandTestUtil.VALID_TAG_NAME_FRUITS_WITH_WHITESPACES;
-import static seedu.foodrem.logic.commands.CommandTestUtil.VALID_TAG_NAME_NUMBERS;
-import static seedu.foodrem.logic.commands.CommandTestUtil.VALID_TAG_NAME_VEGETABLES;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import seedu.foodrem.logic.commands.CommandTestUtil;
 import seedu.foodrem.model.tag.Tag;
 
 /**
  * A utility class containing a list of {@code tag} objects to be used in tests.
  */
 public class TypicalTags {
-    public static final Tag FRUITS = new TagBuilder().withTagName(VALID_TAG_NAME_FRUITS).build();
+    public static final Tag FRUITS = new TagBuilder().withTagName(CommandTestUtil.VALID_TAG_NAME_FRUITS).build();
     public static final Tag FRUITS_WITH_WHITESPACE = new TagBuilder()
-            .withTagName(VALID_TAG_NAME_FRUITS_WITH_WHITESPACES).build();
-    public static final Tag NUMBERS = new TagBuilder().withTagName(VALID_TAG_NAME_NUMBERS).build();
-    public static final Tag VEGETABLES = new TagBuilder().withTagName(VALID_TAG_NAME_VEGETABLES).build();
+            .withTagName(CommandTestUtil.VALID_TAG_NAME_FRUITS_WITH_WHITESPACES).build();
+    public static final Tag NUMBERS = new TagBuilder().withTagName(CommandTestUtil.VALID_TAG_NAME_NUMBERS).build();
+    public static final Tag VEGETABLES = new TagBuilder().withTagName(CommandTestUtil.VALID_TAG_NAME_VEGETABLES).build();
 
     private TypicalTags() {
     } // prevents instantiation
