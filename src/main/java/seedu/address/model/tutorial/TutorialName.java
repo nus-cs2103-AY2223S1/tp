b621b132d253679a4902a0b64e51a44f65c39e42
Thanh Chu <lilyhanky57@gmail.com>
2022-10-13 20:07:56 +0800

    Update regex of module code and tutorial name

--- src/main/java/seedu/address/model/tutorial/TutorialName.java
+++ src/main/java/seedu/address/model/tutorial/TutorialName.java
@@ -16,7 +16,7 @@ public class TutorialName {
      * The first character of the name must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
 
     public final String fullName;
 
