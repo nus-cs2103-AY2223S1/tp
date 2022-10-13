def729847e399e1f06f9d82ecc8d6e7db46b3b80
Jason Christopher <jason.christopher@u.nus.edu>
2022-10-14 03:08:12 +0800

    Enhance argument validation using Java Regex

--- src/main/java/seedu/address/logic/parser/AddFieldCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddFieldCommandParser.java
@@ -3,6 +3,9 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import seedu.address.logic.commands.AddFieldCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -11,27 +14,55 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class AddFieldCommandParser implements Parser<AddFieldCommand> {
 
+    // Regex for the basic format of AddFieldCommand, should contain prefix and field name
+    private static final Pattern ADD_FIELD_COMMAND_FORMAT =
+            Pattern.compile("(?<prefix>\\S+)(?<fieldName>.*)");
+
     /**
      * Parses the given {@code String} of arguments in the context of the {@code AddFieldCommand}
      * and returns a {@code AddFieldCommand} object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddFieldCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+
+        final Matcher matcher = ADD_FIELD_COMMAND_FORMAT.matcher(args.trim());
+
+        if (!matcher.matches()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddFieldCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddFieldCommand.MESSAGE_USAGE)
+            );
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-        String prefixValue = nameKeywords[0];
-        String prefixName = nameKeywords[1];
-        requireNonNull(prefixValue);
-        requireNonNull(prefixName);
-        if (!prefixValue.endsWith("/") || prefixValue.startsWith("/")) {
-            throw new ParseException("Invalid Prefix Format");
+        String prefixString = matcher.group("prefix").trim();
+        String fieldName = matcher.group("fieldName").trim();
+        checkPrefix(prefixString);
+        checkFieldName(fieldName);
+        Prefix prefix = new Prefix(prefixString);
+        return new AddFieldCommand(prefix, fieldName);
+    }
+
+    /**
+     * Checks if a string is a valid prefix.
+     *
+     * @param prefixString a String representing a prefix to be checked.
+     * @throws ParseException if the string does not fulfil the requirement as a Prefix.
+     */
+    private void checkPrefix(String prefixString) throws ParseException {
+        if (!Prefix.isValidPrefix(prefixString)) {
+            throw new ParseException("Invalid Prefix Format: Prefixes should end with a \"/\" character");
         }
-        Prefix prefix = new Prefix(prefixValue);
-        return new AddFieldCommand(prefix, prefixName);
+    }
+
+    /**
+     * Checks if a string is a valid field name.
+     *
+     * @param fieldName a String representing the field name to be checked.
+     * @throws ParseException if {@code fieldName} is an empty string.
+     */
+    private void checkFieldName(String fieldName) throws ParseException {
+        requireNonNull(fieldName);
+        if (fieldName.equals("")) {
+            throw new ParseException("Invalid Field Name: Field name should not be empty");
+        }
     }
 }
--- src/main/java/seedu/address/logic/parser/Prefix.java
+++ src/main/java/seedu/address/logic/parser/Prefix.java
@@ -1,10 +1,17 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.regex.Pattern;
+
 /**
  * A prefix that marks the beginning of an argument in an arguments string.
  * E.g. 't/' in 'add James t/ friend'.
  */
 public class Prefix {
+
+    private static final Pattern PREFIX_VALIDATION_REGEX = Pattern.compile("\\p{Alnum}*/");
+
     private final String prefix;
 
     public Prefix(String prefix) {
@@ -17,6 +24,17 @@ public class Prefix {
 
     public String toString() {
         return getPrefix();
+    }
+
+    /**
+     * Checks whether a string is a valid prefix.
+     *
+     * @param prefix a String representing a prefix.
+     * @return true if the string can be a prefix, false otherwise.
+     */
+    public static boolean isValidPrefix(String prefix) {
+        requireNonNull(prefix);
+        return PREFIX_VALIDATION_REGEX.matcher(prefix).matches();
     }
 
     @Override
