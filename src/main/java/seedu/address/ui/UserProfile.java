dec84888ac3559be264498ec57627e0b56dc4e12
leehuiyulaura <e0>
2022-10-13 12:09:10 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/ui/UserProfile.java
+++ src/main/java/seedu/address/ui/UserProfile.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.person.user.EmptyUser;
 import seedu.address.model.person.user.ExistingUser;
 import seedu.address.model.person.user.User;
 
