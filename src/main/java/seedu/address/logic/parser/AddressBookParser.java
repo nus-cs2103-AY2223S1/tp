ca5a93d5df73ddf872fb9926874d2c8e9b2ea2a9
Coco <chuaziyifaith@gmail.com>
2022-10-13 14:23:54 +0800

    Update AddressBookParser Update FindPetCommandParser Update FindPetCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,17 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddPersonCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FilterLocCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -60,6 +50,9 @@ public class AddressBookParser {
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+
+        case FindPetCommand.COMMAND_WORD:
+            return new FindPetCommandParser().parse(arguments);
 
         case SortCommand.COMMAND_WORD:
             return new SortCommandParser().parse(arguments);
---
+++ src/main/java/seedu/address/logic/parser/FindPetCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindPetCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.PersonCategory;
+import seedu.address.model.pet.PetNameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new FindPetCommand object.
+ */
+public class FindPetCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
+    public FindPetCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+        return new FindPetCommand(new PetNameContainsKeywordsPredicate<>(Arrays.asList(nameKeywords)));
+    }
+}
