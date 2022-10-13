1a86fe4f8ff036c2b301ee8f2d4caf92b5d9c495
YH-15 <a1271755549@gmail.com>
2022-10-13 18:56:39 +0800

    fix checkstyle issues

--- src/test/java/jeryl/fyp/model/student/ProjectNameTest.java
+++ src/test/java/jeryl/fyp/model/student/ProjectNameTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class ProjectNameTest {
-    
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new ProjectName(null));
