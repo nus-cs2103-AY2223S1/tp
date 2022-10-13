1404a896805941056027e3c2cdb358a2d15ba318
Yue Hern <yuehernkang@hotmail.com>
2022-10-13 22:37:44 +0800

    Add Tests for Client class

---
+++ src/test/java/seedu/condonery/model/client/ClientTest.java
@@ -0,0 +1,53 @@
+package seedu.condonery.model.client;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.condonery.logic.commands.CommandTestUtil.*;
+import static seedu.condonery.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.condonery.testutil.Assert.assertThrows;
+import static seedu.condonery.testutil.TypicalClients.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import seedu.condonery.model.person.Person;
+import seedu.condonery.model.tag.Tag;
+import seedu.condonery.testutil.ClientBuilder;
+import seedu.condonery.testutil.PersonBuilder;
+import seedu.condonery.testutil.PropertyBuilder;
+
+public class ClientTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Client(null,null,null));
+    }
+
+    @Test
+    public void isSameClient() {
+        // same object -> returns true
+        assertTrue(ALICE_CLIENT.isSameClient(ALICE_CLIENT));
+
+        // null -> returns false
+        assertFalse(ALICE_CLIENT.isSameClient(null));
+
+        // same name, all other attributes different -> returns true
+        Client editedAlice = new ClientBuilder(ALICE_CLIENT).withName(VALID_NAME_BOB).withAddress(VALID_ADDRESS_BOB)
+                .withTags("tag1", "tag2").build();
+        assertTrue(ALICE_CLIENT.isSameClient(editedAlice));
+
+        // different name, all other attributes same -> returns false
+        editedAlice = new ClientBuilder(ALICE_CLIENT).withName(VALID_NAME_BOB).build();
+        assertFalse(ALICE_CLIENT.isSameClient(editedAlice));
+
+        // name differs in case, all other attributes same -> returns false
+        Client editedBob = new ClientBuilder(BOB_CLIENT).withName(VALID_NAME_BOB.toLowerCase()).build();
+        assertFalse(BOB_CLIENT.isSameClient(editedBob));
+
+        // name has trailing spaces, all other attributes same -> returns false
+        String nameWithTrailingSpaces = VALID_NAME_BOB + " ";
+        editedBob = new ClientBuilder(BOB_CLIENT).withName(nameWithTrailingSpaces).build();
+        assertFalse(BOB_CLIENT.isSameClient(editedBob));
+    }
+
+}
