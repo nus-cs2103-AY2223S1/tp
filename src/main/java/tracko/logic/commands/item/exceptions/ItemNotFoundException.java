ac9396a9ca80a74bfaf66526275d2ea24b0fa911
sohenze <e0725494@u.nus.edu>
2022-10-13 15:07:48 +0800

    Add exceptions for item in model

---
+++ src/main/java/tracko/logic/commands/item/exceptions/ItemNotFoundException.java
@@ -0,0 +1,6 @@
+package tracko.logic.commands.item.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified person.
+ */
+public class ItemNotFoundException extends RuntimeException {}
