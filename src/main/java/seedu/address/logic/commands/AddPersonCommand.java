431cf32f47c622856a9669e5b8d9c2362024ecb9
chengda300 <chengda300@gmail.com>
2022-10-13 19:39:47 +0800

    Fix duplicate person ID (same fix as duplicate internship ID)

--- src/main/java/seedu/address/logic/commands/AddPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/AddPersonCommand.java
@@ -37,13 +37,14 @@ public class AddPersonCommand extends Command {
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "[" + PREFIX_TAG + "TAG]..."
+            + "[" + PREFIX_LINK_INDEX + "]\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney"
+            + PREFIX_TAG + "owesMoney "
             + PREFIX_LINK_INDEX + "1 ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
