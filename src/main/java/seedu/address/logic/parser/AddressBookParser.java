b505c842ec149ddedfc66de980337dc4b8ca78b0
DevanshShah1309 <devanshshah1309@gmail.com>
2022-10-13 22:43:44 +0800

    Reorder case statements for parser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -79,6 +79,9 @@ public class AddressBookParser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
+        case ListMembersCommand.COMMAND_WORD:
+            return new ListMembersCommand();
+
         case AddMemberCommand.COMMAND_WORD:
             return new AddMemberCommandParser().parse(arguments);
 
@@ -94,23 +97,20 @@ public class AddressBookParser {
         case DeleteTeamCommand.COMMAND_WORD:
             return new DeleteTeamCommandParser().parse(arguments);
 
+        case ListTasksCommand.COMMAND_WORD:
+            return new ListTasksCommand();
+
         case AddTaskCommand.COMMAND_WORD:
             return new AddTaskCommandParser().parse(arguments);
+
+        case DeleteTaskCommand.COMMAND_WORD:
+            return new DeleteTaskCommandParser().parse(arguments);
 
         case AssignTaskCommand.COMMAND_WORD:
             return new AssignTaskCommandParser().parse(arguments);
 
-        case ListTasksCommand.COMMAND_WORD:
-            return new ListTasksCommand();
-
         case SetDeadlineCommand.COMMAND_WORD:
             return new SetDeadlineCommandParser().parse(arguments);
-
-        case ListMembersCommand.COMMAND_WORD:
-            return new ListMembersCommand();
-
-        case DeleteTaskCommand.COMMAND_WORD:
-            return new DeleteTaskCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
