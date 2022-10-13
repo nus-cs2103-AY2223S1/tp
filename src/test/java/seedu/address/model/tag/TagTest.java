af06e2b5a456b3b3277e0cf29d0ae3124517f583
izzahaj <izzah.jalil02@gmail.com>
2022-10-14 01:35:33 +0800

    Update TagTest with more unit tests

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,5 +1,7 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -18,9 +20,29 @@ public class TagTest {
     }
 
     @Test
-    public void isValidTagName() {
-        // null tag name
+    public void isValidTagName_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
     }
 
+    @Test
+    public void isValidTagName_validTagName_returnsTrue() {
+        // alphanumeric
+        assertTrue(Tag.isValidTagName("elderly"));
+        assertTrue(Tag.isValidTagName("123"));
+    }
+
+    @Test
+    public void isValidTagName_invalidTagName_returnsFalse() {
+        // empty tag name
+        assertFalse(Tag.isValidTagName(""));
+
+        // blank tag name
+        assertFalse(Tag.isValidTagName(" "));
+
+        // special characters
+        assertFalse(Tag.isValidTagName("@!#$%^&*()-=+_[];.,`~:<>?/"));
+
+        // multiple words
+        assertFalse(Tag.isValidTagName("high risk"));
+    }
 }
