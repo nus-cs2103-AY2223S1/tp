dfee98ac8fed50e60753d68532807af21cd746dd
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-10-15 00:15:40 +0800

    Add `MESSAGE_INVALID_MESSAGE_INDEX`

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,6 +8,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_MESSAGE_INDEX = "The message index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_NO_TARGET_PERSON = "No target person found, use \"show INDEX\" to set a target";
     public static final String MESSAGE_EMPTY_ARGUMENTS = "Please enter a query after \"%1$s\"";
