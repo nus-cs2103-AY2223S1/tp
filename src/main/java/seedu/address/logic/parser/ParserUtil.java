5c02db05b822ae64c63b61327dc23679c38f9e95
arnav-ag <arnav.ctf@gmail.com>
2022-10-15 00:19:36 +0800

    Remove all email and phone from UI

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -10,9 +10,9 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.contact.Email;
+import seedu.address.model.person.contact.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
