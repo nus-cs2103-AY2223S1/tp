9ee866f3c8fa0ad0d5f53d98114d6c2d447680ed
Thanh Chu <lilyhanky57@gmail.com>
2022-10-14 21:43:56 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/ReminderListPanel.java
+++ src/main/java/seedu/address/ui/ReminderListPanel.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -7,8 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.reminder.Reminder;
-
-import java.util.logging.Logger;
 
 /**
  * Panel containing the list of persons.
