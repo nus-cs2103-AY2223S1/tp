8f7eb8d1b1fd008694524456125754e1d882f4a2
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-10-15 00:18:15 +0800

    Define the `MessageCommandGroup`

---
+++ src/main/java/seedu/address/logic/commands/GenerateMessageCommand.java
@@ -0,0 +1,83 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.message.Message;
+import seedu.address.model.person.Person;
+
+
+/**
+ * Generates a message based on a template and a person in the address book.
+ */
+public class GenerateMessageCommand extends MessageCommandGroup {
+    public static final String COMMAND_SPECIFIER = "generate";
+    public static final String COMMAND_WORD = COMMAND_GROUP + " " + COMMAND_SPECIFIER;
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Generates a customised message for the person. "
+            + "Parameters: "
+            + "[PERSON_INDEX] [MESSAGE_INDEX] \n"
+            + "Example: " + COMMAND_WORD + " 1 2";
+
+    public static final String MESSAGE_SUCCESS = "Message generated: %1$s";
+
+    private final Index personIndex;
+    private final Index messageIndex;
+
+    /**
+     * @param personIndex person to generate for
+     * @param messageIndex message template to generate on
+     */
+    public GenerateMessageCommand(Index personIndex, Index messageIndex) {
+        requireNonNull(personIndex);
+        requireNonNull(messageIndex);
+        this.personIndex = personIndex;
+        this.messageIndex = messageIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        List<Person> lastShownList = model.getFilteredPersonList();
+        if (personIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+        Person person = lastShownList.get(personIndex.getZeroBased());
+
+        List<Message> messages = model.getMessages();
+        System.out.println(messageIndex.getZeroBased());
+        if (messageIndex.getZeroBased() >= messages.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_MESSAGE_INDEX);
+        }
+        Message message = messages.get(messageIndex.getZeroBased());
+
+        String generatedMessage = message.generate(person);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, generatedMessage));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof GenerateMessageCommand)) {
+            return false;
+        }
+
+        // state check
+        GenerateMessageCommand e = (GenerateMessageCommand) other;
+        return messageIndex.equals(e.messageIndex) && personIndex.equals(e.personIndex);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/MessageCommandGroupParser.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.CreateMessageCommand;
+import seedu.address.logic.commands.DeleteMessageCommand;
+import seedu.address.logic.commands.GenerateMessageCommand;
+import seedu.address.logic.commands.MessageCommandGroup;
+import seedu.address.logic.commands.ListMessageCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class MessageCommandGroupParser implements Parser<MessageCommandGroup> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * and returns a command in the TagCommandGroup for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public MessageCommandGroup parse(String args) throws ParseException {
+        requireNonNull(args);
+        String trimmedArgs = args.trim();
+        // TODO: add complete list of commands
+        String errorMessage = String.format("%s\n\n%s\n\n%s\n\n%s",
+                ListMessageCommand.MESSAGE_USAGE,
+                CreateMessageCommand.MESSAGE_USAGE,
+                DeleteMessageCommand.MESSAGE_USAGE,
+                GenerateMessageCommand.MESSAGE_USAGE);
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, errorMessage));
+        }
+
+        String[] argArray = trimmedArgs.split("\\s+");
+        String commandSpecifier = argArray[0];
+
+        switch (commandSpecifier) {
+        case CreateMessageCommand.COMMAND_SPECIFIER:
+            String[] argsToPass = Arrays.copyOfRange(argArray, 1, argArray.length);
+            return new CreateMessageCommandParser().parse(String.join(" ", argsToPass));
+        case DeleteMessageCommand.COMMAND_SPECIFIER:
+            argsToPass = Arrays.copyOfRange(argArray, 1, argArray.length);
+            return new DeleteMessageCommandParser().parse(String.join(" ", argsToPass));
+        case GenerateMessageCommand.COMMAND_SPECIFIER:
+            argsToPass = Arrays.copyOfRange(argArray, 1, argArray.length);
+            return new GenerateMessageCommandParser().parse(String.join(" ", argsToPass));
+        case ListMessageCommand.COMMAND_SPECIFIER:
+            return new ListMessageCommandParser().parse("");
+        default:
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, errorMessage));
+        }
+    }
+}
