98f0332febc26d8cf295d758cabb75c8fcda5585
Xu Yi <E0772330@u.nus.edu>
2022-10-14 21:07:43 +0800

    fix test style

--- src/test/java/seedu/taassist/testutil/TypicalModuleClasses.java
+++ src/test/java/seedu/taassist/testutil/TypicalModuleClasses.java
@@ -1,7 +1,6 @@
 package seedu.taassist.testutil;
 
 import seedu.taassist.model.moduleclass.ModuleClass;
-import seedu.taassist.model.session.Session;
 
 /**
  * A utility class containing a list of {@code ModuleClass} objects to be used in tests.
@@ -11,6 +10,4 @@ public class TypicalModuleClasses {
             .withSessions(TypicalSessions.ASSIGNMENT_1).build();
     public static final ModuleClass CS1231S = new ModuleClassBuilder().withName("CS1231S")
             .withSessions(TypicalSessions.TUTORIAL_1).build();
-    public static final ModuleClass CS2100 = new ModuleClassBuilder().withName("CS2100")
-            .withSessions(TypicalSessions.TUTORIAL_1,TypicalSessions.LAB_1).build();
 }
