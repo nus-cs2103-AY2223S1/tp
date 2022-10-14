309b53bfbd473051b55c2d5d8cb670dc16182c93
ryanlml <78495817+ryanlml@users.noreply.github.com>
2022-10-14 16:38:43 +0800

    Extend the definition of equality in the `isSamePerson` method in `Person` class, which now checks the equality of name, phone number, email and survey.

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -2,11 +2,7 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
@@ -31,10 +27,22 @@ public class PersonTest {
         // null -> returns false
         assertFalse(ALICE.isSamePerson(null));
 
-        // same name, all other attributes different -> returns true
+        // same name, all other attributes different -> returns false
         Person editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
                 .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND).build();
+        assertFalse(ALICE.isSamePerson(editedAlice));
+
+        // same name, phone, email and survey -> returns true
+        editedAlice = new PersonBuilder().withName(ALICE.getName().fullName)
+                .withEmail(ALICE.getEmail().value).withPhone(ALICE.getPhone().value)
+                .withSurvey(ALICE.getSurvey().survey).build();
         assertTrue(ALICE.isSamePerson(editedAlice));
+
+        // same name, phone and email, different survey -> returns false
+        editedAlice = new PersonBuilder().withName(ALICE.getName().fullName)
+                .withEmail(ALICE.getEmail().value).withPhone(ALICE.getPhone().value)
+                .withSurvey(VALID_SURVEY_BOB).build();
+        assertFalse(ALICE.isSamePerson(editedAlice));
 
         // different name, all other attributes same -> returns false
         editedAlice = new PersonBuilder(ALICE).withName(VALID_NAME_BOB).build();
