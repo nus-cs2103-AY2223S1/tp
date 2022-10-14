52094b84d61c31b3dc8f909228569f6a9b2c7fac
Xu Yi <E0772330@u.nus.edu>
2022-10-14 21:01:36 +0800

    Adapt to current version

--- src/test/java/seedu/taassist/testutil/TypicalModuleClasses.java
+++ src/test/java/seedu/taassist/testutil/TypicalModuleClasses.java
@@ -1,16 +1,17 @@
 package seedu.taassist.testutil;
 
 import seedu.taassist.model.moduleclass.ModuleClass;
+import seedu.taassist.model.session.Session;
 
 /**
  * A utility class containing a list of {@code ModuleClass} objects to be used in tests.
  */
 public class TypicalModuleClasses {
     public static final ModuleClass CS1101S = new ModuleClassBuilder().withName("CS1101S")
-            .withSessions("Assignment 1").build();
+            .withSessions(TypicalSessions.ASSIGNMENT_1).build();
     public static final ModuleClass CS1231S = new ModuleClassBuilder().withName("CS1231S")
-            .withSessions("Tutorial 1").build();
+            .withSessions(TypicalSessions.TUTORIAL_1).build();
 
     public static final ModuleClass CS2100 = new ModuleClassBuilder().withName("CS2100")
-            .withSessions("Lab 2", "Assignment 4", "Tutorial 3").build();
+            .withSessions(TypicalSessions.TUTORIAL_1,TypicalSessions.LAB_1).build();
 }
