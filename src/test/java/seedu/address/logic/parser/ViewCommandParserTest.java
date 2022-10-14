c933b5b5c74f67ed7c33b7d72fa67b469601a83b
Jessica Mary <jessicamary472@gmail.com>
2022-10-14 11:51:23 +0800

    Add tests for invalid args and getViewedPersonList

--- src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
@@ -4,6 +4,8 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,10 +17,15 @@ public class ViewCommandParserTest {
     @Test
     public void parse_validArgs_returnsViewCommand() {
         assertParseSuccess(parser, "1", new ViewCommand(INDEX_FIRST_PERSON));
+        assertParseSuccess(parser, "2", new ViewCommand(INDEX_SECOND_PERSON));
+        assertParseSuccess(parser, "3", new ViewCommand(INDEX_THIRD_PERSON));
+
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "#", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, " ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
     }
 }
