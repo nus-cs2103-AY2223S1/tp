43905e8c4e9889a82ee245919e8c6b2f768becb5
KIANSEONG <kianseong.quah@gmail.com>
2022-10-14 15:34:14 +0800

    feat: add command navigation

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,8 +1,13 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -18,6 +23,10 @@ public class CommandBox extends UiPart<Region> {
 
     private final CommandExecutor commandExecutor;
 
+    private final List<String> commandLog;
+    private int commandLogPointer;
+    private String currentText;
+
     @FXML
     private TextField commandTextField;
 
@@ -27,8 +36,10 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(CommandExecutor commandExecutor) {
         super(FXML);
         this.commandExecutor = commandExecutor;
+        commandLog = new ArrayList<>();
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+        addNavigateCommandsEvent();
     }
 
     /**
@@ -42,11 +53,61 @@ public class CommandBox extends UiPart<Region> {
         }
 
         try {
-            commandExecutor.execute(commandText);
+            commandLog.add(commandText);
+            commandLogPointer = commandLog.size();
+
             commandTextField.setText("");
+            commandExecutor.execute(commandText);
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
         }
+    }
+
+    /**
+     * Allows the user to navigate between previously entered commands with the UP and DOWN arrow keys.
+     * This function adds an event filter that checks whenever the user presses the UP and DOWN keys.
+     */
+    private void addNavigateCommandsEvent() {
+        commandTextField.addEventFilter(KeyEvent.KEY_PRESSED, this::handleNavigationKeysPressed);
+    }
+
+    /**
+     * If no more previous command and UP key is pressed, nothing happens.
+     * If current text matches the latest command and DOWN key is pressed, nothing happens.
+     */
+    private void handleNavigationKeysPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.UP) {
+            event.consume();
+            // if text field shows the earliest command, nothing changes.
+            if (commandLogPointer == 0) {
+                return;
+            }
+
+            if (commandLogPointer == commandLog.size()) {
+                currentText = commandTextField.getText();
+            }
+
+            commandLogPointer -= 1;
+            commandTextField.setText(commandLog.get(commandLogPointer));
+
+        } else if (event.getCode() == KeyCode.DOWN) {
+            event.consume();
+            commandLogPointer += 1;
+
+            // if text field shows the latest command, nothing changes.
+            if (commandLogPointer >= commandLog.size()) {
+                commandLogPointer = commandLog.size();
+                commandTextField.setText(currentText);
+                return;
+            }
+
+            commandTextField.setText(commandLog.get(commandLogPointer));
+        } else {
+            // do not do anything if any other key is pressed.
+            return;
+        }
+
+        commandTextField.positionCaret(commandTextField.getLength());
     }
 
     /**
