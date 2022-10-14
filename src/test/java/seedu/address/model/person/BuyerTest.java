fc217f688262a786d7ba0fdf5ca1544c8bc4a5f7
Coco <chuaziyifaith@gmail.com>
2022-10-14 23:17:14 +0800

    Update BuyerTest

---
+++ src/test/java/seedu/address/model/person/BuyerTest.java
@@ -0,0 +1,87 @@
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.TypicalPersons;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalBuyers.ALICE;
+import static seedu.address.testutil.TypicalBuyers.BOB;
+
+public class BuyerTest {
+    @Test
+    public void asObservableList_modifyList_throwsUnsupportedOperationException() {
+        Buyer buyer = new PersonBuilder().buildBuyer();
+        assertThrows(UnsupportedOperationException.class, () -> buyer.getTags().remove(0));
+    }
+
+    @Test
+    public void isSameBuyer() {
+        // same object -> returns true
+        assertTrue(ALICE.isSamePerson(ALICE));
+
+        // null -> returns false
+        assertFalse(ALICE.isSamePerson(null));
+
+        // same name, all other attributes different -> returns true
+        Buyer editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
+                .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND).buildBuyer();
+        assertTrue(ALICE.isSamePerson(editedAlice));
+
+        // different name, all other attributes same -> returns false
+        editedAlice = new PersonBuilder(ALICE).withName(VALID_NAME_BOB).buildBuyer();
+        assertFalse(ALICE.isSamePerson(editedAlice));
+
+        // name differs in case, all other attributes same -> returns false
+        Buyer editedBob = new PersonBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).buildBuyer();
+        assertFalse(BOB.isSamePerson(editedBob));
+
+        // name has trailing spaces, all other attributes same -> returns false
+        String nameWithTrailingSpaces = VALID_NAME_BOB + " ";
+        editedBob = new PersonBuilder(BOB).withName(nameWithTrailingSpaces).buildBuyer();
+        assertFalse(BOB.isSamePerson(editedBob));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Buyer aliceCopy = new PersonBuilder(TypicalPersons.ALICE).buildBuyer();
+        assertTrue(ALICE.equals(aliceCopy));
+
+        // same object -> returns true
+        assertTrue(ALICE.equals(ALICE));
+
+        // null -> returns false
+        assertFalse(ALICE.equals(null));
+
+        // different type -> returns false
+        assertFalse(ALICE.equals(5));
+
+        // different person -> returns false
+        assertFalse(ALICE.equals(BOB));
+
+        // different name -> returns false
+        Person editedAlice = new PersonBuilder(ALICE).withName(VALID_NAME_BOB).buildBuyer();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different phone -> returns false
+        editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).buildBuyer();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different email -> returns false
+        editedAlice = new PersonBuilder(ALICE).withEmail(VALID_EMAIL_BOB).buildBuyer();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different address -> returns false
+        editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).buildBuyer();
+        assertFalse(ALICE.equals(editedAlice));
+
+        // different tags -> returns false
+        editedAlice = new PersonBuilder(ALICE).withTags(VALID_TAG_HUSBAND).buildBuyer();
+        assertFalse(ALICE.equals(editedAlice));
+    }
+}
