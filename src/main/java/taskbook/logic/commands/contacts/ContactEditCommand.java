fe5c92f92bdcb15937e14ce984a12d10dca6b62f
Jin Wei <seetohjinwei@gmail.com>
2022-10-14 14:57:48 +0800

    ContactEditCommand: Update usage message

--- src/main/java/taskbook/logic/commands/contacts/ContactEditCommand.java
+++ src/main/java/taskbook/logic/commands/contacts/ContactEditCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static taskbook.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static taskbook.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static taskbook.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static taskbook.logic.parser.CliSyntax.PREFIX_INDEX;
 import static taskbook.logic.parser.CliSyntax.PREFIX_NAME;
 import static taskbook.logic.parser.CliSyntax.PREFIX_PHONE;
 import static taskbook.logic.parser.CliSyntax.PREFIX_TAG;
@@ -35,17 +36,17 @@ public class ContactEditCommand extends Command {
     public static final String COMMAND_WORD = "edit";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
-            + "by the index number used in the displayed person list. "
-            + "Existing values will be overwritten by the input values.\n"
-            + "Parameters: INDEX (must be a positive integer) "
-            + "[" + PREFIX_NAME + "NAME] "
-            + "[" + PREFIX_PHONE + "PHONE] "
-            + "[" + PREFIX_EMAIL + "EMAIL] "
-            + "[" + PREFIX_ADDRESS + "ADDRESS] "
-            + "[" + PREFIX_TAG + "TAG]...\n"
-            + "Example: " + COMMAND_WORD + " 1 "
-            + PREFIX_PHONE + "91234567 "
-            + PREFIX_EMAIL + "johndoe@example.com";
+        + "by the index number used in the displayed person list. "
+        + "Existing values will be overwritten by the input values.\n"
+        + "Parameters: " + PREFIX_INDEX + "INDEX (must be a positive integer) "
+        + "[" + PREFIX_NAME + "NAME] "
+        + "[" + PREFIX_PHONE + "PHONE] "
+        + "[" + PREFIX_EMAIL + "EMAIL] "
+        + "[" + PREFIX_ADDRESS + "ADDRESS] "
+        + "[" + PREFIX_TAG + "TAG]...\n"
+        + "Example: " + COMMAND_WORD + " " + PREFIX_INDEX + " 1 "
+        + PREFIX_PHONE + "91234567 "
+        + PREFIX_EMAIL + "johndoe@example.com";
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
