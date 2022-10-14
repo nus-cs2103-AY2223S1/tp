cd06be054d759e16dce632c7a5795ea593f1a7e2
Rui Han <richardcrh2000@gmail.com>
2022-10-14 17:41:08 +0800

    Realigns Code in Person.java
    Realigns line 109 into a one line code

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -106,12 +106,7 @@ public class Person {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name,
-                phone,
-                email,
-                address,
-                tags,
-                loan);
+        return Objects.hash(name, phone, email, address, tags, loan);
     }
 
     @Override
