a4356c4ec59ed3ada3cb32988d395b17b808e0de
ningtan11 <e0773603@u.nus.edu>
2022-10-14 15:02:33 +0800

    no message

--- src/test/java/seedu/waddle/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/waddle/logic/parser/AddCommandParserTest.java
@@ -1,85 +1,87 @@
 package seedu.waddle.logic.parser;
 
 import static seedu.waddle.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.waddle.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
-import static seedu.waddle.logic.commands.CommandTestUtil.ADDRESS_DESC_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
-import static seedu.waddle.logic.commands.CommandTestUtil.EMAIL_DESC_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_ADDRESS_DESC;
-import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_EMAIL_DESC;
+import static seedu.waddle.logic.commands.CommandTestUtil.COUNTRY_DESC_SUMMER;
+import static seedu.waddle.logic.commands.CommandTestUtil.COUNTRY_DESC_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.END_DATE_DESC_SUMMER;
+import static seedu.waddle.logic.commands.CommandTestUtil.END_DATE_DESC_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_COUNTRY_DESC;
+import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_END_DATE_DESC;
 import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
-import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_PHONE_DESC;
-import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_TAG_DESC;
-import static seedu.waddle.logic.commands.CommandTestUtil.NAME_DESC_AMY;
-import static seedu.waddle.logic.commands.CommandTestUtil.NAME_DESC_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
-import static seedu.waddle.logic.commands.CommandTestUtil.PHONE_DESC_BOB;
+import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_PEOPLE_DESC;
+import static seedu.waddle.logic.commands.CommandTestUtil.INVALID_START_DATE_DESC;
+import static seedu.waddle.logic.commands.CommandTestUtil.NAME_DESC_SUMMER;
+import static seedu.waddle.logic.commands.CommandTestUtil.NAME_DESC_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.PEOPLE_DESC_SUMMER;
+import static seedu.waddle.logic.commands.CommandTestUtil.PEOPLE_DESC_WINTER;
 import static seedu.waddle.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
 import static seedu.waddle.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
-import static seedu.waddle.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
-import static seedu.waddle.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
-import static seedu.waddle.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.waddle.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.waddle.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.waddle.logic.commands.CommandTestUtil.START_DATE_DESC_SUMMER;
+import static seedu.waddle.logic.commands.CommandTestUtil.START_DATE_DESC_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.VALID_COUNTRY_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.VALID_END_DATE_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.VALID_NAME_WINTER;
+import static seedu.waddle.logic.commands.CommandTestUtil.VALID_START_DATE_WINTER;
 import static seedu.waddle.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.waddle.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.waddle.testutil.TypicalPersons.AMY;
-import static seedu.waddle.testutil.TypicalPersons.BOB;
+import static seedu.waddle.testutil.TypicalItineraries.SUMMER;
+import static seedu.waddle.testutil.TypicalItineraries.WINTER;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.waddle.logic.commands.AddCommand;
-import seedu.waddle.model.person.Address;
-import seedu.waddle.model.person.Email;
-import seedu.waddle.model.person.Name;
-import seedu.waddle.model.person.Person;
-import seedu.waddle.model.person.Phone;
-import seedu.waddle.model.tag.Tag;
-import seedu.waddle.testutil.PersonBuilder;
+import seedu.waddle.model.itinerary.Country;
+import seedu.waddle.model.itinerary.Date;
+import seedu.waddle.model.itinerary.Itinerary;
+import seedu.waddle.model.itinerary.Name;
+import seedu.waddle.model.itinerary.People;
+import seedu.waddle.testutil.ItineraryBuilder;
 
 public class AddCommandParserTest {
     private AddCommandParser parser = new AddCommandParser();
 
     @Test
     public void parse_allFieldsPresent_success() {
-        Person expectedPerson = new PersonBuilder(BOB).withTags(VALID_TAG_FRIEND).build();
+        Itinerary expectedItinerary = new ItineraryBuilder(WINTER).build();
 
         // whitespace only preamble
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_WINTER + COUNTRY_DESC_WINTER
+                + START_DATE_DESC_WINTER + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER,
+                new AddCommand(expectedItinerary));
 
         // multiple names - last name accepted
-        assertParseSuccess(parser, NAME_DESC_AMY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
+        assertParseSuccess(parser, NAME_DESC_SUMMER + NAME_DESC_WINTER + COUNTRY_DESC_WINTER
+                + START_DATE_DESC_WINTER + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER,
+                new AddCommand(expectedItinerary));
 
         // multiple phones - last phone accepted
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_AMY + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
+        assertParseSuccess(parser, NAME_DESC_WINTER + COUNTRY_DESC_SUMMER + COUNTRY_DESC_WINTER
+                + START_DATE_DESC_WINTER + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER,
+                new AddCommand(expectedItinerary));
 
         // multiple emails - last email accepted
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_AMY + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
+        assertParseSuccess(parser, NAME_DESC_WINTER + PEOPLE_DESC_WINTER + START_DATE_DESC_SUMMER
+                + START_DATE_DESC_WINTER + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER,
+                new AddCommand(expectedItinerary));
 
         // multiple addresses - last address accepted
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_AMY
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
+        assertParseSuccess(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + END_DATE_DESC_SUMMER + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER,
+                new AddCommand(expectedItinerary));
 
         // multiple tags - all accepted
-        Person expectedPersonMultipleTags = new PersonBuilder(BOB).withTags(VALID_TAG_FRIEND, VALID_TAG_HUSBAND)
-                .build();
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, new AddCommand(expectedPersonMultipleTags));
+        Itinerary expectedPersonMultipleTags = new ItineraryBuilder(WINTER).build();
+        assertParseSuccess(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + END_DATE_DESC_WINTER + PEOPLE_DESC_SUMMER + PEOPLE_DESC_WINTER,
+                new AddCommand(expectedPersonMultipleTags));
     }
 
     @Test
     public void parse_optionalFieldsMissing_success() {
         // zero tags
-        Person expectedPerson = new PersonBuilder(AMY).withTags().build();
-        assertParseSuccess(parser, NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY,
-                new AddCommand(expectedPerson));
+        Itinerary expectedItinerary = new ItineraryBuilder(SUMMER).build();
+        assertParseSuccess(parser, NAME_DESC_SUMMER + COUNTRY_DESC_SUMMER + START_DATE_DESC_SUMMER
+                + END_DATE_DESC_SUMMER, new AddCommand(expectedItinerary));
     }
 
     @Test
@@ -87,55 +89,55 @@ public class AddCommandParserTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
 
         // missing name prefix
-        assertParseFailure(parser, VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
+        assertParseFailure(parser, VALID_NAME_WINTER + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + END_DATE_DESC_WINTER, expectedMessage);
 
         // missing phone prefix
-        assertParseFailure(parser, NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
+        assertParseFailure(parser, NAME_DESC_WINTER + VALID_COUNTRY_WINTER + START_DATE_DESC_WINTER
+                + END_DATE_DESC_WINTER, expectedMessage);
 
         // missing email prefix
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
+        assertParseFailure(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + VALID_START_DATE_WINTER
+                + END_DATE_DESC_WINTER, expectedMessage);
 
         // missing address prefix
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + VALID_ADDRESS_BOB,
-                expectedMessage);
+        assertParseFailure(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + VALID_END_DATE_WINTER, expectedMessage);
 
         // all prefixes missing
-        assertParseFailure(parser, VALID_NAME_BOB + VALID_PHONE_BOB + VALID_EMAIL_BOB + VALID_ADDRESS_BOB,
-                expectedMessage);
+        assertParseFailure(parser, VALID_NAME_WINTER + VALID_COUNTRY_WINTER + VALID_START_DATE_WINTER
+                + VALID_END_DATE_WINTER, expectedMessage);
     }
 
     @Test
     public void parse_invalidValue_failure() {
         // invalid name
-        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Name.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, INVALID_NAME_DESC + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER, Name.MESSAGE_CONSTRAINTS);
 
         // invalid phone
-        assertParseFailure(parser, NAME_DESC_BOB + INVALID_PHONE_DESC + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Phone.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_WINTER + INVALID_COUNTRY_DESC + START_DATE_DESC_WINTER
+                + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER, Country.MESSAGE_CONSTRAINTS);
 
         // invalid email
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + INVALID_EMAIL_DESC + ADDRESS_DESC_BOB
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Email.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + INVALID_START_DATE_DESC
+                + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER, Date.MESSAGE_CONSTRAINTS);
 
         // invalid address
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Address.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + INVALID_END_DATE_DESC + PEOPLE_DESC_WINTER, Date.MESSAGE_CONSTRAINTS);
 
         // invalid tag
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + INVALID_TAG_DESC + VALID_TAG_FRIEND, Tag.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, NAME_DESC_WINTER + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + END_DATE_DESC_WINTER + INVALID_PEOPLE_DESC, People.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
-        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC,
-                Name.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, INVALID_NAME_DESC + COUNTRY_DESC_WINTER + START_DATE_DESC_WINTER
+                + INVALID_END_DATE_DESC, Name.MESSAGE_CONSTRAINTS);
 
         // non-empty preamble
-        assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_WINTER + COUNTRY_DESC_WINTER
+                + START_DATE_DESC_WINTER + END_DATE_DESC_WINTER + PEOPLE_DESC_WINTER,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
     }
 }
---
+++ src/test/java/seedu/waddle/logic/parser/WaddleParserTest.java
@@ -0,0 +1,101 @@
+package seedu.waddle.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.waddle.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.waddle.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.waddle.testutil.Assert.assertThrows;
+import static seedu.waddle.testutil.TypicalIndexes.INDEX_FIRST_ITINERARY;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.waddle.logic.commands.AddCommand;
+import seedu.waddle.logic.commands.ClearCommand;
+import seedu.waddle.logic.commands.DeleteCommand;
+import seedu.waddle.logic.commands.EditCommand;
+import seedu.waddle.logic.commands.EditCommand.EditItineraryDescriptor;
+import seedu.waddle.logic.commands.ExitCommand;
+import seedu.waddle.logic.commands.FindCommand;
+import seedu.waddle.logic.commands.HelpCommand;
+import seedu.waddle.logic.commands.ListCommand;
+import seedu.waddle.logic.parser.exceptions.ParseException;
+import seedu.waddle.model.itinerary.Itinerary;
+import seedu.waddle.model.itinerary.NameContainsKeywordsPredicate;
+import seedu.waddle.testutil.EditItineraryDescriptorBuilder;
+import seedu.waddle.testutil.ItineraryBuilder;
+import seedu.waddle.testutil.ItineraryUtil;
+
+public class WaddleParserTest {
+
+    private final WaddleParser parser = new WaddleParser();
+
+    @Test
+    public void parseCommand_add() throws Exception {
+        Itinerary itinerary = new ItineraryBuilder().build();
+        AddCommand command = (AddCommand) parser.parseCommand(ItineraryUtil.getAddCommand(itinerary));
+        assertEquals(new AddCommand(itinerary), command);
+    }
+
+    @Test
+    public void parseCommand_clear() throws Exception {
+        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
+        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
+    }
+
+    @Test
+    public void parseCommand_delete() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_ITINERARY.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_ITINERARY), command);
+    }
+
+    @Test
+    public void parseCommand_edit() throws Exception {
+        Itinerary person = new ItineraryBuilder().build();
+        EditItineraryDescriptor descriptor = new EditItineraryDescriptorBuilder(person).build();
+        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
+                + INDEX_FIRST_ITINERARY.getOneBased() + " " + ItineraryUtil.getEditItineraryDescriptorDetails(descriptor));
+        assertEquals(new EditCommand(INDEX_FIRST_ITINERARY, descriptor), command);
+    }
+
+    @Test
+    public void parseCommand_exit() throws Exception {
+        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
+        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
+    }
+
+    @Test
+    public void parseCommand_find() throws Exception {
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        FindCommand command = (FindCommand) parser.parseCommand(
+                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    }
+
+    @Test
+    public void parseCommand_help() throws Exception {
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
+    }
+
+    @Test
+    public void parseCommand_list() throws Exception {
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+    }
+
+    @Test
+    public void parseCommand_unrecognisedInput_throwsParseException() {
+        assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), ()
+            -> parser.parseCommand(""));
+    }
+
+    @Test
+    public void parseCommand_unknownCommand_throwsParseException() {
+        assertThrows(ParseException.class, MESSAGE_UNKNOWN_COMMAND, () -> parser.parseCommand("unknownCommand"));
+    }
+}
