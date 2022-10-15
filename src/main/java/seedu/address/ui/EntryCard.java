fb7119277108b9762ccc70ccf6e353cf82549018
sharmaine1028 <sharmaine1028@gmail.com>
2022-10-15 23:14:15 +0800

    Refactor all files in UI person to entry

---
+++ src/main/java/seedu/address/ui/EntryCard.java
@@ -0,0 +1,73 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.entry.Entry;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class EntryCard extends UiPart<Region> {
+
+    private static final String FXML = "EntryListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on PennyWise level 4</a>
+     */
+
+    public final Entry entry;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label description;
+    @FXML
+    private Label id;
+    @FXML
+    private Label date;
+    @FXML
+    private Label amount;
+    @FXML
+    private FlowPane tags;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public EntryCard(Entry entry, int displayedIndex) {
+        super(FXML);
+        this.entry = entry;
+        id.setText(displayedIndex + ". ");
+        description.setText(entry.getDescription().fullDescription);
+        amount.setText(entry.getAmount().toString());
+        date.setText(entry.getDate().date);
+        tags.getChildren().add(new Label(entry.getTag().tagName));
+        // entry.getTag().stream()
+        //         .sorted(Comparator.comparing(tag -> tag.tagName))
+        //         .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EntryCard)) {
+            return false;
+        }
+
+        // state check
+        EntryCard card = (EntryCard) other;
+        return id.getText().equals(card.id.getText())
+                && entry.equals(card.entry);
+    }
+}
