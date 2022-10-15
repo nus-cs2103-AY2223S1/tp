8260ab4c1284ca0f8141d9d008130826c2e1b954
Justin Peng <justinpeng13@gmail.com>
2022-10-15 18:59:14 +0800

    Fix wildcard imports

--- src/test/java/seedu/guest/model/guest/BillTest.java
+++ src/test/java/seedu/guest/model/guest/BillTest.java
@@ -1,7 +1,11 @@
 package seedu.guest.model.guest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.guest.testutil.Assert.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class BillTest {
