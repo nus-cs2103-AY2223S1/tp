dbf96e3121ebea1542537106281e75a371eb5045
peppapighs <ptk.prasertsuk@gmail.com>
2022-10-15 14:07:54 +0800

    Chore: change bridge association

--- src/main/java/swift/model/bridge/PersonTaskBridge.java
+++ src/main/java/swift/model/bridge/PersonTaskBridge.java
@@ -2,58 +2,33 @@ package swift.model.bridge;
 
 import static swift.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-
-import swift.model.Model;
-import swift.model.person.Person;
 
 /**
  * Represents a many-to-many relationship between Person and Task.
  */
 public class PersonTaskBridge {
-    private final List<UUID> personIds;
-    private final List<UUID> taskIds;
+    private final UUID personId;
+    private final UUID taskId;
 
     /**
-     * Initializes empty PersonTaskBridge.
+     * Initializes PersonTaskBridge with the given personId and taskId.
+     *
+     * @param personId A unique identifier for the person.
+     * @param taskId   A unique identifier for the task.
      */
-    public PersonTaskBridge() {
-        this.personIds = new ArrayList<>();
-        this.taskIds = new ArrayList<>();
+    public PersonTaskBridge(UUID personId, UUID taskId) {
+        requireAllNonNull(personId, taskId);
+        this.personId = personId;
+        this.taskId = taskId;
     }
 
-    /**
-     * Initializes PersonTaskBridge with the given personIds and taskIds.
-     *
-     * @param personIds
-     * @param taskIds
-     */
-    public PersonTaskBridge(List<UUID> personIds, List<UUID> taskIds) {
-        requireAllNonNull(personIds, taskIds);
-        this.personIds = personIds;
-        this.taskIds = taskIds;
+    public UUID getPersonId() {
+        return personId;
     }
 
-    /**
-     * Returns true if the given personId is in the PersonTaskBridge.
-     *
-     * @param person Person to check
-     * @return true if the given personId is in the PersonTaskBridge
-     */
-    public boolean hasPerson(Person person) {
-        return personIds.contains(person.getId());
-    }
-
-    /**
-     * Returns a list of persons associated with this bridge.
-     *
-     * @param model Model to retrieve persons from.
-     * @return List of persons associated with this bridge.
-     */
-    public List<Person> getPersons(Model model) {
-        return model.getAddressBook().getPersonList().filtered(person -> personIds.contains(person.getId()));
+    public UUID getTaskId() {
+        return taskId;
     }
 
     @Override
@@ -63,7 +38,7 @@ public class PersonTaskBridge {
         }
         if (obj instanceof PersonTaskBridge) {
             PersonTaskBridge other = (PersonTaskBridge) obj;
-            return personIds.equals(other.personIds) && taskIds.equals(other.taskIds);
+            return personId.equals(other.personId) && taskId.equals(other.taskId);
         }
         return false;
     }
