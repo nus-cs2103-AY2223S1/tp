0f3702eb877765be68ca274fcdec51dd3827500d
Tex Tang <tex_tang@outlook.com>
2022-10-15 23:22:57 +0800

    Storage: Fix code style

--- src/main/java/seedu/address/storage/JsonAdaptedContact.java
+++ src/main/java/seedu/address/storage/JsonAdaptedContact.java
@@ -2,6 +2,7 @@ package seedu.address.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.contact.Contact;
 import seedu.address.model.person.contact.ContactType;
