2063d72bfdf5f809fbf068a83de83a16825698e8
Effixion <syt1702@gmail.com>
2022-10-15 15:34:14 +0800

    Add tests for Module and UniqueModuleList

--- src/test/java/seedu/address/model/module/ModuleTest.java
+++ src/test/java/seedu/address/model/module/ModuleTest.java
@@ -1,4 +1,92 @@
 package seedu.address.model.module;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LECTURE_DETAILS_CS2100;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULE_CODE_CS2100;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TUTORIAL_DETAILS_CS2100;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ZOOM_LINK_CS2100;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalModules.CS2100;
+import static seedu.address.testutil.TypicalModules.CS2103T;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.testutil.ModuleBuilder;
+
 public class ModuleTest {
+
+    @Test
+    public void asObservableList_modifyList_throwsUnsupportedOperationException() {
+        Module module = new ModuleBuilder().build();
+        assertThrows(UnsupportedOperationException.class, () -> module.getAssignmentDetails().remove(0));
+    }
+
+    @Test
+    public void isSameModule() {
+        // same object -> returns true
+        assertTrue(CS2103T.isSameModule(CS2103T));
+
+        // null -> returns false
+        assertFalse(CS2103T.isSameModule(null));
+
+        // same name, all other attributes different -> returns true
+        Module editedCS2103T = new ModuleBuilder(CS2103T)
+                .withLectureDetails(VALID_LECTURE_DETAILS_CS2100)
+                .withTutorialDetails(VALID_TUTORIAL_DETAILS_CS2100)
+                .withZoomLink(VALID_ZOOM_LINK_CS2100)
+                .build();
+        assertTrue(CS2103T.isSameModule(editedCS2103T));
+
+        // different name, all other attributes same -> returns false
+        editedCS2103T = new ModuleBuilder(CS2103T).withModuleCode(VALID_MODULE_CODE_CS2100).build();
+        assertFalse(CS2103T.isSameModule(editedCS2103T));
+
+        // name differs in case, all other attributes same -> returns false
+        Module editedCS2100 = new ModuleBuilder(CS2100).withModuleCode(VALID_MODULE_CODE_CS2100.toLowerCase()).build();
+        assertFalse(CS2100.isSameModule(editedCS2100));
+
+        // name has trailing spaces, all other attributes same -> returns false
+        String nameWithTrailingSpaces = VALID_MODULE_CODE_CS2100 + " ";
+        editedCS2100 = new ModuleBuilder(CS2100).withModuleCode(nameWithTrailingSpaces).build();
+        assertFalse(CS2100.isSameModule(editedCS2100));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Module CS2103TCopy = new ModuleBuilder(CS2103T).build();
+        assertTrue(CS2103T.equals(CS2103TCopy));
+
+        // same object -> returns true
+        assertTrue(CS2103T.equals(CS2103T));
+
+        // null -> returns false
+        assertFalse(CS2103T.equals(null));
+
+        // different type -> returns false
+        assertFalse(CS2103T.equals(5));
+
+        // different Module -> returns false
+        assertFalse(CS2103T.equals(CS2100));
+
+        // different module code -> returns false
+        Module editedCS2103T = new ModuleBuilder(CS2103T).withModuleCode(VALID_MODULE_CODE_CS2100).build();
+        assertFalse(CS2103T.equals(editedCS2103T));
+
+        // different lecture details -> returns false
+        editedCS2103T = new ModuleBuilder(CS2103T).withLectureDetails(VALID_LECTURE_DETAILS_CS2100).build();
+        assertFalse(CS2103T.equals(editedCS2103T));
+
+        // different tutorial details -> returns false
+        editedCS2103T = new ModuleBuilder(CS2103T).withTutorialDetails(VALID_TUTORIAL_DETAILS_CS2100).build();
+        assertFalse(CS2103T.equals(editedCS2103T));
+
+        // different zoom link -> returns false
+        editedCS2103T = new ModuleBuilder(CS2103T).withZoomLink(VALID_ZOOM_LINK_CS2100).build();
+        assertFalse(CS2103T.equals(editedCS2103T));
+
+        // different assignment details -> returns false
+        editedCS2103T = new ModuleBuilder(CS2103T).withAssignmentDetails("easy").build();
+        assertFalse(CS2103T.equals(editedCS2103T));
+    }
 }
--- src/test/java/seedu/address/model/module/UniqueModuleListTest.java
+++ src/test/java/seedu/address/model/module/UniqueModuleListTest.java
@@ -1,4 +1,174 @@
 package seedu.address.model.module;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LECTURE_DETAILS_CS2100;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TUTORIAL_DETAILS_CS2100;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalModules.CS2100;
+import static seedu.address.testutil.TypicalModules.CS2103T;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.module.exceptions.DuplicateModuleException;
+import seedu.address.model.module.exceptions.ModuleNotFoundException;
+import seedu.address.testutil.ModuleBuilder;
+
 public class UniqueModuleListTest {
+
+    private final UniqueModuleList uniqueModuleList = new UniqueModuleList();
+
+    @Test
+    public void contains_nullModule_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.contains(null));
+    }
+
+    @Test
+    public void contains_moduleNotInList_returnsFalse() {
+        assertFalse(uniqueModuleList.contains(CS2103T));
+    }
+
+    @Test
+    public void contains_moduleInList_returnsTrue() {
+        uniqueModuleList.add(CS2103T);
+        assertTrue(uniqueModuleList.contains(CS2103T));
+    }
+
+    @Test
+    public void contains_moduleWithSameIdentityFieldsInList_returnsTrue() {
+        uniqueModuleList.add(CS2103T);
+        Module editedCs2103t = new ModuleBuilder(CS2103T)
+                .withLectureDetails(VALID_LECTURE_DETAILS_CS2100)
+                .withTutorialDetails(VALID_TUTORIAL_DETAILS_CS2100)
+                .build();
+        assertTrue(uniqueModuleList.contains(editedCs2103t));
+    }
+
+    @Test
+    public void add_nullModule_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.add(null));
+    }
+
+    @Test
+    public void add_duplicateModule_throwsDuplicateModuleException() {
+        uniqueModuleList.add(CS2103T);
+        assertThrows(DuplicateModuleException.class, () -> uniqueModuleList.add(CS2103T));
+    }
+
+    @Test
+    public void setModule_nullTargetModule_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.setModule(null, CS2103T));
+    }
+
+    @Test
+    public void setModule_nullEditedModule_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.setModule(CS2103T, null));
+    }
+
+    @Test
+    public void setModule_targetModuleNotInList_throwsModuleNotFoundException() {
+        assertThrows(ModuleNotFoundException.class, () -> uniqueModuleList.setModule(CS2103T, CS2103T));
+    }
+
+    @Test
+    public void setModule_editedModuleIsSameModule_success() {
+        uniqueModuleList.add(CS2103T);
+        uniqueModuleList.setModule(CS2103T, CS2103T);
+        UniqueModuleList expectedUniqueModuleList = new UniqueModuleList();
+        expectedUniqueModuleList.add(CS2103T);
+        assertEquals(expectedUniqueModuleList, uniqueModuleList);
+    }
+
+    @Test
+    public void setModule_editedModuleHasSameIdentity_success() {
+        uniqueModuleList.add(CS2103T);
+        Module editedCS2103T = new ModuleBuilder(CS2103T)
+                .withLectureDetails(VALID_LECTURE_DETAILS_CS2100)
+                .withTutorialDetails(VALID_TUTORIAL_DETAILS_CS2100)
+                .build();
+        uniqueModuleList.setModule(CS2103T, editedCS2103T);
+        UniqueModuleList expectedUniqueModuleList = new UniqueModuleList();
+        expectedUniqueModuleList.add(editedCS2103T);
+        assertEquals(expectedUniqueModuleList, uniqueModuleList);
+    }
+
+    @Test
+    public void setModule_editedModuleHasDifferentIdentity_success() {
+        uniqueModuleList.add(CS2103T);
+        uniqueModuleList.setModule(CS2103T, CS2100);
+        UniqueModuleList expectedUniqueModuleList = new UniqueModuleList();
+        expectedUniqueModuleList.add(CS2100);
+        assertEquals(expectedUniqueModuleList, uniqueModuleList);
+    }
+
+    @Test
+    public void setModule_editedModuleHasNonUniqueIdentity_throwsDuplicateModuleException() {
+        uniqueModuleList.add(CS2103T);
+        uniqueModuleList.add(CS2100);
+        assertThrows(DuplicateModuleException.class, () -> uniqueModuleList.setModule(CS2103T, CS2100));
+    }
+
+    @Test
+    public void remove_nullModule_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.remove(null));
+    }
+
+    @Test
+    public void remove_ModuleDoesNotExist_throwsModuleNotFoundException() {
+        assertThrows(ModuleNotFoundException.class, () -> uniqueModuleList.remove(CS2103T));
+    }
+
+    @Test
+    public void remove_existingModule_removesModule() {
+        uniqueModuleList.add(CS2103T);
+        uniqueModuleList.remove(CS2103T);
+        UniqueModuleList expectedUniqueModuleList = new UniqueModuleList();
+        assertEquals(expectedUniqueModuleList, uniqueModuleList);
+    }
+
+    @Test
+    public void setModules_nullUniqueModuleList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.setModules((UniqueModuleList) null));
+    }
+
+    @Test
+    public void setModules_uniqueModuleList_replacesOwnListWithProvidedUniqueModuleList() {
+        uniqueModuleList.add(CS2103T);
+        UniqueModuleList expectedUniqueModuleList = new UniqueModuleList();
+        expectedUniqueModuleList.add(CS2100);
+        uniqueModuleList.setModules(expectedUniqueModuleList);
+        assertEquals(expectedUniqueModuleList, uniqueModuleList);
+    }
+
+    @Test
+    public void setModules_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueModuleList.setModules((List<Module>) null));
+    }
+
+    @Test
+    public void setModules_list_replacesOwnListWithProvidedList() {
+        uniqueModuleList.add(CS2103T);
+        List<Module> ModuleList = Collections.singletonList(CS2100);
+        uniqueModuleList.setModules(ModuleList);
+        UniqueModuleList expectedUniqueModuleList = new UniqueModuleList();
+        expectedUniqueModuleList.add(CS2100);
+        assertEquals(expectedUniqueModuleList, uniqueModuleList);
+    }
+
+    @Test
+    public void setModules_listWithDuplicateModules_throwsDuplicateModuleException() {
+        List<Module> listWithDuplicateModules = Arrays.asList(CS2103T, CS2103T);
+        assertThrows(DuplicateModuleException.class, () -> uniqueModuleList.setModules(listWithDuplicateModules));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+                -> uniqueModuleList.asUnmodifiableObservableList().remove(0));
+    }
 }
