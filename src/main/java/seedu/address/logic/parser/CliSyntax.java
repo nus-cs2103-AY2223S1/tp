07c74e53c16bc57b5693bf5e078c74bd8f68f30f
wrewsama <andrewlzs512@gmail.com>
2022-10-15 22:16:25 +0800

    logic: add trailing whitespace to CliSyntax prefixes to fix MESSAGE_USAGE formatting

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,12 +6,12 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("/name");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("/address");
-    public static final Prefix PREFIX_TAG = new Prefix("/tag");
-    public static final Prefix PREFIX_EMAIL = new Prefix("/email");
-    public static final Prefix PREFIX_PHONE = new Prefix("/phone");
-    public static final Prefix PREFIX_SLACK = new Prefix("/slack");
-    public static final Prefix PREFIX_TELEGRAM = new Prefix("/telegram");
+    public static final Prefix PREFIX_NAME = new Prefix("/name ");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("/address ");
+    public static final Prefix PREFIX_TAG = new Prefix("/tag ");
+    public static final Prefix PREFIX_EMAIL = new Prefix("/email ");
+    public static final Prefix PREFIX_PHONE = new Prefix("/phone ");
+    public static final Prefix PREFIX_SLACK = new Prefix("/slack ");
+    public static final Prefix PREFIX_TELEGRAM = new Prefix("/telegram ");
 
 }
