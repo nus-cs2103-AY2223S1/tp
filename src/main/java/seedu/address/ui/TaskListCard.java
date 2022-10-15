6c0e4d926f4305393cd5f814aeda2835425569f6
Yeo Jun Jie <54196980+yeojunjie@users.noreply.github.com>
2022-10-15 17:45:00 +0800

    Show task deadline and completion in the UI (#92)

--- src/main/java/seedu/address/ui/TaskListCard.java
+++ src/main/java/seedu/address/ui/TaskListCard.java
@@ -2,8 +2,8 @@ package seedu.address.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.address.model.task.Task;
 
 /**
@@ -13,24 +13,20 @@ public class TaskListCard extends UiPart<Region> {
 
     private static final String FXML = "TaskListCard.fxml";
 
-    /**
-     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
-     * As a consequence, UI elements' variable names cannot be set to such keywords
-     * or an exception will be thrown by JavaFX during runtime.
-     *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
-     */
-
     public final Task task;
 
     @FXML
-    private HBox cardPane;
+    private VBox cardPane;
     @FXML
     private Label id;
     @FXML
     private Label name;
     @FXML
     private Label description;
+    @FXML
+    private Label deadline;
+    @FXML
+    private Label completion;
 
     /**
      * Creates a {@code TaskListCard} with the given {@code Task} and index to display.
@@ -38,9 +34,11 @@ public class TaskListCard extends UiPart<Region> {
     public TaskListCard(Task task, int displayedIndex) {
         super(FXML);
         this.task = task;
-        id.setText(String.valueOf(displayedIndex));
-        name.setText(displayedIndex + ". " + task.getTaskName());
-        description.setText(task.getTaskDescription().toString());
+        id.setText(displayedIndex + ".");
+        name.setText(String.valueOf(task.getTaskName()));
+        description.setText(String.valueOf(task.getTaskDescription()));
+        deadline.setText(String.format("Due by %s", task.getTaskDeadline()));
+        completion.setText("10% completed (3/30 students)"); // Dummy text.
     }
 
     @Override
