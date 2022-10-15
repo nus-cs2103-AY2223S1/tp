585b70af825ce984f97d6fbd214cca4dd3e46021
e1010101 <ezratio15195@gmail.com>
2022-10-15 23:53:58 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,13 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENTCLASS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
 import seedu.address.logic.commands.FindNameCommand;
@@ -16,7 +12,6 @@ import seedu.address.model.person.FindCommandPredicate;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.StudentClass;
 import seedu.address.model.person.subject.Subject;
-import seedu.address.model.person.subject.SubjectHandler;
 
 /**
  * Parses input arguments and creates a new FindCommand object
