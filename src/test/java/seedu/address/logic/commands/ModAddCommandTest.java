607b00d89bd27c9d09b00b0721c1f57635873eef
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2022-10-16 21:31:50 +0800

    Add duplicate mods test for `ModAddCommand`

--- src/test/java/seedu/address/logic/commands/ModAddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ModAddCommandTest.java
@@ -32,16 +32,25 @@ public class ModAddCommandTest {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     }
 
+    /**
+     * Test constructor to throw an exception when null index is provided.
+     */
     @Test
     public void constructor_nullIndex_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new ModAddCommand(null, FXCollections.observableArrayList()));
     }
 
+    /**
+     * Test constructor to throw an exception when null mods is provided.
+     */
     @Test
     public void constructor_nullMods_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new ModAddCommand(INDEX_FIRST_PERSON, null));
     }
 
+    /**
+     * Test the {@code execute} method to add mods to a person.
+     */
     @Test
     public void execute_saveToModels_success() throws CommandException {
 
@@ -66,8 +75,39 @@ public class ModAddCommandTest {
         assertEquals(editedPerson, editedPersonExpected);
     }
 
+    /**
+     * Test the {@code execute} method to add a pre-existing mod.
+     */
     @Test
-    public void execute_indexOutOfBounds_throwsCommandException() throws CommandException {
+    public void execute_saveDuplicateMods_success() throws CommandException {
+
+        // adds a test person to model
+        Person toAdd = new PersonBuilder(BOB).withMods(VALID_MOD_CS2100.getModName()).build();
+        model.addPerson(toAdd);
+
+        // execute ModAddCommand on the test person with existing mod
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
+        ModAddCommand commandToExecute = new ModAddCommand(indexLastPerson,
+                FXCollections.singletonObservableList(VALID_MOD_CS2100));
+        CommandResult commandResult = commandToExecute.execute(model);
+        // get the edited person from ModAddCommand
+        Person editedPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
+
+        // expected edited person
+        Person editedPersonExpected = new PersonBuilder(BOB)
+                .withMods(VALID_MOD_CS2100.getModName())
+                .build();
+
+        assertEquals(String.format(ModAddCommand.MESSAGE_SUCCESS, editedPerson), commandResult.getFeedbackToUser());
+        assertEquals(editedPerson, editedPersonExpected);
+    }
+
+    /**
+     * Test the {@code execute} method to throw an exception when index provided
+     * is out of bounds.
+     */
+    @Test
+    public void execute_indexOutOfBounds_throwsCommandException() {
         Index indexOutOfBounds = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
         ModAddCommand invalidCommand = new ModAddCommand(indexOutOfBounds,
                 FXCollections.singletonObservableList(VALID_MOD_CS2100));
