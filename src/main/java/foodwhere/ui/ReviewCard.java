e16e1850616233e7580ec4eeb02fc63823d14055
Hikoya <hikoyanamikaze@gmail.com>
2022-10-16 15:17:54 +0800

    Add new stars for Rating user interface

--- src/main/java/foodwhere/ui/ReviewCard.java
+++ src/main/java/foodwhere/ui/ReviewCard.java
@@ -1,11 +1,14 @@
 package foodwhere.ui;
 
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import foodwhere.model.review.Review;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 
@@ -40,6 +43,8 @@ public class ReviewCard extends UiPart<Region> {
     @FXML
     private Label rating;
     @FXML
+    private ImageView ratingIcon;
+    @FXML
     private Label tags;
     @FXML
     private Label tagsLabel;
@@ -53,8 +58,9 @@ public class ReviewCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(review.getName().fullName);
         date.setText(review.getDate().value);
-        rating.setText(String.valueOf(review.getRating().value));
         content.setText(review.getContent().value);
+
+        setRatingIcon();
 
         if (!review.getTags().isEmpty()) {
             String assigneesNames = review.getTags()
@@ -67,6 +73,45 @@ public class ReviewCard extends UiPart<Region> {
         } else {
             tagsLabel.setText("");
         }
+    }
+
+    /**
+     * Sets the image of ratingIcon {@code ImageView} and the text of ratings {@code Label}.
+     */
+    private void setRatingIcon() {
+        Image ratingIconImage;
+
+        Integer val = review.getRating().value;
+        switch (val) {
+        case 0:
+            ratingIconImage = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/images/stars_0.png")));
+            break;
+        case 1:
+            ratingIconImage = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/images/stars_1.png")));
+            break;
+        case 2:
+            ratingIconImage = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/images/stars_2.png")));
+            break;
+        case 3:
+            ratingIconImage = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/images/stars_3.png")));
+            break;
+        case 4:
+            ratingIconImage = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/images/stars_4.png")));
+            break;
+        case 5:
+            ratingIconImage = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/images/stars_5.png")));
+            break;
+        default:
+            throw new RuntimeException("Invalid rating");
+        }
+
+        ratingIcon.setImage(ratingIconImage);
     }
 
     @Override
