fc2d4e40d4e679ca4764f9f63cfdd969c2eac369
Siau Wee Goh <siauwee@u.nus.edu>
2022-10-16 16:53:13 +0800

    Update src/main/java/seedu/address/logic/commands/ListAllCommand.java

--- src/main/java/seedu/address/logic/commands/ListAllCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAllCommand.java
@@ -10,7 +10,7 @@ import seedu.address.model.Model;
 public class ListAllCommand extends Command {
     public static final String COMMAND_WORD = "listAll";
 
-    public static final String MESSAGE_SUCCESS = "Listed all items in Salesy!";
+    public static final String MESSAGE_SUCCESS = "Listed all entities in Salesy!";
 
     @Override
     public CommandResult execute(Model model) {
