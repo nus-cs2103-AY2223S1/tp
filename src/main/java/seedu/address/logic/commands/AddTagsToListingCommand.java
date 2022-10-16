91e0c395034f4f3d71f047b121b83d9a1db10438
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2022-10-17 00:55:02 +0800

    Fix  Checkstyle

--- src/main/java/seedu/address/logic/commands/AddTagsToListingCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTagsToListingCommand.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.commands;
 
-import java.util.Set;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Set;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
--- src/main/java/seedu/address/logic/parser/AddTagsToListingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddTagsToListingCommandParser.java
@@ -1,15 +1,15 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LISTING_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
 import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddTagsToListingCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
-
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_LISTING_ID;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 /**
  * Parses input arguments and creates a new AddTagsToListingCommand object.
