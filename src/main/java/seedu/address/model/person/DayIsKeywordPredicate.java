43afb5e82f332073f36166dd341a367a398cfdf7
phyliciachristel <e0775641@u.nus.edu>
2022-10-16 17:24:50 +0800

    Update class with skeleton code

--- src/main/java/seedu/address/model/person/DayIsKeywordPredicate.java
+++ src/main/java/seedu/address/model/person/DayIsKeywordPredicate.java
@@ -1,2 +1,22 @@
-package seedu.address.model;public class DayIsKeywordPredicate {
+package seedu.address.model.person;
+
+import java.util.function.Predicate;
+
+public class DayIsKeywordPredicate implements Predicate<Person> {
+    private final String keyword;
+
+    public DayIsKeywordPredicate(String keyword) {
+        this.keyword = keyword;
+    }
+    @Override
+    public boolean test(Person person) {
+        return keyword.equalsIgnoreCase(""); // person session day
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof DayIsKeywordPredicate
+                && keyword.equals(((DayIsKeywordPredicate) other).keyword));
+    }
 }
