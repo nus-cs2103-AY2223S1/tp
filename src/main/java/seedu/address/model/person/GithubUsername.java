50742b5180cb24efcce13fb5ab3b0bbcf959cb88
Jonaspng <jonaspng@hotmail.com>
2022-10-16 15:56:38 +0800

    Update github field error msg

--- src/main/java/seedu/address/model/person/GithubUsername.java
+++ src/main/java/seedu/address/model/person/GithubUsername.java
@@ -10,9 +10,10 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class GithubUsername {
 
     public static final String MESSAGE_CONSTRAINTS = "Github username must follow the following rules: \n"
-            + "Github username cannot have multiple consecutive hyphens.\n"
-            + "Github username cannot begin or end with a hyphen.\n"
-            + "Maximum is 39 characters.";
+            + " - Github username cannot be blank.\n"
+            + " - Github username cannot have multiple consecutive hyphens.\n"
+            + " - Github username cannot begin or end with a hyphen.\n"
+            + " - Maximum length is 39 characters.";
 
     public static final String DEFAULT_USERNAME = "";
 
