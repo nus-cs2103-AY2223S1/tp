322fcd30fbd80a48f0fd0d460376cfb6c5272ff8
Png Yi Wei Jonas <60882425+Jonaspng@users.noreply.github.com>
2022-10-16 23:12:34 +0800

    Update src/main/java/seedu/address/logic/commands/GithubCommand.java
    Co-authored-by: Gareth Ong <81297805+GarethOng@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/GithubCommand.java
+++ src/main/java/seedu/address/logic/commands/GithubCommand.java
@@ -22,7 +22,7 @@ public class GithubCommand extends Command {
 
     public static final String COMMAND_WORD = "github";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Opens up Github Profile page associated to chosen "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Opens up Github Profile page associated to index specified "
             + "address in default browser.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
