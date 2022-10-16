de3b48b744b2a567465e310fd9da581ab1f8aedf
zicot <zicotjia@gmail.com>
2022-10-16 23:50:30 +0800

    Modify parser to handle exception correctly

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
@@ -9,7 +8,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.DeleteTeamCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
