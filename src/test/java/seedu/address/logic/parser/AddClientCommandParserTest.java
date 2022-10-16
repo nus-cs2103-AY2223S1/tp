8730268a113b6830f34e4dabf7424f1d7a3a4315
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-16 22:50:38 +0800

    Add more tests

--- src/test/java/seedu/address/logic/parser/AddClientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddClientCommandParserTest.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.commands.CommandTestUtil.INVALID_EMAIL_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_PHONE_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.PROJECT_DESC_EMPTY_PROJECT;
 import static seedu.address.logic.commands.CommandTestUtil.PROJECT_DESC_PROJECT;
@@ -18,12 +17,14 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REPOSITORY;
 import static seedu.address.logic.commands.client.AddClientCommand.MESSAGE_ADD_CLIENT_USAGE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.client.AddClientCommand;
+import seedu.address.logic.commands.project.AddProjectCommand;
 import seedu.address.model.Deadline;
 import seedu.address.model.Name;
 import seedu.address.model.client.Client;
@@ -38,26 +39,99 @@ import seedu.address.model.project.Repository;
 
 public class AddClientCommandParserTest {
     private ClientCommandParser parser = new ClientCommandParser();
+
     @Test
-    public void parse_projectNotAdded_failure() {
+    public void parse_compulsoryAndOptionalFieldsPresent_success() {
+        Client client = new Client(new Name(VALID_NAME_AMY), new ClientPhone(VALID_PHONE_AMY),
+                new ClientEmail(VALID_EMAIL_AMY), new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
+        //compulsory and optional fields
+        assertParseSuccess(parser, AddClientCommand.COMMAND_FLAG, NAME_DESC_AMY
+                + PHONE_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_PROJECT, new AddClientCommand(client,
+                Project.EmptyProject.EMPTY_PROJECT));
+    }
+
+    @Test
+    public void parse_optionalFieldsMissing_success() {
+
+        Client client = new Client(new Name(VALID_NAME_AMY), ClientPhone.EmptyClientPhone.EMPTY_PHONE,
+                ClientEmail.EmptyEmail.EMPTY_EMAIL, new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
+
+        // only name
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG, NAME_DESC_AMY + PROJECT_DESC_EMPTY_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG, NAME_DESC_AMY + PROJECT_DESC_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+    }
+
+    @Test
+    public void parse_optionalEmailMissing_success() {
+
+        Client client = new Client(new Name(VALID_NAME_AMY), new ClientPhone(VALID_PHONE_AMY),
+                ClientEmail.EmptyEmail.EMPTY_EMAIL, new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
+
+        // email missing
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + PHONE_DESC_AMY + PROJECT_DESC_EMPTY_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + PHONE_DESC_AMY + PROJECT_DESC_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+    }
+
+    @Test
+    public void parse_optionalPhoneMissing_success() {
+
+        Client client = new Client(new Name(VALID_NAME_AMY), ClientPhone.EmptyClientPhone.EMPTY_PHONE,
+                new ClientEmail(VALID_EMAIL_AMY), new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
+
+        // phone missing
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_EMPTY_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+    }
+
+    @Test
+    public void parse_fieldsInAnyOrder_success() {
+
+        Client client = new Client(new Name(VALID_NAME_AMY), new ClientPhone(VALID_PHONE_AMY),
+                new ClientEmail(VALID_EMAIL_AMY), new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
+
+
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + EMAIL_DESC_AMY + PHONE_DESC_AMY + PROJECT_DESC_EMPTY_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                PHONE_DESC_AMY + NAME_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_PROJECT,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+        assertParseSuccess(parser, AddProjectCommand.COMMAND_FLAG,
+                PROJECT_DESC_PROJECT + PHONE_DESC_AMY + EMAIL_DESC_AMY + NAME_DESC_AMY,
+                new AddClientCommand(client, Project.EmptyProject.EMPTY_PROJECT));
+
+    }
+
+
+    @Test
+    public void parse_clientAdded_success() {
         // all valid fields
         String expected = "No project with this project Id";
         Client client = new Client(new Name(VALID_NAME_AMY), new ClientPhone(VALID_PHONE_AMY),
                 new ClientEmail(VALID_EMAIL_AMY), new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
         Project project = new Project(new Name(VALID_NAME_BOB), new Repository(VALID_REPOSITORY),
                 new Deadline(VALID_DEADLINE), Client.EmptyClient.EMPTY_CLIENT, new ArrayList<>(), new ProjectId(1));
-        assertParseFailure(parser, AddClientCommand.COMMAND_FLAG, NAME_DESC_AMY
-                + PHONE_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_PROJECT, expected);
-    }
-
-    @Test
-    public void parse_emptyProject_failure() {
-        String expected = "No project with this project Id";
-        Client client = new Client(new Name(VALID_NAME_BOB), ClientPhone.EmptyClientPhone.EMPTY_PHONE,
-                ClientEmail.EmptyEmail.EMPTY_EMAIL, new ArrayList<>(), new ClientId(UniqueClientList.generateId()));
-        // only name
-        assertParseFailure(parser, AddClientCommand.COMMAND_FLAG, NAME_DESC_BOB + PROJECT_DESC_EMPTY_PROJECT,
-               expected);
+        assertParseSuccess(parser, AddClientCommand.COMMAND_FLAG, NAME_DESC_AMY
+                + PHONE_DESC_AMY + EMAIL_DESC_AMY + PROJECT_DESC_PROJECT, new AddClientCommand(client, project));
     }
 
     @Test
@@ -85,9 +159,21 @@ public class AddClientCommandParserTest {
     }
 
     @Test
-    public void parse_invalidName_failure() {
+    public void parse_invalidFields_failure() {
         // invalid name
         assertParseFailure(parser, AddClientCommand.COMMAND_FLAG, INVALID_NAME_DESC + PROJECT_DESC_PROJECT,
                 Name.MESSAGE_CONSTRAINTS);
+
+        //invalid email
+        assertParseFailure(parser, AddClientCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + INVALID_EMAIL_DESC + PROJECT_DESC_PROJECT,
+                ClientEmail.MESSAGE_CONSTRAINTS);
+
+        //invalid phone
+        assertParseFailure(parser, AddClientCommand.COMMAND_FLAG,
+                NAME_DESC_AMY + INVALID_PHONE_DESC + PROJECT_DESC_PROJECT,
+                ClientPhone.MESSAGE_CONSTRAINTS);
+
     }
+
 }
