698f2b4e53b575189b46f46ea5f8bb65fb452d65
Chee Jer Hong <e0407708@u.nus.edu>
2022-10-16 17:19:17 +0800

    Include Interview and its fields in Json conversion

--- src/main/java/seedu/application/storage/JsonAdaptedApplication.java
+++ src/main/java/seedu/application/storage/JsonAdaptedApplication.java
@@ -1,5 +1,7 @@
 package seedu.application.storage;
 
+import java.util.Optional;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -10,6 +12,11 @@ import seedu.application.model.application.Contact;
 import seedu.application.model.application.Date;
 import seedu.application.model.application.Email;
 import seedu.application.model.application.Position;
+import seedu.application.model.application.interview.Interview;
+import seedu.application.model.application.interview.InterviewDate;
+import seedu.application.model.application.interview.InterviewTime;
+import seedu.application.model.application.interview.Location;
+import seedu.application.model.application.interview.Round;
 
 /**
  * Jackson-friendly version of {@link Application}.
@@ -18,15 +25,20 @@ class JsonAdaptedApplication {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Application's %s field is missing!";
 
+    //Fields in Application
     private final String company;
     private final String contact;
     private final String email;
     private final String position;
     private final String date;
+    //Fields in Interview
+    private final String round;
+    private final String interviewDate;
+    private final String interviewTime;
+    private final String location;
 
-    /**
-     * Constructs a {@code JsonAdaptedApplication} with the given application details.
-     */
+
+    /*
     @JsonCreator
     public JsonAdaptedApplication(@JsonProperty("company") String company, @JsonProperty("contact") String contact,
                                   @JsonProperty("email") String email, @JsonProperty("position") String position,
@@ -36,6 +48,35 @@ class JsonAdaptedApplication {
         this.email = email;
         this.position = position;
         this.date = date;
+
+        this.round = "";
+        this.interviewDate = "";
+        this.interviewTime = "";
+        this.location = "";
+    }
+
+     */
+
+
+    /**
+     * Constructs a {@code JsonAdaptedApplication} with the given application details with non-empty Interview.
+     */
+    @JsonCreator
+    public JsonAdaptedApplication(@JsonProperty("company") String company, @JsonProperty("contact") String contact,
+                                  @JsonProperty("email") String email, @JsonProperty("position") String position,
+                                  @JsonProperty("date") String date, @JsonProperty("round") String round,
+                                  @JsonProperty("interviewDate") String interviewDate,
+                                  @JsonProperty("interviewTime") String interviewTime,
+                                  @JsonProperty("location") String location) {
+        this.company = company;
+        this.contact = contact;
+        this.email = email;
+        this.position = position;
+        this.date = date;
+        this.round = round;
+        this.interviewDate = interviewDate;
+        this.interviewTime = interviewTime;
+        this.location = location;
     }
 
     /**
@@ -47,6 +88,19 @@ class JsonAdaptedApplication {
         email = source.getEmail().value;
         position = source.getPosition().value;
         date = source.getDate().value.toString();
+
+        Optional<Interview> interview = source.getInterview();
+        if (interview.isEmpty()) {
+            this.round = "";
+            this.interviewDate = "";
+            this.interviewTime = "";
+            this.location = "";
+        } else {
+            this.round = interview.get().getRound().value;
+            this.interviewDate = interview.get().getInterviewDate().value.toString();
+            this.interviewTime = interview.get().getInterviewTime().toCommandString();
+            this.location = interview.get().getLocation().value;
+        }
     }
 
     /**
@@ -96,7 +150,57 @@ class JsonAdaptedApplication {
         }
         final Date modelDate = new Date(date);
 
-        return new Application(modelCompany, modelContact, modelEmail, modelPosition, modelDate);
+        Application modelApplication = new Application(modelCompany, modelContact, modelEmail, modelPosition,
+                modelDate);
+        if (round.equals("") && interviewDate.equals("") && interviewTime.equals("") && location.equals("")) {
+            return modelApplication;
+        } else {
+            Interview modelInterview = toModelTypeInterview();
+            return new Application(modelApplication, modelInterview);
+        }
     }
 
+    /**
+     * Converts this Jackson-friendly adapted interview object into the model's {@code Interview} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted interview.
+     */
+    public Interview toModelTypeInterview() throws IllegalValueException {
+        if (round == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Round.class.getSimpleName()));
+        }
+        if (!Round.isValidRound(round)) {
+            throw new IllegalValueException(Round.MESSAGE_CONSTRAINTS);
+        }
+        final Round modelRound = new Round(round);
+
+        if (interviewDate == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    InterviewDate.class.getSimpleName()));
+        }
+        if (!InterviewDate.isValidDate(interviewDate)) {
+            throw new IllegalValueException(InterviewDate.MESSAGE_CONSTRAINTS);
+        }
+        final InterviewDate modelInterviewDate = new InterviewDate(interviewDate);
+
+        if (interviewTime == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    InterviewTime.class.getSimpleName()));
+        }
+        if (!InterviewTime.isValidTime(interviewTime)) {
+            throw new IllegalValueException(InterviewTime.MESSAGE_CONSTRAINTS);
+        }
+        final InterviewTime modelInterviewTime = new InterviewTime(interviewTime);
+
+        if (location == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Location.class.getSimpleName()));
+        }
+        if (!Location.isValidLocation(location)) {
+            throw new IllegalValueException(Location.MESSAGE_CONSTRAINTS);
+        }
+        final Location modelLocation = new Location(location);
+
+        return new Interview(modelRound, modelInterviewDate, modelInterviewTime, modelLocation);
+    }
 }
