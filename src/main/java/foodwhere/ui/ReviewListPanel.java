59f87b7e2ac19f202add534a5ce2fd747e8fb182
Hikoya <hikoyanamikaze@gmail.com>
2022-10-16 18:24:24 +0800

    Modify user interface to include copy function

--- src/main/java/foodwhere/ui/ReviewListPanel.java
+++ src/main/java/foodwhere/ui/ReviewListPanel.java
@@ -5,9 +5,14 @@ import java.util.logging.Logger;
 import foodwhere.commons.core.LogsCenter;
 import foodwhere.model.review.Review;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.Region;
 
 /**
@@ -28,6 +33,46 @@ public class ReviewListPanel extends UiPart<Region> {
         super(FXML);
         reviewListView.setItems(reviewList);
         reviewListView.setCellFactory(listView -> new ReviewListViewCell());
+    }
+
+    /**
+     * Handles the activity when the user clicks on the review in the list
+     *
+     * Adapted code from https://stackoverflow.com/questions/20635192/how-to-create-popup-menu
+     */
+    @FXML
+    public void handleMouseClicked() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+
+        final ContextMenu contextMenu = new ContextMenu();
+        MenuItem copyReviewName = new MenuItem("Copy Name");
+        MenuItem copyReviewDate = new MenuItem("Copy Date");
+        MenuItem copyReviewContent = new MenuItem("Copy Content");
+        MenuItem copyReviewTag = new MenuItem("Copy Tag");
+        contextMenu.getItems().addAll(copyReviewName, copyReviewDate, copyReviewContent, copyReviewTag);
+
+        reviewListView.setContextMenu(contextMenu);
+
+        copyReviewName.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(reviewListView.getSelectionModel().getSelectedItem().getName().toString());
+            clipboard.setContent(content);
+        });
+
+        copyReviewDate.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(reviewListView.getSelectionModel().getSelectedItem().getDate().toString());
+            clipboard.setContent(content);
+        });
+
+        copyReviewContent.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(reviewListView.getSelectionModel().getSelectedItem().getContent().toString());
+            clipboard.setContent(content);
+        });
+
+        copyReviewTag.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(reviewListView.getSelectionModel().getSelectedItem().getTags().toString());
+            clipboard.setContent(content);
+        });
     }
 
     /**
--- src/main/java/foodwhere/ui/StallListPanel.java
+++ src/main/java/foodwhere/ui/StallListPanel.java
@@ -5,9 +5,14 @@ import java.util.logging.Logger;
 import foodwhere.commons.core.LogsCenter;
 import foodwhere.model.stall.Stall;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.Region;
 
 /**
@@ -27,6 +32,40 @@ public class StallListPanel extends UiPart<Region> {
         super(FXML);
         stallListView.setItems(stallList);
         stallListView.setCellFactory(listView -> new StallListViewCell());
+    }
+
+    /**
+     * Handles the activity when the user clicks on the stall in the list
+     *
+     * Adapted code from https://stackoverflow.com/questions/20635192/how-to-create-popup-menu
+     */
+    @FXML
+    public void handleMouseClicked() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+
+        final ContextMenu contextMenu = new ContextMenu();
+        MenuItem copyStallName = new MenuItem("Copy Name");
+        MenuItem copyStallAddress = new MenuItem("Copy Address");
+        MenuItem copyStallTag = new MenuItem("Copy Tag");
+        contextMenu.getItems().addAll(copyStallName, copyStallAddress, copyStallTag);
+
+        stallListView.setContextMenu(contextMenu);
+
+        copyStallName.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(stallListView.getSelectionModel().getSelectedItem().getName().toString());
+            clipboard.setContent(content);
+        });
+
+        copyStallAddress.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(stallListView.getSelectionModel().getSelectedItem().getAddress().toString());
+            clipboard.setContent(content);
+        });
+
+        copyStallTag.setOnAction((ActionEvent actionEvent) -> {
+            content.putString(stallListView.getSelectionModel().getSelectedItem().getTags().toString());
+            clipboard.setContent(content);
+        });
     }
 
     /**
