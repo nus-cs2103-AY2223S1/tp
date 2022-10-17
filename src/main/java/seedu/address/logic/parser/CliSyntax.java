1343bb0e12c9a3a9454719bfc282bcbaf0b11d63
Shashank Acharya <acharya.s@icloud.com>
2022-10-17 18:01:59 +0800

    Update date prefixes

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,6 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_START_DATE = new Prefix("start/");
-    public static final Prefix PREFIX_END_DATE = new Prefix("end/");
+    public static final Prefix PREFIX_START_DATE = new Prefix("s/");
+    public static final Prefix PREFIX_END_DATE = new Prefix("e/");
+
 }
