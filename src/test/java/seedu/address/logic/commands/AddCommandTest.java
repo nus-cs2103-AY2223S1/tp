ceb401c9c1ce73df0026d59dbc76a2be01ec349f
Darren12345677 <darrencookies100@gmail.com>
2022-10-17 22:41:51 +0800

    Add test cases for sort command

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -134,6 +134,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void sortProjects() {
+            throw new AssertionError(("This method should not be called."));
+        }
+        
+        @Override
         public void setProject(Project target, Project editedProject) {
             throw new AssertionError("This method should not be called.");
         }
