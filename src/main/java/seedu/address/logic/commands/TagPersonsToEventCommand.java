ca238052ebd5b1209968ee0d37c3eca7b1ad7593
Qiaoran-M <e0492494@u.nus.edu>
2022-10-17 13:11:23 +0800

    Add tag persons to event command

---
+++ src/main/java/seedu/address/logic/commands/TagPersonsToEventCommand.java
@@ -0,0 +1,78 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.UidList;
+import seedu.address.model.person.Person;
+
+/**
+ * Tags customers in Person List to a event.
+ */
+public class TagPersonsToEventCommand extends Command {
+    public static final String COMMAND_WORD = "tagEvent";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book "
+            + "by the index number used in the displayed person and event lists. "
+            + "Parameters: "
+            + "EVENT INDEX (must be a positive integer) "
+            + PREFIX_PERSONS + "PERSON INDEX 1 (must be a positive integer) "
+            + "[PERSON INDEX 2 (must be a positive integer) "
+            + "PERSON INDEX 3 (must be a positive integer) ...]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_PERSONS + "1 3 4";
+    public static final String MESSAGE_TAG_EVENT_SUCCESS = "Tagged Persons: %s to Event: %s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the event";
+    private final Index eventIndex;
+    private final List<Index> personIndexes;
+
+    /**
+     * @param eventIndex index of the event to be tagged with persons.
+     * @param personIndexes index of persons to be tagged to the event.
+     */
+    public TagPersonsToEventCommand(Index eventIndex, List<Index> personIndexes) {
+        requireNonNull(eventIndex);
+        requireNonNull(personIndexes);
+
+        this.eventIndex = eventIndex;
+        this.personIndexes = personIndexes;
+    }
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownPersonList = model.getFilteredPersonList();
+        List<Event> lastShownEventList = model.getFilteredEventList();
+        if (eventIndex.getZeroBased() >= lastShownEventList.size()) {
+            throw new CommandException(MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+        }
+        Event eventToTag = lastShownEventList.get(eventIndex.getZeroBased());
+        UidList uids = eventToTag.getUids();
+        String personNames = "";
+        // loop through personIndexes, raise exceptions if any (invalid index or duplicated person)
+        for (Index personIndex : personIndexes) {
+            if (personIndex.getZeroBased() >= lastShownPersonList.size()) { // index is invalid
+                throw new CommandException(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+            Person person = lastShownPersonList.get(personIndex.getZeroBased());
+            if (uids.contains(person.getUid())) { // person's uid already exists in the event to tag
+                throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+            }
+            personNames += String.format("%s, ", person.getName());
+            uids.add(person.getUid());
+        }
+        // create editedEvent, set event, update event list
+        Event taggedEvent = new Event(eventToTag, uids);
+        model.setEvent(eventToTag, taggedEvent);
+        model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
+        personNames = personNames.substring(0, personNames.length() - 2);
+        return new CommandResult(String.format(MESSAGE_TAG_EVENT_SUCCESS, personNames, taggedEvent.getEventTitle()));
+    }
+}
