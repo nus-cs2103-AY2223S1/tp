455eac0b714587063c526dc651f407ca144bd0e7
Shashank Acharya <acharya.s@icloud.com>
2022-10-17 19:42:37 +0800

    Update EventCommand variables
    These will be updated in the pending EventCommand PR as well.

--- src/main/java/seedu/address/logic/commands/event/EventCommand.java
+++ src/main/java/seedu/address/logic/commands/event/EventCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands.event;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_OPTION;
+
 import seedu.address.logic.commands.Command;
 
 /**
@@ -9,8 +11,18 @@ public abstract class EventCommand extends Command {
 
     public static final String COMMAND_WORD = "event";
 
-    public static final String OPTION_UNKNOWN = "That is not a valid option flag. "
-            + " Please use one of the following valid flags: "
-            + "None implemented";
+    public static final String EVENT_FORMAT = "Format: " + COMMAND_WORD + " -option\n";
 
+    public static final String VALID_FLAGS = "Please use one of the following valid flags: "
+            + PREFIX_OPTION + AddEventCommand.COMMAND_OPTION + ".";
+
+    public static final String OPTION_UNKNOWN = "That is not a valid option flag.\n";
+
+    public static final String OPTION_NO_MULTIPLE = "Multiple option flags are not allowed.\n" + EVENT_FORMAT;
+
+    public static final String OPTION_WRONG_ORDER = "Option flag should be in front.\n" + EVENT_FORMAT;
+
+    public static final String OPTION_WRONG_ORDER_NO_MULTIPLE =
+            "Only one option flag should be specified in front.\n" + EVENT_FORMAT;
+    
 }
