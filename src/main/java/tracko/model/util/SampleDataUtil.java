8794c908aebeb7011705a08f24d6a6b0f6cb42fb
donljh <don.limjunhong@gmail.com>
2022-10-18 02:11:29 +0800

    Update sample data to use references

--- src/main/java/tracko/model/util/SampleDataUtil.java
+++ src/main/java/tracko/model/util/SampleDataUtil.java
@@ -20,35 +20,33 @@ import tracko.model.order.Phone;
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
+    private static final Item ITEM_1 = new Item(new ItemName("Chair"), new Description("Carved mahogany wooden chair"),
+        new Quantity(300), new HashSet<>());
+    private static final Item ITEM_2 = new Item(new ItemName("Pillow"), new Description("Ergonomic with recycled polystyrene filling"),
+        new Quantity(300), new HashSet<>());
+    private static final Item ITEM_3 = new Item(new ItemName("Bolster"), new Description("Premium cotton filling"),
+        new Quantity(300), new HashSet<>());
+    private static final Item ITEM_4 = new Item(new ItemName("Mattress"), new Description("King-sized with memory foam"),
+        new Quantity(300), new HashSet<>());
 
     public static Item[] getSampleItems() {
-        return new Item[] {
-                new Item(new ItemName("Chair"), new Description("Carved mahogany wooden chair"),
-                        new Quantity(300), new HashSet<>()),
-                new Item(new ItemName("Pillow"), new Description("Ergonomic with recycled polystyrene filling"),
-                        new Quantity(300), new HashSet<>()),
-                new Item(new ItemName("Bolster"), new Description("Premium cotton filling"),
-                        new Quantity(300), new HashSet<>()),
-                new Item(new ItemName("Mattress"), new Description("King-sized with memory foam"),
-                        new Quantity(300), new HashSet<>()),
-
-        };
+        return new Item[] { ITEM_1, ITEM_2, ITEM_3, ITEM_4 };
     }
 
     public static Order[] getSampleOrders() {
         return new Order[] {
             new Order(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                    List.of(new ItemQuantityPair(getSampleItems()[0], new Quantity(2)))),
+                    List.of(new ItemQuantityPair(ITEM_1, new Quantity(2)))),
             new Order(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    List.of(new ItemQuantityPair(getSampleItems()[1], new Quantity(4)))),
+                    List.of(new ItemQuantityPair(ITEM_2, new Quantity(4)))),
             new Order(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    List.of(new ItemQuantityPair(getSampleItems()[2], new Quantity(3)))),
+                    List.of(new ItemQuantityPair(ITEM_3, new Quantity(3)))),
             new Order(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    List.of(new ItemQuantityPair(getSampleItems()[3], new Quantity(1)))),
+                    List.of(new ItemQuantityPair(ITEM_4, new Quantity(1)))),
         };
     }
 
