08a33c7565c43e05bce989a6d9518a9c919d0d86
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2022-10-18 01:30:31 +0800

    Fix UI for AddListingCommand

--- src/main/java/seedu/address/model/listing/Listing.java
+++ src/main/java/seedu/address/model/listing/Listing.java
@@ -145,7 +145,7 @@ public class Listing {
                 .append("; Address: ")
                 .append(this.address)
                 .append("; Owner: ")
-                .append(this.owner)
+                .append(getName())
                 .append("; Asking Price: ")
                 .append(this.askingPrice);
 
