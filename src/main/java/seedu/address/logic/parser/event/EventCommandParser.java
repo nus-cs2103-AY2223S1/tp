54b531c49b703e5572433039c037346ae42824f7
Shashank Acharya <acharya.s@icloud.com>
2022-10-17 19:46:56 +0800

    Add regex to EventCommandParser
    This matches the same implementation for ProfileCommandParser.

--- src/main/java/seedu/address/logic/parser/event/EventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/event/EventCommandParser.java
@@ -1,15 +1,11 @@
 package seedu.address.logic.parser.event;
 
 import static seedu.address.commons.core.Messages.MESSAGE_FLAG_NOT_SPECIFIED;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_OPTION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.event.EventCommand;
-import seedu.address.logic.parser.ArgumentMultimap;
-import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -17,20 +13,38 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses input arguments and creates a new EventCommand object
  */
 public class EventCommandParser implements Parser<EventCommand> {
+    private static final Pattern EVENT_COMMAND_FORMAT = Pattern.compile(
+            "\\s+(-(?<eventOption>\\S*)(\\s+(?!-)\\S+)?)(\\s+(?!-)\\S*.)*");
+
     /**
      * Parses the given {@code String} of arguments in the context of the EventCommand
      * and returns an EventCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public EventCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args,
-                        PREFIX_OPTION, PREFIX_NAME, PREFIX_START_DATE, PREFIX_END_DATE, PREFIX_TAG);
 
-        String option = argMultimap.getValue(PREFIX_OPTION)
-                .orElseThrow(() -> new ParseException(MESSAGE_FLAG_NOT_SPECIFIED));
+        if (!args.matches("\\s+-.*")) {
+            Matcher matcher = Pattern.compile("(\\s+-\\S*)").matcher(args);
+            long numFlags = matcher.results().count();
+            if (numFlags == 0) {
+                throw new ParseException(String.format(
+                        MESSAGE_FLAG_NOT_SPECIFIED, EventCommand.EVENT_FORMAT + EventCommand.VALID_FLAGS));
+            } else if (numFlags == 1) {
+                throw new ParseException(EventCommand.OPTION_WRONG_ORDER);
+            } else {
+                throw new ParseException(EventCommand.OPTION_WRONG_ORDER_NO_MULTIPLE);
+            }
+        }
 
-        switch (option) {
+        final Matcher eventCommandMatcher = EVENT_COMMAND_FORMAT.matcher(args);
+
+        if (!eventCommandMatcher.matches()) {
+            throw new ParseException(EventCommand.OPTION_NO_MULTIPLE);
+        }
+
+        String eventOption = eventCommandMatcher.group("eventOption");
+
+        switch (eventOption) {
         default:
             throw new ParseException(EventCommand.OPTION_UNKNOWN);
         }
