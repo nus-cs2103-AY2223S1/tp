cedc72ff62973b99aa078dbf9bed1163656fff8d
Fuzanwenn <e0902049@u.nus.edu>
2022-10-17 14:59:12 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/bill/BillDate.java
+++ src/main/java/seedu/address/model/bill/BillDate.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
