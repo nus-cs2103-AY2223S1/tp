cb08fc9f31b2b9fd08b2b8f23bb187e973bfba49
izzahaj <izzah.jalil02@gmail.com>
2022-10-17 17:08:05 +0800

    Add custom condition exceptions

--- src/main/java/seedu/uninurse/model/condition/exceptions/DuplicateConditionException.java
+++ src/main/java/seedu/uninurse/model/condition/exceptions/DuplicateConditionException.java
@@ -1,2 +1,11 @@
-package seedu.uninurse.model.condition.exceptions;public class DuplicateConditionException {
+package seedu.uninurse.model.condition.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Conditions.
+ * Conditions are considered duplicates if they have the same name.
+ */
+public class DuplicateConditionException extends RuntimeException {
+    public DuplicateConditionException() {
+        super("Operation would result in duplicate conditions");
+    }
 }
