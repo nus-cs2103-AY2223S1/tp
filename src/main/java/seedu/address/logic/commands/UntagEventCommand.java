ec50dbf943d9ea2b41fc34eddbcfac1fb4772ad9
Qiaoran-M <e0492494@u.nus.edu>
2022-10-17 17:42:31 +0800

    Add untag event command and parser

--- src/main/java/seedu/address/logic/commands/UntagEventCommand.java
+++ src/main/java/seedu/address/logic/commands/UntagEventCommand.java
@@ -16,12 +16,12 @@ import seedu.address.model.event.UidList;
 import seedu.address.model.person.Person;
 
 /**
- * Tags customers in Person List to an event.
+ * Untags customers in Person List to an event.
  */
-public class TagEventCommand extends Command {
-    public static final String COMMAND_WORD = "tagEvent";
+public class UntagEventCommand extends Command {
+    public static final String COMMAND_WORD = "untagEvent";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Tags 1 or more persons to an event in the address book "
+            + ": Untags 1 or more persons to an event in the address book "
             + "by the index number used in the displayed person and event lists. "
             + "Parameters: "
             + "EVENT INDEX (must be a positive integer) "
@@ -30,16 +30,16 @@ public class TagEventCommand extends Command {
             + "PERSON INDEX 3 (must be a positive integer) ...]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_PERSONS + "1 3 4";
-    public static final String MESSAGE_TAG_EVENT_SUCCESS = "Tagged Persons: %s to Event: %s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person: %d. %s already exists in the event";
+    public static final String MESSAGE_UNTAG_EVENT_SUCCESS = "Untagged Persons: %s to Event: %s";
+    public static final String MESSAGE_INVALID_PERSON = "This person: %d. %s doesn't exist in the event";
     private final Index eventIndex;
     private final List<Index> personIndexes;
 
     /**
-     * @param eventIndex index of the event to be tagged with persons.
-     * @param personIndexes index of persons to be tagged to the event.
+     * @param eventIndex index of the event to be untagged with persons.
+     * @param personIndexes index of persons to be untagged to the event.
      */
-    public TagEventCommand(Index eventIndex, List<Index> personIndexes) {
+    public UntagEventCommand(Index eventIndex, List<Index> personIndexes) {
         requireNonNull(eventIndex);
         requireNonNull(personIndexes);
 
@@ -54,8 +54,8 @@ public class TagEventCommand extends Command {
         if (eventIndex.getZeroBased() >= lastShownEventList.size()) {
             throw new CommandException(MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
         }
-        Event eventToTag = lastShownEventList.get(eventIndex.getZeroBased());
-        UidList uids = eventToTag.getUids();
+        Event eventToUntag = lastShownEventList.get(eventIndex.getZeroBased());
+        UidList uids = eventToUntag.getUids();
         String personNames = "";
         // loop through personIndexes, raise exceptions if any (invalid index or duplicated person)
         for (Index personIndex : personIndexes) {
@@ -63,20 +63,21 @@ public class TagEventCommand extends Command {
                 throw new CommandException(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
             }
             Person person = lastShownPersonList.get(personIndex.getZeroBased());
-            if (uids.contains(person.getUid())) { // person's uid already exists in the event to tag
-                throw new CommandException(String.format(MESSAGE_DUPLICATE_PERSON, personIndex.getOneBased(),
+            if (!uids.contains(person.getUid())) { // person's uid not in the event to untag
+                throw new CommandException(String.format(MESSAGE_INVALID_PERSON, personIndex.getOneBased(),
                         person.getName()));
             }
             personNames += String.format("%s, ", person.getName());
-            uids.add(person.getUid());
+            uids.remove(person.getUid());
         }
         // create editedEvent, set event, update event list
-        Event taggedEvent = new Event(eventToTag, uids);
-        model.setEvent(eventToTag, taggedEvent);
+        Event untaggedEvent = new Event(eventToUntag, uids);
+        model.setEvent(eventToUntag, untaggedEvent);
         model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
         model.updateEventPersonReference();
         personNames = personNames.substring(0, personNames.length() - 2);
 
-        return new CommandResult(String.format(MESSAGE_TAG_EVENT_SUCCESS, personNames, taggedEvent.getEventTitle()));
+        return new CommandResult(String.format(MESSAGE_UNTAG_EVENT_SUCCESS, personNames,
+                untaggedEvent.getEventTitle()));
     }
 }
--- src/main/java/seedu/address/logic/parser/UntagEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UntagEventCommandParser.java
@@ -6,19 +6,19 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSONS;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.TagEventCommand;
+import seedu.address.logic.commands.UntagEventCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new TagEventCommand object
+ * Parses input arguments and creates a new UntagEventCommand object
  */
-public class TagEventCommandParser implements Parser<TagEventCommand> {
+public class UntagEventCommandParser implements Parser<UntagEventCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the TagPersonsToEventCommand
      * and returns an TagPersonsToEventCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public TagEventCommand parse(String args) throws ParseException {
+    public UntagEventCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_PERSONS);
 
         Index eventIndex;
@@ -26,15 +26,15 @@ public class TagEventCommandParser implements Parser<TagEventCommand> {
             eventIndex = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    seedu.address.logic.commands.TagEventCommand.MESSAGE_USAGE), pe);
+                    UntagEventCommand.MESSAGE_USAGE), pe);
         }
 
         if (!arePrefixesPresent(argMultimap, PREFIX_PERSONS)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    seedu.address.logic.commands.TagEventCommand.MESSAGE_USAGE));
+                    UntagEventCommand.MESSAGE_USAGE));
         }
         List<Index> personIndexes = ParserUtil.parseIndexes(argMultimap.getValue(PREFIX_PERSONS).get());
-        return new seedu.address.logic.commands.TagEventCommand(eventIndex, personIndexes);
+        return new UntagEventCommand(eventIndex, personIndexes);
     }
 
     /**
@@ -45,4 +45,3 @@ public class TagEventCommandParser implements Parser<TagEventCommand> {
         return java.util.stream.Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
-
