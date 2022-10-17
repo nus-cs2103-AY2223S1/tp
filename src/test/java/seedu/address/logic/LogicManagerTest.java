8d1f212f499d0927744665b6834ef2b6e95958ea
Tex Tang <tex_tang@outlook.com>
2022-10-17 11:41:33 +0800

    Command: Change List to Reset command

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ResetCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
@@ -62,8 +62,8 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() throws Exception {
-        String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        String resetCommand = ResetCommand.COMMAND_WORD;
+        assertCommandSuccess(resetCommand, ResetCommand.MESSAGE_SUCCESS, model);
     }
 
     @Test
