cc2711b43cf19b0d3117c21bb17ea8365b3bc0e6
LiuFangrui <e0772663@u.nus.edu>
2022-10-17 23:37:33 +0800

    Address Checkstyle issues

--- src/main/java/seedu/address/logic/parser/LinkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LinkCommandParser.java
@@ -1,14 +1,14 @@
 package seedu.address.logic.parser;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.LinkCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.stream.Stream;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
+
+import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.LinkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Parses input arguments and creates a new DeleteInternshipCommandParser object
