49776085cada9600939d8ae6e19e82fa571e21a1
Markus Wee <77526609+yellow-294@users.noreply.github.com>
2022-10-17 21:00:02 +0800

    Add Assignments class and AddAssignmentsCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,6 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import seedu.address.logic.commands.AddAssignmentsCommand;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AttendanceCommand;
 import seedu.address.logic.commands.AvailabilityCommand;
@@ -87,6 +88,9 @@ public class AddressBookParser {
 
         case RolesCommand.COMMAND_WORD:
             return new RolesCommandParser().parse(arguments);
+
+        case AddAssignmentsCommand.COMMAND_WORD:
+            return new AddAssignmentsCommandParser().parse(arguments);
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
