16e4fe9c4f67d233e32d8841424b1041a02e9c5e
Qiaoran-M <e0492494@u.nus.edu>
2022-10-17 16:27:36 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -21,9 +21,8 @@ import seedu.address.model.person.Gender;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.person.Uid;
-
+import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new AddCommand object
