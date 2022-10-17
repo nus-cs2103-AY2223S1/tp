65df5c52938de7ab01e23f788dfd551169f9d32a
wweqg <e0726629@u.nus.edu>
2022-10-17 23:11:01 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/SortCommandUtil.java
+++ src/main/java/seedu/address/logic/commands/SortCommandUtil.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import java.util.Comparator;
+
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Buyer;
 import seedu.address.model.person.Deliverer;
@@ -9,8 +11,10 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Supplier;
 
-import java.util.Comparator;
 
+/**
+ * Provides utils for Sort related classes.
+ */
 public class SortCommandUtil {
     //Comparators for person
     public static final Comparator<Address> ADDRESS_COMPARATOR = (o1, o2) -> o1.compareTo(o2);
