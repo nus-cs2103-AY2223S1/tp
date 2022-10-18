7389acdf3c33164573bb584937e49032d755cf36
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-10-18 10:12:11 +0800

    Fix cancel command and test cases

--- src/test/java/seedu/address/logic/commands/CancelCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CancelCommandTest.java
@@ -2,6 +2,8 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_APPOINTMENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_NON_EXISTENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_APPOINTMENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
@@ -13,9 +15,11 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Appointment;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
@@ -31,16 +35,27 @@ public class CancelCommandTest {
 
     @Test
     public void execute_validIndexCancel_success() {
-        Person personWithAppt = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
-        Person editedPerson = new PersonBuilder(personWithAppt).withAppointmentList(new ArrayList<>()).build();
+        Index targetAppointmentIndex = INDEX_FIRST_APPOINTMENT;
 
-        String expectedCancelMessage = CancelCommand.MESSAGE_CANCEL_APPOINTMENT_SUCCESS + personWithAppt.getName();
+        Model testModel = new ModelManager(new AddressBook(), new UserPrefs());
 
-        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-        expectedModel.setPerson(personWithAppt, editedPerson);
-        expectedModel.deleteAppointments(personWithAppt.getAppointments());
-        CancelCommand cancelCommand = new CancelCommand(INDEX_SECOND_APPOINTMENT);
-        assertCommandSuccess(cancelCommand, model, expectedCancelMessage, expectedModel);
+        Appointment unmarkedAppointment =
+                new Appointment("Cough", "2010-01-01 00:00", "", false);
+        Person personWithAppointment = new PersonBuilder().withAppointment(unmarkedAppointment).build();
+        unmarkedAppointment.setPatient(personWithAppointment);
+
+        testModel.addPerson(personWithAppointment);
+        testModel.addAppointment(unmarkedAppointment);
+
+        CancelCommand cancelCommand = new CancelCommand(targetAppointmentIndex);
+        String expectedCancelMessage =
+                CancelCommand.MESSAGE_CANCEL_APPOINTMENT_SUCCESS + personWithAppointment.getName();
+
+        ModelManager expectedModel = new ModelManager(new AddressBook(), new UserPrefs());
+        Person expectedPerson = new PersonBuilder().build();
+        expectedModel.addPerson(expectedPerson);
+
+        assertCommandSuccess(cancelCommand, testModel, expectedCancelMessage, expectedModel);
     }
 
     @Test
