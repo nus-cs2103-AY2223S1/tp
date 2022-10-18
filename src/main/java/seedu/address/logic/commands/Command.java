ad9f92f010e37d2935a68a6146080f1bcc3e20f8
Donovan <don9617@hotmail.com>
2022-10-18 13:35:48 +0800

    Fix ci

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.ui.Ui;
 
