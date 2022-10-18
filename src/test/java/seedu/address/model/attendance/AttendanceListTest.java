1a77198eccbdd85b0e46eeebdf4518a7fd4d756b
Evande1 <crossjab@gmail.com>
2022-10-18 11:17:45 +0800

    attendance test cases
    isValidAttendance

--- src/test/java/seedu/address/model/attendance/AttendanceListTest.java
+++ src/test/java/seedu/address/model/attendance/AttendanceListTest.java
@@ -1,12 +1,13 @@
 package seedu.address.model.attendance;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 public class AttendanceListTest {
 
+    public static AttendanceList EMPTY_ATTENDANCE = new AttendanceList();
+    public static AttendanceList TEST_ATTENDANCE = new AttendanceList("CS2040","12");
     @Test
     public void equals() {
         AttendanceList attendanceList = new AttendanceList("CS2040", "10");
@@ -29,4 +30,42 @@ public class AttendanceListTest {
         assertFalse(attendanceList.equals(differentAttendanceList));
     }
 
+    @Test
+    public void isValidSize() {
+        // null size of list
+        assertThrows(NullPointerException.class, () -> AttendanceList.isValidSize(null));
+
+        // invalid size of list
+        assertFalse(AttendanceList.isValidSize("")); // empty string
+        assertFalse(AttendanceList.isValidSize(" ")); // spaces only
+        assertFalse(AttendanceList.isValidSize("13")); // more than 12 numbers
+        assertFalse(AttendanceList.isValidSize("phone")); // non-numeric
+
+        // valid size
+        assertTrue(AttendanceList.isValidSize("0")); // empty
+        assertTrue(AttendanceList.isValidSize("12")); // max capacity of 12
+    }
+
+    @Test
+    public void isEmpty() {
+        assertFalse(TEST_ATTENDANCE.isEmpty());
+        assertTrue(EMPTY_ATTENDANCE.isEmpty());
+    }
+
+    @Test
+    public void getSize() {
+
+    }
+
+    @Test
+    public void getAttendanceList() {
+    }
+
+    @Test
+    public void getMod() {
+    }
+
+    @Test
+    public void mark() {
+    }
 }
--- src/test/java/seedu/address/model/attendance/AttendanceTest.java
+++ src/test/java/seedu/address/model/attendance/AttendanceTest.java
@@ -2,8 +2,11 @@ package seedu.address.model.attendance;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.model.student.Phone;
 
 public class AttendanceTest {
     @Test
@@ -26,5 +29,29 @@ public class AttendanceTest {
         // different attendance -> returns false
         Attendance differentAttendance = new Attendance("0");
         assertFalse(attendance.equals(differentAttendance));
+    }
+
+    @Test
+    public void isValidMark() {
+        // null mark
+        assertThrows(NullPointerException.class, () -> Attendance.isValidMark(null));
+
+        // invalid marks
+        assertFalse(Attendance.isValidMark("")); // empty string
+        assertFalse(Attendance.isValidMark(" ")); // spaces only
+        assertFalse(Attendance.isValidMark("2")); // number greater than 1
+        assertFalse(Attendance.isValidMark("phone")); // non-numeric
+
+        assertTrue(Attendance.isValidMark("0")); // valid inputs 0 and 1
+        assertTrue(Attendance.isValidMark("1")); // valid inputs 0 and 1
+    }
+
+    @Test
+    public void constructor_invalid_attendance_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Attendance("3"));
+    }
+    @Test
+    public void constructor_null_attendance_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Attendance(null));
     }
 }
