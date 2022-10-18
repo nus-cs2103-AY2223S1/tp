f7d36e23ca1dbbfe9d8233c98ef5c8e1e777393b
teoyuqi <teo.yuqi@gmail.com>
2022-10-18 22:09:22 +0800

    Fix typo

--- src/main/java/seedu/address/logic/parser/DeleteTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTaskCommandParser.java
@@ -1,14 +1,12 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_TASK_NUMBER;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_NUMBER_TO_DELETE;
 import static seedu.address.logic.parser.ParserUtil.arePrefixesPresent;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.DeleteTaskCommand;
 import seedu.address.logic.commands.DeleteTaskCommand.DeleteTaskFromModuleDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -20,8 +18,8 @@ import seedu.address.model.module.ModuleCode;
 public class DeleteTaskCommandParser implements Parser<DeleteTaskCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the
-     * {@code DeleteCommand} and returns a {@code DeleteCommand} object for
-     * execution.
+     * {@code DeleteTaskCommand} and returns a {@code DeleteTaskCommand} object
+     * for execution.
      * @throws ParseException if the user input does not conform with the
      *                        expected format.
      */
