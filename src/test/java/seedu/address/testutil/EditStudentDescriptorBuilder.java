4801ce32065500424bb239472e6fbb9a324a30d3
Dilysss <e0772712@u.nus.edu>
2022-10-18 17:04:47 +0800

    Fix EditStuCommandTest CI

--- src/test/java/seedu/address/testutil/EditStudentDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditStudentDescriptorBuilder.java
@@ -1,16 +1,11 @@
 package seedu.address.testutil;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import seedu.address.logic.commands.EditStuCommand.EditStudentDescriptor;
 import seedu.address.model.student.Attendance;
 import seedu.address.model.student.StuEmail;
 import seedu.address.model.student.StuName;
 import seedu.address.model.student.Student;
 import seedu.address.model.student.Telegram;
-import seedu.address.model.tag.Tag;
 
 /**
  * A utility class to help with building EditStudentDescriptor objects.
