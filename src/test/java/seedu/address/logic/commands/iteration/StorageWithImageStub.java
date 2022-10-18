c728ef637c4f774f0f9fa16e5c85b8c548489d10
chloeelim <chloelim22@gmail.com>
2022-10-18 13:28:49 +0800

    Test: Add AddIterationCommandParser Tests
    Adds test cases for `AddIterationComamndParser`. Also modifies
    previous testcases to use actual test images so that the test cases
    will not fail when trying to parse the image paths.

--- src/test/java/seedu/address/logic/commands/iteration/StorageWithImageStub.java
+++ src/test/java/seedu/address/logic/commands/iteration/StorageWithImageStub.java
@@ -5,6 +5,6 @@ import seedu.address.model.StorageStub;
 class StorageWithImageStub extends StorageStub {
     @Override
     public String saveIterationImage(String src) {
-        return "/images/placeholderart.png";
+        return src;
     }
 }
--- src/test/java/seedu/address/testutil/TypicalIterations.java
+++ src/test/java/seedu/address/testutil/TypicalIterations.java
@@ -1,11 +1,14 @@
 package seedu.address.testutil;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_FIRST_DAY_OF_YEAR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_LAST_DAY_OF_YEAR;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FEEDBACK_AMAZING;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FEEDBACK_HORRIBLE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FEEDBACK_UGLY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ITERATION_DESCRIPTION_COLOR;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ITERATION_DESCRIPTION_FINALISE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ITERATION_DESCRIPTION_REMOVE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ITERATION_IMAGEPATH_COLOR;
 
 import java.time.LocalDate;
 
@@ -17,13 +20,14 @@ import seedu.address.model.iteration.Iteration;
 public class TypicalIterations {
 
     public static final Iteration FINALISED = new IterationBuilder()
-            .withDate(LocalDate.of(2022, 10, 10))
+            .withDate(LocalDate.parse(VALID_DATE_FIRST_DAY_OF_YEAR))
             .withDescription(VALID_ITERATION_DESCRIPTION_FINALISE)
             .withFeedback(VALID_FEEDBACK_HORRIBLE).build();
     public static final Iteration ADD_COLOR = new IterationBuilder()
-            .withDate(LocalDate.of(2021, 1, 1))
+            .withDate(LocalDate.parse(VALID_DATE_LAST_DAY_OF_YEAR))
             .withDescription(VALID_ITERATION_DESCRIPTION_COLOR)
-            .withFeedback(VALID_FEEDBACK_UGLY).build();
+            .withFeedback(VALID_FEEDBACK_UGLY)
+            .withImagePath(VALID_ITERATION_IMAGEPATH_COLOR).build();
     public static final Iteration REMOVE_CHARACTER = new IterationBuilder()
             .withDescription(VALID_ITERATION_DESCRIPTION_REMOVE)
             .withFeedback(VALID_FEEDBACK_AMAZING).build();
