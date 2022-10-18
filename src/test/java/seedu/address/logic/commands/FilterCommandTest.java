780c2bc7db4e3735018f0a817c6c20a08fded5c3
Brendan Tan <brendantan@outlook.com>
2022-10-18 16:21:50 +0800

    Add test cases for `FilterCommand` and `FilterCommandParser`

---
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -0,0 +1,61 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalInternships.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.internship.ApplicationStatus;
+import seedu.address.model.internship.InternshipHasApplicationStatusPredicate;
+
+public class FilterCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validFilter_success() {
+        ApplicationStatus applicationStatus = ApplicationStatus.Applied;
+        InternshipHasApplicationStatusPredicate predicate = new InternshipHasApplicationStatusPredicate(
+                applicationStatus);
+        FilterCommand filterCommand = new FilterCommand(predicate);
+
+        String expectedMessage = String.format(Messages.MESSAGE_INTERNSHIPS_LISTED_OVERVIEW, 3);
+
+        expectedModel.updateFilteredInternshipList(predicate);
+
+        assertCommandSuccess(filterCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        InternshipHasApplicationStatusPredicate appliedPredicate = new InternshipHasApplicationStatusPredicate(
+                ApplicationStatus.Applied);
+        InternshipHasApplicationStatusPredicate rejectedPredicate = new InternshipHasApplicationStatusPredicate(
+                ApplicationStatus.Rejected);
+
+        FilterCommand firstFilterCommand = new FilterCommand(appliedPredicate);
+        FilterCommand secondFilterCommand = new FilterCommand(rejectedPredicate);
+
+        // same object -> returns true
+        assertEquals(firstFilterCommand, firstFilterCommand);
+
+        // same values -> returns true
+        FilterCommand firstFilterCommandCopy = new FilterCommand(appliedPredicate);
+        assertEquals(firstFilterCommand, firstFilterCommandCopy);
+
+        // different values -> returns false
+        assertNotEquals(1, firstFilterCommand);
+
+        // null -> returns false
+        assertNotEquals(null, firstFilterCommand);
+
+        // different filter -> returns false
+        assertNotEquals(firstFilterCommand, secondFilterCommand);
+    }
+}
