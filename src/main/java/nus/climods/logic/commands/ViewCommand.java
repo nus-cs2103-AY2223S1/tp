c5b437d81cdf5e31a50ecd3c0713f0ab0f05fe80
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-10-18 15:45:51 +0800

    Add skeleton for ViewCommand and its parser

--- src/main/java/nus/climods/logic/commands/ViewCommand.java
+++ src/main/java/nus/climods/logic/commands/ViewCommand.java
@@ -1,2 +1,24 @@
-package nus.climods.logic.commands;public class ViewCommand {
+package nus.climods.logic.commands;
+
+import nus.climods.logic.commands.exceptions.CommandException;
+import nus.climods.model.Model;
+
+/**
+ * View details for a module
+ */
+public class ViewCommand extends Command {
+    public static final String COMMAND_WORD = "view";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": View details for a module.\n"
+            + "Parameters: MODULE-CODE [LESSON-TYPE...]\n"
+            + "Example: " + COMMAND_WORD + " " + "CS2103";
+    private final String moduleCode;
+
+    public ViewCommand(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
 }
