a93bbd8163c11d46822310d5dfe7f51951859c8c
Hongyi6328 <6328huang@gmail.com>
2022-10-18 14:42:13 +0800

    Finish UniqueId and UniqueIdGenerator

--- src/main/java/seedu/address/commons/core/index/UniqueId.java
+++ src/main/java/seedu/address/commons/core/index/UniqueId.java
@@ -1,4 +1,30 @@
 package seedu.address.commons.core.index;
 
-public class UniqueId {
+/**
+ * Represents a unique id that does not change throughout the life cycle of an Object
+ */
+public class UniqueId implements Comparable<UniqueId> {
+    private final String id;
+
+    /**
+     * Constructs a unique id object;
+     *
+     * @param id The string as the key for id.
+     */
+    public UniqueId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int compareTo(UniqueId other) {
+        return this.id.compareTo(other.id);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof UniqueId)) {
+            return false;
+        }
+        return this.id.equals(((UniqueId) other).id);
+    }
 }
--- src/main/java/seedu/address/commons/core/index/UniqueIdGenerator.java
+++ src/main/java/seedu/address/commons/core/index/UniqueIdGenerator.java
@@ -1,8 +1,54 @@
 package seedu.address.commons.core.index;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Generates a unique ID and increments the current ID.
  */
 public class UniqueIdGenerator {
-    
+
+    private static final char START = 'a';
+    private static final char BASE = 26;
+    private final List<Character> currentId = new ArrayList<>();
+
+    /**
+     * Constructs a unique id generator starting from the base id.
+     */
+    public UniqueIdGenerator() {
+        currentId.add(START);
+    }
+
+    /**
+     * Returns the current id and increments.
+     *
+     * @return A unqiue ID.
+     */
+    public UniqueId next() {
+        StringBuilder sb = new StringBuilder();
+        currentId.forEach(x -> sb.insert(0, x));
+        String id = sb.toString();
+        increment();
+        return new UniqueId(id);
+    }
+
+    private void increment() {
+        int idx = 0;
+        boolean carry = false;
+        do {
+            char cur = currentId.get(idx);
+            cur += 1;
+            if (cur == START + BASE) {
+                carry = true;
+                cur = START;
+            }
+            currentId.set(idx, cur);
+            if (carry) {
+                idx++;
+            }
+        } while (carry && idx < currentId.size());
+        if (carry && idx == currentId.size()) {
+            currentId.add(START);
+        }
+    }
 }
