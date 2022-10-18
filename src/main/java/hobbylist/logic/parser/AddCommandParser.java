b3631908a77ef5932c7deb11d6973561f6a3bd9d
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-10-18 23:09:46 +0800

    Fix consistency in argument's name

--- src/main/java/hobbylist/logic/parser/AddCommandParser.java
+++ src/main/java/hobbylist/logic/parser/AddCommandParser.java
@@ -30,7 +30,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         boolean isStatusPresent = args.matches(".*\\bs/.*");
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_NAME,
-                        CliSyntax.PREFIX_DESCRIPTION, CliSyntax.PREFIX_TAG, CliSyntax.PREFIX_DATE, PREFIX_STATUS);
+                        CliSyntax.PREFIX_DESCRIPTION, CliSyntax.PREFIX_TAG, CliSyntax.PREFIX_DATE, CliSyntax.PREFIX_STATUS);
 
         if (!arePrefixesPresent(argMultimap, CliSyntax.PREFIX_NAME, CliSyntax.PREFIX_DESCRIPTION)
                 || !argMultimap.getPreamble().isEmpty()) {
