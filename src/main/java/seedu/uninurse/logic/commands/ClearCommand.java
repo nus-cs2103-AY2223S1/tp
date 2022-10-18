6d091e8819caeb013b19b1c26cc74b88f4da4231
rama-pang <rama.a.pangestu@gmail.com>
2022-10-18 22:51:19 +0800

    Add selective clear feature

--- src/main/java/seedu/uninurse/logic/commands/ClearCommand.java
+++ src/main/java/seedu/uninurse/logic/commands/ClearCommand.java
@@ -2,8 +2,11 @@ package seedu.uninurse.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import seedu.uninurse.model.Model;
-import seedu.uninurse.model.UninurseBook;
+import seedu.uninurse.model.person.Patient;
 
 /**
  * Clears the uninurse book.
@@ -17,7 +20,10 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setUninurseBook(new UninurseBook());
+        List<Patient> lastShownList = new ArrayList<Patient>(model.getFilteredPersonList());
+        for (Patient patientToDelete : lastShownList) {
+            model.deletePerson(patientToDelete);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
