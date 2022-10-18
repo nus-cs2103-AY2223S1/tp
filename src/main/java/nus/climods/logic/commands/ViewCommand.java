0490f363d252f588405eb61e517539a20b80da43
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-10-18 16:38:43 +0800

    Add filtering for ViewCommand

--- src/main/java/nus/climods/logic/commands/ViewCommand.java
+++ src/main/java/nus/climods/logic/commands/ViewCommand.java
@@ -2,6 +2,7 @@ package nus.climods.logic.commands;
 
 import nus.climods.logic.commands.exceptions.CommandException;
 import nus.climods.model.Model;
+import nus.climods.model.module.predicate.ViewModulePredicate;
 
 /**
  * View details for a module
@@ -11,14 +12,20 @@ public class ViewCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": View details for a module.\n"
             + "Parameters: MODULE-CODE [LESSON-TYPE...]\n"
             + "Example: " + COMMAND_WORD + " " + "CS2103";
+    public static final String MESSAGE_MODULE_NOT_FOUND = "'%s' not in current NUS curriculum";
+    public static final String MESSAGE_SUCCESS = "Viewing details for module %s";
     private final String moduleCode;
 
     public ViewCommand(String moduleCode) {
-        this.moduleCode = moduleCode;
+        this.moduleCode = moduleCode.trim();
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        if (!model.hasListModule(moduleCode)) {
+            throw new CommandException(String.format(MESSAGE_MODULE_NOT_FOUND, moduleCode));
+        }
+        model.setFilteredModuleList(new ViewModulePredicate(moduleCode));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, moduleCode));
     }
 }
