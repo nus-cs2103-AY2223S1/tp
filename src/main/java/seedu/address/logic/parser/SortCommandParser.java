31a56ebd7b6cb85597f51cbf74710db23053d1b7
ShummyOwnzYou <jiehui285@gmail.com>
2022-10-19 14:07:13 +0800

    Add Sort Command

---
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Comparator;
+
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.applicant.Applicant;
+
+/**
+ * Parses input arguments and creates a new SortCommand object
+ */
+
+public class SortCommandParser implements Parser<SortCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SortCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.toLowerCase().trim();
+        switch (trimmedArgs) {
+        case "name":
+            return new SortCommand(new Comparator<Applicant>() {
+                @Override
+                public int compare(Applicant o1, Applicant o2) {
+                    return o1.getName().compareTo(o2.getName());
+
+
+                }
+            });
+        default:
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+        }
+    }
+}
--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -9,6 +9,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
@@ -136,6 +137,11 @@ public class AddCommandTest {
 
         @Override
         public void setApplicant(Applicant target, Applicant editedApplicant) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void sortApplicants(Comparator<Applicant> comparator) {
             throw new AssertionError("This method should not be called.");
         }
 
