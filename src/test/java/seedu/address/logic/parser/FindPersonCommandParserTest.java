cec38d7ba88e9d7666ce1c6c6c57a652ca44206b
therealdaofu <xu.daofu20@gmail.com>
2022-10-20 00:05:05 +0800

    Fix error

--- src/test/java/seedu/address/logic/parser/FindPersonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindPersonCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindPersonCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPersonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPersonCommand.MESSAGE_USAGE));
     }
 
     @Test
