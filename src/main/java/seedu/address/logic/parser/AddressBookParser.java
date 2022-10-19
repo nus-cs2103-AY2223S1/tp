5309189a3c10fc7655ab2f381246b8ddab7b2636
ketamethane <cheonghsien@gmail.com>
2022-10-19 22:31:21 +0800

    Correct syntax

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -24,8 +24,8 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.MarkTutorialCommand;
 import seedu.address.logic.commands.MarkQCommand;
+import seedu.address.logic.commands.MarkTutorialCommand;
 import seedu.address.logic.commands.UnmarkQCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
