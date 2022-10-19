ed2a7d97398e4b6cd0a7b2be14b2d634d768ee7d
sarrrdin <sarrrdin@gmail.com>
2022-10-19 14:10:08 +0800

    nit: javadoc error

--- src/main/java/nus/climods/logic/commands/DeleteCommand.java
+++ src/main/java/nus/climods/logic/commands/DeleteCommand.java
@@ -7,7 +7,7 @@ import nus.climods.model.Model;
 import nus.climods.model.module.UserModule;
 
 /**
- * Deletes a person identified using it's displayed index from the address book.
+ * Deletes a module identified using ModuleCode input by user.
  */
 public class DeleteCommand extends Command {
 
