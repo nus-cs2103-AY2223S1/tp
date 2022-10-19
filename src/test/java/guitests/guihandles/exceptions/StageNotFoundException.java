0c9b439a75b6e77841a2882cc2a296dad64bbcc3
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-10-19 15:49:47 +0800

    GUI Testing: Add GuiRobot.java as Robot to handle GUI Testing

---
+++ src/test/java/guitests/guihandles/exceptions/StageNotFoundException.java
@@ -0,0 +1,9 @@
+package guitests.guihandles.exceptions;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Signals that a stage cannot be found.
+ */
+public class StageNotFoundException extends NoSuchElementException  {
+}
