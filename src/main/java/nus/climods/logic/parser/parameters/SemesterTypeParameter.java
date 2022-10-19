ac6eb14cbc9df3bf35430178aebb11fb6e0b87dc
sarrrdin <sarrrdin@gmail.com>
2022-10-19 14:09:13 +0800

    Add new SemesterTypeParamteter to check parameter type.

---
+++ src/main/java/nus/climods/logic/parser/parameters/SemesterTypeParameter.java
@@ -0,0 +1,19 @@
+package nus.climods.logic.parser.parameters;
+
+import nus.climods.logic.parser.ParserUtil;
+
+public class SemesterTypeParameter extends PositionalParameter<String> {
+    public static final String INVALID_INPUT_MESSAGE = "Semester should not be empty!";
+    public static final String PARSE_EXCEPTION_MESSAGE = "Invalid Semester: %s";
+    private static final int LESSON_TYPE_INDEX = 1;
+
+    /**
+     * Creates a SemesterTypeParameter with argumentsString
+     *
+     * @param argumentsString Arguments string to parse lesson type from
+     */
+    public SemesterTypeParameter(String argumentsString) {
+        super(LESSON_TYPE_INDEX, argumentsString, ParserUtil::parseSemesterType, PARSE_EXCEPTION_MESSAGE);
+        this.setEmptyInputMessage(INVALID_INPUT_MESSAGE);
+    }
+}
