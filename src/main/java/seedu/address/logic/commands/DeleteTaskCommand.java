19141c39feb0976ba401fe485b2987491910cdcd
maxng17 <maxngjl@gmail.com>
2022-10-20 01:11:49 +0800

    Fix checkstyle to ensure code readability

--- src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -1,15 +1,18 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.task.Task;
 
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-
+/**
+ * DeleteTaskCommand is the command that deletes a task from the task list at the specified index.
+ */
 public class DeleteTaskCommand extends Command {
     public static final String COMMAND_WORD = "deltask";
 
