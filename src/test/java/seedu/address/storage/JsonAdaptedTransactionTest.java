6b904a0632e82d6a8e671b580b227d310272af58
ElginL <elginlee_2000@hotmail.com>
2022-10-19 13:14:52 +0800

    Update JsonAdaptedTransactionTest

--- src/test/java/seedu/address/storage/JsonAdaptedTransactionTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedTransactionTest.java
@@ -9,6 +9,7 @@ import static seedu.address.testutil.TypicalTransaction.SELL_PAPAYA;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.transaction.Date;
 import seedu.address.model.transaction.Goods;
 import seedu.address.model.transaction.Price;
 import seedu.address.model.transaction.Quantity;
@@ -18,10 +19,12 @@ public class JsonAdaptedTransactionTest {
     private static final String INVALID_GOODS = "@pple";
     private static final String INVALID_PRICE = "Eighteen Dollars";
     private static final String INVALID_QUANTITY = "Fifteen";
+    private static final String INVALID_DATE = "5th January 2021";
 
     private static final String VALID_GOODS = BUY_SHELVES.getGoods().toString();
     private static final String VALID_PRICE = Double.toString(BUY_SHELVES.getPrice().value());
     private static final String VALID_QUANTITY = BUY_SHELVES.getQuantity().toString();
+    private static final String VALID_DATE = "20/01/2000";
 
     @Test
     public void toModelType_validTransactionDetails_returnsTransaction() throws Exception {
@@ -35,7 +38,7 @@ public class JsonAdaptedTransactionTest {
     @Test
     public void toModelType_invalidGoods_throwsIllegalValueException() {
         JsonAdaptedTransaction transaction =
-                new JsonAdaptedTransaction(INVALID_GOODS, VALID_PRICE, VALID_QUANTITY, TransactionType.BUY);
+                new JsonAdaptedTransaction(INVALID_GOODS, VALID_PRICE, VALID_QUANTITY, TransactionType.BUY, VALID_DATE);
         String expectedMessage = Goods.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
     }
@@ -43,7 +46,7 @@ public class JsonAdaptedTransactionTest {
     @Test
     public void toModelType_nullGoods_throwsIllegalValueException() {
         JsonAdaptedTransaction transaction =
-                new JsonAdaptedTransaction(null, VALID_PRICE, VALID_QUANTITY, TransactionType.SELL);
+                new JsonAdaptedTransaction(null, VALID_PRICE, VALID_QUANTITY, TransactionType.SELL, VALID_DATE);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Goods.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
     }
@@ -51,7 +54,8 @@ public class JsonAdaptedTransactionTest {
     @Test
     public void toModelType_invalidPrice_throwsIllegalValueException() {
         JsonAdaptedTransaction transaction =
-                new JsonAdaptedTransaction(VALID_GOODS, INVALID_PRICE, VALID_QUANTITY, TransactionType.SELL);
+                new JsonAdaptedTransaction(VALID_GOODS, INVALID_PRICE, VALID_QUANTITY,
+                        TransactionType.SELL, VALID_DATE);
         String expectedMessage = Price.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
     }
@@ -59,7 +63,7 @@ public class JsonAdaptedTransactionTest {
     @Test
     public void toModelType_nullPrice_throwsIllegalValueException() {
         JsonAdaptedTransaction transaction =
-                new JsonAdaptedTransaction(VALID_GOODS, null, VALID_QUANTITY, TransactionType.BUY);
+                new JsonAdaptedTransaction(VALID_GOODS, null, VALID_QUANTITY, TransactionType.BUY, VALID_DATE);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Price.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
     }
@@ -67,7 +71,8 @@ public class JsonAdaptedTransactionTest {
     @Test
     public void toModelType_invalidQuantity_throwsIllegalValueException() {
         JsonAdaptedTransaction transaction =
-                new JsonAdaptedTransaction(VALID_GOODS, VALID_PRICE, INVALID_QUANTITY, TransactionType.SELL);
+                new JsonAdaptedTransaction(VALID_GOODS, VALID_PRICE, INVALID_QUANTITY,
+                        TransactionType.SELL, VALID_DATE);
         String expectedMessage = Quantity.MESSAGE_CONSTRAINTS;
         assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
     }
@@ -75,8 +80,24 @@ public class JsonAdaptedTransactionTest {
     @Test
     public void toModelType_nullQuantity_throwsIllegalValueException() {
         JsonAdaptedTransaction transaction =
-                new JsonAdaptedTransaction(VALID_GOODS, VALID_PRICE, null, TransactionType.BUY);
+                new JsonAdaptedTransaction(VALID_GOODS, VALID_PRICE, null, TransactionType.BUY, VALID_DATE);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Quantity.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDate_throwsIllegalValueException() {
+        JsonAdaptedTransaction transaction =
+                new JsonAdaptedTransaction(VALID_GOODS, VALID_PRICE, VALID_QUANTITY, TransactionType.BUY, INVALID_DATE);
+        String expectedMessage = Date.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullDate_throwsIllegalValueException() {
+        JsonAdaptedTransaction transaction =
+                new JsonAdaptedTransaction(VALID_GOODS, VALID_PRICE, VALID_QUANTITY, TransactionType.BUY, null);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Date.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, transaction::toModelType);
     }
 
