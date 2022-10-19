484e2ece2f6d8ca163c58c5fe352cd6777d6531b
Tex Tang <tex_tang@outlook.com>
2022-10-19 09:10:01 +0800

    Test: Fix checkStyle issues

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -2,7 +2,10 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
