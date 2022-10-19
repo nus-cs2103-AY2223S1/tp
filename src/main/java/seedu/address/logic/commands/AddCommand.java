e3dbe28b69f13f07d63ccec91d15c2d09af503e2
Tex Tang <tex_tang@outlook.com>
2022-10-19 09:20:39 +0800

    Command: Add contacts to usage

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,9 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -26,7 +24,11 @@ public class AddCommand extends Command {
         + PREFIX_NAME + "John Doe "
         + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
         + PREFIX_TAG + "friends "
-        + PREFIX_TAG + "owesMoney";
+        + PREFIX_TAG + "owesMoney"
+        + PREFIX_PHONE + "81828384"
+        + PREFIX_EMAIL + "test@example.com"
+        + PREFIX_SLACK + "helloalex"
+        + PREFIX_TELEGRAM + "@helloalex";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
