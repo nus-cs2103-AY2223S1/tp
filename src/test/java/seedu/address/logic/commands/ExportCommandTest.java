ac9b3ccc60e144565e157709806e4a863bf7ef2b
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-20 00:42:59 +0800

    Add ExportCommandTest

---
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ExportCommandTest {
+
+    @Test
+    public void execute_emptyShownList_success() {
+        Model model = new ModelManager();
+
+        assertDoesNotThrow(() -> new ExportCommand().execute(model));
+    }
+
+    @Test
+    public void execute_nonEmptyShownList_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        assertDoesNotThrow(() -> new ExportCommand().execute(model));
+    }
+}
