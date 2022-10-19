349986b352ccf82868497a06fa405e2cd9c04de7
chengda300 <chengda300@gmail.com>
2022-10-20 01:36:24 +0800

    Swap method order

--- src/test/java/seedu/address/testutil/Assert.java
+++ src/test/java/seedu/address/testutil/Assert.java
@@ -20,17 +20,6 @@ public class Assert {
     }
 
     /**
-     * Asserts that the {@code executable} does not throw any exceptions.
-     * This is a wrapper method that invokes {@link Assertions#assertDoesNotThrow(Executable)}, to maintain consistency
-     * with our custom {@link #assertThrows(Class, String, Executable)} method.
-     * To standardize API calls in this project, users should use this method instead of
-     * {@link Assertions#assertDoesNotThrow(Executable)}.
-     */
-    public static void assertDoesNotThrow(Executable executable) {
-        Assertions.assertDoesNotThrow(executable);
-    }
-
-    /**
      * Asserts that the {@code executable} throws the {@code expectedType} Exception with the {@code expectedMessage}.
      * If there's no need for the verification of the exception's error message, call
      * {@link #assertThrows(Class, Executable)} instead.
@@ -42,4 +31,16 @@ public class Assert {
         Throwable thrownException = Assertions.assertThrows(expectedType, executable);
         Assertions.assertEquals(expectedMessage, thrownException.getMessage());
     }
+
+    /**
+     * Asserts that the {@code executable} does not throw any exceptions.
+     * This is a wrapper method that invokes {@link Assertions#assertDoesNotThrow(Executable)}, to maintain consistency
+     * with our custom {@link #assertThrows(Class, String, Executable)} method.
+     * To standardize API calls in this project, users should use this method instead of
+     * {@link Assertions#assertDoesNotThrow(Executable)}.
+     */
+    public static void assertDoesNotThrow(Executable executable) {
+        Assertions.assertDoesNotThrow(executable);
+    }
+
 }
