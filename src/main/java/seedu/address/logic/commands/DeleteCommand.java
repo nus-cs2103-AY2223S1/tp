f207f9b3e3dcf5377b2cfbd876f4a5b51ffe20fd
cfyjoanna <joannachanfy@gmail.com>
2022-10-19 15:51:18 +0800

    Remove unnecessary import

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 
