84a22b99d00a69737c53b0a98253b947fef0514a
Tex Tang <tex_tang@outlook.com>
2022-10-19 09:28:36 +0800

    Logic: Fix checkStyle issues

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -53,8 +53,8 @@ public class AddCommandParser implements Parser<AddCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap =
-        ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_ROLE,
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_NAME, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_ROLE,
                 PREFIX_PHONE, PREFIX_EMAIL, PREFIX_SLACK, PREFIX_TELEGRAM);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_ROLE)
