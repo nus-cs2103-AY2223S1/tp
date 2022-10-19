0961e5b99ff45a3e4a861f5fa4fe0f2e5b8ee634
dlimyy <douglas.lim.yy@gmail.com>
2022-10-19 17:26:54 +0800

    Fix issue with test checkstyle
    Fixed issue with checkstyle as sortTaskList method was not implemented for
    stub.

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -135,6 +135,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void sortTaskList(String criteria) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
