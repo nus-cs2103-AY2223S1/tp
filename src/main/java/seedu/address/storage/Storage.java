3854c5ec562ff1a5678b7484425f2b74817d0785
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-20 00:00:36 +0800

    Add saveShownList method in Storage

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -2,12 +2,20 @@ package seedu.address.storage;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import javafx.collections.ObservableList;
 import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.commons.util.FileUtil;
+import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * API of the Storage component
@@ -28,5 +36,14 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage {
 
     @Override
     void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
+
+    static void saveShownList(ObservableList<Person> shownList, String filePathString) throws IOException {
+        requireNonNull(shownList);
+        requireNonNull(filePathString);
+
+        Path filePath = Paths.get(filePathString);
+        FileUtil.createIfMissing(filePath);
+        JsonUtil.saveJsonFile(shownList.stream().map(JsonAdaptedPerson::new).collect(Collectors.toList()), filePath);
+    };
 
 }
