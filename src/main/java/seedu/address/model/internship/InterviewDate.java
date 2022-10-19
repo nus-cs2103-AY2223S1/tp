0a138baca8c6e286ed0cba4beec7448761b3c972
chengda300 <chengda300@gmail.com>
2022-10-19 15:06:02 +0800

    Remove unused import

--- src/main/java/seedu/address/model/internship/InterviewDate.java
+++ src/main/java/seedu/address/model/internship/InterviewDate.java
@@ -1,6 +1,5 @@
 package seedu.address.model.internship;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDateTime;
