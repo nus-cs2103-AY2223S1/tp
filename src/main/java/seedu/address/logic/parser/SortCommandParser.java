356b0b808f0626561d81254a3b77a08beee02452
ShummyOwnzYou <jiehui285@gmail.com>
2022-10-19 18:37:34 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -2,13 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Comparator;
-
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.applicant.Applicant;
-import seedu.address.model.applicant.Name;
-import seedu.address.model.applicant.Scholarship;
 
 /**
  * Parses input arguments and creates a new SortCommand object
