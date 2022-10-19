0792b982887e48db5ed1eb4bfbbef7635d2cbbfa
albertarielw <albertariel2002@gmail.com>
2022-10-20 01:37:33 +0800

    fix storage bugs: exception called does not match issues

--- src/main/java/seedu/address/storage/JsonAdaptedTask.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTask.java
@@ -1,13 +1,10 @@
 package seedu.address.storage;
 
 import java.time.LocalDate;
-import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import seedu.address.MainApp;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Email;
 import seedu.address.model.task.Description;
@@ -23,7 +20,6 @@ import seedu.address.model.task.TaskName;
  * Jackson-friendly version of {@link Task}.
  */
 public class JsonAdaptedTask {
-    private static final Logger logger = LogsCenter.getLogger(MainApp.class);
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Task's %s field is missing!";
 
     private final String name;
@@ -107,9 +103,8 @@ public class JsonAdaptedTask {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     Description.class.getSimpleName()));
         }
-        logger.info("ASDASDASDA");
+        
         if (!Description.isValidTaskDescription(description)) {
-            logger.info("123232");
             throw new IllegalValueException(Description.MESSAGE_CONSTRAINTS);
         }
         final Description modelDescription = new Description(description);
