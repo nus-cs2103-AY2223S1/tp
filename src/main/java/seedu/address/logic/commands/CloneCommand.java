d2d2c842396c51e1b061ab6f04f05a700ba8e579
GQ-PC\gqins <gqinsg@gmail.com>
2022-10-19 10:10:06 +0800

    Revamp CloneCommand

--- src/main/java/seedu/address/logic/commands/CloneCommand.java
+++ src/main/java/seedu/address/logic/commands/CloneCommand.java
@@ -1,12 +1,14 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Address;
@@ -30,22 +32,39 @@ public class CloneCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clones the details of the person identified "
             + "by the index number used in the displayed person list. "
-            + "A new person with different name but same details will be added to the address book.\n"
-            + "Parameters: INDEX (must be a positive integer) \n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (the index of the person needs to be exist) "
+            + "[Optional parameters in square brackets] "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_GENDER + "GENDER] "
+            + "[" + PREFIX_BIRTHDATE + "BIRTHDATE] "
+            + "[" + PREFIX_RACE + "RACE] "
+            + "[" + PREFIX_RELIGION + "RELIGION] "
+            + "[" + PREFIX_SURVEY+ "SURVEY] "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_NAME + "John "
+            + PREFIX_PHONE + "91234567 "
+            + PREFIX_EMAIL + "johndoe@example.com";
 
     public static final String MESSAGE_CLONE_PERSON_SUCCESS = "Cloned Person: %1$s";
-
-    public static final String DUPLICATE_NAME_EXTENSION = " copy";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_CLONED_PERSON = "This person already exists in the address book, try again with different details";
 
     private final Index index;
+    private final ClonePersonDescriptor clonePersonDescriptor;
+
 
     /**
      * @param index of the person in the filtered person list to cloned
      */
-    public CloneCommand(Index index) {
+    public CloneCommand(Index index, ClonePersonDescriptor clonePersonDescriptor) {
         requireNonNull(index);
         this.index = index;
+        this.clonePersonDescriptor = new ClonePersonDescriptor(clonePersonDescriptor);
     }
 
     @Override
@@ -58,7 +77,12 @@ public class CloneCommand extends Command {
         }
 
         Person personToClone = lastShownList.get(index.getZeroBased());
-        Person clonedPerson = createClonedPerson(model, personToClone);
+        Person clonedPerson = createClonedPerson(personToClone, clonePersonDescriptor);
+
+        if (!personToClone.isSamePerson(clonedPerson) && model.hasPerson(clonedPerson)) {
+            throw new CommandException(MESSAGE_DUPLICATE_CLONED_PERSON);
+        }
+
         model.addPerson(clonedPerson);
         return new CommandResult(String.format(MESSAGE_CLONE_PERSON_SUCCESS, clonedPerson));
     }
@@ -66,27 +90,22 @@ public class CloneCommand extends Command {
     /**
      * Creates and returns a {@code Person} with the details of {@code personToClone}.
      */
-    private static Person createClonedPerson(Model model, Person personToClone) {
+    private static Person createClonedPerson(Person personToClone, ClonePersonDescriptor clonePersonDescriptor) {
         assert personToClone != null;
 
-        if (!model.hasPerson(personToClone)) {
-            return personToClone;
-        }
+        Name clonedName = clonePersonDescriptor.getName().orElse(personToClone.getName());
+        Phone clonedPhone = clonePersonDescriptor.getPhone().orElse(personToClone.getPhone());
+        Email clonedEmail = clonePersonDescriptor.getEmail().orElse(personToClone.getEmail());
+        Address clonedAddress = clonePersonDescriptor.getAddress().orElse(personToClone.getAddress());
+        Gender clonedGender = clonePersonDescriptor.getGender().orElse(personToClone.getGender());
+        Birthdate clonedBirthdate = clonePersonDescriptor.getBirthdate().orElse(personToClone.getBirthdate());
+        Race clonedRace = clonePersonDescriptor.getRace().orElse(personToClone.getRace());
+        Religion clonedReligion = clonePersonDescriptor.getReligion().orElse(personToClone.getReligion());
+        Survey clonedSurvey = clonePersonDescriptor.getSurvey().orElse(personToClone.getSurvey());
+        Set<Tag> clonedTags = clonePersonDescriptor.getTags().orElse(personToClone.getTags());
 
-        Name updatedName = new Name(personToClone.getName() + DUPLICATE_NAME_EXTENSION);
-        Phone clonedPhone = personToClone.getPhone();
-        Email clonedEmail = personToClone.getEmail();
-        Address clonedAddress = personToClone.getAddress();
-        Gender clonedGender = personToClone.getGender();
-        Birthdate clonedBirthdate = personToClone.getBirthdate();
-        Race clonedRace = personToClone.getRace();
-        Religion clonedReligion = personToClone.getReligion();
-        Survey clonedSurvey = personToClone.getSurvey();
-        Set<Tag> clonedTags = (personToClone.getTags() != null) ? personToClone.getTags() : null;
-
-        Person clonedPerson = new Person(updatedName, clonedPhone, clonedEmail, clonedAddress,
+        return new Person(clonedName, clonedPhone, clonedEmail, clonedAddress,
                 clonedGender, clonedBirthdate, clonedRace, clonedReligion, clonedSurvey, clonedTags);
-        return createClonedPerson(model, clonedPerson);
     }
 
     @Override
@@ -103,6 +122,166 @@ public class CloneCommand extends Command {
 
         // state check
         CloneCommand e = (CloneCommand) other;
-        return index.equals(e.index);
+        return index.equals(e.index)
+                && clonePersonDescriptor.equals(e.clonePersonDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class ClonePersonDescriptor {
+        private Name name;
+        private Phone phone;
+        private Email email;
+        private Address address;
+        private Gender gender;
+        private Birthdate birthdate;
+        private Race race;
+        private Religion religion;
+        private Survey survey;
+        private Set<Tag> tags;
+
+        public ClonePersonDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public ClonePersonDescriptor(CloneCommand.ClonePersonDescriptor toCopy) {
+            setName(toCopy.name);
+            setPhone(toCopy.phone);
+            setEmail(toCopy.email);
+            setAddress(toCopy.address);
+            setGender(toCopy.gender);
+            setBirthdate(toCopy.birthdate);
+            setRace(toCopy.race);
+            setReligion(toCopy.religion);
+            setSurvey(toCopy.survey);
+            setTags(toCopy.tags);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, gender, birthdate, race, religion, survey, tags);
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = phone;
+        }
+
+        public Optional<Phone> getPhone() {
+            return Optional.ofNullable(phone);
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+        }
+
+        public Optional<Email> getEmail() {
+            return Optional.ofNullable(email);
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        public Optional<Address> getAddress() {
+            return Optional.ofNullable(address);
+        }
+
+        public void setGender(Gender gender) {
+            this.gender = gender;
+        }
+
+        public Optional<Gender> getGender() {
+            return Optional.ofNullable(gender);
+        }
+
+        public void setBirthdate(Birthdate birthdate) {
+            this.birthdate = birthdate;
+        }
+
+        public Optional<Birthdate> getBirthdate() {
+            return Optional.ofNullable(birthdate);
+        }
+
+        public void setRace(Race race) {
+            this.race = race;
+        }
+
+        public Optional<Race> getRace() {
+            return Optional.ofNullable(race);
+        }
+
+        public void setReligion(Religion religion) {
+            this.religion = religion;
+        }
+
+        public Optional<Religion> getReligion() {
+            return Optional.ofNullable(religion);
+        }
+
+        public void setSurvey(Survey survey) {
+            this.survey = survey;
+        }
+
+        public Optional<Survey> getSurvey() {
+            return Optional.ofNullable(survey);
+        }
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<Tag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof CloneCommand.ClonePersonDescriptor)) {
+                return false;
+            }
+
+            // state check
+            CloneCommand.ClonePersonDescriptor e = (CloneCommand.ClonePersonDescriptor) other;
+
+            return getName().equals(e.getName())
+                    && getPhone().equals(e.getPhone())
+                    && getEmail().equals(e.getEmail())
+                    && getAddress().equals(e.getAddress())
+                    && getGender().equals(e.getGender())
+                    && getBirthdate().equals(e.getBirthdate())
+                    && getRace().equals(e.getRace())
+                    && getReligion().equals(e.getReligion())
+                    && getSurvey().equals(e.getSurvey())
+                    && getTags().equals(e.getTags());
+        }
     }
 }
--- src/main/java/seedu/address/logic/parser/CloneCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CloneCommandParser.java
@@ -2,10 +2,18 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CloneCommand;
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Parses input arguments and creates a new CloneCommand object
@@ -14,17 +22,73 @@ public class CloneCommandParser implements Parser<CloneCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the CloneCommand
-     * and returns a CloneCommand object for execution.
+     * and returns an CloneCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public CloneCommand parse(String args) throws ParseException {
         requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                        PREFIX_GENDER, PREFIX_BIRTHDATE, PREFIX_RACE, PREFIX_RELIGION, PREFIX_SURVEY, PREFIX_TAG);
+
+        Index index;
+
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new CloneCommand(index);
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CloneCommand.MESSAGE_USAGE), pe);
         }
 
+        CloneCommand.ClonePersonDescriptor clonePersonDescriptor = new CloneCommand.ClonePersonDescriptor();
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            clonePersonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+        }
+        if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
+            clonePersonDescriptor.setPhone(ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
+            clonePersonDescriptor.setEmail(ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()));
+        }
+        if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
+            clonePersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
+        }
+        if (argMultimap.getValue(PREFIX_GENDER).isPresent()) {
+            clonePersonDescriptor.setGender(ParserUtil.parseGender(argMultimap.getValue(PREFIX_GENDER).get()));
+        }
+        if (argMultimap.getValue(PREFIX_BIRTHDATE).isPresent()) {
+            clonePersonDescriptor.setBirthdate(ParserUtil.parseBirthdate(argMultimap.getValue(PREFIX_BIRTHDATE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_RACE).isPresent()) {
+            clonePersonDescriptor.setRace(ParserUtil.parseRace(argMultimap.getValue(PREFIX_RACE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_RELIGION).isPresent()) {
+            clonePersonDescriptor.setReligion(ParserUtil.parseReligion(argMultimap.getValue(PREFIX_RELIGION).get()));
+        }
+        if (argMultimap.getValue(PREFIX_SURVEY).isPresent()) {
+            clonePersonDescriptor.setSurvey(ParserUtil.parseSurvey(argMultimap.getValue(PREFIX_SURVEY).get()));
+        }
+        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(clonePersonDescriptor::setTags);
+
+        if (!clonePersonDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(CloneCommand.MESSAGE_NOT_EDITED);
+        }
+
+        return new CloneCommand(index, clonePersonDescriptor);
     }
+
+    /**
+     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
+     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
+     * {@code Set<Tag>} containing zero tags.
+     */
+    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
+        assert tags != null;
+
+        if (tags.isEmpty()) {
+            return Optional.empty();
+        }
+        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
+        return Optional.of(ParserUtil.parseTags(tagSet));
+    }
+
 }
