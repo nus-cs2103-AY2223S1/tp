8d7c4b136ff6da3a8363b1003c8cd7a13bacff44
yunruu <yunruuchin@gmail.com>
2022-10-19 15:56:00 +0800

    Feat: add TaskListPanel to ui

---
+++ src/main/java/swift/ui/TaskListPanel.java
@@ -0,0 +1,49 @@
+package swift.ui;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import swift.commons.core.LogsCenter;
+import swift.model.task.Task;
+
+import java.util.logging.Logger;
+
+/**
+ * Panel containing the list of tasks.
+ */
+public class TaskListPanel extends UiPart<Region> {
+    private static final String FXML = "TaskListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+
+    @FXML
+    private ListView<Task> taskListView;
+
+    /**
+     * Creates a {@code TaskListPanel} with the given {@code ObservableList}.
+     */
+    public TaskListPanel(ObservableList<Task> taskList) {
+        super(FXML);
+        taskListView.setItems(taskList);
+        taskListView.setCellFactory(listView -> new TaskListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Task} using a {@code TaskCard}.
+     */
+    class TaskListViewCell extends ListCell<Task> {
+        @Override
+        protected void updateItem(Task task, boolean empty) {
+            super.updateItem(task, empty);
+
+            if (empty || task == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new TaskCard(task, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
