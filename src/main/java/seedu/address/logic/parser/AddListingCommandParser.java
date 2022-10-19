66b468890b8efb9509fa05be28159333be266b9a
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2022-10-20 00:39:56 +0800

    Revert "Fix UI for Add Listing Command"

--- src/main/java/seedu/address/logic/parser/AddListingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddListingCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ASKING_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
 
 import java.util.stream.Stream;
 
@@ -25,9 +26,9 @@ public class AddListingCommandParser implements Parser<AddListingCommand> {
      */
     public AddListingCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_ID, PREFIX_ADDRESS, PREFIX_NAME, PREFIX_ASKING_PRICE);
+                ArgumentTokenizer.tokenize(args, PREFIX_ID, PREFIX_ADDRESS, PREFIX_PERSON, PREFIX_ASKING_PRICE);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_ID, PREFIX_ADDRESS, PREFIX_NAME, PREFIX_ASKING_PRICE)
+        if (!arePrefixesPresent(argMultimap, PREFIX_ID, PREFIX_ADDRESS, PREFIX_PERSON, PREFIX_ASKING_PRICE)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddListingCommand.MESSAGE_USAGE));
         }
@@ -35,7 +36,7 @@ public class AddListingCommandParser implements Parser<AddListingCommand> {
         String id = argMultimap.getValue(PREFIX_ID).get();
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
-        int askingPrice = Integer.parseInt(argMultimap.getValue(PREFIX_ASKING_PRICE).get());
+        int askingPrice = Integer.parseInt(String.valueOf(argMultimap.getValue(PREFIX_ASKING_PRICE)));
 
         return new AddListingCommand(id, address, name, askingPrice);
     }
