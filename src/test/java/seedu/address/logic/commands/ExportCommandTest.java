e8d1525885bd7a312b8781500f8271a0a66952c7
cliftonfelix <cliftonfelix12@yahoo.com>
2022-10-20 01:03:06 +0800

    Remove JavaDoc in ExportCommandTest

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -9,9 +9,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-/**
- * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
- */
 public class ExportCommandTest {
 
     @Test
