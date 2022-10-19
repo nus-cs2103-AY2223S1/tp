218dfd94856306c76e3801b6b23f2ada98030261
Yap Zhi Heng <yapzhhg@gmail.com>
2022-10-19 20:41:58 +0800

    Add Reminder list to TargetPersonCard

--- src/main/java/seedu/address/ui/TargetPersonCard.java
+++ src/main/java/seedu/address/ui/TargetPersonCard.java
@@ -1,9 +1,13 @@
 package seedu.address.ui;
 
+import java.time.LocalDateTime;
 import java.util.Comparator;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -25,12 +29,12 @@ public class TargetPersonCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
-    public final Person person;
-
     private static final String NO_REMARKS_VALUE = "No remarks";
 
     private static final String ITALIC_FONT_STYLE = "-fx-font-style: italic";
     private static final String NORMAL_FONT_STYLE = "-fx-font-style: normal";
+
+    public final Person person;
 
     @FXML
     private HBox cardPane;
@@ -46,6 +50,8 @@ public class TargetPersonCard extends UiPart<Region> {
     private Label remark;
     @FXML
     private FlowPane tags;
+    @FXML
+    private ListView<TempReminder> reminderView;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -67,6 +73,17 @@ public class TargetPersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+
+        ObservableList<TempReminder> remindersPlaceholder = FXCollections.observableArrayList(
+            new TempReminder("Reminder 1", LocalDateTime.now()),
+            new TempReminder("Reminder 2", LocalDateTime.now().plusDays(1)),
+            new TempReminder("Reminder 3 which is a very long reminder just to test"
+                    + " whether the text UI component will warp.", LocalDateTime.now().plusDays(4)),
+            new TempReminder("Reminder 4", LocalDateTime.now().plusDays(3)),
+            new TempReminder("Reminder 5", LocalDateTime.now().plusDays(2))
+        );
+        reminderView.setItems(remindersPlaceholder);
+        reminderView.setCellFactory(listView -> new TempReminderListViewCell());
     }
 
     @Override
