6f33bbb3d7887600eb9efec5a6a9f2ad1d629bf5
Yeo Jun Jie <54196980+yeojunjie@users.noreply.github.com>
2022-10-20 12:48:08 +0800

    Clicking on a `TaskListCard` now shows the `Student`s which are associated with the `Task`. (#100)
    * Remove showMore
    
    showMore is responsible for the "Click again to expand" helper text in the UI.
    
    * Rename taskCard to optionalInfo
    
    * Rename isToggle to isExpanded
    
    * Update comments
    
    * Show Students assigned to each Task
    
    * Make clicking on Tasks with no Students do nothing
    
    * Amend <code>X</code> to {@code X}
    
    * Remove unnecessary setText()
    
    * Reorder import statements to placate checkstyle
    
    * Add one empty line between import statements to placate checkstyle

--- src/main/java/seedu/address/ui/TaskListCard.java
+++ src/main/java/seedu/address/ui/TaskListCard.java
@@ -1,13 +1,16 @@
 package seedu.address.ui;
 
+import java.util.Set;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import seedu.address.model.student.Student;
 import seedu.address.model.task.Task;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Task}.
  */
 public class TaskListCard extends UiPart<Region> {
 
@@ -16,9 +19,7 @@ public class TaskListCard extends UiPart<Region> {
     public final Task task;
 
     @FXML
-    private VBox cardPane;
-    @FXML
-    private VBox taskCard;
+    private VBox cardPane; // Think of this as the entire TaskListCard.
     @FXML
     private Label id;
     @FXML
@@ -29,24 +30,46 @@ public class TaskListCard extends UiPart<Region> {
     private Label deadline;
     @FXML
     private Label completion;
+    private boolean isExpanded;
     @FXML
-    private Label showMore;
-
-    private boolean isToggle;
+    private VBox optionalInfo; // This is the optional information to be displayed.
+    @FXML
+    private Label studentsHeading;
+    @FXML
+    private Label studentsBodyText;
 
     /**
      * Creates a {@code TaskListCard} with the given {@code Task} and index to display.
      */
-    public TaskListCard(Task task, int displayedIndex, boolean isToggle) {
+    public TaskListCard(Task task, int displayedIndex, boolean isExpanded) {
         super(FXML);
         this.task = task;
         id.setText(displayedIndex + ".");
         name.setText(String.valueOf(task.getTaskName()));
         description.setText(String.valueOf(task.getTaskDescription()));
         deadline.setText(String.format("Due by %s", task.getTaskDeadline()));
-        completion.setText("10% completed (3/30 students)"); // Dummy text.
-        this.isToggle = isToggle;
-        showMore.setVisible(false);
+
+        Set<Student> setOfStudents = task.getStudents();
+
+        if (setOfStudents.size() == 0) {
+            completion.setText("No students are assigned to this task.");
+            studentsHeading.setVisible(false);
+            studentsBodyText.setVisible(false);
+        } else {
+            // Populate optionalInfo with the relevant information.
+            completion.setText(String.format("??%% completed (??/%d) students", setOfStudents.size()));
+            studentsHeading.setText("List of Students:");
+            StringBuilder studentsBodyTextString = new StringBuilder();
+            for (Student student : setOfStudents) {
+                studentsBodyTextString.append(String.format("[?] %s\n", student.getName()));
+            }
+            studentsBodyText.setText(studentsBodyTextString.toString());
+        }
+
+
+
+        this.isExpanded = isExpanded;
+        // Use the isExpanded parameter to determine whether to (immediately) show the optional information.
         onCardClicked();
     }
 
@@ -69,19 +92,25 @@ public class TaskListCard extends UiPart<Region> {
     }
 
     /**
-     * Toggles the task list card by expanding or minimizing.
+     * When the {@code TaskListCard} is clicked, it toggles between showing and not showing the students for whom
+     * the {@code Task} has been completed. However, if no {@code Student}s are assigned to the {@code Task}, then the
+     * {@code TaskListCard} will not expand, since there is no information to display.
      */
     @FXML
     public void onCardClicked() {
-        isToggle = !isToggle;
-        if (isToggle) {
-            taskCard.setVisible(true);
-            taskCard.setManaged(true);
-            System.out.println("Toggled");
+
+        if (this.task.getStudents().size() == 0) {
+            return;
+        }
+
+        isExpanded = !isExpanded;
+
+        if (isExpanded) {
+            optionalInfo.setVisible(true);
+            optionalInfo.setManaged(true);
         } else {
-            taskCard.setVisible(false);
-            taskCard.setManaged(false);
-            System.out.println("UnToggled");
+            optionalInfo.setVisible(false);
+            optionalInfo.setManaged(false);
         }
     }
 }
