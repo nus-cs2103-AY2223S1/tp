b0ea3eb93fa197908f7654cbfb956fa3d3ef2263
richavm14 <richavm14@gmail.com>
2022-10-20 21:07:50 +0800

    Remove unused import statement

--- src/main/java/seedu/address/logic/parser/AssignMemberParser.java
+++ src/main/java/seedu/address/logic/parser/AssignMemberParser.java
@@ -3,11 +3,11 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.ArgumentMultimap.arePrefixesPresent;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAM_INDEX;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AssignMemberCommand;
-import seedu.address.logic.commands.TaskAddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
