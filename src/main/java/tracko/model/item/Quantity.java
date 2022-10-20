c755af8ff02a681ec6de19f96361f97da5732317
donljh <don.limjunhong@gmail.com>
2022-10-20 20:17:22 +0800

    Update Quantity constraint message

--- src/main/java/tracko/model/item/Quantity.java
+++ src/main/java/tracko/model/item/Quantity.java
@@ -8,7 +8,7 @@ import static tracko.commons.util.CollectionUtil.requireAllNonNull;
  */
 public class Quantity {
     public static final String MESSAGE_CONSTRAINTS = "Quantity should be valid."
-            + "\nA valid Quantity value should not non-empty, non-negative, and should not exceed 2,147,483,647.";
+            + "\nA valid Quantity value should be non-empty, non-negative, and should not exceed 2,147,483,647.";
 
     public final Integer quantity;
 
