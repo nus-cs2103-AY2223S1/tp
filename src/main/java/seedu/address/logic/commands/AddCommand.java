ab9972a357a0b250502d54604c14cf13c9ed3772
avock <chunkhai.k@gmail.com>
2022-10-20 22:31:34 +0800

    Small bug fix

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -51,7 +51,6 @@ public class AddCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        assert false;
         if (model.hasPerson(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
