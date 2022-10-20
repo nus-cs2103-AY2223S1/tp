2529a89347fedbbfd0465f592adb2e1df77a9901
Paul Lee <85117239+paullee18@users.noreply.github.com>
2022-10-20 19:08:21 +0800

    Update Person.java

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -52,9 +52,9 @@ public class Person {
      * Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address,
-                Birthday birthday, Insurance healthInsurance, Insurance disabilityInsurance,
-                Insurance criticalIllnessInsurance, Insurance lifeInsurance,
-                Reminder reminders, Set<Tag> tags) {
+                  Birthday birthday, Insurance healthInsurance, Insurance disabilityInsurance,
+                  Insurance criticalIllnessInsurance, Insurance lifeInsurance,
+                  Reminder reminders, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
