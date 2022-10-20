9d41f5ff0bd8a6ad0cd42e5b999dd7db7e56262d
nmtuan2001 <nmtuan2001@gmail.com>
2022-10-20 23:56:00 +0800

    Fix interview list not updated bug after edit and clear

--- src/main/java/seedu/application/logic/commands/ClearCommand.java
+++ src/main/java/seedu/application/logic/commands/ClearCommand.java
@@ -18,6 +18,7 @@ public class ClearCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setApplicationBook(new ApplicationBook());
+        model.updateApplicationListWithInterview();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
