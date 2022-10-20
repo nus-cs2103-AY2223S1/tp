bd51483eb0d592d4b6212bb8cd241e9c49238407
chao890 <chaoyungchor@gmail.com>
2022-10-20 16:42:12 +0800

    Fix linting error

--- src/test/java/coydir/model/person/PhoneTest.java
+++ src/test/java/coydir/model/person/PhoneTest.java
@@ -31,7 +31,7 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
-        assertFalse(Phone.isValidPhone("*()&*")); // non-alphanumeric 
+        assertFalse(Phone.isValidPhone("*()&*")); // non-alphanumeric
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
