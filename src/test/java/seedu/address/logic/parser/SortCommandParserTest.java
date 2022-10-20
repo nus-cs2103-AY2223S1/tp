f3e5c1b00b4fb13d78a4cca5089f2fb8b9a2fcb8
DarrenCsAcc <darrentng@yahoo.com.sg>
2022-10-20 13:34:13 +0800

    Added SortCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.model.applicant.Applicant;
+
+public class SortCommandParserTest {
+    private SortCommandParser parser = new SortCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSortCommand() {
+        SortCommand expectedNameSortCommand =
+            new SortCommand(Applicant.sortByName());
+        SortCommand expectedScholarshipSortCommand =
+            new SortCommand(Applicant.sortByScholarship());
+
+        // no leading and trailing whitespaces
+        assertParseSuccess(parser, "name", expectedNameSortCommand);
+        assertParseSuccess(parser, "scholarship", expectedScholarshipSortCommand);
+
+        // with multiple trailing and leading whitespaces
+        assertParseSuccess(parser, "\n name \n", expectedNameSortCommand);
+        assertParseSuccess(parser, "\n scholarship \n", expectedScholarshipSortCommand);
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsError() {
+        assertParseFailure(parser, "ILoveSoftwareEngineering",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        //Throws error when sort by phone
+        assertParseFailure(parser, "phone",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        //Throws error when sort by email
+        assertParseFailure(parser, "email",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        //Throw error when sort by applicationStatuses
+        assertParseFailure(parser, "applicationStatus",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyArg_throwsError() {
+        assertParseFailure(parser, "     ",
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+}
