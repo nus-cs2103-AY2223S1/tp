035e9d22e111d72c39b0d4b339f3285a394ff7ae
ElginL <elginlee_2000@hotmail.com>
2022-10-20 12:15:52 +0800

    Update TransactionLog, TransactionLogTest
    Add method to delete a transaction in the log by index, and get the
    size of transaction log

--- src/test/java/seedu/address/model/transaction/TransactionLogTest.java
+++ src/test/java/seedu/address/model/transaction/TransactionLogTest.java
@@ -3,6 +3,10 @@ package seedu.address.model.transaction;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTransaction.BUY_BOOKS;
+import static seedu.address.testutil.TypicalTransaction.BUY_ORANGE;
+import static seedu.address.testutil.TypicalTransaction.BUY_SHELVES;
+import static seedu.address.testutil.TypicalTransaction.SELL_CLOTHES;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,4 +74,27 @@ class TransactionLogTest {
         assertEquals(log.asUnmodifiableObservableList(), FXCollections.unmodifiableObservableList(internalList));
     }
 
+    @Test
+    public void deleteByIndex_removesCorrectTransaction_success() {
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(BUY_BOOKS);
+        transactions.add(SELL_CLOTHES);
+
+        TransactionLog transactionLog = new TransactionLog(transactions);
+        Transaction deletedTransaction = transactionLog.deleteTransaction(1);
+
+        assertEquals(deletedTransaction, SELL_CLOTHES);
+        assertEquals(transactionLog.size(), 1);
+    }
+
+    public void size_returnsCorrectSize_success() {
+        TransactionLog transactionLog = new TransactionLog();
+        assertEquals(transactionLog.size(), 0);
+
+        transactionLog.addTransaction(BUY_ORANGE);
+        assertEquals(transactionLog.size(), 1);
+
+        transactionLog.addTransaction(BUY_SHELVES);
+        assertEquals(transactionLog.size(), 2);
+    }
 }
