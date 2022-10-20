06b3119893e4ecf5d40cd509a9ec02b3162a934f
L1uY1jun <yijunlau@gmail.com>
2022-10-21 07:30:42 +0800

    Create skeleton for Remove Command developer guide documentation

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -4,6 +4,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.applicant.ApplicationStatus;
 import seedu.address.model.applicant.ApplicationStatusPredicate;
 
 /**
@@ -18,15 +19,11 @@ public class FilterCommandParser implements Parser<FilterCommand> {
      */
     public FilterCommand parse(String args) throws ParseException {
         String trimmedArgs = args.toLowerCase().trim();
-        switch (trimmedArgs) {
-        case "pending":
-        case "accepted":
-        case "rejected":
+        if (ApplicationStatus.isValidApplicationStatus(trimmedArgs))
             return new FilterCommand(new ApplicationStatusPredicate(trimmedArgs));
-        default:
+        else {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
         }
     }
-
 }
