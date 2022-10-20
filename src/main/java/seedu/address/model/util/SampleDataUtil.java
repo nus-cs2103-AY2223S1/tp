365282f8e5d56b6a4b2320aa8ed3ee5b979fcfd7
ThomasHoooo <e0725843@u.nus.edu>
2022-10-20 15:21:14 +0800

    Fix unable to start at clean state bug

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -20,17 +20,17 @@ public class SampleDataUtil {
     public static Client[] getSampleClients() {
         return new Client[] {
             new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), getProductSet("")),
+                new Address("Blk 30 Geylang Street 29, #06-40"), getProductSet("Product1")),
             new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getProductSet("")),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getProductSet("Product2")),
             new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), getProductSet("")),
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), getProductSet("Product3")),
             new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), getProductSet("")),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), getProductSet("Product4")),
             new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), getProductSet("")),
+                new Address("Blk 47 Tampines Street 20, #17-35"), getProductSet("Product5")),
             new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), getProductSet(""))
+                new Address("Blk 45 Aljunied Street 85, #11-31"), getProductSet("Product6"))
         };
     }
 
