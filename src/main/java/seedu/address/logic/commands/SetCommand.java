2cc700849704b9abb8aeefcb5ad4999199c46f3a
wrewsama <andrewlzs512@gmail.com>
2022-10-20 15:46:15 +0800

    logic: remove unnecessary comment

--- src/main/java/seedu/address/logic/commands/SetCommand.java
+++ src/main/java/seedu/address/logic/commands/SetCommand.java
@@ -36,7 +36,6 @@ import seedu.address.ui.MainPanelName;
 public class SetCommand extends Command {
     public static final String COMMAND_WORD = "set";
 
-    // TODO: update with every single possible prefix
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sets a person's Contacts. \n"
             + "Parameters: "
             + PREFIX_NAME + "NAME"
