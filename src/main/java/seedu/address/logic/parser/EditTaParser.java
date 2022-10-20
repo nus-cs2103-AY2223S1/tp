9573ee1953d6c9a32100c420b3c6ecac4db91dce
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-10-20 19:45:57 +0800

    Update javadoc issues

--- src/main/java/seedu/address/logic/parser/EditTaParser.java
+++ src/main/java/seedu/address/logic/parser/EditTaParser.java
@@ -26,13 +26,13 @@ import seedu.address.model.module.ModuleCode;
 import seedu.address.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new EditCommand object
+ * Parses input arguments and creates a new EditTeachingAssistantCommand object
  */
 public class EditTaParser implements Parser<EditTeachingAssistantCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the EditCommand
-     * and returns an EditCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the EditTeachingAssistantCommand
+     * and returns an EditTeachingAssistantCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditTeachingAssistantCommand parse(String args) throws ParseException {
