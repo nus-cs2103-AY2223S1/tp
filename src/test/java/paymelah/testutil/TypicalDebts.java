e69c5ceb46de81fac0a06269bd6b17f269ac67cd
waynezsy <waynezsy0317@gmail.com>
2022-10-21 02:57:21 +0800

    Update TypicalDebts

--- src/test/java/paymelah/testutil/TypicalDebts.java
+++ src/test/java/paymelah/testutil/TypicalDebts.java
@@ -9,15 +9,15 @@ public class TypicalDebts {
     public static final Debt MCDONALDS = new DebtBuilder().withDescription("McDonald's")
             .withMoney("10.80").withDate("2022-10-11").withTime("12:34").build();
     public static final Debt CHICKEN_RICE = new DebtBuilder().withDescription("chicken rice")
-            .withMoney("5.60").withDate("2022-10-12").withTime("00:00").build();
+            .withMoney("5.60").withDate("2022-10-6").withTime("9:08").build();
     public static final Debt SUPPER = new DebtBuilder().withDescription("supper jio")
-            .withMoney("$4.5").withDate("2022-10-12").withTime("00:00").build();
+            .withMoney("$4.5").withDate("2022-9-12").withTime("00:00").build();
     public static final Debt TAOBAO = new DebtBuilder().withDescription("Taobao group buy")
-            .withMoney("34.52").withDate("2022-10-12").withTime("00:00").build();
+            .withMoney("34.52").withDate("2022-10-12").withTime("10:00").build();
     public static final Debt BURGER = new DebtBuilder().withDescription("burger king")
             .withMoney("10.40").withDate("2022-09-28").withTime("00:00").build();
     public static final Debt MEALS = new DebtBuilder().withDescription("Volunteer meal")
-            .withMoney("5").withDate("2022-10-12").withTime("00:00").build();
+            .withMoney("5").withDate("2022-10-06").withTime("09:44").build();
     public static final Debt KFC_BURGER = new DebtBuilder().withDescription("kfc zinger burger")
             .withMoney("$4.5").withDate("2022-10-11").withTime("18:23").build();
     public static final Debt COOP = new DebtBuilder().withDescription("coop textbooks")
