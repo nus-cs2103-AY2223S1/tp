ceb9368ada6d0d066b7eb894912fd94669897745
KSHan29 <seow.han@outlook.com>
2022-10-20 16:17:02 +0800

    Refactor file structure for commands test files

--- src/test/java/seedu/address/logic/commands/event/AddEventCommandTest.java
+++ src/test/java/seedu/address/logic/commands/event/AddEventCommandTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.event;
 
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.event.AddEventCommand;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ModelStub;
