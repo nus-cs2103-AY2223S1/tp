041ba47ee1b06441b7fb13d3681910d247a7bfe2
Tan How Suen <howsuen@gmail.com>
2022-10-20 21:57:03 +0800

    Create Grades and GradesList classes
    Grade represents an examination/assessment and the grade scored by
    a student, in terms of percentage (from 0% to 100%).
    Currently, there is a fixed list of assessments, namely RA1, RA2,
    Mid-terms, Practical, and Finals.
    GradesList represents a list of all the possible Grades.

---
+++ src/main/java/friday/model/grades/GradesList.java
@@ -0,0 +1,37 @@
+package friday.model.grades;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GradesList {
+    public final Map<String, Grade> gradesMap;
+    public static final String[] EXAMS = {"RA1", "Mid-Term", "RA2", "Practical", "Final"};
+
+    public GradesList() {
+        this.gradesMap = new HashMap<>();
+        initialiseGrades(gradesMap);
+    }
+
+    public void initialiseGrades(Map<String, Grade> gradesMap) {
+        for (String exam : EXAMS) {
+            gradesMap.putIfAbsent(exam, new Grade(exam, "0"));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return gradesMap.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof GradesList // instanceof handles nulls
+                && gradesMap.equals(((GradesList) other).gradesMap)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return gradesMap.hashCode();
+    }
+}
