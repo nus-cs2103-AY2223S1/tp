bd6a9599a92b11208de65d060c98292cb880f68a
KSHan29 <seow.han@outlook.com>
2022-10-20 20:22:47 +0800

    Add test for equals method in ViewUpcomingEventsCommand

--- src/test/java/seedu/address/logic/commands/event/ViewUpcomingEventsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/event/ViewUpcomingEventsCommandTest.java
@@ -1,4 +1,32 @@
 package seedu.address.logic.commands.event;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class ViewUpcomingEventsCommandTest {
+    private static final int ONE_DAY = 1;
+    private static final int FIVE_DAYS = 5;
+
+    @Test
+    public void equals() {
+        ViewUpcomingEventsCommand viewUpcomingFirstCommand = new ViewUpcomingEventsCommand(ONE_DAY);
+        ViewUpcomingEventsCommand viewUpcomingSecondCommand = new ViewUpcomingEventsCommand(FIVE_DAYS);
+
+        // same values -> returns true
+        assertTrue(viewUpcomingFirstCommand.equals(new ViewUpcomingEventsCommand(ONE_DAY)));
+
+        // same object -> returns true
+        assertTrue(viewUpcomingFirstCommand.equals(viewUpcomingFirstCommand));
+
+        // null -> returns false
+        assertFalse(viewUpcomingFirstCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(viewUpcomingFirstCommand.equals(1));
+
+        // different value -> returns false
+        assertFalse(viewUpcomingFirstCommand.equals(viewUpcomingSecondCommand));
+    }
 }
