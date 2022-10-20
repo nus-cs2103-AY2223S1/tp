a76f2cc73838a82fc79b855d181ed7886b6c22af
JustinS00 <justinsiaw00@gmail.com>
2022-10-21 01:19:03 +0800

    Add functionality for reminder

--- src/main/java/seedu/phu/ui/ReminderBarFooter.java
+++ src/main/java/seedu/phu/ui/ReminderBarFooter.java
@@ -1,13 +1,15 @@
 package seedu.phu.ui;
 
+import java.time.LocalDate;
+import java.util.function.Predicate;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.layout.Region;
+import seedu.phu.commons.util.StringUtil;
 import seedu.phu.model.ReadOnlyInternshipBook;
 import seedu.phu.model.internship.Internship;
 
-import java.nio.file.Paths;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
@@ -16,11 +18,12 @@ public class ReminderBarFooter extends UiPart<Region> {
 
     private static final String FXML = "ReminderBarFooter.fxml";
 
+    private static final String DEFAULT_REMINDER_TEXT = "In the next 7 days, you have: ";
+    private static final String PROCESS_ASSESSMENT = "ASSESSMENT";
+    private static final String PROCESS_INTERVIEW = "INTERVIEW";
+
     @FXML
     private Label reminderStatus;
-
-    private final static String DEFAULT_REMINDER_TEXT = "In the next 7 days, you have: ";
-    private int counter = 0;
 
     /**
      * Creates a {@code ReminderBarFooter}
@@ -31,7 +34,22 @@ public class ReminderBarFooter extends UiPart<Region> {
     }
 
     public void setReminderText(ReadOnlyInternshipBook book) {
-        reminderStatus.setText(String.valueOf(counter++));
+        String upcomingAssessments = getStatusCount(PROCESS_ASSESSMENT, book) + " " + PROCESS_ASSESSMENT + " ";
+        String upcomingInterviews = getStatusCount(PROCESS_INTERVIEW, book) + " " + PROCESS_INTERVIEW + " ";
+        reminderStatus.setText(DEFAULT_REMINDER_TEXT + upcomingAssessments + upcomingInterviews);
+    }
+
+    private int getStatusCount(String ap, ReadOnlyInternshipBook book) {
+
+        return book.getInternshipList().filtered(new Predicate<Internship>() {
+            @Override
+            public boolean test(Internship internship) {
+                return LocalDate.now().isBefore(internship.getDate().value)
+                        && internship.getDate().value.isBefore(LocalDate.now().plusDays(7))
+                        && StringUtil.containsWordIgnoreCase(
+                                String.valueOf(internship.getApplicationProcess().value), ap);
+            }
+        }).size();
     }
 
 }
