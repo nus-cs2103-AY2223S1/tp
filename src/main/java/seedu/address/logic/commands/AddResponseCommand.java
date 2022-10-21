69be8599248d850a9bb53c19f4d27f77c8349396
elyseleong <elyse.xuan@gmail.com>
2022-10-21 22:13:45 +0800

    Fix Java CI

--- src/main/java/seedu/address/logic/commands/AddResponseCommand.java
+++ src/main/java/seedu/address/logic/commands/AddResponseCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MESSAGE_COUNT;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
 
