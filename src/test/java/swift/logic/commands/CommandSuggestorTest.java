c821f3a1dee42f7e3bc6d4301b940488d15e4d93
Santosh3007 <muthukrishnansantosh@gmail.com>
2022-10-21 15:23:25 +0800

    test: add tests for CommandSuggestor

--- src/test/java/swift/logic/commands/CommandSuggestorTest.java
+++ src/test/java/swift/logic/commands/CommandSuggestorTest.java
@@ -18,7 +18,7 @@ public class CommandSuggestorTest {
     }
 
     @Test
-    public void autocomplete_validCommand_success() {
+    public void suggestCommand_validCommand_success() {
         String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " "
                                         + PREFIX_CONTACT + "<contact_index>" + " "
                                         + PREFIX_NAME + "<name>";
@@ -27,41 +27,62 @@ public class CommandSuggestorTest {
             System.out.println(commandSuggestor.suggestCommand("add_t"));
             assertEquals(expectedSuggestion, commandSuggestor.suggestCommand("add_t"));
         } catch (CommandException e) {
-            throw new AssertionError("Autocomplete should not fail.", e);
+            throw new AssertionError("Command Suggestion should not fail.", e);
         }
     }
 
     @Test
-    public void autocomplete_invalidCommand_failure() {
+    public void suggestCommand_invalidCommand_failure() {
         assertThrows(CommandException.class, () -> commandSuggestor.suggestCommand("asdfdf"));
     }
 
     @Test
-    public void autocomplete_validCommandWithPrefix_success() {
+    public void suggestCommand_validCommandWithPrefix_success() {
         String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " "
                                         + PREFIX_NAME + " "
                                         + PREFIX_CONTACT + "<contact_index>";
         try {
             assertEquals(expectedSuggestion, commandSuggestor.suggestCommand("add_task n/ "));
         } catch (CommandException e) {
-            throw new AssertionError("Autocomplete should not fail.", e);
+            throw new AssertionError("Command Suggestion should not fail.", e);
         }
     }
 
     @Test
-    public void autocomplete_validCommandWithPrefixAndValue_success() {
+    public void suggestCommand_validCommandWithPrefixAndValue_success() {
         String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " "
                                         + PREFIX_NAME + "abc "
                                         + PREFIX_CONTACT + "<contact_index>";
         try {
             assertEquals(expectedSuggestion, commandSuggestor.suggestCommand("add_task n/abc "));
         } catch (CommandException e) {
+            throw new AssertionError("Command Suggestion should not fail.", e);
+        }
+    }
+
+    @Test
+    public void suggestCommand_validCommandWithInvalidPrefix_failure() {
+        assertThrows(CommandException.class, () -> commandSuggestor.suggestCommand("add_task a"));
+    }
+
+    @Test
+    public void autocomplete_validCommandWithNoPrefix_success() {
+        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " ";
+        try {
+            assertEquals(expectedSuggestion, commandSuggestor.autocompleteCommand("add_t", commandSuggestor.suggestCommand("add_t")));
+        } catch (CommandException e) {
             throw new AssertionError("Autocomplete should not fail.", e);
         }
     }
 
     @Test
-    public void autocomplete_validCommandWithInvalidPrefix_failure() {
-        assertThrows(CommandException.class, () -> commandSuggestor.suggestCommand("add_task a"));
+    public void autocomplete_validCommandWithPrefix_success() {
+        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " " + PREFIX_CONTACT;
+        try {
+            assertEquals(expectedSuggestion, commandSuggestor.autocompleteCommand("add_task ",
+                    commandSuggestor.suggestCommand("add_task ")));
+        } catch (CommandException e) {
+            throw new AssertionError("Autocomplete should not fail.", e);
+        }
     }
 }
