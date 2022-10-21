aac93255c0663df2d908e4d81e34d9a16e0ce05c
ShummyOwnzYou <jiehui285@gmail.com>
2022-10-21 19:30:18 +0800

    Add private string constants to avoid using string literals

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -16,6 +16,11 @@ import seedu.address.model.applicant.Applicant;
 
 public class SortCommandParser implements Parser<SortCommand> {
 
+    private static final String NAME = "name";
+    private static final String SCHOLARSHIP = "scholarship";
+    private static final String STATUS = "status";
+    private static final String REVERSE_FLAG = "-r";
+
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
      * and returns a SortCommand object for execution.
@@ -27,11 +32,11 @@ public class SortCommandParser implements Parser<SortCommand> {
         String[] keywords = trimmedArgs.split("\\s+");
         List<String> keywordsList = Arrays.asList(keywords);
 
-        if (keywordsList.contains("name")) {
+        if (keywordsList.contains(NAME)) {
             return new SortCommand(checkInputSizeAndReverseFlag(Applicant.sortByName(), keywordsList));
-        } else if (keywordsList.contains("scholarship")) {
+        } else if (keywordsList.contains(SCHOLARSHIP)) {
             return new SortCommand(checkInputSizeAndReverseFlag(Applicant.sortByScholarship(), keywordsList));
-        } else if (keywordsList.contains("status")) {
+        } else if (keywordsList.contains(STATUS)) {
             return new SortCommand(checkInputSizeAndReverseFlag(Applicant.sortByStatus(), keywordsList));
         } else {
             throw new ParseException(
@@ -41,7 +46,7 @@ public class SortCommandParser implements Parser<SortCommand> {
 
     private static Comparator<Applicant> checkInputSizeAndReverseFlag(
             Comparator<Applicant> comparator, List<String> list) throws ParseException {
-        if (list.size() == 2 && list.contains("-r")) {
+        if (list.size() == 2 && list.contains(REVERSE_FLAG)) {
             return comparator.reversed();
         } else if (list.size() == 1) {
             return comparator;
