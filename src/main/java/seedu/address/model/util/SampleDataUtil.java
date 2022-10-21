a3c6ca7ee2f7480cda835809106e7d7966a98350
gnahzeus <zhangyue1689@gmail.com>
2022-10-21 22:08:02 +0800

    Update model for compulsory nok phone in add command

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -19,22 +19,22 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"),
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Phone("89705432"),
                     new Email("alexyeoh@example.com"), new Address("Blk 30 Geylang Street 29, #06-40"),
                     getTagSet("python")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Phone("93706732"),
                     new Email("berniceyu@example.com"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                     getTagSet("Java", "python")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Phone("66554163"),
                     new Email("charlotte@example.com"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                     getTagSet("Java")),
-            new Person(new Name("David Li"), new Phone("91031282"),
+            new Person(new Name("David Li"), new Phone("91031282"), new Phone("91726932"),
                     new Email("lidavid@example.com"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                     getTagSet("python")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Phone("84311256"),
                     new Email("irfan@example.com"), new Address("Blk 47 Tampines Street 20, #17-35"),
                     getTagSet("javascript")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Phone("99703332"),
                     new Email("royb@example.com"), new Address("Blk 45 Aljunied Street 85, #11-31"),
                     getTagSet("c"))
         };
