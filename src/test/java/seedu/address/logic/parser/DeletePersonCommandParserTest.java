4b4d86e0728282bb92ca12c4ed7fa83900cbf849
ekweirui <ekweirui@gmail.com>
2022-10-21 13:45:23 +0800

    Refactor DeleteCommand to DeletePersonCommand

--- src/test/java/seedu/address/logic/parser/DeletePersonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeletePersonCommandParserTest.java
@@ -18,7 +18,7 @@ import seedu.address.logic.commands.DeleteCommand;
  */
 public class DeleteCommandParserTest {
 
-    private DeleteCommandParser parser = new DeleteCommandParser();
+    private DeletePersonCommandParser parser = new DeletePersonCommandParser();
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
