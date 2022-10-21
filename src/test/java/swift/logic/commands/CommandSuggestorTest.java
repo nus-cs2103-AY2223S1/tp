8ea735ed2f99497b37ffd8450357196eb20fc16a
Santosh3007 <muthukrishnansantosh@gmail.com>
2022-10-21 14:12:16 +0800

    style: fix checkstyle errors for tests

--- src/test/java/swift/logic/commands/CommandSuggestorTest.java
+++ src/test/java/swift/logic/commands/CommandSuggestorTest.java
@@ -1,19 +1,22 @@
 package swift.logic.commands;
 
-import org.junit.jupiter.api.Test;
-import swift.logic.commands.exceptions.CommandException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static swift.logic.parser.CliSyntax.PREFIX_CONTACT;
 import static swift.logic.parser.CliSyntax.PREFIX_NAME;
 
+import org.junit.jupiter.api.Test;
+
+import swift.logic.commands.exceptions.CommandException;
+
 public class CommandSuggestorTest {
     private final CommandSuggestor commandSuggestor = new CommandSuggestor();
-    
+
     @Test
     public void autocomplete_validCommand_success() {
-        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " " + PREFIX_CONTACT + "<contact_index>" + " " + PREFIX_NAME + "<name>";
+        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " "
+                                        + PREFIX_CONTACT + "<contact_index>" + " "
+                                        + PREFIX_NAME + "<name>";
         try {
             System.out.println(expectedSuggestion);
             System.out.println(commandSuggestor.suggestCommand("add_t"));
@@ -30,7 +33,9 @@ public class CommandSuggestorTest {
 
     @Test
     public void autocomplete_validCommandWithPrefix_success() {
-        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " " + PREFIX_NAME + " " + PREFIX_CONTACT               + "<contact_index>";
+        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " "
+                                        + PREFIX_NAME + " "
+                                        + PREFIX_CONTACT + "<contact_index>";
         try {
             assertEquals(expectedSuggestion, commandSuggestor.suggestCommand("add_task n/ "));
         } catch (CommandException e) {
@@ -40,7 +45,9 @@ public class CommandSuggestorTest {
 
     @Test
     public void autocomplete_validCommandWithPrefixAndValue_success() {
-        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " " + PREFIX_NAME + "abc " + PREFIX_CONTACT + "<contact_index>";
+        String expectedSuggestion = AddTaskCommand.COMMAND_WORD + " "
+                                        + PREFIX_NAME + "abc "
+                                        + PREFIX_CONTACT + "<contact_index>";
         try {
             assertEquals(expectedSuggestion, commandSuggestor.suggestCommand("add_task n/abc "));
         } catch (CommandException e) {
@@ -52,5 +59,4 @@ public class CommandSuggestorTest {
     public void autocomplete_validCommandWithInvalidPrefix_failure() {
         assertThrows(CommandException.class, () -> commandSuggestor.suggestCommand("add_task a"));
     }
-    
 }
