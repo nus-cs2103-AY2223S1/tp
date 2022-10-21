19bb85640e30c689e0caff251b639fd531ae82e9
waynezsy <waynezsy0317@gmail.com>
2022-10-22 05:15:37 +0800

    Enhance Money with service charge, GST calculation

--- src/main/java/paymelah/model/debt/Money.java
+++ src/main/java/paymelah/model/debt/Money.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static paymelah.commons.util.AppUtil.checkArgument;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Represents a Debt's money amount in the address book.
@@ -12,10 +13,13 @@ import java.math.BigDecimal;
 public class Money implements Comparable<Money> {
     public static final String MESSAGE_CONSTRAINTS =
             "Money amounts should use numbers to represent the amount in dollars,"
-                    + " separating dollar and cent values with a .";
-
-    public static final String VALIDATION_REGEX = "\\$?\\d+(\\.\\d{1,2})?";
+                    + " separating dollar and cent values with a '.'"
+                    + "\nEnd with + to add GST to the amount, or ++ to add both GST and service charge.";
+    public static final String VALIDATION_REGEX = "\\$?\\d+(\\.\\d{1,2})?\\s*(\\+{1,2})?";
     public static final int CENTS_PRECISION = 2;
+    public static final BigDecimal GST_MULTIPLER = new BigDecimal("1.07");
+    public static final BigDecimal SERVICE_CHARGE_MULTIPLIER = new BigDecimal("1.10");
+
     private final BigDecimal value;
 
     /**
@@ -29,7 +33,24 @@ public class Money implements Comparable<Money> {
         if (money.startsWith("$")) {
             money = money.substring(1);
         }
-        value = new BigDecimal(money).setScale(CENTS_PRECISION);
+
+        int plusCount = 0;
+        while (money.endsWith("+")) {
+            plusCount += 1;
+            money = money.substring(0, money.length() - 1).trim();
+        }
+
+        BigDecimal value = new BigDecimal(money).setScale(CENTS_PRECISION);
+        if (plusCount == 1) {
+            value = value.multiply(GST_MULTIPLER).setScale(CENTS_PRECISION, RoundingMode.CEILING);
+        } else if (plusCount == 2) {
+            value = value.multiply(SERVICE_CHARGE_MULTIPLIER).multiply(GST_MULTIPLER)
+                    .setScale(CENTS_PRECISION, RoundingMode.CEILING);
+        } else if (plusCount != 0) {
+            assert false : ">2 '+' not detected as invalid Money input.";
+        }
+
+        this.value = value;
     }
 
     /**
