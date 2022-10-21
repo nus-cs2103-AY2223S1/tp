46c7b12f6e6464ea5bfc92ddc52f92df617087ae
JovonLim <jovonlim@gmail.com>
2022-10-21 23:13:43 +0800

    Fix sampleData for tags

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -22,22 +22,22 @@ public class SampleDataUtil {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), new ArrayList<>(),
-                getTagSet("friends")),
+                getTagSet("Ear")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new ArrayList<>(),
-                getTagSet("colleagues", "friends")),
+                getTagSet("Nose", "Throat")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new ArrayList<>(),
-                getTagSet("neighbours")),
+                getTagSet("Ear")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new ArrayList<>(),
-                getTagSet("family")),
+                getTagSet("Throat")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), new ArrayList<>(),
-                getTagSet("classmates")),
+                getTagSet("Ear")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), new ArrayList<>(),
-                getTagSet("colleagues"))
+                getTagSet("Nose"))
         };
     }
 
