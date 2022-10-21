a3e9ce12f9628e6b0941b4a476a2db2137bde1c6
bensohh <lok200910@gmail.com>
2022-10-21 21:49:08 +0800

    Abstract error messages for DeleteCommandParser

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_BOTH_EMAIL_AND_PHONE;
+import static seedu.address.commons.core.Messages.MESSAGE_EMPTY_EMAIL_AND_PHONE;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_NO_UNIQUE_PREFIX_IDENTIFIER;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -38,42 +41,54 @@ public class DeleteCommandParserTest {
     @Test
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_EMPTY_EMAIL_AND_PHONE, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_emptyPhoneArg_throwsParseException() {
         assertParseFailure(parser, " p/",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+                String.format(Phone.MESSAGE_CONSTRAINTS, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_emptyEmailArg_throwsParseException() {
         assertParseFailure(parser, " e/",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+                String.format(Email.MESSAGE_CONSTRAINTS, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_invalidArg_throwsParseException() {
         assertParseFailure(parser, "testing123",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_EMPTY_EMAIL_AND_PHONE, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_bothArgs_throwsParseException() {
         assertParseFailure(parser, " p/12345678 e/test@test.com",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_BOTH_EMAIL_AND_PHONE, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_multiplePhoneArgs_throwsParseException() {
         assertParseFailure(parser, " p/12345678 p/87654321",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_NO_UNIQUE_PREFIX_IDENTIFIER, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_multipleEmailArgs_throwsParseException() {
         assertParseFailure(parser, " e/test@test.com e/test123@test.com",
+                String.format(MESSAGE_NO_UNIQUE_PREFIX_IDENTIFIER, DeleteCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_preambleAndPhone_throwsParseException() {
+        assertParseFailure(parser, " test p/12345678",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_preambleAndEmail_throwsParseException() {
+        assertParseFailure(parser, " test e/test@test.com",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
