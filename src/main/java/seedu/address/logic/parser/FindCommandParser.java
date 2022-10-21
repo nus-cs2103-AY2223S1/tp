57b5016b60121ff712c2c40f0c05607f211453d7
joosxi <64764352+joosxi@users.noreply.github.com>
2022-10-21 14:27:05 +0800

    Fix checkstyles for find command classes

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -5,26 +5,17 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENTCLASS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.FindNameCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.FindCommandPredicate;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.StudentClass;
-import seedu.address.model.person.subject.Subject;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindNameCommand> {
-
-    private static final Name DEFAULT_NAME = new Name("AMY");
-    private static final StudentClass DEFAULT_CLASS = new StudentClass("1A");
-    private static final Subject DEFAULT_SUBJECT = new Subject("MATH");
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
