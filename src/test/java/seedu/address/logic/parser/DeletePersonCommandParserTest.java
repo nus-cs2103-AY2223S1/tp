a15a4093fce7430e8cab94b3d57276d7a8b8fcdb
Qiaoran-M <e0492494@u.nus.edu>
2022-10-21 14:44:33 +0800

    Rename delete person command parser test

--- src/test/java/seedu/address/logic/parser/DeletePersonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeletePersonCommandParserTest.java
@@ -16,7 +16,7 @@ import seedu.address.logic.commands.DeletePersonCommand;
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class DeleteCommandParserTest {
+public class DeletePersonCommandParserTest {
 
     private DeletePersonCommandParser parser = new DeletePersonCommandParser();
 
