09b9d39b63cf461276c8cdd5b2fd90b11d0e95fa
Tex Tang <tex_tang@outlook.com>
2022-10-21 22:18:17 +0800

    Test: Fix test

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,13 +1,18 @@
 package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SLACK;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIMEZONE;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.contact.ContactType;
 
 
 /**
@@ -32,6 +37,23 @@ public class PersonUtil {
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
+
+        if (person.getContacts().containsKey(ContactType.TELEGRAM)) {
+            sb.append(PREFIX_TELEGRAM + person.getContacts().get(ContactType.TELEGRAM).getValue() + " ");
+        }
+
+        if (person.getContacts().containsKey(ContactType.SLACK)) {
+            sb.append(PREFIX_SLACK + person.getContacts().get(ContactType.SLACK).getValue() + " ");
+        }
+
+        if (person.getContacts().containsKey(ContactType.PHONE)) {
+            sb.append(PREFIX_PHONE + person.getContacts().get(ContactType.PHONE).getValue() + " ");
+        }
+
+        if (person.getContacts().containsKey(ContactType.EMAIL)) {
+            sb.append(PREFIX_EMAIL + person.getContacts().get(ContactType.EMAIL).getValue() + " ");
+        }
+
         person.getRole().ifPresent(r -> sb.append(PREFIX_ROLE + r.role + " "));
         person.getTimezone().ifPresent(t -> sb.append(PREFIX_TIMEZONE + t.timezone + " "));
         return sb.toString();
