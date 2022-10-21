4e09ce56f8567a0e7e1b64d19ddb752b24e86bf9
Aloynz <aloysiusneozeen@gmail.com>
2022-10-21 23:35:35 +0800

    Fix numbering in GroupCard

--- src/main/java/seedu/address/ui/GroupListPanel.java
+++ src/main/java/seedu/address/ui/GroupListPanel.java
@@ -39,7 +39,7 @@ public class GroupListPanel extends UiPart<Region> {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new GroupCard(group, 1).getRoot());
+                setGraphic(new GroupCard(group, getIndex() + 1).getRoot());
             }
         }
     }
