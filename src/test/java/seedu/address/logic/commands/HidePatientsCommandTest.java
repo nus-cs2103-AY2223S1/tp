365ffcaaf02fb98a3c23099ab94927c0fb87fb9a
guanzhou03 <guanzhou03@gmail.com>
2022-10-21 22:17:33 +0800

    Update test

--- src/test/java/seedu/address/logic/commands/HidePatientsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HidePatientsCommandTest.java
@@ -17,7 +17,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.TagContainsKeywordsPredicate;
 import seedu.address.model.person.predicates.AppointmentOfFilteredPersonsPredicate;
 import seedu.address.model.person.predicates.NameContainsKeywordsPredicate;
 
