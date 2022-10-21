db899e44299ca0bb72b9414b6127b8e770e8fe61
Elgin Lee Yiheng <88147891+ElginL@users.noreply.github.com>
2022-10-21 10:58:27 +0800

    Update FilterTransCommand.java

--- src/main/java/seedu/address/logic/commands/FilterTransCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterTransCommand.java
@@ -28,7 +28,7 @@ public class FilterTransCommand extends Command {
      * Executes the command and returns the result message.
      *
      * @param model {@code Model} which the command should operate on.
-     * @return feedback message of the operation result for display
+     * @return feedback message of the operation result for display.
      * @throws CommandException If an error occurs during command execution.
      */
     @Override
