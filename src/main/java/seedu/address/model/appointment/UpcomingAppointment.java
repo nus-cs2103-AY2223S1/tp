3ac411e37a589eb8a93beb75da2fda9e9933caa8
PeiYee88 <e0550356@u.nus.edu>
2022-10-22 04:25:29 +0800

    Update UpcomingAppointment.java

--- src/main/java/seedu/address/model/appointment/UpcomingAppointment.java
+++ src/main/java/seedu/address/model/appointment/UpcomingAppointment.java
@@ -2,6 +2,7 @@ package seedu.address.model.appointment;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Represents an upcoming appointment for a patient.
