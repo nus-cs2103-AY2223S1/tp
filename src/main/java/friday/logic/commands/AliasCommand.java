7624ea4b4bb5ed54c948a1da91c3475acc3a229c
darrtzj <darrenteozj@u.nus.edu>
2022-10-23 01:04:22 +0800

    Add feature to add aliases

--- src/main/java/friday/logic/commands/AliasCommand.java
+++ src/main/java/friday/logic/commands/AliasCommand.java
@@ -6,6 +6,8 @@ import static java.util.Objects.requireNonNull;
 
 import friday.logic.commands.exceptions.CommandException;
 import friday.model.Model;
+import friday.model.alias.Alias;
+import friday.model.alias.ReservedKeyword;
 
 /**
  * Adds an alias for commands to FRIDAY.
@@ -24,26 +26,39 @@ public class AliasCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "New alias added: %1$s";
     public static final String MESSAGE_DUPLICATE_ALIAS = "This alias already exists in FRIDAY";
+    public static final String MESSAGE_INVALID_ALIAS = "Alias cannot be a reserved keyword";
+    public static final String MESSAGE_INVALID_KEYWORD = "Keyword given is not a reserved keyword";
 
-    private final String toAdd;
+    private final Alias toAdd;
+    private final ReservedKeyword keyword;
 
     /**
      * Creates an AliasCommand to add the specified {@code alias}
      */
-    public AliasCommand(String alias) {
+    public AliasCommand(Alias alias, ReservedKeyword reservedKeyword) {
         requireNonNull(alias);
+        requireNonNull(reservedKeyword);
         toAdd = alias;
+        keyword = reservedKeyword;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
+        if (!ReservedKeyword.isValidReservedKeyword(keyword)) {
+            throw new CommandException(MESSAGE_INVALID_KEYWORD);
+        }
+
+        if (!Alias.isValidAlias(toAdd)) {
+            throw new CommandException(MESSAGE_INVALID_ALIAS);
+        }
+
         if (model.hasAlias(toAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_ALIAS);
         }
 
-        model.addAlias(toAdd);
+        model.addAlias(toAdd, keyword);
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
--- src/main/java/friday/logic/parser/AliasCommandParser.java
+++ src/main/java/friday/logic/parser/AliasCommandParser.java
@@ -1,19 +1,14 @@
 package friday.logic.parser;
 
 import static friday.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static friday.logic.parser.CliSyntax.*;
+import static friday.logic.parser.CliSyntax.PREFIX_ALIAS;
+import static friday.logic.parser.CliSyntax.PREFIX_RESERVED_KEYWORD;
 import static java.util.Objects.requireNonNull;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
-
-import friday.commons.core.index.Index;
 import friday.logic.commands.AliasCommand;
-import friday.logic.commands.EditCommand;
 import friday.logic.parser.exceptions.ParseException;
-import friday.model.tag.Tag;
+import friday.model.alias.Alias;
+import friday.model.alias.ReservedKeyword;
 
 /**
  * Parses input arguments and creates a new AliasCommand object
@@ -30,51 +25,21 @@ public class AliasCommandParser implements Parser<AliasCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_ALIAS, PREFIX_RESERVED_KEYWORD);
 
-        String alias;
-        String reservedKeyword;
+        Alias alias;
+        ReservedKeyword reservedKeyword;
 
         if (!argMultimap.getValue(PREFIX_ALIAS).isPresent()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AliasCommand.MESSAGE_USAGE);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AliasCommand.MESSAGE_USAGE));
         }
 
         if (!argMultimap.getValue(PREFIX_RESERVED_KEYWORD).isPresent()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AliasCommand.MESSAGE_USAGE);
-        }
-        if (argMultimap.getValue(PREFIX_TELEGRAMHANDLE).isPresent()) {
-            editStudentDescriptor.setTelegramHandle(ParserUtil.parseTelegramHandle(
-                    argMultimap.getValue(PREFIX_TELEGRAMHANDLE).get()));
-        }
-        if (argMultimap.getValue(PREFIX_CONSULTATION).isPresent()) {
-            editStudentDescriptor.setConsultation(ParserUtil.parseConsultation(
-                    argMultimap.getValue(PREFIX_CONSULTATION).get()));
-        }
-        if (argMultimap.getValue(PREFIX_MASTERYCHECK).isPresent()) {
-            editStudentDescriptor.setMasteryCheck(ParserUtil.parseMasteryCheck(
-                    argMultimap.getValue(PREFIX_MASTERYCHECK).get()));
-        }
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editStudentDescriptor::setTags);
-
-        if (!editStudentDescriptor.isAnyFieldEdited()) {
-            throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AliasCommand.MESSAGE_USAGE));
         }
 
-        return new EditCommand(index, editStudentDescriptor);
+        alias = ParserUtil.parseAlias(argMultimap.getValue(PREFIX_ALIAS).get());
+        reservedKeyword = ParserUtil.parseReservedKeyword(argMultimap.getValue(PREFIX_RESERVED_KEYWORD).get());
+
+        return new AliasCommand(alias, reservedKeyword);
     }
-
-    /**
-     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
-     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
-     * {@code Set<Tag>} containing zero tags.
-     */
-    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
-        assert tags != null;
-
-        if (tags.isEmpty()) {
-            return Optional.empty();
-        }
-        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
-        return Optional.of(ParserUtil.parseTags(tagSet));
-    }
-
 }
 
