84b457cbc9600c5f854a18250cea5c06feded652
elyseleong <elyse.xuan@gmail.com>
2022-10-22 14:16:55 +0800

    Fix Java CI

--- src/main/java/seedu/address/logic/parser/AddStuCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddStuCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
