35ae13038198876a02fb907adfa43d00f364c5b0
Marcus Pang <marcuspangyuyang@gmail.com>
2022-10-22 15:40:51 +0800

    Fix empty args test for add member parser

--- src/test/java/seedu/address/logic/parser/AddMemberCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddMemberCommandParserTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -27,6 +28,7 @@ public class AddMemberCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", Name.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, Name.MESSAGE_CONSTRAINTS));
     }
 }
