29e7830f716a9208cfb7d54039b8f5c9a06001f7
Jonathan Tay <jonathantaymy@gmail.com>
2022-10-22 20:49:57 +0800

    Rephrase command descriptions

--- src/main/java/modtrekt/logic/commands/tasks/ArchiveTaskCommand.java
+++ src/main/java/modtrekt/logic/commands/tasks/ArchiveTaskCommand.java
@@ -3,6 +3,7 @@ package modtrekt.logic.commands.tasks;
 import java.util.List;
 
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 
 import modtrekt.commons.core.index.Index;
 import modtrekt.logic.commands.Command;
@@ -13,8 +14,9 @@ import modtrekt.model.Model;
 import modtrekt.model.task.Task;
 
 /**
- * Archives a task in the task book belonging to a module.
+ * Archives a task in the task book.
  */
+@Parameters(commandDescription = "Archives a task in the task book.")
 public class ArchiveTaskCommand extends Command {
     public static final String COMMAND_WORD = "archive";
 
