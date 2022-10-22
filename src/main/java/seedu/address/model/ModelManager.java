5b6da0c190a86cc6bafe48fe091e45bebea1c87b
jwdavis0200 <jwdavis0200@gmail.com>
2022-10-23 04:18:23 +0800

    Update message

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -23,7 +23,7 @@ import seedu.address.model.util.NextSessionUtil;
  * Represents the in-memory model of the address book data.
  */
 public class ModelManager implements Model {
-    public static final String MESSAGE_NO_NEXT_SESSION_FOUND = "Don't worry, no next session for you this week!";
+    public static final String MESSAGE_NO_NEXT_SESSION_FOUND = "No next Session timing!";
     private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
 
     private final AddressBook addressBook;
