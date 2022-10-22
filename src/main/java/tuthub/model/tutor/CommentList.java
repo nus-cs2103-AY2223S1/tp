66117fd22850ed5daee41b4d98cbae6d3fa3674f
marcuslowhuiyu <marcuslowhuiyu@users.noreply.github.com>
2022-10-22 15:09:05 +0800

    Add JavaDocs

--- src/main/java/tuthub/model/tutor/CommentList.java
+++ src/main/java/tuthub/model/tutor/CommentList.java
@@ -9,23 +9,32 @@ import java.util.ArrayList;
 public class CommentList {
     private final ArrayList<Comment> comments = new ArrayList<>();
 
+    /**
+     * Adds a Comment to the CommentList.
+     *
+     * @param comment Comment to be added to the CommentList.
+     * @return The CommentList itself.
+     */
     public CommentList addComment(Comment comment) {
         this.comments.add(comment);
         return this;
     }
 
-    public Comment getComment(int index) {
-        return comments.get(index);
-    }
-
+    /**
+     * Deletes a Comment from the CommentList.
+     *
+     * @param index The index of the Comment in the CommentList.
+     */
     public void deleteComment(int index) {
         comments.remove(index);
     }
 
-    public void clearComments() {
-        comments.clear();
-    }
 
+    /**
+     * Converts the CommentList to a string for display in the Tutor Card.
+     *
+     * @return A string where every Comment is appended with a newline.
+     */
     public String toStringForTutorCard() {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < comments.size(); i++) {
@@ -38,6 +47,11 @@ public class CommentList {
         return result.toString();
     }
 
+    /**
+     * Converts the CommentList into a string.
+     *
+     * @return A string where every Comment is padded with their position in the CommentList.
+     */
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
