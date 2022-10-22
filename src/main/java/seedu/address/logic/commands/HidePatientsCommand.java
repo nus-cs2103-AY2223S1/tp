05a5e431245f4b4cdc3100444e5a462551caa53f
JovonLim <jovonlim@gmail.com>
2022-10-22 11:37:14 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/HidePatientsCommand.java
+++ src/main/java/seedu/address/logic/commands/HidePatientsCommand.java
@@ -9,9 +9,9 @@ import java.util.function.Predicate;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
-import seedu.address.model.person.predicates.HiddenPredicateSingleton;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.predicates.AppointmentOfFilteredPersonsPredicate;
+import seedu.address.model.person.predicates.HiddenPredicateSingleton;
 
 /**
  * Hides all persons and their appointments in address book whose name contains any of the argument keywords.
