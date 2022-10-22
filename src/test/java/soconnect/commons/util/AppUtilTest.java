5193cc997eb73a5f710eacfabd43c73c1bddf73c
florentianayuwono <e0851439@u.nus.edu>
2022-10-22 10:09:01 +0800

    Change logo name

--- src/test/java/soconnect/commons/util/AppUtilTest.java
+++ src/test/java/soconnect/commons/util/AppUtilTest.java
@@ -9,7 +9,7 @@ public class AppUtilTest {
 
     @Test
     public void getImage_exitingImage() {
-        assertNotNull(AppUtil.getImage("/images/SoConnect Logo.png"));
+        assertNotNull(AppUtil.getImage("/images/SoConnect_logo.png"));
     }
 
     @Test
