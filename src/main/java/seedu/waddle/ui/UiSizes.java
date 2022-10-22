3237639d1b0dc06aa1a7b6033619bf9a402bbabb
Clement Foo <e0726745@u.nus.edu>
2022-10-23 06:26:15 +0800

    Fix checkstyle

--- src/main/java/seedu/waddle/ui/UiSizes.java
+++ src/main/java/seedu/waddle/ui/UiSizes.java
@@ -1,5 +1,8 @@
 package seedu.waddle.ui;
 
+/**
+ * Standard sizes for UI elements.
+ */
 public class UiSizes {
     public static final double ITEM_CARD_HEIGHT = 120;
     public static final double ITEM_LIST_MIN_HEIGHT = 20;
