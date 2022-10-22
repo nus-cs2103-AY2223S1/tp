14a30555ab2ae21780dd8264e2a8a30b6a656b77
aarontangr <aarontangr@gmail.com>
2022-10-22 18:58:39 +0800

    Remove unused imports

--- src/test/java/seedu/phu/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/phu/logic/commands/CommandResultTest.java
@@ -3,7 +3,6 @@ package seedu.phu.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
