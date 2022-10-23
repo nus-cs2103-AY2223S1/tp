4fae970e585e45491b92dbe90ec7a2b80191446f
SpecOps2016 <76586680+SpecOps2016@users.noreply.github.com>
2022-10-23 23:34:18 +0800

    Update RedoCommand.java

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -13,7 +13,7 @@ public class RedoCommand extends Command {
     public static final CommandWord COMMAND_WORD =
             new CommandWord("redo");
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Redoes the last command\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD+ ": Redoes the last command\n"
             + "Example: " + COMMAND_WORD + " ";
 
     public static final String MESSAGE_SUCCESS = "Redo successful";
