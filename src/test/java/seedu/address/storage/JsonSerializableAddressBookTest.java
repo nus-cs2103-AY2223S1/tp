0398b8ccc3f1cd222820d82e01744a65529d73d8
zicot <zicotjia@gmail.com>
2022-10-23 15:06:50 +0800

    Create team builder and typicalTeams for testing purposes and modified getTypicalAddressBook

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.AddressBook;
+import seedu.address.testutil.TypicalPersons;
 import seedu.address.testutil.TypicalTeams;
 
 public class JsonSerializableAddressBookTest {
@@ -26,7 +27,7 @@ public class JsonSerializableAddressBookTest {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
                 JsonSerializableAddressBook.class).get();
         AddressBook addressBookFromFile = dataFromFile.toModelType();
-        AddressBook typicalPersonsAddressBook = TypicalTeams.getTypicalAddressBookWithTeams();
+        AddressBook typicalPersonsAddressBook = TypicalPersons.getTypicalAddressBookWithPersons();
         assertEquals(addressBookFromFile, typicalPersonsAddressBook);
     }
 
