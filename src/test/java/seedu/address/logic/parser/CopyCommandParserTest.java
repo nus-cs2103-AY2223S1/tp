8bf771bb9c05ab5a240c8e0b7748da8d892e6ade
jovitaanderson <jovita@live.com.sg>
2022-10-24 01:16:39 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/CopyCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CopyCommandParserTest.java
@@ -2,8 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+//import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
