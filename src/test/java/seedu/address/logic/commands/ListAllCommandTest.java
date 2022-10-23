9c58553e35e32c50bdc53cd2637a4b971d7f7703
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-10-23 11:18:37 +0800

    Add unit tests for other related components

---
+++ src/test/java/seedu/address/logic/commands/ListAllCommandTest.java
@@ -0,0 +1,40 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalSupplyItems.getTypicalInventory;
+import static seedu.address.testutil.TypicalTasks.getTypicalTaskList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class ListAllCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs(),
+                getTypicalTaskList(), getTypicalInventory());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs(),
+                getTypicalTaskList(), getTypicalInventory());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListAllCommand(), model, ListAllCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        // Simulate a situation where not all details are shown in the 3 lists.
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListAllCommand(), model, ListAllCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
