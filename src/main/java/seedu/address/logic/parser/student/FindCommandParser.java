30a11fdc5378a896960bb5fe9d90af49be29d44b
heyzec <61238538+heyzec@users.noreply.github.com>
2022-10-23 13:35:39 +0800

    Enable search by name, id, module, tutorial

--- src/main/java/seedu/address/logic/parser/student/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/student/FindCommandParser.java
@@ -9,6 +9,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.JsonUtil;
@@ -17,9 +18,13 @@ import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.student.Student;
+import seedu.address.model.student.predicates.ModuleContainsKeywordPredicate;
 import seedu.address.model.student.predicates.NameContainsKeywordsPredicate;
+import seedu.address.model.student.predicates.StudentIdContainsKeywordPredicate;
+import seedu.address.model.student.predicates.TutorialContainsKeywordPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -38,13 +43,26 @@ public class FindCommandParser implements Parser<FindCommand> {
         ParserUtil.assertAnyPrefixesPresent(argMultimap,
                 PREFIX_NAME, PREFIX_ID, PREFIX_MODULE, PREFIX_TUTORIAL);
 
-        String trimmedName = argMultimap.getValue(PREFIX_NAME).get().trim();
-        String[] nameKeywords = trimmedName.split("\\s+");
-
         List<Predicate<Student>> predicates = new ArrayList<>();
-        predicates.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
-        predicates.add(new NameContainsKeywordsPredicate(List.of(new String[]{"test"})));
 
+        Prefix[] prefixes = {PREFIX_NAME, PREFIX_ID, PREFIX_MODULE, PREFIX_TUTORIAL};
+        for (Prefix prefix : prefixes) {
+            Optional<String> str = argMultimap.getValue(prefix);
+            if (str.isEmpty()) {
+                continue;
+            }
+            String trimmedKeywords = str.get().trim();
+            if (prefix.equals(PREFIX_NAME)) {
+                String[] nameKeywords = trimmedKeywords.split("\\s+");
+                predicates.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+            } else if (prefix.equals(PREFIX_ID)) {
+                predicates.add(new StudentIdContainsKeywordPredicate(trimmedKeywords));
+            } else if (prefix.equals(PREFIX_MODULE)) {
+                predicates.add(new ModuleContainsKeywordPredicate(trimmedKeywords));
+            } else if (prefix.equals(PREFIX_TUTORIAL)) {
+                predicates.add(new TutorialContainsKeywordPredicate(trimmedKeywords));
+            }
+        }
 
         Predicate<Student> chainedPredicates = student -> {
             for (Predicate<Student> predicate : predicates) {
