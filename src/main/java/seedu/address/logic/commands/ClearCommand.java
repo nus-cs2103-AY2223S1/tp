702376be3c7ea52f5258408e42418b8377b87b0b
zzthian <zzthian@hotmail.com>
2022-10-23 23:20:17 +0800

    Testing if certain function is causing failure

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -18,7 +18,7 @@ public class ClearCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
-        model.clearStudentPictures();
+        //model.clearStudentPictures();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
