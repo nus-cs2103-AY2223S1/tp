74a7ee353787286f1b5c5e26e704803270beb2a5
zylee348 <e0725436@u.nus.edu>
2022-10-23 23:23:07 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/ui/AssignmentCard.java
+++ src/main/java/seedu/address/ui/AssignmentCard.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.assignment.Assignment;
 import seedu.address.model.person.Person;
 
 /**
