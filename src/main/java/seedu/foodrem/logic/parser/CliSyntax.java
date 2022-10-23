2ce9d53ca8285af188738f7a0529c24f134505bf
Tan Yi Xian <>
2022-10-23 12:00:29 +0800

    Remove id prefix

--- src/main/java/seedu/foodrem/logic/parser/CliSyntax.java
+++ src/main/java/seedu/foodrem/logic/parser/CliSyntax.java
@@ -13,5 +13,4 @@ public class CliSyntax {
     public static final Prefix PREFIX_ITEM_EXPIRY_DATE = new Prefix("exp/");
     public static final Prefix PREFIX_ITEM_PRICE = new Prefix("p/");
     public static final Prefix PREFIX_ITEM_REMARKS = new Prefix("r/");
-    public static final Prefix PREFIX_ID = new Prefix("id/");
 }
