58143a8db6f259c413e27608b81ab4d2af6bfddf
yeoyujie <yeo.yujie@gmail.com>
2022-10-24 04:12:19 +0800

    Merge priority into reminder test cases

--- src/test/java/seedu/address/testutil/TypicalReminders.java
+++ src/test/java/seedu/address/testutil/TypicalReminders.java
@@ -6,8 +6,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_DESCRIPTION_REM
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DESCRIPTION_REMINDER2;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_REMINDER1;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_REMINDER2;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_PRIORITY_REMINDER1;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_PRIORITY_REMINDER2;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PRIORITY_REMINDER1;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PRIORITY_REMINDER2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,27 +22,29 @@ import seedu.address.model.reminder.Reminder;
 public class TypicalReminders {
 
     public static final Reminder REMINDER1 = new ReminderBuilder().withName("Mark question 4 of test 1")
-            .withDeadline("14:00").withDescription("200 more to go").build();
+            .withDeadline("14:00").withPriority("MEDIUM").withDescription("200 more to go").build();
     public static final Reminder REMINDER2 = new ReminderBuilder().withName("Set paper for test 2")
-            .withDeadline("15:00").withDescription("3 more questions to set").build();
+            .withDeadline("15:00").withPriority("HIGH").withDescription("3 more questions to set").build();
     public static final Reminder REMINDER3 = new ReminderBuilder().withName("Zoom makeup tut")
-            .withDeadline("16:00").withDescription("Tutorial 4 makeup").build();
+            .withDeadline("16:00").withPriority("HIGH").withDescription("Tutorial 4 makeup").build();
     public static final Reminder REMINDER4 = new ReminderBuilder().withName("Upload HW1")
-            .withDeadline("17:00").withDescription("HW1 of CS2030").build();
+            .withDeadline("17:00").withPriority("LOW").withDescription("HW1 of CS2030").build();
     public static final Reminder REMINDER5 = new ReminderBuilder().withName("Upload zoom recording")
-            .withDeadline("18:00").withDescription("For lecture 4").build();
+            .withDeadline("18:00").withPriority("LOW").withDescription("For lecture 4").build();
 
     // Manually added
     public static final Reminder MEETING_9AM = new ReminderBuilder().withName("Meeting at 9am").withDeadline("08:45")
-            .withDescription("with Prof Anne").build();
+            .withPriority("MEDIUM").withDescription("with Prof Anne").build();
     public static final Reminder MEETING_10AM = new ReminderBuilder().withName("Meeting at 10am").withDeadline("09:45")
-            .withDescription("with Dean").build();
+            .withPriority("HIGH").withDescription("with Dean").build();
 
     // Manually added - Tutorial's details found in {@code CommandTestUtil}
     public static final Reminder SET_HW1 = new ReminderBuilder().withName(VALID_NAME_REMINDER1)
-            .withDeadline(VALID_DEADLINE_REMINDER1).withDescription(VALID_DESCRIPTION_REMINDER1).build();
+            .withDeadline(VALID_DEADLINE_REMINDER1).withPriority(VALID_PRIORITY_REMINDER1)
+            .withDescription(VALID_DESCRIPTION_REMINDER1).build();
     public static final Reminder MARK_FINALS = new ReminderBuilder().withName(VALID_NAME_REMINDER2)
-            .withDeadline(VALID_DEADLINE_REMINDER2).withDescription(VALID_DESCRIPTION_REMINDER2).build();
+            .withDeadline(VALID_DEADLINE_REMINDER2).withPriority(VALID_PRIORITY_REMINDER2)
+            .withDescription(VALID_DESCRIPTION_REMINDER2).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
