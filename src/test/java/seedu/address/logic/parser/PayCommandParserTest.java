b2667f536ebc6adb48b632a8da04ece56bd812fb
Caden <63772723+cadencjk@users.noreply.github.com>
2022-10-23 16:34:41 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/parser/PayCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/PayCommandParserTest.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.PayCommand;
 import seedu.address.model.person.Money;
 
--- src/test/java/seedu/address/model/person/MoneyTest.java
+++ src/test/java/seedu/address/model/person/MoneyTest.java
@@ -1,9 +1,12 @@
 package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 
 
