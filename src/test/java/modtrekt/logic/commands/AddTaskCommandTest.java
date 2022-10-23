5a0fdd738f74218115ae7991eddd1d4752c44ff5
David <davids89034@gmail.com>
2022-10-23 19:10:10 +0800

    Add tasks for AddTaskCommand & parsing

---
+++ src/test/java/modtrekt/logic/commands/AddTaskCommandTest.java
@@ -0,0 +1,294 @@
+package modtrekt.logic.commands;
+
+import static modtrekt.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import modtrekt.commons.core.GuiSettings;
+import modtrekt.logic.commands.exceptions.CommandException;
+import modtrekt.model.Model;
+import modtrekt.model.ReadOnlyModuleList;
+import modtrekt.model.ReadOnlyTaskBook;
+import modtrekt.model.ReadOnlyUserPrefs;
+import modtrekt.model.module.ModCode;
+import modtrekt.model.module.Module;
+import modtrekt.model.task.Task;
+import modtrekt.testutil.AddTaskCommandBuilder;
+
+public class AddTaskCommandTest {
+
+    @Test
+    public void testCommand_sameObjectReferenceEquals_returnsTrue() {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, null);
+        assertEquals(cmd, cmd);
+    }
+
+    @Test
+    public void testCommand_differentObjectValuesEquals_returnsFalse() {
+        AddTaskCommand cmd1 = AddTaskCommandBuilder.build("desc", "2022-01-01", "CS2103T");
+        AddTaskCommand cmd2 = AddTaskCommandBuilder.build("des", "2022-01-01", "CS2103T");
+        AddTaskCommand cmd3 = AddTaskCommandBuilder.build("desc", "2022-01-02", "CS2103T");
+        AddTaskCommand cmd4 = AddTaskCommandBuilder.build("desc", "2022-01-01", "CS2103");
+        assertNotEquals(cmd1, cmd2);
+        assertNotEquals(cmd1, cmd3);
+        assertNotEquals(cmd1, cmd4);
+        assertNotEquals(cmd2, cmd3);
+        assertNotEquals(cmd3, cmd4);
+    }
+
+    @Test
+    public void testCommand_sameObjectValuesEquals_returnsTrue() {
+        AddTaskCommand cmd1 = AddTaskCommandBuilder.build("desc", "2022-01-01", "CS2103T");
+        AddTaskCommand cmd2 = AddTaskCommandBuilder.build("desc", "2022-01-01", "CS2103T");
+        assertEquals(cmd1, cmd2);
+    }
+
+    @Test
+    public void testCommand_sameObjectValuesEqualsWithNulls_returnsTrue() {
+        AddTaskCommand cmd1 = AddTaskCommandBuilder.build("desc", null, "CS2103T");
+        AddTaskCommand cmd2 = AddTaskCommandBuilder.build("desc", null, "CS2103T");
+        AddTaskCommand cmd3 = AddTaskCommandBuilder.build("desc", "2022-01-01", null);
+        AddTaskCommand cmd4 = AddTaskCommandBuilder.build("desc", "2022-01-01", null);
+        assertEquals(cmd1, cmd2);
+        assertEquals(cmd3, cmd4);
+    }
+
+    @Test
+    public void testCommandNoCurrentModuleAndNoModuleCode_throws() {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, null);
+        assertThrows(CommandException.class, () -> cmd.execute(new ModelStub()));
+    }
+
+    @Test
+    public void testCommandNoCurrentModuleWithModuleCode_returnsTrue() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, "CS2102");
+        CommandResult result = cmd.execute(new ModelHasModuleWithModCode());
+        assertTrue(result.getFeedbackToUser().contains("CS2102"));
+        assertTrue(result.getFeedbackToUser().contains("desc"));
+    }
+
+    @Test
+    public void testCommandDeadlineNoCurrentModuleWithModuleCode_returnsTrue() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", "2022-04-15", "CS2102");
+        CommandResult result = cmd.execute(new ModelHasModuleWithModCode());
+        assertTrue(result.getFeedbackToUser().contains("CS2102"));
+        assertTrue(result.getFeedbackToUser().contains("desc"));
+        assertTrue(result.getFeedbackToUser().contains("due 2022-04-15"));
+    }
+
+    @Test
+    public void testCommandCurrentModuleWithoutModuleCode_returnsTrue() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, null);
+        ModelStub model = new ModelStub();
+        model.setCurrentModule(new ModCode("CS2106"));
+        CommandResult result = cmd.execute(model);
+        assertTrue(result.getFeedbackToUser().contains("CS2106"));
+        assertTrue(result.getFeedbackToUser().contains("desc"));
+    }
+
+    @Test
+    public void testCommandDeadlineCurrentModuleWithoutModuleCode_returnsTrue() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", "2022-04-15", null);
+        ModelStub model = new ModelStub();
+        model.setCurrentModule(new ModCode("CS2106"));
+        CommandResult result = cmd.execute(model);
+        assertTrue(result.getFeedbackToUser().contains("CS2106"));
+        assertTrue(result.getFeedbackToUser().contains("desc"));
+        assertTrue(result.getFeedbackToUser().contains("due 2022-04-15"));
+    }
+
+    @Test
+    public void testCommandModuleCodeInvalid_throws() {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, "CS2000");
+        assertThrows(CommandException.class, "Module code does not exist", ()
+                -> cmd.execute(new ModelStub()));
+    }
+
+    @Test
+    public void testCommandCurrentModuleWithModuleCode_returnsCodeSpecified() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, "CS2102");
+        ModelStub model = new ModelHasModuleWithModCode();
+        model.setCurrentModule(new ModCode("CS2106"));
+        CommandResult result = cmd.execute(model);
+        assertTrue(result.getFeedbackToUser().contains("CS2102"));
+    }
+
+    @Test
+    public void testCommandDeadlineCurrentModuleWithModuleCode_returnsCodeSpecified() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", "2022-04-15", "CS2102");
+        ModelStub model = new ModelHasModuleWithModCode();
+        model.setCurrentModule(new ModCode("CS2106"));
+        CommandResult result = cmd.execute(model);
+        assertTrue(result.getFeedbackToUser().contains("CS2102"));
+        assertTrue(result.getFeedbackToUser().contains("due 2022-04-15"));
+    }
+
+    /**
+     * Model stub that implements getCurrentModule() and setCurrentModule() methods for testing.
+     */
+    private class ModelStub implements Model {
+        private ModCode currentModule = null;
+
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            return null;
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            return null;
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+
+        }
+
+        @Override
+        public Path getTaskBookFilePath() {
+            return null;
+        }
+
+        @Override
+        public Path getModuleListFilePath() {
+            return null;
+        }
+
+        @Override
+        public void setTaskBookFilePath(Path taskBookFilePath) {
+
+        }
+
+        @Override
+        public void setModuleListFilePath(Path moduleListFilePath) {
+
+        }
+
+        @Override
+        public void setTaskBook(ReadOnlyTaskBook taskBook) {
+
+        }
+
+        @Override
+        public ReadOnlyTaskBook getTaskBook() {
+            return null;
+        }
+
+        @Override
+        public void setModuleList(ReadOnlyModuleList moduleList) {
+
+        }
+
+        @Override
+        public ReadOnlyModuleList getModuleList() {
+            return null;
+        }
+
+        @Override
+        public void deleteTask(Task target) {
+
+        }
+
+        @Override
+        public boolean hasModule(Module module) {
+            return false;
+        }
+
+        @Override
+        public boolean hasModuleWithModCode(ModCode code) {
+            return false;
+        }
+
+        @Override
+        public void updateModuleTask(Task t) {
+
+        }
+
+        @Override
+        public void deleteTasksOfModule(Module target) {
+
+        }
+
+        @Override
+        public void addTask(Task t) {
+
+        }
+
+        @Override
+        public void deleteModule(Module target) {
+
+        }
+
+        @Override
+        public Module parseModuleFromCode(ModCode code) {
+            return null;
+        }
+
+        @Override
+        public ModCode getCurrentModule() {
+            return currentModule;
+        }
+
+        @Override
+        public void setCurrentModule(ModCode code) {
+            currentModule = code;
+        }
+
+        @Override
+        public void setTask(Task target, Task editedTask) {
+
+        }
+
+        @Override
+        public ObservableList<Task> getFilteredTaskList() {
+            return null;
+        }
+
+        @Override
+        public void updateFilteredTaskList(Predicate<Task> predicate) {
+
+        }
+
+        @Override
+        public void addModule(Module module) {
+
+        }
+
+        @Override
+        public void setModule(Module target, Module editedModule) {
+
+        }
+
+        @Override
+        public ObservableList<Module> getFilteredModuleList() {
+            return null;
+        }
+
+        @Override
+        public void updateFilteredModuleList(Predicate<Module> predicate) {
+
+        }
+    }
+
+    /**
+     * A model stub that returns true for the hasModuleWithModCode method, to mimic specified behaviour.
+     */
+    private class ModelHasModuleWithModCode extends ModelStub {
+        @Override
+        public boolean hasModuleWithModCode(ModCode code) {
+            return true;
+        }
+    }
+}
--- src/test/java/modtrekt/logic/parser/ModtrektParserTest.java
+++ src/test/java/modtrekt/logic/parser/ModtrektParserTest.java
@@ -1,5 +1,7 @@
 package modtrekt.logic.parser;
 
+import static modtrekt.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static modtrekt.logic.commands.utils.AddCommandMessages.MESSAGE_ADD_COMMAND_PREFIXES;
 import static modtrekt.testutil.Assert.assertThrows;
 import static modtrekt.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,12 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import modtrekt.commons.core.Messages;
+import modtrekt.logic.commands.AddCommand;
 import modtrekt.logic.commands.AddTaskCommand;
 import modtrekt.logic.commands.ExitCommand;
 import modtrekt.logic.commands.HelpCommand;
 import modtrekt.logic.commands.RemoveTaskCommand;
 import modtrekt.logic.parser.exceptions.ParseException;
 import modtrekt.model.task.Task;
+import modtrekt.testutil.AddTaskCommandBuilder;
 import modtrekt.testutil.TaskBuilder;
 import modtrekt.testutil.TaskUtil;
 
@@ -57,4 +61,37 @@ public class ModtrektParserTest {
     public void parseCommand_unknownCommand_throwsParseException() {
         assertThrows(ParseException.class, () -> parser.parseCommand("unknownCommand"));
     }
+
+    @Test
+    public void parseCommand_addCommandNoFlags_throwsParseException() {
+        assertThrows(ParseException.class, String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_ADD_COMMAND_PREFIXES), ()
+                -> parser.parseCommand(AddCommand.COMMAND_WORD));
+    }
+
+    @Test
+    public void parseCommand_addCommandMFlag_throwsParseException() {
+        assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE), ()
+                -> parser.parseCommand(String.format("%s %s", AddCommand.COMMAND_WORD, CliSyntax.PREFIX_MODULE)));
+    }
+
+    @Test
+    public void parseCommand_addTaskCommandNoDeadline_throws() {
+        assertThrows(Exception.class, () -> parser.parseCommand("add -t -c CS2102"));
+        assertThrows(Exception.class, () -> parser.parseCommand("add -t -d 2022-09-19 -c CS2102"));
+    }
+
+    @Test
+    public void parseCommand_addTaskCommandNoModCode_returnsTrue() throws Exception {
+        AddTaskCommand cmd = AddTaskCommandBuilder.build("desc", null, null);
+        assertEquals(parser.parseCommand("add -t desc"), cmd);
+    }
+
+    @Test
+    public void parseCommand_addTaskCommandInvalidDate_throws() {
+        assertThrows(ParseException.class, "Invalid date, date must be in YYYY-MM-DD format", ()
+                -> parser.parseCommand("add -t desc -d 4"));
+    }
+
+
 }
