0f7646e483d1cf80b39be42ec8fa44206d5a0e34
Shashank Acharya <acharya.s@icloud.com>
2022-10-23 11:45:42 +0800

    Add some shortcuts to TagUtil

---
+++ src/main/java/seedu/address/model/tag/TagUtil.java
@@ -0,0 +1,32 @@
+package seedu.address.model.tag;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Helper class that contains tag shortcuts.
+ */
+public class TagUtil {
+    private static final Map<String, String> SHORTCUTS = new HashMap<>() {{
+            put("tut", "tutorial");
+            put("assm", "assignment");
+            put("pres", "presentation");
+            put("lec", "lecture");
+        }};
+
+    // to prevent instantiation
+    private TagUtil() {}
+
+    /**
+     * Retrieves the expanded string if shortcut is in variable, else returns the original string
+     */
+    public static String retrieveTagString(String string) {
+        for (String key : SHORTCUTS.keySet()) {
+            if (string.toLowerCase().equals(key)) {
+                return SHORTCUTS.get(key);
+            }
+        }
+        return string;
+    }
+
+}
