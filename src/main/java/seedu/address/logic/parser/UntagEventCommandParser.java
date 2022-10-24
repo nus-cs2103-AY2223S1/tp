81f33abc664b477659b063317eea806790feba64
Qiaoran-M <72024294+Qiaoran-M@users.noreply.github.com>
2022-10-24 10:40:22 +0800

    Update src/main/java/seedu/address/logic/parser/UntagEventCommandParser.java
    Co-authored-by: Benjamin-Sim <benjamin.sim@u.nus.edu>

--- src/main/java/seedu/address/logic/parser/UntagEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UntagEventCommandParser.java
@@ -14,8 +14,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class UntagEventCommandParser implements Parser<UntagEventCommand> {
     /**
-     * Parses the given {@code String} of arguments in the context of the TagPersonsToEventCommand
-     * and returns an TagPersonsToEventCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the UntagEventCommand
+     * and returns an UntagEventCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public UntagEventCommand parse(String args) throws ParseException {
