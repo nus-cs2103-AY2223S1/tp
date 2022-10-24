f6d2d765bf0530a6437502af2a3e61221a49ffcf
florentianayuwono <e0851439@u.nus.edu>
2022-10-25 02:40:13 +0800

    Add validation checker

--- src/main/java/soconnect/logic/parser/SearchCommandParser.java
+++ src/main/java/soconnect/logic/parser/SearchCommandParser.java
@@ -8,6 +8,7 @@ import static soconnect.logic.parser.CliSyntax.PREFIX_NAME;
 import static soconnect.logic.parser.CliSyntax.PREFIX_PHONE;
 import static soconnect.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import soconnect.logic.commands.SearchCommand;
@@ -38,8 +39,23 @@ public class SearchCommandParser implements Parser<SearchCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
+        if (!isValidSearchKeyword(argMultimap)) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
+        }
         String condition = argMultimap.getPreamble().toLowerCase();
         return parseSearchCondition(argMultimap, condition);
+    }
+
+    /**
+     * Checks whether the search keywords are of valid types or not.
+     * Invalid types are keywords that do not contain any letters or numbers.
+     * For example, "++" is invalid, but "a+l+e+x" is valid.
+     */
+    private boolean isValidSearchKeyword(ArgumentMultimap argMultimap) {
+        List<String> keywords = argMultimap.getAllValues();
+        return keywords.stream().anyMatch(
+                keyword -> !keyword.replaceAll("[^a-zA-Z0-9]", "").trim().isEmpty());
     }
 
     /**
