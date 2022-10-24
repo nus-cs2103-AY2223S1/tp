b26ad347a128a054d49ef5a37fe815a3abdca778
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-10-24 14:20:13 +0800

    Clean up refactored validators

--- src/main/java/seedu/foodrem/model/item/itemvalidators/ItemPriceValidator.java
+++ src/main/java/seedu/foodrem/model/item/itemvalidators/ItemPriceValidator.java
@@ -8,21 +8,10 @@ import seedu.foodrem.commons.util.ValidationUtil;
  * Validation class for item quantities.
  */
 public class ItemPriceValidator implements Validator {
-    // Validation for characters used in price
-    private static final String MESSAGE_FOR_NOT_A_NUMBER =
-            "The item price should be a number.";
-    private static final String MESSAGE_FOR_PRICE_IS_NEGATIVE =
-            "The item price should not be negative.";
-
     // Validation for price precision
     private static final int MAX_DECIMAL_PLACE = 2;
-    private static final String MESSAGE_FOR_PRECISION_TOO_HIGH =
-            String.format("The item price should not have more than %d decimal places.", MAX_DECIMAL_PLACE);
-
     // Validation for size of price
     private static final int MAX_PRICE = 1_000_000;
-    private static final String MESSAGE_FOR_PRICE_TOO_LARGE =
-            String.format("The item price should not be more than %,d.", MAX_PRICE);
 
     /**
      * Validates a given input String. This is to be used during construction.
@@ -30,12 +19,15 @@ public class ItemPriceValidator implements Validator {
      * @param itemPriceString String representation of item price to validate against.
      */
     public static Void validate(String itemPriceString) {
-        return validateNumericString(itemPriceString, MAX_DECIMAL_PLACE, MESSAGE_FOR_NOT_A_NUMBER,
-                MESSAGE_FOR_PRECISION_TOO_HIGH, MAX_PRICE, MESSAGE_FOR_PRICE_TOO_LARGE, MESSAGE_FOR_PRICE_IS_NEGATIVE);
+        return validateNumericString(itemPriceString, MAX_DECIMAL_PLACE, MAX_PRICE,
+                "The item price should be a number.",
+                String.format("The item price should not have more than %d decimal places.", MAX_DECIMAL_PLACE),
+                String.format("The item price should not be more than %,d.", MAX_PRICE),
+                "The item price should not be negative.");
     }
 
-    static Void validateNumericString(String numericString, int maxDecimalPlace, String messageNotANumber,
-                                      String messageTooPrecise, int maximum, String messageTooLarge,
+    static Void validateNumericString(String numericString, int maxDecimalPlace, int maximum,
+                                      String messageNotANumber, String messageTooPrecise, String messageTooLarge,
                                       String messageIsNegative) {
         boolean isParsable = ValidationUtil.isParsableDouble(numericString);
         boolean isTooPrecise = ValidationUtil.isDoubleTooPrecise(numericString, maxDecimalPlace);
@@ -44,7 +36,6 @@ public class ItemPriceValidator implements Validator {
         checkArgument(!isTooPrecise, messageTooPrecise);
 
         double number = Double.parseDouble(numericString);
-
         boolean isWithinMaximum = number <= maximum;
         boolean isNonNegative = ValidationUtil.isNonNegative(number);
 
--- src/main/java/seedu/foodrem/model/item/itemvalidators/ItemQuantityValidator.java
+++ src/main/java/seedu/foodrem/model/item/itemvalidators/ItemQuantityValidator.java
@@ -6,21 +6,10 @@ import static seedu.foodrem.model.item.itemvalidators.ItemPriceValidator.validat
  * Validation class for item quantities.
  */
 public class ItemQuantityValidator implements Validator {
-    // Validation for characters used in quantity
-    private static final String MESSAGE_FOR_NOT_A_NUMBER =
-            "The item quantity should be a number.";
-    private static final String MESSAGE_FOR_QUANTITY_IS_NEGATIVE =
-            "The item quantity should not be negative.";
-
     // Validation for quantity precision
     private static final int MAX_DECIMAL_PLACE = 4;
-    private static final String MESSAGE_FOR_PRECISION_TOO_HIGH =
-            String.format("The item quantity should not have more than %d decimal places.", MAX_DECIMAL_PLACE);
-
     // Validation for size of quantity
     private static final int MAX_QUANTITY = 1_000_000;
-    private static final String MESSAGE_FOR_QUANTITY_TOO_LARGE =
-            String.format("The item quantity should not be more than %,d.", MAX_QUANTITY);
 
     /**
      * Validates a given input String. This is to be used during construction.
@@ -28,8 +17,10 @@ public class ItemQuantityValidator implements Validator {
      * @param itemQuantityString String representation of item quantity to validate against.
      */
     public static Void validate(String itemQuantityString) {
-        return validateNumericString(itemQuantityString, MAX_DECIMAL_PLACE, MESSAGE_FOR_NOT_A_NUMBER,
-                MESSAGE_FOR_PRECISION_TOO_HIGH, MAX_QUANTITY, MESSAGE_FOR_QUANTITY_TOO_LARGE,
-                MESSAGE_FOR_QUANTITY_IS_NEGATIVE);
+        return validateNumericString(itemQuantityString, MAX_DECIMAL_PLACE, MAX_QUANTITY,
+                "The item quantity should be a number.",
+                String.format("The item quantity should not have more than %d decimal places.", MAX_DECIMAL_PLACE),
+                String.format("The item quantity should not be more than %,d.", MAX_QUANTITY),
+                "The item quantity should not be negative.");
     }
 }
