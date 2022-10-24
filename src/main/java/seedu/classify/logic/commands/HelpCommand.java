d4c68871f8c44b535fb66fbc2002efb69c7c31d6
jeromepui <jeromepui00@gmail.com>
2022-10-24 19:09:40 +0800

    Update HelpCommand message usage

--- src/main/java/seedu/classify/logic/commands/HelpCommand.java
+++ src/main/java/seedu/classify/logic/commands/HelpCommand.java
@@ -9,7 +9,7 @@ public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows command summary table.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
