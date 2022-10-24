5f67506909789cf696a90a9a981824e0bf370506
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-24 14:15:29 +0800

    Fix message to account for a single item on list

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,9 +10,9 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX = "The project index provided is invalid";
     public static final String MESSAGE_INVALID_PROJECT_DISPLAYED_ID = "The project index provided is invalid";
-    public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d clients listed!";
-    public static final String MESSAGE_ISSUES_LISTED_OVERVIEW = "%1$d issues listed!";
-    public static final String MESSAGE_PROJECTS_LISTED_OVERVIEW = "%1$d projects listed!";
+    public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d client(s) listed!";
+    public static final String MESSAGE_ISSUES_LISTED_OVERVIEW = "%1$d issue(s) listed!";
+    public static final String MESSAGE_PROJECTS_LISTED_OVERVIEW = "%1$d project(s) listed!";
     public static final String MESSAGE_INVALID_ISSUE_DISPLAYED_ID = "The issue id provided is invalid";
     public static final String MESSAGE_MISSING_ARGUMENTS = "Missing some arguments! \n%1$s";
     public static final String MESSAGE_ISSUE_NOT_FOUND = "This issue id does not exist in the address book";
