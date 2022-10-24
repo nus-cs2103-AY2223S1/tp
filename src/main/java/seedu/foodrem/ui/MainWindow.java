2175acf83d2c6414fdb76805c1094420af55ea9c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-10-24 15:21:48 +0800

    Prevents window from being resized

--- src/main/java/seedu/foodrem/ui/MainWindow.java
+++ src/main/java/seedu/foodrem/ui/MainWindow.java
@@ -123,6 +123,7 @@ public class MainWindow extends UiPart<Stage> {
     private void setWindowDefaultSize(GuiSettings guiSettings) {
         primaryStage.setHeight(guiSettings.getWindowHeight());
         primaryStage.setWidth(guiSettings.getWindowWidth());
+        primaryStage.setResizable(false);
         if (guiSettings.getWindowCoordinates() != null) {
             primaryStage.setX(guiSettings.getWindowCoordinates().getX());
             primaryStage.setY(guiSettings.getWindowCoordinates().getY());
