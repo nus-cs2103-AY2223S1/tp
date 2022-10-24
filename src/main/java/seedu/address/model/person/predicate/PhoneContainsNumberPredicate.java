76941b60e4ae375e1642831028f5b820114d1826
Caden <63772723+cadencjk@users.noreply.github.com>
2022-10-25 00:21:16 +0800

    Add ability to find by phone number using p/ tag

---
+++ src/main/java/seedu/address/model/person/predicate/PhoneContainsNumberPredicate.java
@@ -0,0 +1,29 @@
+package seedu.address.model.person.predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.person.Person;
+
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Person}'s {@code Class} matches the phone given.
+ */
+public class PhoneContainsNumberPredicate implements Predicate<Person> {
+    private String keyword;
+
+    public PhoneContainsNumberPredicate(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return StringUtil.containsWord(person.getPhone().value, keyword);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PhoneContainsNumberPredicate // instanceof handles nulls
+                && keyword.equals(((PhoneContainsNumberPredicate) other).keyword)); // state check
+    }
+}
