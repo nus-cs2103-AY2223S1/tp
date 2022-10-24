62c2d41056ed397e0346092d96244070e773277e
liviamil <liviamil@u.nus.edu>
2022-10-24 16:46:51 +0800

    Add Tutorial Class

---
+++ src/main/java/seedu/address/model/person/Tutorial.java
@@ -0,0 +1,60 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's tutorial group in uNivUSal
+ */
+public class Tutorial {
+
+    public static final String MESSAGE_CONSTRAINTS = "Tutorial should only contain alphanumeric characters,"
+            + "and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}].*";
+
+    public final String tut;
+
+    /**
+     * Constructs a {@code Tutorial}.
+     *
+     * @param tutorial A valid tutorial group.
+     */
+    public Tutorial(String tutorial) {
+        requireNonNull(tutorial);
+        checkArgument(isValidTutorial(tutorial), MESSAGE_CONSTRAINTS);
+        this.tut = tutorial;
+    }
+
+    /**
+     * Returns true if input is a valid Tutorial.
+     *
+     * @param test Input to be tested.
+     * @return True for valid input.
+     */
+    public static boolean isValidTutorial(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return tut;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Tutorial // instanceof handles nulls
+                && tut.equals(((Tutorial) other).tut)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return tut.hashCode();
+    }
+
+}
