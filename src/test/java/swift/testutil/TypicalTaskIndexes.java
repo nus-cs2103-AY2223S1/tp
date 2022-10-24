9f7d2a26c0bb472dbcb3bc4bf8f40a618eb502a7
yunruu <yunruuchin@gmail.com>
2022-10-24 23:45:17 +0800

    Test: add INDEX_THIRD_TASK for select task testing

--- src/test/java/swift/testutil/TypicalTaskIndexes.java
+++ src/test/java/swift/testutil/TypicalTaskIndexes.java
@@ -8,4 +8,5 @@ import swift.commons.core.index.Index;
 public class TypicalTaskIndexes {
     public static final Index INDEX_FIRST_TASK = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_TASK = Index.fromOneBased(2);
+    public static final Index INDEX_THIRD_TASK = Index.fromOneBased(3);
 }
