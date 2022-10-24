2d333b44e0cda24300403277968cbd5fb01a8bca
RachelChua <e0772465@u.nus.edu>
2022-10-24 15:22:08 +0800

    Correct javadocs

--- src/main/java/seedu/address/logic/parser/EditExamCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditExamCommandParser.java
@@ -12,13 +12,13 @@ import seedu.address.logic.commands.EditExamCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.module.Module;
 /**
- * Parses input arguments and creates a new EditCommand object
+ * Parses input arguments and creates a new EditExamCommand object
  */
 public class EditExamCommandParser implements Parser<EditExamCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the EditCommand
-     * and returns an EditCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the EditExamCommand
+     * and returns an EditExamCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditExamCommand parse(String args) throws ParseException {
