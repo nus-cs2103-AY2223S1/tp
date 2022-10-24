f9d2555ef437067f686d61c2a80444477b705966
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-10-24 18:15:33 +0800

    Add test and equals methods

--- src/main/java/seedu/address/logic/parser/predicates/IssueContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/logic/parser/predicates/IssueContainsKeywordsPredicate.java
@@ -4,11 +4,12 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.model.issue.Issue;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Tests that an Issue object matches any of the keywords given.
  */
-public class IssueContainsKeywordsPredicate {
+public class IssueContainsKeywordsPredicate implements Predicate<Issue> {
 
     private final List<String> descriptionKeywords;
     private final List<String> statusKeywords;
@@ -34,6 +35,20 @@ public class IssueContainsKeywordsPredicate {
     public boolean testStatus(Issue issue) {
         return statusKeywords.isEmpty() ? true : statusKeywords.stream()
                 .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(issue.getStatus().toString(), keyword));
+    }
+
+    @Override
+    public boolean test(Issue issue) {
+        return testDescription(issue) && testPriority(issue) && testStatus(issue);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof IssueContainsKeywordsPredicate // instanceof handles nulls
+                && descriptionKeywords.equals(((IssueContainsKeywordsPredicate) other).descriptionKeywords)
+                && statusKeywords.equals(((IssueContainsKeywordsPredicate) other).statusKeywords) //state checks
+                && priorityKeywords.equals(((IssueContainsKeywordsPredicate) other).priorityKeywords));
     }
 
 }
