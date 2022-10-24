e83bc8033fc2e834678d6b8466bb2726dd5a47b4
Tan-Jia-Rong <tanjiarongtjr@gmail.com>
2022-10-25 01:44:46 +0800

    Remove FindCommand and update test cases

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.logic.commands.AddPersonCommand;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.HomeCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
@@ -63,8 +63,8 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() throws Exception {
-        String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        String homeCommand = HomeCommand.COMMAND_WORD;
+        assertCommandSuccess(homeCommand, HomeCommand.MESSAGE_SUCCESS, model);
     }
 
     @Test
