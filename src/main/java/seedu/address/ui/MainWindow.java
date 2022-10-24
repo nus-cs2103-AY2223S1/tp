a7800a05d92374e03eeda97a1d26d5f019f7ecde
Tex Tang <tex_tang@outlook.com>
2022-10-24 23:01:45 +0800

    Ui: Remove unused import

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import java.util.Stack;
 import java.util.logging.Logger;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCode;
