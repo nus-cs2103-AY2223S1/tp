a073cd01b40929239b1f641f2b81c194ca13ccc3
Tex Tang <tex_tang@outlook.com>
2022-10-24 23:01:06 +0800

    Ui: Remove unused method

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -200,10 +200,6 @@ public class MainWindow extends UiPart<Stage> {
         }
     }
 
-    public PersonListPanel getPersonListPanel() {
-        return personListPanel;
-    }
-
     /**
      * Executes the command and returns the result.
      *
