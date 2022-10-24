91a5e04aa533ce5c5317a414667cc08f471be059
nickeltea <e0772462@u.nus.edu>
2022-10-25 02:00:25 +0800

    Implement the ReminderCommand class
    We need to implement the remindT command.
    
    There should be a ReminderCommand class.
    
    Let's implement the ReminderCommand class.

--- src/main/java/seedu/address/logic/commands/task/ReminderCommand.java
+++ src/main/java/seedu/address/logic/commands/task/ReminderCommand.java
@@ -1,2 +1,36 @@
-package seedu.address.logic.commands.task;public class ReminderCommand {
+package seedu.address.logic.commands.task;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+import seedu.address.model.task.TaskContainsKeywordsPredicate;
+
+import static java.util.Objects.requireNonNull;
+
+public class ReminderCommand extends Command {
+
+    public static final String COMMAND_WORD = "remindT";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows tasks with deadlines up to and including the "
+            + "date provided. "
+            + "Parameters: DEADLINE (must be in dd-mm-yyyy format)\n"
+            + "Example: " + COMMAND_WORD + " "
+            + "12-09-2022";
+
+    private final TaskContainsKeywordsPredicate predicate;
+
+    public static final String REMINDER_SUCCESS = "Here are your upcoming deadlines.";
+
+    public ReminderCommand(TaskContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredTaskList(predicate);
+        return new CommandResult(REMINDER_SUCCESS);
+    }
+
+
 }
