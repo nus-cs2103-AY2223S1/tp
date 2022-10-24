68952b298d44f907cf8b17541f43507657f38adb
CeereeC <myself3216@gmail.com>
2022-10-24 20:23:07 +0800

    Fix delete event command

--- src/main/java/seedu/travelr/model/ModelManager.java
+++ src/main/java/seedu/travelr/model/ModelManager.java
@@ -220,6 +220,11 @@ public class ModelManager implements Model {
     }
 
     @Override
+    public ObservableList<Event> getBucketList() {
+        return bucketList;
+    }
+
+    @Override
     public boolean equals(Object obj) {
         // short circuit if same object
         if (obj == this) {
