d85dbb1f94fbb96c4d9bc438447425074d05ec4a
张博坤 <>
2022-10-24 23:03:40 +0800

    no message

--- src/main/java/hobbylist/logic/commands/FindCommand.java
+++ src/main/java/hobbylist/logic/commands/FindCommand.java
@@ -16,9 +16,11 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all activities whose names or descriptions"
             + "contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " sleep exercise code";
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers "
+            + "or find all activities on a certain date/in a certain month/year.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...or yyyy-mm-dd/yyyy-mm/yyyy\n"
+            + "Example: " + COMMAND_WORD + " sleep exercise code\n"
+            + "Example: " + COMMAND_WORD + " 1974-02-02\n";
 
     private final NameOrDescContainsKeywordsPredicate predicate;
 
