115a8e3b2ade4ea4e2750e6b2e2bb8fdc0080023
Lester Ong <lesterong776@gmail.com>
2022-10-24 18:27:54 +0800

    Minor fixes

--- src/main/java/seedu/address/model/event/DateTime.java
+++ src/main/java/seedu/address/model/event/DateTime.java
@@ -201,10 +201,6 @@ public class DateTime {
         return this.date;
     }
 
-    public Optional<LocalTime> getTime() {
-        return this.time;
-    }
-
     @Override
     public String toString() {
         return date.format(DateTimeFormatter.ofPattern(RECOMMENDED_DATE_FORMAT))
