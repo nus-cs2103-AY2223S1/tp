355d1f36a433f9fdc193d3d06ae774eaafc6c970
JustinS00 <justinsiaw00@gmail.com>
2022-10-24 16:02:35 +0800

    Add test case for GuiSettings and UserPrefs

---
+++ src/test/java/seedu/phu/commons/core/GuiSettingsTest.java
@@ -0,0 +1,27 @@
+package seedu.phu.commons.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.Point;
+
+
+public class GuiSettingsTest {
+    @Test
+    public void constructor() {
+        GuiSettings guiSettings = new GuiSettings(920,650,0,0);
+        assertEquals(guiSettings.getWindowWidth(), 920);
+        assertEquals(guiSettings.getWindowHeight(), 650);
+        assertEquals(guiSettings.getWindowCoordinates(), new Point(0,0));
+    }
+    @Test
+    public void equals() {
+        GuiSettings guiSettings = new GuiSettings(920,650,0,0);
+        assertTrue(new GuiSettings().equals(new GuiSettings()));
+        assertFalse(guiSettings.equals(new GuiSettings(920,650,10,10)));
+        assertFalse(guiSettings.equals(null));
+    }
+}
