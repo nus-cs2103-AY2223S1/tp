983b9e21e5b382ba6983327d85167c1a1d6047cf
yeoyujie <yeo.yujie@gmail.com>
2022-10-24 23:49:32 +0800

    Add SortReminderCommand

---
+++ src/main/java/seedu/address/logic/commands/reminder/SortReminderCommand.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands.reminder;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Sorts reminder by their priority. Reminders with same priority will be sorted lexicographically.
+ */
+public class SortReminderCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort reminder";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts reminder by their priority level ";
+
+    public static final String MESSAGE_SUCCESS = "Reminders sorted";
+    
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
