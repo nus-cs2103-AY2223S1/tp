9473b5c9f62caee8d4a7e3f013cd5dc03adb992a
Kee Seow Han <79247296+KSHan29@users.noreply.github.com>
2022-10-24 16:44:45 +0800

    Update src/main/java/seedu/address/logic/parser/ParserUtil.java
    Co-authored-by: lesterong <84223259+lesterong@users.noreply.github.com>

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -22,7 +22,7 @@ import seedu.address.model.tag.Tag;
  */
 public class ParserUtil {
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
-    public static final String MESSAGE_INVALID_DAYS = "Days provided is not a non-zeo unsigned integer";
+    public static final String MESSAGE_INVALID_DAYS = "Days provided is not a non-zero unsigned integer";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
