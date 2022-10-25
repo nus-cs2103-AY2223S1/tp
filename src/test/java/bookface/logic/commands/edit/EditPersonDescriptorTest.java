69ece869639f730fc5d41c6ef73244a31f52756f
parth-io <67057645+parth-io@users.noreply.github.com>
2022-10-26 00:30:31 +0800

    Fix checkstyle

--- src/test/java/bookface/logic/commands/edit/EditPersonDescriptorTest.java
+++ src/test/java/bookface/logic/commands/edit/EditPersonDescriptorTest.java
@@ -1,9 +1,7 @@
 package bookface.logic.commands.edit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
--- src/test/java/bookface/logic/commands/edit/EditUserCommandTest.java
+++ src/test/java/bookface/logic/commands/edit/EditUserCommandTest.java
@@ -3,7 +3,6 @@ package bookface.logic.commands.edit;
 import static bookface.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static bookface.testutil.TypicalPersons.getTypicalBookFaceData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
