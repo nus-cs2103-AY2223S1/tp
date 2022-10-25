f5e82f45c74bf0fdb23dcf761d56c24dcbc16f3c
KeithPJX <KeithPJX@gmail.com>
2022-10-25 06:58:43 -0700

    Fix checkstyle issue

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -30,7 +30,7 @@ public class AddCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book.\n" + "Parameters: "
             + PREFIX_NAME + "NAME " + PREFIX_PHONE + "PHONE " + PREFIX_EMAIL + "EMAIL " + PREFIX_ADDRESS + "ADDRESS "
             + PREFIX_GENDER + "GENDER " + PREFIX_BIRTHDATE + "BIRTHDATE " + PREFIX_RACE + "RACE " + PREFIX_RELIGION
-            + "RELIGION " + "[" + PREFIX_SURVEY + "SURVEY]... "  + "[" + PREFIX_TAG + "TAG]...\n"
+            + "RELIGION " + "[" + PREFIX_SURVEY + "SURVEY]... " + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "John Doe " + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com " + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_GENDER + "male " + PREFIX_BIRTHDATE + "1998-11-28 " + PREFIX_RACE + "Chinese " + PREFIX_RELIGION
