e657b73f627b412f89a349acd7902a6a35636ebf
Bryan Yang <e0772641@u.nus.edu>
2022-10-26 00:39:46 +0800

    Fix JavaDoc comment in DebtNotFoundException

--- src/main/java/paymelah/model/debt/exceptions/DebtNotFoundException.java
+++ src/main/java/paymelah/model/debt/exceptions/DebtNotFoundException.java
@@ -1,6 +1,6 @@
 package paymelah.model.debt.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified person.
+ * Signals that the operation is unable to find the specified debt.
  */
 public class DebtNotFoundException extends RuntimeException {}
