23cff49a55a5ae510318fe0811e237369d8d1637
Tex Tang <tex_tang@outlook.com>
2022-10-25 22:12:03 +0800

    Test: Fix HelpCommand test

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -77,7 +77,7 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_help() throws Exception {
         assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
-        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " " + AddCommand.COMMAND_WORD) instanceof HelpCommand);
     }
 
     @Test
