7248e82695dc258ddcdb4cf5813e0d094969a453
Marcus Pang <marcuspangyuyang@gmail.com>
2022-10-25 16:55:06 +0800

    Refactor LocalDateTime parsing and improve error message

--- src/main/java/seedu/address/logic/parser/LocalDateTimeConverter.java
+++ src/main/java/seedu/address/logic/parser/LocalDateTimeConverter.java
@@ -1,31 +1,27 @@
 package seedu.address.logic.parser;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Stack;
 
 import picocli.CommandLine;
-import seedu.address.logic.commands.SetDeadlineCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Converter from {@code String} arguments to {@code LocalDateTime}.
  */
 public class LocalDateTimeConverter implements CommandLine.IParameterConsumer {
+
     @Override
     public void consumeParameters(Stack<String> args, CommandLine.Model.ArgSpec argSpec,
                                   CommandLine.Model.CommandSpec commandSpec) {
         if (args.size() != 2) {
-            throw new CommandLine.ParameterException(commandSpec.commandLine(), "Requires date and time");
+            throw new CommandLine.ParameterException(commandSpec.commandLine(), ParserUtil.MESSAGE_INVALID_DATETIME);
         }
 
         try {
             String datetimeString = args.pop() + " " + args.pop();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            argSpec.setValue(LocalDateTime.parse(datetimeString, formatter));
-        } catch (DateTimeParseException | NumberFormatException pe) {
-            // TODO: Use separate error message instead of command usage message
-            throw new CommandLine.TypeConversionException(SetDeadlineCommand.MESSAGE_USAGE);
+            argSpec.setValue(ParserUtil.parseLocalDateTime(datetimeString));
+        } catch (ParseException e) {
+            throw new CommandLine.TypeConversionException(e.getMessage());
         }
     }
 }
