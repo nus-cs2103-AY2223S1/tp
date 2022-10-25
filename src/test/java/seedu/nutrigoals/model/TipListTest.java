df167c68470be879fe78164461a39ae15f465531
AkkFiros <81766175+AkkFiros@users.noreply.github.com>
2022-10-26 03:58:07 +0800

    Update TipListTest.java for CheckStyle

--- src/test/java/seedu/nutrigoals/model/TipListTest.java
+++ src/test/java/seedu/nutrigoals/model/TipListTest.java
@@ -1,8 +1,5 @@
 package seedu.nutrigoals.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
