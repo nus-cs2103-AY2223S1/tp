6f63ca39426fec21774eac034f34d6bd01b70d40
njxue <ngjxue@gmail.com>
2022-10-25 18:45:59 +0800

    Change empty command message and help command message

--- src/main/java/seedu/taassist/commons/core/Messages.java
+++ src/main/java/seedu/taassist/commons/core/Messages.java
@@ -1,11 +1,15 @@
 package seedu.taassist.commons.core;
 
+import seedu.taassist.logic.commands.HelpCommand;
+
 /**
  * Container for user visible messages.
  */
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command.";
+    public static final String MESSAGE_EMPTY_COMMAND = "Use the [ " + HelpCommand.COMMAND_WORD + " ] to view all "
+            + "available commands and their usages.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format for [ %1$s ]: \n%2$s.";
     public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index provided is invalid.";
     public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
