efbf6bd0def04ab94544c5530bbac1f7d90a56d6
melissaharijanto <melissa.anastasia.h@gmail.com>
2022-10-25 22:40:10 +0800

    Fix checkstyle issues

--- src/main/java/tracko/model/order/Order.java
+++ src/main/java/tracko/model/order/Order.java
@@ -101,6 +101,7 @@ public class Order {
             }
         }
         this.itemList.add(itemQuantityPair);
+        calculateTotalOrderPrice();
     }
 
     public LocalDateTime getTimeCreated() {
--- src/test/java/tracko/model/order/ItemQuantityPairTest.java
+++ src/test/java/tracko/model/order/ItemQuantityPairTest.java
@@ -1,6 +1,10 @@
 package tracko.model.order;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tracko.testutil.Assert.assertThrows;
 
 import java.util.HashSet;
