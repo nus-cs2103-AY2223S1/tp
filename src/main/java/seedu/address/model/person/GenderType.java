354e407d1129ad10bae98f3001aca215aba355ee
Qiaoran-M <e0492494@u.nus.edu>
2022-10-25 18:11:28 +0800

    Change test data and GenderType methods according to PR review

--- src/main/java/seedu/address/model/person/GenderType.java
+++ src/main/java/seedu/address/model/person/GenderType.java
@@ -1,5 +1,7 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,9 +15,11 @@ public enum GenderType {
     // Declaration order defines the natural order of GenderType
     // Natural order is used in the compareTo() method
     FEMALE("Female"),
-    MALE("Male");
+    MALE("Male"),
+    NO_GENDER("NA");
 
     private static final Set<String> MALE_GENDERS = new HashSet<>(Arrays.asList("m", "male", "M", "Male"));
+    private static final Set<String> FEMALE_GENDERS = new HashSet<>(Arrays.asList("F", "Female", "f", "female"));
     private String gender;
 
 
@@ -24,10 +28,13 @@ public enum GenderType {
     }
 
     public static GenderType getGenderType(String gender) {
+        requireNonNull(gender);
         if (MALE_GENDERS.contains(gender)) {
             return MALE;
-        } else {
+        } else if (FEMALE_GENDERS.contains(gender)) {
             return FEMALE;
+        } else {
+            return NO_GENDER;
         }
     }
 
