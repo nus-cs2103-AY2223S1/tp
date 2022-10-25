89799c4419f9da47d2196da68c8bd8c5a4d1a9da
guanzhou03 <guanzhou03@gmail.com>
2022-10-25 15:55:59 +0800

    Fix comments

--- src/main/java/seedu/address/logic/commands/UnhideAppointmentsCommand.java
+++ src/main/java/seedu/address/logic/commands/UnhideAppointmentsCommand.java
@@ -14,7 +14,7 @@ import seedu.address.model.person.Appointment;
 import seedu.address.model.person.HiddenPredicateSingleton;
 
 /**
- * Hides all persons and their appointments in address book whose name contains any of the argument keywords.
+ * Unhides appointments in idENTify previously hidden by some condition.
  * Keyword matching is case insensitive.
  */
 public class UnhideAppointmentsCommand extends Command {
