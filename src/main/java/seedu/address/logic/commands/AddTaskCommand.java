46f34373642ec31037134503223b5c58fd8b569e
Marcus Pang <marcuspangyuyang@gmail.com>
2022-10-25 16:33:23 +0800

    Fix format for add task help message

--- src/main/java/seedu/address/logic/commands/AddTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTaskCommand.java
@@ -15,13 +15,12 @@ public class AddTaskCommand extends Command {
     public static final String COMMAND_WORD = "add task";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-        + ": Adds a task to the current team.\n"
-        + "Parameters: name of task (cannot be empty and cannot begin with a space) \n"
-        + "Example: " + COMMAND_WORD + " merge PR#12";
+            + ": Adds a task to the current team.\n"
+            + "Parameters: TASK_NAME\n"
+            + "Example: " + COMMAND_WORD + " merge PR#12";
 
     public static final String MESSAGE_ADD_TASK_SUCCESS = "Added Task: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the team";
-    public static final String MESSAGE_TASK_NAME_FORMAT_ERROR = "Task name cannot be empty";
 
     @CommandLine.Parameters(arity = "1")
     private Task task;
