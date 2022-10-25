81ae0a06c9642673c5fa59c468e5fc611a595e6c
LianGuoYang <e0735382@u.nus.edu>
2022-10-25 18:50:57 +0800

    Fix test cases

--- src/test/java/seedu/address/ui/CommandFeaturesTest.java
+++ src/test/java/seedu/address/ui/CommandFeaturesTest.java
@@ -12,8 +12,29 @@ public class CommandFeaturesTest {
     private static final CommandFeatures ADDPERSON_COMMAND_FEATURES = new CommandFeatures(
             "addperson",
             "Adds a person to the address book.",
-            "addperson n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS [t/TAG]",
-            "addperson n/John Doe p/98765432 e/johnd@example.com a/John street, block 123, #01-01"
+            "addperson n/NAME p/PHONE e/EMAIL a/ADDRESS [t/TAG]",
+            "addperson n/John Doe p/98765432 e/john@example.com a/31, Clementi Ave 2, #025 t/friend t/owesMoney"
+    );
+
+    private static final CommandFeatures ADDGROUP_COMMAND_FEATURES = new CommandFeatures(
+            "addgroup",
+            "Adds an empty group to the address book.",
+            "addgroup g/GROUP",
+            " addgroup g/CS2103T Team Project"
+    );
+
+    private static final CommandFeatures ASSIGNTASK_COMMAND_FEATURES = new CommandFeatures(
+            "assigntask",
+            "Assigns a task to a user with the given name in a group.",
+            "assigntask NAME g/GROUP task/TASK w/WORKLOAD [d/DEADLINE]",
+            "assigntask Alice g/Group Alpha task/Coursework 0 w/High d/2022-01-01 23:59"
+    );
+
+    private static final CommandFeatures CLEAR_COMMAND_FEATURES = new CommandFeatures(
+            "clear",
+            "Clears all entries from the address book.",
+            "clear",
+            "clear"
     );
 
     @Test
@@ -34,10 +55,29 @@ public class CommandFeaturesTest {
 
     @Test
     public void getCommandFeaturesList_success() {
-        ObservableList<CommandFeatures> featureList = CommandFeatures.getCommandFeaturesList();
+        ObservableList<CommandFeatures> commandFeatureList = CommandFeatures.getCommandFeaturesList();
+        assertEquals(commandFeatureList.get(0).getCommand(), ADDPERSON_COMMAND_FEATURES.getCommand());
+        assertEquals(commandFeatureList.get(0).getDescription(), ADDPERSON_COMMAND_FEATURES.getDescription());
 
-        assertEquals(featureList.get(0).getCommand(), ADDPERSON_COMMAND_FEATURES.getCommand());
-        assertEquals(featureList.get(0).getDescription(), ADDPERSON_COMMAND_FEATURES.getDescription());
+        ObservableList<CommandFeatures> personFeatureList = CommandFeatures.getPersonFeaturesList();
+
+        assertEquals(personFeatureList.get(0).getCommand(), ADDPERSON_COMMAND_FEATURES.getCommand());
+        assertEquals(personFeatureList.get(0).getDescription(), ADDPERSON_COMMAND_FEATURES.getDescription());
+
+        ObservableList<CommandFeatures> groupFeatureList = CommandFeatures.getGroupFeaturesList();
+
+        assertEquals(groupFeatureList.get(0).getCommand(), ADDGROUP_COMMAND_FEATURES.getCommand());
+        assertEquals(groupFeatureList.get(0).getDescription(), ADDGROUP_COMMAND_FEATURES.getDescription());
+
+        ObservableList<CommandFeatures> taskFeatureList = CommandFeatures.getTaskFeaturesList();
+
+        assertEquals(taskFeatureList.get(0).getCommand(), ASSIGNTASK_COMMAND_FEATURES.getCommand());
+        assertEquals(taskFeatureList.get(0).getDescription(), ASSIGNTASK_COMMAND_FEATURES.getDescription());
+
+        ObservableList<CommandFeatures> utilityFeatureList = CommandFeatures.getUtilityFeaturesList();
+
+        assertEquals(utilityFeatureList.get(0).getCommand(), CLEAR_COMMAND_FEATURES.getCommand());
+        assertEquals(utilityFeatureList.get(0).getDescription(), CLEAR_COMMAND_FEATURES.getDescription());
     }
 
 }
