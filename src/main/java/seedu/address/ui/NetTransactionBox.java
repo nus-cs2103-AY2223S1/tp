7eb92c510c461990a8bd183ddfd7f82a632a2405
ElginL <elginlee_2000@hotmail.com>
2022-10-25 12:40:15 +0800

    Update Price, ClientCard, NetTransactionBox
    Utilise new price format

--- src/main/java/seedu/address/ui/NetTransactionBox.java
+++ src/main/java/seedu/address/ui/NetTransactionBox.java
@@ -1,20 +1,19 @@
 package seedu.address.ui;
 
-import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.PriceFormatter.formatPrice;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
-
 /**
  * The UI component that is responsible for displaying the total amount transacted with clients in the displayed list.
  */
 public class NetTransactionBox extends UiPart<Region> {
-    public static final String LABEL = "Net amount: $";
+    public static final String LABEL = "Net amount: ";
 
     private static final String FXML = "NetTransactionBox.fxml";
-    private double netTransactionAmount;
+
     @FXML
     private TextField netTransaction;
 
@@ -25,8 +24,7 @@ public class NetTransactionBox extends UiPart<Region> {
      */
     public NetTransactionBox(double amount) {
         super(FXML);
-        this.netTransactionAmount = amount;
-        netTransaction.setText(LABEL + Double.toString(amount));
+        setNetTransaction(amount);
     }
 
     /**
@@ -35,8 +33,6 @@ public class NetTransactionBox extends UiPart<Region> {
      * @param netTransactionAmount double value to be used to update the box.
      */
     public void setNetTransaction(double netTransactionAmount) {
-        this.netTransactionAmount = netTransactionAmount;
-        requireNonNull(netTransactionAmount);
-        netTransaction.setText(LABEL + Double.toString(netTransactionAmount));
+        netTransaction.setText(LABEL + formatPrice(netTransactionAmount));
     }
 }
