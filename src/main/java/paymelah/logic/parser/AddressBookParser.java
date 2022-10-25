5f8e7be596c0a762fe33966aa51ba3f84026a0ab
Bryan Yang <e0772641@u.nus.edu>
2022-10-26 01:23:53 +0800

    Add mark and unmark command to addressbook parser

--- src/main/java/paymelah/logic/parser/AddressBookParser.java
+++ src/main/java/paymelah/logic/parser/AddressBookParser.java
@@ -20,9 +20,11 @@ import paymelah.logic.commands.FindDebtCommand;
 import paymelah.logic.commands.HelpCommand;
 import paymelah.logic.commands.ListCommand;
 import paymelah.logic.commands.ListDebtorsCommand;
+import paymelah.logic.commands.MarkCommand;
 import paymelah.logic.commands.SortCommand;
 import paymelah.logic.commands.SplitDebtCommand;
 import paymelah.logic.commands.StatementCommand;
+import paymelah.logic.commands.UnmarkCommand;
 import paymelah.logic.parser.exceptions.ParseException;
 
 /**
@@ -69,6 +71,12 @@ public class AddressBookParser {
 
         case DeleteDebtCommand.COMMAND_WORD:
             return new DeleteDebtCommandParser().parse(arguments);
+
+        case MarkCommand.COMMAND_WORD:
+            return new MarkCommandParser().parse(arguments);
+
+        case UnmarkCommand.COMMAND_WORD:
+            return new UnmarkCommandParser().parse(arguments);
 
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
