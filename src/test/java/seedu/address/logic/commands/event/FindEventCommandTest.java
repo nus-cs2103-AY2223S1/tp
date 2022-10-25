ca9e46f1896ab3c8c32353936ec50dec4c6ba8cc
Lester Ong <lesterong776@gmail.com>
2022-10-25 16:25:35 +0800

    Add test cases for dates

--- src/test/java/seedu/address/logic/commands/event/FindEventCommandTest.java
+++ src/test/java/seedu/address/logic/commands/event/FindEventCommandTest.java
@@ -10,15 +10,19 @@ import static seedu.address.logic.commands.event.FindEventCommand.MESSAGE_NO_MAT
 import static seedu.address.testutil.TypicalEvents.DINNER;
 import static seedu.address.testutil.TypicalEvents.PRACTICE;
 import static seedu.address.testutil.TypicalEvents.PRESENTATION;
+import static seedu.address.testutil.TypicalEvents.PROBLEM_SET;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.event.DateTime;
+import seedu.address.model.event.StartDateTimeContainsDatePredicate;
 import seedu.address.model.event.TitleContainsKeywordsPredicate;
 import seedu.address.testutil.TypicalAddressBook;
 
@@ -55,7 +59,7 @@ public class FindEventCommandTest {
 
     @Test
     public void execute_zeroKeywords_noEventFound() {
-        TitleContainsKeywordsPredicate predicate = preparePredicate(" ");
+        TitleContainsKeywordsPredicate predicate = prepareTitlePredicate(" ");
         FindEventCommand command = new FindEventCommand(predicate);
         expectedModel.updateFilteredEventList(predicate);
         assertCommandSuccess(command, model, MESSAGE_NO_MATCH, expectedModel);
@@ -64,7 +68,7 @@ public class FindEventCommandTest {
 
     @Test
     public void execute_singleKeyword_singleEventFound() {
-        TitleContainsKeywordsPredicate predicate = preparePredicate("Presentation");
+        TitleContainsKeywordsPredicate predicate = prepareTitlePredicate("Presentation");
         FindEventCommand command = new FindEventCommand(predicate);
         expectedModel.updateFilteredEventList(predicate);
         assertCommandSuccess(command, model, MESSAGE_EVENT_LISTED_OVERVIEW, expectedModel);
@@ -74,7 +78,7 @@ public class FindEventCommandTest {
     @Test
     public void execute_multipleKeywords_multipleEventsFound() {
         String expectedMessage = String.format(MESSAGE_EVENTS_LISTED_OVERVIEW, 3);
-        TitleContainsKeywordsPredicate predicate = preparePredicate("Presentation Practice Dinner");
+        TitleContainsKeywordsPredicate predicate = prepareTitlePredicate("Presentation Practice Dinner");
         FindEventCommand command = new FindEventCommand(predicate);
         expectedModel.updateFilteredEventList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
@@ -84,18 +88,37 @@ public class FindEventCommandTest {
     @Test
     public void execute_partialKeywords_multipleEventsFound() {
         String expectedMessage = String.format(MESSAGE_EVENTS_LISTED_OVERVIEW, 3);
-        TitleContainsKeywordsPredicate predicate = preparePredicate("Pre PraC Din");
+        TitleContainsKeywordsPredicate predicate = prepareTitlePredicate("Pre PraC Din");
         FindEventCommand command = new FindEventCommand(predicate);
         expectedModel.updateFilteredEventList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertEquals(Arrays.asList(PRESENTATION, PRACTICE, DINNER), model.getFilteredEventList());
     }
 
+    @Test
+    public void execute_multipleDates_multipleEventsFound() {
+        String expectedMessage = String.format(MESSAGE_EVENTS_LISTED_OVERVIEW, 4);
+        StartDateTimeContainsDatePredicate predicate = prepareDatePredicate(
+                "11/10/2022 12-10-2022");
+        FindEventCommand command = new FindEventCommand(predicate);
+        expectedModel.updateFilteredEventList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(PRESENTATION, PRACTICE, PROBLEM_SET, DINNER), model.getFilteredEventList());
+    }
+
     /**
      * Parses {@code userInput} into a {@code TitleContainsKeywordsPredicate}.
      */
-    private TitleContainsKeywordsPredicate preparePredicate(String userInput) {
+    private TitleContainsKeywordsPredicate prepareTitlePredicate(String userInput) {
         return new TitleContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code StartDateTimeContainsDatePredicate}.
+     */
+    private StartDateTimeContainsDatePredicate prepareDatePredicate(String userInput) {
+        return new StartDateTimeContainsDatePredicate(
+                Arrays.stream(userInput.split("\\s+")).map(DateTime::new).collect(Collectors.toList()));
     }
 
 }
