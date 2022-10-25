84edee4941a3a32e7bdd9af8c0b359018b3089bc
Pinran-J <e0725480@u.nus.edu>
2022-10-25 21:43:31 +0800

    Updating ListNote command

--- src/main/java/seedu/address/logic/commands/ListNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/ListNoteCommand.java
@@ -1,10 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_NOTES;
 
-import javafx.collections.ObservableList;
 import seedu.address.model.Model;
-import seedu.address.model.note.Note;
 
 /**
  * Lists all the notes in address book to the user.
@@ -18,15 +17,7 @@ public class ListNoteCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        ObservableList<Note> notebook = model.getAddressBook().getNoteBook();
-        final StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < notebook.size(); i++) {
-            builder.append((i + 1) + ") ")
-                    .append(notebook.get(i).toString())
-                    .append("\n");
-
-        }
-        builder.append(MESSAGE_SUCCESS);
-        return new CommandResult(builder.toString());
+        model.updateFilteredNoteList(PREDICATE_SHOW_ALL_NOTES);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
