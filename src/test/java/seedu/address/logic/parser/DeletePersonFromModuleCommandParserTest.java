b18c0690a5505791a13774c7d6074f8c9e44ad56
ekweirui <ekweirui@gmail.com>
2022-10-25 23:55:41 +0800

    Add test cases to test for fields

--- src/test/java/seedu/address/logic/parser/DeletePersonFromModuleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeletePersonFromModuleCommandParserTest.java
@@ -4,7 +4,10 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_MODULE_CODE_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.MODULE_CODE_DESC_CS2106;
+import static seedu.address.logic.commands.CommandTestUtil.MODULE_CODE_DESC_MA2001;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CS2106_MODULE_CODE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -44,5 +47,41 @@ public class DeletePersonFromModuleCommandParserTest {
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 DeletePersonFromModuleCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        ModuleCode expectedModuleCode = new ModuleCode(VALID_CS2106_MODULE_CODE);
+        Name expectedName = new Name(VALID_NAME_AMY);
+
+        // whitespace only preamble
+        assertParseSuccess(parser,
+                PREAMBLE_WHITESPACE + MODULE_CODE_DESC_CS2106 + NAME_DESC_AMY,
+                new DeletePersonFromModuleCommand(expectedModuleCode, expectedName));
+
+        // multiple module codes - last module code accepted
+        assertParseSuccess(parser,
+                NAME_DESC_AMY + MODULE_CODE_DESC_MA2001 + MODULE_CODE_DESC_CS2106,
+                new DeletePersonFromModuleCommand(expectedModuleCode, expectedName));
+
+        // multiple names - last name accepted
+        assertParseSuccess(parser,
+                NAME_DESC_BOB + NAME_DESC_AMY + MODULE_CODE_DESC_CS2106,
+                new DeletePersonFromModuleCommand(expectedModuleCode, expectedName));
+    }
+
+    @Test
+    public void parse_compulsoryFieldMissing_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeletePersonFromModuleCommand.MESSAGE_USAGE);
+
+        // missing module code prefix
+        assertParseFailure(parser, NAME_DESC_AMY, expectedMessage);
+
+        // missing name prefix
+        assertParseFailure(parser, MODULE_CODE_DESC_CS2106, expectedMessage);
+
+        // all prefixes missing
+        assertParseFailure(parser, VALID_NAME_AMY + VALID_CS2106_MODULE_CODE, expectedMessage);
     }
 }
