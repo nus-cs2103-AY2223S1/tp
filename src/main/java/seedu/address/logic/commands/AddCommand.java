f24398f7083fe946a0b891861e8247191afa7e57
alexandermula <alexandersinghabdullah@gmail.com>
2022-10-25 18:02:57 +0800

    Update add command message usage

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -35,7 +35,7 @@ public class AddCommand extends Command {
         + PREFIX_EMAIL + "johnd@example.com "
         + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
         + PREFIX_INCOME + "$1000 "
-        + PREFIX_MEETING_DATE + "20 Nov 2022 "
+        + PREFIX_MEETING_DATE + "20 Nov 2022 16:30 "
         + PREFIX_TAG + "VIPClient";
 
     public static final String MESSAGE_SUCCESS = "New client added: %1$s";
