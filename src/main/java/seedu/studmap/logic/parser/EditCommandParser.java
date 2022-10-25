25700d89e06243fde49dec3f769e4a4cb3c65f5b
dr-arrgghh <dr.arrgghh@gmail.com>
2022-10-26 01:35:36 +0800

    Update EditCommandParser.java

--- src/main/java/seedu/studmap/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/studmap/logic/parser/EditCommandParser.java
@@ -29,7 +29,8 @@ public class EditCommandParser extends EditStudentCommandParser<EditCommandStude
     @Override
     public Prefix[] getPrefixes() {
         return new Prefix[]{
-                PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ID, PREFIX_GIT, PREFIX_HANDLE, PREFIX_ADDRESS, PREFIX_TAG
+            PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
+            PREFIX_ID, PREFIX_GIT, PREFIX_HANDLE, PREFIX_ADDRESS, PREFIX_TAG
         };
     }
 
