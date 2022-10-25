464e70807a2f9512ded0a7a2b94e5f5feaefddb0
comicalromance <silasyeoshuenyu@gmail.com>
2022-10-26 06:08:45 +0800

    Fix Checkstyle

--- src/test/java/seedu/studmap/model/student/ModuleTest.java
+++ src/test/java/seedu/studmap/model/student/ModuleTest.java
@@ -1,10 +1,10 @@
 package seedu.studmap.model.student;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.studmap.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class ModuleTest {
 
