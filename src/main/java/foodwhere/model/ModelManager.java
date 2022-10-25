c1356246bdd8683786d1cf7ebef382ac9d38e13b
Hikoya <hikoyanamikaze@gmail.com>
2022-10-26 01:42:47 +0800

    Update ModelManager.java

--- src/main/java/foodwhere/model/ModelManager.java
+++ src/main/java/foodwhere/model/ModelManager.java
@@ -32,7 +32,7 @@ public class ModelManager implements Model {
     public ModelManager(ReadOnlyAddressBook addressBook, ReadOnlyUserPrefs userPrefs) {
         requireAllNonNull(addressBook, userPrefs);
 
-        logger.fine("Initializing with address book: " + addressBook + " and user prefs " + userPrefs);
+        logger.fine("Initializing with stall list: " + addressBook + " and user prefs " + userPrefs);
 
         this.addressBook = new AddressBook(addressBook);
         this.userPrefs = new UserPrefs(userPrefs);
