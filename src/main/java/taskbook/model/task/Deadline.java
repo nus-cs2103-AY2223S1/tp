93fbb3e4561d5885d3b5fb36c13c75ea0dce7c7c
Tjan Eng Ger, Kevin <kevintjan27@gmail.com>
2022-10-25 16:10:47 +0800

    Remove trailing whitespaces

--- src/main/java/taskbook/model/task/Deadline.java
+++ src/main/java/taskbook/model/task/Deadline.java
@@ -50,7 +50,7 @@ public class Deadline extends Task {
         super(name, assignment, description, isDone, tags);
         this.date = date;
     }
-  
+
     @Override
     public LocalDate getDate() {
         return date;
