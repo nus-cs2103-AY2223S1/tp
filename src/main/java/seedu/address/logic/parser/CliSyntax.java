f81d70d7a53c6576d87d9a92d2355685977f6a61
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-10-25 15:46:34 +0800

    Change all usage of prefixes to new set of prefixes

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,13 +6,13 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_NOTES_TITLE = new Prefix("n_t/");
-    public static final Prefix PREFIX_NOTES_CONTENT = new Prefix("n_c/");
+    public static final Prefix PREFIX_NAME = new Prefix("name/");
+    public static final Prefix PREFIX_PHONE = new Prefix("phone/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("email/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("home/");
+    public static final Prefix PREFIX_TAG = new Prefix("tag/");
+    public static final Prefix PREFIX_NOTES_TITLE = new Prefix("title/");
+    public static final Prefix PREFIX_NOTES_CONTENT = new Prefix("content/");
     public static final Prefix PREFIX_LOAN = new Prefix("l/");
 
 }
