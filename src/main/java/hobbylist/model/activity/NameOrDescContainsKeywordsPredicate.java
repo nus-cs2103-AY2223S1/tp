8e8eb7504e96db2a9a0d16f7c7ad8daeddfbbfd4
张博坤 <>
2022-10-26 14:44:55 +0800

    Allow Find rate

--- src/main/java/hobbylist/model/activity/NameOrDescContainsKeywordsPredicate.java
+++ src/main/java/hobbylist/model/activity/NameOrDescContainsKeywordsPredicate.java
@@ -3,6 +3,7 @@ package hobbylist.model.activity;
 import java.util.List;
 import java.util.function.Predicate;
 
+import com.sun.jdi.IntegerValue;
 import hobbylist.commons.util.StringUtil;
 
 /**
@@ -17,6 +18,15 @@ public class NameOrDescContainsKeywordsPredicate implements Predicate<Activity> 
 
     @Override
     public boolean test(Activity activity) {
+        for(int i =0;i<keywords.size();i++){
+            if (keywords.get(i).split("rate/").length==2){
+                String[] t = keywords.get(i).split("rate/");
+                int index = Integer.valueOf(t[1]);
+                if(activity.getRating()==index){
+                    return true;
+                }
+            }
+        }
         return keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
                 activity.getName().fullName, keyword)
             || StringUtil.containsWordIgnoreCase(activity.getDescription().value, keyword));
