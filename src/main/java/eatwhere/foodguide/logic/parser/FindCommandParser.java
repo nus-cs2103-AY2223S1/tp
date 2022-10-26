e475fc9843a4d5ab7ef54415de4d6f578e414f11
Francis-Tan <francistanty@gmail.com>
2022-10-26 10:50:34 +0800

    Implement random find, where user inputs -r with a positive integer to randomly select the given number of eateries

--- src/main/java/eatwhere/foodguide/logic/parser/FindCommandParser.java
+++ src/main/java/eatwhere/foodguide/logic/parser/FindCommandParser.java
@@ -1,14 +1,14 @@
 package eatwhere.foodguide.logic.parser;
 
-import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_HELP;
-import static eatwhere.foodguide.logic.parser.ParserUtil.arePrefixesPresent;
+import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_RANDOM;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 import eatwhere.foodguide.commons.core.Messages;
 import eatwhere.foodguide.logic.commands.FindCommand;
-import eatwhere.foodguide.logic.parser.exceptions.DisplayCommandHelpException;
 import eatwhere.foodguide.logic.parser.exceptions.ParseException;
+import eatwhere.foodguide.model.eatery.Eatery;
 import eatwhere.foodguide.model.eatery.NameContainsKeywordsPredicate;
 
 /**
@@ -20,24 +20,25 @@ public class FindCommandParser implements Parser<FindCommand> {
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
-     * @throws DisplayCommandHelpException if the user input is for displaying command help
      */
-    public FindCommand parse(String args) throws ParseException, DisplayCommandHelpException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_HELP);
-
-        if (arePrefixesPresent(argMultimap, PREFIX_HELP)) {
-            throw new DisplayCommandHelpException(FindCommand.MESSAGE_USAGE);
-        }
-
+    public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_RANDOM);
+        Predicate<Eatery> predicate;
+        if (argMultimap.getValue(PREFIX_RANDOM).isPresent()) {
+            predicate = eatery -> true;
+            return new FindCommand(predicate, Integer.parseInt(argMultimap.getValue(PREFIX_RANDOM).get()));
+        } else {
+            String[] nameKeywords = trimmedArgs.split("\\s+");
+            predicate = new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords));
+        }
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(predicate);
     }
 
 }
