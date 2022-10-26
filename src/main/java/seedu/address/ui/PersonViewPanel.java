cba6ab8834e1fe4aaa637633a8f2eef26be0ad6f
Charles1026 <xiaozhehao@gmail.com>
2022-10-27 02:59:55 +0800

    Fix Checkstyle Issues

--- src/main/java/seedu/address/ui/PersonViewPanel.java
+++ src/main/java/seedu/address/ui/PersonViewPanel.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import com.sun.javafx.collections.ObservableListWrapper;
 
 import javafx.collections.ObservableList;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
