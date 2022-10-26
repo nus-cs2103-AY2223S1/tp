da2d0eff74a55891702dddbda4f06820d09cfde1
Qiaoran-M <72024294+Qiaoran-M@users.noreply.github.com>
2022-10-26 22:57:31 +0800

    Update src/main/java/seedu/address/logic/parser/MailEventCommandParser.java
    Co-authored-by: lfrostbytee <87323811+lfrostbytee@users.noreply.github.com>

--- src/main/java/seedu/address/logic/parser/MailEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MailEventCommandParser.java
@@ -14,7 +14,7 @@ public class MailEventCommandParser implements Parser<MailEventCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the MailEventCommand
      * and returns a MailEventCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform to the expected format.
      */
     public MailEventCommand parse(String args) throws ParseException {
         requireNonNull(args);
