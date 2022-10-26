0e3cb444aea8e76cbbf738355cc2b9d8e78c8bdb
lulucopter <lzydaniel.lee@gmail.com>
2022-10-26 22:44:48 +0800

    Add tests for email predicate

--- src/main/java/seedu/address/model/person/predicates/EmailContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/predicates/EmailContainsKeywordsPredicate.java
@@ -19,7 +19,7 @@ public class EmailContainsKeywordsPredicate implements Predicate<Person> {
     @Override
     public boolean test(Person person) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword));
+                .anyMatch(keyword -> StringUtil.containsSubstringIgnoreCase(person.getEmail().value, keyword));
     }
 
     @Override
