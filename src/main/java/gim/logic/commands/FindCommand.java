7748ab5bb1a69790eabf4bfe6d00752257ee62f4
kavantan <kavantan10@gmail.com>
2022-10-26 22:27:19 +0800

    Rename Command Words

--- src/main/java/gim/logic/commands/FindCommand.java
+++ src/main/java/gim/logic/commands/FindCommand.java
@@ -12,7 +12,7 @@ import gim.model.exercise.NameContainsKeywordsPredicate;
  */
 public class FindCommand extends Command {
 
-    public static final String COMMAND_WORD = "find";
+    public static final String COMMAND_WORD = ":find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all exercises whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
