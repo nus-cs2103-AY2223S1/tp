0679358773981309d64e2c65d72ceda3bfcb7c01
Sheryl <sherylkong07@gmail.com>
2022-10-26 17:29:36 +0800

    Add testcases

---
+++ src/test/java/seedu/address/model/person/CountryTest.java
@@ -0,0 +1,34 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class CountryTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Country(null));
+    }
+
+
+    @Test
+    public void isValidCountry() {
+        // null country
+        assertThrows(NullPointerException.class, () -> Country.isValidCountry(null));
+
+        // valid country
+        assertTrue(Country.isValidCountry("singapore")); // alphabets only
+        assertTrue(Country.isValidCountry("United States of America")); // alphabets with capital letters only
+        assertTrue(Country.isValidCountry(
+                "Al Jumahiriyah al Arabiyah al Libiyah ash Shabiyah al Ishtirakiyah al Uzma")); // long country
+
+        // invalid country
+        assertFalse(Country.isValidCountry("54th street in London")); // alphanumeric letters;
+        assertFalse(Country.isValidCountry("best.country.ever")); // with symbols
+    }
+}
+
+
