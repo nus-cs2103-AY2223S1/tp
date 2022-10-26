9f76bdbb32cdfcfb756aa46a2755a8195cbfe710
Brendan Tan <brendantan@outlook.com>
2022-10-26 18:16:32 +0800

    Modify tag display behavior

--- src/main/java/seedu/address/ui/InternshipCard.java
+++ src/main/java/seedu/address/ui/InternshipCard.java
@@ -24,6 +24,8 @@ public class InternshipCard extends UiPart<Region> {
 
     private static final String FXML = "InternshipListCard.fxml";
 
+    private static final int MAX_TAGS = 5;
+
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
@@ -76,17 +78,19 @@ public class InternshipCard extends UiPart<Region> {
 
         company.setText(internship.getCompany().value);
         linkButton.setText(internship.getLink().value);
+
         appliedDateLabel.setText("Applied:");
+        appliedDateLabel.setMinWidth(Region.USE_PREF_SIZE);
         appliedDate.setText(internship.getAppliedDate().value);
+        appliedDate.setMinWidth(Region.USE_PREF_SIZE);
+
         applicationStatus.setText(applicationStatusString);
         description.setText(internship.getDescription().value);
 
-        internship.getTags().stream()
-                .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
-
+        handleTags();
         handleInterviewDateTimeLine();
-        handleTooltips();
+        handleLinkTooltip();
+        handleApplicationStatusTooltip();
 
         applicationStatus.getStyleClass().add(applicationStatusString.toLowerCase());
         applicationStatus.setMinWidth(Region.USE_PREF_SIZE);
@@ -101,6 +105,63 @@ public class InternshipCard extends UiPart<Region> {
         final ClipboardContent url = new ClipboardContent();
         url.putString(linkButton.getText());
         clipboard.setContent(url);
+    }
+
+    /**
+     * Formats tag into a {@code Label} and adds a {@code Tooltip} with full tag name.
+     * @param tagName The tag name.
+     */
+    private void formatAndAddTag(String tagName) {
+        Tooltip tagTooltip = new Tooltip(tagName);
+        tagTooltip.setShowDelay(Duration.millis(250));
+
+        Label l = new Label(tagName);
+        l.setTooltip(tagTooltip);
+
+        tags.getChildren().add(l);
+    }
+
+    /**
+     * Removes extra tags if more than max tags allowed, and show a count of extra tags with a Tooltip on hover.
+     */
+    private void consolidateExtraTags() {
+        int count = 0;
+        StringBuilder tooltipText = new StringBuilder();
+
+        while (tags.getChildren().size() > MAX_TAGS) {
+            int lastChildIndex = tags.getChildren().size() - 1;
+
+            // ok to cast since all children of tags are Labels
+            Label lastTag = (Label) tags.getChildren().get(lastChildIndex);
+
+            tags.getChildren().remove(lastChildIndex);
+            count++;
+
+            tooltipText.append("[").append(lastTag.getText()).append("]");
+        }
+
+        Tooltip moreTagsTooltip = new Tooltip(tooltipText.toString());
+        moreTagsTooltip.setShowDelay(Duration.millis(250));
+
+        Label moreTagsLabel = new Label("+" + count);
+        moreTagsLabel.setTooltip(moreTagsTooltip);
+        moreTagsLabel.getStyleClass().add("more-tags-label");
+        moreTagsLabel.setMinWidth(Region.USE_PREF_SIZE);
+
+        tags.getChildren().add(moreTagsLabel);
+    }
+
+    /**
+     * Adds tags to be displayed.
+     */
+    private void handleTags() {
+        internship.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> formatAndAddTag(tag.tagName));
+
+        if (tags.getChildren().size() > MAX_TAGS) {
+            consolidateExtraTags();
+        }
     }
 
     /**
@@ -126,13 +187,18 @@ public class InternshipCard extends UiPart<Region> {
     }
 
     /**
-     * Adds tooltips to links and {@code ApplicationStatus} Shortlisted.
+     * Adds tooltip to internship link.
      */
-    private void handleTooltips() {
+    private void handleLinkTooltip() {
         Tooltip linkTooltip = new Tooltip("Copy link");
         linkTooltip.setShowDelay(Duration.millis(250));
         linkButton.setTooltip(linkTooltip);
+    }
 
+    /**
+     * Adds tooltip to {@code ApplicationStatus} Shortlisted.
+     */
+    private void handleApplicationStatusTooltip() {
         if (internship.getApplicationStatus() == ApplicationStatus.Shortlisted) {
             Tooltip shortlistedTooltip = new Tooltip("Shortlisted for interview");
             shortlistedTooltip.setShowDelay(Duration.millis(250));
