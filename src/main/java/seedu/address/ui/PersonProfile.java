8c23ac5ea319a2740f18e8a9e4f83ed00ab672b1
Shaune Ang <shauneangjx@gmail.com>
2022-10-26 12:16:40 +0800

    Fix CI Checks

--- src/main/java/seedu/address/ui/PersonProfile.java
+++ src/main/java/seedu/address/ui/PersonProfile.java
@@ -13,10 +13,10 @@ import seedu.address.model.person.Person;
  * An UI component that displays information of a {@code Person}.
  */
 public class PersonProfile extends UiPart<Region> {
-    private static final String FXML = "PersonProfile.fxml";
     public static final String EMPTY_DISPLAY_VALUE = "-";
-
     public final Person person;
+
+    private static final String FXML = "PersonProfile.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
