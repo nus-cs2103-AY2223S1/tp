fd53716f371474d0f83958e4387eca353363f769
ruihan00 <83448744+ruihan00@users.noreply.github.com>
2022-10-26 14:49:06 +0800

    update summary testcases

--- src/test/java/seedu/address/logic/commands/SummaryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SummaryCommandTest.java
@@ -14,27 +14,56 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.entry.Date;
+import seedu.address.model.entry.EntryInYearMonthPredicate;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 //import seedu.address.model.entry.EntryType;
 //import seedu.address.model.entry.Expenditure;
 
-//public class SummaryCommandTest {
-//    private Model model = new ModelManager(getTypicalPennyWise(), new UserPrefs());
-//    @Test
-//    public void execute_noSpecifiedDate_summarySuccessful() {
-//        SummaryCommand summaryCommand = new SummaryCommand();
-//        Double totalExpenditure = model.getFilteredExpenditureList()
-//                .stream()
-//                .mapToDouble(entry -> Double.parseDouble(entry.getAmount().toString()))
-//                .sum();
-//        Double totalIncome = model.getFilteredIncomeList()
-//                .stream()
-//                .mapToDouble(entry -> Double.parseDouble(entry.getAmount().toString()))
-//                .sum();
-//        String expectedMessage = String.format(
-//                SummaryCommand.MESSAGE_SUCCESS,
-//                totalExpenditure,
-//                totalIncome,
-//                totalIncome - totalExpenditure);
-//        assertCommandSuccess(summaryCommand, model, expectedMessage, model);
-//    }
-//}
+public class SummaryCommandTest {
+    private Model model = new ModelManager(getTypicalPennyWise(), new UserPrefs());
+
+    @Test
+    public void execute_noSpecifiedDate_summarySuccessful() {
+        SummaryCommand summaryCommand = new SummaryCommand();
+        Double totalExpenditure = model.getFilteredExpenditureList()
+                .stream()
+                .mapToDouble(entry -> Double.parseDouble(entry.getAmount().toString()))
+                .sum();
+        Double totalIncome = model.getFilteredIncomeList()
+                .stream()
+                .mapToDouble(entry -> Double.parseDouble(entry.getAmount().toString()))
+                .sum();
+        String expectedMessage = String.format(
+                SummaryCommand.MESSAGE_SUCCESS,
+                totalExpenditure,
+                totalIncome,
+                totalIncome - totalExpenditure);
+        assertCommandSuccess(summaryCommand, model, expectedMessage, model);
+    }
+
+    @Test
+    public void execute_specifiedDate_summarySuccessful() {
+        EntryInYearMonthPredicate predicate = new EntryInYearMonthPredicate(YearMonth.parse(
+                CommandTestUtil.VALID_MONTH_APRIL,
+                DateTimeFormatter.ofPattern(Date.YEAR_MONTH_PATTERN)));
+        SummaryCommand summaryCommand = new SummaryCommand(predicate);
+        model.updateFilteredEntryList(predicate);
+        Double totalExpenditure = model.getFilteredExpenditureList()
+                .stream()
+                .mapToDouble(entry -> Double.parseDouble(entry.getAmount().toString()))
+                .sum();
+        Double totalIncome = model.getFilteredIncomeList()
+                .stream()
+                .mapToDouble(entry -> Double.parseDouble(entry.getAmount().toString()))
+                .sum();
+        String expectedMessage = String.format(
+                SummaryCommand.MESSAGE_SUCCESS,
+                totalExpenditure,
+                totalIncome,
+                totalIncome - totalExpenditure);
+        assertCommandSuccess(summaryCommand, model, expectedMessage, model);
+    }
+}
--- src/test/java/seedu/address/logic/parser/SummaryCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SummaryCommandParserTest.java
@@ -1,29 +1,36 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MONTH;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.SummaryCommand;
 import seedu.address.model.entry.Date;
+import seedu.address.model.entry.EntryInYearMonthPredicate;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 public class SummaryCommandParserTest {
     private SummaryCommandParser parser = new SummaryCommandParser();
 
-//    @Test
-//    public void parse_validArgs_returnsSummaryCommand() {
-//        // no leading and trailing whitespaces
-//        SummaryCommand expectedSummaryCommand =
-//                new SummaryCommand();
-//        assertParseSuccess(parser, " ", expectedSummaryCommand);
-//    }
+    @Test
+    public void parse_validArgs_returnsSummaryCommand() {
+        // no leading and trailing whitespaces
+        SummaryCommand expectedSummaryCommand =
+                new SummaryCommand();
+        assertParseSuccess(parser, " ", expectedSummaryCommand);
+    }
 
-//    @Test
-//    public void parse_validArgs_returnsSummaryCommandWithDate() {
-//        // no leading and trailing whitespaces
-//        SummaryCommand expectedSummaryCommand =
-//                new SummaryCommand(new Date("10-10-2022"));
-//        assertParseSuccess(parser, " " + PREFIX_DATE + "10-10-2022 ", expectedSummaryCommand);
-//    }
+    @Test
+    public void parse_validArgs_returnsSummaryCommandWithDate() {
+        // no leading and trailing whitespaces
+        SummaryCommand expectedSummaryCommand =
+                new SummaryCommand(new EntryInYearMonthPredicate(
+                        YearMonth.parse("2022-09",
+                                DateTimeFormatter.ofPattern(Date.YEAR_MONTH_PATTERN))));
+        assertParseSuccess(parser, " " + PREFIX_MONTH+ "2022-09 ", expectedSummaryCommand);
+    }
 }
