68157be8219c7d9a8136a1236614046c20f06ccf
KIANSEONG <kianseong.quah@gmail.com>
2022-10-26 13:39:27 +0800

    Remove autocomplete feature due to numerous bugs

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,24 +1,15 @@
 package seedu.address.ui;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Side;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.CustomMenuItem;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.TaskCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -34,14 +25,7 @@ public class CommandBox extends UiPart<Region> {
 
     // command log navigation
     private final List<String> commandLog;
-    private int commandLogPointer;
-
-    // suggestions (excludes task related commands)
-    private final SortedSet<String> suggestionsMain;
-    // task related suggestions
-    private final SortedSet<String> suggestionsTask;
-    // pop up used to select a suggestion
-    private final ContextMenu popupMenu;
+    private int pointer;
 
     @FXML
     private TextField commandTextField;
@@ -53,17 +37,14 @@ public class CommandBox extends UiPart<Region> {
         super(FXML);
         this.commandExecutor = commandExecutor;
         commandLog = new ArrayList<>();
-        popupMenu = new ContextMenu();
-        suggestionsMain = new TreeSet<>(
-            Arrays.asList("add", "clear", "delete", "edit", "exit", "find", "help", "list", "task"));
-        suggestionsTask = new TreeSet<>(
-            Arrays.asList("add", "assign", "deadline", "delete", "edit", "list", "mark", "unmark"));
+        load();
     }
 
     /**
      * Load objects and data.
      */
     private void load() {
+        pointer = -1;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
         commandTextField.addEventFilter(KeyEvent.KEY_PRESSED, this::handleNavigationKeysPressed);
@@ -78,28 +59,22 @@ public class CommandBox extends UiPart<Region> {
     }
 
     /**
-     * Add events to commandTextField.
-     */
-    private void addEvents() {
-
-    }
-
-    /**
      * Handles the Enter button pressed event.
      */
     @FXML
     private void handleCommandEntered() {
+        String currentText = commandTextField.getText();
         // Guard: If command text field is empty, no command is entered
-        if (isCommandTextFieldEmpty()) {
+        if (currentText.isEmpty()) {
             return;
         }
 
         try {
-            commandLog.add(getCommandText());
-            commandLogPointer = commandLog.size();
+            commandLog.add(currentText);
+            pointer = commandLog.size();
 
             commandTextField.setText("");
-            commandExecutor.execute(getCommandText());
+            commandExecutor.execute(currentText);
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
         }
@@ -111,52 +86,41 @@ public class CommandBox extends UiPart<Region> {
      * If current text matches the latest command and DOWN key is pressed, nothing happens.
      */
     private void handleNavigationKeysPressed(KeyEvent event) {
+        // Guard: Checks if either command log navigation key are pressed
+        if (event.getCode() != KeyCode.UP && event.getCode() != KeyCode.DOWN) {
+            return;
+        }
+        event.consume();
 
-        // Reset the popup
-        if (popupMenu.isShowing()) {
-            popupMenu.hide();
+        // Guard: Checks if command text field satisfies condition.
+        if (!isNavigable()) {
             return;
         }
 
-        // Checks if either command log navigation key are pressed
-        if (event.getCode() == KeyCode.UP || event.getCode() == KeyCode.DOWN) {
-            event.consume();
-
-            // Guard: Prevents the user from navigating if there is text in the command text field.
-            if (!getCommandText().isEmpty()) {
-                return;
-            }
-
-            // Guard: If there are no commands, nothing happens
-            if (commandLog.size() == 0) {
-                return;
-            }
-
-            if (event.getCode() == KeyCode.UP) {
-
-                // Guard: If the text field shows the earliest command, UP arrow does nothing
-                if (commandLogPointer == 0) {
-                    return;
-                }
-
-                commandTextField.setText(commandLog.get(--commandLogPointer));
-            } else if (event.getCode() == KeyCode.DOWN) {
-                commandLogPointer += 1;
-
-                // If text field shows the latest command, nothing changes
-                if (commandLogPointer >= commandLog.size()) {
-                    commandLogPointer = commandLog.size();
-                    return;
-                }
-
-                commandTextField.setText(commandLog.get(commandLogPointer));
-            } else {
-                // do not do anything if any other key is pressed
-                return;
-            }
-
-            commandTextField.positionCaret(commandTextField.getText().length());
+        // Guard: If there are no commands, nothing happens.
+        if (commandLog.size() == 0) {
+            return;
         }
+
+        if (event.getCode() == KeyCode.UP) {
+            // If the text field shows the earliest command, UP arrow does nothing
+            if (pointer == 0) {
+                return;
+            } else {
+                commandTextField.setText(commandLog.get(--pointer));
+            }
+        } else if (event.getCode() == KeyCode.DOWN) {
+            if (pointer == commandLog.size()) {
+                return;
+            } else if (pointer == commandLog.size() - 1) {
+                pointer++;
+                commandTextField.setText("");
+            } else {
+                commandTextField.setText(commandLog.get(++pointer));
+            }
+        }
+
+        commandTextField.positionCaret(commandTextField.getText().length());
     }
 
     // Detects text changes in commandTextField to update suggestions popup
@@ -164,7 +128,6 @@ public class CommandBox extends UiPart<Region> {
     private void handleTextChanged() {
         // Guard: Stop if no text in command text field
         if (isCommandTextFieldEmpty()) {
-            popupMenu.hide();
             return;
         }
 
@@ -172,64 +135,6 @@ public class CommandBox extends UiPart<Region> {
         if (getCommandText().length() == 1 && getCommandText().equals("/")) {
             commandTextField.clear();
         }
-
-        // Guard: If suggestions do not match text in command text field, stop
-        if (suggestionsMain.size() == 0 || suggestionsTask.size() == 0) {
-            return;
-        }
-
-        List<String> searchResult;
-
-        // Populate suggestions popup with task related commands
-        if (getCommandText().startsWith(TaskCommand.COMMAND_WORD + " ")
-            || getCommandText().startsWith(TaskCommand.COMMAND_WORD_ALIAS + " ")) {
-            String resultFrom = getCommandText().substring(getCommandText().indexOf(" ")).stripLeading();
-            searchResult = new LinkedList<>(suggestionsTask
-                .subSet(resultFrom, resultFrom + Character.MAX_VALUE));
-            // Guard: If the text in command text field matches the only suggestion, hide the popup
-            if (searchResult.size() == 1 && resultFrom.equalsIgnoreCase(searchResult.get(0))) {
-                return;
-            }
-        } else {
-            searchResult = new LinkedList<>(suggestionsMain
-                .subSet(getCommandText(), getCommandText() + Character.MAX_VALUE));
-            if (searchResult.size() == 1 && getCommandText().equalsIgnoreCase(searchResult.get(0))) {
-                return;
-            }
-        }
-
-        populatePopup(searchResult);
-        popupMenu.show(
-            getRoot(), Side.BOTTOM, 14 + getCommandText().length() * 7, -8);
-    }
-
-    /**
-     * Populate the suggestions popups with the given search results.
-     * @param searchResult set of matching strings
-     */
-    private void populatePopup(List<String> searchResult) {
-        List<CustomMenuItem> menuItems = new LinkedList<>();
-        for (final String result : searchResult) {
-            Label entryLabel = new Label(result);
-            CustomMenuItem item = new CustomMenuItem(entryLabel, true);
-            item.setOnAction(actionEvent -> {
-                // Checks if user is typing a task command
-                if (getCommandText().startsWith(TaskCommand.COMMAND_WORD + " ")
-                    || getCommandText().startsWith(TaskCommand.COMMAND_WORD_ALIAS + " ")) {
-                    // Keep the front part (i.e. 'task or 't') the same
-                    commandTextField.setText(getCommandText().split(" ")[0] + " " + result);
-                } else {
-                    // Autocomplete the user's current command
-                    commandTextField.setText(result);
-                }
-                commandTextField.positionCaret(getCommandText().length());
-                // Hide the popup once the autocomplete happens
-                popupMenu.hide();
-            });
-            menuItems.add(item);
-        }
-        popupMenu.getItems().clear();
-        popupMenu.getItems().addAll(menuItems);
     }
 
     /**
@@ -251,6 +156,14 @@ public class CommandBox extends UiPart<Region> {
      */
     private boolean isCommandTextFieldEmpty() {
         return commandTextField.getText().isEmpty();
+    }
+
+    /**
+     * Checks if command text field can be navigated using the UP and DOWN arrow keys.
+     */
+    private boolean isNavigable() {
+        return isCommandTextFieldEmpty()
+            || (!isCommandTextFieldEmpty() && getCommandText().equals(commandLog.get(pointer)));
     }
 
     /**
