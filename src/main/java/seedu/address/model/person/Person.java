db5f1a91080c1b0d6414d94ed230e007032c2bcb
lfrostbytee <tr.lfrostbyte@gmail.com>
2022-10-27 01:43:28 +0800

    Update removed tags based on reviews

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -99,8 +99,7 @@ public class Person {
 
     @Override
     public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, gender); //add gender field
+        return Objects.hash(name, phone, email, address, gender);
     }
 
     @Override
