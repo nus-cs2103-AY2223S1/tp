424b02b7eb53d9fb2c12bb4c4f36b46312d385d1
jasonqiu212 <jason_qiu@hotmail.com>
2022-10-26 12:03:15 +0800

    Fix: tokenize description and deadline fields for task

--- src/main/java/swift/logic/parser/EditTaskCommandParser.java
+++ src/main/java/swift/logic/parser/EditTaskCommandParser.java
@@ -25,7 +25,7 @@ public class EditTaskCommandParser implements Parser<EditTaskCommand> {
     public EditTaskCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_CONTACT);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_CONTACT, PREFIX_DESCRIPTION, PREFIX_DEADLINE);
 
         Index index;
 
