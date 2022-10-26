d15b9c42729cff3970eb6ae9c5061c917309a556
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-10-26 16:35:18 +0800

    Change regex of Birthday to include more cases

--- src/main/java/seedu/address/model/person/Birthday.java
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -13,10 +13,9 @@ public class Birthday implements DeepCopyable {
 
     public static final String MESSAGE_CONSTRAINTS = "Birthdays should only contain numbers and forward slashes";
 
-    private static final String DAY_REGEX = "^([0-2][0-9]||3[0-1])";
-    private static final String MONTH_REGEX = "(0[0-9]||1[0-2])";
-    private static final String YEAR_REGEX = "([0-9][0-9])?[0-9][0-9]$";
-    public static final String DATE_REGEX = DAY_REGEX + "/" + MONTH_REGEX + "/" + YEAR_REGEX;
+    public static final String DATE_REGEX = "(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/]"
+        + "(0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/]"
+        + "(19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)";
 
 
     public final String value;
