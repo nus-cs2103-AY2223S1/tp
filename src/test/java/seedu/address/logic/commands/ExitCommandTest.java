308fa878010b59c2f57c3affa793284b4eee0c9f
Coco <chuaziyifaith@gmail.com>
2022-10-26 23:10:32 +0800

    Update ExitCommandTest

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -1,16 +1,20 @@
 package seedu.address.logic.commands;
 
+import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
 
 public class ExitCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
-    /*@Test
+    @Test
     public void execute_exit_success() {
-        CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false,
-                true);
+        CommandResult expectedCommandResult = CommandResult.createExitCommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT);
         assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
-    }*/
+    }
 }
