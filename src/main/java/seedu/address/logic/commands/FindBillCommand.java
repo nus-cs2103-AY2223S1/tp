b5e42117fa62303ccd6a198b870ba59de6c4a4ee
Wadia <g.wadia08@gmail.com>
2022-10-26 20:08:36 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/FindBillCommand.java
+++ src/main/java/seedu/address/logic/commands/FindBillCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Predicate;
 
 import seedu.address.commons.core.Messages;
