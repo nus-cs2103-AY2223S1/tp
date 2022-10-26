b636882808ddf487a00c1d43c2eb98c5cc23ea92
jia-han <neo.jiahan1234@gmail.com>
2022-10-27 00:46:23 +0800

    Update utils for changes to `Email`

--- src/test/java/tuthub/logic/parser/ParserUtilTest.java
+++ src/test/java/tuthub/logic/parser/ParserUtilTest.java
@@ -22,12 +22,12 @@ import tuthub.model.tutor.Phone;
 public class ParserUtilTest {
     private static final String INVALID_NAME = "R@chel";
     private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_EMAIL = "a1234567";
     private static final String INVALID_TAG = "#friend";
 
     private static final String VALID_NAME = "Rachel Walker";
     private static final String VALID_PHONE = "123456";
-    private static final String VALID_EMAIL = "rachel@example.com";
+    private static final String VALID_EMAIL = "e1234567";
     private static final String VALID_TAG_1 = "friend";
     private static final String VALID_TAG_2 = "neighbour";
 
