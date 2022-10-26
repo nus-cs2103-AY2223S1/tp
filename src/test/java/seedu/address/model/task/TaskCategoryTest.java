b12980cf5f09dc4d9d2ea6de9ea8fdeda45b859b
albertarielw <albertariel2002@gmail.com>
2022-10-26 20:51:44 +0800

    fix test

--- src/test/java/seedu/address/model/task/TaskCategoryTest.java
+++ src/test/java/seedu/address/model/task/TaskCategoryTest.java
@@ -4,8 +4,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.task.exceptions.LevelOutOfRangeException;
-
 public class TaskCategoryTest {
 
     @Test
