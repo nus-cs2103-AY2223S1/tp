b141f4bb09d7fcf893e424bce659c211b99fbca6
junweimoo <junweimoo@gmail.com>
2022-10-26 20:16:41 +0800

    Add client name column to the meetinglist view

--- src/main/java/seedu/address/ui/MeetingCard.java
+++ src/main/java/seedu/address/ui/MeetingCard.java
@@ -21,6 +21,8 @@ public class MeetingCard extends UiPart<Region> {
     private Label date;
     @FXML
     private Label time;
+    @FXML
+    private Label name;
 
     /**
      * Creates a {@code MeetingCard} with the given {@code Meeting} and index to display.
@@ -32,6 +34,7 @@ public class MeetingCard extends UiPart<Region> {
         description.setText(meeting.getDescription().toString());
         date.setText(meeting.getMeetingDate().toString());
         time.setText(meeting.getMeetingStartTime().toString() + " - " + meeting.getMeetingEndTime().toString());
+        name.setText(meeting.getClientName().toString());
     }
 
     @Override
