2be5e632222763dd0c5c5f7b6ecd2da58fd0e0b5
ongwenpin <wenpin2108@gmail.com>
2022-10-26 17:31:31 +0800

    Remove commented code

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -89,16 +89,6 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_edit() throws Exception {
-        /*
-        Person person = new PersonBuilder().build();
-        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
-        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor),
-                listType);
-        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
-        */
-
-
         Student student = new StudentBuilder().build();
         EditStudentDescriptor studentDescriptor = new EditStudentDescriptorBuilder(student).build();
         EditCommand studentCommand = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
