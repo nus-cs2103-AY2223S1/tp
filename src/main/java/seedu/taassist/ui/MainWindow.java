1f3354331a7a36a6322edad6d3e56223d42dd1a0
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-10-26 22:06:56 +0800

    Wrap result display

--- src/main/java/seedu/taassist/ui/MainWindow.java
+++ src/main/java/seedu/taassist/ui/MainWindow.java
@@ -76,8 +76,6 @@ public class MainWindow extends UiPart<Stage> {
         setWindowDefaultSize(logic.getGuiSettings());
 
         helpWindow = new HelpWindow();
-
-        helpButton.setId("helpBtn");
     }
 
     public Stage getPrimaryStage() {
@@ -128,16 +126,14 @@ public class MainWindow extends UiPart<Stage> {
      * Opens a particular webpage, if unable to do so, help window will be shown for them to copy the URL.
      */
     public static void openWebpage(String urlString) {
-        try {
-            Desktop.getDesktop().browse(new URL(urlString).toURI());
-        } catch (Exception e) {
-            if (!helpWindow.isShowing()) {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(new URL(urlString).toURI());
+            } catch (Exception e) {
                 helpWindow.show();
-            } else {
-                helpWindow.focus();
             }
-            e.printStackTrace();
-
+        } else {
+            helpWindow.show();
         }
     }
 
