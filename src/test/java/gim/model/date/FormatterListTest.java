9e6d9f8c3124637a7e2d2da52183cef454ba80b8
Steven Lim <limhwsteven@gmail.com>
2022-10-26 15:55:31 +0800

    Add tests for RegexList and FormatterList

---
+++ src/test/java/gim/model/date/FormatterListTest.java
@@ -0,0 +1,60 @@
+package gim.model.date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FormatterListTest {
+    FormatterList formatterList = FormatterList.getFormatterList();
+
+    @Test
+    public void constructor_returnsSingletonInstance() {
+        assertEquals(formatterList, FormatterList.getFormatterList());
+    }
+
+    @Test
+    public void validDateWithFormatters_validInput_success() {
+        String[] dateStrings = {
+                "1/1/2020", "01/1/2020", "1/01/2020", "01/01/2020",
+                "2020/1/1", "2020/1/01", "2020/01/1", "2020/01/01",
+
+                "1/1/20", "01/1/20", "1/01/20", "01/01/20",
+                "20/1/1", "20/1/01", "20/01/1", "20/01/01",
+
+                "01-01-2020", "01-1-2020", "1-01-2020", "1-1-2020",
+                "2020-01-01", "2020-1-01", "2020-01-1", "2020-1-1",
+
+                "01-01-20", "01-1-20", "1-01-20", "1-1-20",
+                "20-01-01", "20-1-01", "20-01-1", "20-1-1",
+
+                "01 01 2020", "01 1 2020", "1 01 2020", "1 1 2020",
+                "2020 01 01", "2020 1 01", "2020 01 1", "2020 1 1",
+
+                "01 01 20", "01 1 20", "1 01 20", "1 1 20",
+                "20 01 01", "20 1 01", "20 01 1", "20 1 1",
+        };
+        for (String dateString: dateStrings) {
+            assertNotNull(formatterList.validateDateWithFormatters(dateString));
+        }
+    }
+
+    @Test
+    public void validDateWithFormatters_invalidInput_failure() {
+        String[] dateStrings = {
+                "1 /1/2020", "01/ 1/2020", "1/01 /2020", "01/01/ 2020",
+                "2020 /1/1", "2020/ 1/01", "2020/01 /1", "2020/01/ 01",
+
+                "1 /1/20", "01/ 1/20", "1/01 /20", "01/01/ 20",
+                "20 /1/1", "20/ 1/01", "20/01 /1", "20/01/ 01",
+
+                "01 -01-2020", "01- 1-2020", "1-01 -2020", "1-1- 2020",
+                "2020 -01-01", "2020- 1-01", "2020-01 -1", "2020-1- 1",
+
+                "01 -01-20", "01- 1-20", "1-01 -20", "1-1- 20",
+                "20 -01-01", "20- 1-01", "20-01 -1", "20-1- 1",
+        };
+        for (String dateString: dateStrings) {
+            assertNull(formatterList.validateDateWithFormatters(dateString));
+        }
+    }
+}
---
+++ src/test/java/gim/model/date/RegexListTest.java
@@ -0,0 +1,54 @@
+package gim.model.date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegexListTest {
+    RegexList regexList = RegexList.getRegexList();
+
+    @Test
+    public void constructor_returnsSingletonInstance() {
+        assertEquals(regexList, RegexList.getRegexList());
+    }
+
+    @Test
+    public void isValidDateByRegex_validInput_success() {
+        String[] dateStrings = {
+                "1/1/2020", "01/1/2020", "1/01/2020", "01/01/2020",
+                "2020/1/1", "2020/1/01", "2020/01/1", "2020/01/01",
+
+                "1/1/20", "01/1/20", "1/01/20", "01/01/20",
+                "20/1/1", "20/1/01", "20/01/1", "20/01/01",
+
+                "01-01-2020", "01-1-2020", "1-01-2020", "1-1-2020",
+                "2020-01-01", "2020-1-01", "2020-01-1", "2020-1-1",
+
+                "01-01-20", "01-1-20", "1-01-20", "1-1-20",
+                "20-01-01", "20-1-01", "20-01-1", "20-1-1",
+
+                "01 01 2020", "01 1 2020", "1 01 2020", "1 1 2020",
+                "2020 01 01", "2020 1 01", "2020 01 1", "2020 1 1",
+
+                "01 01 20", "01 1 20", "1 01 20", "1 1 20",
+                "20 01 01", "20 1 01", "20 01 1", "20 1 1",
+        };
+        for (String dateString: dateStrings) {
+            assertTrue(regexList.isValidDateByRegex(dateString));
+        }
+    }
+
+    @Test
+    public void isValidDateByRegex_invalidInput_failure() {
+        String[] dateStrings = {
+                "1 /1/2020", "01/ 1/2020", "1/01 /2020", "01/01/ 2020",
+                "2020 /1/1", "2020/ 1/01", "2020/01 /1", "2020/01/ 01",
+
+                "1 /1/20", "01/ 1/20", "1/01 /20", "01/01/ 20",
+                "20 /1/1", "20/ 1/01", "20/01 /1", "20/01/ 01",
+        };
+        for (String dateString: dateStrings) {
+            assertFalse(regexList.isValidDateByRegex(dateString));
+        }
+    }
+}
