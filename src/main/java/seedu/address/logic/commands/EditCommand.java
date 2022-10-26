3212b97b043250940a8b13c7671b58d8cd1944a1
shaniceng <ngshanice1@yahoo.com.sg>
2022-10-26 23:06:07 +0800

    Update Checkstyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -189,7 +189,8 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, income, meetingDate, tags, risk, plans, note);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, income, meetingDate, tags,
+                    risk, plans, note);
         }
 
         public void setName(Name name) {
