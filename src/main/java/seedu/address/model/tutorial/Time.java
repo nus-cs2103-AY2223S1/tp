fc80313f908393b9572db326ca2791fbc1af4148
elyseleong <elyse.xuan@gmail.com>
2022-10-26 16:43:14 +0800

    Update Time.java

--- src/main/java/seedu/address/model/tutorial/Time.java
+++ src/main/java/seedu/address/model/tutorial/Time.java
@@ -1,5 +1,9 @@
 package seedu.address.model.tutorial;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
@@ -16,44 +20,49 @@ public class Time {
      * otherwise " " (a blank string) becomes a valid input.
      */
     //public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+//    public static final String VALIDATION_REGEX = "[^\\s].*";
     // I still don't understand what it means right now, and I will find out and change it later.
 
-    public final String time;
+    public final LocalDateTime dateTime;
 
     /**
      * Constructs a {@code time}.
      *
-     * @param time A valid time.
+     * @param dateTime A valid time.
      */
-    public Time(String time) {
-        requireNonNull(time);
-        checkArgument(isValidTime(time), MESSAGE_CONSTRAINTS);
-        this.time = time;
+    public Time(String dateTime) {
+        requireNonNull(dateTime);
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+            this.dateTime = LocalDateTime.parse(dateTime);
+        } catch (DateTimeParseException e) {
+            checkArgument(false, MESSAGE_CONSTRAINTS);
+        }
     }
 
-    /**
-     * Returns true if a given string is a valid time.
-     */
-    public static boolean isValidTime(String test) {
-        return test.matches(VALIDATION_REGEX);
-    }
+//    /**
+//     * Returns true if a given string is a valid time.
+//     */
+//    public static boolean isValidTime(String test) {
+//        return test.matches(VALIDATION_REGEX);
+//    }
 
 
     @Override
     public String toString() {
-        return time;
+        return this.dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyy h:mma"));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Time // instanceof handles nulls
-                && time.equals(((Time) other).time)); // state check
+                && dateTime.equals(((Time) other).dateTime)); // state check
     }
 
     @Override
     public int hashCode() {
-        return time.hashCode();
+        return dateTime.hashCode();
     }
 }
