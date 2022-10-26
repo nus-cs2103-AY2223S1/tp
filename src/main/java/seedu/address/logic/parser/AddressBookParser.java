15994a888a272b87bce883af066332cbd2f884bb
Daniel Luo <hurinturin123@gmail.com>
2022-10-26 12:44:03 +0800

    Add new feature to delete meetings
    Currently the user is unable to delete meetings
    if they decide to cancel or reschedule existing meetings
    
    Adding a command to do delete a meeting from the UI and storage
    allows users to better organise and manage their meetings.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,24 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddPersonToMeetingCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CreateMeetingCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.DeletePersonFromMeetingCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.EditMeetingCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FilterMeetingCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.FindMeetingCommand;
-import seedu.address.logic.commands.FindTagCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.ListMeetingCommand;
-import seedu.address.logic.commands.SortMeetingsCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -103,6 +86,9 @@ public class AddressBookParser {
 
         case SortMeetingsCommand.COMMAND_WORD:
             return new SortMeetingsCommandParser().parse(arguments);
+
+        case DeleteMeetingCommand.COMMAND_WORD:
+            return new DeleteMeetingCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
