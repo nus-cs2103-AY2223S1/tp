a1b81f1d0b3d4de720529058e0c004ba5066911f
TERENCE CHONG <97284219+cxyterence@users.noreply.github.com>
2022-10-26 23:46:20 +0800

    Update Student.java for coding style violations

--- src/main/java/seedu/address/model/person/position/Student.java
+++ src/main/java/seedu/address/model/person/position/Student.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.Assignment;
