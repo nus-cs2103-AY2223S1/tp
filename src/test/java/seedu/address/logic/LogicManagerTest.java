b32e520d90ca1270053f9c89d3b5c9c617f82b82
Xu Daofu <97419716+therealdaofu@users.noreply.github.com>
2022-10-26 23:56:20 +0800

    Update LogicManagerTest.java
    Fix test case

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -67,7 +67,7 @@
 //    public void execute_validCommand_success() throws Exception {
 //        String listCommand = ListCommand.COMMAND_WORD;
 //        assertCommandSuccess(listCommand, ListCommand.MESSAGE_NO_CONTACTS_IN_LIST
-                + ListCommand.MESSAGE_NO_MODULES_IN_LIST, model);
+//                + ListCommand.MESSAGE_NO_MODULES_IN_LIST, model);
 //    }
 //
 //    @Test
