057861bf15ef86061c6a756d72569d695acddbba
lolfoollorS <Lolfoollor47@gmail.com>
2022-10-27 09:13:51 +0800

    Update checkstyle for HelpWindow.java and CommandNameListPanel.java

--- src/main/java/seedu/address/ui/CommandNameListPanel.java
+++ src/main/java/seedu/address/ui/CommandNameListPanel.java
@@ -2,15 +2,11 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.person.Person;
 
 /**
  * Panel containing the list of command names.
--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -36,67 +36,67 @@ public class HelpWindow extends UiPart<Stage> {
     private static final String FXML = "HelpWindow.fxml";
     private static final double DURATION_FOR_TEXT_ANIMATION = 0.1;
     private static final String PATIENT_SEARCH_TEXT = "Add Patient";
-    private static final String ADD_DEMO_INPUT_PATIENT = "add c/P n/John Doe g/M p/98765432 e/johnd@example.com " +
-            "a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney dt/2022-11-11T12:30 v/True";
-    private static final String ADD_DEMO_OUTPUT_PATIENT = "New Patient added: Category: P Uid: 10; Name: John Doe;" +
-            " Gender: M; Phone: 98765432; Email: johnd@example.com; Address: 311, Clementi Ave 2, #02-25;" +
-            " Tags: [owesMoney][friends]; Home Visits Date and Time:11/11/2022 12:30 , ; Visit Status: visited";
+    private static final String ADD_DEMO_INPUT_PATIENT = "add c/P n/John Doe g/M p/98765432 e/johnd@example.com "
+            + "a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney dt/2022-11-11T12:30 v/True";
+    private static final String ADD_DEMO_OUTPUT_PATIENT = "New Patient added: Category: P Uid: 10; Name: John Doe;"
+            + " Gender: M; Phone: 98765432; Email: johnd@example.com; Address: 311, Clementi Ave 2, #02-25;"
+            + " Tags: [owesMoney][friends]; Home Visits Date and Time:11/11/2022 12:30 , ; Visit Status: visited";
     private static final String NURSE_SEARCH_TEXT = "Add Nurse";
-    private static final String ADD_DEMO_INPUT_NURSE = "add c/N n/Cola t/Pediatric e/cola@example.com g/F p/98345432" +
-            " a/Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03 t/heartDiseaseSpecialist";
-    private static final String ADD_DEMO_OUTPUT_NURSE = "New person added: Category: N Uid: 3; Name: Cola;" +
-            " Gender: F; Phone: 98345432; Email: cola@example.com; " +
-            "Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03; Tags: [heartDiseaseSpecialist][Pediatric]";
-    private static final String ADD_PATIENT_USAGE_HELP = "Add Patient \n" +
-            "add c/Category n/Name g/Gender p/Phone e/Email a/Address v/Visted " +
-            "[t/tag] [dt/Date&Time (2022-11-11T12:30)] *[] is optional. ";
-    private static final String ADD_NURSE_USAGE_HELP = "Add Nurse\n" +
-            "add c/Category n/Name g/Gender p/Phone e/Email a/Address v/Visited " +
-            "[t/tag] *[] is optional. ";
+    private static final String ADD_DEMO_INPUT_NURSE = "add c/N n/Cola t/Pediatric e/cola@example.com g/F p/98345432"
+            + " a/Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03 t/heartDiseaseSpecialist";
+    private static final String ADD_DEMO_OUTPUT_NURSE = "New person added: Category: N Uid: 3; Name: Cola;"
+            + " Gender: F; Phone: 98345432; Email: cola@example.com; "
+            + "Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03; Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String ADD_PATIENT_USAGE_HELP = "Add Patient \n"
+            + "add c/Category n/Name g/Gender p/Phone e/Email a/Address v/Visted "
+            + "[t/tag] [dt/Date&Time (2022-11-11T12:30)] *[] is optional. ";
+    private static final String ADD_NURSE_USAGE_HELP = "Add Nurse\n"
+            + "add c/Category n/Name g/Gender p/Phone e/Email a/Address v/Visited "
+            + "[t/tag] *[] is optional. ";
     private static final String CLEAR_SEARCH_TEXT = "Clear All";
     private static final String CLEAR_DEMO_INPUT = "clear";
     private static final String CLEAR_DEMO_OUTPUT = "Address book has been cleared!";
     private static final String DELETE_SEARCH_TEXT = "Delete via ID";
     private static final String DELETE_DEMO_INPUT = "delete id/3";
-    private static final String DELETE_DEMO_OUTPUT = "Deleted person: Category: N Uid: 3; Name: Cola; Gender: F;" +
-            " Phone: 98345432; Email: cola@example.com;" +
-            " Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03; Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String DELETE_DEMO_OUTPUT = "Deleted person: Category: N Uid: 3; Name: Cola; Gender: F;"
+            + " Phone: 98345432; Email: cola@example.com;"
+            + " Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03; Tags: [heartDiseaseSpecialist][Pediatric]";
     private static final String EDIT_NAME_SEARCH_TEXT = "Edit Name";
     private static final String EDIT_NAME_DEMO_INPUT = "edit id/12 n/Kola";
-    private static final String EDIT_NAME_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: F;" +
-            " Phone: 98345432; Email: cola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;" +
-            " Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String EDIT_NAME_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: F;"
+            + " Phone: 98345432; Email: cola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;"
+            + " Tags: [heartDiseaseSpecialist][Pediatric]";
     private static final String EDIT_GENDER_SEARCH_TEXT = "Edit Gender";
     private static final String EDIT_GENDER_DEMO_INPUT = "edit id/12 g/M";
-    private static final String EDIT_GENDER_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola;" +
-            " Gender: M; Phone: 98345432; Email: cola@example.com;" +
-            " Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03; Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String EDIT_GENDER_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola;"
+            + " Gender: M; Phone: 98345432; Email: cola@example.com;"
+            + " Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03; Tags: [heartDiseaseSpecialist][Pediatric]";
     private static final String EDIT_PHONE_SEARCH_TEXT = "Edit Phone";
     private static final String EDIT_PHONE_DEMO_INPUT = "edit id/12 p/88888888";
-    private static final String EDIT_PHONE_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: M;" +
-            " Phone: 88888888; Email: cola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;" +
-            " Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String EDIT_PHONE_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: M;"
+            + " Phone: 88888888; Email: cola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;"
+            + " Tags: [heartDiseaseSpecialist][Pediatric]";
     private static final String EDIT_EMAIL_SEARCH_TEXT = "Edit Email";
     private static final String EDIT_EMAIL_DEMO_INPUT = "edit id/12 e/Kola@example.com";
-    private static final String EDIT_EMAIL_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: M;" +
-            " Phone: 88888888; Email: Kola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;" +
-            " Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String EDIT_EMAIL_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: M;"
+            + " Phone: 88888888; Email: Kola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;"
+            + " Tags: [heartDiseaseSpecialist][Pediatric]";
     private static final String EDIT_ADDRESS_SEARCH_TEXT = "Edit Address";
     private static final String EDIT_ADDRESS_DEMO_INPUT = "edit id/12 a/Blk 768 Woodlands Ave 6, Singapore 730768 ";
-    private static final String EDIT_ADDRESS_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola;" +
-            " Gender: M; Phone: 88888888; Email: Kola@example.com;" +
-            " Address: Blk 768 Woodlands Ave 6, Singapore 730768; Tags: [heartDiseaseSpecialist][Pediatric]";
+    private static final String EDIT_ADDRESS_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola;"
+            + " Gender: M; Phone: 88888888; Email: Kola@example.com;"
+            + " Address: Blk 768 Woodlands Ave 6, Singapore 730768; Tags: [heartDiseaseSpecialist][Pediatric]";
     private static final String EDIT_TAG_SEARCH_TEXT = "Edit Tag";
     private static final String EDIT_TAG_DEMO_INPUT = "edit id/12 t/Pediatric";
-    private static final String EDIT_TAG_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: M;" +
-            " Phone: 88888888; Email: Kola@example.com; Address: Blk 768 Woodlands Ave 6, Singapore 730768;" +
-            " Tags: [Pediatric]";
+    private static final String EDIT_TAG_DEMO_OUTPUT = "Edited person: Category: N Uid: 12; Name: Kola; Gender: M;"
+            + " Phone: 88888888; Email: Kola@example.com; Address: Blk 768 Woodlands Ave 6, Singapore 730768;"
+            + " Tags: [Pediatric]";
     private static final String EDIT_MIX_SEARCH_TEXT = "Edit Mix";
-    private static final String EDIT_MIX_DEMO_INPUT = "edit id/12 g/F p/98345432 " +
-            "a/Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03";
-    private static final String EDIT_MIX_DEMO_OUTPUT = "dited person: Category: N Uid: 12; Name: Kola; Gender: F;" +
-            " Phone: 98345432; Email: Kola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;" +
-            " Tags: [Pediatric]";
+    private static final String EDIT_MIX_DEMO_INPUT = "edit id/12 g/F p/98345432 "
+            + "a/Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03";
+    private static final String EDIT_MIX_DEMO_OUTPUT = "dited person: Category: N Uid: 12; Name: Kola; Gender: F;"
+            + " Phone: 98345432; Email: Kola@example.com; Address: Blk 431 Ang Mo Kio Ave 10, Singapore 560431 #01-03;"
+            + " Tags: [Pediatric]";
     private static final String EXIT_SEARCH_TEXT = "Exit Program";
     private static final String EXIT_DEMO_INPUT = "exit";
     private static final String EXIT_DEMO_OUTPUT = "Thank you for using Healthcare Xpress!";
@@ -108,48 +108,48 @@ public class HelpWindow extends UiPart<Stage> {
     private static final String HELP_DEMO_OUTPUT = "*Brings you to this window*";
     private static final String LIST_NTH_SEARCH_TEXT = "List (Default)";
     private static final String LIST_NTH_DEMO_INPUT = "list";
-    private static final String LIST_NTH_DEMO_OUTPUT = "Listed all persons with specifications: " +
-            "ADDRESS: NIL, CATEGORY: NIL, GENDER: NIL, TAG: NIL";
+    private static final String LIST_NTH_DEMO_OUTPUT = "Listed all persons with specifications: "
+            + "ADDRESS: NIL, CATEGORY: NIL, GENDER: NIL, TAG: NIL";
     private static final String LIST_ADDRESS_SEARCH_TEXT = "List all with X Address";
     private static final String LIST_ADDRESS_DEMO_INPUT = "list a/431";
-    private static final String LIST_ADDRESS_DEMO_OUTPUT = "Listed all persons with specifications: " +
-            "ADDRESS: 431, CATEGORY: NIL, GENDER: NIL, TAG: NIL";
+    private static final String LIST_ADDRESS_DEMO_OUTPUT = "Listed all persons with specifications: "
+            + "ADDRESS: 431, CATEGORY: NIL, GENDER: NIL, TAG: NIL";
     private static final String LIST_CATEGORY_SEARCH_TEXT = "List all with X Category";
     private static final String LIST_CATEGORY_DEMO_INPUT = "list c/N";
-    private static final String LIST_CATEGORY_DEMO_OUTPUT = "Listed all persons with specifications: " +
-            "ADDRESS: NIL, CATEGORY: N, GENDER: NIL, TAG: NIL";
+    private static final String LIST_CATEGORY_DEMO_OUTPUT = "Listed all persons with specifications: "
+            + "ADDRESS: NIL, CATEGORY: N, GENDER: NIL, TAG: NIL";
     private static final String LIST_GENDER_SEARCH_TEXT = "List all with X Gender";
     private static final String LIST_GENDER_DEMO_INPUT = "list g/F";
-    private static final String LIST_GENDER_DEMO_OUTPUT = "Listed all persons with specifications: " +
-            "ADDRESS: NIL, CATEGORY: NIL, GENDER: F, TAG: NIL";
+    private static final String LIST_GENDER_DEMO_OUTPUT = "Listed all persons with specifications: "
+            + "ADDRESS: NIL, CATEGORY: NIL, GENDER: F, TAG: NIL";
     private static final String LIST_TAG_SEARCH_TEXT = "List all with X Tag";
     private static final String LIST_TAG_DEMO_INPUT = "list t/Pediatric";
-    private static final String LIST_TAG_DEMO_OUTPUT = "Listed all persons with specifications: " +
-            "ADDRESS: NIL, CATEGORY: NIL, GENDER: NIL, TAG: Pediatric";
+    private static final String LIST_TAG_DEMO_OUTPUT = "Listed all persons with specifications: "
+            + "ADDRESS: NIL, CATEGORY: NIL, GENDER: NIL, TAG: Pediatric";
     private static final String LIST_MIX_SEARCH_TEXT = "List (Mix)";
     private static final String LIST_MIX_DEMO_INPUT = "list a/431 g/F";
-    private static final String LIST_MIX_DEMO_OUTPUT = "Listed all persons with specifications: " +
-            "ADDRESS: 431, CATEGORY: NIL, GENDER: F, TAG: NIL";
+    private static final String LIST_MIX_DEMO_OUTPUT = "Listed all persons with specifications: "
+            + "ADDRESS: 431, CATEGORY: NIL, GENDER: F, TAG: NIL";
     private static final String MARK_SEARCH_TEXT = "Marking a Patient";
     private static final String MARK_DEMO_INPUT = "mark id/3";
-    private static final String MARK_DEMO_OUTPUT = "Marked Patient: Category: P Uid: 3; Name: Alex Yeoh; Gender: M;" +
-            " Phone: 87438807; Email: alexyeoh@example.com; Address: Blk 30 Geylang Street 29, #06-40;" +
-            " Tags: [friends]; Home Visits Date and Time:11/11/2022 14:00 , ; Visit Status: visited";
+    private static final String MARK_DEMO_OUTPUT = "Marked Patient: Category: P Uid: 3; Name: Alex Yeoh; Gender: M;"
+            + " Phone: 87438807; Email: alexyeoh@example.com; Address: Blk 30 Geylang Street 29, #06-40;"
+            + " Tags: [friends]; Home Visits Date and Time:11/11/2022 14:00 , ; Visit Status: visited";
     private static final String CLEAR_USAGE_HELP = "";
-    private static final String DELETE_USAGE_HELP = "Delete Person from list: \n" +
-            "delete id/ID ";
-    private static final String EDIT_USAGE_HELP = "Edit Person from list: \n" +
-            "edit [c/Category] [n/Name] [g/gender] [p/Phone] [e/Email] [a/Address] [t/Tags] [dt/Date&Time]" +
-            "*[] is optional.";
+    private static final String DELETE_USAGE_HELP = "Delete Person from list: \n"
+            + "delete id/ID ";
+    private static final String EDIT_USAGE_HELP = "Edit Person from list: \n"
+            + "edit [c/Category] [n/Name] [g/gender] [p/Phone] [e/Email] [a/Address] [t/Tags] [dt/Date&Time]"
+            + "*[] is optional.";
     private static final String EXIT_USAGE_HELP = "";
-    private static final String FIND_USAGE_HELP = "Find a person from the list that contains keyword\n" +
-            "find KEYWORD";
+    private static final String FIND_USAGE_HELP = "Find a person from the list that contains keyword\n"
+            + "find KEYWORD";
     private static final String HELP_USAGE_HELP = "";
-    private static final String LIST_USAGE_HELP = "List all that meets the critiera:\n" +
-            "list [c/Category] [g/Gender] [a/Address] [t/Tag]\n" +
-            "*[] is optional";
-    private static final String MARK_USAGE_HELP = "Mark a patient visited:\n" +
-            "mark id/ID";
+    private static final String LIST_USAGE_HELP = "List all that meets the critiera:\n"
+            + "list [c/Category] [g/Gender] [a/Address] [t/Tag]\n"
+            + "*[] is optional";
+    private static final String MARK_USAGE_HELP = "Mark a patient visited:\n"
+            + "mark id/ID";
 
     private final List<String> commandList = Arrays.asList(PATIENT_SEARCH_TEXT, NURSE_SEARCH_TEXT, CLEAR_SEARCH_TEXT,
             DELETE_SEARCH_TEXT, EDIT_NAME_SEARCH_TEXT, EDIT_GENDER_SEARCH_TEXT, EDIT_PHONE_SEARCH_TEXT,
@@ -200,8 +200,8 @@ public class HelpWindow extends UiPart<Stage> {
     public void init() {
         initDictionary();
         initDictionaryForUsageHelp();
-        String EMPTY_STR = "";
-        String[] result = searchResult(EMPTY_STR);
+        String emptyStr = "";
+        String[] result = searchResult(emptyStr);
         ObservableList<String> obvListResult = convertStrArrToObservableList(result);
         fillResultDisplay(obvListResult);
         addListenerToListView();
@@ -238,10 +238,10 @@ public class HelpWindow extends UiPart<Stage> {
      * Initialize dictionary for usage help.
      */
     public void initDictionaryForUsageHelp() {
-        dictionaryForUsageHelp.put(PATIENT_SEARCH_TEXT,ADD_PATIENT_USAGE_HELP);
-        dictionaryForUsageHelp.put(NURSE_SEARCH_TEXT,ADD_NURSE_USAGE_HELP);
-        dictionaryForUsageHelp.put(CLEAR_SEARCH_TEXT,CLEAR_USAGE_HELP);
-        dictionaryForUsageHelp.put(DELETE_SEARCH_TEXT,DELETE_USAGE_HELP);
+        dictionaryForUsageHelp.put(PATIENT_SEARCH_TEXT, ADD_PATIENT_USAGE_HELP);
+        dictionaryForUsageHelp.put(NURSE_SEARCH_TEXT, ADD_NURSE_USAGE_HELP);
+        dictionaryForUsageHelp.put(CLEAR_SEARCH_TEXT, CLEAR_USAGE_HELP);
+        dictionaryForUsageHelp.put(DELETE_SEARCH_TEXT, DELETE_USAGE_HELP);
         dictionaryForUsageHelp.put(EDIT_NAME_SEARCH_TEXT, EDIT_USAGE_HELP);
         dictionaryForUsageHelp.put(EDIT_GENDER_SEARCH_TEXT, EDIT_USAGE_HELP);
         dictionaryForUsageHelp.put(EDIT_PHONE_SEARCH_TEXT, EDIT_USAGE_HELP);
@@ -344,7 +344,7 @@ public class HelpWindow extends UiPart<Stage> {
                 stopAnimationIfAny();
                 String[] inputOutput = dictionary.get(newValue);
                 helpOutputTextField.clear();
-                animateTextInTextField(inputOutput,helpCommandTextField,false);
+                animateTextInTextField(inputOutput, helpCommandTextField, false);
                 String usageHelp = dictionaryForUsageHelp.get(newValue);
                 tipTextArea.setText(usageHelp);
             }
@@ -367,7 +367,7 @@ public class HelpWindow extends UiPart<Stage> {
      * @param isOutput Check if it is on input's or on output's textfield.
      */
     public void animateTextInTextField(String[] inputOutput, TextField txtField, boolean isOutput) {
-        String str = inputOutput[isOutput?1:0];
+        String str = inputOutput[isOutput ? 1 : 0];
         final IntegerProperty counter = new SimpleIntegerProperty(0);
         Timeline timeline = new Timeline();
         KeyFrame keyFrame = new KeyFrame(
