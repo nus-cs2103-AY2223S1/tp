54229e995e4626a915bac4ba48665f39760c3bed
Rama <kmram0908@gmail.com>
2022-10-27 11:25:44 +0800

    Fix TaskTests failing due to DateTime creation

--- src/test/java/seedu/uninurse/testutil/TypicalTasks.java
+++ src/test/java/seedu/uninurse/testutil/TypicalTasks.java
@@ -1,12 +1,14 @@
 package seedu.uninurse.testutil;
 
+import static seedu.uninurse.testutil.TypicalDateTime.DATE_TIME_ONE;
+
 import seedu.uninurse.model.task.Task;
 
 /**
  * A utility class containing a list of {@code Task} objects to be used in tests.
  */
 public class TypicalTasks {
-    public static final Task TASK_INSULIN = new Task("Administer 1 unit of insulin");
+    public static final Task TASK_INSULIN = new Task("Administer 1 unit of insulin", DATE_TIME_ONE);
     public static final Task TASK_HEALTH_RECORDS = new Task("Update health records");
     public static final Task TASK_CARE_PLAN = new Task("Discuss care plan with family");
 }
