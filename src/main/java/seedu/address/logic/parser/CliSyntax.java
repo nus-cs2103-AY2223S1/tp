c0b32dc20980ef98f9dd15e00592fc948bfb2ba2
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2022-10-27 17:08:30 +0800

    Update CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -14,7 +14,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ID = new Prefix("id/");
     public static final Prefix PREFIX_CLIENT = new Prefix("cli/");
     public static final Prefix PREFIX_PERSON = new Prefix("per/");
-    public static final Prefix PREFIX_ASKING_PRICE = new Prefix("a/");
+    public static final Prefix PREFIX_ASKING_PRICE = new Prefix("ap/");
     public static final Prefix PREFIX_OFFER = new Prefix("o/");
     public static final Prefix PREFIX_LISTING_ID = new Prefix("l/");
 
