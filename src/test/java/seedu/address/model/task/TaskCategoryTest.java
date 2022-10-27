3901fa4dc8a4190f1c26a37ed79ca751fafc684f
ALWHgithub <alwhgithub@gmail.com>
2022-10-27 15:12:27 +0800

    Remove outdated test cases

--- src/test/java/seedu/address/model/task/TaskCategoryTest.java
+++ src/test/java/seedu/address/model/task/TaskCategoryTest.java
@@ -8,7 +8,7 @@ public class TaskCategoryTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new TaskCategory(null));
+        assertThrows(NullPointerException.class, () -> new TaskCategory( null));
     }
 
 }
