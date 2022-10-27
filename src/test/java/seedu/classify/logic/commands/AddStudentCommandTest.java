d72f673d5d120bae2446a9258e43ccaabe7a83d6
maxtance <e0772757@u.nus.edu>
2022-10-27 19:24:38 +0800

    Fix test errors

--- src/test/java/seedu/classify/logic/commands/AddStudentCommandTest.java
+++ src/test/java/seedu/classify/logic/commands/AddStudentCommandTest.java
@@ -169,12 +169,12 @@ public class AddStudentCommandTest {
 
         @Override
         public boolean isStudentListInfoConcise() {
-            return false;
+            throw new AssertionError("This method should not be called");
         }
 
         @Override
         public FilteredStudents getFilteredStudents() {
-            return null;
+            throw new AssertionError("This method should not be called");
         }
 
         @Override
