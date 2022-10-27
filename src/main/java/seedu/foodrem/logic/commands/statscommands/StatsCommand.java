4ae0501fa9b3a1b6c337c8c916c7e16428d7e01d
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-28 04:17:18 +0800

    Replace getTopThreeExpensiveItems stub

--- src/main/java/seedu/foodrem/logic/commands/statscommands/StatsCommand.java
+++ src/main/java/seedu/foodrem/logic/commands/statscommands/StatsCommand.java
@@ -3,7 +3,6 @@ package seedu.foodrem.logic.commands.statscommands;
 import static java.util.Objects.requireNonNull;
 import static seedu.foodrem.commons.enums.CommandType.STATS_COMMAND;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +10,7 @@ import seedu.foodrem.logic.commands.Command;
 import seedu.foodrem.logic.commands.CommandResult;
 import seedu.foodrem.model.Model;
 import seedu.foodrem.model.item.Item;
-import seedu.foodrem.model.tag.Tag;
-import seedu.foodrem.model.util.SampleDataUtil;
+import seedu.foodrem.model.item.itemcomparators.ItemCostComparator;
 import seedu.foodrem.viewmodels.Stats;
 
 /**
@@ -22,22 +20,17 @@ public class StatsCommand extends Command {
     @Override
     public CommandResult<Stats> execute(Model model) {
         requireNonNull(model);
-        List<Item> expensiveItems = getTopThreeExpensiveItems();
-        List<Tag> commonTags = getTopThreeCommonTags();
+        List<Item> expensiveItems = getTopThreeExpensiveItems(model.getFoodRem().getItemList());
         double amountWasted = getAmountWastedFromWastage();
-        return CommandResult.from(new Stats(amountWasted, expensiveItems, commonTags));
+        return CommandResult.from(new Stats(amountWasted, expensiveItems));
     }
 
     public static String getUsage() {
         return STATS_COMMAND.getUsage();
     }
 
-    private List<Item> getTopThreeExpensiveItems() {
-        return Arrays.stream(SampleDataUtil.getSampleItems()).limit(3).collect(Collectors.toList());
-    }
-
-    private List<Tag> getTopThreeCommonTags() {
-        return Arrays.stream(SampleDataUtil.getSampleTags()).limit(3).collect(Collectors.toList());
+    private List<Item> getTopThreeExpensiveItems(List<Item> itemList) {
+        return itemList.stream().sorted(new ItemCostComparator().reversed()).limit(3).collect(Collectors.toList());
     }
 
     private double getAmountWastedFromWastage() {
