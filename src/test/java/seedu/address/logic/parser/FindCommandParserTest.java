219ab6f33e76bd75926ba0dcecd23b9e71c1646e
Nisemono <james041200@gmail.com>
2022-10-28 06:11:50 +0800

    =Comply checkstyle (Seriously I hate checkstyle)

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.ArrayList;
@@ -18,10 +16,11 @@ public class FindCommandParserTest {
     private FindCommandParser parser = new FindCommandParser();
 
     // TODO Update since keyword can be empty now
-//    @Test
-//    public void parse_emptyArg_throwsParseException() {
-//        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-//    }
+    //    @Test
+    //    public void parse_emptyArg_throwsParseException() {
+    //        assertParseFailure(parser, "     ",
+    //        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    //    }
 
     // TODO Update
     @Test
