d28508926c60e4b9131d6ca918982869522f3dd8
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-28 06:08:46 +0800

    Add getTopThreeCommonTags method

--- src/main/java/seedu/foodrem/logic/commands/statscommands/StatsCommand.java
+++ src/main/java/seedu/foodrem/logic/commands/statscommands/StatsCommand.java
@@ -4,7 +4,10 @@ import static java.util.Objects.requireNonNull;
 import static seedu.foodrem.commons.enums.CommandType.STATS_COMMAND;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javafx.collections.ObservableList;
@@ -13,6 +16,7 @@ import seedu.foodrem.logic.commands.CommandResult;
 import seedu.foodrem.model.Model;
 import seedu.foodrem.model.item.Item;
 import seedu.foodrem.model.item.itemcomparators.ItemCostComparator;
+import seedu.foodrem.model.tag.Tag;
 import seedu.foodrem.viewmodels.Stats;
 
 /**
@@ -23,9 +27,11 @@ public class StatsCommand extends Command {
     public CommandResult<Stats> execute(Model model) {
         requireNonNull(model);
         ObservableList<Item> itemList = model.getFoodRem().getItemList();
+        ObservableList<Tag> tagList = model.getFoodRem().getTagList();
         List<Item> expensiveItems = getTopThreeExpensiveItems(itemList);
+        List<Tag> commonTags = getTopThreeCommonTags(itemList, tagList);
         double amountWasted = getAmountWasted(itemList);
-        return CommandResult.from(new Stats(amountWasted, expensiveItems));
+        return CommandResult.from(new Stats(amountWasted, expensiveItems, commonTags));
     }
 
     public static String getUsage() {
@@ -44,5 +50,29 @@ public class StatsCommand extends Command {
         return itemList.stream()
                 .filter(i -> !i.getQuantity().isZero() && i.getExpiryDate().isAfterExpiryDate(LocalDate.now()))
                 .map(i -> i.getPrice().getItemPrice()).reduce(0.0, Double::sum);
+    }
+
+    private List<Tag> getTopThreeCommonTags(List<Item> itemList, List<Tag> tagList) {
+        HashMap<Tag, Integer> tagToFrequencyMap = new HashMap<>();
+
+        // TODO: Replace with streams, this is janky and ugly
+        for (int i = 0; i < tagList.size(); i++) {
+            Tag tag = tagList.get(i);
+            for (int j = 0; j < itemList.size(); j++) {
+                if (itemList.get(j).getTagSet().contains(tag)) {
+                    if (tagToFrequencyMap.containsKey(tag)) {
+                        tagToFrequencyMap.put(tag, tagToFrequencyMap.get(tag) + 1);
+                    } else {
+                        tagToFrequencyMap.put(tag, 1);
+                    }
+                }
+            }
+        }
+
+        return tagToFrequencyMap.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
+                .limit(3)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
