9769c1a6610c38fff154ba0bce72905776978316
gerardstevan <gerardstevan0406@gmail.com>
2022-10-27 22:48:04 +0800

    Improve help window

--- src/main/java/seedu/travelr/ui/HelpWindow.java
+++ src/main/java/seedu/travelr/ui/HelpWindow.java
@@ -7,16 +7,67 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import seedu.travelr.commons.core.LogsCenter;
 
 /**
+ * Everything related HelpWindow, including fxml and css is based on AY2223S1-CS2103T-W17-4 team
+ * Pleasehireus implementation of it which can be seen <a href="https://github.com/AY2223S1-CS2103T-W17-4/tp">here</a>
+ *
  * Controller for a help page
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String WELCOME_MESSAGE = "Welcome to TravelR!!";
+    public static final String MESSAGE = "Refer to the following table for a quick guide on the commands to use this app.";
+    public static final String UG_URL = "https://ay2223s1-cs2103t-w17-1.github.io/tp/UserGuide.html";
+    public static final String USER_GUIDE_MESSAGE = "For more details refer to the user guide: " + UG_URL;
+    public static final String COMMAND_SUMMARY =
+            "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Action                      | Format                                                      |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Add a trip                  | add n/TITLE d/DESCRIPTION                                   |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Add an event                | add-e n/TITLE d/DESCRIPTION                                 |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Delete an event             | delete-e INDEX                                              |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Delete a trip               | delete INDEX                                                |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Add an event to a trip      | add-et n/EVENT_TITLE t/TRIP_TITLE                           |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Remove an event from trip   | delete-et n/EVENT_TITLE t/TRIP_TITLE                        |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| View trips list             | list                                                        |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| View events list            | list-e                                                      |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Mark a trip as done         | mark INDEX                                                  |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Mark a trip as not done     | unmark INDEX                                                |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| View completed events and   | completed                                                   |\n"
+                    + "| trips                       |                                                             |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| View all trips and events   | view                                                        |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| View lifetime summary       | summary                                                     |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| View events in a trip       | select INDEX                                                |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Clear the storage and data  | clear                                                       |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Find a trip                 | find TRIP_TITLE                                             |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Sort events in bucket list  | sort-e                                                      |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| Sort Trips                 | sort                                                         |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+\n"
+                    + "| List available commands     | help                                                        |\n"
+                    + "| and link to User Guide      |                                                             |\n"
+                    + "+-----------------------------+-------------------------------------------------------------+";
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
@@ -25,7 +76,19 @@ public class HelpWindow extends UiPart<Stage> {
     private Button copyButton;
 
     @FXML
-    private Label helpMessage;
+    private Label message;
+
+    @FXML
+    private Label welcomeMessage;
+
+
+    @FXML
+    private Label userGuideMessage;
+
+    @FXML
+    private Label commandSummary;
+    @FXML
+    private Label commandSummaryIntro;
 
     /**
      * Creates a new HelpWindow.
@@ -34,7 +97,10 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
-        helpMessage.setText(HELP_MESSAGE);
+        message.setText(MESSAGE);
+        userGuideMessage.setText(USER_GUIDE_MESSAGE);
+        commandSummary.setText(COMMAND_SUMMARY);
+        welcomeMessage.setText(WELCOME_MESSAGE);
     }
 
     /**
@@ -42,6 +108,7 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow() {
         this(new Stage());
+        setKeys();
     }
 
     /**
@@ -65,7 +132,27 @@ public class HelpWindow extends UiPart<Stage> {
     public void show() {
         logger.fine("Showing help page about the application.");
         getRoot().show();
+        getRoot().setMaximized(true);
         getRoot().centerOnScreen();
+    }
+
+    /**
+     * Bind keys as shortcuts for help window.
+     */
+    private void setKeys() {
+        //Bind ESC to minimized help window
+        getRoot().addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (KeyCode.ESCAPE == event.getCode()) {
+                hide();
+            }
+        });
+
+        //Bind C to copy user guide url
+        getRoot().addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (KeyCode.C == event.getCode()) {
+                copyUrl();
+            }
+        });
     }
 
     /**
@@ -96,7 +183,7 @@ public class HelpWindow extends UiPart<Stage> {
     private void copyUrl() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
-        url.putString(USERGUIDE_URL);
+        url.putString(UG_URL);
         clipboard.setContent(url);
     }
 }
