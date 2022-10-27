a1fceacb7cdce0a9b993621797c336555756dab0
Tan Yi Xian <>
2022-10-27 20:24:34 +0800

    Convert NewTagCommand execute to return TagsWithMessage

--- src/main/java/seedu/foodrem/logic/commands/tagcommands/NewTagCommand.java
+++ src/main/java/seedu/foodrem/logic/commands/tagcommands/NewTagCommand.java
@@ -3,16 +3,21 @@ package seedu.foodrem.logic.commands.tagcommands;
 import static java.util.Objects.requireNonNull;
 import static seedu.foodrem.commons.enums.CommandType.NEW_TAG_COMMAND;
 
+import java.util.List;
+
 import seedu.foodrem.logic.commands.Command;
 import seedu.foodrem.logic.commands.CommandResult;
 import seedu.foodrem.logic.commands.exceptions.CommandException;
 import seedu.foodrem.model.Model;
 import seedu.foodrem.model.tag.Tag;
+import seedu.foodrem.viewmodels.TagsWithMessage;
 
 /**
  * Adds a tag to FoodRem.
  */
 public class NewTagCommand extends Command {
+    private static final String MESSAGE_SUCCESS = "New tag added:";
+
     private final Tag toAdd;
 
     /**
@@ -26,7 +31,7 @@ public class NewTagCommand extends Command {
     }
 
     @Override
-    public CommandResult<String> execute(Model model) throws CommandException {
+    public CommandResult<TagsWithMessage> execute(Model model) throws CommandException {
         requireNonNull(model);
 
         if (model.hasTag(toAdd)) {
@@ -34,7 +39,7 @@ public class NewTagCommand extends Command {
         }
 
         model.addTag(toAdd);
-        return CommandResult.from(String.format("New tag added: %1$s", toAdd));
+        return CommandResult.from(new TagsWithMessage(List.of(toAdd), MESSAGE_SUCCESS));
     }
 
     public static String getUsage() {
