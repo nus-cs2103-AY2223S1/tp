ae078b6d14542681fd40a35cd3ca84cd857e21c7
Markus Wee <77526609+yellow-294@users.noreply.github.com>
2022-10-27 20:13:02 +0800

    Fix JavaCI issues

--- src/main/java/seedu/address/logic/commands/RolesCommand.java
+++ src/main/java/seedu/address/logic/commands/RolesCommand.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLES;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
-import java.util.Locale;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
