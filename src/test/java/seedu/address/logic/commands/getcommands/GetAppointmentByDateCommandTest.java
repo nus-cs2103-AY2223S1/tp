cf2579e666895af9d5c8874377b6d3d9fa839afd
kxrt <kvrtikeya@gmail.com>
2022-10-28 00:16:29 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/getcommands/GetAppointmentByDateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/getcommands/GetAppointmentByDateCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
