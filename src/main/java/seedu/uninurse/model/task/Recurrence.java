e1cd501b88c489d366871639f5b05dd4f3d5a269
Rama <kmram0908@gmail.com>
2022-10-27 14:38:21 +0800

    Updated recurrence to have singular version of the TIME_PERIOD e.g day, month etc

--- src/main/java/seedu/uninurse/model/task/Recurrence.java
+++ src/main/java/seedu/uninurse/model/task/Recurrence.java
@@ -4,6 +4,10 @@ package seedu.uninurse.model.task;
  * Frequency is a enum which represents the different time frequencies the user can have for a recurring task.
  */
 public enum Recurrence {
+    DAY,
+    WEEK,
+    MONTH,
+    YEAR,
     DAYS,
     WEEKS,
     MONTHS,
