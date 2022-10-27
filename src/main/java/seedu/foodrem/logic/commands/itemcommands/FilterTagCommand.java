ba00ab63376e8728ff8fdb43813ccf1956b1df45
bryanljx1 <bryanljx1@gmail.com>
2022-10-27 23:58:12 +0800

    Refactor FilterTagCommand to remove FilterCommand

--- src/main/java/seedu/foodrem/logic/commands/itemcommands/FilterTagCommand.java
+++ src/main/java/seedu/foodrem/logic/commands/itemcommands/FilterTagCommand.java
@@ -1,36 +1,57 @@
 package seedu.foodrem.logic.commands.itemcommands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.foodrem.commons.enums.CommandType.FILTER_TAG_COMMAND;
 
+import seedu.foodrem.commons.core.Messages;
+import seedu.foodrem.logic.commands.Command;
+import seedu.foodrem.logic.commands.CommandResult;
+import seedu.foodrem.logic.commands.exceptions.CommandException;
+import seedu.foodrem.model.Model;
+import seedu.foodrem.model.item.TagSetContainsTagPredicate;
 import seedu.foodrem.model.tag.Tag;
 
 /**
  * Filters all items in FoodRem for items that contain the specified tag
  */
-public class FilterTagCommand extends FilterCommand {
-    private final Tag tag;
+public class FilterTagCommand extends Command {
+    private final TagSetContainsTagPredicate pred;
 
     /**
      * @param tag to filter the Item list for Items tagged with it
      */
     public FilterTagCommand(Tag tag) {
-        super(item -> item.getTagSet().contains(tag));
-        this.tag = tag;
+        requireNonNull(tag);
+        this.pred = new TagSetContainsTagPredicate(tag);
+    }
+
+    @Override
+    public CommandResult<String> execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (!model.hasTag(pred.getTag())) {
+            throw new CommandException("This tag does not exist in the FoodRem");
+        }
+
+        model.updateFilteredItemList(pred);
+        return CommandResult.from(String.format(this.getSuccessMessage(),
+                model.getCurrentList().size()));
     }
 
     public static String getUsage() {
         return FILTER_TAG_COMMAND.getUsage();
     }
 
-    @Override
     protected String getSuccessMessage() {
-        return String.format("Filtered by tag: %s\n%s", this.tag.getName(), super.getSuccessMessage());
+        return String.format("Filtered by tag: %s\n%s",
+                this.pred.getTag().getName(),
+                Messages.MESSAGE_ITEMS_FILTERED_OVERVIEW);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this
                 || (other instanceof FilterTagCommand
-                && this.tag.equals(((FilterTagCommand) other).tag));
+                && this.pred.equals(((FilterTagCommand) other).pred));
     }
 }
