50caa208df7810df9cb9639dfabb1f885a3da05a
sh4nH <e0866259@u.nus.edu>
2022-10-27 23:40:39 +0800

    changes to comments and access modifier

--- src/main/java/seedu/address/model/person/PersonMatchesKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/PersonMatchesKeywordsPredicate.java
@@ -9,37 +9,46 @@ import seedu.address.commons.util.StringUtil;
  * Tests that a {@code Person} matches any of the keywords given.
  */
 public class PersonMatchesKeywordsPredicate implements Predicate<Person> {
-    private final String keywords;
+    protected final String keywords;
 
     public PersonMatchesKeywordsPredicate(String keywords) {
         this.keywords = keywords;
     }
 
-    // This method is used to compute the Levenshtein distance, which
-    // how different two strings are from one another by counting the minimum
-    // number of operations (insertions, deletions and substitutions of characters)
-    // required to transform one string to another.
-    private int getLevenshteindist(String str1, String str2) {
+    /**
+     * Computes the Levenshtein distance, which tells how different two
+     * strings are from one another by counting the minimum
+     * number of operations (insertions, deletions and substitutions of characters)
+     * required to transform one string to another.
+     *
+     * @param str1 string1 to be compared
+     * @param str2 string2 to be compared
+     * @return the Levenshtein distance between the 2 strings
+     */
+    //
+    private int getLevenshteinDist(String str1, String str2) {
 
-        // A 2-D matrix to store previously calculated
-        // answers of subproblems
+        // A 2-D matrix to store previously calculated answers of subproblems
         int[][] arr = new int[str1.length() + 1][str2.length() + 1];
 
         for (int i = 0; i <= str1.length(); i++) {
             for (int j = 0; j <= str2.length(); j++) {
 
-                // If str1 is empty, the only possible
-                // method of conversion with minimum operations
-                // would be for all characters of
-                // str2 to be inserted into str1,
+                /**
+                 * If str1 is empty, the only possible
+                 * method of conversion with minimum operations
+                 * would be for all characters of
+                 * str2 to be inserted into str1.
+                 */
                 if (i == 0) {
                     arr[i][j] = j;
                 }
 
-                // If str2 is empty the only possible
-                // method of conversion with minimum operations
-                // would be for all characters of str1
-                // to be removed.
+                /**
+                 * If str2 is empty the only possible
+                 * method of conversion with minimum operations
+                 * would be for all characters of str1 to be removed.
+                 */
                 else if (j == 0) {
                     arr[i][j] = i;
                 }
@@ -56,25 +65,45 @@ public class PersonMatchesKeywordsPredicate implements Predicate<Person> {
         return arr[str1.length()][str2.length()];
     }
 
-    // This method uses the levenshtein distance to calculate the similarity
-    // between two strings in the range [0, 1].
+    /**
+     * This method uses the levenshtein distance to calculate the similarity
+     * between two strings in the range [0, 1].
+     * @param x the first string to be compared
+     * @param y second string to be compared
+     * @return the similarity between the 2 strings in the range [0,1]
+     */
+
     private double findSimilarity(String x, String y) {
         double maxLength = Double.max(x.length(), y.length());
         if (maxLength > 0) {
-            return (maxLength - getLevenshteindist(x, y)) / maxLength;
+            return (maxLength - getLevenshteinDist(x, y)) / maxLength;
         }
         return 1.0;
     }
 
-    // This method is used to check for distinct charecters in str1 and str2.
-    static int numOfReplacement(char c1, char c2) {
+    /**
+     * This method is used to check for distinct
+     * characters in str1 and str2.
+     *
+     * @param c1 char1 to be compared
+     * @param c2 char2 to be compared
+     * @return 0 if both chars are same, else 1
+     */
+    //
+    private int numOfReplacement(char c1, char c2) {
         return c1 == c2 ? 0 : 1;
     }
 
-    // This method is used to receive the count of different
-    // operations performed and return the
-    // minimum value among them.
-    static int minEdits(int... nums) {
+    /**
+     * This method is used to receive the count of different
+     * operations performed and return the minimum
+     * value among them.
+     *
+     * @param nums the number of operations
+     * @return minimum number of operations
+     */
+
+    private int minEdits(int... nums) {
         return Arrays.stream(nums).min().orElse(Integer.MAX_VALUE);
     }
 
