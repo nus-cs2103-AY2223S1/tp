1c00325bae9fd9f966c4be27b47f173a332e82c8
Yuanzheng <yuanzheng.tan.tyz@gmail.com>
2022-10-27 10:41:25 +0800

    Add toString methods to models to have easily comparable outputs
    AddressBook, ModelManager, and UniquePersonList all have cryptic and
    therefore not very helpful outputs when printed or logged, which
    makes debugging difficult.
    
    Add toString methods to these classes to have an output that is
    descriptive of the class contents, and is easily comparable with
    diffing operations.

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -22,12 +22,12 @@ public class AddressBook implements ReadOnlyAddressBook {
      *
      * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
      *   among constructors.
-     */
-    {
+     */ {
         persons = new UniquePersonList();
     }
 
-    public AddressBook() {}
+    public AddressBook() {
+    }
 
     /**
      * Creates an AddressBook using the Persons in the {@code toBeCopied}
@@ -98,8 +98,7 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     @Override
     public String toString() {
-        return persons.asUnmodifiableObservableList().size() + " persons";
-        // TODO: refine later
+        return persons.toString();
     }
 
     @Override
@@ -110,8 +109,8 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof AddressBook // instanceof handles nulls
-                && persons.equals(((AddressBook) other).persons));
+            || (other instanceof AddressBook // instanceof handles nulls
+            && persons.equals(((AddressBook) other).persons));
     }
 
     @Override
