381e51d3db47672db9acdf708ad70ff4c42bd0fb
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-28 04:13:53 +0800

    Add ItemCostComparator

---
+++ src/main/java/seedu/foodrem/model/item/itemcomparators/ItemCostComparator.java
@@ -0,0 +1,19 @@
+package seedu.foodrem.model.item.itemcomparators;
+
+import seedu.foodrem.model.item.Item;
+
+/**
+ * Comparator comparing between ItemPrices
+ */
+public class ItemCostComparator implements ItemComparator {
+    @Override
+    public int compare(Item item1, Item item2) {
+
+        return Double.compare(item1.getItemCost(), item2.getItemCost());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
+    }
+}
