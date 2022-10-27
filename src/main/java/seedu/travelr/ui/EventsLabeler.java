8609f2a8955eb6ce69b7fd1592be752a7ad28961
seanmanik <seanmanik@gmail.com>
2022-10-27 18:39:18 +0800

    Add icons for location, dates, and labels

--- src/main/java/seedu/travelr/ui/EventsLabeler.java
+++ src/main/java/seedu/travelr/ui/EventsLabeler.java
@@ -1,5 +1,10 @@
 package seedu.travelr.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 
 /**
@@ -7,13 +12,20 @@ import javafx.scene.layout.Region;
  */
 public class EventsLabeler extends UiPart<Region> {
 
+    @FXML
+    private Label eventsTextField;
     private static final String FXML = "EventsLabeler.fxml";
 
     /**
      * Labels event.
      */
-    public EventsLabeler() {
+    public EventsLabeler(Image icon) {
         super(FXML);
+        ImageView eventsIcon = new ImageView(icon);
+        eventsIcon.setFitWidth(25);
+        eventsIcon.setFitHeight(25);
+        eventsTextField.setGraphic(eventsIcon);
+        eventsTextField.setContentDisplay(ContentDisplay.RIGHT);
     }
 
 
