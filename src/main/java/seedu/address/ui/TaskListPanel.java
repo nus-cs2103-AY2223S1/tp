778cb882133dfae0d0151ed76af34211f138a98e
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-10-27 18:02:00 +0800

    Add task completion to UI

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -3,6 +3,8 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
@@ -18,14 +20,25 @@ public class TaskListPanel extends UiPart<Region> {
     private static final String FXML = "TaskListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
 
-    @javafx.fxml.FXML
+    @FXML
     private ListView<Task> taskListView;
+    @FXML
+    private Label taskCompletion;
 
     /**
      * Creates a {@code TaskListPanel} with the given {@code ObservableList}.
      */
     public TaskListPanel(ObservableList<Task> taskList) {
         super(FXML);
+        int numTasks = taskList.size();
+        if (numTasks > 0) {
+            long numCompletedTasks = taskList.stream().filter(Task::isComplete).count();
+            double completionPercentage = (double) numCompletedTasks / numTasks * 100;
+            taskCompletion.setText(
+                    String.format("%d of %d (%,.1f%%) completed", numCompletedTasks, numTasks, completionPercentage));
+        } else {
+            taskCompletion.setText("No tasks added yet!");
+        }
         taskListView.setItems(taskList);
         taskListView.setCellFactory(listView -> new TaskListViewCell());
     }
