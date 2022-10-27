7736d178d1f9cf846aada945697f98abe7648f34
nealetham <nealetham2000@gmail.com>
2022-10-27 22:44:29 +0800

    Fix checkstyle

--- src/test/java/seedu/rc4hdb/ui/CommandHistoryTest.java
+++ src/test/java/seedu/rc4hdb/ui/CommandHistoryTest.java
@@ -2,7 +2,6 @@ package seedu.rc4hdb.ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
