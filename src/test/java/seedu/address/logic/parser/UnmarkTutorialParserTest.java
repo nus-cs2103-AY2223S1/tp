e0af34bfc5dc96543ab86028c87758315de15320
qiaoen17 <92434807+qiaoen17@users.noreply.github.com>
2022-10-28 00:47:38 +0800

    Add Unmarktutparser and marktutparser tests

---
+++ src/test/java/seedu/address/logic/parser/UnmarkTutorialParserTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.UnmarkTutorialCommand;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TUTORIAL;
+
+public class UnmarkTutorialParserTest {
+    private UnmarkTutorialCommandParser parser = new UnmarkTutorialCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsMarkTutorialCommand() {
+        assertParseSuccess(parser, "1", new UnmarkTutorialCommand(INDEX_FIRST_TUTORIAL));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "abc", String.format(MESSAGE_INVALID_COMMAND_FORMAT
+                , UnmarkTutorialCommand.MESSAGE_USAGE));
+    }
+}
