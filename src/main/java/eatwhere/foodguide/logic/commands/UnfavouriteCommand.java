6034044c4d6d2db0c8f7eeaf6400f09d9d0756a2
debwy <e0773217@u.nus.edu>
2022-10-28 05:11:51 +0800

    Remove unused import statements for checkstyle

--- src/main/java/eatwhere/foodguide/logic/commands/UnfavouriteCommand.java
+++ src/main/java/eatwhere/foodguide/logic/commands/UnfavouriteCommand.java
@@ -1,29 +1,10 @@
 package eatwhere.foodguide.logic.commands;
 
-import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_CUISINE;
-import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_LOCATION;
-import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_NAME;
-import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_PRICE;
-import static eatwhere.foodguide.logic.parser.CliSyntax.PREFIX_TAG;
 import static java.util.Objects.requireNonNull;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import eatwhere.foodguide.commons.core.Messages;
 import eatwhere.foodguide.commons.core.index.Index;
-import eatwhere.foodguide.commons.util.CollectionUtil;
 import eatwhere.foodguide.logic.commands.exceptions.CommandException;
 import eatwhere.foodguide.model.Model;
-import eatwhere.foodguide.model.eatery.Cuisine;
-import eatwhere.foodguide.model.eatery.Eatery;
-import eatwhere.foodguide.model.eatery.Location;
-import eatwhere.foodguide.model.eatery.Name;
-import eatwhere.foodguide.model.eatery.Price;
-import eatwhere.foodguide.model.tag.Tag;
 
 /**
  * Edits the details of an existing eatery in the food guide.
