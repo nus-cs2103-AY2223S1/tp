370705cf388796793188639398211e6d10264dc9
Li Tianze <92414640+TZL0@users.noreply.github.com>
2022-10-27 16:55:14 +0800

    Update Remark.java

--- src/main/java/seedu/address/model/patient/Remark.java
+++ src/main/java/seedu/address/model/patient/Remark.java
@@ -8,7 +8,7 @@ import static java.util.Objects.requireNonNull;
  */
 public class Remark {
 
-    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values";
 
     /*
      * The first character of the remark must not be a whitespace,
