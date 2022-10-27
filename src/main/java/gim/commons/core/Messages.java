cb5bfc8e04c021bf01ab4f77620f5908c9580f29
24Donovan24 <donovanlee_25@hotmail.com>
2022-10-27 21:11:38 +0800

    Edit message used for filter command

--- src/main/java/gim/commons/core/Messages.java
+++ src/main/java/gim/commons/core/Messages.java
@@ -10,8 +10,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_EXERCISE_DISPLAYED_INDEX = "The exercise index provided is invalid";
     public static final String MESSAGE_EXERCISES_LISTED_OVERVIEW = "%1$d exercises listed!";
     public static final String MESSAGE_FIND_EMPTY_EXERCISES_LIST = "%1$d exercises listed!\n"
-            + "Note that :filter only filters for you the list shown on the Exercise List Window!\n"
-            + "If you think your keyword is correct, try executing :list before executing :filter again!";
+            + "Note that :filter only filters for you the list shown on the Exercise List Window.\n"
+            + "If you think your keyword is correct, try executing :list before executing :filter again.";
     public static final String MESSAGE_INCORRECT_INDEX_FORMAT = "Incorrect format for index(es)";
     public static final String MESSAGE_MISSING_LEVEL = "No difficulty level selected!";
     public static final String MESSAGE_INVALID_LEVEL = "Difficulty level not supported!";
