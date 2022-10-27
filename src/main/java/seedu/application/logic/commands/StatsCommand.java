e2092dcf99ef8fd6edc748618e1d11a373dd2e9a
eesung00 <e0407725@u.nus.edu>
2022-10-27 17:53:22 +0800

    Update stats command

--- src/main/java/seedu/application/logic/commands/StatsCommand.java
+++ src/main/java/seedu/application/logic/commands/StatsCommand.java
@@ -7,38 +7,39 @@ import javafx.collections.ObservableList;
 import seedu.application.model.Model;
 import seedu.application.model.ReadOnlyApplicationBook;
 import seedu.application.model.application.Application;
+import seedu.application.model.application.Status;
 
-public class StatsCommand {
+public class StatsCommand extends Command {
     public static final String COMMAND_WORD = "stats";
 
     public static final String MESSAGE_SUCCESS = "Listed all applications";
 
-    private int archived;
-    private int rejected;
-    private int interviews;
-    private int offered;
-    private int pending;
-    private int totalApplications;
-
-    StatsCommand() {
-        archived = 0;
-        rejected = 0;
-        interviews = 0;
-        offered = 0;
-        pending = 0;
-        totalApplications = 0;
-    }
-
     @Override
     public CommandResult execute(Model model) {
+        int archived = 0;
+        int rejected = 0;
+        int interviews = 0;
+        int offered = 0;
+        int pending = 0;
         requireNonNull(model);
         ObservableList<Application> currentList = model.getAllApplicationsInBook();
-        totalApplications += currentList.size();
+        int totalApplications = currentList.size();
         for (Application app : currentList) {
             if (app.isArchived()) {
-                archived += 1;
+                archived++;
             }
-            if (app.getStatus().toString.equals())
+            if (app.getStatus().equals(Status.REJECTED)) {
+                rejected++;
+            }
+            if (app.getStatus().equals(Status.OFFERED)) {
+                offered++;
+            }
+            if (app.getStatus().equals(Status.PENDING)) {
+                pending++;
+            }
+            if (app.hasInterview()) {
+                interviews++;
+            }
         }
         return new CommandResult(MESSAGE_SUCCESS);
     }
