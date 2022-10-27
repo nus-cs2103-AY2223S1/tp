10b218b63fa27b1c7092f283ba863d57ce572fb9
Agamjyot Singh Chadha <agamjyot01@gmail.com>
2022-10-28 02:28:04 +0800

    Implement -r command with findTag command

--- src/main/java/eatwhere/foodguide/logic/commands/FindTagCommand.java
+++ src/main/java/eatwhere/foodguide/logic/commands/FindTagCommand.java
@@ -4,7 +4,14 @@ import static java.util.Objects.requireNonNull;
 
 import eatwhere.foodguide.commons.core.Messages;
 import eatwhere.foodguide.model.Model;
+import eatwhere.foodguide.model.eatery.Eatery;
 import eatwhere.foodguide.model.eatery.TagsContainsKeywordsPredicate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Finds and lists all eateries in food guide whose tags contains any of the argument keywords.
@@ -19,18 +26,41 @@ public class FindTagCommand extends Command {
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " mala";
 
-    private final TagsContainsKeywordsPredicate predicate;
+    private final Predicate<Eatery> predicate;
+    private final int numRandPicks;
 
-    public FindTagCommand(TagsContainsKeywordsPredicate predicate) {
+    public FindTagCommand(Predicate<Eatery> predicate) {
+        this(predicate, -1);
+    }
+
+    public FindTagCommand(Predicate<Eatery> predicate, int numRandPicks) {
         this.predicate = predicate;
+        this.numRandPicks = numRandPicks;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredEateryList(predicate);
+        if (numRandPicks > 0) {
+            int numToShow = Math.min(numRandPicks, model.getFilteredEateryList().size());
+            List<Integer> randomIndexes = new ArrayList<>(model.getFilteredEateryList().size());
+            for (int i = 0; i < model.getFilteredEateryList().size(); ++i) {
+                randomIndexes.add(i);
+            }
+            Collections.shuffle(randomIndexes);
+            randomIndexes = randomIndexes.subList(0, numToShow);
+            randomIndexes.sort(null);
+
+            ArrayList<Eatery> eateriesChosen = new ArrayList<>(numToShow);
+            for (Integer i : randomIndexes) {
+                eateriesChosen.add(model.getFilteredEateryList().get(i));
+            }
+            model.updateFilteredEateryList(eateriesChosen::contains);
+        }
         return new CommandResult(
                 String.format(Messages.MESSAGE_EATERIES_LISTED_OVERVIEW, model.getFilteredEateryList().size()));
+
     }
 
     @Override
