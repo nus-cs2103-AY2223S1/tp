ac558b69a8fcb62271dd9e16cb1e5f71efe338ba
Bryan Yang <e0772641@u.nus.edu>
2022-10-28 01:20:38 +0800

    Enhance UI for debt list and person list

--- src/main/java/paymelah/ui/PersonListPanel.java
+++ src/main/java/paymelah/ui/PersonListPanel.java
@@ -1,11 +1,19 @@
 package paymelah.ui;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import paymelah.commons.core.LogsCenter;
 import paymelah.model.person.Person;
@@ -18,32 +26,74 @@ public class PersonListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     @FXML
-    private ListView<Person> personListView;
+    private Accordion personListView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
     public PersonListPanel(ObservableList<Person> personList) {
         super(FXML);
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
-    }
-
-    /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
-     */
-    class PersonListViewCell extends ListCell<Person> {
-        @Override
-        protected void updateItem(Person person, boolean empty) {
-            super.updateItem(person, empty);
-
-            if (empty || person == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
-            }
+        List<TitledPane> titledPanes = new ArrayList<TitledPane>();
+        for (Person person : personList) {
+            titledPanes.add(createTitledPane(person, personList));
         }
+        personListView.getPanes().addAll(titledPanes);
+        personList.addListener((ListChangeListener<Person>) c -> {
+            while (c.next()) {
+                if (c.wasReplaced()) {
+                    for (int i = c.getFrom(); i < c.getTo(); ++i) {
+                        titledPanes.remove(i);                        
+                        titledPanes.add(i, createTitledPane(c.getAddedSubList().get(i - c.getFrom()), personList));
+                    }
+                } else if (c.wasAdded()) {
+                    for (Person person : c.getAddedSubList()) {
+                        titledPanes.add(createTitledPane(person, personList));
+                    }
+                } else if (c.wasRemoved()) {
+                    for (int i = c.getFrom(); i < c.getTo(); ++i) {
+                        titledPanes.remove(i);
+                    }
+                }
+            }
+            personListView.getPanes().clear();
+            personListView.getPanes().addAll(titledPanes);
+        });
     }
 
+    private TitledPane createTitledPane(Person person, ObservableList<? extends Person> observableList) {
+        int oneBasedIndex = observableList.indexOf(person) + 1;
+        if (oneBasedIndex == 0) {
+            oneBasedIndex = observableList.size();
+        }
+        TitledPane titledPane = new TitledPane();
+        titledPane.setAlignment(Pos.CENTER);
+
+        // Create HBox to hold our 2 labels
+        HBox contentPane = new HBox();
+        contentPane.setAlignment(Pos.CENTER);
+
+        // Set padding on the left to avoid overlapping TitledPane's expand arrow
+        contentPane.setPadding(new Insets(0, 10, 0, 25));
+
+        // Now, since the TitlePane's graphic node generally has a fixed size, we need to bind our
+        // content pane's width to match the width of the TitledPane. This will account for resizing as well
+        contentPane.minWidthProperty().bind(titledPane.widthProperty());
+
+        // Create a Region to act as a separator for the 2 labels
+        HBox region = new HBox();
+        region.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(region, Priority.ALWAYS);
+
+        // Add our nodes to the contentPane
+        contentPane.getChildren().addAll(
+            new Label(oneBasedIndex + ". " + person.getName().fullName),
+            region,
+            new Label("Total: $" + person.getDebtsAmountAsMoney().toString())
+        );
+
+        // Add the contentPane as the graphic for the TitledPane
+        titledPane.setGraphic(contentPane);
+        titledPane.setContent(new PersonCard(person).personCardPane);
+        return titledPane;
+    }
 }
