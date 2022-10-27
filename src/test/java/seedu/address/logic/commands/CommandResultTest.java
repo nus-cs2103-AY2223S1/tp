574fe9192c380ab6c4a92059340d013c03e0dce9
Haohui Wu <thewuhh@gmail.com>
2022-10-27 22:19:28 +0800

    Fix style

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -15,7 +15,7 @@ public class CommandResultTest {
         // same values -> returns true
         assertTrue(commandResult.equals(new CommandResult("feedback")));
         assertTrue(commandResult.equals(new CommandResult("feedback", false,
-                false, false, false,false)));
+                false, false, false, false)));
 
         // same object -> returns true
         assertTrue(commandResult.equals(commandResult));
