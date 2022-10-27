2f6af615d0383f571f8e392bcb9eb5d8a84389f1
Chua Yu Xuan Jordan <jcyx1999@gmail.com>
2022-10-27 19:02:41 +0800

    Resolve merge conflicts

--- src/main/java/jeryl/fyp/logic/commands/AddStudentCommand.java
+++ src/main/java/jeryl/fyp/logic/commands/AddStudentCommand.java
@@ -20,7 +20,7 @@ public class AddStudentCommand extends Command {
 
     public static final String ALTERNATIVE_COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to the FYP manager. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to the FYP manager.\n"
             + "Parameters: "
             + PREFIX_STUDENT_ID + "STUDENT_ID (should be in format  \"A\" + (7 digits) + (1 letter), e.g. A0123456G) "
             + PREFIX_STUDENT_NAME + "STUDENT_NAME "
