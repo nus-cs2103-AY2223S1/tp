2abd687c1b8bb6c842a3404325de99de051799ae
John Benedict Yan <johnbenedictyan@gmail.com>
2022-10-27 21:32:51 +0800

    Initialised the appointment slot number enumeration

---
+++ src/main/java/seedu/address/model/appointment/enums/AppointmentSlotNumber.java
@@ -0,0 +1,31 @@
+package seedu.address.model.appointment.enums;
+
+import java.time.LocalTime;
+
+/**
+ * Represents the appointment slot number
+ */
+public enum AppointmentSlotNumber {
+    ONE(LocalTime.of(10, 0, 0)),
+    TWO(LocalTime.of(12, 0, 0)),
+    THREE(LocalTime.of(14, 0, 0)),
+    FOUR(LocalTime.of(16, 0, 0));
+
+    private LocalTime time;
+
+    private AppointmentSlotNumber(LocalTime time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the time
+     */
+    public LocalTime getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(this.name(), time.toString());
+    }
+}
