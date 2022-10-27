687e9d9fa3a0a359b618bea75920312113dec12b
Cjun1039 <e0726202@u.nus.edu>
2022-10-28 02:40:19 +0800

    Fix checkstyle issue

--- src/main/java/seedu/travelr/ui/MainWindow.java
+++ src/main/java/seedu/travelr/ui/MainWindow.java
@@ -142,7 +142,6 @@ public class MainWindow extends UiPart<Stage> {
 
         StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
-
         TripsLabeler tripsLabel = new TripsLabeler(tripsIcon);
         tripsTextField.getChildren().add(tripsLabel.getRoot());
 
