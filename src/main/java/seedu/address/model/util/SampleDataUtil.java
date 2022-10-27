ba379d5e5935ff44d7ab07ec665af1b7d5743c5f
bensohh <lok200910@gmail.com>
2022-10-27 17:03:48 +0800

    Update Sample Data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,23 +21,23 @@ public class SampleDataUtil {
     public static Customer[] getSamplePersons() {
         return new Customer[] {
             new Customer(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new BirthdayMonth("1"), new Reward("4200"),
-                getTagSet("friends")),
+                new BirthdayMonth("10"), new Reward("2300"),
+                getTagSet("bronze", "warning")),
             new Customer(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new BirthdayMonth("3"), new Reward("6900"),
-                getTagSet("colleagues", "friends")),
+                new BirthdayMonth("11"), new Reward("6900"),
+                getTagSet("gold")),
             new Customer(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new BirthdayMonth("5"), new Reward("12345"),
-                getTagSet("neighbours")),
+                new BirthdayMonth("10"), new Reward("12345"),
+                getTagSet("diamond")),
             new Customer(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new BirthdayMonth("6"), new Reward("0"),
-                getTagSet("family")),
+                getTagSet("member", "banned")),
             new Customer(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new BirthdayMonth("12"), new Reward("4000"),
-                getTagSet("classmates")),
+                getTagSet("silver")),
             new Customer(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new BirthdayMonth("9"), new Reward("5700"),
-                getTagSet("colleagues"))
+                new BirthdayMonth("9"), new Reward("8000"),
+                getTagSet("platinum"))
         };
     }
 
