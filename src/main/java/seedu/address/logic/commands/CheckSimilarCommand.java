f0f6b2f3afff23d0c8c8f707c9e515316fec975c
John Benedict Yan <johnbenedictyan@gmail.com>
2022-10-28 01:33:48 +0800

    Fix checkstyle issue

--- src/main/java/seedu/address/logic/commands/CheckSimilarCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckSimilarCommand.java
@@ -2,17 +2,15 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Optional;
+
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 
-import java.util.Optional;
-
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Finds and lists all persons in address book whose are similar.
  */
 public class CheckSimilarCommand extends Command {
 
@@ -20,15 +18,15 @@ public class CheckSimilarCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Returns persons that maybe similar";
 
-
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         ObservableList<Person> personList = model.getFilteredPersonList();
         for (int i = 0; i < personList.size(); i++) {
             Person curr = personList.get(i);
-            Optional<Person> similarPerson = personList.stream().filter(x-> x.isSimilarPerson(curr) && !x.isSamePerson(curr)).findAny();
-            if(similarPerson.isPresent()) {
+            Optional<Person> similarPerson = personList.stream()
+                    .filter(x -> x.isSimilarPerson(curr) && !x.isSamePerson(curr)).findAny();
+            if (similarPerson.isPresent()) {
                 model.updateFilteredPersonList(x -> x.isSamePerson(curr) || x.isSamePerson(similarPerson.get()));
                 return new CommandResult(
                         String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
