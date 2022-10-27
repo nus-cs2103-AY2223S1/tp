4d7ebe2f7601585eeb4329d3615ad8aa79a73603
liviamil <liviamil@u.nus.edu>
2022-10-27 14:04:38 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,8 +7,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddAddressCommand;
-import seedu.address.logic.commands.AddToGroupCommand;
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddToGroupCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
