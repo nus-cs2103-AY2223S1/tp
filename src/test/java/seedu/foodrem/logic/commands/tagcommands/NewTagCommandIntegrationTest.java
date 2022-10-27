a31529022f322d8bd97192122226aedb04e30ce4
Tan Yi Xian <>
2022-10-27 20:34:51 +0800

    Modify NewTagCommandIntegrationTest to work with new CommandResult

--- src/test/java/seedu/foodrem/logic/commands/tagcommands/NewTagCommandIntegrationTest.java
+++ src/test/java/seedu/foodrem/logic/commands/tagcommands/NewTagCommandIntegrationTest.java
@@ -4,6 +4,8 @@ import static seedu.foodrem.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.foodrem.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.foodrem.testutil.TypicalFoodRem.getTypicalFoodRem;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +14,14 @@ import seedu.foodrem.model.ModelManager;
 import seedu.foodrem.model.UserPrefs;
 import seedu.foodrem.model.tag.Tag;
 import seedu.foodrem.testutil.TagBuilder;
+import seedu.foodrem.viewmodels.TagsWithMessage;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code AddTagCommand}.
  */
 public class NewTagCommandIntegrationTest {
     private static final String EXPECTED_ERROR_DUPLICATE = "This tag already exists in FoodRem";
-    private static final String EXPECTED_FORMAT_SUCCESS = "New tag added: %1$s";
+    private static final String EXPECTED_FORMAT_SUCCESS = "New tag added:";
 
     private Model model;
 
@@ -34,8 +37,10 @@ public class NewTagCommandIntegrationTest {
         Model expectedModel = new ModelManager(model.getFoodRem(), new UserPrefs());
         expectedModel.addTag(validTag);
 
+        TagsWithMessage expectedResult = new TagsWithMessage(List.of(validTag) , EXPECTED_FORMAT_SUCCESS);
+
         assertCommandSuccess(new NewTagCommand(validTag), model,
-                String.format(EXPECTED_FORMAT_SUCCESS, validTag), expectedModel);
+                             expectedResult, expectedModel);
     }
 
     @Test
