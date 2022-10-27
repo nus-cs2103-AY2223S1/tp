0d51b4dc5f3c28468d044833aadce51b1180ffe8
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-10-28 00:39:54 +0800

    Update Sample Util

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,15 +1,22 @@
 package seedu.address.model.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import seedu.address.logic.commands.schedule.AddScheduleCommand;
 import seedu.address.model.ProfNus;
 import seedu.address.model.ReadOnlyProfNus;
 import seedu.address.model.module.Module;
 import seedu.address.model.module.ModuleCode;
 import seedu.address.model.module.ModuleDescription;
 import seedu.address.model.module.ModuleName;
+import seedu.address.model.module.schedule.ClassType;
+import seedu.address.model.module.schedule.Schedule;
+import seedu.address.model.module.schedule.Venue;
+import seedu.address.model.module.schedule.Weekdays;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -28,37 +35,37 @@ public class SampleDataUtil {
         return new Person[] {
             new Student(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Address("Blk 30 Geylang Street 29, #06-40"),
-                    getTagSet("friends"),
+                    getTagSet("attentive"),
                     new StudentId("A0123457W"), new TelegramHandle("@good_user"),
                     getModuleCodeSet("CS2040S", "CS2030S"), getModuleCodeSet("CS1101S"),
                     getClassGroups("CS2030S:Tut07")),
             new Student(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    getTagSet("colleagues", "friends"),
+                    getTagSet("good"),
                     new StudentId("A0123627W"), new TelegramHandle("@good_1234"),
                     getModuleCodeSet("CS2030S"), getModuleCodeSet("CS1101S"),
                     getClassGroups("CS1101S:Tut02")),
             new Student(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    getTagSet("neighbours"),
+                    getTagSet("lazy"),
                     new StudentId("A0523457W"), new TelegramHandle("@good_1234r"),
                     getModuleCodeSet("CS2030S"), getModuleCodeSet("CS1101S"),
                     getClassGroups("CS1101S:Tut03")),
             new Student(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                     new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("family"),
+                    getTagSet("hardworking"),
                     new StudentId("A0125357W"), new TelegramHandle("@1241_user"),
                     getModuleCodeSet("CS2030S"), getModuleCodeSet("CS1101S"),
                     getClassGroups("CS1101S:Rec09")),
             new Student(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                     new Address("Blk 47 Tampines Street 20, #17-35"),
-                    getTagSet("classmates"),
+                    getTagSet("patient"),
                     new StudentId("A0897457W"), new TelegramHandle("@bad_user"),
                     getModuleCodeSet("CS2030S"), getModuleCodeSet("CS1101S"),
                     getClassGroups("CS1101S:Tut07")),
             new Student(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                     new Address("Blk 45 Aljunied Street 85, #11-31"),
-                    getTagSet("colleagues"),
+                    getTagSet("late"),
                     new StudentId("A0123234K"), new TelegramHandle("@new_user"),
                     getModuleCodeSet("CS2030S"), getModuleCodeSet("CS1101S"),
                     getClassGroups("CS1101S:Tut07"))
@@ -87,9 +94,22 @@ public class SampleDataUtil {
                 }
             }
         }
-        for (Module sampleModule: getSampleModules()) {
-            sampleAb.addModule(sampleModule);
+        Module[] moduleArray = getSampleModules();
+        List<Weekdays> weekdaysArrayList = Arrays.asList(Weekdays.Monday, Weekdays.Tuesday, Weekdays.Wednesday,
+                Weekdays.Thursday, Weekdays.Friday, Weekdays.Saturday, Weekdays.Sunday);
+        List<ClassType> classTypes = Arrays.asList(ClassType.Lab, ClassType.Lecture, ClassType.Tutorial,
+                ClassType.Reflection);
+
+        for (int i = 0; i < moduleArray.length; i++) {
+            sampleAb.addModule(moduleArray[i]);
+            moduleArray[i].addSchedule(new Schedule(moduleArray[i].getCode().fullCode,
+                    new Venue("School"),
+                    weekdaysArrayList.get(i%7), "10:00", "12:00",
+                    classTypes.get(i%4), moduleArray[i].getCode().fullCode+ ":" + classTypes.get(i%4)));
+
         }
+
+
         return sampleAb;
     }
 
