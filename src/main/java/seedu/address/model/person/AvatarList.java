aa914a1e23fd6726eb1dce41d8b77b1d9c15a40b
Justin Cheng <justcheng2000@gmail.com>
2022-10-27 23:46:57 +0800

    Fix bugs

--- src/main/java/seedu/address/model/person/AvatarList.java
+++ src/main/java/seedu/address/model/person/AvatarList.java
@@ -13,31 +13,25 @@ import javafx.scene.image.Image;
  */
 public class AvatarList {
 
-    public static final Image STEVE_IMAGE = new Image(
-            new File("images/steve.png").toURI().toString()
-    );
-
-    public static final Image RANDOM_SKIN_1 = new Image(
-            new File("images/randomSkin1.png").toURI().toString()
-    );
-
-    public static final Image RANDOM_SKIN_2 = new Image(
-            new File("images/randomSkin2.png").toURI().toString()
-    );
-
-    public static final Image RANDOM_SKIN_3 = new Image(
-            new File("images/randomSkin3.png").toURI().toString()
-    );
-
-    public static final Image RANDOM_SKIN_4 = new Image(
-            new File("images/randomSkin4.png").toURI().toString()
-    );
-
-    public static final Image RANDOM_SKIN_5 = new Image(
-            new File("images/randomSkin5.png").toURI().toString()
-    );
-
     public static final Random RANDOM = new Random();
+
+    public final Image steveImage = new Image(getClass().getResource("/images/steve.png")
+            .toString());
+
+    public final Image randomSkin1 = new Image(getClass().getResource("/images/randomSkin1.png")
+            .toString());
+
+    public final Image randomSkin2 = new Image(getClass().getResource("/images/randomSkin2.png")
+            .toString());
+
+    public final Image randomSkin3 = new Image(getClass().getResource("/images/randomSkin3.png")
+            .toString());
+
+    public final Image randomSkin4 = new Image(getClass().getResource("/images/randomSkin4.png")
+            .toString());
+
+    public final Image randomSkin5 = new Image(getClass().getResource("/images/randomSkin5.png")
+            .toString());
     public final List<Image> avatarList;
 
     /**
@@ -45,12 +39,12 @@ public class AvatarList {
      */
     public AvatarList() {
         this.avatarList = new ArrayList<>();
-        avatarList.add(STEVE_IMAGE);
-        avatarList.add(RANDOM_SKIN_1);
-        avatarList.add(RANDOM_SKIN_2);
-        avatarList.add(RANDOM_SKIN_3);
-        avatarList.add(RANDOM_SKIN_4);
-        avatarList.add(RANDOM_SKIN_5);
+        avatarList.add(steveImage);
+        avatarList.add(randomSkin1);
+        avatarList.add(randomSkin2);
+        avatarList.add(randomSkin3);
+        avatarList.add(randomSkin4);
+        avatarList.add(randomSkin5);
     }
     public static Image getRandomImage() {
         AvatarList listOfAvatars = new AvatarList();
