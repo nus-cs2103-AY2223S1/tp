00bcba0f4b39d99cd284d1c26f4e5f3ffbd4183f
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-10-28 04:44:10 +0800

    Fix UI element placing

--- src/main/java/seedu/foodrem/views/StatsView.java
+++ src/main/java/seedu/foodrem/views/StatsView.java
@@ -1,16 +1,12 @@
 package seedu.foodrem.views;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
 import seedu.foodrem.model.item.Item;
 import seedu.foodrem.viewmodels.Stats;
 
@@ -33,39 +29,33 @@ public class StatsView {
 
         // Statistics header on top left
         final Label statsHeader = new Label("FoodRem Statistics");
-        statsHeader.setTextAlignment(TextAlignment.LEFT);
-
-        final VBox col1 = new VBox(statsHeader);
-        col1.setSpacing(SPACING_UNIT);
-        final HBox row1 = new HBox(col1);
-        HBox.setHgrow(col1, Priority.ALWAYS);
-        row1.setSpacing(SPACING_UNIT);
 
         // Section for amount wasted due to expired food
         final Label amountWasted = new Label("Total cost due to food wastage: " + stats.getAmountWasted());
         amountWasted.setWrapText(true);
-        final HBox row2 = new HBox(amountWasted);
         new Separator();
 
         // Section for top 3 most expensive items
-        final Label expensiveItems =
-                new Label("Your most expensive items are: \n" + buildTopThreeMostExpensiveItemsListFrom(stats));
-        expensiveItems.setWrapText(true);
-        final HBox row3 = new HBox(expensiveItems);
+        final Label expensiveItemsLabel =
+                new Label("Your most expensive items are: \n");
+        expensiveItemsLabel.setLineSpacing(SPACING_UNIT);
+        final Region spacer = new Region();
+        final VBox row3 = new VBox(expensiveItemsLabel,
+                spacer,
+                new VBox(buildTopThreeMostExpensiveItemsListFrom(stats)));
         new Separator();
 
         // Combine everything
-        final VBox itemView = new VBox(row1, row2, row3);
+        final VBox itemView = new VBox(statsHeader, amountWasted, row3);
         itemView.setSpacing(SPACING_UNIT);
         return itemView;
     }
 
-    private static String buildTopThreeMostExpensiveItemsListFrom(Stats stats) {
+    private static Node[] buildTopThreeMostExpensiveItemsListFrom(Stats stats) {
         List<Item> expensiveItems = stats.getTopThreeMostExpensiveItems();
-        return IntStream.range(1, expensiveItems.size() + 1)
-                .mapToObj(i -> expensiveItems.get(i - 1))
-                .map(Object::toString)
-                .collect(Collectors.joining("\n"));
+        return expensiveItems.stream()
+                .map(ItemView::from)
+                .toArray(Node[]::new);
     }
 
 }
