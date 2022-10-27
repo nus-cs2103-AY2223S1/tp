04fafe99e5fa9802590b8712d0c8a13fd9d3dfa7
John Benedict Yan <johnbenedictyan@gmail.com>
2022-10-28 01:04:33 +0800

    Initialise checkSimilar command

---
+++ src/main/java/seedu/address/logic/commands/CheckSimilarCommand.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+
+import java.util.Optional;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class CheckSimilarCommand extends Command {
+
+    public static final String COMMAND_WORD = "checkSimilar";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Returns persons that maybe similar";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        ObservableList<Person> personList = model.getFilteredPersonList();
+        for (int i = 0; i < personList.size(); i++) {
+            Person curr = personList.get(i);
+            Optional<Person> similarPerson = personList.stream().filter(x-> x.isSimilarPerson(curr) && !x.isSamePerson(curr)).findAny();
+            if(similarPerson.isPresent()) {
+                model.updateFilteredPersonList(x -> x.isSamePerson(curr) || x.isSamePerson(similarPerson.get()));
+                return new CommandResult(
+                        String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+            }
+        }
+        return new CommandResult(
+                String.format("No similar People Found!"));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
+    }
+}
