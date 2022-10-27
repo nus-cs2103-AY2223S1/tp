92472fd5e8d526470568db82a5b7eee41746d98c
Cjun1039 <e0726202@u.nus.edu>
2022-10-27 18:06:44 +0800

    Fix bug where selectedTrip is no resetted

--- src/main/java/seedu/travelr/logic/commands/SummaryCommand.java
+++ src/main/java/seedu/travelr/logic/commands/SummaryCommand.java
@@ -2,12 +2,14 @@ package seedu.travelr.logic.commands;
 
 import seedu.travelr.logic.commands.exceptions.CommandException;
 import seedu.travelr.model.Model;
+import seedu.travelr.model.trip.TripCompletedPredicate;
 
 public class SummaryCommand extends Command {
 
     public static final String COMMAND_WORD = "summary";
 
-    public static final String SHOWING_SUMMARY_MESSAGE = "Opened summary window.";
+    public static final String SHOWING_SUMMARY_MESSAGE = "Opened summary window. \n" +
+            "Showing all trips and bucketlist in Travelr window";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows the summary of your trips and events.\n"
             + "Example: " + COMMAND_WORD;
@@ -17,6 +19,13 @@ public class SummaryCommand extends Command {
         model.updateFilteredTripList(Model.PREDICATE_SHOW_ALL_TRIPS);
         model.updateFilteredEventList(Model.PREDICATE_SHOW_ALL_EVENTS);
         model.refreshSummaryVariables();
+
+        model.updateFilteredTripList(new TripCompletedPredicate());
+
+        // resets to AllTrips and bucketList
+        model.updateFilteredTripList(Model.PREDICATE_SHOW_ALL_TRIPS);
+        model.updateFilteredEventList(model.getBucketPredicate());
+        model.resetSelectedTrip();
 
         return new CommandResult(SHOWING_SUMMARY_MESSAGE, false, false, true);
     }
