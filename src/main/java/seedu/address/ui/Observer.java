857cfc50fe133d74d78f6219ed0337158e2865ed
Shaune Ang <shauneangjx@gmail.com>
2022-10-27 23:48:15 +0800

    Fix CI Checks

--- src/main/java/seedu/address/ui/Observer.java
+++ src/main/java/seedu/address/ui/Observer.java
@@ -5,6 +5,7 @@ import seedu.address.model.person.Person;
 public interface Observer {
     /**
      * Updates UI when model changes.
+     * @param Person person that was updated
      */
     void updateUi(Person updatedPerson);
 }
