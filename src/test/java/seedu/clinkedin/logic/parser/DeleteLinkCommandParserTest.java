3c1a69df55e319e7cc3b6417151a45060236eb06
Ying Ting <95749518+ytingtan@users.noreply.github.com>
2022-10-28 06:11:58 +0800

    Add tests

--- src/test/java/seedu/clinkedin/logic/parser/DeleteLinkCommandParserTest.java
+++ src/test/java/seedu/clinkedin/logic/parser/DeleteLinkCommandParserTest.java
@@ -7,27 +7,27 @@ import static seedu.clinkedin.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.clinkedin.logic.commands.DeleteRateCommand;
+import seedu.clinkedin.logic.commands.DeleteNoteCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteRateCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteRateCommand, and therefore we test only one of them.
+ * outside of the DeleteLinkCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteLinkCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class DeleteRateCommandParserTest {
+public class DeleteLinkCommandParserTest {
 
-    private DeleteRateCommandParser parser = new DeleteRateCommandParser();
+    private DeleteLinkCommandParser parser = new DeleteLinkCommandParser();
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteRateCommand(INDEX_FIRST_PERSON));
+        assertParseSuccess(parser, "1", new DeleteNoteCommand(INDEX_FIRST_PERSON));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteRateCommand.MESSAGE_USAGE));
+                DeleteNoteCommand.MESSAGE_USAGE));
     }
 }
--- src/test/java/seedu/clinkedin/logic/parser/DeleteNoteCommandParserTest.java
+++ src/test/java/seedu/clinkedin/logic/parser/DeleteNoteCommandParserTest.java
@@ -7,12 +7,12 @@ import static seedu.clinkedin.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.clinkedin.logic.commands.DeleteRateCommand;
+import seedu.clinkedin.logic.commands.DeleteNoteCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
+ * outside of the DeleteNoteCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteNoteCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
