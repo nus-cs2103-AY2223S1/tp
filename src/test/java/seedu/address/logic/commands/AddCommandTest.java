a93ac2f3a459c512da0c430284317a634cf939bd
ElginL <elginlee_2000@hotmail.com>
2022-10-27 23:38:36 +0800

    Update test files to use AddCommand new constructor

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -27,7 +27,7 @@ public class AddCommandTest {
 
     @Test
     public void constructor_nullClient_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new AddCommand(null));
+        assertThrows(NullPointerException.class, () -> new AddCommand(null, ""));
     }
 
     @Test
@@ -35,7 +35,7 @@ public class AddCommandTest {
         ModelStubAcceptingClientAdded modelStub = new ModelStubAcceptingClientAdded();
         Client validClient = new ClientBuilder().build();
 
-        CommandResult commandResult = new AddCommand(validClient).execute(modelStub);
+        CommandResult commandResult = new AddCommand(validClient, "").execute(modelStub);
 
         assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validClient), commandResult.getFeedbackToUser());
         assertEquals(Arrays.asList(validClient), modelStub.clientsAdded);
@@ -44,7 +44,7 @@ public class AddCommandTest {
     @Test
     public void execute_duplicateClient_throwsCommandException() {
         Client validClient = new ClientBuilder().build();
-        AddCommand addCommand = new AddCommand(validClient);
+        AddCommand addCommand = new AddCommand(validClient, "");
         ModelStub modelStub = new ModelStubWithClient(validClient);
 
         assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_CLIENT, () -> addCommand.execute(modelStub));
@@ -54,14 +54,14 @@ public class AddCommandTest {
     public void equals() {
         Client alice = new ClientBuilder().withName("Alice").build();
         Client bob = new ClientBuilder().withName("Bob").build();
-        AddCommand addAliceCommand = new AddCommand(alice);
-        AddCommand addBobCommand = new AddCommand(bob);
+        AddCommand addAliceCommand = new AddCommand(alice, "");
+        AddCommand addBobCommand = new AddCommand(bob, "");
 
         // same object -> returns true
         assertTrue(addAliceCommand.equals(addAliceCommand));
 
         // same values -> returns true
-        AddCommand addAliceCommandCopy = new AddCommand(alice);
+        AddCommand addAliceCommandCopy = new AddCommand(alice, "");
         assertTrue(addAliceCommand.equals(addAliceCommandCopy));
 
         // different types -> returns false
