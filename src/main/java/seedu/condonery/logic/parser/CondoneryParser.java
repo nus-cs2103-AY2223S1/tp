937e8b896c177809b9c3555e031b67aa0853bf79
yuehernkang <yuehernkang@hotmail.com>
2022-10-28 08:50:25 +0800

    Fix checkstyle errors

--- src/main/java/seedu/condonery/logic/parser/CondoneryParser.java
+++ src/main/java/seedu/condonery/logic/parser/CondoneryParser.java
@@ -24,8 +24,8 @@ import seedu.condonery.logic.commands.property.FilterPropertyCommand;
 import seedu.condonery.logic.commands.property.FindPropertyCommand;
 import seedu.condonery.logic.commands.property.ListPropertyCommand;
 import seedu.condonery.logic.commands.property.RangePropertyCommand;
-import seedu.condonery.logic.commands.property.TypePropertyCommand;
 import seedu.condonery.logic.commands.property.SelectPropertyCommand;
+import seedu.condonery.logic.commands.property.TypePropertyCommand;
 import seedu.condonery.logic.parser.client.AddClientCommandParser;
 import seedu.condonery.logic.parser.client.FilterClientCommandParser;
 import seedu.condonery.logic.parser.client.FindClientCommandParser;
@@ -37,8 +37,8 @@ import seedu.condonery.logic.parser.property.EditPropertyCommandParser;
 import seedu.condonery.logic.parser.property.FilterPropertyCommandParser;
 import seedu.condonery.logic.parser.property.FindPropertyCommandParser;
 import seedu.condonery.logic.parser.property.RangePropertyCommandParser;
-import seedu.condonery.logic.parser.property.TypePropertyCommandParser;
 import seedu.condonery.logic.parser.property.SelectPropertyCommandParser;
+import seedu.condonery.logic.parser.property.TypePropertyCommandParser;
 
 /**
  * Parses user input.
