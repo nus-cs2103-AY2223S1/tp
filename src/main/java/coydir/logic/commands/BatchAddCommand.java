d023b5d926c5aab3aad563b57cff39288960d401
Prittam Ravi <prittam867@gmail.com>
2022-10-28 12:59:28 +0800

    Remove print

--- src/main/java/coydir/logic/commands/BatchAddCommand.java
+++ src/main/java/coydir/logic/commands/BatchAddCommand.java
@@ -68,7 +68,6 @@ public class BatchAddCommand extends Command {
                         arg += PREFIX_LIST[i] + data[i] + " ";
                     }
                 }
-                System.out.println(arg);
                 addCommandList.add(new AddCommandParser().parse(arg));
             }
         } catch (FileNotFoundException e) {
