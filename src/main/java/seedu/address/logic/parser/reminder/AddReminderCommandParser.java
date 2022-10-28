9db930f4ae472b2839da95a54e3c20f685690f59
yeoyujie <yeo.yujie@gmail.com>
2022-10-28 09:41:30 +0800

    Fix minor error messages

--- src/main/java/seedu/address/logic/parser/reminder/AddReminderCommandParser.java
+++ src/main/java/seedu/address/logic/parser/reminder/AddReminderCommandParser.java
@@ -36,7 +36,7 @@ public class AddReminderCommandParser implements Parser<AddReminderCommand> {
                         PREFIX_PRIORITY);
 
         ParserUtil.assertAllPrefixesPresent(argMultimap, AddReminderCommand.MESSAGE_USAGE,
-                PREFIX_NAME, PREFIX_TIME, PREFIX_DESCRIPTION, PREFIX_PRIORITY);
+                PREFIX_NAME, PREFIX_TIME, PREFIX_DATE_DAY, PREFIX_DESCRIPTION, PREFIX_PRIORITY);
         if (!argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddReminderCommand.MESSAGE_USAGE));
         }
