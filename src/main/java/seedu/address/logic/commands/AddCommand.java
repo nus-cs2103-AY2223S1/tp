ecb7765878967c4e798953bbcbfd6ff3eb97fb0a
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-10-29 16:00:26 +0800

    Add missing space in example

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -33,7 +33,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_REMARK + "loves fishing"
+            + PREFIX_REMARK + "loves fishing "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
