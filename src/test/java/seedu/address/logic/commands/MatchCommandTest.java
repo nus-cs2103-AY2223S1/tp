b0092955f80e37f4ce40501aab4849ff6326efa0
Coco <chuaziyifaith@gmail.com>
2022-10-29 14:45:32 +0800

    Update MatchCommandTest

--- src/test/java/seedu/address/logic/commands/MatchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MatchCommandTest.java
@@ -1,12 +1,38 @@
 package seedu.address.logic.commands;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.order.Order;
+import seedu.address.testutil.TypicalOrders;
+import seedu.address.testutil.TypicalPets;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 public class MatchCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(TypicalPets.getTypicalPetsAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(TypicalPets.getTypicalPetsAddressBook(), new UserPrefs());
+        List<Order> orders = TypicalOrders.getTypicalOrders();
+
+        for (Order order : orders) {
+            model.addOrder(order);
+            expectedModel.addOrder(order);
+        }
+    }
+
     @Test
     public void equals() {
         MatchCommand firstCommand = new MatchCommand(INDEX_FIRST);
@@ -27,5 +53,13 @@ public class MatchCommandTest {
 
         // different indexes -> return false
         assertFalse(firstCommand.equals(secondCommand));
+    }
+
+    @Test
+    public void execute() {
+        //Index is 1
+        MatchCommand command = new MatchCommand(INDEX_FIRST);
+        CommandResult commandResult = new CommandResult(MatchCommand.MESSAGE_SUCCESS);
+        assertCommandSuccess(command, model, commandResult, expectedModel);
     }
 }
