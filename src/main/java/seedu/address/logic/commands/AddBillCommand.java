4e4d7bba26ca4d3132cf7c68f3e2697f4802b827
guokweijie <guokweijie@gmail.com>
2022-10-29 23:20:47 +0800

    Bug fix

--- src/main/java/seedu/address/logic/commands/AddBillCommand.java
+++ src/main/java/seedu/address/logic/commands/AddBillCommand.java
@@ -27,7 +27,7 @@ public class AddBillCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Adds a bill that corresponds to an appointment to HealthContact.\n"
-            + "Parameters: INDEX_OF_APPOINTMENT (must be a positive integer)"
+            + "Parameters: INDEX_OF_APPOINTMENT (must be a positive integer) "
             + PREFIX_BILL_DATE + " DATE<yyyy-MM-dd> "
             + PREFIX_AMOUNT + " AMOUNT\n"
             + "Example: " + COMMAND_WORD + " 1 "
