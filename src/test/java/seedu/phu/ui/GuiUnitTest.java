13d20d3daf8f702d78c9cf0531e1bf37612965a2
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-10-29 11:27:27 +0800

    Setup classes required for ui testing

---
+++ src/test/java/seedu/phu/ui/GuiUnitTest.java
@@ -0,0 +1,41 @@
+package seedu.phu.ui;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testfx.api.FxRobot;
+
+import javafx.scene.Node;
+
+/**
+ * A GUI unit test class for InternshipBook.
+ *
+ * Reused from https://github.com/se-edu/addressbook-level4 with minor modifications.
+ *
+ * @@author se-edu
+ */
+public abstract class GuiUnitTest {
+    // TODO: Remove this workaround after using JavaFX version 13 or above
+    // This is a workaround to solve headless test failure on Windows OS
+    // Refer to https://github.com/javafxports/openjdk-jfx/issues/66 for more details.
+    static {
+        if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
+            System.loadLibrary("WindowsCodecs");
+        }
+    }
+
+    @RegisterExtension
+    public final UiPartExtension uiPartExtension = new UiPartExtension();
+
+    protected final FxRobot robot = new FxRobot();
+
+    /**
+     * Retrieves the {@code query} node owned by the {@code rootNode}.
+     *
+     * @param query name of the CSS selector of the node to retrieve.
+     */
+    protected <T extends Node> T getChildNode(Node rootNode, String query) {
+        Optional<T> node = robot.from(rootNode).lookup(query).tryQuery();
+        return node.orElse(null);
+    }
+}
---
+++ src/test/java/seedu/phu/ui/UiPartExtension.java
@@ -0,0 +1,45 @@
+package seedu.phu.ui;
+
+import java.util.concurrent.TimeoutException;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testfx.api.FxToolkit;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * Ui Part extension to handle GUI test cases.
+ *
+ * Reused from https://github.com/se-edu/addressbook-level4 with minor modifications.
+ *
+ * @@author se-edu
+ */
+public class UiPartExtension implements AfterEachCallback, BeforeEachCallback {
+    private static final String[] CSS_FILES = {"view/DarkTheme.css", "view/Extensions.css"};
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        FxToolkit.registerPrimaryStage();
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        FxToolkit.cleanupStages();
+    }
+
+    public void setUiPart(final UiPart<? extends Parent> uiPart) {
+        try {
+            FxToolkit.setupScene(() -> {
+                Scene scene = new Scene(uiPart.getRoot());
+                scene.getStylesheets().setAll(CSS_FILES);
+                return scene;
+            });
+            FxToolkit.showStage();
+        } catch (TimeoutException error) {
+            throw new AssertionError("Timeout should not happen.", error);
+        }
+    }
+}
