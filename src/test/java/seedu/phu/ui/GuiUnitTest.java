6b67191b72d895c348bc31a20aa5e644d4ecb598
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-10-29 17:08:30 +0800

    Set GUI testing to headless

--- src/test/java/seedu/phu/ui/GuiUnitTest.java
+++ src/test/java/seedu/phu/ui/GuiUnitTest.java
@@ -22,6 +22,8 @@ public abstract class GuiUnitTest {
         if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
             System.loadLibrary("WindowsCodecs");
         }
+
+        setHeadlessModeToTrue();
     }
 
     @RegisterExtension
@@ -37,5 +39,13 @@ public abstract class GuiUnitTest {
     protected <T extends Node> T getChildNode(Node rootNode, String query) {
         Optional<T> node = robot.from(rootNode).lookup(query).tryQuery();
         return node.orElse(null);
+    }
+
+    private static void setHeadlessModeToTrue() {
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("java.awt.headless", "true");
     }
 }
