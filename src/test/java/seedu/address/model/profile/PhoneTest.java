032792541faaedfe0783a5ad108fbd504745b0d9
Cindy <cindyshen336@gmail.com>
2022-10-29 20:42:30 +0800

    Add more tests for Attendees

--- src/test/java/seedu/address/model/profile/PhoneTest.java
+++ src/test/java/seedu/address/model/profile/PhoneTest.java
@@ -29,6 +29,7 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("")); // empty string
         assertFalse(Phone.isValidPhone(" ")); // spaces only
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
+        assertFalse(Phone.isValidPhone("1234567890123456")); // longer than 15 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
@@ -36,7 +37,7 @@ public class PhoneTest {
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
         assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+        assertTrue(Phone.isValidPhone("124293842033123")); // exactly 15 numbers
     }
 
     @Test
