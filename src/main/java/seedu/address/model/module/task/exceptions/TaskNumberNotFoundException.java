59d0d5005acc8e90e12d1578d4fdd8cc201e3a89
teoyuqi <teo.yuqi@gmail.com>
2022-10-30 03:35:13 +0800

    Remove unused code

--- src/main/java/seedu/address/model/module/task/exceptions/TaskNumberNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.module.task.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified task number.
- */
-public class TaskNumberNotFoundException extends RuntimeException {}
