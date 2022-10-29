ffff3147f6e9ced944cb79cde8904ee359f02002
Coco <chuaziyifaith@gmail.com>
2022-10-30 01:45:47 +0800

    Update AddPetCommandParserTet

--- src/test/java/seedu/address/logic/parser/AddPetCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddPetCommandParserTest.java
@@ -1,17 +1,12 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.MatchCommand;
-import seedu.address.logic.commands.addcommands.AddPetCommand;
-import seedu.address.logic.parser.addcommandparser.AddPetCommandParser;
-import seedu.address.model.pet.Pet;
-import seedu.address.testutil.PetBuilder;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.addcommands.AddPetCommand;
+import seedu.address.logic.parser.addcommandparser.AddPetCommandParser;
 
 public class AddPetCommandParserTest {
     private AddPetCommandParser parser = new AddPetCommandParser();
