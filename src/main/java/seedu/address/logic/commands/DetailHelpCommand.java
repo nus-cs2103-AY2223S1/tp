f5108ada538cc9406d3156ce9e82af94d67b3943
Tex Tang <tex_tang@outlook.com>
2022-10-29 14:27:33 +0800

    Command: Remove unused import

--- src/main/java/seedu/address/logic/commands/DetailHelpCommand.java
+++ src/main/java/seedu/address/logic/commands/DetailHelpCommand.java
@@ -3,8 +3,6 @@ package seedu.address.logic.commands;
 import seedu.address.model.Model;
 import seedu.address.ui.MainPanelName;
 
-import java.util.Set;
-
 /**
  * Format full help instructions for every command for display.
  */
