898753aac4b5456f71a5d705a4098d6c2835bb7e
Cindy <cindyshen336@gmail.com>
2022-10-29 22:23:15 +0800

    Update UG for phone number limit

--- src/test/java/seedu/address/model/ModelStub.java
+++ src/test/java/seedu/address/model/ModelStub.java
@@ -11,7 +11,7 @@ import seedu.address.model.profile.EventsAttending;
 import seedu.address.model.profile.Profile;
 
 /**
- * A default model stub that have all of the methods failing.
+ * A default model stub that has all of the methods failing.
  */
 public class ModelStub implements Model {
     @Override
