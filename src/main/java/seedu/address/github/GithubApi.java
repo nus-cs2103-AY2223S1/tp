9ea6b787f3a2580711d132add5cc07c80bbc3c05
arnav-ag <arnav.ctf@gmail.com>
2022-10-29 16:27:51 +0800

    Fix handling of API errors

--- src/main/java/seedu/address/github/GithubApi.java
+++ src/main/java/seedu/address/github/GithubApi.java
@@ -8,7 +8,6 @@ import kong.unirest.HttpResponse;
 import kong.unirest.Interceptor;
 import kong.unirest.UnirestInstance;
 import seedu.address.github.exceptions.NetworkConnectionException;
-import seedu.address.github.exceptions.RepoNotFoundException;
 import seedu.address.github.exceptions.UserInvalidException;
 import seedu.address.model.person.github.User;
 
@@ -27,16 +26,26 @@ public class GithubApi {
         Config config = new Config().connectTimeout(2000)
             .interceptor(new Interceptor() {
                 @Override
-                public void onResponse(HttpResponse<?> response, HttpRequestSummary request, Config config) {
+                public void onResponse(HttpResponse<?> response, HttpRequestSummary request, Config config)
+                        throws UserInvalidException, NetworkConnectionException {
                     if (response.getStatus() == 404) {
                         throw new UserInvalidException(
                             "User does not exist. Please provide an existing GitHub username.");
+                    } else if (response.getStatus() == 403) {
+                        throw new NetworkConnectionException(
+                            "Unable to get user from GitHub as too many calls have been made. Please wait a while "
+                                + "before trying this request again.");
                     }
                 }
 
                 @Override
                 public HttpResponse<?> onFail(Exception e, HttpRequestSummary request, Config config)
-                        throws RepoNotFoundException {
+                        throws UserInvalidException, NetworkConnectionException {
+                    if (e instanceof NetworkConnectionException) {
+                        throw new NetworkConnectionException(e.getMessage());
+                    } else if (e instanceof UserInvalidException) {
+                        throw new UserInvalidException(e.getMessage());
+                    }
                     throw new NetworkConnectionException("Error while getting request, unable to get results.");
                 }
             });
