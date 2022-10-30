b2b2bea6fe835a69c04da89ecd06a5a9b21a9122
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-10-30 16:14:24 +0800

    Fix CI

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -7,10 +7,8 @@ import org.controlsfx.control.textfield.TextFields;
 
 import impl.org.controlsfx.skin.AutoCompletePopup;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
