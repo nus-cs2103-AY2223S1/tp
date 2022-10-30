94ed3370580b30d1037b2e148d6518393932326f
e1010101 <89738860+e1010101@users.noreply.github.com>
2022-10-30 21:09:30 +0800

    Fix #176 and change Tag creation constraints

--- src/main/java/seedu/watson/model/tag/Tag.java
+++ src/main/java/seedu/watson/model/tag/Tag.java
@@ -9,8 +9,7 @@ import static seedu.watson.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be not be blank!";
 
     public final String tagName;
 
@@ -29,7 +28,7 @@ public class Tag {
      * Returns true if a given string is a valid tag name.
      */
     public static boolean isValidTagName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return !test.isBlank();
     }
 
     @Override
