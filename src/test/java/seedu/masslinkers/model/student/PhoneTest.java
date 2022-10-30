2ccffa3c05bc0c04fb94d82c779170fbf18c1a9c
ElijahS67 <e0271260@u.nus.edu>
2022-10-30 23:41:26 +0800

    Edit test cases

--- src/test/java/seedu/masslinkers/model/student/PhoneTest.java
+++ src/test/java/seedu/masslinkers/model/student/PhoneTest.java
@@ -34,14 +34,17 @@ public class PhoneTest {
         assertTrue(Phone.isCorrect("+1234567")); // acceptable international number
         assertTrue(Phone.isCorrect("+12345674534534")); // acceptable international number
 
-        // "incorrect" phone numbers that are actually still considered valid inputs
+        // blank phone numbers which are not correct and are not valid inputs
         assertFalse(Phone.isCorrect("")); // empty string
         assertFalse(Phone.isCorrect(" ")); // spaces only
+
+        // "incorrect" phone numbers that fail the Regex pattern match but are still considered valid inputs
         assertFalse(Phone.isCorrect("91")); // less than 7 digits
-        assertFalse(Phone.isCorrect("93121534444444444444444")); // more than 16 digits
+        assertFalse(Phone.isCorrect("93121534444444444")); // more than 16 characters (length: 17)
+        assertFalse(Phone.isCorrect("+9312153444444444")); // more than 16 digits (length: 17)
+        assertFalse(Phone.isCorrect("+++++999"));
         assertFalse(Phone.isCorrect("this is a phone")); // non-numeric
         assertFalse(Phone.isCorrect("9011p041")); // alphabets within digits
         assertFalse(Phone.isCorrect("9312 1534")); // spaces within digits
-
     }
 }
