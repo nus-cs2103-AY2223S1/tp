2cf81d33355807618ac16f5c644ee7132b47df89
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-10-30 23:56:20 +0800

    Fix error message and fix TLD requirement

--- src/main/java/seedu/phu/model/internship/Email.java
+++ src/main/java/seedu/phu/model/internship/Email.java
@@ -20,9 +20,9 @@ public class Email {
             + "separated by periods.\n"
             + "The domain name must:\n"
             + "    - have at least 2 domain labels, with the last label being the top level domain label.\n"
-            + "    - top level domain label at least 2 characters long.\n"
-            + "    - top level domain label must start with an alphabet.\n"
-            + "    - top level domain label must be separated from the other domain labels by a period.\n"
+            + "    - have the top level domain label at least 2 characters long.\n"
+            + "    - have the top level domain label ends with an alphabet.\n"
+            + "    - have the top level domain label separated from the other domain labels by a period.\n"
             + "    - have each domain label start and end with alphanumeric characters.\n"
             + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.\n"
             + "3. Emails must not exceed 254 characters.";
@@ -35,7 +35,7 @@ public class Email {
             + "(-" + ALPHANUMERIC_NO_UNDERSCORE + ")*";
 
     // At least two chars, first char must be alphabet
-    private static final String DOMAIN_LAST_PART_REGEX = ALPHABET + "(" + DOMAIN_PART_REGEX + "){1,}$";
+    private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + ")+" + ALPHABET + "$";
     private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.){1,}" + DOMAIN_LAST_PART_REGEX;
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
 
