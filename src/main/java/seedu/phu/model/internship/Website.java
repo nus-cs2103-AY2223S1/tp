89265450967dc7b4dc58260f54f4165dcf0f4b6b
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-10-31 00:06:28 +0800

    Fix checkstyle error

--- src/main/java/seedu/phu/model/internship/Website.java
+++ src/main/java/seedu/phu/model/internship/Website.java
@@ -8,22 +8,6 @@ import static seedu.phu.commons.util.AppUtil.checkArgument;
  */
 public class Website {
     public static final int MAX_LENGTH = 2048;
-    private static final String SPECIAL_CHARACTERS = ";,/?:@&=+$-_.!~*'()#";
-    public static final String MESSAGE_CONSTRAINTS =
-            "Web must be of the format PROTOCOL://DOMAIN-NAME/PATH-QUERY-FRAGMENT"
-            + " and adhere to the following constraints:\n"
-            + "1. The protocol must either be \"https\" or \"http\".\n"
-            + "2. The domain name is made up of domain labels separated by periods.\n"
-            + "The domain name must:\n"
-            + "    - have at least 2 domain labels, with the last label being the top level domain label.\n"
-            + "    - have the top level domain label ends with an alphabet.\n"
-            + "    - have the top level domain label at least 2 characters long.\n"
-            + "    - have each domain label start and end with alphanumeric characters.\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.\n"
-            + "3. The path-query-fragment should only contain alphanumeric characters and these special "
-            + "characters, excluding the outer parentheses, ( "+ SPECIAL_CHARACTERS + ").\n"
-            + "4. Web must not exceed 2048 characters.";
-
     public static final String VALIDATION_WITHOUT_PATH_REGEX =
             "^https?://([a-zA-Z0-9]+[-.])+([a-zA-Z0-9]+[a-zA-Z])$";
 
@@ -31,6 +15,22 @@ public class Website {
             "^https?://([a-zA-Z0-9]+[-.])+([a-zA-Z0-9]+[a-zA-Z]/)[-a-zA-Z0-9+;,/?:@&=$_.!~*'()#]*$";
 
     public static final String DEFAULT_VALUE = "NA";
+    private static final String SPECIAL_CHARACTERS = ";,/?:@&=+$-_.!~*'()#";
+    public static final String MESSAGE_CONSTRAINTS =
+            "Website must be of the format PROTOCOL://DOMAIN-NAME/PATH-QUERY-FRAGMENT"
+            + " and adhere to the following constraints:\n"
+            + "1. The protocol must either be \"https\" or \"http\".\n"
+            + "2. The domain name is made up of domain labels separated by periods.\n"
+            + "The domain name must:\n"
+            + "    - have at least 2 domain labels, with the last label being the top level domain label.\n"
+            + "    - have the top level domain label ends with an alphabet.\n"
+            + "    - have the top level domain label at least 2 characters long.\n"
+            + "    - have the top level domain label separated from the other domain labels by a period.\n"
+            + "    - have each domain label start and end with alphanumeric characters.\n"
+            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.\n"
+            + "3. The path-query-fragment should only contain alphanumeric characters and these special "
+            + "characters, excluding the outer parentheses, ( " + SPECIAL_CHARACTERS + ").\n"
+            + "4. Website must not exceed 2048 characters.";
 
     public final String value;
 
