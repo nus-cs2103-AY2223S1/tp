bc31986770fd2d19869a9c173353b8c6f39f1778
thamruicong <e0726692@u.nus.edu>
2022-10-30 21:18:49 +0800

    Clear removes all tasks

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.TaskBook;
 import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
@@ -23,9 +24,18 @@ public class ClearCommandTest {
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
-        Model model = new ModelManager(getTypicalAddressBook(), getTypicalTaskBookWithToDos(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTaskBookWithToDos(), new UserPrefs());
+        Model model = new ModelManager(getTypicalAddressBook(), new TaskBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new TaskBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyTaskBook_success() {
+        Model model = new ModelManager(new AddressBook(), getTypicalTaskBookWithToDos(), new UserPrefs());
+        Model expectedModel = new ModelManager(new AddressBook(), getTypicalTaskBookWithToDos(), new UserPrefs());
+        expectedModel.setTaskBook(new TaskBook());
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
