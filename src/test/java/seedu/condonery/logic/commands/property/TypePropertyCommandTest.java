651de574580988a6276bbc84282daca4746b5103
yuehernkang <yuehernkang@hotmail.com>
2022-10-30 23:47:19 +0800

    Fix checkstyle errors

--- src/test/java/seedu/condonery/logic/commands/property/TypePropertyCommandTest.java
+++ src/test/java/seedu/condonery/logic/commands/property/TypePropertyCommandTest.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.condonery.logic.commands.CommandTestUtil;
 import seedu.condonery.model.Model;
 import seedu.condonery.model.ModelManager;
