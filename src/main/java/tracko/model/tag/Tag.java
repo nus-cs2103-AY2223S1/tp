07f40ceb5c6b98b6cebd29d1d13bc006327fe87d
sohenze <e0725494@u.nus.edu>
2022-10-30 11:37:40 +0800

    Update error message and UG to indicate that tag should be 1 word

--- src/main/java/tracko/model/tag/Tag.java
+++ src/main/java/tracko/model/tag/Tag.java
@@ -9,7 +9,7 @@ import static tracko.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric and should be only 1 word";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String tagName;
