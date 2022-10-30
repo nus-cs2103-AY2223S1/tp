7372151eba7ad92e7e4e1f7da47b2314a1b3ca27
desmondyst <desmondyong06@gmail.com>
2022-10-31 00:46:49 +0800

    Update test cases for Mark command and Edit command

--- src/test/java/seedu/address/logic/commands/MarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showInternshipAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_INTERNSHIP;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_INTERNSHIP;
 import static seedu.address.testutil.TypicalInternships.getTypicalFindMyIntern;
@@ -49,6 +50,41 @@ public class MarkCommandTest {
         MarkCommand markCommand = new MarkCommand(outOfBoundIndex, ApplicationStatus.Applied);
 
         assertCommandFailure(markCommand, model, Messages.MESSAGE_INVALID_INTERNSHIP_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showInternshipAtIndex(model, INDEX_FIRST_INTERNSHIP);
+
+        Internship internshipInFilteredList =
+                model.getFilteredInternshipList().get(INDEX_FIRST_INTERNSHIP.getZeroBased());
+        Internship editedInternship =
+                new InternshipBuilder(internshipInFilteredList).withApplicationStatus(ApplicationStatus.Rejected)
+                        .build();
+
+        MarkCommand markCommand = new MarkCommand(INDEX_FIRST_INTERNSHIP, ApplicationStatus.Rejected);
+
+        String expectedMessage = String.format(MarkCommand.MESSAGE_MARK_INTERNSHIP_SUCCESS, editedInternship);
+
+        Model expectedModel = new ModelManager(new FindMyIntern(model.getFindMyIntern()), new UserPrefs());
+        expectedModel.markInternship(model.getFilteredInternshipList().get(0), editedInternship);
+        expectedModel.updateFilteredInternshipList(model.getCurrentPredicate());
+
+        assertCommandSuccess(markCommand, model, expectedMessage, expectedModel);
+    }
+
+
+    @Test
+    public void execute_duplicateInternshipFilteredList_failure() {
+        showInternshipAtIndex(model, INDEX_FIRST_INTERNSHIP);
+
+        // mark the internship with the current applicationStatus
+        Internship internshipInList =
+                model.getFindMyIntern().getInternshipList().get(INDEX_FIRST_INTERNSHIP.getZeroBased());
+        MarkCommand markCommand = new MarkCommand(INDEX_FIRST_INTERNSHIP,
+                internshipInList.getApplicationStatus());
+
+        assertCommandFailure(markCommand, model, MarkCommand.MESSAGE_SAME_APPLICATION_STATUS);
     }
 
     @Test
