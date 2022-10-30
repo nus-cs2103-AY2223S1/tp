b10caac3e3c38fe96e3cca468c2f500c297ddfdb
Coco <chuaziyifaith@gmail.com>
2022-10-30 16:21:19 +0800

    Update JsonAdaptedRequestTest

--- src/test/java/seedu/address/storage/JsonAdaptedPetTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPetTest.java
@@ -1,4 +1,52 @@
 package seedu.address.storage;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.model.person.Supplier;
+import seedu.address.model.pet.PetCertificate;
+import seedu.address.model.tag.Tag;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.TypicalPets;
+import seedu.address.testutil.TypicalSuppliers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.TypicalSuppliers.BENSON;
+
 public class JsonAdaptedPetTest {
+
+    private final String INVALID_PET_NAME = "@shy";
+    private final JsonAdaptedSupplier INVALID_SUPPLIER = new JsonAdaptedSupplier(new PersonBuilder().buildSupplier());
+    private final String INVALID_COLOR = "@quam&rine";
+    private final String INVALID_COLOR_PATTERN = "$tripe$";
+    private final String INVALID_DATE_OF_BIRTH = "2030-16-100";
+    private final String INVALID_SPECIES = "&xolotl!";
+    private final Double INVALID_WEIGHT = -999999999.99999999999;
+    private final Double INVALID_HEIGHT = -0.00000000099994844;
+    private final Double INVALID_PRICE = -8888.674;
+    private static final String INVALID_TAG = "#friend";
+
+    private final String VALID_PET_NAME = TypicalPets.DOJA.getName().toString();
+    private final Supplier VALID_SUPPLIER = TypicalPets.DOJA.getSupplier();
+    private final String VALID_COLOR = TypicalPets.DOJA.getColor().getValue();
+    private final String VALID_COLOR_PATTERN = TypicalPets.DOJA.getColorPattern().getValue();
+    private final String VALID_DATE_OF_BIRTH = TypicalPets.DOJA.getDateOfBirth().toString();
+    private final String VALID_SPECIES = TypicalPets.DOJA.getSpecies().getValue();
+    private final Double VALID_WEIGHT = TypicalPets.DOJA.getWeight().getValue();
+    private final Double VALID_HEIGHT = TypicalPets.DOJA.getHeight().getValue();
+    private final Double VALID_PRICE = TypicalPets.DOJA.getPrice().getPrice();
+    private final List<PetCertificate> VALID_CERTIFICATES = TypicalPets.DOJA.getCertificates().stream()
+                                                                        .collect(Collectors.toList());
+    private final List<Tag> VALID_TAGS = TypicalPets.DOJA.getTags().stream().collect(Collectors.toList());
+
+    /*
+    @Test
+    public void toModelType_validPetDetails_returnsPet() throws Exception {
+        JsonAdaptedPet pet = new JsonAdaptedPet(TypicalPets.DOJA);
+        assertEquals(TypicalPets.DOJA, pet.toModelType());
+    }
+
+     */
+
 }
--- src/test/java/seedu/address/storage/JsonAdaptedRequestTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedRequestTest.java
@@ -1,4 +1,42 @@
 package seedu.address.storage;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.order.Request;
+import seedu.address.model.person.PersonCategory;
+import seedu.address.testutil.TypicalBuyers;
+import seedu.address.testutil.TypicalOrders;
+import seedu.address.testutil.TypicalPets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalBuyers.BENSON;
+
 public class JsonAdaptedRequestTest {
+    private final int INVALID_AGE = -1;
+    private final String INVALID_COLOR = "@qu@m&rine";
+    private final String INVALID_COLOR_PATTERN = "p0lkad0t$";
+    private final String INVALID_SPECIES = "c&pyb&r&";
+
+    private final int VALID_AGE = 1;
+    private final String VALID_COLOR = "white";
+    private final String VALID_COLOR_PATTERN = "random";
+    private final String VALID_SPECIES = "chinchilla";
+
+    @Test
+    public void toModelType_validRequestDetails_returnsRequest() throws Exception {
+        JsonAdaptedRequest request = new JsonAdaptedRequest(TypicalOrders.ORDER_1.getRequest());
+        assertEquals(TypicalOrders.ORDER_1.getRequest(), request.toModelType());
+    }
+
+    /*
+    @Test
+    public void toModelType_invalidRequestCategory_throwsIllegalValueException() {
+        JsonAdaptedRequest buyer = new JsonAdaptedRequest(INVALID_AGE, INVALID_COLOR, INVALID_COLOR_PATTERN,
+                VALID_SPECIES);
+        String expectedMessage = Request.MESSAGE_USAGE;
+        assertThrows(IllegalValueException.class, expectedMessage, Request::toModelType);
+    }
+
+     */
 }
