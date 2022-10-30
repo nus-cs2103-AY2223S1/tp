e203a49bdb6fb9cd3a5af8d71ebd34ea69d4c3ce
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-10-30 17:58:14 +0800

    Set maximum length for email address

--- src/main/java/seedu/phu/model/internship/Email.java
+++ src/main/java/seedu/phu/model/internship/Email.java
@@ -22,7 +22,8 @@ public class Email {
             + "    - top level domain label at least 2 characters long.\n"
             + "    - have each domain label start and end with alphanumeric characters.\n"
             + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.\n"
-            + "    - top level domain label must be separated from the other domain labels by a period.";
+            + "    - top level domain label must be separated from the other domain labels by a period.\n"
+            + "3. Emails must not exceed 254 characters.";
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
     private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
@@ -32,7 +33,7 @@ public class Email {
     private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,}$"; // At least two chars
     private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.){1,}" + DOMAIN_LAST_PART_REGEX;
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
-
+    public static final int MAX_LENGTH = 254;
     public final String value;
 
     /**
@@ -50,7 +51,7 @@ public class Email {
      * Returns if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
-        return test.matches(VALIDATION_REGEX) || test.equals(DEFAULT_VALUE);
+        return (test.matches(VALIDATION_REGEX) && test.length() <= MAX_LENGTH) || test.equals(DEFAULT_VALUE);
     }
 
     @Override
