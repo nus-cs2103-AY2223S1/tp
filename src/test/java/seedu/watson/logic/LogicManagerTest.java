0d9c549ebcb7b016ed0a6c71aef7ef1b12ec24e8
e1010101 <89738860+e1010101@users.noreply.github.com>
2022-10-31 21:20:05 +0800

    Fix test results

--- src/test/java/seedu/watson/logic/LogicManagerTest.java
+++ src/test/java/seedu/watson/logic/LogicManagerTest.java
@@ -82,7 +82,7 @@ public class LogicManagerTest {
         logic = new LogicManager(model, storage);
 
         // Execute add command
-        String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + INDEX_NUMBER_DESC_AMY
+        String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY
                             + PHONE_DESC_AMY + EMAIL_DESC_AMY
                             + ADDRESS_DESC_AMY + STUDENTCLASS_DUMMY;
         Student expectedStudent = new StudentBuilder(AMY).withTags().withRemarks()
