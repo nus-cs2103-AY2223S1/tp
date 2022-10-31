df8610b5f20dac4d68defc39584c12a04b35244b
Yap Zhi Heng <yapzhhg@gmail.com>
2022-10-31 21:34:01 +0800

    Update phone number regex to at least 7 digits

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers should only contain numbers, and it should be at least 7 digits long";
+    public static final String VALIDATION_REGEX = "\\d{7,}"; // shortest IDD in the world is 7 digits
     public final String value;
 
     /**
