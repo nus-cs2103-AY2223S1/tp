a360881636a70d94d2ef2e6c8c4fcc1c137f0f03
shaniceng <ngshanice1@yahoo.com.sg>
2022-10-31 16:12:12 +0800

    Update UG, remove and sort command

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -29,7 +29,7 @@ public class SortCommand extends Command {
         requireNonNull(model);
         model.sort(sortParam);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()), -1);
     }
 
     @Override
