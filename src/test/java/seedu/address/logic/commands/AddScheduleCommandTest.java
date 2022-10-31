9a2a26aae9ef04e842d7ad8dc0784670bdd48582
bryanngzh <bryanngzihao@gmail.com>
2022-10-31 12:23:16 +0800

    Fix test cases

--- src/test/java/seedu/address/logic/commands/AddScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddScheduleCommandTest.java
@@ -200,6 +200,11 @@ public class AddScheduleCommandTest {
         }
 
         @Override
+        public void viewModuleDetails(ModuleCode moduleCode) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
--- src/test/java/seedu/address/logic/commands/AddStuCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddStuCommandTest.java
@@ -216,6 +216,11 @@ public class AddStuCommandTest {
         }
 
         @Override
+        public void viewModuleDetails(ModuleCode moduleCode) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
