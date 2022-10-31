91576ac12a5eacce9b9a4ad551bf7c5601089b1e
Sheryl <sherylkong07@gmail.com>
2022-10-31 12:13:20 +0800

    fix codestyle error

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -6,7 +6,6 @@ import javafx.animation.PauseTransition;
 import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.image.Image;
