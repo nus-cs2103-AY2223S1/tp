f1fbe408e816bb9c903bc6d327b736fc3e0c064a
Justin Cheng <justcheng2000@gmail.com>
2022-11-01 01:07:26 +0800

    Fix CI

--- src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
+++ src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
-import static seedu.address.testutil.TypicalPersons.HOON;
-import static seedu.address.testutil.TypicalPersons.IDA;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.io.IOException;
