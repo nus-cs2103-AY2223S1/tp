74e51b75d7d3d3ee69a002335d5aaec5fa80b7f4
Gowri <gowri.bhat@u.nus.edu>
2022-10-31 23:29:52 +0800

    Add test cases for CommandResult and EditTaskCommandParser

--- src/test/java/seedu/address/logic/parser/EditTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditTaskCommandParserTest.java
@@ -1,13 +1,22 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.DEADLINE_DATE_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.TASK_DESCRIPTION_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.TASK_TITLE_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DEADLINE_DATE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_DESCRIPTION;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_TITLE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_TASK;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditTaskCommand;
+import seedu.address.testutil.EditTaskDescriptorBuilder;
 
 class EditTaskCommandParserTest {
 
@@ -23,6 +32,9 @@ class EditTaskCommandParserTest {
 
         // no field specified
         assertParseFailure(parser, "1", EditTaskCommand.MESSAGE_NOT_EDITED);
+
+        // only one specified
+        assertParseFailure(parser, INDEX_FIRST_TASK + VALID_TASK_TITLE, MESSAGE_INVALID_FORMAT);
 
         // no index and no field specified
         assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
@@ -41,5 +53,30 @@ class EditTaskCommandParserTest {
 
         // invalid prefix being parsed as preamble
         assertParseFailure(parser, "1 a/ string", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_allFieldsSpecifiedToDo_success() {
+        Index targetIndex = INDEX_SECOND_TASK;
+        String userInput = targetIndex.getOneBased() + TASK_TITLE_DESC + TASK_DESCRIPTION_DESC;
+
+        EditTaskCommand.EditTaskDescriptor descriptor = new EditTaskDescriptorBuilder().withTitle(VALID_TASK_TITLE)
+                .withDescription(VALID_TASK_DESCRIPTION).build();
+        EditTaskCommand expectedCommand = new EditTaskCommand(targetIndex, descriptor);
+
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_allFieldsSpecifiedDeadline_success() {
+        Index targetIndex = INDEX_SECOND_TASK;
+        String userInput = targetIndex.getOneBased() + TASK_TITLE_DESC + TASK_DESCRIPTION_DESC
+                + DEADLINE_DATE_DESC;
+
+        EditTaskCommand.EditTaskDescriptor descriptor = new EditTaskDescriptorBuilder().withTitle(VALID_TASK_TITLE)
+                .withDescription(VALID_TASK_DESCRIPTION).withDate(VALID_DEADLINE_DATE).build();
+        EditTaskCommand expectedCommand = new EditTaskCommand(targetIndex, descriptor);
+
+        assertParseSuccess(parser, userInput, expectedCommand);
     }
 }
