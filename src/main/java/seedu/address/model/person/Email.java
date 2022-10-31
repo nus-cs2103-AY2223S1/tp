ce3047c07004d067cba47175dc8b10ebfe77eea4
yewjon <e0726230@u.nus.edu>
2022-10-31 18:02:10 +0800

    Set limit for email length to 320 characters

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -29,7 +29,7 @@ public class Email {
             + "(-" + ALPHANUMERIC_NO_UNDERSCORE + ")*";
     private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,}$"; // At least two chars
     private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)*" + DOMAIN_LAST_PART_REGEX;
-    public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
+    public static final String VALIDATION_REGEX =  LOCAL_PART_REGEX  + "@" + DOMAIN_REGEX;
 
     public final String value;
 
@@ -48,6 +48,9 @@ public class Email {
      * Returns if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
+        if(test.length() > 320) {
+            return false;
+        }
         return test.matches(VALIDATION_REGEX);
     }
 
