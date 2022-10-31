afce9bdb33105979ab6dbbe9aa8def7eff7bfb1b
lzq <li.zhaoqi@dhs.sg>
2022-10-31 21:33:15 +0800

    Removes unused imports

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -11,7 +11,6 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.persons.DeleteCommand;
 import seedu.address.model.Model;
