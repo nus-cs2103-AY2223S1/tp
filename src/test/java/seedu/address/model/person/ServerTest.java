fa765674cf7dc8404f8e1c5c901f6ca2d3dc7862
Justin Cheng <justcheng2000@gmail.com>
2022-10-31 18:19:14 +0800

    Add test cases

---
+++ src/test/java/seedu/address/model/person/ServerTest.java
@@ -0,0 +1,30 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class ServerTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Server(null));
+    }
+
+    @Test
+    public void isValidServerName() {
+        // null Server name
+        assertThrows(NullPointerException.class, () -> Server.isValidServerName(null));
+
+        // Invalid server names
+        assertFalse(Server.isValidServerName("randomMinecraftServer")); // does not include server address
+        assertFalse(Server.isValidServerName("111.222.333")); // does not include server name
+        assertFalse(Server.isValidServerName("server#111.222.333")); // invalid use of # symbol
+
+        // Valid server names
+        assertTrue(Server.isValidServerName("server@111.222.333"));
+        assertTrue(Server.isValidServerName("server_name@server_address"));
+    }
+}
