fe2e5bf5eab4da5ed025dc4ade238d9cf7d8948c
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2022-10-31 15:29:33 +0800

    Add test cases

--- src/test/java/seedu/address/model/internship/LinkTest.java
+++ src/test/java/seedu/address/model/internship/LinkTest.java
@@ -28,11 +28,24 @@ public class LinkTest {
         assertFalse(Link.isValidLink("")); // empty string
         assertFalse(Link.isValidLink(" ")); // spaces only
         assertFalse(Link.isValidLink("google")); // no '.'
+        assertFalse(Link.isValidLink("htt://google.com")); // invalid scheme
         assertFalse(Link.isValidLink("https://www")); // missing domain
+        assertFalse(Link.isValidLink("https://www.example.c")); // domain is below min length (2 chars)
+        assertFalse(Link.isValidLink("https://www.example.comcomcom")); // domain exceeds max length (8 chars)
+        assertFalse(Link.isValidLink("https://www." +
+                "exampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexample" +
+                "exampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexampleexample" +
+                "exampleexampleexampleexampleexample" + ".com")); // hostname exceeds max length (227 chars)
+        assertFalse(Link.isValidLink("https://www.example%.com")); // invalid characters in hostname ("%")
+        assertFalse(Link.isValidLink("https://www.example.com%")); // invalid characters in domain ("%")
 
         // valid links
         assertTrue(Link.isValidLink("google.com")); // without scheme
-        assertTrue(Link.isValidLink("https://www.google.com")); // with scheme
-        assertTrue(Link.isValidLink("https://www.google.com/careers")); // with path
+        assertTrue(Link.isValidLink("https://google.com")); // with scheme
+        assertTrue(Link.isValidLink("https://careers.google.com")); // with domain at the front
+        assertTrue(Link.isValidLink("https://careers.google.com/")); // with "/"
+        assertTrue(Link.isValidLink("https://careers.google.com/students")); // with path
+        assertTrue(Link.isValidLink("https://careers.google.com.sg")); // with multiple domains
+
     }
 }
