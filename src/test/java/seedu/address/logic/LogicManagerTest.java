0aecac42dd666c48e0442f310be8b3a7bbb9cfb3
Dilysss <e0772712@u.nus.edu>
2022-10-31 16:16:05 +0800

    Remove List and Find Commands

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListStuCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
@@ -64,8 +64,8 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() throws Exception {
-        String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        String listStuCommand = ListStuCommand.COMMAND_WORD;
+        assertCommandSuccess(listStuCommand, ListStuCommand.MESSAGE_SUCCESS, model);
     }
 
     @Test
