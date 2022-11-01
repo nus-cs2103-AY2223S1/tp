bba39d7a790a5312e455f6c0f829f9446339676d
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-11-01 17:49:35 +0800

    Update ListTasks to modify filtered task list

--- src/main/java/seedu/address/logic/commands/ListTasksCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTasksCommand.java
@@ -8,17 +8,18 @@ import static seedu.address.logic.parser.CliSyntax.FLAG_INCOMPLETE_TASKS_STR;
 import static seedu.address.logic.parser.CliSyntax.FLAG_INCOMPLETE_TASKS_STR_LONG;
 import static seedu.address.logic.parser.CliSyntax.FLAG_INCOMPLETE_TASK_DESCRIPTION;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
-import static seedu.address.model.team.TaskList.NO_TASKS;
 
 import picocli.CommandLine;
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.team.Task;
 
 
 /**
  * Lists all tasks of the current team.
  */
-@CommandLine.Command(name = "tasks", aliases = {"t"}, mixinStandardHelpOptions = true)
+@CommandLine.Command(name = "tasks", aliases = { "t" }, mixinStandardHelpOptions = true)
 public class ListTasksCommand extends Command {
     public static final String COMMAND_WORD = "list tasks";
 
@@ -29,13 +30,17 @@ public class ListTasksCommand extends Command {
             + FLAG_COMPLETE_TASKS_STR + " (for completed tasks) \n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_LIST_TASK_SUCCESS = "Tasks: \n%1$s";
+    public static final String MESSAGE_LIST_COMPLETE_TASKS_SUCCESS = "Showing all %1$d completed task(s).\n"
+            + "Type `list tasks` to show all tasks again.";
+    public static final String MESSAGE_LIST_INCOMPLETE_TASKS_SUCCESS = "Showing all %1$d incomplete task(s).\n"
+            + "Type `list tasks` to show all tasks again.";
 
-    @CommandLine.Option(names = {FLAG_COMPLETE_TASKS_STR, FLAG_COMPLETE_TASKS_STR_LONG},
+
+    @CommandLine.Option(names = { FLAG_COMPLETE_TASKS_STR, FLAG_COMPLETE_TASKS_STR_LONG },
             description = FLAG_COMPLETE_TASK_DESCRIPTION)
     private boolean hasCompleteFlag;
 
-    @CommandLine.Option(names = {FLAG_INCOMPLETE_TASKS_STR, FLAG_INCOMPLETE_TASKS_STR_LONG}, description =
+    @CommandLine.Option(names = { FLAG_INCOMPLETE_TASKS_STR, FLAG_INCOMPLETE_TASKS_STR_LONG }, description =
             FLAG_INCOMPLETE_TASK_DESCRIPTION)
     private boolean hasIncompleteFlag;
 
@@ -45,20 +50,19 @@ public class ListTasksCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        String tasks = model.getTeam().getTasksAsString();
-        String completedTasks = model.getTeam().getCompletedTasksAsString();
-        String incompleteTasks = model.getTeam().getIncompleteTasksAsString();
-        if (tasks.equals(NO_TASKS)) {
-            return new CommandResult(NO_TASKS);
-        }
 
         if ((hasCompleteFlag == hasIncompleteFlag)) {
             model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
-            return new CommandResult(String.format(MESSAGE_LIST_TASK_SUCCESS, tasks));
+            return new CommandResult(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW,
+                    model.getFilteredTaskList().size()));
         } else if (hasCompleteFlag) {
-            return new CommandResult(String.format(MESSAGE_LIST_TASK_SUCCESS, completedTasks));
+            model.updateFilteredTaskList(Task::isComplete);
+            return new CommandResult(String.format(MESSAGE_LIST_COMPLETE_TASKS_SUCCESS,
+                    model.getFilteredTaskList().size()));
         } else if (hasIncompleteFlag) {
-            return new CommandResult(String.format(MESSAGE_LIST_TASK_SUCCESS, incompleteTasks));
+            model.updateFilteredTaskList((task) -> !task.isComplete());
+            return new CommandResult(String.format(MESSAGE_LIST_INCOMPLETE_TASKS_SUCCESS,
+                    model.getFilteredTaskList().size()));
         }
         return new CommandResult("Invalid command format!\n" + MESSAGE_USAGE);
     }
