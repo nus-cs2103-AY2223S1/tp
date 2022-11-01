b07cd85e45d7ce2854049021989e64c4da74e10f
jia-han <neo.jiahan1234@gmail.com>
2022-11-01 14:54:18 +0800

    Fix module tests

--- src/test/java/tuthub/model/tutor/ModuleTest.java
+++ src/test/java/tuthub/model/tutor/ModuleTest.java
@@ -29,15 +29,21 @@ public class ModuleTest {
         assertFalse(Module.isValidModule(" ")); // spaces only
         assertFalse(Module.isValidModule("^")); // only non-alphanumeric characters
         assertFalse(Module.isValidModule("CS*100")); // contains non-alphanumeric characters
-        assertFalse(Module.isValidModule("C1000")); // less than 2 numbers
-        assertFalse(Module.isValidModule("CSSS1000")); // more than 3 letters
+        assertFalse(Module.isValidModule("C1000")); // less than 2 letter
+        assertFalse(Module.isValidModule("CSSSS1000")); // more than 4 letters
         assertFalse(Module.isValidModule("CS100")); // less than 4 numbers
         assertFalse(Module.isValidModule("CS10000")); // more than 4 numbers
 
         // valid module
         assertTrue(Module.isValidModule("CS2103")); // 2 letters, no letter behind
         assertTrue(Module.isValidModule("CS2103T")); // 2 letters, letter behind
+        assertTrue(Module.isValidModule("CS2103AB")); // 2 letters, 2 letter behind
         assertTrue(Module.isValidModule("ACC1703")); // 3 letters, no letter behind
         assertTrue(Module.isValidModule("ACC1703X")); // 3 letters, letter behind
+        assertTrue(Module.isValidModule("ACC1703AB")); // 2 letters, 2 letter behind
+        assertTrue(Module.isValidModule("GESS1025")); // 4 letters, no letter behind
+        assertTrue(Module.isValidModule("GESS1025A")); // 4 letters, letter behind
+        assertTrue(Module.isValidModule("GESS1025AB")); // 4 letters, 2 letter behind
+
     }
 }
