5e6f74da8d04578202e44d4c6197eacbf7f0e534
teekaytai <90845551+teekaytai@users.noreply.github.com>
2022-11-02 05:33:47 +0800

    Edit InterviewComparator to allow empty interviews

--- src/main/java/seedu/application/model/application/interview/InterviewComparator.java
+++ src/main/java/seedu/application/model/application/interview/InterviewComparator.java
@@ -5,12 +5,18 @@ import java.util.Comparator;
 import seedu.application.model.application.Application;
 
 /**
- * Creates a comparator for application with overriden compare method to compare two applications with their interview
+ * Creates a comparator for application with overridden compare method to compare two applications with their interview
  * fields. Applications are sorted by interview date first then interview time if date is the same.
  */
 public class InterviewComparator implements Comparator<Application> {
     @Override
     public int compare(Application i1, Application i2) {
+        if (!i1.hasInterview()) {
+            return i2.hasInterview() ? 1 : 0;
+        }
+        if (!i2.hasInterview()) {
+            return -1;
+        }
         int compareDate = i1.getInterview().get().getInterviewDate().value
                 .compareTo(i2.getInterview().get().getInterviewDate().value);
         int compareTime = i1.getInterview().get().getInterviewTime().value
