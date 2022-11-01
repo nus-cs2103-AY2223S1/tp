9b551703def1ce572e61d35d329bf7725516de55
shenyihong <shen_yi_hong@outlook.com>
2022-11-01 19:33:25 +0800

    Minor clean up

--- src/main/java/nus/climods/MainApp.java
+++ src/main/java/nus/climods/MainApp.java
@@ -155,7 +155,7 @@ public class MainApp extends Application {
                     + "Using default user prefs");
             initializedPrefs = new UserPrefs();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
+            logger.warning("Problem while reading from the file. Will be starting with an empty user module list");
             initializedPrefs = new UserPrefs();
         }
 
