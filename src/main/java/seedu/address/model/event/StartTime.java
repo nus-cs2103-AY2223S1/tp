3e949a469a9368d399bfc54144a06f342ea5ed54
Qiaoran-M <e0492494@u.nus.edu>
2022-11-01 21:38:22 +0800

    Modify validation method according to PR review

--- src/main/java/seedu/address/model/event/StartTime.java
+++ src/main/java/seedu/address/model/event/StartTime.java
@@ -16,9 +16,9 @@ public class StartTime implements Comparable<StartTime> {
 
     public static final String MESSAGE_FORMAT_CONSTRAINTS = "Start time must be in format: hh:mm";
     public static final String MESSAGE_VALUE_CONSTRAINTS = "%s exceeds the range of valid time value.";
-    private static final DecimalFormat DF = new DecimalFormat("00");
+    private static final DecimalFormat TIME_FORMATTER = new DecimalFormat("00");
+    private static final String VALIDATION_REGEX = "\\d{2}:\\d{2}";
     public final LocalTime time;
-
 
     /**
      * Constructs a {@code StartTime}.
@@ -27,32 +27,35 @@ public class StartTime implements Comparable<StartTime> {
     public StartTime(String startTime) {
         requireNonNull(startTime);
         assert !startTime.isBlank();
-        checkArgument(isValidStartTime(startTime) != 0, MESSAGE_FORMAT_CONSTRAINTS);
-        checkArgument(isValidStartTime(startTime) != -1, String.format(MESSAGE_VALUE_CONSTRAINTS, startTime));
+        checkArgument(isValidStartTime(startTime), MESSAGE_FORMAT_CONSTRAINTS);
+        checkArgument(isValidStartTimeValue(startTime), String.format(MESSAGE_VALUE_CONSTRAINTS, startTime));
         String [] parsedTime = startTime.split(":", 2);
         this.time = LocalTime.of(Integer.parseInt(parsedTime[0]), Integer.parseInt(parsedTime[1]));
     }
 
     /**
-     * Checks if a given string is a valid StartTime input.
-     * @return int value, 0 represent invalid time format, -1 represents invalid time value, 1 represents valid time.
+     * Checks if a given string follows the valid StartTime input format.
+     * @return boolean value.
      */
-    public static int isValidStartTime(String test) {
+    public static boolean isValidStartTime(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    /**
+     * Checks if a given string can be parsed to a valid StartTime value.
+     * @param test string input, it is guaranteed that the format check for test is done before this function.
+     * @return boolean value.
+     */
+    public static boolean isValidStartTimeValue(String test) {
         try {
             String [] parsedTime = test.split(":", 2);
-            Integer formatCheck = Integer.parseInt(parsedTime[0]) + Integer.parseInt(parsedTime[1]);
-            boolean lengthCheck = parsedTime[0].length() == 2 && parsedTime[1].length() == 2;
             LocalTime.of(Integer.parseInt(parsedTime[0]), Integer.parseInt(parsedTime[1]));
-            if (!lengthCheck) {
-                return 0;
-            }
         } catch (DateTimeException e) {
-            return -1;
-        } catch (NumberFormatException e) {
-            return 0;
+            return false;
         }
-        return 1;
+        return true;
     }
+
 
     @Override
     public int compareTo(StartTime t) {
@@ -64,7 +67,8 @@ public class StartTime implements Comparable<StartTime> {
      * @return String
      */
     public String toLogFormat() {
-        return String.format("%s:%s", df.format(this.time.getHour()), df.format(this.time.getMinute()));
+        return String.format("%s:%s", TIME_FORMATTER.format(this.time.getHour()),
+                TIME_FORMATTER.format(this.time.getMinute()));
     }
 
     @Override
