f011d08b76dc4a97699add8d6f7bf82ec391310c
Coco <chuaziyifaith@gmail.com>
2022-11-01 13:24:24 +0800

    looked through some stuff not much changes

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 //import seedu.address.commons.exceptions.IllegalValueException;
 //import seedu.address.commons.util.JsonUtil;
 //import seedu.address.model.AddressBook;
+//import seedu.address.testutil.TypicalBuyers;
 //import seedu.address.testutil.TypicalPersons;
 
 public class JsonSerializableAddressBookTest {
@@ -20,15 +21,22 @@ public class JsonSerializableAddressBookTest {
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
 
+    //added
+    private static final Path TYPICAL_BUYERS_FILE = TEST_DATA_FOLDER.resolve("typicalBuyersAddressBook.json");
+
     //TODO Uncomment this
-    //    @Test
-    //    public void toModelType_typicalPersonsFile_success() throws Exception {
-    //        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
-    //                JsonSerializableAddressBook.class).get();
-    //        AddressBook addressBookFromFile = dataFromFile.toModelType();
-    //        AddressBook typicalPersonsAddressBook = TypicalPersons.getTypicalAddressBook();
-    //        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
-    //    }
+    /*
+    @Test
+    public void toModelType_typicalPersonsFile_success() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_BUYERS_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+        AddressBook typicalPersonsAddressBook = TypicalBuyers.getTypicalBuyerAddressBook();
+        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
+    }
+
+     */
+
 
     //TODO Uncomment this
     //    @Test
