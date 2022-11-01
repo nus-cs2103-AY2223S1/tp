6a487dc89917728331e17aa87aef7fbfb36cadf7
Shaune Ang <shauneangjx@gmail.com>
2022-11-01 21:51:18 +0800

    Clear command throws error for consecutive clear calls

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
@@ -14,15 +15,19 @@ public class ClearCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "clear";
     public static final String COMMAND_SHORTCUT = "cl";
     public static final String MESSAGE_SUCCESS = "FABook has been cleared!";
+    public static final String MESSAGE_EMPTY_ADDRESSBOOK = "FABook is already empty";
     public static final String MESSAGE_UNDO = "FABook has been restored";
     public static final String MESSAGE_REDO = "FABook has been re-cleared!";
 
     private ReadOnlyAddressBook clearedAddressBook;
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         clearedAddressBook = new AddressBook(model.getAddressBook());
+        if (clearedAddressBook.getPersonList().isEmpty()) {
+            throw new CommandException(MESSAGE_EMPTY_ADDRESSBOOK);
+        }
         model.setAddressBook(new AddressBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
