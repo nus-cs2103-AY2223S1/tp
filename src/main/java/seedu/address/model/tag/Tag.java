28e07a10d0fe07564f7386eed0688595ee8b0454
maxng17 <maxngjl@gmail.com>
2022-11-02 01:59:02 +0800

    Fix bugs for tags to allow for multi-word tags but a limit of 50 characters

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -9,8 +9,9 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_CONSTRAINTS =
+            "Tags names should be alphanumeric, with a limit of 50 characters ";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum} ]{1,50}";
 
     public final String tagName;
 
