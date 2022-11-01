e72696c96af12ee2caa80a696066f970b08d1718
qiaoen17 <92434807+qiaoen17@users.noreply.github.com>
2022-11-01 22:30:58 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -18,7 +18,6 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
-import seedu.address.model.student.Response;
 import seedu.address.model.tag.Tag;
 
 public class ParserUtilTest {
