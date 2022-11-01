62617bdd7bf1b171e2b305daecd7aba7cbbc51f3
gnahzeus <zhangyue1689@gmail.com>
2022-11-01 16:22:47 +0800

    Fix addressbook occurrence in duplicate person message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -46,7 +46,7 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "New student added: %1$s";
 
-    public static final String MESSAGE_DUPLICATE_PERSON = "This student already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This student already exists.";
 
     private final Student toAdd;
 
