58df4319e7bdb82e837eba3623914fe6a01044f3
elyseleong <elyse.xuan@gmail.com>
2022-11-02 23:33:58 +0800

    Update DeleteStuCommandParserTest

--- src/test/java/seedu/address/logic/parser/DeleteStuCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteStuCommandParserTest.java
@@ -8,14 +8,9 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.DeleteStuCommand;
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
 public class DeleteStuCommandParserTest {
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteStuCommand.MESSAGE_USAGE);
 
     private DeleteStuCommandParser parser = new DeleteStuCommandParser();
 
@@ -26,6 +21,16 @@ public class DeleteStuCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteStuCommand.MESSAGE_USAGE));
+        // string as index
+        assertParseFailure(parser, "a", MESSAGE_INVALID_FORMAT);
+
+        // blank space as index
+        assertParseFailure(parser, " ", MESSAGE_INVALID_FORMAT);
+
+        // 0 as index
+        assertParseFailure(parser, "0", MESSAGE_INVALID_FORMAT);
+
+        //negative value as index
+        assertParseFailure(parser, "-1", MESSAGE_INVALID_FORMAT);
     }
 }
