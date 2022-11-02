b8b1e88e94e55a1c233364ee300109aba0349878
Lan-Jingbo <lanjingbo114@163.com>
2022-11-02 22:30:03 +0800

    ERROR(dun care)

--- src/main/java/seedu/guest/commons/core/Messages.java
+++ src/main/java/seedu/guest/commons/core/Messages.java
@@ -8,6 +8,6 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_GUEST_DISPLAYED_INDEX = "The guest index provided is invalid";
-    public static final String MESSAGE_GUESTS_LISTED_OVERVIEW = "%1$d guest(s) listed!";
+    public static final String MESSAGE_GUESTS_LISTED_OVERVIEW = "%1$d guests listed!";
 
 }
