d005262fbac9723c2f7ada3905c6a0309fd21f70
Elton Chua <notle1706@gmail.com>
2022-11-02 12:07:02 +0800

    Remove unused imports

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -2,7 +2,6 @@ package seedu.address.model.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
