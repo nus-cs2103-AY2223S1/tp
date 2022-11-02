95375414312d5eb7727cb6a37418ec183bc70a9a
Echomo Xinyu <seanxinyu2@gmail.com>
2022-11-02 23:40:17 +0800

    Add testing for UndoCommand

---
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalStudents.getTypicalTeachersPet;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class UndoCommandTest {
+    private Model model = new ModelManager(getTypicalTeachersPet(), new UserPrefs());
+
+    @Test
+    public void execute_success() {
+        UndoCommand undoCommand = new UndoCommand();
+        String expectedMessage = UndoCommand.MESSAGE_SUCCESS;
+        model.updateTeachersPetHistory();
+        model.updateTeachersPetHistory();
+        ModelManager expectedModel = new ModelManager(getTypicalTeachersPet(), new UserPrefs());
+        assertCommandSuccess(undoCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        assertTrue(new UndoCommand().equals(new UndoCommand()));
+    }
+}
