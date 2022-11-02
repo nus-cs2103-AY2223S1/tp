2423717da8eee670bb04363ed7bd712095123fbd
Phua Li Ting <phualiting8@gmail.com>
2022-11-02 23:43:48 +0800

    Add JUnit tests for ClearAllCommand and ClearTasksCommand classes

--- src/test/java/seedu/address/logic/commands/ClearAllCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearAllCommandTest.java
@@ -2,24 +2,31 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTasks.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 public class ClearAllCommandTest {
     @Test
     public void execute_emptyAddressBook_throwsCommandException() {
         Model model = new ModelManager();
 
-        assertThrows(CommandException.class, ClearAllCommand.MESSAGE_ALL_LISTS_ALREADY_EMPTY,
-                () -> new ClearAllCommand().execute(model));
+        assertThrows(CommandException.class, ClearAllCommand.MESSAGE_ALL_LISTS_ALREADY_EMPTY, () ->
+                new ClearAllCommand().execute(model));
     }
 
-//    @Test
-//    public void execute_nonEmptyAddressBook_success() {
-//        Model model = new ModelManager()
-//    }
+    @Test
+    public void execute_nonEmptyAddressBook_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel.setAddressBook(new AddressBook());
+
+        assertCommandSuccess(new ClearAllCommand(), model, ClearAllCommand.MESSAGE_SUCCESSS, expectedModel);
+    }
 }
