6d7b0dfcf119c4716913b482d6be115a8b3d54b6
Eugene-Ong-W-X <e0725338@u.nus.edu>
2022-11-02 21:32:55 +0800

    Change persons to contacts

--- src/main/java/soconnect/commons/core/Messages.java
+++ src/main/java/soconnect/commons/core/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid!";
     public static final String MESSAGE_INVALID_TODO_DISPLAYED_INDEX = "The todo index provided is invalid!";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d contact(s) listed!";
     public static final String MESSAGE_TODOS_LISTED_OVERVIEW = "%1$d todos listed!";
 
 }
