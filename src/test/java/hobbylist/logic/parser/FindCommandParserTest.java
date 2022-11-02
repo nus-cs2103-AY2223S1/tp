6d8e6474880a0bc7879a0283c0f79e922a0b63dd
Nguyen Quy Duc <quyduc711@gmail.com>
2022-11-03 02:43:27 +0800

    Add more tests for FindCommandParser

--- src/test/java/hobbylist/logic/parser/FindCommandParserTest.java
+++ src/test/java/hobbylist/logic/parser/FindCommandParserTest.java
@@ -23,16 +23,65 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_invalidDateKeyword_throwsParseException() {
+        assertParseFailure(parser, "date/20 02", FindCommand.MESSAGE_INVALID_DATE_VALUE);
+    }
+
+    @Test
+    public void parse_invalidRatingKeyword_throwsParseException() {
+        assertParseFailure(parser, "rate/abc", FindCommand.MESSAGE_INVALID_RATING_VALUE);
+    }
+
+    @Test
+    public void parse_validArgsNoSpecialKeywords_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
-                new FindCommand(new NameOrDescContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")),
+                new FindCommand(new NameOrDescContainsKeywordsPredicate(Arrays.asList("Battle", "Game")),
                         new DateMatchesGivenDatePredicate(""),
                         new RatingMatchesGivenValuePredicate(-1));
-        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+        assertParseSuccess(parser, "Battle Game", expectedFindCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        assertParseSuccess(parser, " \n Battle \n \t Game  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgsWithDateKeywords_returnFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(
+                        new NameOrDescContainsKeywordsPredicate(Arrays.asList("soccer", "Final", "date/2022-12-20")),
+                        new DateMatchesGivenDatePredicate("2022-12-20"),
+                        new RatingMatchesGivenValuePredicate(-1));
+        assertParseSuccess(parser, "soccer Final date/2022-12-20", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n soccer \n \t Final  \t  \t date/2022-12-20   \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgsWithRateKeywords_returnFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(
+                        new NameOrDescContainsKeywordsPredicate(Arrays.asList("soccer", "Final", "rate/4")),
+                        new DateMatchesGivenDatePredicate(""),
+                        new RatingMatchesGivenValuePredicate(4));
+        assertParseSuccess(parser, "soccer Final rate/4", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n soccer \n \t Final  \t  \t rate/4   \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgsAllKeywords_returnFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(
+                        new NameOrDescContainsKeywordsPredicate(Arrays.asList("soccer", "date/2022-01-01", "rate/4")),
+                        new DateMatchesGivenDatePredicate("2022-01-01"),
+                        new RatingMatchesGivenValuePredicate(4));
+        assertParseSuccess(parser, "soccer date/2022-01-01 rate/4", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n soccer \n   date/2022-01-01  \t  \t rate/4   \t", expectedFindCommand);
     }
 
 }
