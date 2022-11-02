8c471e85d2bb748f2b3146a2dde50d289515ddc1
Coco <chuaziyifaith@gmail.com>
2022-11-02 14:09:28 +0800

    Update BuyerTest

--- src/test/java/seedu/address/model/person/BuyerTest.java
+++ src/test/java/seedu/address/model/person/BuyerTest.java
@@ -10,24 +10,39 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.testutil.TypicalBuyers.ALICE;
 import static seedu.address.testutil.TypicalBuyers.BOB;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.UniqueId;
+import seedu.address.model.order.AdditionalRequests;
+import seedu.address.model.order.Order;
+import seedu.address.model.order.Price;
+import seedu.address.model.order.PriceRange;
+import seedu.address.model.order.Request;
+import seedu.address.model.pet.Age;
+import seedu.address.model.pet.Color;
+import seedu.address.model.pet.ColorPattern;
+import seedu.address.model.pet.Species;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.TypicalPersons;
 
 public class BuyerTest {
 
-    //    @Test
-    //    public void addOrder() {
-    //        Buyer buyer = new PersonBuilder().buildBuyer();
-    //        Order order = new Order(buyer, new PriceRange(new Price(50), new Price(100)),
-    //                new Request(new Age(1), new Color("brown"), new ColorPattern("brown and white"),
-    //                        new Species("cat")), new AdditionalRequests("cute"), LocalDate.now(),
-    //                new Price(100));
-    //        buyer.addOrder(order);
-    //        assertTrue(buyer.getOrderIds().size() == 1);
-    //        assertEquals(buyer.getOrderIds(), Arrays.asList(order));
-    //    }
+    @Test
+    public void addOrder() {
+        Buyer buyer = new PersonBuilder().buildBuyer();
+        Order order = new Order(buyer, new PriceRange(new Price(50), new Price(100)), new Request(new Age(1),
+                new Color("brown"), new ColorPattern("brown and white"), new Species("cat")),
+                new AdditionalRequests("cute"), LocalDate.now(), new Price(100));
+        List<UniqueId> expectedList = Arrays.asList(order).stream().map(x -> x.getId()).collect(Collectors.toList());
+        buyer.addOrders(Arrays.asList(order).stream().map(x -> x.getId()).collect(Collectors.toList()));
+        assertTrue(buyer.getOrderIds().size() == 1);
+        assertEquals(buyer.getOrderIds(), expectedList);
+    }
 
     @Test
     public void isSameBuyer() {
