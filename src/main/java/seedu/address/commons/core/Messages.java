e8311e8844292e8bf574cd363d241eb2d6922182
Zsigmond <e0725064@u.nus.edu>
2022-11-02 20:48:49 +0800

    Standardise punctuation for messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,10 +7,10 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_BUYER_DISPLAYED_INDEX = "The buyer index provided is invalid";
-    public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_INDEX = "The property index provided is invalid";
-    public static final String MESSAGE_BUYERS_LISTED_OVERVIEW = "%1$d buyers listed!";
-    public static final String MESSAGE_PROPERTY_LISTED_OVERVIEW = "%1$d properties listed!";
+    public static final String MESSAGE_INVALID_BUYER_DISPLAYED_INDEX = "The buyer index provided is invalid.";
+    public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_INDEX = "The property index provided is invalid.";
+    public static final String MESSAGE_BUYERS_LISTED_OVERVIEW = "%1$d buyers listed.";
+    public static final String MESSAGE_PROPERTY_LISTED_OVERVIEW = "%1$d properties listed.";
     public static final String MESSAGE_INVALID_PROPERTY_BUYER_MATCH =
-            "Cannot match Buyer who has no desired characteristics or price range indicated!";
+            "Cannot match buyer who has no desired characteristics or budget indicated.";
 }
