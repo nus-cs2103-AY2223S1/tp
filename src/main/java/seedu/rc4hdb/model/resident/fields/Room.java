4db840cc027ae6a7fe357675ada2bc2997c3d519
Naren <narensreekanth@gmail.com>
2022-11-02 18:09:37 +0800

    Edit regex for room number

--- src/main/java/seedu/rc4hdb/model/resident/fields/Room.java
+++ src/main/java/seedu/rc4hdb/model/resident/fields/Room.java
@@ -13,13 +13,13 @@ public class Room extends StringField implements ResidentField {
     public static final String IDENTIFIER = "Room";
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Room Number should be in the format FLOOR-UNIT, where FLOOR and UNIT are both positive integers written "
-                   + "in a two digit format e.g. 01-01";
+            "Room Number should be in the format FLOOR-UNIT, where FLOOR and UNIT are both positive integers and FLOOR"
+                    + "should be below 30";
 
     /**
      * The characters before and after the '-' must be a positive integer.
      */
-    public static final String VALIDATION_REGEX = "^[0-9]*[1-9][0-9]*[-][0-9]*[1-9][0-9]*$";
+    public static final String VALIDATION_REGEX = "^[0]*[0-3]?[1-9][-][0-9]*[1-9][0-9]*$";
 
     /**
      * Constructs a {@code Room}.
