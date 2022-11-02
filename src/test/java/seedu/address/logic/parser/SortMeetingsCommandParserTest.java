4f6546c53bb00801140fb32b6bd68d4730025a9c
GenFusion122 <infusion122@hotmail.com>
2022-11-02 09:53:04 +0800

    Create tests for FilterMeeting and SortMeeting and its supporting classes

--- src/test/java/seedu/address/logic/parser/SortMeetingsCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortMeetingsCommandParserTest.java
@@ -1,22 +1,25 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.FindMeetingCommand;
-import seedu.address.logic.commands.SortMeetingsCommand;
-import seedu.address.model.meeting.MeetingContainsKeywordsPredicate;
-
-import java.util.Arrays;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.SortMeetingsCommand;
 
 public class SortMeetingsCommandParserTest {
     private SortMeetingsCommandParser parser = new SortMeetingsCommandParser();
 
     @Test
-    public void parse_emptyArg_throwsParseException() {
+    public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                SortMeetingsCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "invalid", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                SortMeetingsCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "ASc desc", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                SortMeetingsCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "ascc descc", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 SortMeetingsCommand.MESSAGE_USAGE));
     }
     @Test
@@ -24,6 +27,17 @@ public class SortMeetingsCommandParserTest {
         // no leading and trailing whitespaces
         SortMeetingsCommand expectedSortMeetingCommandDescription =
                 new SortMeetingsCommand(true);
-        assertParseSuccess(parser, "sortmeetings asc", expectedSortMeetingCommandDescription);
+        assertParseSuccess(parser, "asc", expectedSortMeetingCommandDescription);
+        // leading/trailing whitespaces
+        assertParseSuccess(parser, " \t  asc \t \n", expectedSortMeetingCommandDescription);
+        // testing case-sensitive
+        assertParseSuccess(parser, " \t  AsC \t \n", expectedSortMeetingCommandDescription);
+
+        // testing descending
+        SortMeetingsCommand expectedSortMeetingCommandDescriptionDesc =
+                new SortMeetingsCommand(false);
+        assertParseSuccess(parser, "desc", expectedSortMeetingCommandDescriptionDesc);
+        assertParseSuccess(parser, "DeSc", expectedSortMeetingCommandDescriptionDesc);
+
     }
 }
