6c5ef37b8e110dbbe3510762a00f025c33d0a712
Coco <chuaziyifaith@gmail.com>
2022-11-03 01:29:51 +0800

    Update ListCommandTest

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -1,7 +1,11 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showBuyerAtIndex;
+import static seedu.address.logic.commands.ListCommand.LIST_BUYER;
+import static seedu.address.logic.commands.ListCommand.LIST_DELIVERER;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -45,8 +49,28 @@ public class ListCommandTest {
     @Test
     public void execute_listBuyer_success() {
         CommandResult expectedCommandResult = CommandResult.createListCommandResult(
-                String.format(ListCommand.MESSAGE_SUCCESS, ListCommand.LIST_BUYER), ListCommand.LIST_BUYER);
-        assertCommandSuccess(new ListCommand(ListCommand.LIST_BUYER), model, expectedCommandResult,
+                String.format(ListCommand.MESSAGE_SUCCESS, LIST_BUYER), LIST_BUYER);
+        assertCommandSuccess(new ListCommand(LIST_BUYER), model, expectedCommandResult,
                 expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        ListCommand firstCommand = new ListCommand(LIST_BUYER);
+        ListCommand secondCommand = new ListCommand(LIST_DELIVERER);
+
+        //same object -> returns true
+        assertEquals(firstCommand, firstCommand);
+
+        //same list -> returns true
+        ListCommand firstCommandCopy = new ListCommand(LIST_BUYER);
+        assertEquals(firstCommand, firstCommandCopy);
+
+        //different types -> returns false
+        assertFalse(firstCommand.equals(1));
+        assertFalse(firstCommand.equals(null));
+
+        //different lists -> return false
+        assertFalse(secondCommand.equals(firstCommand));
     }
 }
