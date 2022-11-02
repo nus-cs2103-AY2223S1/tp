dc39e43bfd37c389380e506ba0ff765f32fb20af
Joseph Aedan Marcus <josephaedan@gmail.com>
2022-11-02 18:59:12 +0800

    Fix checkstyle

--- src/test/java/seedu/clinkedin/logic/commands/AddLinkCommandTest.java
+++ src/test/java/seedu/clinkedin/logic/commands/AddLinkCommandTest.java
@@ -1,10 +1,10 @@
 package seedu.clinkedin.logic.commands;
 
-import static seedu.clinkedin.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.clinkedin.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.clinkedin.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.net.MalformedURLException;
