e6171bcccd1a011082021dcce6b93ed1375c87af
Zsigmond <e0725064@u.nus.edu>
2022-11-02 17:54:51 +0800

    Edit message in ParseException thrown by parseIndex
    the index passed into parseIndex should not be empty, thus edit message
    to match this.

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -28,8 +28,7 @@ import seedu.address.model.property.PropertyName;
  */
 public class ParserUtil {
 
-    public static final String MESSAGE_INVALID_INDEX = "Index is not a positive integer (may be too large)"
-            + " or is unspecified.";
+    public static final String MESSAGE_INVALID_INDEX = "Index is not a positive integer (may be too large)";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
