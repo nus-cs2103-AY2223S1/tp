5a5455d01bf5e685f313397512bde7a2f4e0b781
Nguyen Quy Duc <quyduc711@gmail.com>
2022-11-02 08:43:20 +0800

    Fix checkstyle errors

--- src/main/java/hobbylist/logic/parser/RateAboveCommandParser.java
+++ src/main/java/hobbylist/logic/parser/RateAboveCommandParser.java
@@ -10,8 +10,8 @@ public class RateAboveCommandParser implements Parser<RateAboveCommand> {
     public static final String INPUT_EMPTY = "Input value cannot be empty!";
     public static final String INPUT_FORMAT_WRONG = "Input format should be r/above value!\n"
                     + "The input value should be an integer between 1-5 (both inclusive).";
-    public static final String INPUT_OUT_OF_BOUND = "Input value out of bound!\n" +
-            "The input value should be an integer between 1-5 (both inclusive).";
+    public static final String INPUT_OUT_OF_BOUND = "Input value out of bound!\n"
+            + "The input value should be an integer between 1-5 (both inclusive).";
 
     /**
      * Parses the given {@code String} of arguments in the context of the RateAboveCommand
