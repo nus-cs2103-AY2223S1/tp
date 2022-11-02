818a01fff9541545cbb6b7adde4babc99ca2e1b7
junweimoo <junweimoo@gmail.com>
2022-11-02 12:53:44 +0800

    Update test cases for new error messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,8 +7,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "The client index provided could not be " +
-            "found in the list of clients.";
+    public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "The client index provided could not be "
+            + "found in the list of clients.";
     public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d client listed!";
     public static final String MESSAGE_INVALID_MEETING_DISPLAYED_INDEX = "The meeting index provided is invalid";
     public static final String MESSAGE_INVALID_PRODUCT_DISPLAYED_INDEX = "The product index provided is invalid";
