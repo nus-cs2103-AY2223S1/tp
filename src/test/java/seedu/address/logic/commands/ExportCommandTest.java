5dcc37fb3aca7cfa79f501f10f12948ce87330e4
cliftonfelix <cliftonfelix12@yahoo.com>
2022-11-02 23:47:47 +0800

    Fix wrong JSON format for export

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -207,7 +207,7 @@ public class ExportCommandTest {
         }
 
         @Override
-        public void exportDisplayedList(ObservableList<Person> displayedList, String filePathString)
+        public void exportDisplayedListAddressBook(ReadOnlyAddressBook displayedListAddressBook, String filePathString)
                 throws IOException {
             throw new AssertionError("This method should not be called.");
         }
