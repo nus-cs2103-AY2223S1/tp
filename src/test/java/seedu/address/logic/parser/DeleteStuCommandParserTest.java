253b21f41b8ec93c85d3562e39bf59cb684de982
Dilysss <e0772712@u.nus.edu>
2022-11-03 01:44:57 +0800

    Update ParserUtilTest

--- src/test/java/seedu/address/logic/parser/DeleteStuCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteStuCommandParserTest.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +17,7 @@ public class DeleteStuCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteStuCommand(INDEX_FIRST_PERSON));
+        assertParseSuccess(parser, "1", new DeleteStuCommand(INDEX_FIRST_STUDENT));
     }
 
     @Test
