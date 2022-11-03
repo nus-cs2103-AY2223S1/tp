891cdd76255b5ab44cad1a0fcc4b817f08ea9129
Janelle Loh <janelleloh13@gmail.com>
2022-11-04 00:10:09 +0800

    Fix checkstyle errors

--- src/test/java/tracko/logic/commands/HelpCommandTest.java
+++ src/test/java/tracko/logic/commands/HelpCommandTest.java
@@ -9,13 +9,12 @@ import tracko.model.Model;
 import tracko.model.ModelManager;
 
 public class HelpCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
 
-     private Model model = new ModelManager();
-     private Model expectedModel = new ModelManager();
-
-     @Test
-     public void execute_help_success() {
-         CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
-         assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
-     }
+    @Test
+    public void execute_help_success() {
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    }
 }
