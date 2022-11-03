8b78a727c93db549e6a76dcb3c07567b3e105990
KJunWei <khorjunweiwei@gmail.com>
2022-11-03 17:08:27 +0800

    Edit Documentation

--- src/test/java/seedu/address/logic/commands/AttendanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AttendanceCommandTest.java
@@ -18,7 +18,7 @@ import seedu.address.model.UserPrefs;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
- * {@code DeleteCommand}.
+ * {@code AttendanceCommand}.
  */
 public class AttendanceCommandTest {
 
