3e3929ebfc20f43fc05ae5d3f6904b255cd1db83
Dilysss <e0772712@u.nus.edu>
2022-11-03 16:58:53 +0800

    Edit ParserUtilTest

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -16,22 +16,15 @@ import seedu.address.model.student.Telegram;
 
 public class ParserUtilTest {
     private static final String INVALID_NAME = "R@chel";
-    private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_ADDRESS = " ";
-    private static final String INVALID_EMAIL = "example.com";
-    private static final String INVALID_TAG = "#friend";
-    private static final String INVALID_RESPONSE = "rawr7";
     private static final String INVALID_TELEGRAM = "hello";
+    private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_RESPONSE = "rawr7";
     private static final String INVALID_ATTENDANCE = "hihi";
 
     private static final String VALID_NAME = "Rachel Walker";
-    private static final String VALID_PHONE = "123456";
-    private static final String VALID_ADDRESS = "123 Main Street #0505";
-    private static final String VALID_EMAIL = "rachel@example.com";
-    private static final String VALID_TAG_1 = "friend";
-    private static final String VALID_TAG_2 = "neighbour";
-    private static final String VALID_RESPONSE = "81";
     private static final String VALID_TELEGRAM = "@hello";
+    private static final String VALID_EMAIL = "rachel@example.com";
+    private static final String VALID_RESPONSE = "81";
     private static final String VALID_ATTENDANCE = "10";
 
     private static final String WHITESPACE = " \t\r\n";
@@ -118,33 +111,6 @@ public class ParserUtilTest {
         assertEquals(expectedEmail, ParserUtil.parseStuEmail(emailWithWhitespace));
     }
 
-    // to replace with student class tests
-    /*
-    @Test
-    public void parseAddress_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> ParserUtil.parseAddress((String) null));
-    }
-
-    @Test
-    public void parseAddress_invalidValue_throwsParseException() {
-        assertThrows(ParseException.class, () -> ParserUtil.parseAddress(INVALID_ADDRESS));
-    }
-
-    @Test
-    public void parseAddress_validValueWithoutWhitespace_returnsAddress() throws Exception {
-        Address expectedAddress = new Address(VALID_ADDRESS);
-        assertEquals(expectedAddress, ParserUtil.parseAddress(VALID_ADDRESS));
-    }
-
-    @Test
-    public void parseAddress_validValueWithWhitespace_returnsTrimmedAddress() throws Exception {
-        String addressWithWhitespace = WHITESPACE + VALID_ADDRESS + WHITESPACE;
-        Address expectedAddress = new Address(VALID_ADDRESS);
-        assertEquals(expectedAddress, ParserUtil.parseAddress(addressWithWhitespace));
-    }
-
-    */
-
     @Test
     public void parseResponse_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> ParserUtil.parseResponse(null));
@@ -176,5 +142,32 @@ public class ParserUtilTest {
         Attendance expectedAttendance = new Attendance(VALID_ATTENDANCE);
         assertEquals(expectedAttendance, ParserUtil.parseAttendance(VALID_ATTENDANCE));
     }
+
+    // to replace with student class tests
+    /*
+    @Test
+    public void parseAddress_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseAddress((String) null));
+    }
+
+    @Test
+    public void parseAddress_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseAddress(INVALID_ADDRESS));
+    }
+
+    @Test
+    public void parseAddress_validValueWithoutWhitespace_returnsAddress() throws Exception {
+        Address expectedAddress = new Address(VALID_ADDRESS);
+        assertEquals(expectedAddress, ParserUtil.parseAddress(VALID_ADDRESS));
+    }
+
+    @Test
+    public void parseAddress_validValueWithWhitespace_returnsTrimmedAddress() throws Exception {
+        String addressWithWhitespace = WHITESPACE + VALID_ADDRESS + WHITESPACE;
+        Address expectedAddress = new Address(VALID_ADDRESS);
+        assertEquals(expectedAddress, ParserUtil.parseAddress(addressWithWhitespace));
+    }
+
+    */
 
 }
