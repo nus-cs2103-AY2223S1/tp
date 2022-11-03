c20ffb652d247d77da99377f275555ab1258a548
John Benedict Yan <johnbenedictyan@gmail.com>
2022-11-04 00:33:21 +0800

    Fix checkstyle issue

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -92,9 +92,9 @@ public class EditCommand extends Command {
     public static final String MESSAGE_OUT_OF_BOUND_DATESLOTINDEX = "The dateSlot index given is out of bounds "
             + "of the existing list." + "Please retype another index that is within the range or left it empty.";
 
-    public static final String MESSAGE_INVALID_NUMBERS_OF_UNAVAILABLEDATES_AND_UNAVAILABLEDATESINDEX = "The unavailable date index "
-            + "provided is more than the unavailable date provided."
-            + "Please remove the unavailable date index or add more unavailable date.";
+    public static final String MESSAGE_INVALID_NUMBERS_OF_UNAVAILABLEDATES_AND_UNAVAILABLEDATESINDEX =
+            "The unavailable date index " + "provided is more than the unavailable date provided."
+                + "Please remove the unavailable date index or add more unavailable date.";
 
     public static final String MESSAGE_OUT_OF_BOUND_UNAVAILABLEDATESINDEX = "The unavailable date index "
             + "given is out of bounds of the existing list."
