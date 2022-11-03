529f514b39a1b0025a5ad6f29351d8de078b7574
yunruu <yunruuchin@gmail.com>
2022-11-04 02:02:06 +0800

    Checkstyle: remove unused import

--- src/test/java/swift/model/AddressBookTest.java
+++ src/test/java/swift/model/AddressBookTest.java
@@ -3,7 +3,6 @@ package swift.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static swift.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static swift.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static swift.testutil.Assert.assertThrows;
 import static swift.testutil.TypicalPersons.ALICE;
