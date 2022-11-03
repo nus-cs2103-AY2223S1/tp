a6c9175eb9672824a038f41c8568766ac6c0be81
LimAiLin <59479000+LimAiLin@users.noreply.github.com>
2022-11-04 03:20:09 +0800

    checkstyle

--- src/test/java/seedu/address/model/commons/ModuleTest.java
+++ src/test/java/seedu/address/model/commons/ModuleTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.tutorial.TutorialName;
 
 public class ModuleTest {
     @Test
