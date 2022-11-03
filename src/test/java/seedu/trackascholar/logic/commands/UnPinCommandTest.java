1583b469ba41a2b2dc894d17e2dcb89e6d9f5f05
benjytan45678 <Benjytan45678@gmail.com>
2022-11-03 22:45:43 +0800

    Checkstyle fixes

--- src/test/java/seedu/trackascholar/logic/commands/UnPinCommandTest.java
+++ src/test/java/seedu/trackascholar/logic/commands/UnPinCommandTest.java
@@ -1,6 +1,15 @@
 package seedu.trackascholar.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.trackascholar.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.trackascholar.testutil.TypicalApplicants.ALICE;
+import static seedu.trackascholar.testutil.TypicalApplicants.BENSON;
+import static seedu.trackascholar.testutil.TypicalApplicants.getTypicalTrackAScholar;
+import static seedu.trackascholar.testutil.TypicalIndexes.INDEX_SECOND_APPLICANT;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.trackascholar.model.Model;
 import seedu.trackascholar.model.ModelManager;
 import seedu.trackascholar.model.TrackAScholar;
@@ -8,19 +17,6 @@ import seedu.trackascholar.model.UserPrefs;
 import seedu.trackascholar.model.applicant.Applicant;
 import seedu.trackascholar.model.applicant.Name;
 import seedu.trackascholar.testutil.ApplicantBuilder;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.trackascholar.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.trackascholar.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.trackascholar.logic.commands.CommandTestUtil.showApplicantAtIndex;
-import static seedu.trackascholar.testutil.TypicalApplicants.getTypicalTrackAScholar;
-import static seedu.trackascholar.testutil.TypicalApplicants.ALICE;
-import static seedu.trackascholar.testutil.TypicalApplicants.BENSON;
-import static seedu.trackascholar.testutil.TypicalIndexes.INDEX_FIRST_APPLICANT;
-import static seedu.trackascholar.testutil.TypicalIndexes.INDEX_SECOND_APPLICANT;
-
-
 public class UnPinCommandTest {
     private Model model = new ModelManager(getTypicalTrackAScholar(), new UserPrefs());
     @Test
@@ -45,15 +41,16 @@ public class UnPinCommandTest {
         assertCommandSuccess(unPinCommand, model, expectedMessage, expectedModel);
     }
 
-//    @Test
-//    public void execute_unPinSameApplicantAgainFilteredList_failure() {
-//        showApplicantAtIndex(model, INDEX_FIRST_APPLICANT);
-//        Applicant applicantInFilteredList = model.getFilteredApplicantList().get(INDEX_FIRST_APPLICANT.getZeroBased());
-//        // trying to unpin an unpinned applicant in filtered list into a duplicate in TrackAScholar
-//        Name name = applicantInFilteredList.getName();
-//        UnPinCommand unPinCommand = new UnPinCommand(name);
-//        assertCommandFailure(unPinCommand, model, UnPinCommand.MESSAGE_APPLICANT_ALREADY_UNPINNED);
-//    }
+    //    @Test
+    //    public void execute_unPinSameApplicantAgainFilteredList_failure() {
+    //        showApplicantAtIndex(model, INDEX_FIRST_APPLICANT);
+    //        Applicant applicantInFilteredList = model.getFilteredApplicantList().get(
+    //        INDEX_FIRST_APPLICANT.getZeroBased());
+    //        // trying to unpin an unpinned applicant in filtered list into a duplicate in TrackAScholar
+    //        Name name = applicantInFilteredList.getName();
+    //        UnPinCommand unPinCommand = new UnPinCommand(name);
+    //        assertCommandFailure(unPinCommand, model, UnPinCommand.MESSAGE_APPLICANT_ALREADY_UNPINNED);
+    //    }
 
     @Test
     public void equals() {
