e90a88fe8be3c3da6f39879d36b5ef82e2ec6513
teekaytai <90845551+teekaytai@users.noreply.github.com>
2022-11-04 03:31:01 +0800

    Make email format error message accurate

--- src/main/java/seedu/application/model/application/Email.java
+++ src/main/java/seedu/application/model/application/Email.java
@@ -13,14 +13,16 @@ public class Email {
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
-            + "characters.\n"
+            + "the parentheses, (" + SPECIAL_CHARACTERS + ").\nThe local-part may not start or end with any special "
+            + "characters, nor can it contain consecutive special characters.\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
             + "separated by periods.\n"
             + "The domain name must:\n"
-            + "    - end with a domain label at least 2 characters long\n"
+            + "    - have each domain label consist of alphanumeric characters,"
+            + " separated only by singular hyphens, if any\n"
             + "    - have each domain label start and end with alphanumeric characters\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
+            + "    - end with a domain label that contains at least 2 consecutive alphanumeric characters\n";
+
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
     private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
