eeca0b6ff648460d1a5d35595c336a106939dfa8
Gowri <gowri.bhat@u.nus.edu>
2022-11-03 19:02:55 +0800

    Fix bug in edit-task

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -32,8 +32,8 @@ import seedu.address.model.task.TaskTitle;
 public class ParserUtil {
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
     public static final String INFO_NOT_AVAILABLE = "NA";
-    public static final String BLANK_STUDENT_LIST = "Student list cannot be blank. " +
-            "Add at least one student for an assignment task.";
+    public static final String BLANK_STUDENT_LIST = "Student list cannot be blank. "
+            + "Add at least one student for an assignment task.";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
@@ -229,11 +229,14 @@ public class ParserUtil {
     public static List<String> parseStudents(String students) throws ParseException {
         requireNonNull(students);
         String[] arr = students.split(",");
-        if (arr.length == 0) {
+        if (students.isBlank() || arr.length == 0) {
             throw new ParseException(BLANK_STUDENT_LIST);
         }
-        ArrayList<String> result = Arrays.stream(arr).map(String::trim).filter(x -> !x.isBlank())
+        ArrayList<String> result = Arrays.stream(arr).map(String::trim).filter(str -> !str.isBlank())
                 .collect(Collectors.toCollection(ArrayList::new));
+        if (result.size() == 0) {
+            throw new ParseException(BLANK_STUDENT_LIST);
+        }
         return result;
     }
 }
