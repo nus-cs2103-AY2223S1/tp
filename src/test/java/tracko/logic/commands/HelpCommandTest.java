8ad1588b7a83a305f8e90392f6fdcd080551f8a9
Janelle Loh <janelleloh13@gmail.com>
2022-11-04 00:04:56 +0800

    Add help command test

--- src/test/java/tracko/logic/commands/HelpCommandTest.java
+++ src/test/java/tracko/logic/commands/HelpCommandTest.java
@@ -1,22 +1,21 @@
 package tracko.logic.commands;
 
-// import static tracko.tracko.commands.logic.CommandTestUtil.assertCommandSuccess;
-// import static tracko.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
-//
-// import org.junit.jupiter.api.Test;
-//
-// import tracko.model.Model;
-// import tracko.model.ModelManager;
+import static tracko.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static tracko.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+
+import org.junit.jupiter.api.Test;
+
+import tracko.model.Model;
+import tracko.model.ModelManager;
 
 public class HelpCommandTest {
-    // // TODO: Update test cases according to new implementations
-    //
-    // private Model model = new ModelManager();
-    // private Model expectedModel = new ModelManager();
-    //
-    // @Test
-    // public void execute_help_success() {
-    //     CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
-    //     assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
-    // }
+
+     private Model model = new ModelManager();
+     private Model expectedModel = new ModelManager();
+
+     @Test
+     public void execute_help_success() {
+         CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+         assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+     }
 }
