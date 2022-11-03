51032a8d163753cf1d58bd9d78baf3da59059b7f
L1uY1jun <yijunlau@gmail.com>
2022-11-04 06:48:58 +0800

    Add test cases for RemoveCommandTest.java

--- src/test/java/seedu/trackascholar/logic/commands/RemoveCommandTest.java
+++ src/test/java/seedu/trackascholar/logic/commands/RemoveCommandTest.java
@@ -1,22 +1,66 @@
 package seedu.trackascholar.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.trackascholar.testutil.TypicalApplicants.getTypicalTrackAScholar;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.trackascholar.model.Model;
+import seedu.trackascholar.model.ModelManager;
+import seedu.trackascholar.model.UserPrefs;
 import seedu.trackascholar.model.applicant.ApplicationStatus;
 
 
 public class RemoveCommandTest {
 
-    //    public void confirmRemoveAccepted_nonEmptyTrackAScholar_success() {
-    //        Model model = new ModelManager(getTypicalTrackAScholar(), new UserPrefs());
-    //        Model expectedModel = new ModelManager(getTypicalTrackAScholar(), new UserPrefs());
-    //        ApplicationStatus acceptedStatus = new ApplicationStatus("accepted");
-    //        RemoveCommand removeCommand = new RemoveCommand(acceptedStatus);
-    //        removeCommand.confirmRemove(model); //
-    //    }
+    @Test
+    public void confirmRemove_nonEmptyTrackAScholar_success() {
+        RemoveCommand firstRemoveCommand =
+                new RemoveCommand(new ApplicationStatus("accepted"));
+        RemoveCommand secondRemoveCommand =
+                new RemoveCommand(new ApplicationStatus("rejected"));
+        Model model = new ModelManager(getTypicalTrackAScholar(), new UserPrefs());
+        CommandResult commandResult = firstRemoveCommand.confirmRemove(model);
+        CommandResult successResult = new CommandResult(RemoveCommand.MESSAGE_REMOVE_APPLICANTS_SUCCESS);
+        assertEquals(commandResult, successResult);
+
+        commandResult = secondRemoveCommand.confirmRemove(model);
+        assertEquals(commandResult, successResult);
+
+    }
+
+    @Test
+    public void confirmRemove_emptyTrackAScholar_success() {
+        RemoveCommand firstRemoveCommand =
+                new RemoveCommand(new ApplicationStatus("accepted"));
+        RemoveCommand secondRemoveCommand =
+                new RemoveCommand(new ApplicationStatus("rejected"));
+        Model model = new ModelManager();
+        CommandResult commandResult = firstRemoveCommand.confirmRemove(model);
+        CommandResult successResult = new CommandResult(RemoveCommand.MESSAGE_REMOVE_APPLICANTS_SUCCESS);
+        assertEquals(commandResult, successResult);
+
+        commandResult = secondRemoveCommand.confirmRemove(model);
+        assertEquals(commandResult, successResult);
+
+    }
+
+    @Test
+    public void cancelClear_trackAScholar_success() {
+        RemoveCommand firstRemoveCommand =
+                new RemoveCommand(new ApplicationStatus("accepted"));
+        RemoveCommand secondRemoveCommand =
+                new RemoveCommand(new ApplicationStatus("rejected"));
+        CommandResult commandResult = firstRemoveCommand.cancelRemove();
+        CommandResult terminationResult = new CommandResult(RemoveCommand.MESSAGE_REMOVE_APPLICANTS_TERMINATION);
+        assertEquals(commandResult, terminationResult);
+
+        commandResult = secondRemoveCommand.cancelRemove();
+        assertEquals(commandResult, terminationResult);
+
+    }
 
     @Test
     public void equals() {
