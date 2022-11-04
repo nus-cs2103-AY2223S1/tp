eb7103303c31eeea00bd6a6fde1656ae34563908
dlimyy <douglas.lim.yy@gmail.com>
2022-11-04 21:07:48 +0800

    Fix checkstyle issues
    Removed redundant imports in Logic and LogicManager classes.

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -10,7 +10,6 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.exam.Exam;
 import seedu.address.model.module.Module;
-import seedu.address.model.person.Person;
 import seedu.address.model.task.Task;
 
 /**
