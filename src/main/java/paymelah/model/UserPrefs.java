bba8c0f72575417209dd5c96647b3df49a05f044
waynezsy <waynezsy0317@gmail.com>
2022-11-04 18:15:53 +0800

    Update default datafile path

--- src/main/java/paymelah/model/UserPrefs.java
+++ src/main/java/paymelah/model/UserPrefs.java
@@ -14,7 +14,7 @@ import paymelah.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path addressBookFilePath = Paths.get("data" , "paymelah.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
