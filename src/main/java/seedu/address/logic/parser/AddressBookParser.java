64da6cdc79c5ed0fb36963e28b2d5a771cf85f71
ama-chi <89921925+ama-chi@users.noreply.github.com>
2022-11-04 10:19:24 +0800

    Import checkstyle

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,7 +6,30 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.*;
+
+import seedu.address.logic.commands.AddClientCommand;
+import seedu.address.logic.commands.AddInterestedClientCommand;
+import seedu.address.logic.commands.AddListingCommand;
+import seedu.address.logic.commands.AddMeetingCommand;
+import seedu.address.logic.commands.AddOfferCommand;
+import seedu.address.logic.commands.AddTagsToListingCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteClientCommand;
+import seedu.address.logic.commands.DeleteListingCommand;
+import seedu.address.logic.commands.DeleteMeetingCommand;
+import seedu.address.logic.commands.DeleteOfferCommand;
+import seedu.address.logic.commands.EditClientCommand;
+import seedu.address.logic.commands.EditListingCommand;
+import seedu.address.logic.commands.EditMeetingCommand;
+import seedu.address.logic.commands.EditOfferCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindClientCommand;
+import seedu.address.logic.commands.FindOffersForListingCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ViewClientListCommand;
+import seedu.address.logic.commands.ViewListingClientsCommand;
+import seedu.address.logic.commands.ViewListingsCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 
