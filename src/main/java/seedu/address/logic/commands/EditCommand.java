cf63363497093528d8080eaac10036373a5bb811
yusiangeng <engyusiang@icloud.com>
2022-11-04 12:43:37 +0800

    Update task on person edit

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -106,8 +106,14 @@ public class EditCommand extends Command {
         }
 
         model.setPerson(personToEdit, editedPerson);
+
+        for (Task t : personToEdit.getTasks()) {
+            t.setPerson(editedPerson);
+        }
+
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         model.update();
+
         return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson));
     }
 
