ab7bdef3e906c23540153e10aca24585c377490a
KIANSEONG <kianseong.quah@gmail.com>
2022-11-04 13:14:00 +0800

    Remove unused import

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -2,7 +2,6 @@ package seedu.address.ui;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentMap;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
