93755747d425f901515ba5713ed708fe6aa27fa0
Julian Wong <e0725200@u.nus.edu>
2022-11-04 18:45:38 +0800

    Update HistoryCommandTest.java

--- src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.HistoryCommand.MESSAGE_EMPTY;
 import static seedu.address.logic.commands.HistoryCommand.MESSAGE_SUCCESS;
 
 import org.junit.jupiter.api.Test;
