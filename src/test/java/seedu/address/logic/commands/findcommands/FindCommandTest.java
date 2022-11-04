6524927d8a46d98ecd00eaca015f3b56bdfed70f
Coco <chuaziyifaith@gmail.com>
2022-11-05 00:32:27 +0800

    Add FindBuyerCOmmandTest

--- src/test/java/seedu/address/logic/commands/findcommands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/findcommands/FindCommandTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.findcommands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -13,7 +13,6 @@ import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.findcommands.FindCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
