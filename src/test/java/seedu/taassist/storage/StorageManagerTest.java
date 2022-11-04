57298f32ef963934e2b45bb1e4dc8dea0c35d7e4
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-11-04 20:04:49 +0800

    Add tests for StorageManager#backupFile

--- src/test/java/seedu/taassist/storage/StorageManagerTest.java
+++ src/test/java/seedu/taassist/storage/StorageManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.taassist.testutil.TypicalStudents.getTypicalTaAssist;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import seedu.taassist.commons.core.GuiSettings;
+import seedu.taassist.commons.util.FileUtil;
 import seedu.taassist.model.ReadOnlyTaAssist;
 import seedu.taassist.model.TaAssist;
 import seedu.taassist.model.UserPrefs;
@@ -24,8 +26,8 @@ public class StorageManagerTest {
 
     @BeforeEach
     public void setUp() {
-        JsonTaAssistStorage taAssistStorage = new JsonTaAssistStorage(getTempFilePath("ab"));
-        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
+        JsonTaAssistStorage taAssistStorage = new JsonTaAssistStorage(getTempFilePath("tassist.json"));
+        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("preferences.json"));
         storageManager = new StorageManager(taAssistStorage, userPrefsStorage);
     }
 
@@ -63,6 +65,29 @@ public class StorageManagerTest {
     @Test
     public void getTaAssistFilePath() {
         assertNotNull(storageManager.getTaAssistFilePath());
+    }
+
+    @Test
+    public void backupFile_backUpDoesntExist_createNewBackup() throws IOException {
+        String content = "test content";
+        Path tempFilePath = getTempFilePath("temp.json");
+        FileUtil.writeToFile(tempFilePath, content);
+        storageManager.backupFile(tempFilePath);
+        Path backupFilePath = getTempFilePath("temp.json.bak");
+        assertEquals(FileUtil.readFromFile(backupFilePath), content);
+    }
+
+    @Test
+    public void backupFile_backUpExists_overwriteBackup() throws IOException {
+        String content = "test content";
+        Path tempFilePath = getTempFilePath("temp.json");
+        FileUtil.writeToFile(tempFilePath, content);
+        storageManager.backupFile(tempFilePath);
+        String newContent = "new test content";
+        FileUtil.writeToFile(tempFilePath, newContent);
+        storageManager.backupFile(tempFilePath);
+        Path backupFilePath = getTempFilePath("temp.json.bak");
+        assertEquals(FileUtil.readFromFile(backupFilePath), newContent);
     }
 
 }
