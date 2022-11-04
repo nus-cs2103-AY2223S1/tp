96165909ac928e8a9d6deb05de64d589abec5bad
Julian Wong <e0725200@u.nus.edu>
2022-11-04 17:50:42 +0800

    Update HistoryCommandTest.java

--- src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HistoryCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.HistoryCommand.MESSAGE_EMPTY;
 import static seedu.address.logic.commands.HistoryCommand.MESSAGE_SUCCESS;
@@ -22,9 +24,35 @@ public class HistoryCommandTest {
 
     @Test
     public void execute_history_filled() {
-        HistoryList.addToHistory("test");
-        CommandResult expectedCommandResult = new CommandResult(MESSAGE_SUCCESS + HistoryList.printList());
+        HistoryList history = new HistoryList();
+        history.addToHistory("test");
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_SUCCESS + history.printList());
         assertCommandSuccess(new HistoryCommand(), model, expectedCommandResult, expectedModel);
-        HistoryList.clearList();
+    }
+
+    @Test
+    public void execute_history_success() {
+        HistoryList history = new HistoryList();
+        history.addToHistory("test");
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_SUCCESS + history.printList());
+        assertCommandSuccess(new HistoryCommand(), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    void equals() {
+        HistoryCommand historyCommand1 = new HistoryCommand();
+        HistoryCommand historyCommand2 = new HistoryCommand();
+
+        // same object -> returns true
+        assertTrue(historyCommand1.equals(historyCommand1));
+
+        // different types -> returns false
+        assertFalse(historyCommand1.equals(1));
+
+        // null -> returns false
+        assertFalse(historyCommand1.equals(null));
+
+        // different historyLists -> returns false
+        assertFalse(historyCommand1.equals(historyCommand2));
     }
 }
