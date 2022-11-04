bead1c1698a1c87656ef471b7657aa0a93c7515f
ningtan11 <e0773603@u.nus.edu>
2022-11-04 20:03:39 +0800

    no message

--- src/main/java/seedu/waddle/model/itinerary/exceptions/ItineraryNotFoundException.java
+++ src/main/java/seedu/waddle/model/itinerary/exceptions/ItineraryNotFoundException.java
@@ -1,6 +1,6 @@
 package seedu.waddle.model.itinerary.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified itinerary.
+ * Signals that the operation is unable to find the specified person.
  */
 public class ItineraryNotFoundException extends RuntimeException {}
