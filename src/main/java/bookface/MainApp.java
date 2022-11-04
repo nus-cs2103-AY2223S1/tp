a40e71364cd32ceb157547d5dc53fc3ffbf8218b
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2022-11-04 14:25:00 +0800

    Fix minor bugs

--- src/main/java/bookface/MainApp.java
+++ src/main/java/bookface/MainApp.java
@@ -173,7 +173,7 @@ public class MainApp extends Application {
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Address Book ] =============================");
+        logger.info("============================ [ Stopping BookFace ] =============================");
         try {
             storage.saveUserPrefs(model.getUserPrefs());
         } catch (IOException e) {
