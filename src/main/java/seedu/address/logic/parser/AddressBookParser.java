9cf93f854aea27f0aaa7efba55a4a9ffe8b2b9a8
ama-chi <89921925+ama-chi@users.noreply.github.com>
2022-11-04 10:21:34 +0800

    Line issue

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import seedu.address.logic.commands.AddClientCommand;
 import seedu.address.logic.commands.AddInterestedClientCommand;
 import seedu.address.logic.commands.AddListingCommand;
