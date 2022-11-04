586dc46158024a876414ae7d18ec12c88de0f7e0
Julian Wong <e0725200@u.nus.edu>
2022-11-04 17:56:58 +0800

    Update HistoryListTest.java

--- src/test/java/seedu/address/storage/HistoryListTest.java
+++ src/test/java/seedu/address/storage/HistoryListTest.java
@@ -1,10 +1,12 @@
 package seedu.address.storage;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class HistoryListTest {
 
