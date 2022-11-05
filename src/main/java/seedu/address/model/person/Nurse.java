dea483a5713be68709ff89823ae36a16df1fa99a
xhphoong <xhphoong@gmail.com>
2022-11-05 23:03:58 +0800

    Make the unavailable date and home visit to display in order
    It is to fix bug as the edit, deassign operation depends on the index
    given by user and also on the sorted home visit and unavailable date
    list. Hence, if the display list is not sorted, then the user might
    give index that does not corresponds to what they intend to change.
    
    Hence, this is a bug that need to be fixed.

--- src/main/java/seedu/address/model/person/Nurse.java
+++ src/main/java/seedu/address/model/person/Nurse.java
@@ -73,7 +73,7 @@ public class Nurse extends Person {
     }
 
     public String getHomesVisitsInString() {
-        String homeVisitsString = homeVisitList.stream()
+        String homeVisitsString = getHomeVisits().stream()
                 .map(x -> x.toString()).collect(Collectors.joining(","));
         if (homeVisitsString.length() == 0) {
             return MESSAGE_FOR_EMPTY_HOMEVISITLIST;
@@ -82,7 +82,7 @@ public class Nurse extends Person {
     }
 
     public String getUnavailableDatesInString() {
-        String unavailableString = unavailableDateList.stream()
+        String unavailableString = getUnavailableDates().stream()
                 .map(x -> x.toString()).collect(Collectors.joining(","));
         if (unavailableString.length() == 0) {
             return MESSAGE_FOR_EMPTY_UNAVAILABLEDATE;
