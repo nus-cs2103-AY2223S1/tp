1147fbd7c9b7beb211e8b24603b82566d55ddf86
richavm14 <richavm14@gmail.com>
2022-11-06 00:22:23 +0800

    Add javadoc comment

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -162,6 +162,10 @@ public class CommandTestUtil {
         assertEquals(1, model.getFilteredPersonList().size());
     }
 
+    /**
+     * Updates {@code model}'s filtered list to show only the team at the given {@code targetIndex} in the
+     * {@code model}'s address book.
+     */
     public static void showTeamAtIndex(Model model, Index targetIndex) {
         assertTrue(targetIndex.getZeroBased() < model.getFilteredTeamList().size());
 
