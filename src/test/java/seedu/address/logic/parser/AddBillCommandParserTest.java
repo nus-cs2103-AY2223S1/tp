5fa20da0f584de6fd63bbc6fd39b69e30f237887
SpecOps2016 <76586680+SpecOps2016@users.noreply.github.com>
2022-11-05 23:12:43 +0800

    Update AddBillCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/AddBillCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddBillCommandParserTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.AMOUNT_DESC_7;
 import static seedu.address.logic.commands.CommandTestUtil.BILL_DATE_DESC_7;
-
 import static seedu.address.logic.commands.CommandTestUtil.BILL_DATE_DESC_8;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_AMOUNT_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_BILL_DATE_DESC;
@@ -23,10 +22,8 @@ import seedu.address.model.bill.Bill;
 import seedu.address.model.bill.BillDate;
 import seedu.address.testutil.BillBuilder;
 
-
 public class AddBillCommandParserTest {
     private AddBillCommandParser parser = new AddBillCommandParser();
-
 
     @Test
     public void parse_allFieldsPresent_success() {
@@ -48,22 +45,6 @@ public class AddBillCommandParserTest {
     @Test
     public void parse_compulsoryFieldMissing_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddBillCommand.MESSAGE_USAGE);
-
-        // missing index
-        assertParseFailure(parser, PREAMBLE_WHITESPACE + BILL_DATE_DESC_7 + AMOUNT_DESC_7,
-                expectedMessage);
-
-        // missing date prefix
-        assertParseFailure(parser, PREAMBLE_WHITESPACE + 1 + AMOUNT_DESC_7,
-                expectedMessage);
-
-        // missing amount prefix
-        assertParseFailure(parser, PREAMBLE_WHITESPACE + 1 + BILL_DATE_DESC_7,
-                expectedMessage);
-
-        // all prefixes missing
-        assertParseFailure(parser, PREAMBLE_WHITESPACE + 1 + VALID_BILL_DATE_7 + VALID_AMOUNT_7,
-                expectedMessage);
 
         // missing bill date prefix
         assertParseFailure(parser, AMOUNT_DESC_7, expectedMessage);
