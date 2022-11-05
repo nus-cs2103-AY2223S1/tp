c1f3f27d2dd245eb344323738b9691caf03f037b
Yuchen <94856691+nehcuy@users.noreply.github.com>
2022-11-06 00:47:04 +0800

    Update HospitalWingTest.java

--- src/test/java/seedu/address/model/person/HospitalWingTest.java
+++ src/test/java/seedu/address/model/person/HospitalWingTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
