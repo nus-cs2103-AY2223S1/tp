35fb3a4d6d38f1ed19c56ed9d9503b5a738da792
richavm14 <richavm14@gmail.com>
2022-11-06 00:13:20 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/team/TeamNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/team/TeamNameContainsKeywordsPredicate.java
@@ -1,9 +1,9 @@
 package seedu.address.model.team;
 
-import seedu.address.commons.util.StringUtil;
-
 import java.util.List;
 import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
