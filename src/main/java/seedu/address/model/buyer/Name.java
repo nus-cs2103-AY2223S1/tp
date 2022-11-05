a3fd8b04d9fae38a26aef35ac421273991bfaf55
Richard Qi <55354921+riccqi@users.noreply.github.com>
2022-11-06 00:25:44 +0800

    add edit command to dg

--- src/main/java/seedu/address/model/buyer/Name.java
+++ src/main/java/seedu/address/model/buyer/Name.java
@@ -10,13 +10,15 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank.";
+            "Names should only be in English and can contain special characters like hyphens and periods. " +
+                    "It should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[a-zA-Z \\-.']*";
+
 
     public final String fullName;
 
@@ -35,7 +37,7 @@ public class Name {
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) && !test.isBlank();
     }
 
 
