a31eef1d9b1f2d9a2e3797028deb5e2dd8d31623
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-11-06 03:20:04 +0800

    Fix errors due to rate limiting
    Only check internal links instead.

--- cli-test/linkchecker/check-links.js
+++ cli-test/linkchecker/check-links.js
@@ -1,7 +1,7 @@
 // Adapted from https://github.com/w3c/node-linkchecker
 import { check } from "node-linkchecker";
 const options = {
-  schemes: ["https:", "http:"],
+  schemes: ["http:"], // Do not check external links to prevent rate-limiting
   userAgent:
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
   fragments: true,
