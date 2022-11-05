4980e3c43bd2e8314e475223f042c80debd6071a
totsukatomofumi <totsukatomofumi@gmail.com>
2022-11-06 05:00:44 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/person/nextofkin/NextOfKin.java
+++ src/main/java/seedu/address/model/person/nextofkin/NextOfKin.java
@@ -10,7 +10,6 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.student.Student;
 import seedu.address.model.tag.Tag;
 
 /**
