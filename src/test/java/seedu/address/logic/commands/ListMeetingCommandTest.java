33e4c61beb89180bb5ce9c26bb229fe34768606f
junweimoo <junweimoo@gmail.com>
2022-11-05 12:25:08 +0800

    Add test cases to test different filtering scenarios in listMeeting

--- src/test/java/seedu/address/logic/commands/ListMeetingCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListMeetingCommandTest.java
@@ -1,14 +1,23 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalClients.getTypicalMyInsuRec;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_MEETING;
+import static seedu.address.testutil.TypicalMeetings.MEETING1;
+import static seedu.address.testutil.TypicalMeetings.MEETING2;
+import static seedu.address.testutil.TypicalMeetings.MEETING3;
+import static seedu.address.testutil.TypicalMeetings.getTypicalMyInsuRec;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.parser.DateKeyword;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.meeting.Meeting;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListMeetingCommand.
@@ -26,7 +35,34 @@ public class ListMeetingCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        // Currently, there are no filters for listMeeting.
         assertCommandSuccess(new ListMeetingCommand(), model, ListMeetingCommand.MESSAGE_SUCCESS, expectedModel);
     }
+
+    @Test
+    public void execute_listFilteredToTomorrow_showsFilteredList() {
+        List<Meeting> expectedMeetingList = Arrays.asList(MEETING1);
+        expectedModel.updateFilteredMeetingList(meeting -> expectedMeetingList.contains(meeting));
+        assertCommandSuccess(new ListMeetingCommand(DateKeyword.TOMORROW), model,
+                ListMeetingCommand.MESSAGE_SUCCESS, expectedModel);
+        expectedModel.updateFilteredMeetingList(PREDICATE_SHOW_ALL_MEETING);
+    }
+
+    @Test
+    public void execute_listFilteredToWeek_showsFilteredList() {
+        List<Meeting> expectedMeetingList = Arrays.asList(MEETING1, MEETING2);
+        expectedModel.updateFilteredMeetingList(meeting -> expectedMeetingList.contains(meeting));
+        assertCommandSuccess(new ListMeetingCommand(DateKeyword.THIS_WEEK), model,
+                ListMeetingCommand.MESSAGE_SUCCESS, expectedModel);
+        expectedModel.updateFilteredMeetingList(PREDICATE_SHOW_ALL_MEETING);
+    }
+
+    @Test
+    public void execute_listFilteredToMonth_showsFilteredList() {
+        List<Meeting> expectedMeetingList = Arrays.asList(MEETING1, MEETING2, MEETING3);
+        expectedModel.updateFilteredMeetingList(meeting -> expectedMeetingList.contains(meeting));
+        assertCommandSuccess(new ListMeetingCommand(DateKeyword.THIS_MONTH), model,
+                ListMeetingCommand.MESSAGE_SUCCESS, expectedModel);
+        expectedModel.updateFilteredMeetingList(PREDICATE_SHOW_ALL_MEETING);
+    }
+
 }
