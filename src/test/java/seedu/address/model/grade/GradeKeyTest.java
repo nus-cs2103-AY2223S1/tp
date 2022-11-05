6cb4ccf4886b7a57b6715c282035d43a7c0fa290
tensaida <48109070+tensaida@users.noreply.github.com>
2022-11-05 14:12:27 +0800

    Add units tests for Grade and GradeKey (#224)

---
+++ src/test/java/seedu/address/model/grade/GradeKeyTest.java
@@ -0,0 +1,83 @@
+package seedu.address.model.grade;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import seedu.address.model.student.Student;
+import seedu.address.model.task.Task;
+
+
+
+class GradeKeyTest {
+
+    private @Mock Student student;
+    private @Mock Task task;
+    private GradeKey testKey;
+
+    @BeforeEach
+    public void setUp() {
+        student = mock(Student.class);
+        task = mock(Task.class);
+        testKey = new GradeKey(student, task);
+    }
+
+    @Test
+    void sameKey_testEquals() {
+        GradeKey sameKey = new GradeKey(student, task);
+        assertEquals(testKey, sameKey);
+    }
+    @Test
+    void differentStudent_testEquals() {
+        GradeKey differentStudentKey = new GradeKey(mock(Student.class), task);
+        assertNotEquals(testKey, differentStudentKey);
+    }
+
+    @Test
+    void differentTask_testEquals() {
+        GradeKey differentTaskKey = new GradeKey(student, mock(Task.class));
+        assertNotEquals(testKey, differentTaskKey);
+    }
+
+    @Test
+    void differentBoth_testEquals() {
+        GradeKey differentKey = new GradeKey(mock(Student.class), mock(Task.class));
+        assertNotEquals(testKey, differentKey);
+    }
+    @Test
+    void sameKey_testHashCode() {
+        GradeKey sameKey = new GradeKey(student, task);
+        assertEquals(testKey.hashCode(), sameKey.hashCode());
+    }
+    @Test
+    void differentStudent_testHashCode() {
+        GradeKey differentStudentKey = new GradeKey(mock(Student.class), task);
+        assertNotEquals(testKey.hashCode(), differentStudentKey.hashCode());
+    }
+
+    @Test
+    void differentTask_testHashCode() {
+        GradeKey differentTaskKey = new GradeKey(student, mock(Task.class));
+        assertNotEquals(testKey.hashCode(), differentTaskKey.hashCode());
+    }
+
+    @Test
+    void differentBoth_testHashCode() {
+        GradeKey differentKey = new GradeKey(mock(Student.class), mock(Task.class));
+        assertNotEquals(testKey.hashCode(), differentKey.hashCode());
+    }
+
+    @Test
+    void getStudent() {
+        assertEquals(testKey.getStudent(), student);
+    }
+
+    @Test
+    void getTask() {
+        assertEquals(testKey.getTask(), task);
+    }
+}
--- src/test/java/seedu/address/model/grade/GradeTest.java
+++ src/test/java/seedu/address/model/grade/GradeTest.java
@@ -1,76 +1,60 @@
-//package seedu.address.model.grade;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.mockito.Mockito.mock;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//
-//import seedu.address.model.student.Student;
-//import seedu.address.model.task.Task;
-//
-//class GradeTest {
-//
-//    private @Mock Student student;
-//    private @Mock Task task;
-//    private Grade underTest;
-//
-//    @BeforeEach
-//    void setUp() {
-//        student = mock(Student.class);
-//        task = mock(Task.class);
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//    }
-//
-//    @Test
-//    void initiallyGraded_setAsGraded() {
-//        underTest = new Grade(student, task, true);
-//        underTest.setAsGraded();
-//        assertTrue(underTest.isGraded());
-//    }
-//
-//    @Test
-//    void initiallyNotGraded_setAsGraded() {
-//        underTest = new Grade(student, task);
-//        underTest.setAsGraded();
-//        assertTrue(underTest.isGraded());
-//    }
-//
-//    @Test
-//    void initiallyNotGraded_setAsNotGraded() {
-//        underTest = new Grade(student, task);
-//        underTest.setAsNotGraded();
-//        assertFalse(underTest.isGraded());
-//    }
-//
-//    @Test
-//    void initiallyGraded_setAsNotGraded() {
-//        underTest = new Grade(student, task, true);
-//        underTest.setAsNotGraded();
-//        assertFalse(underTest.isGraded());
-//    }
-//
-//    @Test
-//    void twoParameterConstructor_isGraded() {
-//        underTest = new Grade(student, task);
-//        assertFalse(underTest.isGraded());
-//    }
-//
-//    @Test
-//    void threeParameterConstructorFalse_isGraded() {
-//        underTest = new Grade(student, task, false);
-//        assertFalse(underTest.isGraded());
-//    }
-//
-//    @Test
-//    void threeParameterConstructorTrue_isGraded() {
-//        underTest = new Grade(student, task, true);
-//        assertTrue(underTest.isGraded());
-//    }
-//}
+package seedu.address.model.grade;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+
+class GradeTest {
+
+    @Test
+    void stringT_isValidDescription() {
+        assertTrue(Grade.isValidDescription("T"));
+    }
+
+    @Test
+    void stringF_isValidDescription() {
+        assertTrue(Grade.isValidDescription("F"));
+    }
+
+    @Test
+    void stringOther_isValidDescription() {
+        String testString = Long.toHexString(new Random().nextLong());
+        while (testString.equals("T") || testString.equals("F")) {
+            testString = Long.toHexString(new Random().nextLong());
+        }
+        assertFalse(Grade.isValidDescription(testString));
+    }
+
+    @Test
+    void values() {
+        Grade[] grades = {Grade.GRADED, Grade.UNGRADED};
+        assertArrayEquals(grades, Grade.values());
+    }
+
+    @Test
+    void graded_valueOf() {
+        assertEquals(Grade.GRADED, Grade.valueOf("GRADED"));
+    }
+
+    @Test
+    void ungraded_valueOf() {
+        assertEquals(Grade.UNGRADED, Grade.valueOf("UNGRADED"));
+    }
+
+    @Test
+    void special_valueOf() {
+        String testString = Long.toHexString(new Random().nextLong());
+        while (testString.equals("GRADED") || testString.equals("UNGRADED")) {
+            testString = Long.toHexString(new Random().nextLong());
+        }
+        String finalTestString = testString;
+        assertThrows(IllegalArgumentException.class, () -> Grade.valueOf(finalTestString));
+    }
+}
