d0824b6ef7f51ef7570295c659aea81cf3dd7d60
KIANSEONG <kianseong.quah@gmail.com>
2022-11-05 20:04:33 +0800

    Remove unused import

--- src/test/java/seedu/address/model/task/DeadlineTest.java
+++ src/test/java/seedu/address/model/task/DeadlineTest.java
@@ -1,7 +1,6 @@
 package seedu.address.model.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.time.LocalDate;
