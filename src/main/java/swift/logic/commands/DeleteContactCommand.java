305bda30a5dd9899776c9e5ef272db7c78e32079
peppapighs <ptk.prasertsuk@gmail.com>
2022-11-05 14:18:04 +0800

    Fix: change terminology from person to contact

--- src/main/java/swift/logic/commands/DeleteContactCommand.java
+++ src/main/java/swift/logic/commands/DeleteContactCommand.java
@@ -21,11 +21,11 @@ public class DeleteContactCommand extends Command {
     public static final ArrayList<Prefix> ARGUMENT_PREFIXES = new ArrayList<>(List.of(new Prefix("", "contact_index")));
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
+            + ": Deletes the contact identified by the index number used in the displayed contact list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Contact: %1$s";
 
     private final Index targetIndex;
 
