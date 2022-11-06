de95309df4bb4803a3aa018771fd987bf2e19a65
Alvin  Jiang <alvinjiang1@gmail.com>
2022-11-06 16:17:35 +0800

    Fix whitespace in error message

--- src/main/java/seedu/rc4hdb/logic/commands/venuecommands/BookCommand.java
+++ src/main/java/seedu/rc4hdb/logic/commands/venuecommands/BookCommand.java
@@ -37,7 +37,7 @@ public class BookCommand extends VenueCommand implements ModelCommand {
             + PREFIX_VENUE_NAME + "VENUE_NAME "
             + PREFIX_TIME_PERIOD + "START_TIME-END_TIME "
             + PREFIX_DAY + "DAY \n"
-            + "Example: " + COMMAND_WORD + " 3"
+            + "Example: " + COMMAND_WORD + " 3 "
             + PREFIX_VENUE_NAME + "meeting "
             + PREFIX_TIME_PERIOD + "10-14 "
             + PREFIX_DAY + "TUE ";
