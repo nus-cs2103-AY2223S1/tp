4138b1b1e62f6c7e8360127f0f8953331511d9fd
24Donovan24 <donovanlee_25@hotmail.com>
2022-11-06 15:11:54 +0800

    Add more test for Sort command

--- src/test/java/gim/logic/commands/SortCommandTest.java
+++ src/test/java/gim/logic/commands/SortCommandTest.java
@@ -3,26 +3,34 @@ package gim.logic.commands;
 import static gim.testutil.TypicalExercises.getSampleUnsortedExercisesExerciseTracker;
 import static gim.testutil.TypicalExercises.getSortedDifferentExercisesDifferentDatesExerciseTracker;
 import static gim.testutil.TypicalExercises.getSortedDifferentExercisesSameDatesExerciseTracker;
+import static gim.testutil.TypicalExercises.getSortedSameExercisesDifferentDates;
 import static gim.testutil.TypicalExercises.getSortedSameExercisesDifferentDatesExerciseTracker;
 import static gim.testutil.TypicalExercises.getTypicalExerciseTracker;
+import static gim.testutil.TypicalExercises.getTypicalExercises;
 import static gim.testutil.TypicalExercises.getUnsortedDifferentExercisesDifferentDatesExerciseTracker;
 import static gim.testutil.TypicalExercises.getUnsortedDifferentExercisesSameDatesExerciseTracker;
+import static gim.testutil.TypicalExercises.getUnsortedSameExercisesDifferentDates;
 import static gim.testutil.TypicalExercises.getUnsortedSameExercisesDifferentDatesExerciseTracker;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import gim.model.Model;
 import gim.model.ModelManager;
 import gim.model.UserPrefs;
+import gim.model.exercise.Exercise;
+import gim.model.exercise.ExerciseList;
 import gim.model.exercise.NameContainsKeywordsPredicate;
 
 
 public class SortCommandTest {
     private Model model;
     private Model expectedModel;
+    private ExerciseList exerciseList = new ExerciseList();
 
     @Test
     public void execute_sortedList_showsSameList() {
@@ -69,6 +77,28 @@ public class SortCommandTest {
         CommandResult result = new SortCommand().execute(model);
         assertEquals(new CommandResult(SortCommand.MESSAGE_SUCCESS), result);
         assertEquals(model.getFilteredExerciseList(), expectedModel.getFilteredExerciseList());
+    }
+
+    @Test
+    public void execute_sortInExerciseList_showsSameList() {
+        List<Exercise> sampleUnsortedList = getUnsortedSameExercisesDifferentDates();
+        exerciseList.setExercises(sampleUnsortedList);
+        exerciseList.sortDisplayedList();
+        assertEquals(exerciseList.asDisplayedList(), getSortedSameExercisesDifferentDates());
+    }
+
+    @Test
+    public void execute_duplicatedDisplayedList_showsSameList() {
+        exerciseList.setExercises(getTypicalExercises());
+        assertEquals(exerciseList.asDisplayedList(), exerciseList.asDuplicatedDisplayedList());
+    }
+
+    @Test
+    public void execute_resetDisplayedList_showsSameList() {
+        exerciseList.setExercises(getTypicalExercises());
+        Collections.reverse(exerciseList.asDisplayedList());
+        exerciseList.resetDisplayedList();
+        assertEquals(exerciseList.asDisplayedList(), getTypicalExercises());
     }
 
     /**
