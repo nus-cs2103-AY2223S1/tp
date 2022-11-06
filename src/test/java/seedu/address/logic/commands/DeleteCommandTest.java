d170ae582653fe8290f94e68b2cca53d45c4b79d
mohamedsaf1 <mohdsaf1@hotmail.com>
2022-11-06 15:33:26 +0800

    Checkstyle fix

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -1,27 +1,28 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.core.index.Index;
-
-import seedu.address.commons.util.FunctionalInterfaces.Changer;
-import seedu.address.commons.util.FunctionalInterfaces.Getter;
-import seedu.address.logic.parser.CmdBuilder;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.item.DisplayItem;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.exceptions.PersonOutOfBoundException;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_USAGE;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.FunctionalInterfaces.Changer;
+import seedu.address.commons.util.FunctionalInterfaces.Getter;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.CmdBuilder;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.item.DisplayItem;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.exceptions.PersonOutOfBoundException;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for {@code DeleteCommand}.
@@ -83,7 +84,8 @@ public class DeleteCommandTest {
 
         DeleteCommand<Person> deleteCommand = CmdBuilder.makeDelPerson(outOfBoundIndex);
 
-        assertCommandFailure(deleteCommand, model, String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE, //String.format(PersonOutOfBoundException.ERR_MSG,
+        assertCommandFailure(deleteCommand, model, String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE,
+                //String.format(PersonOutOfBoundException.ERR_MSG,
                 model.getFilteredPersonList().size(), outOfBoundIndex.getOneBased()));
     }
 
