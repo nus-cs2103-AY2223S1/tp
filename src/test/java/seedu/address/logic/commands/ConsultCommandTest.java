569f98dd697ac45db8861a6a62c5a1ed9ccb03c9
Charles1026 <xiaozhehao@gmail.com>
2022-11-07 03:03:44 +0800

    Fix Checkstyle Issues

--- src/test/java/seedu/address/logic/commands/ConsultCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ConsultCommandTest.java
@@ -1,18 +1,5 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.core.index.Index;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.appointment.PastAppointment;
-import seedu.address.model.person.Person;
-import seedu.address.testutil.PastAppointmentBuilder;
-import seedu.address.testutil.PersonBuilder;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,6 +10,19 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.appointment.PastAppointment;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PastAppointmentBuilder;
+import seedu.address.testutil.PersonBuilder;
 
 class ConsultCommandTest {
 
