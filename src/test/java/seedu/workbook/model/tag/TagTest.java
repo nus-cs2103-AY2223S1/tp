e1d9b3b32631f00acc96dd2017fa390441d90330
rylzxc <chuachdaryl@gmail.com>
2022-11-06 22:22:29 +0800

    Update TagTest

--- src/test/java/seedu/workbook/model/tag/TagTest.java
+++ src/test/java/seedu/workbook/model/tag/TagTest.java
@@ -1,5 +1,6 @@
 package seedu.workbook.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.workbook.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,25 @@ public class TagTest {
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new Tag(null));
+    }
+
+    @Test
+    public void isValidTagName() {
+        // null tag name
+        assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+    }
+
+    @Test
+    public void equals() {
+        Tag tagOne = new Tag("Same name");
+        Tag tagTwo = new Tag("Same name");
+
+        // Compare two tag objects of same tagName
+        assertTrue(tagOne.equals(tagTwo));
+
+        // Compare two same tag objects
+        assertTrue(tagOne.equals(tagOne));
+
     }
 
 }
