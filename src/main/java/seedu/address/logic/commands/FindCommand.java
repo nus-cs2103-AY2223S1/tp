1a64d1a8cb687b40f74c76f8d300476cd4b202c9
avock <chunkhai.k@gmail.com>
2022-11-06 13:40:13 +0800

    Fix Error Message for Optional Fields

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -17,15 +17,16 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = COMMAND_TYPE;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Finds all Modules and Persons which contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " cs2100";
+        + ": Finds all Modules and Persons which contain any of "
+        + "the specified keywords (not case-sensitive) and displays them as a list with index numbers.\n"
+        + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+        + "Example: " + COMMAND_WORD + " cs2100";
 
     private final List<String> keywords;
 
     /**
      * Constructor
+     *
      * @param keywords keywords to filter lists by.
      */
     public FindCommand(List<String> keywords) {
@@ -38,15 +39,15 @@ public class FindCommand extends Command {
         new FindPersonCommand(keywords).execute(model);
         new FindModuleCommand(keywords).execute(model);
         return new CommandResult(String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW,
-                model.getFilteredPersonList().size())
-                        + " " + String.format(Messages.MESSAGE_MODULES_LISTED_OVERVIEW,
-                model.getFilteredModuleList().size()));
+            model.getFilteredPersonList().size())
+            + " " + String.format(Messages.MESSAGE_MODULES_LISTED_OVERVIEW,
+            model.getFilteredModuleList().size()));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof FindCommand // instanceof handles nulls
-                && keywords.equals(((FindCommand) other).keywords)); // state check
+            || (other instanceof FindCommand // instanceof handles nulls
+            && keywords.equals(((FindCommand) other).keywords)); // state check
     }
 }
