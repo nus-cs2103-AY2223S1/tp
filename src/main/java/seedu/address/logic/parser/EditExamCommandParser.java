ef4cb7fdc138b138e2f4f22c46d5a34dd3ee6a45
RachelChua <e0772465@u.nus.edu>
2022-11-06 18:05:14 +0800

    Add tests for integer overflow

--- src/main/java/seedu/address/logic/parser/EditExamCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditExamCommandParser.java
@@ -9,12 +9,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EXAM_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
 import static seedu.address.logic.parser.ParserUtil.parseIndex;
 
+import java.util.regex.Pattern;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditExamCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.module.Module;
-
-import java.util.regex.Pattern;
 
 /**
  * Parses input arguments and creates a new EditExamCommand object
