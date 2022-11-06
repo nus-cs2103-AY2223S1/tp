b26fa4370b83adc9315c566b936ea52b53001e12
sltsheryl <sltsheryl@gmail.com>
2022-11-06 18:25:06 +0800

    Standardize capitalization of UI

--- src/main/java/seedu/masslinkers/ui/StatusBarFooter.java
+++ src/main/java/seedu/masslinkers/ui/StatusBarFooter.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 
 /**
- * Ui for the status bar that is displayed at the footer of the application.
+ * UI for the status bar that is displayed at the footer of the application.
  */
 public class StatusBarFooter extends UiPart<Region> {
 
