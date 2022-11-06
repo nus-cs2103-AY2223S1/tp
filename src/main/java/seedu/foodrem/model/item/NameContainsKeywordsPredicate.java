eabf24e955d7ccf713d4930bd82fb69460ef053a
Tan Yi Xian <>
2022-11-06 15:10:53 +0800

    Add javadoc to NameContainsKeywordsPredicate

--- src/main/java/seedu/foodrem/model/item/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/foodrem/model/item/NameContainsKeywordsPredicate.java
@@ -11,16 +11,25 @@ import seedu.foodrem.commons.util.StringUtil;
 public class NameContainsKeywordsPredicate implements Predicate<Item> {
     private final List<String> keywords;
 
+    /**
+     * Constructs a {@link NameContainsKeywordsPredicate}.
+     */
     public NameContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
+    /**
+     * Tests if the item name matches all the keywords.
+     */
     @Override
     public boolean test(Item item) {
         return keywords.stream()
                 .allMatch(keyword -> StringUtil.containsSubstringIgnoreCase(item.getName().toString(), keyword));
     }
 
+    /**
+     * Returns {@code true} if both {@link NameContainsKeywordsPredicate} have the same keywords.
+     */
     @Override
     public boolean equals(Object other) {
         return other == this
