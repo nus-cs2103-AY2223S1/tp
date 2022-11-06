d8680488109b0653264260db8541ae1888346a6d
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-11-06 08:30:35 +0800

    Add tests for AddEventCommand

---
+++ src/test/java/seedu/address/logic/commands/AddEventCommandIntegrationTest.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalEvents.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.event.Event;
+import seedu.address.testutil.EventBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code AddEventCommand}.
+ */
+public class AddEventCommandIntegrationTest {
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newEvent_success() {
+        Event validEvent = new EventBuilder().build();
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.addEvent(validEvent);
+
+        assertCommandSuccess(new AddEventCommand(validEvent), model,
+                String.format(AddEventCommand.MESSAGE_SUCCESS, validEvent), expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateEvent_throwsCommandException() {
+        Event eventInList = model.getAddressBook().getEventList().get(0);
+        assertCommandFailure(new AddEventCommand(eventInList), model, AddEventCommand.MESSAGE_DUPLICATE_EVENT);
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/commands/AddEventCommandTest.java
@@ -0,0 +1,254 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart.Data;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.EventSortField;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonSortField;
+import seedu.address.testutil.EventBuilder;
+import seedu.address.testutil.PersonBuilder;
+
+public class AddEventCommandTest {
+
+    @Test
+    public void constructor_nullPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddEventCommand(null));
+    }
+
+    @Test
+    public void execute_eventAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingEventAdded modelStub = new ModelStubAcceptingEventAdded();
+        Event validEvent = new EventBuilder().build();
+
+        CommandResult commandResult = new AddEventCommand(validEvent).execute(modelStub);
+
+        assertEquals(String.format(AddEventCommand.MESSAGE_SUCCESS, validEvent), commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(validEvent), modelStub.eventsAdded);
+    }
+
+    @Test
+    public void execute_duplicateEvent_throwsCommandException() {
+        Event validEvent = new EventBuilder().build();
+        AddEventCommand addEventCommand = new AddEventCommand(validEvent);
+        ModelStub modelStub = new ModelStubWithEvent(validEvent);
+
+        assertThrows(CommandException.class,
+                AddEventCommand.MESSAGE_DUPLICATE_EVENT, () -> addEventCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        Event carSale = new EventBuilder().withEventTitle("Car Sale").build();
+        Event shoeSale = new EventBuilder().withEventTitle("Shoe Sale").build();
+        AddEventCommand addCarSaleCommand = new AddEventCommand(carSale);
+        AddEventCommand addShoeSaleCommand = new AddEventCommand(shoeSale);
+
+        // same object -> returns true
+        assertTrue(addCarSaleCommand.equals(addCarSaleCommand));
+
+        // same values -> returns true
+        AddEventCommand addCarSaleCommandCopy = new AddEventCommand(carSale);
+        assertTrue(addCarSaleCommand.equals(addCarSaleCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addCarSaleCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addCarSaleCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(addCarSaleCommand.equals(addShoeSaleCommand));
+    }
+
+    /**
+     * A default model stub that have all of the methods failing.
+     */
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyAddressBook newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+        @Override
+        public void setEvent(Event target, Event editedEvent) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPerson(Person target, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void sortPersons(PersonSortField sortField) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void sortEvents(EventSortField sortField) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPersonList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Event> getFilteredEventList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredEventList(Predicate<Event> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Data> getPieChartData() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setData(ObservableList<Data> newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateEventPersonReference() {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that contains a single event.
+     */
+    private class ModelStubWithEvent extends ModelStub {
+        private final Event event;
+
+        ModelStubWithEvent(Event event) {
+            requireNonNull(event);
+            this.event = event;
+        }
+
+        @Override
+        public boolean hasEvent(Event event) {
+            requireNonNull(event);
+            return this.event.isSameEvent(event);
+        }
+    }
+
+    /**
+     * A Model stub that always accept the event being added.
+     */
+    private class ModelStubAcceptingEventAdded extends ModelStub {
+        final ArrayList<Event> eventsAdded = new ArrayList<>();
+
+        @Override
+        public boolean hasEvent(Event event) {
+            requireNonNull(event);
+            return eventsAdded.stream().anyMatch(event::isSameEvent);
+        }
+
+        @Override
+        public void addEvent(Event event) {
+            requireNonNull(event);
+            eventsAdded.add(event);
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            return new AddressBook();
+        }
+    }
+
+}
