d957f756fc7eb363876308a49b38925b6fce0f0f
Haohui Wu <thewuhh@gmail.com>
2022-11-06 13:58:20 +0800

    Add tests for calculator gui

--- src/test/java/seedu/boba/logic/commands/CalculatorGuiCommandTest.java
+++ src/test/java/seedu/boba/logic/commands/CalculatorGuiCommandTest.java
@@ -1,7 +1,10 @@
 package seedu.boba.logic.commands;
 
 import static seedu.boba.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.boba.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.boba.logic.commands.CalculatorGuiCommand.SHOWING_CALC_MESSAGE;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +16,21 @@ public class CalculatorGuiCommandTest {
     private BobaBotModel expectedBobaBotModel = new BobaBotModelManager();
 
     @Test
-    public void execute_help_success() {
-        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false, false, false, false);
-        assertCommandSuccess(new HelpCommand(), bobaBotModel, expectedCommandResult, expectedBobaBotModel);
+    public void execute_calc_gui_success() {
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_CALC_MESSAGE, false, false, false, false, true);
+        assertCommandSuccess(new CalculatorGuiCommand(), bobaBotModel, expectedCommandResult, expectedBobaBotModel);
+    }
+
+    @Test
+    public void equals_same_object() {
+        final CalculatorGuiCommand calculatorGuiCommand = new CalculatorGuiCommand();
+        assertEquals(calculatorGuiCommand, calculatorGuiCommand);
+    }
+
+    @Test
+    public void equals_different_object() {
+        final CalculatorGuiCommand cmd1 = new CalculatorGuiCommand();
+        final CalculatorGuiCommand cmd2 = new CalculatorGuiCommand();
+        assertEquals(cmd1, cmd2);
     }
 }
