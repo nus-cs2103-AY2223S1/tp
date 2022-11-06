eafc5c2bcd67773e8b8de08109aa327ea5ae9f35
Mohamed Hamas <e0726186@u.nus.edu>
2022-11-06 15:40:22 +0800

    Added missing tests for ItemContainsKeywordsPredicate and JsonAdaptedTask

--- src/test/java/seedu/address/model/person/ItemContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/ItemContainsKeywordsPredicateTest.java
@@ -1,2 +1,75 @@
-package seedu.address.model.person;public class ItemContainsKeywordsPredicate {
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
+
+public class ItemContainsKeywordsPredicateTest {
+
+    @Test
+    public void equals() {
+        List<String> firstItemPredicateKeywordList = Collections.singletonList("item1");
+        List<String> secondItemPredicateKeywordList = Arrays.asList("item1", "item2");
+
+        ItemContainsKeywordsPredicate firstItemPredicate = new ItemContainsKeywordsPredicate(firstItemPredicateKeywordList);
+        ItemContainsKeywordsPredicate secondItemPredicate = new ItemContainsKeywordsPredicate(secondItemPredicateKeywordList);
+
+        // same object -> returns true
+        assertTrue(firstItemPredicate.equals(firstItemPredicate));
+
+        // same values -> returns true
+        ItemContainsKeywordsPredicate firstItemPredicateCopy = new ItemContainsKeywordsPredicate(firstItemPredicateKeywordList);
+        assertTrue(firstItemPredicate.equals(firstItemPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstItemPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstItemPredicate.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstItemPredicate.equals(secondItemPredicate));
+    }
+
+    @Test
+    public void test_itemContainsKeywords_returnsTrue() {
+        // One keyword
+        ItemContainsKeywordsPredicate predicate = new ItemContainsKeywordsPredicate(Collections.singletonList("Chicken"));
+        assertTrue(predicate.test(new PersonBuilder().withItem("Chicken Egg").build()));
+
+        // Multiple keywords
+        predicate = new ItemContainsKeywordsPredicate(Arrays.asList("Chicken", "Egg"));
+        assertTrue(predicate.test(new PersonBuilder().withItem("Chicken Egg").build()));
+
+        // Only one matching keyword
+        predicate = new ItemContainsKeywordsPredicate(Arrays.asList("Chicken", "Egg"));
+        assertTrue(predicate.test(new PersonBuilder().withItem("Chicken Beef").build()));
+
+        // Mixed-case keywords
+        predicate = new ItemContainsKeywordsPredicate(Arrays.asList("cHiCken", "eGg"));
+        assertTrue(predicate.test(new PersonBuilder().withItem("Chicken Egg").build()));
+    }
+
+    @Test
+    public void test_nameDoesNotContainKeywords_returnsFalse() {
+        // Zero keywords
+        ItemContainsKeywordsPredicate predicate = new ItemContainsKeywordsPredicate(Collections.emptyList());
+        assertFalse(predicate.test(new PersonBuilder().withItem("Chicken").build()));
+
+        // Non-matching keyword
+        predicate = new ItemContainsKeywordsPredicate(Arrays.asList("Water"));
+        assertFalse(predicate.test(new PersonBuilder().withName("Chicken Egg").build()));
+
+        // Keywords match name, phone, price and address, but does not match item
+        predicate = new ItemContainsKeywordsPredicate(Arrays.asList("12345", "$1.20", "Alice", "Main", "Street"));
+        assertFalse(predicate.test(new PersonBuilder().withName("Alice").withPhone("12345678")
+                .withPrice("$1.20").withItem("Chicken").withAddress("Main Street").build()));
+    }
 }
--- src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
@@ -1,7 +1,61 @@
 package seedu.address.storage;
 
-/**
- * TODO: Add test to test valid Task.
- */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedTask.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTasks.TASK_FUEL;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.exceptions.IllegalValueException;
+
 public class JsonAdaptedTaskTest {
+    private static final String INVALID_DEADLINE = "+651234";
+    private static final String INVALID_TAG = "#friend";
+
+    private static final String VALID_TITLE = TASK_FUEL.getTitle();
+    private static final String VALID_DEADLINE= TASK_FUEL.getDeadline().toString();
+    private static final boolean VALID_STATUS = TASK_FUEL.getStatus();
+    private static final List<JsonAdaptedTag> VALID_TAGS = TASK_FUEL.getTags().stream()
+            .map(JsonAdaptedTag::new)
+            .collect(Collectors.toList());
+
+
+    @Test
+    public void toModelType_validTaskDetails_returnsTask() throws Exception {
+        JsonAdaptedTask task = new JsonAdaptedTask(TASK_FUEL);
+        assertEquals(TASK_FUEL, task.toModelType());
+    }
+
+    @Test
+    public void toModelType_nullTitle_throwsIllegalValueException() {
+        JsonAdaptedTask task = new JsonAdaptedTask(null,VALID_DEADLINE,VALID_STATUS,VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, "title");
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullDeadline_throwsIllegalValueException() {
+        JsonAdaptedTask task = new JsonAdaptedTask(VALID_TITLE,null,VALID_STATUS,VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, "deadline");
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDeadline_throwsIllegalValueException() {
+        JsonAdaptedTask task = new JsonAdaptedTask(VALID_TITLE,INVALID_DEADLINE,VALID_STATUS,VALID_TAGS);
+        String expectedMessage = "Incorrect LocalDate format !";
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidTags_throwsIllegalValueException() {
+        List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
+        invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
+        JsonAdaptedTask task = new JsonAdaptedTask(VALID_TITLE,INVALID_DEADLINE,VALID_STATUS,invalidTags);
+        assertThrows(IllegalValueException.class, task::toModelType);
+    }
 }
