385d24a4a56203b8df8b4d8e310a0c969ba67aec
Jason Christopher <jason.christopher@u.nus.edu>
2022-11-07 04:04:31 +0800

    Fix Checkstyle Issue

--- src/test/java/seedu/address/model/item/AbstractDisplayItemTest.java
+++ src/test/java/seedu/address/model/item/AbstractDisplayItemTest.java
@@ -2,8 +2,8 @@ package seedu.address.model.item;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalAttributes.AGE;
 import static seedu.address.testutil.TypicalAttributes.POSITION;
 import static seedu.address.testutil.TypicalPersons.ALICE;
