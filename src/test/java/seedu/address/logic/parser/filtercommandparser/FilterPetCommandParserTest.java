aa826e13c0fca12236ae90be0977838706fc9c50
Coco <chuaziyifaith@gmail.com>
2022-11-06 18:40:30 +0800

    Update FilterPetCommandParserTest

--- src/test/java/seedu/address/logic/parser/filtercommandparser/FilterPetCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/filtercommandparser/FilterPetCommandParserTest.java
@@ -1,11 +1,19 @@
 package seedu.address.logic.parser.filtercommandparser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PET_NAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.filtercommands.FilterPetCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.pet.Pet;
+import seedu.address.model.pet.predicates.PetNameContainsKeywordsPredicate;
 
 public class FilterPetCommandParserTest {
     private FilterPetCommandParser parser = new FilterPetCommandParser();
@@ -20,6 +28,29 @@ public class FilterPetCommandParserTest {
     public void parse_invalidPrefix_throwsParseException() {
         assertParseFailure(parser, "ft/invalid", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 FilterPetCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_name_success() {
+        Predicate<Pet> defaultPredicate = new Predicate<Pet>() {
+            @Override
+            public boolean test(Pet pet) {
+                return true;
+            }
+            @Override
+            public boolean equals(Object object) {
+                return object instanceof Predicate;
+            }
+        };
+        String input = " " + PREFIX_PET_NAME + "ashy";
+        FilterPetCommand expectedCommand = new FilterPetCommand(new PetNameContainsKeywordsPredicate<>(
+                Arrays.asList("ashy")),
+                defaultPredicate, defaultPredicate, defaultPredicate, defaultPredicate);
+        try {
+            assertEquals(parser.parse(input), expectedCommand);
+        } catch (ParseException e) {
+            assert false;
+        }
     }
 
 }
