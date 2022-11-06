27d0e76d19442a9c3d43bca420446142511af48e
Julian Wong <e0725200@u.nus.edu>
2022-11-06 12:20:05 +0800

    update AddressBookParserTests

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.parser.CliSyntax.*;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
@@ -13,18 +13,13 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonContainsKeywords;
+import seedu.address.model.sort.NameComparator;
+import seedu.address.storage.HistoryList;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
@@ -38,12 +33,20 @@ public class AddressBookParserTest {
         Person person = new PersonBuilder().build();
         AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
         assertEquals(new AddCommand(person), command);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
     public void parseCommand_clear() throws Exception {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_history() throws Exception {
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD) instanceof HistoryCommand);
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD + " 3") instanceof HistoryCommand);
     }
 
     @Test
@@ -51,23 +54,65 @@ public class AddressBookParserTest {
         DeleteCommand command = (DeleteCommand) parser.parseCommand(
                 DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_deleteOccupation() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_OCCUPATION.getPrefix());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON, "o/"), command);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_deletePhone() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_PHONE.getPrefix());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON, "p/"), command);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_deleteEmail() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_EMAIL.getPrefix());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON, "e/"), command);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_deleteAddress() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_ADDRESS.getPrefix());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON, "a/"), command);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_deleteTag() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_TAG.getPrefix());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON, "t/"), command);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
     public void parseCommand_edit() throws Exception {
-
         Person person = new PersonBuilder().withGroups("friends").build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
         EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
 
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
     public void parseCommand_exit() throws Exception {
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
@@ -76,18 +121,57 @@ public class AddressBookParserTest {
         FindCommand command = (FindCommand) parser.parseCommand(
                 FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
         assertEquals(new FindCommand(new PersonContainsKeywords(keywords)), command);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
     public void parseCommand_help() throws Exception {
         assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
         assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
     public void parseCommand_list() throws Exception {
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_undo() throws Exception {
+        assertTrue(parser.parseCommand(UndoCommand.COMMAND_WORD) instanceof UndoCommand);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_sortName() throws Exception {
+        assertTrue(parser.parseCommand(
+                SortCommand.COMMAND_WORD + " " + PREFIX_NAME.getPrefix()) instanceof SortCommand);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_sortPhone() throws Exception {
+        assertTrue(parser.parseCommand(
+                SortCommand.COMMAND_WORD + " " + PREFIX_PHONE.getPrefix()) instanceof SortCommand);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_sortEmail() throws Exception {
+        assertTrue(parser.parseCommand(
+                SortCommand.COMMAND_WORD + " " + PREFIX_EMAIL.getPrefix()) instanceof SortCommand);
+        assertFalse(HistoryList.isEmpty());
+    }
+
+    @Test
+    public void parseCommand_include() throws Exception {
+        String social = "TELEGRAM";
+        String link = "Alex";
+        assertTrue((IncludeCommand) parser.parseCommand(
+                IncludeCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_SOCIAL.getPrefix() + social + " #/" + link) instanceof IncludeCommand);
+        assertFalse(HistoryList.isEmpty());
     }
 
     @Test
