437b82a207af36ee332885346d8d6bc1ed0e3198
nqt230 <76938663+nqt230@users.noreply.github.com>
2022-11-07 01:07:27 +0800

    Add unit test and minor refactor
    - Add testing for Internship class
    - Refactor Link and Unlink command parser to use common ParserUtil methods

--- src/main/java/seedu/address/logic/parser/LinkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LinkCommandParser.java
@@ -4,8 +4,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
 
-import java.util.stream.Stream;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.LinkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -28,7 +26,7 @@ public class LinkCommandParser implements Parser<LinkCommand> {
                         PREFIX_PERSON,
                         PREFIX_INTERNSHIP);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_PERSON, PREFIX_INTERNSHIP)
+        if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_PERSON, PREFIX_INTERNSHIP)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LinkCommand.MESSAGE_USAGE));
         }
@@ -37,14 +35,6 @@ public class LinkCommandParser implements Parser<LinkCommand> {
         Index internshipIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_INTERNSHIP).get());
 
         return new LinkCommand(personIndex, internshipIndex);
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
--- src/main/java/seedu/address/logic/parser/UnlinkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnlinkCommandParser.java
@@ -4,8 +4,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
 
-import java.util.stream.Stream;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.UnlinkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -28,7 +26,7 @@ public class UnlinkCommandParser implements Parser<UnlinkCommand> {
                         PREFIX_PERSON,
                         PREFIX_INTERNSHIP);
 
-        if (!(arePrefixesPresent(argMultimap, PREFIX_PERSON) || arePrefixesPresent(argMultimap, PREFIX_INTERNSHIP))
+        if (!(ParserUtil.isAnyPrefixPresent(argMultimap, PREFIX_PERSON, PREFIX_INTERNSHIP))
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnlinkCommand.MESSAGE_USAGE));
         }
@@ -44,14 +42,6 @@ public class UnlinkCommandParser implements Parser<UnlinkCommand> {
         }
 
         return new UnlinkCommand(personIndex, internshipIndex);
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
