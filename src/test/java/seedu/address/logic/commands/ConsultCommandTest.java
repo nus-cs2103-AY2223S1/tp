8a8c4b5162574481124610f92aaf9490a1313853
Charles1026 <xiaozhehao@gmail.com>
2022-11-07 00:10:17 +0800

    Created ConsultCommandTest

---
+++ src/test/java/seedu/address/logic/commands/ConsultCommandTest.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultCommandTest {
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
