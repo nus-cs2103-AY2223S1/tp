47e5ee53eec49b0bc060c47a03a89d5df9781f2d
Coco <chuaziyifaith@gmail.com>
2022-11-06 17:30:27 +0800

    Update FindCommandParser

--- src/main/java/seedu/address/logic/parser/findcommandparser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/findcommandparser/FindCommandParser.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.parser.findcommandparser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.logic.commands.findcommands.FindCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.PredicateParser;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -27,6 +31,31 @@ public class FindCommandParser implements Parser<FindCommand> {
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        int totalPrefixesPresent = 0;
+        if (trimmedArgs.contains(PREFIX_ADDRESS.getPrefix())) {
+            totalPrefixesPresent += 1;
+        }
+        if (trimmedArgs.contains(PREFIX_EMAIL.getPrefix())) {
+            totalPrefixesPresent += 1;
+        }
+        if (trimmedArgs.contains(PREFIX_INDEX.getPrefix())) {
+            totalPrefixesPresent += 1;
+        }
+        if (trimmedArgs.contains(PREFIX_LOCATION.getPrefix())) {
+            totalPrefixesPresent += 1;
+        }
+        if (trimmedArgs.contains(PREFIX_NAME.getPrefix())) {
+            totalPrefixesPresent += 1;
+        }
+        if (trimmedArgs.contains(PREFIX_PHONE.getPrefix())) {
+            totalPrefixesPresent += 1;
+        }
+
+        if (totalPrefixesPresent > 1) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, "More than 1 prefix present"));
         }
 
         Predicate<Buyer> buyerPredicate = PredicateParser.parseBuyer(trimmedArgs);
