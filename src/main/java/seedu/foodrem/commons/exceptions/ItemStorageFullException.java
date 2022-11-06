2c03f41ab36c697263427b9fc56769abc3b66cef
Tan Yi Xian <>
2022-11-06 14:45:51 +0800

    Add javadoc headers for exceptions

--- src/main/java/seedu/foodrem/commons/exceptions/ItemStorageFullException.java
+++ src/main/java/seedu/foodrem/commons/exceptions/ItemStorageFullException.java
@@ -4,9 +4,16 @@ package seedu.foodrem.commons.exceptions;
  * Represents an error when more items are added when the storage is full.
  */
 public class ItemStorageFullException extends StorageFullException {
+    /**
+     * Constructs an ItemStorageFullException.
+     */
     public ItemStorageFullException(int maxNumberOfItems) {
         super(String.format("The item storage is full. FoodRem can only hold up to %s items.", maxNumberOfItems));
     }
+
+    /**
+     * Constructs an ItemStorageFullException.
+     */
     public ItemStorageFullException(String message) {
         super(message);
     }
--- src/main/java/seedu/foodrem/commons/exceptions/StorageFullException.java
+++ src/main/java/seedu/foodrem/commons/exceptions/StorageFullException.java
@@ -4,6 +4,9 @@ package seedu.foodrem.commons.exceptions;
  * Represents an error when the storage is full.
  */
 public class StorageFullException extends RuntimeException {
+    /**
+     * Constructs a StorageFullException.
+     */
     public StorageFullException(String message) {
         super(message);
     }
