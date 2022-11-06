d2f0481044414c3864067d4d8bae2d2199e3b58b
Chang Jing Yan <60026139+changjy44@users.noreply.github.com>
2022-11-06 23:04:43 +0800

    Add test for sort commands

--- src/test/java/seedu/address/logic/commands/SortMemberCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortMemberCommandTest.java
@@ -1,7 +1,57 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.getTypicalTruthTable;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+import seedu.address.logic.parser.Order;
+import seedu.address.logic.parser.OrderConverter;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 class SortMemberCommandTest {
+    private Model model = new ModelManager(getTypicalTruthTable(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalTruthTable(), new UserPrefs());
+
+    private final Command commandToBeTested = new SortMemberCommand();
+    private final CommandLine commandLine = new CommandLine(commandToBeTested)
+            .registerConverter(Order.class, new OrderConverter());
+
+    @BeforeEach
+    public void setUp() {
+        model.getTeam().addMember(ALICE);
+        model.getTeam().addMember(CARL);
+        model.getTeam().addMember(BENSON);
+        expectedModel.getTeam().addMember(ALICE);
+        expectedModel.getTeam().addMember(CARL);
+        expectedModel.getTeam().addMember(BENSON);
+    }
+
+    @Test
+    public void execute_unsortedMembersToAscending_success() {
+        commandLine.parseArgs(new String[] {"asc"});
+        expectedModel.getTeam().sortMembers((t1, t2) -> t1.getName().toString().compareTo(t2.getName().toString()));
+        assertCommandSuccess(commandToBeTested, model, SortMemberCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+    @Test
+    public void execute_unsortedMembersToDescending_success() {
+        commandLine.parseArgs(new String[] {"dsc"});
+        expectedModel.getTeam().sortMembers((t1, t2) -> t2.getName().toString().compareTo(t1.getName().toString()));
+        assertCommandSuccess(commandToBeTested, model, SortMemberCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+    @Test
+    public void execute_sortedAscendingMembersToUnsorted_success() {
+        model.getTeam().sortMembers((t1, t2) -> t1.getName().toString().compareTo(t2.getName().toString()));
+        commandLine.parseArgs(new String[] {"res"});
+        assertCommandSuccess(commandToBeTested, model, SortMemberCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
 
 }
--- src/test/java/seedu/address/logic/commands/SortTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortTaskCommandTest.java
@@ -1,7 +1,55 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalTruthTable;
+import static seedu.address.testutil.TypicalTasks.TASK_1;
+import static seedu.address.testutil.TypicalTasks.TASK_CODE;
+import static seedu.address.testutil.TypicalTasks.TASK_REVIEW;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+import seedu.address.logic.parser.Order;
+import seedu.address.logic.parser.OrderConverter;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 class SortTaskCommandTest {
+    private Model model = new ModelManager(getTypicalTruthTable(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalTruthTable(), new UserPrefs());
 
+    private final Command commandToBeTested = new SortTaskCommand();
+    private final CommandLine commandLine = new CommandLine(commandToBeTested)
+            .registerConverter(Order.class, new OrderConverter());
+
+    @BeforeEach
+    public void setUp() {
+        model.getTeam().addTask(TASK_CODE);
+        model.getTeam().addTask(TASK_1);
+        model.getTeam().addTask(TASK_REVIEW);
+        expectedModel.getTeam().addTask(TASK_CODE);
+        expectedModel.getTeam().addTask(TASK_1);
+        expectedModel.getTeam().addTask(TASK_REVIEW);
+    }
+
+    @Test
+    public void execute_unsortedMembersToAscending_success() {
+        commandLine.parseArgs(new String[] {"asc"});
+        expectedModel.getTeam().sortTasks((t1, t2) -> t1.getName().toString().compareTo(t2.getName().toString()));
+        assertCommandSuccess(commandToBeTested, model, SortTaskCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+    @Test
+    public void execute_unsortedMembersToDescending_success() {
+        commandLine.parseArgs(new String[] {"dsc"});
+        expectedModel.getTeam().sortTasks((t1, t2) -> t2.getName().toString().compareTo(t1.getName().toString()));
+        assertCommandSuccess(commandToBeTested, model, SortTaskCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+    @Test
+    public void execute_sortedAscendingMembersToUnsorted_success() {
+        model.getTeam().sortTasks((t1, t2) -> t1.getName().toString().compareTo(t2.getName().toString()));
+        commandLine.parseArgs(new String[] {"res"});
+        assertCommandSuccess(commandToBeTested, model, SortTaskCommand.MESSAGE_SUCCESS, expectedModel);
+    }
 }
