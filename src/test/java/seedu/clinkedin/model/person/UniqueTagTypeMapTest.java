db4409adf0888e842fe852bc0826ae471ac29c1d
pratham31012002 <pratham31012002@gmail.com>
2022-11-06 17:45:19 +0800

    Fix checkstyle

--- src/test/java/seedu/clinkedin/model/person/UniqueTagTypeMapTest.java
+++ src/test/java/seedu/clinkedin/model/person/UniqueTagTypeMapTest.java
@@ -1,8 +1,8 @@
 package seedu.clinkedin.model.person;
 
-import org.junit.jupiter.api.Test;
-
 import static seedu.clinkedin.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class UniqueTagTypeMapTest {
 
