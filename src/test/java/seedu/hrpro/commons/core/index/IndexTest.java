9bc0479f5d81e9c611b3afc518641e5293b756ac
maxng17 <maxngjl@gmail.com>
2022-11-06 19:16:24 +0800

    Update name to fit context of HR Pro Max++

--- src/test/java/seedu/hrpro/commons/core/index/IndexTest.java
+++ src/test/java/seedu/hrpro/commons/core/index/IndexTest.java
@@ -39,22 +39,22 @@ public class IndexTest {
 
     @Test
     public void equals() {
-        final Index fifthPersonIndex = Index.fromOneBased(5);
+        final Index fifthProjectIndex = Index.fromOneBased(5);
 
         // same values -> returns true
-        assertTrue(fifthPersonIndex.equals(Index.fromOneBased(5)));
-        assertTrue(fifthPersonIndex.equals(Index.fromZeroBased(4)));
+        assertTrue(fifthProjectIndex.equals(Index.fromOneBased(5)));
+        assertTrue(fifthProjectIndex.equals(Index.fromZeroBased(4)));
 
         // same object -> returns true
-        assertTrue(fifthPersonIndex.equals(fifthPersonIndex));
+        assertTrue(fifthProjectIndex.equals(fifthProjectIndex));
 
         // null -> returns false
-        assertFalse(fifthPersonIndex.equals(null));
+        assertFalse(fifthProjectIndex.equals(null));
 
         // different types -> returns false
-        assertFalse(fifthPersonIndex.equals(5.0f));
+        assertFalse(fifthProjectIndex.equals(5.0f));
 
         // different index -> returns false
-        assertFalse(fifthPersonIndex.equals(Index.fromOneBased(1)));
+        assertFalse(fifthProjectIndex.equals(Index.fromOneBased(1)));
     }
 }
