8cd4b6266c31fb25fc18f1fd5bd35e3b335f1945
Chang Jing Yan <60026139+changjy44@users.noreply.github.com>
2022-11-06 23:33:29 +0800

    Add test for taskof command

--- src/test/java/seedu/address/logic/commands/TasksOfCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TasksOfCommandTest.java
@@ -1,7 +1,78 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FOUR;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THREE;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.getTypicalTruthTable;
+import static seedu.address.testutil.TypicalTasks.TASK_1;
+import static seedu.address.testutil.TypicalTasks.TASK_1_DETAILS;
+import static seedu.address.testutil.TypicalTasks.TASK_2_DETAILS;
+
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.IndexConverter;
+import seedu.address.logic.parser.TaskNameConverter;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.team.Task;
+import seedu.address.model.team.TaskName;
 
 class TasksOfCommandTest {
+    private Model model = new ModelManager(getTypicalTruthTable(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalTruthTable(), new UserPrefs());
+    private final Command commandToBeTested = new TasksOfCommand();
+    private final CommandLine commandLine = new CommandLine(commandToBeTested)
+            .registerConverter(Index.class, new IndexConverter());
 
+    @BeforeEach
+    public void setUp() {
+        model.getTeam().addMember(ALICE);
+        model.getTeam().addMember(BENSON);
+        model.getTeam().addMember(CARL);
+        expectedModel.getTeam().addMember(ALICE);
+        expectedModel.getTeam().addMember(BENSON);
+        expectedModel.getTeam().addMember(CARL);
+        model.getTeam().addTask(TASK_1_DETAILS);
+        expectedModel.getTeam().addTask(TASK_1_DETAILS);
+    }
+
+    @Test
+    public void execute_haveTasks_success() {
+        commandLine.parseArgs(new String[] {"1"});
+        Predicate<Task> predicate = task -> task.checkAssignee(ALICE);
+        expectedModel.updateFilteredTaskList(predicate);
+        CommandResult expectedResult = new CommandResult(
+                String.format(TasksOfCommand.MESSAGE_SUCCESS, 1, ALICE.getName()));
+        assertCommandSuccess(commandToBeTested, model, expectedResult, expectedModel);
+    }
+
+    @Test
+    public void execute_haveNoTasks_success() {
+        commandLine.parseArgs(new String[] {"3"});
+        Predicate<Task> predicate = task -> task.checkAssignee(CARL);
+        expectedModel.updateFilteredTaskList(predicate);
+        CommandResult expectedResult = new CommandResult(
+                String.format(TasksOfCommand.MESSAGE_SUCCESS, 0, CARL.getName()));
+        assertCommandSuccess(commandToBeTested, model, expectedResult, expectedModel);
+    }
+
+    @Test
+    public void execute_indexOutOfBounds_throwsCommandException() {
+        int memberSize = model.getFilteredMemberList().size();
+        commandLine.parseArgs(new String[] {String.valueOf(memberSize + 1)});
+        String resultString = String.format(TasksOfCommand.MESSAGE_MEMBER_INDEX_TOO_LARGE, INDEX_FOUR.getOneBased());
+        assertThrows(CommandException.class, resultString, ()
+                -> commandToBeTested.execute(model));
+    }
 }
--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,6 +9,7 @@ public class TypicalIndexes {
     public static final Index INDEX_ONE = Index.fromOneBased(1);
     public static final Index INDEX_TWO = Index.fromOneBased(2);
     public static final Index INDEX_THREE = Index.fromOneBased(3);
+    public static final Index INDEX_FOUR = Index.fromOneBased(4);
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PERSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PERSON = Index.fromOneBased(3);
