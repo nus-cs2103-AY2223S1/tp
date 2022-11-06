b6e9d9ccd65e36172e70e81edd7ddc61bd61cf63
sikai00 <sikai4111@gmail.com>
2022-11-06 15:53:41 +0800

    Add checks for products in addClient

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -12,6 +12,6 @@ public class Messages {
     public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d client listed!";
     public static final String MESSAGE_INVALID_MEETING_DISPLAYED_INDEX = "The meeting index provided is invalid";
     public static final String MESSAGE_INVALID_PRODUCT_DISPLAYED_INDEX = "The product index provided is invalid";
-    public static final String MESSAGE_NON_EXISTING_PRODUCT = "There are products not already added into MyInsuRec";
+    public static final String MESSAGE_NON_EXISTING_PRODUCT = "The product(s) provided do not exist.";
     public static final String MESSAGE_END_TIME_BEFORE_START_TIME = "The ending time is earlier than starting time!";
 }
