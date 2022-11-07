8fb1aa924eb9c18f278a4f83397cab2691d4c09e
PangKuangWei <pangkuangwei@gmail.com>
2022-11-07 18:48:15 +0800

    Remove import

--- src/main/java/soconnect/ui/PersonCard.java
+++ src/main/java/soconnect/ui/PersonCard.java
@@ -6,7 +6,6 @@ import static soconnect.logic.commands.customise.CustomiseCommand.NUMBER_OF_CUST
 
 import java.util.Comparator;
 
-import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
