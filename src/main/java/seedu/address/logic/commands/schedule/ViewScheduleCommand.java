4442b92c62396241c43215a1cf713bddbcf534a3
lizekai-richard <lzknus@gmail.com>
2022-11-07 23:07:20 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/schedule/ViewScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/schedule/ViewScheduleCommand.java
@@ -27,3 +27,4 @@ public class ViewScheduleCommand extends Command {
     }
 
 }
+
--- src/main/java/seedu/address/logic/parser/schedule/ViewScheduleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/schedule/ViewScheduleCommandParser.java
@@ -1,21 +1,12 @@
 package seedu.address.logic.parser.schedule;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_OF_SCHEDULE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_WEEKDAY;
-
-import java.util.ArrayList;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.schedule.ViewScheduleCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
-import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
-import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.module.schedule.ScheduleContainsKeywordsPredicate;
 
 
 /**
