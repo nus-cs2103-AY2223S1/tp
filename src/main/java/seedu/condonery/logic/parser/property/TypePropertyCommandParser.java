2fea5e83fea9dc3ddc4bf11c26cf5e4cfe3354b8
yuehernkang <yuehernkang@hotmail.com>
2022-11-07 14:49:47 +0800

    check property type to be only HDB, CONDO or LANDED only

--- src/main/java/seedu/condonery/logic/parser/property/TypePropertyCommandParser.java
+++ src/main/java/seedu/condonery/logic/parser/property/TypePropertyCommandParser.java
@@ -1,13 +1,17 @@
 package seedu.condonery.logic.parser.property;
 
 import static seedu.condonery.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.condonery.logic.parser.CliSyntax.PREFIX_PROPERTY_TYPE;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 import seedu.condonery.logic.commands.property.TypePropertyCommand;
 import seedu.condonery.logic.parser.Parser;
+import seedu.condonery.logic.parser.ParserUtil;
 import seedu.condonery.logic.parser.exceptions.ParseException;
 import seedu.condonery.model.property.PropertyTypeContainsKeywordsPredicate;
+import seedu.condonery.model.tag.PropertyTypeEnum;
 
 /**
  * Parses input arguments and creates a new TypePropertyCommand object
@@ -25,8 +29,21 @@ public class TypePropertyCommandParser implements Parser<TypePropertyCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, TypePropertyCommand.MESSAGE_USAGE));
         }
+        System.out.println(trimmedArgs);
+
+
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        for (int i = 0; i < nameKeywords.length; i++) {
+            try {
+                ParserUtil.parsePropertyType(
+                        nameKeywords[i]);
+            } catch (IllegalArgumentException ex) {
+                throw new ParseException(
+                        "Invalid Property Type specified! Property Type must be one of HDB, CONDO, or LANDED");
+            }
+        }
 
         return new TypePropertyCommand(new PropertyTypeContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
