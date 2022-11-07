5890557f07ffee4f3761deb73238654622ea4fc7
agarwal-anjali <e0866261@u.nus.edu>
2022-11-07 20:21:16 +0800

    Add test cases for AddToCommandParser

--- src/test/java/seedu/clinkedin/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/clinkedin/logic/parser/AddCommandParserTest.java
@@ -1,5 +1,6 @@
 package seedu.clinkedin.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.clinkedin.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.clinkedin.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
 import static seedu.clinkedin.logic.commands.CommandTestUtil.ADDRESS_DESC_BOB;
@@ -37,6 +38,7 @@ import static seedu.clinkedin.testutil.TypicalPersons.BOB;
 import org.junit.jupiter.api.Test;
 
 import seedu.clinkedin.logic.commands.AddCommand;
+import seedu.clinkedin.logic.commands.AddNoteCommand;
 import seedu.clinkedin.logic.parser.exceptions.ParseException;
 import seedu.clinkedin.model.person.Address;
 import seedu.clinkedin.model.person.Email;
@@ -48,6 +50,17 @@ import seedu.clinkedin.testutil.PersonBuilder;
 
 public class AddCommandParserTest {
     private AddCommandParser parser = new AddCommandParser();
+
+    @Test
+    public void parse_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> parser.parse(null));
+    }
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+    }
 
     @Test
     public void parse_allFieldsPresent_success() throws ParseException {
--- src/test/java/seedu/clinkedin/logic/parser/AddToCommandParserTest.java
+++ src/test/java/seedu/clinkedin/logic/parser/AddToCommandParserTest.java
@@ -1,6 +1,42 @@
 package seedu.clinkedin.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.clinkedin.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.clinkedin.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.clinkedin.logic.commands.AddToCommand;
+
 public class AddToCommandParserTest {
 
+    private final AddToCommandParser parser = new AddToCommandParser();
+    @Test
+    public void parse_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> parser.parse(null));
+    }
 
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidIndex_throwsParseException() {
+        assertParseFailure(parser, "addTo",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidPrefix_throwsParseException() {
+        assertParseFailure(parser, "addTo 1 n/name",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_noAttribute_throwsParserException() {
+        assertParseFailure(parser, "addTo",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToCommand.MESSAGE_USAGE));
+    }
 }
