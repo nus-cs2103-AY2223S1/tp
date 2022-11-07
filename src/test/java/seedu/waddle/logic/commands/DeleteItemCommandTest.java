0b1f87e2195ea7752c3d5c64a88c77dcf2fee830
Henry <>
2022-11-07 21:28:44 +0800

    Fix checkstyle errors and change variable name

--- src/test/java/seedu/waddle/logic/commands/DeleteItemCommandTest.java
+++ src/test/java/seedu/waddle/logic/commands/DeleteItemCommandTest.java
@@ -1,7 +1,10 @@
 package seedu.waddle.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.waddle.testutil.TypicalItineraries.getTypicalWaddle;
+
 import org.junit.jupiter.api.Test;
-import seedu.waddle.commons.core.index.Index;
+
 import seedu.waddle.commons.core.index.MultiIndex;
 import seedu.waddle.logic.StageManager;
 import seedu.waddle.logic.commands.exceptions.CommandException;
@@ -10,13 +13,6 @@ import seedu.waddle.model.ModelManager;
 import seedu.waddle.model.UserPrefs;
 import seedu.waddle.model.item.Item;
 import seedu.waddle.model.itinerary.Itinerary;
-import seedu.waddle.testutil.ItemBuilder;
-import seedu.waddle.testutil.ItineraryBuilder;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.waddle.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.waddle.testutil.TypicalIndexes.INDEX_SECOND_ITINERARY;
-import static seedu.waddle.testutil.TypicalItineraries.getTypicalWaddle;
 
 public class DeleteItemCommandTest {
     private Model model = new ModelManager(getTypicalWaddle(), new UserPrefs());
@@ -31,7 +27,7 @@ public class DeleteItemCommandTest {
         stageManager.setWishStage(itinerary);
         CommandResult commandResult = new DeleteItemCommand(targetIndex).execute(model);
 
-        assertEquals(String.format(DeleteItemCommand.MESSAGE_DELETE_ITINERARY_SUCCESS, targetItem),
+        assertEquals(String.format(DeleteItemCommand.MESSAGE_DELETE_ITEM_SUCCESS, targetItem),
                 commandResult.getFeedbackToUser());
         assertEquals(false, itinerary.hasItem(targetItem));
     }
