870114b580f23c2e40a96a66efed91758cb9dc52
richavm14 <richavm14@gmail.com>
2022-11-07 23:34:12 +0800

    Add java docs to model manager

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -50,6 +50,9 @@ public class ModelManager implements Model {
 
     }
 
+    /**
+     * Creates a ModelManager
+     */
     public ModelManager() {
         this(new AddressBook(), new UserPrefs());
     }
@@ -101,23 +104,45 @@ public class ModelManager implements Model {
         return addressBook;
     }
 
+    /**
+     * Returns true if addressBook has person.
+     *
+     * @param person Person object.
+     * @return boolean
+     */
     @Override
     public boolean hasPerson(Person person) {
         requireNonNull(person);
         return addressBook.hasPerson(person);
     }
 
+    /**
+     * Deleted a person from addressBook.
+     *
+     * @param target Person object to be removed from addressBook.
+     */
     @Override
     public void deletePerson(Person target) {
         addressBook.removePerson(target);
     }
 
+    /**
+     * Adds a person to addressBook.
+     *
+     * @param person Person object to be added.
+     */
     @Override
     public void addPerson(Person person) {
         addressBook.addPerson(person);
         updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
     }
 
+    /**
+     * Sets an existing person to a new person.
+     *
+     * @param target Person object.
+     * @param editedPerson Edited Person object.
+     */
     @Override
     public void setPerson(Person target, Person editedPerson) {
         requireAllNonNull(target, editedPerson);
@@ -125,12 +150,24 @@ public class ModelManager implements Model {
         addressBook.setPerson(target, editedPerson);
     }
 
+    /**
+     * Returns true is addressBook has team.
+     *
+     * @param team Team object.
+     * @return boolean true if addressBook has team.
+     */
     @Override
     public boolean hasTeam(Team team) {
         requireNonNull(team);
         return addressBook.hasTeam(team);
     }
 
+    /**
+     * Sets team name of a team.
+     *
+     * @param targetIndex Index of team.
+     * @param newTeamTeamName New name of team
+     */
     @Override
     public void setTeamName(Index targetIndex, TeamName newTeamTeamName) {
         requireAllNonNull(targetIndex, newTeamTeamName);
@@ -139,33 +176,68 @@ public class ModelManager implements Model {
         updateFilteredTeamList(PREDICATE_SHOW_ALL_TEAMS);
     }
 
+    /**
+     * Deletes a team from addressBook.
+     *
+     * @param team Team object which is to be deleted.
+     */
     @Override
     public void deleteTeam(Team team) {
         addressBook.removeTeam(team);
     }
 
+    /**
+     * Adds a team to addressBook.
+     *
+     * @param team Team object which is added.
+     */
     @Override
     public void addTeam(Team team) {
         addressBook.addTeam(team);
         updateFilteredTeamList(PREDICATE_SHOW_ALL_TEAMS);
     }
 
+    /**
+     * Adds a person to a team.
+     *
+     * @param person Person object needed to be added.
+     * @param team Team object.
+     */
     @Override
     public void addPersonToTeam(Person person, Team team) {
         addressBook.addPersonToTeam(person, team);
     }
 
+    /**
+     * Removes a person from a team.
+     *
+     * @param person Person object that needs to be removed.
+     * @param team Team object that needs to be removed.
+     */
     @Override
     public void removePersonFromTeam(Person person, Team team) {
         addressBook.removePersonFromTeam(person, team);
     }
 
+    /**
+     * Returns true if team has a task.
+     *
+     * @param index Index of team.
+     * @param task Index of task.
+     * @return boolean if team has a task.
+     */
     @Override
     public boolean teamHasTask(Index index, Task task) {
         requireAllNonNull(index, task);
         return addressBook.teamHasTask(index, task);
     }
 
+    /**
+     * Adds a task to a team.
+     *
+     * @param index Index of team in which task has to be added.
+     * @param task Task that needs to be added.
+     */
     @Override
     public void addTask(Index index, Task task) {
         requireAllNonNull(index, task);
@@ -174,6 +246,14 @@ public class ModelManager implements Model {
         updateFilteredTeamList(PREDICATE_SHOW_ALL_TEAMS);
     }
 
+    /**
+     * Edits the task name and deadline (optional) in a team.
+     *
+     * @param teamIndex Index of team whose task needs to be edited.
+     * @param taskIndex Index of task that needs to be edited.
+     * @param newTaskName New name of task.
+     * @param newDeadline New deadline of task.
+     */
     @Override
     public void editTask(Index teamIndex, Index taskIndex,
                          TaskName newTaskName, LocalDate newDeadline) {
@@ -183,6 +263,12 @@ public class ModelManager implements Model {
         updateFilteredTeamList(PREDICATE_SHOW_ALL_TEAMS);
     }
 
+    /**
+     * Marks a task in a team.
+     *
+     * @param teamIndex Index of team whose task needs to be marked.
+     * @param taskIndex Index of task which needs to be marked.
+     */
     @Override
     public void markTask(Index teamIndex, Index taskIndex) {
         requireAllNonNull(teamIndex, taskIndex);
@@ -191,6 +277,12 @@ public class ModelManager implements Model {
         updateFilteredTeamList(PREDICATE_SHOW_ALL_TEAMS);
     }
 
+    /**
+     * Removes mark from a task in a team.
+     *
+     * @param teamIndex Index of team whose task needs to be unmarked.
+     * @param taskIndex Index of task which needs to be unmarked.
+     */
     @Override
     public void unmarkTask(Index teamIndex, Index taskIndex) {
         requireAllNonNull(teamIndex, taskIndex);
@@ -199,6 +291,12 @@ public class ModelManager implements Model {
         updateFilteredTeamList(PREDICATE_SHOW_ALL_TEAMS);
     }
 
+    /**
+     * Deletes a task from a team.
+     *
+     * @param teamIndex Index of team whose task needs to be deleted.
+     * @param taskIndex Index of task that needs to be deleted.
+     */
     @Override
     public void deleteTask(Index teamIndex, Index taskIndex) {
         requireAllNonNull(teamIndex, taskIndex);
@@ -232,7 +330,8 @@ public class ModelManager implements Model {
 
     /**
      * Returns person object which has given name.
-     * @param name Name of person.
+     *
+     * @param name Name of person whose person object is needed.
      * @return Person object.
      */
     @Override
@@ -250,7 +349,8 @@ public class ModelManager implements Model {
 
     /**
      * Returns person name from given person index.
-     * @param personIndex Index of person.
+     *
+     * @param personIndex Index of person whose name is needed.
      * @return Name of person.
      */
     @Override
@@ -262,7 +362,8 @@ public class ModelManager implements Model {
 
     /**
      * Returns person object which has given index.
-     * @param personIndex Index of person.
+     *
+     * @param personIndex Index of person .
      * @return Person object.
      */
     @Override
@@ -285,6 +386,7 @@ public class ModelManager implements Model {
 
     /**
      * Returns the team object which has the given team name.
+     *
      * @param teamName Name of team.
      * @return Team object.
      */
@@ -301,6 +403,12 @@ public class ModelManager implements Model {
         throw new TeamNotFoundException();
     }
 
+    /**
+     * Returns true if the given team name exists.
+     *
+     * @param teamName Name of team.
+     * @return boolean true if team name exists
+     */
     @Override
     public boolean teamNameExists(TeamName teamName) {
         List<Team> teams = getFilteredTeamList();
@@ -316,6 +424,7 @@ public class ModelManager implements Model {
 
     /**
      * Returns team name from given team index.
+     *
      * @param teamIndex Index of team.
      * @return Name of team.
      */
@@ -328,7 +437,8 @@ public class ModelManager implements Model {
 
     /**
      * Returns team object which has given index.
-     * @param teamIndex Index of team.
+     *
+     * @param teamIndex Index of team whose team object is needed.
      * @return Team object.
      */
     @Override
@@ -340,6 +450,7 @@ public class ModelManager implements Model {
 
     /**
      * Return list of all teams in which the given person is a member in.
+     *
      * @param p Person object.
      * @return List of teams.
      */
@@ -362,11 +473,11 @@ public class ModelManager implements Model {
     }
 
     /**
-     * Returns true if person is a member in the given team
+     * Returns true if person is a member in the given team.
      *
-     * @param p index of person
-     * @param t index of team
-     * @return true if team has member
+     * @param p Index of person.
+     * @param t Index of team.
+     * @return True if team has member.
      */
     public boolean teamHasMember(Index p, Index t) {
         List<Team> teams = getFilteredTeamList();
