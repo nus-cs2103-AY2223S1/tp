ba6964c313cc926c71df095014d5eaeb6afb097d
Daniel Luo <hurinturin123@gmail.com>
2022-11-07 16:03:57 +0800

    Add test cases for DeleteMeetingCommandTest and DeleteMeetingCommandParserTest

--- src/test/java/seedu/address/logic/parser/CreateMeetingCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CreateMeetingCommandParserTest.java
@@ -12,13 +12,7 @@ import seedu.address.logic.commands.CreateMeetingCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Copied over from DeleteCommandParserTest:
- *
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
+ * Class to test CreateMeetingCommandParser.java
  */
 public class CreateMeetingCommandParserTest {
 
