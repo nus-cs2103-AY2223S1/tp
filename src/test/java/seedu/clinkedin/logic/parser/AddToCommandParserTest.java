f9f88c3143fcd5ad745f2d70541a2ceccb84448f
agarwal-anjali <e0866261@u.nus.edu>
2022-11-07 21:02:43 +0800

    Add UpdatePersonBuilder class and update AddToCommandParserTest

--- src/test/java/seedu/clinkedin/logic/parser/AddToCommandParserTest.java
+++ src/test/java/seedu/clinkedin/logic/parser/AddToCommandParserTest.java
@@ -3,10 +3,14 @@ package seedu.clinkedin.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.clinkedin.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.clinkedin.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.clinkedin.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.clinkedin.commons.core.index.Index;
 import seedu.clinkedin.logic.commands.AddToCommand;
+import seedu.clinkedin.logic.commands.AddToCommand.UpdatePersonDescriptor;
+import seedu.clinkedin.testutil.UpdatePersonDescriptorBuilder;
 
 public class AddToCommandParserTest {
 
@@ -38,5 +42,15 @@ public class AddToCommandParserTest {
     public void parse_noAttribute_throwsParserException() {
         assertParseFailure(parser, "addTo",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsAddToCommand() {
+        // no leading and trailing whitespaces
+        UpdatePersonDescriptor updatePersonDescriptor = new UpdatePersonDescriptorBuilder().withNote("Test").build();
+        AddToCommand expectedAddToCommand = new AddToCommand(Index.fromOneBased(1), updatePersonDescriptor);
+        assertParseSuccess(parser, "1 note/Test", expectedAddToCommand);
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n \t1 note/Test\t", expectedAddToCommand);
     }
 }
