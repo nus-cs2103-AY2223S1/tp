c78d11025823447a1c3161f227f6457971069eac
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-11-07 22:33:23 +0800

    Add plural and singular message forms

--- src/main/java/seedu/foodrem/commons/core/Messages.java
+++ src/main/java/seedu/foodrem/commons/core/Messages.java
@@ -7,11 +7,17 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command. Enter \"help\" to view all commands.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_ITEMS_DISPLAYED_INDEX = "The item index provided is invalid.";
-    // TODO: Fix "1 items listed" grammar bug instead of "1 item listed"
+
+    // Singular
     public static final String MESSAGE_ITEMS_LISTED_OVERVIEW = "%1$d items listed!";
-    // TODO: Fix "1 items sorted" grammar bug instead of "1 item sorted"
+    public static final String MESSAGE_ITEM_SORTED_OVERVIEW = "%1$d items sorted!";
+    public static final String MESSAGE_ITEM_FILTERED_OVERVIEW = "%1$d items after filtering!";
+
+    // Plural or Zero
+    public static final String MESSAGE_ITEMS_LISTED_OVERVIEW = "%1$d items listed!";
     public static final String MESSAGE_ITEMS_SORTED_OVERVIEW = "%1$d items sorted!";
     public static final String MESSAGE_ITEMS_FILTERED_OVERVIEW = "%1$d items after filtering!";
+
     public static final String MESSAGE_NON_POSITIVE_INDEX =
             "The index of an item should be a positive number.";
 }
