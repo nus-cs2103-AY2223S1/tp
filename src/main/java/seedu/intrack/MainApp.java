570a4d84d5e4889ac117d04168e311a2545e8507
chantellyu <e0758927@u.nus.edu>
2022-11-07 14:58:47 +0800

    Fix minor issues

--- src/main/java/seedu/intrack/MainApp.java
+++ src/main/java/seedu/intrack/MainApp.java
@@ -36,7 +36,7 @@ import seedu.intrack.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 3, 2, true);
+    public static final Version VERSION = new Version(1, 4, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
