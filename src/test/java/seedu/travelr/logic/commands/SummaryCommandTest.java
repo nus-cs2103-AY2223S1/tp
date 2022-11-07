c2c692516bdf787edeb3ff27b9182a01d896318b
Cjun1039 <e0726202@u.nus.edu>
2022-11-07 20:24:23 +0800

    Add some test cases

--- src/test/java/seedu/travelr/logic/commands/SummaryCommandTest.java
+++ src/test/java/seedu/travelr/logic/commands/SummaryCommandTest.java
@@ -1,10 +1,22 @@
 package seedu.travelr.logic.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.travelr.logic.commands.exceptions.CommandException;
+import seedu.travelr.model.Model;
+import seedu.travelr.model.ModelManager;
+import seedu.travelr.model.UserPrefs;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.travelr.testutil.TypicalTrips.getTypicalTravelr;
 
 class SummaryCommandTest {
 
+    private Model model = new ModelManager(getTypicalTravelr(), new UserPrefs());
+
     @Test
-    void execute() {
+    void commandResultTest() throws CommandException {
+        SummaryCommand sc = new SummaryCommand();
+        CommandResult cr = sc.execute(model);
+        assertTrue(cr.isShowSummary());
     }
 }
