54e35fb4ec4a016a1afa9ad0f7c9543cf4e901ba
crvstalphua <>
2022-11-07 22:31:52 +0800

    fix ci errors

--- src/test/java/seedu/address/model/issue/StatusTest.java
+++ src/test/java/seedu/address/model/issue/StatusTest.java
@@ -1,11 +1,11 @@
 package seedu.address.model.issue;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class StatusTest {
 
