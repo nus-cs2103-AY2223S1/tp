faded8270ac4e7a94d8e0623c6840dcb9ebd95d2
mohamedsaf1 <mohdsaf1@hotmail.com>
2022-11-07 19:58:08 +0800

    More checkstyle fix

--- src/test/java/seedu/address/logic/commands/FieldPrefixesTest.java
+++ src/test/java/seedu/address/logic/commands/FieldPrefixesTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
