898a31507944c7a6d40a9869d3cd0dd4b92f01e8
guokweijie <guokweijie@gmail.com>
2022-11-07 14:25:18 +0800

    Improve code quality for FindPatientCommandParser

--- src/main/java/seedu/address/logic/parser/FindPatientCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindPatientCommandParser.java
@@ -52,104 +52,42 @@ public class FindPatientCommandParser implements Parser<FindPatientCommand> {
 
         //Check for name prefix
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-
-            checkNumberOfPrefixes(PREFIX_NAME, argMultimap);
-
-            String trimmedArgs = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()).toString().trim();
-
-            final String finalPredicateString = createPredicateString(trimmedArgs);
-
-            Predicate<Name> namePredicate = (name -> name.fullName.toLowerCase()
-                    .contains(finalPredicateString.toLowerCase()));
-
-            this.namePredicate = namePredicate;
+            createNamePredicate(argMultimap);
         }
 
-        Optional<Predicate<Name>> finalNamePredicate = Optional.ofNullable(this.namePredicate);
+        Optional<Predicate<Name>> finalNamePredicate = Optional.ofNullable(namePredicate);
 
         //Check for phone prefix
         if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
-
-            checkNumberOfPrefixes(PREFIX_PHONE, argMultimap);
-
-            String trimmedArgs = argMultimap.getValue(PREFIX_PHONE).get().trim();
-
-            if (!trimmedArgs.matches("[0-9]+")) {
-                throw new ParseException("Input for finding by phone number must be a number consisting of one or"
-                        + " more digits");
-            }
-
-            Predicate<Phone> phonePredicate = (phone -> phone.value.contains(trimmedArgs));
-
-            this.phonePredicate = phonePredicate;
+            createPhonePredicate(argMultimap);
         }
 
         Optional<Predicate<Phone>> finalPhonePredicate = Optional.ofNullable(this.phonePredicate);
 
         //Check for email prefix
         if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
-
-            checkNumberOfPrefixes(PREFIX_EMAIL, argMultimap);
-
-            String trimmedArgs = argMultimap.getValue(PREFIX_EMAIL).get().trim();
-
-            if (trimmedArgs.isEmpty()) {
-                throw new ParseException("Input for finding by email should not be empty");
-            }
-
-            final String finalPredicateString = createPredicateString(trimmedArgs);
-
-            Predicate<Email> emailPredicate = (email -> email.value.toLowerCase()
-                    .contains(finalPredicateString.toLowerCase()));
-
-            this.emailPredicate = emailPredicate;
+            createEmailPredicate(argMultimap);
         }
 
         Optional<Predicate<Email>> finalEmailPredicate = Optional.ofNullable(this.emailPredicate);
 
         //Check for address prefix
         if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
-
-            checkNumberOfPrefixes(PREFIX_ADDRESS, argMultimap);
-
-            String trimmedArgs = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()).toString().trim();
-
-            final String finalPredicateString = createPredicateString(trimmedArgs);
-
-            Predicate<Address> addressPredicate = (address -> address.toString().toLowerCase()
-                    .contains(finalPredicateString.toLowerCase()));
-
-            this.addressPredicate = addressPredicate;
+            createAddressPredicate(argMultimap);
         }
 
         Optional<Predicate<Address>> finalAddressPredicate = Optional.ofNullable(this.addressPredicate);
 
         //Check for remark prefix
         if (argMultimap.getValue(PREFIX_REMARK).isPresent()) {
-
-            checkNumberOfPrefixes(PREFIX_REMARK, argMultimap);
-
-            String trimmedArgs = ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).get()).toString().trim();
-
-            final String finalPredicateString = createPredicateString(trimmedArgs);
-
-            Predicate<Remark> remarkPredicate = (remark -> remark.toString().toLowerCase()
-                    .contains(finalPredicateString.toLowerCase()));
-
-            this.remarkPredicate = remarkPredicate;
+            createRemarkPredicate(argMultimap);
         }
 
         Optional<Predicate<Remark>> finalRemarkPredicate = Optional.ofNullable(this.remarkPredicate);
 
         //Check for tag prefix
         if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
-            Set<Tag> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
-
-            Predicate<Set<Tag>> tagPredicate = patientTags -> tagSet.stream().allMatch(inputTag -> patientTags
-                    .stream().anyMatch(patientTag -> patientTag.tagName.toLowerCase()
-                            .contains(inputTag.tagName.toLowerCase())));
-
-            this.tagPredicate = tagPredicate;
+            createTagPredicate(argMultimap);
         }
 
         Optional<Predicate<Set<Tag>>> finalTagPredicate = Optional.ofNullable(this.tagPredicate);
@@ -196,5 +134,116 @@ public class FindPatientCommandParser implements Parser<FindPatientCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     FindPatientCommand.MESSAGE_USAGE));
         }
+    }
+
+    /**
+     * Creates the name predicate.
+     * @param argMultimap The argument multimap to get the input from.
+     * @throws ParseException If the prefix is present more than once.
+     */
+    public void createNamePredicate(ArgumentMultimap argMultimap) throws ParseException {
+        checkNumberOfPrefixes(PREFIX_NAME, argMultimap);
+
+        String trimmedArgs = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()).toString().trim();
+
+        final String finalPredicateString = createPredicateString(trimmedArgs);
+
+        Predicate<Name> namePredicate = (name -> name.fullName.toLowerCase()
+                .contains(finalPredicateString.toLowerCase()));
+
+        this.namePredicate = namePredicate;
+    }
+
+    /**
+     * Creates the phone predicate.
+     * @param argMultimap The argument multimap to get the input from.
+     * @throws ParseException If the prefix is present more than once.
+     */
+    public void createPhonePredicate(ArgumentMultimap argMultimap) throws ParseException {
+        checkNumberOfPrefixes(PREFIX_PHONE, argMultimap);
+
+        String trimmedArgs = argMultimap.getValue(PREFIX_PHONE).get().trim();
+
+        if (!trimmedArgs.matches("[0-9]+")) {
+            throw new ParseException("Input for finding by phone number must be a number consisting of one or"
+                    + " more digits");
+        }
+
+        Predicate<Phone> phonePredicate = (phone -> phone.value.contains(trimmedArgs));
+
+        this.phonePredicate = phonePredicate;
+    }
+
+    /**
+     * Creates the email predicate.
+     * @param argMultimap The argument multimap to get the input from.
+     * @throws ParseException If the prefix is present more than once.
+     */
+    public void createEmailPredicate(ArgumentMultimap argMultimap) throws ParseException {
+        checkNumberOfPrefixes(PREFIX_EMAIL, argMultimap);
+
+        String trimmedArgs = argMultimap.getValue(PREFIX_EMAIL).get().trim();
+
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException("Input for finding by email should not be empty");
+        }
+
+        final String finalPredicateString = createPredicateString(trimmedArgs);
+
+        Predicate<Email> emailPredicate = (email -> email.value.toLowerCase()
+                .contains(finalPredicateString.toLowerCase()));
+
+        this.emailPredicate = emailPredicate;
+    }
+
+    /**
+     * Creates the address predicate.
+     * @param argMultimap The argument multimap to get the input from.
+     * @throws ParseException If the prefix is present more than once.
+     */
+    public void createAddressPredicate(ArgumentMultimap argMultimap) throws ParseException {
+        checkNumberOfPrefixes(PREFIX_ADDRESS, argMultimap);
+
+        String trimmedArgs = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()).toString().trim();
+
+        final String finalPredicateString = createPredicateString(trimmedArgs);
+
+        Predicate<Address> addressPredicate = (address -> address.toString().toLowerCase()
+                .contains(finalPredicateString.toLowerCase()));
+
+        this.addressPredicate = addressPredicate;
+    }
+
+    /**
+     * Creates the remark predicate.
+     * @param argMultimap The argument multimap to get the input from.
+     * @throws ParseException If the prefix is present more than once.
+     */
+    public void createRemarkPredicate(ArgumentMultimap argMultimap) throws ParseException {
+        checkNumberOfPrefixes(PREFIX_REMARK, argMultimap);
+
+        String trimmedArgs = ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).get()).toString().trim();
+
+        final String finalPredicateString = createPredicateString(trimmedArgs);
+
+        Predicate<Remark> remarkPredicate = (remark -> remark.toString().toLowerCase()
+                .contains(finalPredicateString.toLowerCase()));
+
+        this.remarkPredicate = remarkPredicate;
+    }
+
+    /**
+     * Creates the tag predicate.
+     * @param argMultimap The argument multimap to get the input from.
+     * @throws ParseException If the prefix is present more than once.
+     */
+    public void createTagPredicate(ArgumentMultimap argMultimap) throws ParseException {
+        Set<Tag> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        Predicate<Set<Tag>> tagPredicate = patientTags -> tagSet.stream().allMatch(inputTag -> patientTags
+                .stream().anyMatch(patientTag -> patientTag.tagName.toLowerCase()
+                        .contains(inputTag.tagName.toLowerCase())));
+
+        this.tagPredicate = tagPredicate;
     }
 }
