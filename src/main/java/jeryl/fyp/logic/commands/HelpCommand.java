5669877de2d264cf9d15fc627e6503c8418c93b1
LAPTOP-Q59MTK9C\Pokezard <e0538156@u.nus.edu>
2022-11-07 20:24:09 +0800

    white space error

--- src/main/java/jeryl/fyp/logic/commands/HelpCommand.java
+++ src/main/java/jeryl/fyp/logic/commands/HelpCommand.java
@@ -47,7 +47,7 @@ public class HelpCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model){
+    public CommandResult execute(Model model) {
         return new CommandResult(helpMessage, showHelp, false, false , false);
     }
 
