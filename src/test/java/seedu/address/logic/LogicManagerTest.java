b9e0a21bd1b40016bbda721a4808ec58f4e87ba5
Zsigmond <e0725064@u.nus.edu>
2022-11-07 21:15:57 +0800

    Remove commented out code

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -3,7 +3,15 @@ package seedu.address.logic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_BUYER_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.ADDRESS_DESC_AMY;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.DESIRED_CHARACTERISTICS_DESC_AMY;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.EMAIL_DESC_AMY;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.NAME_DESC_AMY;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.PHONE_DESC_AMY;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.PRICE_RANGE_DESC_AMY;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.TAG_DESC_PRIORITY_HIGH;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalBuyers.AMY;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -13,16 +21,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.buyer.AddBuyerCommand;
+import seedu.address.logic.commands.buyer.ListBuyersCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyBuyerBook;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.buyer.Buyer;
 import seedu.address.storage.JsonBuyerBookStorage;
 import seedu.address.storage.JsonPropertyBookStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
 import seedu.address.storage.StorageManager;
+import seedu.address.testutil.BuyerBuilder;
 
 public class LogicManagerTest {
     private static final IOException DUMMY_IO_EXCEPTION = new IOException("dummy exception");
@@ -56,34 +68,34 @@ public class LogicManagerTest {
         assertCommandException(deleteBuyerCommand, MESSAGE_INVALID_BUYER_DISPLAYED_INDEX);
     }
 
-    //    @Test
-    //    public void execute_validCommand_success() throws Exception {
-    //        String listCommand = ListBuyersCommand.COMMAND_WORD;
-    //        assertCommandSuccess(listCommand, ListBuyersCommand.MESSAGE_SUCCESS, model);
-    //    }
+    @Test
+    public void execute_validCommand_success() throws Exception {
+        String listCommand = ListBuyersCommand.COMMAND_WORD;
+        assertCommandSuccess(listCommand, ListBuyersCommand.MESSAGE_SUCCESS, model);
+    }
 
-    // @Test
-    // public void execute_storageThrowsIoException_throwsCommandException() {
-    //     // Setup LogicManager with JsonBuyerBookIoExceptionThrowingStub
-    //     JsonBuyerBookStorage personBookStorage =
-    //             new JsonBuyerBookIoExceptionThrowingStub(temporaryFolder.resolve("ioExceptionPersonBook.json"));
-    //     JsonUserPrefsStorage userPrefsStorage =
-    //             new JsonUserPrefsStorage(temporaryFolder.resolve("ioExceptionUserPrefs.json"));
-    //     JsonPropertyBookStorage propertyBookStorage =
-    //             new JsonPropertyBookStorage(temporaryFolder.resolve("ioExceptionPropertyBook.json"));
-    //     StorageManager storage = new StorageManager(personBookStorage, propertyBookStorage, userPrefsStorage);
-    //     logic = new LogicManager(model, storage);
+    @Test
+    public void execute_storageThrowsIoException_throwsCommandException() {
+        // Setup LogicManager with JsonBuyerBookIoExceptionThrowingStub
+        JsonBuyerBookStorage personBookStorage =
+                new JsonBuyerBookIoExceptionThrowingStub(temporaryFolder.resolve("ioExceptionPersonBook.json"));
+        JsonUserPrefsStorage userPrefsStorage =
+                new JsonUserPrefsStorage(temporaryFolder.resolve("ioExceptionUserPrefs.json"));
+        JsonPropertyBookStorage propertyBookStorage =
+                new JsonPropertyBookStorage(temporaryFolder.resolve("ioExceptionPropertyBook.json"));
+        StorageManager storage = new StorageManager(personBookStorage, propertyBookStorage, userPrefsStorage);
+        logic = new LogicManager(model, storage);
 
-    //     // Execute add command
-    //     String addCommand = AddBuyerCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY
-    //             + ADDRESS_DESC_AMY + PRICE_RANGE_DESC_AMY + DESIRED_CHARACTERISTICS_DESC_AMY
-    //             + TAG_DESC_PRIORITY_HIGH;
-    //     Buyer expectedBuyer = new BuyerBuilder(AMY).build();
-    //     ModelManager expectedModel = new ModelManager();
-    //     expectedModel.addPerson(expectedBuyer);
-    //     String expectedMessage = LogicManager.FILE_OPS_ERROR_MESSAGE + DUMMY_IO_EXCEPTION;
-    //     assertCommandFailure(addCommand, CommandException.class, expectedMessage, expectedModel);
-    // }
+        // Execute add command
+        String addCommand = AddBuyerCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY
+                + ADDRESS_DESC_AMY + PRICE_RANGE_DESC_AMY + DESIRED_CHARACTERISTICS_DESC_AMY
+                + TAG_DESC_PRIORITY_HIGH;
+        Buyer expectedBuyer = new BuyerBuilder(AMY).build();
+        ModelManager expectedModel = new ModelManager();
+        expectedModel.addBuyer(expectedBuyer);
+        String expectedMessage = LogicManager.FILE_OPS_ERROR_MESSAGE + DUMMY_IO_EXCEPTION;
+        assertCommandFailure(addCommand, CommandException.class, expectedMessage, expectedModel);
+    }
 
     @Test
     public void getFilteredBuyerList_modifyList_throwsUnsupportedOperationException() {
@@ -98,7 +110,7 @@ public class LogicManagerTest {
      * @see #assertCommandFailure(String, Class, String, Model)
      */
     private void assertCommandSuccess(String inputCommand, String expectedMessage,
-            Model expectedModel) throws CommandException, ParseException {
+                                      Model expectedModel) throws CommandException, ParseException {
         CommandResult result = logic.execute(inputCommand);
         assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(expectedModel, model);
@@ -125,7 +137,7 @@ public class LogicManagerTest {
      * @see #assertCommandFailure(String, Class, String, Model)
      */
     private void assertCommandFailure(String inputCommand, Class<? extends Throwable> expectedException,
-            String expectedMessage) {
+                                      String expectedMessage) {
         Model expectedModel = new ModelManager(model.getBuyerBook(), model.getPropertyBook(), new UserPrefs());
         assertCommandFailure(inputCommand, expectedException, expectedMessage, expectedModel);
     }
@@ -138,7 +150,7 @@ public class LogicManagerTest {
      * @see #assertCommandSuccess(String, String, Model)
      */
     private void assertCommandFailure(String inputCommand, Class<? extends Throwable> expectedException,
-            String expectedMessage, Model expectedModel) {
+                                      String expectedMessage, Model expectedModel) {
         assertThrows(expectedException, expectedMessage, () -> logic.execute(inputCommand));
         assertEquals(expectedModel, model);
     }
