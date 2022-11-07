9efed56d0dc8ed8e9b85c12200f4e7e80fdcc0ef
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2022-11-07 20:50:16 +0800

    Add 100 character limit to addresses

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -27,11 +27,13 @@ public class AddressTest {
         // invalid addresses
         // note: the address parser will never return this string
         assertFalse(Address.isValidAddress(" ")); // spaces only
+        assertFalse(Address.isValidAddress("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); // 101 characters long
 
         // valid addresses
         assertTrue(Address.isValidAddress("")); // empty string
         assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
         assertTrue(Address.isValidAddress("-")); // one character
         assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
+        assertTrue(Address.isValidAddress("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); // 100 characters long
     }
 }
