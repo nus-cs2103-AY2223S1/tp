82a7133c8efcef656d424942239d78adbe4e80fa
CeereeC <myself3216@gmail.com>
2022-11-07 13:49:07 +0800

    Fix checkstyle

--- src/test/java/seedu/travelr/logic/LogicManagerTest.java
+++ src/test/java/seedu/travelr/logic/LogicManagerTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
 import seedu.travelr.commons.core.GuiSettings;
 import seedu.travelr.logic.commands.CommandResult;
 import seedu.travelr.logic.commands.ListCommand;
