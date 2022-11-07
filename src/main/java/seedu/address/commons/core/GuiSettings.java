56312f9741e8d8ca17f0495d5ec0e160c1cedd02
Rui Han <richardcrh2000@gmail.com>
2022-11-07 20:44:09 +0800

    GUISetting Starting Screen Size Change
    Change starting width to 1024 and height to 640, which is the minimum window size

--- src/main/java/seedu/address/commons/core/GuiSettings.java
+++ src/main/java/seedu/address/commons/core/GuiSettings.java
@@ -10,8 +10,8 @@ import java.util.Objects;
  */
 public class GuiSettings implements Serializable {
 
-    private static final double DEFAULT_HEIGHT = 600;
-    private static final double DEFAULT_WIDTH = 740;
+    private static final double DEFAULT_HEIGHT = 640;
+    private static final double DEFAULT_WIDTH = 1024;
 
     private final double windowWidth;
     private final double windowHeight;
