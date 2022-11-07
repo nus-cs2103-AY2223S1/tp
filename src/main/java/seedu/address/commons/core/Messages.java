558a98f35ae8f23fbbd2197616aa21fa05051ce3
chloeelim <chloelim22@gmail.com>
2022-11-07 22:58:25 +0800

    fix: punctuation

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -6,7 +6,7 @@ package seedu.address.commons.core;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command.";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s.";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_CUSTOMER_DISPLAYED_INDEX = "The customer index provided is invalid.";
     public static final String MESSAGE_CUSTOMERS_LISTED_OVERVIEW = "%1$d customers listed!";
     public static final String MESSAGE_NO_ACTIVE_CUSTOMER = "This command requires you to open a customer first!";
@@ -20,7 +20,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMISSION_DISPLAYED_INDEX = "The commission index provided is invalid.";
     public static final String MESSAGE_NO_ACTIVE_COMMISSION = "This command requires you to open a commission first!";
     public static final String MESSAGE_INVALID_COMMISSION_DEADLINE = "The commission deadline provided is invalid. "
-            + "The input format should be YYYY-MM-DD";
+            + "The input format should be YYYY-MM-DD.";
     public static final String MESSAGE_INVALID_COMMISSION_FEE = "The commission fee provided is invalid.";
 
     /* Iteration Messages */
@@ -29,7 +29,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_ITERATION_DISPLAYED_INDEX = "The iteration index provided is invalid.";
     public static final String MESSAGE_NONEXISTENT_IMAGE_PATH = "The image path provided does not exist.";
     public static final String MESSAGE_NOT_AN_IMAGE = "The file at the given path is not an image.";
-    public static final String MESSAGE_INVALID_PATH = "The path provided was invalid: %1$s.";
+    public static final String MESSAGE_INVALID_PATH = "The path provided was invalid: %1$s";
 
     public static final String MESSAGE_KEYWORD_EMPTY = "Keywords should not be blank or empty.";
 
