60587208884d734fd6f44bb692228b0c330c12d4
L1uY1jun <yijunlau@gmail.com>
2022-11-07 16:04:03 +0800

    Update sample data for TrackAScholar

--- src/main/java/seedu/trackascholar/model/util/SampleDataUtil.java
+++ src/main/java/seedu/trackascholar/model/util/SampleDataUtil.java
@@ -6,12 +6,7 @@ import java.util.stream.Collectors;
 
 import seedu.trackascholar.model.ReadOnlyTrackAScholar;
 import seedu.trackascholar.model.TrackAScholar;
-import seedu.trackascholar.model.applicant.Applicant;
-import seedu.trackascholar.model.applicant.ApplicationStatus;
-import seedu.trackascholar.model.applicant.Email;
-import seedu.trackascholar.model.applicant.Name;
-import seedu.trackascholar.model.applicant.Phone;
-import seedu.trackascholar.model.applicant.Scholarship;
+import seedu.trackascholar.model.applicant.*;
 import seedu.trackascholar.model.major.Major;
 
 /**
@@ -22,7 +17,7 @@ public class SampleDataUtil {
         return new Applicant[] {
             new Applicant(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Scholarship("NUS Global Merit Scholarship"), new ApplicationStatus("pending"),
-                getMajorSet("Medicine")),
+                getMajorSet("Medicine"), new Pin(true)),
             new Applicant(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Scholarship("NUS Merit Scholarship"),
                 new ApplicationStatus("pending"), getMajorSet("Business", "Accountancy")),
@@ -31,7 +26,7 @@ public class SampleDataUtil {
                 new ApplicationStatus("accepted"), getMajorSet()),
             new Applicant(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Scholarship("NUS Performing & Visual Arts Scholarship"),
-                new ApplicationStatus("rejected"), getMajorSet("Psychology")),
+                new ApplicationStatus("rejected"), getMajorSet("Psychology"), new Pin(true)),
             new Applicant(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Scholarship("NUS Merit Scholarship"), new ApplicationStatus("pending"),
                 getMajorSet("Software Engineering")),
