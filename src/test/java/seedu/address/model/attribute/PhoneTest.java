76dc9480b9acc9ea1e9000ba7555a23fabf6de5a
Jason Christopher <jason.christopher@u.nus.edu>
2022-11-07 12:56:48 +0800

    Fix Checkstyle Issues

--- src/test/java/seedu/address/model/attribute/PhoneTest.java
+++ src/test/java/seedu/address/model/attribute/PhoneTest.java
@@ -6,8 +6,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.attribute.Phone;
-
 public class PhoneTest {
 
     @Test
