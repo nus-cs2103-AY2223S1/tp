729bf5b7d8744f1b08c14286ed5f483ddebd5249
Tan Yuan Wei <tyw2811@gmail.com>
2022-11-07 21:32:16 +0800

    Fix bug with duplicate tags allowed

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -10,6 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Tag {
 
     public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS_DUPLICATE = "Tags names should not be the same (case-insensitive)";
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String tagName;
